f0e32b688a2765afcd81a9331f624a9fd5b649b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package wistiapy/0.0.5/wistiapy-0.0.5-py3-none-any.whl

---
+++ packages/wistiapy/wistiapy-0.0.5-py3-none-any.whl/tests/test_media.py
@@ -0,0 +1,21 @@
+import logging
+from os import path
+
+import pytest
+import json
+
+from wistia.schema import Media
+
+log = logging.getLogger("test_wistia")
+
+
+@pytest.fixture
+def media_list():
+    json_string = open(path.join(path.dirname(__file__), "test_media.json")).read()
+    return json.loads(json_string)
+
+
+def test_media_parsing(media_list):
+    medias = [Media(media_data) for media_data in media_list]
+    assert len(medias) == 2
+    assert len(medias[0].assets) == 3
---
+++ packages/wistiapy/wistiapy-0.0.5-py3-none-any.whl/wistia/__init__.py
@@ -0,0 +1,5 @@
+from .__version__ import __version__
+
+from .client import WistiaClient
+from .dummy import DummyWistiaClient
+from .helpers import get_wistia_client
