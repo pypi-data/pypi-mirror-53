bbb9e4dda91521a1e0689e015fa0684da166434a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:17 +0000

    Add package ArtNetRouter/1.0.3/ArtNetRouter-1.0.3-py3-none-any.whl

---
+++ packages/ArtNetRouter/ArtNetRouter-1.0.3-py3-none-any.whl/ArtNetRouter/__init__.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+"""
+    __init__.py
+
+    Copyright (c) 2018 Balus CO., LTD.
+    Coder: T. Shinaji 
+    
+    2018/11/01
+
+"""
+from __future__ import absolute_import
+from .core import ArtNetRouter
+__version__ = "1.0.3"
