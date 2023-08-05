d118f976bec8fe08a23d5ad76afe85cc9dc6b45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package ssutils/0.4.3/ssutils-0.4.3.tar.gz

---
+++ packages/ssutils/ssutils-0.4.3.tar.gz/ssutils-0.4.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='ssutils',
+	version='0.4.3',
+	author="S Satapathy",
+	author_email="shubhakant.satapathy@gmail.com",
+	description="collection of useful python functions",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/satapathy/pypi-ssutils",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		'simple_salesforce',
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+ )
