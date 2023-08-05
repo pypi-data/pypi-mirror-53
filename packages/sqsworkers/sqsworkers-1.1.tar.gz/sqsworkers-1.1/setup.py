51d8d79d1169317bcbe90273e8c8206093d974ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:35 +0000

    Add package sqsworkers/1.1/sqsworkers-1.1.tar.gz

---
+++ packages/sqsworkers/sqsworkers-1.1.tar.gz/sqsworkers-1.1/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = '1.1'
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='sqsworkers',
+    version=VERSION,
+    packages=find_packages(exclude=('tests')),
+    license='Apache License 2.0',
+    long_description=read('README.rst'),
+    url='https://github.com/goodmanship/sqsworkers',
+    author='Rio Goodman',
+    author_email='riogoodman@gmail.com',
+    install_requires=[]
+)
