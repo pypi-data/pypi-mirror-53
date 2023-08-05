5f72d4a431c74c35ec7168a8c00835da64ff6b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:16 +0000

    Add package rfoauthenticator/3.8/rfoauthenticator-3.8.tar.gz

---
+++ packages/rfoauthenticator/rfoauthenticator-3.8.tar.gz/rfoauthenticator-3.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="rfoauthenticator",
+    version="3.8",
+    description="oauthenticator custom auth0 authentication",
+    author="refactored",
+    author_email="info@refactored.ai",
+    install_requires=["oauthenticator", "requests"],
+    packages=["rfoauthenticator"],
+    zip_safe=False,
+)
