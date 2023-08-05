bcb0ca77bce3c6a2fed6654ede1f3b74dbcec830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:32 +0000

    Add package amici/0.10.12/amici-0.10.12.tar.gz

---
+++ packages/amici/amici-0.10.12.tar.gz/amici-0.10.12/amici/include/amici/amici.h
@@ -0,0 +1,79 @@
+#ifndef amici_h
+#define amici_h
+
+#include "amici/cblas.h"
+#include "amici/defines.h"
+#include "amici/edata.h"
+#include "amici/exception.h"
+#include "amici/model.h"
+#include "amici/rdata.h"
+#include "amici/solver.h"
+#include "amici/symbolic_functions.h"
+
+namespace amici {
+
+/*!
+ * @brief printErrMsgIdAndTxt prints a specified error message associated to the
+ * specified identifier
+ *
+ * @param[in] identifier error identifier @type char
+ * @param[in] format string with error message printf-style format
+ * @param ... arguments to be formatted
+ */
+void
+printErrMsgIdAndTxt(const char* identifier, const char* format, ...);
+
+/*!
+ * @brief printErrMsgIdAndTxt prints a specified warning message associated to
+ * the specified identifier
+ *
+ * @param[in] identifier warning identifier @type char
+ * @param[in] format string with error message printf-style format
+ * @param ... arguments to be formatted
+ */
+void
+printWarnMsgIdAndTxt(const char* identifier, const char* format, ...);
+
+/** errMsgIdAndTxt is a function pointer for printErrMsgIdAndTxt  */
+extern msgIdAndTxtFp errMsgIdAndTxt;
+
+/** warnMsgIdAndTxt is a function pointer for printWarnMsgIdAndTxt  */
+extern msgIdAndTxtFp warnMsgIdAndTxt;
+
+/*!
+ * runAmiciSimulation is the core integration routine. It initializes the solver
+ * and runs the forward and backward problem.
+ *
+ * @param solver Solver instance
+ * @param edata pointer to experimental data object
+ * @param model model specification object
+ * @param rethrow rethrow integration exceptions?
+ * @return rdata pointer to return data object
+ */
+std::unique_ptr<ReturnData>
+runAmiciSimulation(Solver& solver,
+                   const ExpData* edata,
+                   Model& model,
+                   bool rethrow = false);
+
+/*!
+ * runAmiciSimulations does the same as runAmiciSimulation, but for multiple
+ * ExpData instances.
+ *
+ * @param solver Solver instance
+ * @param edatas experimental data objects
+ * @param model model specification object
+ * @param failfast flag to allow early termination
+ * @param num_threads number of threads for parallel execution
+ * @return vector of pointers to return data objects
+ */
+std::vector<std::unique_ptr<ReturnData>>
+runAmiciSimulations(Solver const& solver,
+                    const std::vector<ExpData*>& edatas,
+                    Model const& model,
+                    bool failfast,
+                    int num_threads);
+
+} // namespace amici
+
+#endif /* amici_h */
---
+++ packages/amici/amici-0.10.12.tar.gz/amici-0.10.12/amici/include/amici/misc.h
@@ -0,0 +1,102 @@
+#ifndef AMICI_MISC_H
+#define AMICI_MISC_H
+
+#include "amici/defines.h"
+#include <sunmatrix/sunmatrix_sparse.h> // SUNMatrixContent_Sparse
+
+#include <algorithm>
+#include <vector>
+#include <memory>
+
+#include <gsl/gsl-lite.hpp>
+
+namespace amici {
+
+/**
+ * @brief creates a slice from existing data
+ *
+ * @param data to be sliced
+ * @param index slice index
+ * @param size slice size
+ * @return span of the slice
+ */
+
+ gsl::span<realtype> slice(std::vector<realtype> &data, int index,
+                           unsigned size);
+
+/**
+ * @brief Checks the values in an array for NaNs and Infs
+ *
+ * @param array array
+ * @param fun name of calling function
+ * @return AMICI_RECOVERABLE_ERROR if a NaN/Inf value was found, AMICI_SUCCESS otherwise
+ */
+int checkFinite(gsl::span<const realtype> array, const char* fun);
+
+
+/**
+  * @brief Remove parameter scaling according to the parameter scaling in pscale
+  *
+  * All vectors must be of same length.
+  *
+  * @param bufferScaled scaled parameters
+  * @param pscale parameter scaling
+  * @param bufferUnscaled unscaled parameters are written to the array
+  */
+void unscaleParameters(gsl::span<const realtype> bufferScaled,
+                       gsl::span<const ParameterScaling> pscale,
+                       gsl::span<realtype> bufferUnscaled);
+
+/**
+  * @brief Remove parameter scaling according to `scaling`
+  *
+  * @param scaledParameter scaled parameter
+  * @param scaling parameter scaling
+  *
+  * @return Unscaled parameter
+  */
+double getUnscaledParameter(double scaledParameter, ParameterScaling scaling);
+
+
+/**
+ * @brief Apply parameter scaling according to `scaling`
+ * @param unscaledParameter
+ * @param scaling parameter scaling
+ * @return Scaled parameter
+ */
+double getScaledParameter(double unscaledParameter, ParameterScaling scaling);
+
+
+/**
+ * @brief Apply parameter scaling according to `scaling`
+ * @param bufferUnscaled
+ * @param pscale parameter scaling
+ * @param bufferScaled destination
+ */
+void scaleParameters(gsl::span<const realtype> bufferUnscaled,
+                     gsl::span<const ParameterScaling> pscale,
+                     gsl::span<realtype> bufferScaled);
+
+/**
+ * @brief Returns the current backtrace as std::string
+ * @param maxFrames Number of frames to include
+ * @return Backtrace
+ */
+std::string backtraceString(int maxFrames);
+
+
+} // namespace amici
+
+#ifndef __cpp_lib_make_unique
+// custom make_unique while we are still using c++11
+namespace std {
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+}
+#endif
+
+#endif // AMICI_MISC_H
+
