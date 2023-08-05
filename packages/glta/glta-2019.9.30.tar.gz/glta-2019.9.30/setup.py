c4ad35fe28297ea1ace378bad1c0f5ad3c61aefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:21 +0000

    Add package glta/2019.9.30/glta-2019.9.30.tar.gz

---
+++ packages/glta/glta-2019.9.30.tar.gz/glta-2019.9.30/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    cffi_modules="glta/process_plink/_build.py:ffi",
+    name='glta',
+    version='2019.9.30',
+    description='Genomic Longitudinal Trait Analysis Tool',
+    long_description=open('README.rst').read(),
+    author='Chao Ning',
+    author_email='ningchao91@gmail.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url="https://github.com/chaoning/GLTA",
+    include_package_data = True,
+    install_requires=[
+        'numpy>=1.17.2',
+        'pandas>=0.25.1',
+        'scipy>=1.3.1',
+        'cffi>=1.12.3',
+        'pandas_plink>=2.0.1',
+    ],
+
+)
+
+
