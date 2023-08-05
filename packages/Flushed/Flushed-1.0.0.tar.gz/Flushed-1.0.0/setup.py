5c3024030c9dbeab976b082146d07ced61d52825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package Flushed/1.0.0/Flushed-1.0.0.tar.gz

---
+++ packages/Flushed/Flushed-1.0.0.tar.gz/Flushed-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Flushed',
+   version='1.0.0',
+   description='Library to work with in memory buffers and caches',
+   author='Alan Bacon',
+   author_email='alan@bacontowers.co.uk',
+   packages=['Flushed'],
+   install_requires=[]
+)
