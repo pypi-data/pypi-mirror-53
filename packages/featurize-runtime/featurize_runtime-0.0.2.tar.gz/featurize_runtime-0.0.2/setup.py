8246099dd877d1f9b033e70ff7a4729f6502dfd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:13 +0000

    Add package featurize-runtime/0.0.2/featurize_runtime-0.0.2.tar.gz

---
+++ packages/featurize-runtime/featurize_runtime-0.0.2.tar.gz/featurize_runtime-0.0.2/setup.py
@@ -0,0 +1,26 @@
+"""
+Setup Module to setup Python Handlers (Git Handlers) for the Git Plugin.
+"""
+import os
+import setuptools
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')) as f:
+    install_requires = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='featurize_runtime',
+    version='0.0.2',
+    author='',
+    description="Featurize runtime",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    package_data={'featurize_runtime': ['*']},
+    entry_points = {
+        'console_scripts': ['featurize=featurize_runtime:cli'],
+    }
+)
