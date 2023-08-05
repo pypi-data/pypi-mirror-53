8fa8eafc62e9bbfd908216c27ef73c3ec3d8752b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:15 +0000

    Add package winpwn/0.0.7/winpwn-0.0.7-py2.py3-none-any.whl

---
+++ packages/winpwn/winpwn-0.0.7-py2.py3-none-any.whl/winpwn/__init__.py
@@ -0,0 +1,22 @@
+import os
+import sys
+cwd = os.path.dirname(__file__)
+sys.path.append(cwd)
+
+from .winpwn import process,remote
+from .dbg import dbg,gdb,windbg,x64dbg
+from .context import context
+from .misc import p16,p32,p64,u16,u32,u64,parse,pause
+
+from var import init_var
+init_var()
+
+__all__=['process','remote','dbg','gdb','windbg','x64dbg','context',
+    'p16','p32','p64','u16','u32','u64','parse','pause'
+]
+
+
+# winpwn  dbg  misc
+# |
+# |
+# win    context
---
+++ packages/winpwn/winpwn-0.0.7-py2.py3-none-any.whl/winpwn/context.py
@@ -0,0 +1,10 @@
+class context():
+    debugger='gdb'
+    arch='i386'
+    endian='little'
+    log_level=""
+    terminal=[]
+    timeout=512
+    tick=0.0625
+    length=None
+    newline='\r\n'
