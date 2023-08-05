d9e4e05c7f8f80cea083f0cf31a5cce5c45bf672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package taboo/0.8.6/taboo-0.8.6.tar.gz

---
+++ packages/taboo/taboo-0.8.6.tar.gz/taboo-0.8.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="taboo",
+    version="0.8.6",
+    author="Zhong Haoxi",
+    author_email="zhonghaoxi@yeah.net",
+    description="Codes for taboo.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haoxizhong/taboo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
---
+++ packages/taboo/taboo-0.8.6.tar.gz/taboo-0.8.6/taboo/formatter.py
@@ -0,0 +1,6 @@
+class TabooStupidFormatter:
+    def __init__(self):
+        pass
+
+    def format(self, sent):
+        return sent.strip()
