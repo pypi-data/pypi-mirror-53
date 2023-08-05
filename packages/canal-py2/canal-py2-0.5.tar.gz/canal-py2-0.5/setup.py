0cf7bc7d23deb668cacbe4fed722fa89acebfbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:02:54 +0000

    Add package canal-py2/0.5/canal-py2-0.5.tar.gz

---
+++ packages/canal-py2/canal-py2-0.5.tar.gz/canal-py2-0.5/setup.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="canal-py2",
+    version="0.5",
+    author="magic",
+    author_email="guanpeixiang@foxmail.com",
+    description="A canal-cilent for python2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Guanpx/canal-python',
+    # packages=["canal", "canal/protocol"],
+    packages=setuptools.find_packages(),
+)
