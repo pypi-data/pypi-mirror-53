d3c2e6692ba0653eaa03ef0faade4698e12eeb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:11 +0000

    Add package soupstars/2.10.3/soupstars-2.10.3-py3-none-any.whl

---
+++ packages/soupstars/soupstars-2.10.3-py3-none-any.whl/soupstars/api.py
@@ -0,0 +1,43 @@
+"""
+API
+===
+
+Entrypoints for some of the functionality exposed by the package
+"""
+
+from .runners import Runner
+from .loaders import Loader
+from .parsers import BeautifulSoupStar, Url
+
+
+def run(parser, **kwargs):
+    """
+    Executes a parser, returning the data from the run
+    """
+
+    return Runner(parser, **kwargs).run()
+
+
+# I think we should rename the associated methods on the loader and parser
+def fetch(url) -> BeautifulSoupStar:
+    """
+    Downloads a url and converts it into a BeautifulSoupStars object
+    """
+
+    loader = Loader()
+    load = loader.load(url)
+    soup = BeautifulSoupStar(load)
+    return soup
+
+
+# I think we should rename the associated methods on the loader and parser
+def parse(parser, url):
+    """
+    Download a url and process it with a parser, returning the data and links
+    discovered by the parsers
+    """
+
+    loader = Loader()
+    load = loader.load(url)
+    data, links = parser.parse(load)
+    return data, links
