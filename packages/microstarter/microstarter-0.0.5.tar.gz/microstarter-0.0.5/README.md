382268b8153ddb40a6517fed2b9bafe5fcb31863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:29 +0000

    Add package microstarter/0.0.5/microstarter-0.0.5.tar.gz

---
+++ packages/microstarter/microstarter-0.0.5.tar.gz/microstarter-0.0.5/README.md
@@ -0,0 +1,27 @@
+# microstarter
+
+Microservice simple Starter Flask example lightweight app
+
+# Getting Started
+
+Install microstarter
+Linux Installation
+```bash
+sudo pip3 install microstarter
+```
+
+### Usage example
+
+```bash
+microstarter folder_name
+
+cd folder_name/
+
+source .venv/bin/activate
+
+python run.py
+```
+
+# Contributing
+Fell free to open issue and send pull request.
+
---
+++ packages/microstarter/microstarter-0.0.5.tar.gz/microstarter-0.0.5/startmicro/conf/app/api.py
@@ -0,0 +1,65 @@
+producer_restful = """
+from flask import Blueprint, request, jsonify
+
+from app.utils import Success
+from app.utils.logger import log
+
+micro_api = Blueprint('micro_api', __name__)
+
+
+@micro_api.route('/', methods=['GET'])
+def handler():
+    # Todo base algorithm here
+    log.debug("Success")
+    success = Success(
+        status=200,
+        description="It is work!",
+        message={
+            "data": "Success"
+        }
+    )
+    return jsonify(success.serialize())
+
+"""
+
+producer_redis = """
+from app.utils import Success
+from app.utils.logger import log
+
+
+def handler(request):
+    # Todo base algorithm here
+    print("Requested data dictionary", request)
+    log.debug("Success RPC data")
+    success = Success(
+        status=200,
+        description="It is work!",
+        message={
+            "data": request
+        }
+    )
+    return success.serialize() # response to client 
+
+"""
+
+producer_rabbitmq = """
+import json
+from app.utils import Success
+from app.utils.logger import log
+
+
+def handler(request):
+    # Todo base algorithm here
+    data = json.loads(request)
+    print("Requested data dictionary", data)
+    log.debug("Success RPC Rabbit mq data")
+    success = Success(
+        status=200,
+        description="It is work!",
+        message={
+            "data": data
+        }
+    )
+    return success.serialize() # response to client 
+
+"""
