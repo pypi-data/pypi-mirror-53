a9b1624af70064900294f5a33fcd569ad7450126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:10 +0000

    Add package django-saml2-auth-signout-slo/0.0.10/django_saml2_auth_signout_slo-0.0.10.tar.gz

---
+++ packages/django-saml2-auth-signout-slo/django_saml2_auth_signout_slo-0.0.10.tar.gz/django_saml2_auth_signout_slo-0.0.10/django_saml2_auth_signout_slo/apps.py
@@ -0,0 +1,24 @@
+from django import apps
+
+
+class AppConfig(apps.AppConfig):
+    name = 'django_saml2_auth_signout_slo'
+
+    def ready(self):
+        # import plugins
+        # noinspection PyUnresolvedReferences
+        from . import plugins
+        # must import in the ready function to ensure app is loaded
+        from django.conf import settings
+        from django_saml2_auth import signals, handlers
+        from django.core.exceptions import ImproperlyConfigured
+
+        if 'SLO' in settings.SAML2_AUTH.get('PLUGINS', {}).get('SIGNOUT', []):
+            # NameID is required
+            if settings.SAML2_AUTH.get('NAME_ID_FORMAT', 'NOT SET') in [None, 'None']:
+                raise ImproperlyConfigured(
+                    "django-saml2-auth-signout-slo requires a valid SAML2_AUTH.NAME_ID_FORMAT, not None"
+                )
+
+            # cache the NameID in the session
+            signals.before_authenticated.connect(handlers.store_name_id)
