fa8ce2f15702cebdcdecd174df4866013ea6a6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:11 +0000

    Add package shy-tools/0.0.8/shy_tools-0.0.8-py3-none-any.whl

---
+++ packages/shy-tools/shy_tools-0.0.8-py3-none-any.whl/shy_tools/base_utils.py
@@ -0,0 +1,173 @@
+# coding=utf-8
+'''
+工具.
+'''
+import os
+import pickle
+import warnings
+import time
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+from itertools import combinations
+from sklearn.utils.class_weight import compute_sample_weight
+from sklearn.metrics import log_loss
+from hashlib import md5
+
+
+warnings.filterwarnings('ignore')
+
+
+def save(path='smileshy.pkl', obj=None):
+    prefix = '/'.join(path.split('/')[: -1])
+    if len(prefix) == 0:
+        pass
+    else:
+        mkdir(prefix)
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f)
+        f.close()
+    print('保存%s完毕!' % path)
+
+
+def load(path='smileshy.pkl'):
+    with open(path, 'rb') as f:
+        obj = pickle.load(f)
+        f.close()
+    print('加载%s完毕!' % path)
+    return obj
+
+
+def mkdir(path='./path'):
+    '''
+    根据给定的路径创建目录.
+    '''
+    os.makedirs(path.encode('utf-8'), exist_ok=True)
+
+
+def timer(func):
+    '''
+    计算时间的装饰器.
+    '''
+
+    def wraper(*args, **kwargs):
+        start_time = time.time()
+        print('=' * 40)
+        print('%s开始执行' % func.__doc__)
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        time_long = end_time - start_time
+        if time_long < 3600:
+            print('%s执行完毕, 花费时间为: %f 分钟.' % (func.__name__, round(time_long / 60, 2)))
+        else:
+            print('%s执行完毕, 花费时间为: %f 小时.' % (func.__name__, round(time_long / 3600, 2)))
+        print('=' * 40)
+        return result
+    return wraper
+
+
+def random_num_generator(n=1000, random_state=777):
+    '''
+    随机数生成器, 范围0-1.
+    '''
+    np.random.seed(random_state)
+    for i in range(n):
+        yield np.random.rand()
+
+
+def make_miss_value(df=None, label='target', row_perc=0.2, col_perc=0.8, random_state=7):
+    '''
+    随机将原数据中部分数据设置为NaN.
+    '''
+    ft_list = df.columns.tolist()
+    if label in ft_list:
+        ft_list.remove(label)
+    np.random.seed(7)
+    np.random.shuffle(ft_list)
+    ft_miss_list = []
+    for i in range(int(len(ft_list) * col_perc)):
+        ft_miss_list.append(ft_list.pop())
+    for i, ft in enumerate(ft_miss_list):
+        rand_num = random_num_generator(n=df.shape[0], random_state=i)
+        df[ft] = [np.nan if rand_num.__next__() < row_perc else x for x in df[ft]]
+
+
+def split_X_y(df=None, label='target'):
+    if label in df.columns.tolist():
+        return df.drop(label, axis=1).reset_index(drop=True), df[label].values
+    else:
+        print('数据集中没有目标列.')
+
+
+def rank_sort(data):
+    len_data = len(data)
+    sort_data = sorted(data)
+    rank_data = list(range(1, len_data + 1))
+    map_dict = defaultdict(float)
+    count_dict = defaultdict(int)
+    for i, d in enumerate(sort_data):
+        map_dict[d] += rank_data[i]
+        count_dict[d] += 1
+    for k in count_dict:
+        map_dict[k] /= count_dict[k]
+    return [map_dict[x] for x in data]
+
+
+def rank_sort_combine(data_list, weight=None):
+    rank_data_list = []
+    for data in data_list:
+        rank_data_list.append(rank_sort(data))
+    res = 0
+    if weight is None:
+        for data in rank_data_list:
+            res = res + np.array(data)
+        return res
+    else:
+        for i, data in enumerate(rank_data_list):
+            res = res + np.array(data) * weight[i]
+        return res
+
+
+def traversal_search(data_list, n_range=(1, 2)):
+    for n in n_range:
+        for cb in combinations(data_list, n):
+            yield list(cb)
+
+
+def neg_log_loss(y_true, y_pred):
+    weight = compute_sample_weight(class_weight='balanced', y=y_true)
+    return -log_loss(y_true=y_true, y_pred=y_pred, sample_weight=weight)
+
+
+def clean_float_data(data=None, data_type='df'):
+    def clean_data(data):
+        try:
+            return float(data)
+        except:
+            return np.nan
+
+    if data_type == 'df':
+        for ft in data.columns.tolist():
+            data[ft] = data[ft].apply(clean_data)
+    elif data_type == 'list':
+        data = map(clean_data, data)
+    return data
+
+
+def md5_encode(data):
+    return md5(data.encode()).hexdigest()
+
+
+def equal_frequency_bin(data, n_bins=10):
+    '''
+    等频分箱.
+    返回分箱后的编码和边界.
+    '''
+    return pd.qcut(data, n_bins, labels=False, retbins=True, precision=8, duplicates='drop')
+
+
+if __name__ == '__main__':
+    a=['a','b','c', 'w']
+    for i in traversal_search(a, (1, 2, 3, 4)):
+        print(i)
+    pass
---
+++ packages/shy-tools/shy_tools-0.0.8-py3-none-any.whl/shy_tools/pipline.py
@@ -0,0 +1,222 @@
+# coding=utf-8
+'''
+流程整合.
+减少灵活性, 增加便利性.
+'''
+import gc
+import pandas as pd
+from sklearn.metrics import roc_auc_score
+
+from shy_tools.data_transform import LinearModelPreprocessor
+from shy_tools.param_optimize import LightBayesOptimizer, LROptimizer
+from shy_tools.feature_select import FeatureSelector
+from shy_tools.base_utils import mkdir, split_X_y
+from shy_tools.feature_select import ModelParamsGenerator, LGBRFE
+
+def gen_model_params_and_ft_select(df=None, label='target'):
+    mpg = ModelParamsGenerator(df=df, label=label)
+    model_params_dict = mpg.run()
+
+    lgbrfe = LGBRFE(df=df, label=label, params=model_params_dict)
+    lgbrfe.run()
+    lgbrfe.plot()
+
+
+
+def ft_select_and_param_opt(select_method='null_imp', train=None,
+                            test=None, label='target', time_filed=None,
+                            is_pred=False, name='origin', n_iter=50):
+    '''
+    将特征选择与超参数调节, 模型评估放在一起.
+    若没有测试集, 则保存训练好的模型.
+    若没有测试集标签, 则进一步保存并输出测试集预测结果.
+    若有测试集标签, 则输出测试集AUC.
+    :param select_method: str, 特征选择的方法, 默认"null_imp".
+    :param train: df, 训练集.
+    :param test: df, 测试集.
+    :param label: str, 标签列, 默认"target".
+    :param time_filed: str, 时间列.
+    :param is_pred: bool, 是否输出测试集预测结果, 默认False, 即输出AUC.
+    :param name: str, 名称标准, 以区分不同的数据.
+    :param n_iter: int, 超参数随机搜索次数, 默认20.
+    :return: null or float, 根据模式不返回或返回AUC.
+    '''
+    # 创建文件夹保存信息.
+    mkdir('./feature/')
+    mkdir('./model/')
+    mkdir('./result/')
+    ###############################################################
+    # 参数选择.
+    if select_method == 'null_imp':
+        if time_filed is None:
+            fs = FeatureSelector(df=train, label=label)
+        else:
+            fs = FeatureSelector(df=train.drop(time_filed, axis=1), label=label)
+        fs.null_imp_select(verbose=0)
+        fs.save_feature(path='./feature/' + name + '_' + select_method,
+                        is_save_ft_list=False, is_save_ft_list_dict=True)
+        ft_list_dict = fs.select_ft_list_dict.copy()
+    elif select_method == 'boruta':
+        if time_filed is None:
+            fs = FeatureSelector(df=train, label=label)
+        else:
+            fs = FeatureSelector(df=train.drop(time_filed, axis=1), label=label)
+        fs.boruta_select(verbose=0)
+        fs.save_feature(path='./feature/' + name + '_' + select_method,
+                        is_save_ft_list=False, is_save_ft_list_dict=True)
+        ft_list_dict = fs.select_ft_list_dict.copy()
+    elif select_method == 'eli5':
+        if time_filed is None:
+            fs = FeatureSelector(df=train, label=label)
+        else:
+            fs = FeatureSelector(df=train.drop(time_filed, axis=1), label=label)
+        fs.eli5_permutation_select()
+        fs.save_feature(path='./feature/' + name + '_' + select_method,
+                        is_save_ft_list=False, is_save_ft_list_dict=True)
+        ft_list_dict = fs.select_ft_list_dict.copy()
+    elif select_method == 'gain':
+        if time_filed is None:
+            fs = FeatureSelector(df=train, label=label)
+        else:
+            fs = FeatureSelector(df=train.drop(time_filed, axis=1), label=label)
+        fs.gain_imp_select()
+        fs.save_feature(path='./feature/' + name + '_' + select_method,
+                        is_save_ft_list=False, is_save_ft_list_dict=True)
+        ft_list_dict = fs.select_ft_list_dict.copy()
+    # 释放内存.
+    del fs
+    gc.collect()
+
+    ##############################################################
+    # 模型训练.
+
+    for p in ft_list_dict:
+        ft_list = ft_list_dict[p]
+    ##############################################################
+        # cv logloss.
+        lgb_opt = LightBayesOptimizer(df=train[ft_list + [label]], time_filed=None,
+                                      label=label, opt_mode='cv', metrics='logloss')
+        lgb_opt.run(init_points=n_iter, n_iter=0, verbose=0, is_verbose=0)
+        lgb_opt.fit_model()
+        # 保存模型.
+        lgb_opt.save_model(path='./model/' + name + '_' + select_method + '_'
+                                + str(p) + '_' + 'cv_logloss')
+        # 模型预测/评估.
+        if test is None:
+            pass
+        else:
+            if is_pred:
+                result = lgb_opt.predict(test)
+                result = pd.DataFrame({'predict': result})
+                result.to_csv('./result/' + name + '_' + select_method + '_'
+                              + str(p) + '_' + 'cv_logloss.csv')
+            else:
+                print(name + '_' + select_method + '_'
+                      + str(p) + '_' + 'cv_logloss AUC is : %f' %
+                      roc_auc_score(test[label], lgb_opt.predict(test)))
+    ##############################################################
+        # ho logloss
+        lgb_opt = LightBayesOptimizer(df=train[ft_list + [label]], time_filed=None,
+                                      label=label, opt_mode='ho', metrics='logloss')
+        lgb_opt.run(init_points=n_iter, n_iter=0, verbose=0, is_verbose=0)
+        lgb_opt.fit_model()
+        # 保存模型.
+        lgb_opt.save_model(path='./model/' + name + '_' + select_method + '_'
+                                + str(p) + '_' + 'ho_logloss')
+        # 模型预测/评估.
+        if test is None:
+            pass
+        else:
+            if is_pred:
+                result = lgb_opt.predict(test)
+                result = pd.DataFrame({'predict': result})
+                result.to_csv('./result/' + name + '_' + select_method + '_'
+                              + str(p) + '_' + 'ho_logloss.csv')
+            else:
+                print(name + '_' + select_method + '_'
+                      + str(p) + '_' + 'ho_logloss AUC is : %f' %
+                      roc_auc_score(test[label], lgb_opt.predict(test)))
+    ##############################################################
+        # ho logloss time
+        if time_filed is not None:
+            lgb_opt = LightBayesOptimizer(df=train[ft_list + [label, time_filed]], time_filed=time_filed,
+                                          label=label, opt_mode='ho', metrics='logloss')
+            lgb_opt.run(init_points=n_iter, n_iter=0, verbose=0, is_verbose=0)
+            lgb_opt.fit_model()
+            # 保存模型.
+            lgb_opt.save_model(path='./model/' + name + '_' + select_method + '_'
+                                    + str(p) + '_' + 'ho_logloss_time')
+            # 模型预测/评估.
+            if test is None:
+                pass
+            else:
+                if is_pred:
+                    result = lgb_opt.predict(test)
+                    result = pd.DataFrame({'predict': result})
+                    result.to_csv('./result/' + name + '_' + select_method + '_'
+                                  + str(p) + '_' + 'cv_logloss_time.csv')
+                else:
+                    print(name + '_' + select_method + '_'
+                          + str(p) + '_' + 'cv_logloss_time AUC is : %f' %
+                          roc_auc_score(test[label], lgb_opt.predict(test)))
+
+
+def linear_model_preprocess_and_fit(train=None,
+                                    test=None,
+                                    label='target',
+                                    is_boxcox=True,
+                                    boxcox_mode='yeo-johnson',
+                                    is_miss_impute=True,
+                                    min_miss_num=100,
+                                    miss_mode='mean',
+                                    is_indicate=True,
+                                    nfold=7,
+                                    Cs=10):
+    '''
+    :param train: 数据集.
+    :param label: 标签名称.
+    :param is_boxcox: 是否BoxCox.
+    :param boxcox_mode: boxcox模式.
+    :param is_miss_impute: 是否填充缺失值.
+    :param min_miss_num: 最小缺失填充阈值.
+    :param miss_mode: 填充缺失值模式.
+    :param is_indicate: 是否添加指示列.
+    :param nfold: 交叉验证折数.
+    :param Cs: 正则化系数.
+    :return: 训练好的逻辑回归模型.
+    '''
+    # 数据预处理.
+    lmp = LinearModelPreprocessor(is_boxcox=is_boxcox,
+                                  boxcox_mode=boxcox_mode,
+                                  is_miss_impute=is_miss_impute,
+                                  min_miss_num=min_miss_num,
+                                  miss_mode=miss_mode,
+                                  is_indicate=is_indicate)
+    train_X, train_y = split_X_y(train, label=label)
+    test_X, test_y = split_X_y(test, label=label)
+
+    train_X = lmp.fit_transform(X=train_X, y=train_y)
+    test_X = lmp.transform(X=test_X)
+    train_X_y, test_X_y = train_X, test_X
+    train_X_y[label], test_X_y[label] = train_y, test_y
+    del train_X, test_X
+    gc.collect()
+
+    # 逻辑回归调参.
+    lro = LROptimizer(df=train_X_y, label=label, nfold=nfold, Cs=Cs)
+    lro.run()
+
+    # 输出结果.
+    print('训练集结果:')
+    print(roc_auc_score(train_X_y[label], lro.predict(train_X_y)))
+    print('测试集结果.')
+    print(roc_auc_score(test_X_y[label], lro.predict(test_X_y)))
+
+    return lmp, lro
+
+
+
+if __name__ == '__main__':
+
+    train = pd.read_csv('./data/bc_train.csv')
+    gen_model_params_and_ft_select(df=train)
