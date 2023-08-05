3d86ed8973e9a51b9f6ace9b5f0573b27b696000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:30 +0000

    Add package ANTConnect/0.1.4/ANTConnect-0.1.4.tar.gz

---
+++ packages/ANTConnect/ANTConnect-0.1.4.tar.gz/ANTConnect-0.1.4/antconnect/__init__.py
@@ -0,0 +1,12 @@
+"""ANT - ANT enables easy communication to an ANT CDE"""
+
+from .api import API
+from .column import Column
+from .project import Project
+from .record import Record
+from .revision import Revision
+from .table import Table
+
+__version__ = '0.1.4'
+__author__ = 'ANT CDE'
+__all__ = []
