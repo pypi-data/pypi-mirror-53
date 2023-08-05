69be37f4cef8fc330a519f3f065297daaa53e65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:03:26 +0000

    Add package pastebin-archiver/0.1.0/pastebin_archiver-0.1.0.tar.gz

---
+++ packages/pastebin-archiver/pastebin_archiver-0.1.0.tar.gz/pastebin_archiver-0.1.0/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = "pastebin_archiver"
+DESCRIPTION = "Fetch public posts from Pastebin.com for archival"
+URL = "https://gitlab.com/jonpavelich/pastebin-archiver"
+EMAIL = "pypi@jonpavelich.com"
+AUTHOR = "Jon Pavelich"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "0.1.0"
+
+# What packages are required for this module to be executed?
+REQUIRED = ["requests", "sqlalchemy", "apscheduler"]
+
+# What packages are optional?
+EXTRAS = {"postgresql": ["psycopg2-binary"]}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+    entry_points={
+        "console_scripts": ["pastebin-archiver=pastebin_archiver.__main__:main"]
+    },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
