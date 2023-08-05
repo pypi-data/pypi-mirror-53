d2e6199b4378a2e940eeb9f28e31f1d6b6c533d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:40 +0000

    Add package otto-ml/0.1.13/otto_ml-0.1.13-py3-none-any.whl

---
+++ packages/otto-ml/otto_ml-0.1.13-py3-none-any.whl/otto/files/settings.py
@@ -0,0 +1,8 @@
+import os
+
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+S3_BUCKET = os.getenv("S3_BUCKET")
+
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_SERVER_TRACKING_URI", "")
+print(MLFLOW_TRACKING_URI)
