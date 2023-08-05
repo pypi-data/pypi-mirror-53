052da8071ad087bad8d5c9a92f54646c5f41432f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:33 +0000

    Add package django-admin-relation-links/0.2.2/django-admin-relation-links-0.2.2.tar.gz

---
+++ packages/django-admin-relation-links/django-admin-relation-links-0.2.2.tar.gz/django-admin-relation-links-0.2.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='django-admin-relation-links',
+    keywords='django admin relation foreignkey link',
+    version='0.2.2',
+    author='gitaarik',
+    author_email='gitaarik@gmail.com',
+    packages=['django_admin_relation_links'],
+    url='https://github.com/gitaarik/django-admin-relation-links/',
+    license='LGPL licence, see LICENCE.txt',
+    description='An easy way to add links to relations in the Django Admin site.',
+    zip_safe=False
+)
