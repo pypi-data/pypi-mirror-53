3234107cb58c5a54c27fd3cf4a245e424ba93c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:05:27 +0000

    Add package jupyterlab-nvdashboard/0.1.11/jupyterlab-nvdashboard-0.1.11.tar.gz

---
+++ packages/jupyterlab-nvdashboard/jupyterlab-nvdashboard-0.1.11.tar.gz/jupyterlab-nvdashboard-0.1.11/jupyterlab_nvdashboard/server.py
@@ -0,0 +1,40 @@
+import sys
+
+from bokeh.server.server import Server
+from tornado.ioloop import IOLoop
+from tornado import web
+
+from jupyterlab_nvdashboard import apps
+
+
+routes = {
+    "/GPU-Utilization": apps.gpu.gpu,
+    "/GPU-Memory": apps.gpu.gpu_mem,
+    "/GPU-Resources": apps.gpu.gpu_resource_timeline,
+    "/PCIe-Throughput": apps.gpu.pci,
+    "/NVLink-Throughput": apps.gpu.nvlink,
+    "/NVLink-Timeline": apps.gpu.nvlink_timeline,
+    # "/CPU-Utilization": apps.cpu.cpu,
+    "/Machine-Resources": apps.cpu.resource_timeline,
+}
+
+
+class RouteIndex(web.RequestHandler):
+    """ A JSON index of all routes present on the Bokeh Server """
+
+    def get(self):
+        self.write({route: route.strip("/").replace('-', ' ')
+                    for route in routes})
+
+
+if __name__ == "__main__":
+
+    server = Server(routes, port=int(
+        sys.argv[1]), allow_websocket_origin=["*"])
+    server.start()
+
+    server._tornado.add_handlers(
+        r".*", [(server.prefix + "/" + "index.json", RouteIndex, {})]
+    )
+
+    IOLoop.current().start()
---
+++ packages/jupyterlab-nvdashboard/jupyterlab-nvdashboard-0.1.11.tar.gz/jupyterlab-nvdashboard-0.1.11/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+import json
+
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+
+# read the contents of README file
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get version from package.json (to avoid duplicating)
+with open(path.join(this_directory, 'package.json'), encoding='utf-8') as f:
+    version = json.load(f)['version']
+
+
+setuptools.setup(
+    name="jupyterlab-nvdashboard",
+    version=version,
+    url="https://github.com/jacobtomlinson/jupyterlab-nvdashboard",
+    author="NV Dashbaord contributors",
+    description="A JupyterLab extension for displaying dashboards of GPU usage.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="BSD",
+    packages=setuptools.find_packages(),
+        keywords=['Jupyter'],
+        classifiers=['Framework :: Jupyter'],
+    install_requires=list(open("requirements.txt").read().strip().split("\n")),
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'nvdashboard = jupyterlab_nvdashboard:launch_server',
+        ]
+    },
+    package_data={
+        'jupyterlab_nvdashboard': ['icons/*'],
+    },
+)
