2bbea9929f2ad4a344b053a63172aa77e32ec812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:46 +0000

    Add package toai/0.1.10/toai-0.1.10.tar.gz

---
+++ packages/toai/toai-0.1.10.tar.gz/toai-0.1.10/toai/image/ImageDataset.py
@@ -0,0 +1,101 @@
+import math
+import os
+import re
+from pathlib import Path
+from typing import Callable, List, Optional, Tuple, Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+class ImageDataset:
+    def __init__(
+        self,
+        x: np.ndarray,
+        y: np.ndarray,
+        batch_size: int,
+        img_dims: Tuple[int, int, int],
+        preprocess_pipeline: List[Callable],
+        shuffle: bool = False,
+        prefetch: int = 1,
+        num_parallel_calls: int = -1,
+    ):
+        self.x = x
+        self.y = y
+        self.length = len(y)
+        self.batch_size = batch_size
+        self.steps = math.ceil(self.length / self.batch_size)
+        self.classes = np.unique(y)
+        self.n_classes = len(self.classes)
+        self.img_dims = img_dims
+        self.shuffle = shuffle
+        self.prefetch = prefetch
+        self.preprocess_pipeline = preprocess_pipeline
+
+        image_ds = tf.data.Dataset.from_tensor_slices(x)
+        image_ds = self.preprocess(image_ds, num_parallel_calls)
+        label_ds = tf.data.Dataset.from_tensor_slices(y)
+        dataset = tf.data.Dataset.zip((image_ds, label_ds))
+        if shuffle:
+            dataset = dataset.shuffle(batch_size)
+        self.data = dataset.repeat().batch(batch_size).prefetch(prefetch)
+
+    @classmethod
+    def from_df(
+        cls, df: pd.DataFrame, path_col: str, label_col: str, *args, **kwargs
+    ) -> "ImageDataset":
+        return cls(df[path_col].values, df[label_col].values, *args, **kwargs)
+
+    @classmethod
+    def from_subfolders(cls, path: Union[Path, str], *args, **kwargs) -> "ImageDataset":
+        pass
+
+    @classmethod
+    def from_re(
+        cls,
+        path: Union[Path, str],
+        regex: str,
+        default_label: Optional[str] = None,
+        *args,
+        **kwargs,
+    ) -> "ImageDataset":
+        paths = []
+        labels = []
+        for value in os.listdir(path):
+            match = re.match(regex, value)
+            if match:
+                labels.append(match.group(1))
+            elif default_label:
+                labels.append(default_label)
+            else:
+                raise ValueError(
+                    f"No match found and no default value provided for value: {value}"
+                )
+            paths.append(f"{path}/{value}")
+        return cls(np.asarray(paths), np.asarray(labels).astype(int), *args, **kwargs)
+
+    def show(self, cols: int = 8, n_batches: int = 1):
+        if cols >= self.batch_size * n_batches:
+            cols = self.batch_size * n_batches
+            rows = 1
+        else:
+            rows = math.ceil(self.batch_size * n_batches / cols)
+        _, ax = plt.subplots(rows, cols, figsize=(3 * cols, 3 * rows))
+        i = 0
+        for x_batch, y_batch in self.data.take(n_batches):
+            for (x, y) in zip(x_batch.numpy(), y_batch.numpy()):
+                idx = (i // cols, i % cols) if rows > 1 else i % cols
+                ax[idx].axis("off")
+                ax[idx].imshow(x)
+                ax[idx].set_title(y)
+                i += 1
+
+    def preprocess(
+        self, image_ds: tf.data.Dataset, num_parallel_calls: int = -1
+    ) -> tf.data.Dataset:
+        for fun in self.preprocess_pipeline:
+            image_ds = image_ds.map(fun, num_parallel_calls=num_parallel_calls)
+
+        return image_ds
---
+++ packages/toai/toai-0.1.10.tar.gz/toai-0.1.10/toai/image/__init__.py
@@ -0,0 +1,8 @@
+# pylama:ignore=W0611
+
+from .ImageAugmentor import ImageAugmentor
+from .ImageDataset import ImageDataset
+from .ImageLearner import ImageLearner
+from .ImageParser import ImageParser
+from .ImageResizer import ImageResizer
+from .LearningRateFinder import LearningRateFinder
