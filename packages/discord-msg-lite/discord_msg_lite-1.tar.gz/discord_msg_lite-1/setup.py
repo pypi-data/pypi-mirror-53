5990df89e15092fc7eb35e6a9b4e12bc725ff0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:36 +0000

    Add package discord-msg-lite/1/discord_msg_lite-1.tar.gz

---
+++ packages/discord-msg-lite/discord_msg_lite-1.tar.gz/discord_msg_lite-1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fp:
+    long_desc = fp.read()
+
+setup(
+    name='discord_msg_lite',
+    version='1',
+    author='Tom YU Choe',
+    author_email='yonguk.choe@gmail.com',
+    description='A simple and lightweight Python PyPI module to send messages easily using Discord Webhooks.',
+    long_description=long_desc,
+    url='https://github.com/YUChoe/discord_msg_lite',
+    long_description_content_type="text/markdown",
+    py_modules=['discord_msg_lite'],
+    package_dir={'': 'src'},
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
