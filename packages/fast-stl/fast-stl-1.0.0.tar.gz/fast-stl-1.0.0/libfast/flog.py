b7263f4cdddd5320ca09855af5f3fd1c0e10b5c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:30 +0000

    Add package fast-stl/1.0.0/fast-stl-1.0.0.tar.gz

---
+++ packages/fast-stl/fast-stl-1.0.0.tar.gz/fast-stl-1.0.0/libfast/flog.py
@@ -0,0 +1,9 @@
+# _*_coding:utf-8_*_
+# Created by #Suyghur, on 2019-09-29.
+# Copyright (c) 2019 3KWan.
+# Description :
+
+
+def log(content) -> None:
+    print("some errors occurred in the fast-stl")
+    print("error : " + str(content))
