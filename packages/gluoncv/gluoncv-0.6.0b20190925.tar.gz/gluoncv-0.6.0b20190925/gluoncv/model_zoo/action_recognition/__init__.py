e548b79fb6b42fbc41ced97e3c346760053c9aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:25 +0000

    Add package gluoncv/0.6.0b20190925/gluoncv-0.6.0b20190925.tar.gz

---
+++ packages/gluoncv/gluoncv-0.6.0b20190925.tar.gz/gluoncv-0.6.0b20190925/gluoncv/model_zoo/action_recognition/__init__.py
@@ -0,0 +1,7 @@
+# pylint: disable=wildcard-import
+"""Video action recognition."""
+from __future__ import absolute_import
+from .vgg16_ucf101 import *
+from .inceptionv3_ucf101 import *
+from .inceptionv3_kinetics400 import *
+from .i3d_resnet import *
