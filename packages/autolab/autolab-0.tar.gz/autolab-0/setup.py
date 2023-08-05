4b5fb793811a5bb67601230c22a1da9bb2d80078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package autolab/0/autolab-0.tar.gz

---
+++ packages/autolab/autolab-0.tar.gz/autolab-0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 20 17:30:03 2019
+
+@author: qchat
+"""
+
+from setuptools import setup,find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'autolab',
+    version = '0',  # Ideally should be same as your GitHub release tag varsion
+    author = 'Quentin Chateiller',
+    author_email = 'q.chateiller@gmail.com',
+    description = 'Python package for scientific experiments automation',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/qcha41/autolab',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    package_data={'': ['*.ini']},# If any package contains *.ini files, include them:
+    keywords = ['Universal','Scanning','Interface','automation','scientific','laboratory','experiments','measures']
+)
+
