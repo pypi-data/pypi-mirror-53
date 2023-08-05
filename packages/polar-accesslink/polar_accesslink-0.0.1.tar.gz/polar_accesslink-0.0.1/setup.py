e96e89f0cfd77f7d2f5be58246c57ff0b6890cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:04 +0000

    Add package polar-accesslink/0.0.1/polar_accesslink-0.0.1.tar.gz

---
+++ packages/polar-accesslink/polar_accesslink-0.0.1.tar.gz/polar_accesslink-0.0.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'requests'
+]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Nat Burns",
+    author_email='nbaccount@burnskids.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    description="Python wrapper for Polar Open AccessLink API.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords='polar_accesslink',
+    name='polar_accesslink',
+    packages=find_packages(include=['accesslink']),
+    setup_requires=setup_requirements,
+    url='https://github.com/burnnat/polar_accesslink',
+    version='0.0.1',
+    zip_safe=False,
+)
