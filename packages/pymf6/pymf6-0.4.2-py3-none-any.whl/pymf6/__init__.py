40ee754b8fc28db08c81d575a11a1ce7e887b079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:44 +0000

    Add package pymf6/0.4.2/pymf6-0.4.2-py3-none-any.whl

---
+++ packages/pymf6/pymf6-0.4.2-py3-none-any.whl/pymf6/__init__.py
@@ -0,0 +1,13 @@
+"""
+Python Wrapper for MODFLOW 6
+"""
+
+__all__ = ['__version__', '__name__', '__author__']
+
+__version__ = '0.4.2'
+__name__ = 'pymf6'  # pylint: disable=redefined-builtin
+__author__ = 'Mike Müller'
+
+MFSIM_NAM = 'mfsim.nam'
+BACKUP_DIR = '__pymf6__backup'
+DATA_MAPPING = 'data_mapping.pkl'
