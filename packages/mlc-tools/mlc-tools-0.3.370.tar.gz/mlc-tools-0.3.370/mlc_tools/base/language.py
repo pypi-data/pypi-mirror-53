247b7077cddfdd2e363200279dbc8ee762fda2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:06 +0000

    Add package mlc-tools/0.3.370/mlc-tools-0.3.370.tar.gz

---
+++ packages/mlc-tools/mlc-tools-0.3.370.tar.gz/mlc-tools-0.3.370/mlc_tools/base/language.py
@@ -0,0 +1,27 @@
+
+
+class Language(object):
+
+    def __init__(self, module_lang, model):
+        from pydoc import locate
+        module_name = 'module_%s' % (module_lang if module_lang != 'py' else 'python')
+
+        self.generator = locate('mlc_tools.%s.Generator' % module_name)()
+        self.translator = locate('mlc_tools.%s.Translator' % module_name)()
+        self.serializer = locate('mlc_tools.%s.Serializer' % module_name)()
+        self.writer = locate('mlc_tools.%s.Writer' % module_name)(model.out_directory)
+        self.save_plugin = locate('mlc_tools.%s.SavePlugin' % module_name)(model)
+        self.writer.model = model
+        self.writer.serializer = self.serializer
+
+    def get_generator(self):
+        return self.generator
+
+    def get_translator(self):
+        return self.translator
+
+    def get_serializer(self):
+        return self.serializer
+
+    def get_writer(self):
+        return self.writer
