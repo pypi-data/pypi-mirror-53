af4b3b3e559f567525bdeb4240e12344ad77ba85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package radicale-imaps/0.1.1/radicale-imaps-0.1.1.tar.gz

---
+++ packages/radicale-imaps/radicale-imaps-0.1.1.tar.gz/radicale-imaps-0.1.1/README.md
@@ -0,0 +1,18 @@
+# Radicale IMAP authentication plugin
+
+IMAP authentication plugin for [Radicale](http://radicale.org/).
+This is a fork of [radicale-imap](https://github.com/comzeradd/radicale-imap) that uses IMAP4_SSL instead of IMAP4 to initiate connection.
+
+## Installation
+
+    python3 -m pip install radicale-imap
+
+## Configuration
+
+    [auth]
+    type = radicale_imap
+    imaps_host = address:port
+
+## License
+
+[GPL-3.0](LICENSE)
---
+++ packages/radicale-imaps/radicale-imaps-0.1.1.tar.gz/radicale-imaps-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='radicale-imaps',
+    version='0.1.1',
+    author='Louis Guidez',
+    author_email='louis@hostux.fr',
+    url='https://github.com/reind33r/radicale-imaps',
+    description='Radicale IMAPs authentication plugin',
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=['radicale'],
+    license='LICENSE',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
