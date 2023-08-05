6d15e08cc4cdee3c800d120984adc7fa8bc0b90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:23 +0000

    Add package django-saml2-auth-signout-slo/0.0.4/django_saml2_auth_signout_slo-0.0.4.tar.gz

---
+++ packages/django-saml2-auth-signout-slo/django_saml2_auth_signout_slo-0.0.4.tar.gz/django_saml2_auth_signout_slo-0.0.4/django_saml2_auth_signout_slo/plugins.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import logout
+from django_saml2_auth import utils
+from django_saml2_auth.plugins import SignoutPlugin
+from django_saml2_auth.views import _get_saml_client
+from saml2.client_base import LogoutError
+from saml2.ident import decode
+
+
+class SingleLogOutSignoutHandler(SignoutPlugin):
+    KEY = 'SLO'
+
+    @classmethod
+    def signout(cls, request):
+        """Logs the user out of the local system and redirects them to the SLO URL in the SAML Metadata"""
+        name_id = decode(request.session['name_id'])
+        logout(request)
+
+        # from pysaml2/example/sp-wsgi/sp.py
+        try:
+            saml_client = _get_saml_client(utils.get_current_domain(request))
+            responses = saml_client.global_logout(name_id).items()
+            return responses[0]
+        except LogoutError:
+            pass
