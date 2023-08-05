d887c3187e7e8e8d926a2b82fd1ea986f928036a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:54 +0000

    Add package geocube/0.0.8/geocube-0.0.8.tar.gz

---
+++ packages/geocube/geocube-0.0.8.tar.gz/geocube-0.0.8/geocube/rasterize.py
@@ -0,0 +1,156 @@
+# -- coding: utf-8 --
+"""
+This module contains tools for rasterizing vector data.
+"""
+import numpy
+import rasterio.features
+import rasterio.transform
+import rasterio.warp
+from rasterio.enums import MergeAlg
+from scipy.interpolate import Rbf, griddata
+from shapely.geometry import mapping
+
+from geocube.logger import get_logger
+
+
+def rasterize_image(
+    geometry_array,
+    data_values,
+    geobox,
+    fill=-9999.0,
+    merge_alg=MergeAlg.replace,
+    **ignored_kwargs
+):
+    """
+    Rasterize a list of shapes+values for a given GeoBox.
+
+    Parameters
+    -----------
+    geometry_array: geopandas.GeometryArray
+        A geometry array of points.
+    data_values: list
+        Data values associated with the list of geojson shapes
+    geobox: :obj:`datacube.utils.geometry.GeoBox`
+        Transform of the resulting image.
+    fill: float, optional
+        The value to fill in the grid with for nodata. Default is -9999.0.
+    merge_alg: `rasterio.enums.MergeAlg`, optional
+        The algorithm for merging values into one cell. Default is `MergeAlg.replace`.
+    **ignored_kwargs:
+        These are there to be flexible with additional rasterization methods and
+        will be ignored.
+
+    Returns
+    -------
+    :obj:`numpy.ndarray` or None
+        The vector data in the rasterized format.
+
+    """
+    logger = get_logger()
+
+    try:
+        image = rasterio.features.rasterize(
+            zip(geometry_array.apply(mapping).values, data_values),
+            out_shape=(geobox.height, geobox.width),
+            transform=geobox.affine,
+            fill=fill,
+            merge_alg=merge_alg,
+            dtype=numpy.float64,
+        )
+        return image
+    except TypeError as ter:
+        if "cannot perform reduce with flexible type" in str(ter):
+            logger.warning("{warning}".format(warning=ter))
+            return None
+        raise
+
+
+def rasterize_points_griddata(
+    geometry_array,
+    data_values,
+    grid_coords,
+    fill=-9999.0,
+    method="nearest",
+    **ignored_kwargs
+):
+    """
+    This method uses scipy.interpolate.griddata to interpolate point data
+    to a grid.
+
+    Parameters
+    ----------
+    geometry_array: geopandas.GeometryArray
+        A geometry array of points.
+    data_values: list
+        Data values associated with the list of geojson shapes
+    grid_coords: dict
+        Output from `rioxarray.rioxarray.affine_to_coords`
+    fill: float, optional
+        The value to fill in the grid with for nodata. Default is -9999.0.
+    method: {‘linear’, ‘nearest’, ‘cubic’}, optional
+        The method to use for interpolation in `scipy.interpolate.griddata`.
+    **ignored_kwargs:
+        These are there to be flexible with additional rasterization methods and
+        will be ignored.
+
+    Returns
+    -------
+    :class:`numpy.ndarray`: An interpolated :class:`numpy.ndarray`.
+
+    """
+    if data_values.dtype == object:
+        return None
+    try:
+        return griddata(
+            points=(geometry_array.x, geometry_array.y),
+            values=data_values,
+            xi=tuple(numpy.meshgrid(grid_coords["x"], grid_coords["y"])),
+            method=method,
+            fill_value=fill,
+        )
+    except ValueError as ver:
+        if "could not convert string to float" in str(ver):
+            return None
+        raise
+
+
+def rasterize_points_radial(
+    geometry_array, data_values, grid_coords, method="linear", **ignored_kwargs
+):
+    """
+    This method uses scipy.interpolate.griddata to interpolate point data
+    to a grid.
+
+    Parameters
+    ----------
+    geometry_array: geopandas.GeometryArray
+        A geometry array of points.
+    data_values: list
+        Data values associated with the list of geojson shapes
+    grid_coords: dict
+        Output from `rioxarray.rioxarray.affine_to_coords`
+    fill: float, optional
+        The value to fill in the grid with for nodata. Default is -9999.0.
+    method: str, optional
+        The function to use for interpolation in `scipy.interpolate.Rbf`.
+        {'multiquadric', 'inverse', 'gaussian', 'linear',
+        'cubic', 'quintic', 'thin_plate'}
+    **ignored_kwargs:
+        These are there to be flexible with additional rasterization methods and
+        will be ignored.
+
+    Returns
+    -------
+    :class:`numpy.ndarray`: An interpolated :class:`numpy.ndarray`.
+
+    """
+    logger = get_logger()
+
+    try:
+        interp = Rbf(geometry_array.x, geometry_array.y, data_values, function=method)
+        return interp(*numpy.meshgrid(grid_coords["x"], grid_coords["y"]))
+    except ValueError as ter:
+        if "object arrays are not supported" in str(ter):
+            logger.warning("{warning}".format(warning=ter))
+            return None
+        raise
---
+++ packages/geocube/geocube-0.0.8.tar.gz/geocube-0.0.8/sphinx/history.rst
@@ -0,0 +1,19 @@
+History
+=======
+
+0.0.8
+-----
+- Add merge algorithm option for rasterization (issue #5)
+- Drop Python 2 support (issue #6)
+
+0.0.7
+-----
+- Remove geocube pin (pull #4)
+
+0.0.6
+-----
+- Added additional methods for resampling points to a 2D grid (pull #3)
+
+0.0.5
+-----
+- Fix converting to another projection to ensure bounds are correctly accounted for (pull #2)
