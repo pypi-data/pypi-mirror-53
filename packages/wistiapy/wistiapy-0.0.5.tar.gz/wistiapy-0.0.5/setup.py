c2ef467f030a0b54f2386526a2420bca1b36bc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package wistiapy/0.0.5/wistiapy-0.0.5.tar.gz

---
+++ packages/wistiapy/wistiapy-0.0.5.tar.gz/wistiapy-0.0.5/setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(here, "wistia", "__version__.py"), "r", encoding="utf-8") as f:
+    exec(f.read(), about)
+
+setup(
+    name="wistiapy",
+    version=about["__version__"],
+    description="A Python client for the Wistia data API",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url="https://github.com/Edrolo/wistiapy",
+    license="MIT",
+    author="Matt Fisher",
+    author_email="matt@edrolo.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="wistia api client",
+    include_package_data=False,
+    install_requires=["requests>=2.22.0,<3.0.0", "factory-boy>=2.7.0,<3.0.0"],
+    extras_require={"dev": ["black", "pytest", "responses"]},
+    entry_points={"console_scripts": ["wistia = wistia.cli:main"]},
+)
---
+++ packages/wistiapy/wistiapy-0.0.5.tar.gz/wistiapy-0.0.5/wistia/client.py
@@ -0,0 +1,245 @@
+import logging
+from itertools import count
+from typing import Iterable
+
+import requests
+from wistia.schema import Media, Project, CaptionTrack
+
+log = logging.getLogger("wistiapy")
+
+
+class WistiaClient:
+    API_BASE_URL = "https://api.wistia.com/v1/"
+
+    def __init__(self, user="api", api_password=""):
+        # https://wistia.com/support/developers/data-api#authentication
+        self.session = requests.Session()
+        self.session.auth = (user, api_password)
+
+    def request(self, method, rel_path, **kwargs):
+        url = f"{self.API_BASE_URL}{rel_path}"
+        response = self.session.request(method=method, url=url, **kwargs)
+        response.raise_for_status()
+        response_data = response.json()
+        return response_data
+
+    def get(self, rel_path: str, params: dict = None):
+        return self.request("GET", rel_path, params=params)
+
+    def post(self, rel_path: str, **kwargs):
+        return self.request("POST", rel_path, **kwargs)
+
+    def put(self, rel_path: str, **kwargs):
+        return self.request("PUT", rel_path, **kwargs)
+
+    def delete(self, rel_path: str):
+        return self.request("DELETE", rel_path)
+
+    # Projects
+
+    def list_projects(
+        self,
+        sort_by=None,  # None:ProjectID,  name, created, or updated
+        sort_direction=1,
+        page=1,
+        per_page=100,
+    ) -> Iterable[Project]:
+        # https://wistia.com/support/developers/data-api#projects_list
+        params = {"page": page, "per_page": per_page}
+        if sort_by:
+            params["sort_by"] = sort_by
+            params["sort_direction"] = sort_direction
+
+        project_list = self.get("projects.json", params=params)
+        return [Project(project_data) for project_data in project_list]
+
+    def list_all_projects(self) -> Iterable[Project]:
+        log.info("Listing all projects")
+        for page in count(start=1):
+            next_page_of_projects = self.list_projects(page=page)
+            if next_page_of_projects:
+                yield from next_page_of_projects
+            else:
+                return
+
+    def show_project(self, project_hashed_id: str) -> Project:
+        # https://wistia.com/support/developers/data-api#projects_show
+        rel_path = f"projects/{project_hashed_id}.json"
+        project = Project(self.get(rel_path))
+        log.debug("Retrieved project details: {}".format(project))
+        return project
+
+    # https://wistia.com/support/developers/data-api#projects_create
+    # https://wistia.com/support/developers/data-api#projects_update
+    # https://wistia.com/support/developers/data-api#projects_delete
+    # https://wistia.com/support/developers/data-api#projects_copy
+
+    # Project Sharings
+    # https://wistia.com/support/developers/data-api#project_sharings_list
+    # https://wistia.com/support/developers/data-api#project_sharings_show
+    # https://wistia.com/support/developers/data-api#project_sharings_create
+    # https://wistia.com/support/developers/data-api#project_sharings_update
+    # https://wistia.com/support/developers/data-api#project_sharings_delete
+
+    # Medias
+
+    def list_medias(
+        self,
+        sort_by="name",
+        sort_direction=1,
+        page=1,
+        per_page=100,
+        project_id=None,
+        name=None,
+        media_type=None,
+    ) -> Iterable[Media]:
+        # https://wistia.com/support/developers/data-api#medias_list
+        params = {
+            "sort_by": sort_by,
+            "sort_direction": sort_direction,
+            "page": page,
+            "per_page": per_page,
+        }
+        if project_id:
+            params["project_id"] = project_id
+        if name is not None:
+            params["name"] = name
+        if media_type is not None:
+            params["type"] = media_type
+
+        medias_list = self.get("medias.json", params=params)
+
+        return [Media(media_data) for media_data in medias_list]
+
+    def show_media(self, wistia_hashed_id: str) -> Media:
+        # https://wistia.com/support/developers/data-api#medias_show
+        rel_path = f"medias/{wistia_hashed_id}.json"
+        media_data = self.get(rel_path)
+        return Media(media_data)
+
+    # https://wistia.com/support/developers/data-api#medias_update
+    # https://wistia.com/support/developers/data-api#medias_delete
+    # https://wistia.com/support/developers/data-api#medias_copy
+    # https://wistia.com/support/developers/data-api#medias_stats
+
+    # Account
+    # https://wistia.com/support/developers/data-api#account
+
+    # Customizations
+
+    def show_media_customizations(self, wistia_hashed_id: str) -> dict:
+        # https://wistia.com/support/developers/data-api#customizations_show
+        rel_path = f"medias/{wistia_hashed_id}/customizations.json"
+        media_customizations_data = self.get(rel_path)
+        return media_customizations_data
+
+    # https://wistia.com/support/developers/data-api#customizations_create
+    # https://wistia.com/support/developers/data-api#customizations_update
+    # https://wistia.com/support/developers/data-api#customizations_delete
+
+    # Captions
+
+    def list_captions(self, wistia_hashed_id: str) -> Iterable[CaptionTrack]:
+        rel_path = f"medias/{wistia_hashed_id}/captions.json"
+        caption_list = self.get(rel_path)
+        return [CaptionTrack(caption_data) for caption_data in caption_list]
+
+    def create_captions(
+        self,
+        wistia_hashed_id: str,
+        language_code: str = "eng",
+        caption_filename: str = "",
+        caption_text: str = "",
+    ) -> None:
+        # https://wistia.com/support/developers/data-api#captions_create
+        # Empty 200: OK; 400: already exist; 404: video DNE
+        rel_path = f"medias/{wistia_hashed_id}/captions.json"
+        if caption_text:
+            r = self.post(
+                rel_path, data={"language": language_code, "caption_file": caption_text}
+            )
+        elif caption_filename:
+            with open(caption_filename, "rb") as caption_file:
+                r = self.post(
+                    rel_path,
+                    data={"language": language_code},
+                    files={"caption_file": caption_file},
+                )
+        else:
+            raise ValueError(
+                "create_captions requires subtitle_filename or subtitle_text"
+            )
+
+        r.raise_for_status()
+
+    def show_captions(
+        self, wistia_hashed_id, language_code: str = "eng"
+    ) -> CaptionTrack:
+        # https://wistia.com/support/developers/data-api#captions_show
+        rel_path = f"medias/{wistia_hashed_id}/captions/{language_code}.json"
+        return CaptionTrack(self.get(rel_path))
+
+    def update_captions(
+        self, wistia_hashed_id, language_code, caption_filename="", caption_text=""
+    ) -> None:
+        # https://wistia.com/support/developers/data-api#captions_update
+        rel_path = f"medias/{wistia_hashed_id}/captions/{language_code}.json"
+        if caption_text:
+            r = self.session.put(rel_path, data={"caption_file": caption_text})
+        elif caption_filename:
+            with open(caption_filename, "rb") as caption_file:
+                r = self.put(rel_path, files={"caption_file": caption_file})
+        else:
+            raise ValueError(
+                "update_captions requires subtitle_filename or subtitle_text"
+            )
+
+        r.raise_for_status()
+
+    def delete_captions(
+        self, wistia_hashed_id: str, language_code: str = "eng"
+    ) -> None:
+        # https://wistia.com/support/developers/data-api#captions_delete
+        rel_path = f"medias/{wistia_hashed_id}/captions/{language_code}.json"
+        self.delete(rel_path)
+
+    def purchase_captions(self, wistia_hashed_id: str) -> None:
+        # https://wistia.com/support/developers/data-api#captions_purchase
+        rel_path = f"medias/{wistia_hashed_id}/captions/purchase.json"
+        self.post(rel_path)
+
+    def enable_captions_for_media(
+        self, wistia_hashed_id: str, enabled: bool = True
+    ) -> dict:
+        # https://wistia.com/support/developers/data-api#customizations_update
+        # > If a value is null, then that key will be deleted from the saved customizations.
+        # > If it is not null, that value will be set.
+        rel_path = f"medias/{wistia_hashed_id}/customizations.json"
+        if enabled:
+            payload = {"plugin": {"captions-v1": {"onByDefault": False}}}
+        else:
+            payload = {"plugin": {"captions-v1": None}}
+
+        media_customizations_data = self.put(rel_path, json=payload)
+        return media_customizations_data
+
+    def upload_subtitle_file_to_wistia_video(
+        self,
+        wistia_hashed_id: str,
+        subtitle_file_name: str,
+        replace=False,
+        language_code: str = "eng",
+    ) -> None:
+        captions_list = self.list_captions(wistia_hashed_id=wistia_hashed_id)
+        replaceable_captions = [
+            track for track in captions_list if track.language == language_code
+        ]
+
+        if replace and replaceable_captions:
+            self.update_captions(
+                wistia_hashed_id, language_code, caption_filename=subtitle_file_name
+            )
+        else:
+            self.create_captions(
+                wistia_hashed_id, language_code, caption_filename=subtitle_file_name
+            )
