176d1e645f44534946d8bb8b0acde0957e80daae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:54 +0000

    Add package larq-zoo/0.4.2/larq-zoo-0.4.2.tar.gz

---
+++ packages/larq-zoo/larq-zoo-0.4.2.tar.gz/larq-zoo-0.4.2/README.md
@@ -0,0 +1,20 @@
+# Larq Zoo
+
+[![Azure DevOps builds](https://img.shields.io/azure-devops/build/plumerai/larq/16.svg?logo=azure-devops)](https://plumerai.visualstudio.com/larq/_build/latest?definitionId=16&branchName=master) [![Azure DevOps coverage](https://img.shields.io/azure-devops/coverage/plumerai/larq/16.svg?logo=azure-devops)](https://plumerai.visualstudio.com/larq/_build/latest?definitionId=16&branchName=master) [![PyPI - Python Version](https://img.shields.io/pypi/pyversions/larq-zoo.svg)](https://pypi.org/project/larq-zoo/) [![PyPI](https://img.shields.io/pypi/v/larq-zoo.svg)](https://pypi.org/project/larq-zoo/) [![Join the community on Spectrum](https://withspectrum.github.io/badge/badge.svg)](https://spectrum.chat/larq) [![PyPI - License](https://img.shields.io/pypi/l/larq-zoo.svg)](https://github.com/plumerai/larq-zoo/blob/master/LICENSE) [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
+
+Larq Zoo contains reference implementations of popular Binarized Neural Networks.
+
+## Requirements
+
+Before installing Larq Zoo, please install:
+
+- [Python](https://python.org) version `3.6` or `3.7`
+- [Tensorflow](https://www.tensorflow.org/install) version `1.13+` or `2.0.0`
+
+## Installation
+
+You can install Larq Zoo with Python's [pip](https://pip.pypa.io/en/stable/) package manager:
+
+```shell
+pip install larq-zoo
+```
---
+++ packages/larq-zoo/larq-zoo-0.4.2.tar.gz/larq-zoo-0.4.2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(
+    name="larq-zoo",
+    version="0.4.2",
+    author="Plumerai",
+    author_email="lukas@plumerai.co.uk",
+    description="Reference implementations of popular Binarized Neural Networks",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/plumerai/larq-zoo",
+    packages=find_packages(),
+    license="Apache 2.0",
+    install_requires=["numpy~=1.15", "larq~=0.7.2", "zookeeper~=0.5.3"],
+    extras_require={
+        "tensorflow": ["tensorflow>=1.13.1"],
+        "tensorflow_gpu": ["tensorflow-gpu>=1.13.1"],
+        "test": ["pytest>=4.3.1", "pytest-cov>=2.6.1", "Pillow==6.1.0", "scipy==1.3.1"],
+    },
+    entry_points="""
+        [console_scripts]
+        lqz=larq_zoo.train:cli
+    """,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+    ],
+)
