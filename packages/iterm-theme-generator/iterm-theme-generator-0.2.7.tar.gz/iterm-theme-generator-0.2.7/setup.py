5691d7422ff2d56adb0921aef9c8652f9a866026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:05 +0000

    Add package iterm-theme-generator/0.2.7/iterm-theme-generator-0.2.7.tar.gz

---
+++ packages/iterm-theme-generator/iterm-theme-generator-0.2.7.tar.gz/iterm-theme-generator-0.2.7/setup.py
@@ -0,0 +1,46 @@
+"""Setup for iterm theme generator."""
+
+import io
+import setuptools
+
+def requirements():
+  with io.open('requirements.txt') as out:
+    return out.read()
+
+
+def version():
+  with io.open('VERSION') as out:
+    return out.read()
+
+
+def readme():
+  with io.open('README.rst') as out:
+    return out.read()
+
+
+setuptools.setup(
+    name='iterm-theme-generator',
+    version=version(),
+    description='Generate iTerm2 colors from an image',
+    long_description=readme(),
+    license='MIT',
+    author='Artyom Goncharov',
+    author_email='m4yers@gmail.com',
+    url='https://github.com/m4yers/iterm-theme-generator',
+    keywords='iterm, iterm2, colors, theme, image, wallpaper',
+    install_requires=requirements(),
+    packages=setuptools.find_packages(),
+    package_data={
+      '': ['LICENSE', 'VERSION', '*.txt', '*.rst']
+    },
+    python_requires='>=2.7, <3',
+    scripts=['iterm_theme_generator'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ])
