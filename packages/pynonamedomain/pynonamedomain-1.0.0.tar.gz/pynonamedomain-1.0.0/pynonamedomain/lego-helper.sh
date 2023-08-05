53022f023580855230bcf54639b0d312b798746a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:53 +0000

    Add package pynonamedomain/1.0.0/pynonamedomain-1.0.0.tar.gz

---
+++ packages/pynonamedomain/pynonamedomain-1.0.0.tar.gz/pynonamedomain-1.0.0/pynonamedomain/lego-helper.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+###############################################
+# PyNonameDomain helper script for using lego #
+#                                             #
+###############################################
+
+# Set your API username and password below #
+export NND_USERNAME=""
+export NND_PASSWORD=""
+
+####### DO NOT CHANGE BELOW THIS LINE #######
+export NND_DOMAIN="$2"
+export CERTBOT_VALIDATION="$3"
+export NND_TOKEN="$(nnd-cli login -q)"
+
+if [ $1 == "present" ]; then
+	nnd-cli create -c
+elif [ $1 == "cleanup" ]; then
+	HASH=$(nnd-cli read -q -r "{\"text\":\"$CERTBOT_VALIDATION\"}")
+	nnd-cli remove -q --hash $HASH
+else
+	exit 1
+fi
