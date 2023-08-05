527041980dd474b5b7941c5d1c89858f95baf383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:42 +0000

    Add package coinpy/1.1/coinpy-1.1.tar.gz

---
+++ packages/coinpy/coinpy-1.1.tar.gz/coinpy-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='coinpy',
+      version='1.1',
+      description='Gaussian distributions',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=['coinpy'],
+      zip_safe=False,
+      url="https://github.com/feeblefruits/coinpy",
+      author="Jacques Coetzee",
+      author_email="j.coetzee0@gmail.com",
+      license="MIT")
