7efcc72d8f7d08966468edb7ff610971dcfb9791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:47 +0000

    Add package Image-Kit/0.0.3/Image-Kit-0.0.3.tar.gz

---
+++ packages/Image-Kit/Image-Kit-0.0.3.tar.gz/Image-Kit-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='Image-Kit',
+    url='https://github.com/syamkakarla98/Image-Kit',
+    author='Syam Kakarla',
+    author_email='Syamkakarla98@gmail.com',
+    packages=['ImageKit'],
+    install_requires=['numpy', 'matplotlib', 'Pillow', 'scipy'],
+    version='0.0.3',
+    license='MIT',
+    description='A module for basic image processing.',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    python_requires=">=3.5.3",
+    classifiers=["Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",] ,
+)
