2c479a0cd7806e16a1e0fdd461c9fd94f8de4b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package hedgehog-protocol-spec/0.9.0/hedgehog_protocol_spec-0.9.0-py3-none-any.whl

---
+++ packages/hedgehog-protocol-spec/hedgehog_protocol_spec-0.9.0-py3-none-any.whl/gsl_protocol/grammar/HedgehogProtocolLexer.py
@@ -0,0 +1,138 @@
+# Generated from gsl_protocol/grammar/HedgehogProtocol.g4 by ANTLR 4.7
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\33")
+        buf.write("\u00a7\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30")
+        buf.write("\4\31\t\31\4\32\t\32\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\3\3")
+        buf.write("\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\5\3\5\3\5\3\5\3\5\3\5\3\5\7\5S\n\5\f\5\16\5V\13\5\3")
+        buf.write("\5\3\5\3\5\3\5\3\6\3\6\3\6\3\6\7\6`\n\6\f\6\16\6c\13\6")
+        buf.write("\3\6\3\6\3\7\3\7\3\b\3\b\3\t\3\t\3\n\3\n\3\13\3\13\3\f")
+        buf.write("\3\f\3\r\3\r\3\16\3\16\3\16\3\17\3\17\3\17\3\20\3\20\3")
+        buf.write("\20\3\21\3\21\3\22\3\22\3\23\3\23\3\24\3\24\3\25\3\25")
+        buf.write("\3\26\3\26\3\27\3\27\7\27\u008c\n\27\f\27\16\27\u008f")
+        buf.write("\13\27\3\30\6\30\u0092\n\30\r\30\16\30\u0093\3\31\3\31")
+        buf.write("\3\31\3\31\7\31\u009a\n\31\f\31\16\31\u009d\13\31\3\31")
+        buf.write("\3\31\3\32\6\32\u00a2\n\32\r\32\16\32\u00a3\3\32\3\32")
+        buf.write("\4Ta\2\33\3\3\5\4\7\5\t\6\13\7\r\b\17\t\21\n\23\13\25")
+        buf.write("\f\27\r\31\16\33\17\35\20\37\21!\22#\23%\24\'\25)\26+")
+        buf.write("\27-\30/\31\61\32\63\33\3\2\b\3\2^^\5\2C\\aac|\6\2\62")
+        buf.write(";C\\aac|\3\2\62;\4\2\f\f\16\17\5\2\13\f\16\17\"\"\2\u00ae")
+        buf.write("\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3\2\2\2\2\13")
+        buf.write("\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3")
+        buf.write("\2\2\2\2\25\3\2\2\2\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2")
+        buf.write("\2\2\2\35\3\2\2\2\2\37\3\2\2\2\2!\3\2\2\2\2#\3\2\2\2\2")
+        buf.write("%\3\2\2\2\2\'\3\2\2\2\2)\3\2\2\2\2+\3\2\2\2\2-\3\2\2\2")
+        buf.write("\2/\3\2\2\2\2\61\3\2\2\2\2\63\3\2\2\2\3\65\3\2\2\2\5<")
+        buf.write("\3\2\2\2\7E\3\2\2\2\tK\3\2\2\2\13[\3\2\2\2\rf\3\2\2\2")
+        buf.write("\17h\3\2\2\2\21j\3\2\2\2\23l\3\2\2\2\25n\3\2\2\2\27p\3")
+        buf.write("\2\2\2\31r\3\2\2\2\33t\3\2\2\2\35w\3\2\2\2\37z\3\2\2\2")
+        buf.write("!}\3\2\2\2#\177\3\2\2\2%\u0081\3\2\2\2\'\u0083\3\2\2\2")
+        buf.write(")\u0085\3\2\2\2+\u0087\3\2\2\2-\u0089\3\2\2\2/\u0091\3")
+        buf.write("\2\2\2\61\u0095\3\2\2\2\63\u00a1\3\2\2\2\65\66\7p\2\2")
+        buf.write("\66\67\7g\2\2\678\7u\2\289\7v\2\29:\7g\2\2:;\7f\2\2;\4")
+        buf.write("\3\2\2\2<=\7t\2\2=>\7g\2\2>?\7r\2\2?@\7g\2\2@A\7c\2\2")
+        buf.write("AB\7v\2\2BC\7g\2\2CD\7f\2\2D\6\3\2\2\2EF\7q\2\2FG\7p\2")
+        buf.write("\2GH\7g\2\2HI\7q\2\2IJ\7h\2\2J\b\3\2\2\2KL\7$\2\2LM\7")
+        buf.write("$\2\2MN\7$\2\2NT\3\2\2\2OS\n\2\2\2PQ\7^\2\2QS\13\2\2\2")
+        buf.write("RO\3\2\2\2RP\3\2\2\2SV\3\2\2\2TU\3\2\2\2TR\3\2\2\2UW\3")
+        buf.write("\2\2\2VT\3\2\2\2WX\7$\2\2XY\7$\2\2YZ\7$\2\2Z\n\3\2\2\2")
+        buf.write("[a\7$\2\2\\`\n\2\2\2]^\7^\2\2^`\13\2\2\2_\\\3\2\2\2_]")
+        buf.write("\3\2\2\2`c\3\2\2\2ab\3\2\2\2a_\3\2\2\2bd\3\2\2\2ca\3\2")
+        buf.write("\2\2de\7$\2\2e\f\3\2\2\2fg\7\60\2\2g\16\3\2\2\2hi\7.\2")
+        buf.write("\2i\20\3\2\2\2jk\7<\2\2k\22\3\2\2\2lm\7=\2\2m\24\3\2\2")
+        buf.write("\2no\7\61\2\2o\26\3\2\2\2pq\7?\2\2q\30\3\2\2\2rs\7,\2")
+        buf.write("\2s\32\3\2\2\2tu\7?\2\2uv\7@\2\2v\34\3\2\2\2wx\7>\2\2")
+        buf.write("xy\7?\2\2y\36\3\2\2\2z{\7>\2\2{|\7/\2\2| \3\2\2\2}~\7")
+        buf.write("*\2\2~\"\3\2\2\2\177\u0080\7+\2\2\u0080$\3\2\2\2\u0081")
+        buf.write("\u0082\7]\2\2\u0082&\3\2\2\2\u0083\u0084\7_\2\2\u0084")
+        buf.write("(\3\2\2\2\u0085\u0086\7}\2\2\u0086*\3\2\2\2\u0087\u0088")
+        buf.write("\7\177\2\2\u0088,\3\2\2\2\u0089\u008d\t\3\2\2\u008a\u008c")
+        buf.write("\t\4\2\2\u008b\u008a\3\2\2\2\u008c\u008f\3\2\2\2\u008d")
+        buf.write("\u008b\3\2\2\2\u008d\u008e\3\2\2\2\u008e.\3\2\2\2\u008f")
+        buf.write("\u008d\3\2\2\2\u0090\u0092\t\5\2\2\u0091\u0090\3\2\2\2")
+        buf.write("\u0092\u0093\3\2\2\2\u0093\u0091\3\2\2\2\u0093\u0094\3")
+        buf.write("\2\2\2\u0094\60\3\2\2\2\u0095\u0096\7\61\2\2\u0096\u0097")
+        buf.write("\7\61\2\2\u0097\u009b\3\2\2\2\u0098\u009a\n\6\2\2\u0099")
+        buf.write("\u0098\3\2\2\2\u009a\u009d\3\2\2\2\u009b\u0099\3\2\2\2")
+        buf.write("\u009b\u009c\3\2\2\2\u009c\u009e\3\2\2\2\u009d\u009b\3")
+        buf.write("\2\2\2\u009e\u009f\b\31\2\2\u009f\62\3\2\2\2\u00a0\u00a2")
+        buf.write("\t\7\2\2\u00a1\u00a0\3\2\2\2\u00a2\u00a3\3\2\2\2\u00a3")
+        buf.write("\u00a1\3\2\2\2\u00a3\u00a4\3\2\2\2\u00a4\u00a5\3\2\2\2")
+        buf.write("\u00a5\u00a6\b\32\2\2\u00a6\64\3\2\2\2\13\2RT_a\u008d")
+        buf.write("\u0093\u009b\u00a3\3\2\3\2")
+        return buf.getvalue()
+
+
+class HedgehogProtocolLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    T__1 = 2
+    T__2 = 3
+    DOCSTRING = 4
+    STRING = 5
+    DOT = 6
+    COMMA = 7
+    COLON = 8
+    SEMI = 9
+    SLASH = 10
+    EQUALS = 11
+    STAR = 12
+    REQ = 13
+    REP = 14
+    UPD = 15
+    LPAREN = 16
+    RPAREN = 17
+    LBRACKET = 18
+    RBRACKET = 19
+    LBRACE = 20
+    RBRACE = 21
+    IDENTIFIER = 22
+    NUMBER = 23
+    COMMENT = 24
+    WS = 25
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'nested'", "'repeated'", "'oneof'", "'.'", "','", "':'", "';'", 
+            "'/'", "'='", "'*'", "'=>'", "'<='", "'<-'", "'('", "')'", "'['", 
+            "']'", "'{'", "'}'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "DOCSTRING", "STRING", "DOT", "COMMA", "COLON", "SEMI", "SLASH", 
+            "EQUALS", "STAR", "REQ", "REP", "UPD", "LPAREN", "RPAREN", "LBRACKET", 
+            "RBRACKET", "LBRACE", "RBRACE", "IDENTIFIER", "NUMBER", "COMMENT", 
+            "WS" ]
+
+    ruleNames = [ "T__0", "T__1", "T__2", "DOCSTRING", "STRING", "DOT", 
+                  "COMMA", "COLON", "SEMI", "SLASH", "EQUALS", "STAR", "REQ", 
+                  "REP", "UPD", "LPAREN", "RPAREN", "LBRACKET", "RBRACKET", 
+                  "LBRACE", "RBRACE", "IDENTIFIER", "NUMBER", "COMMENT", 
+                  "WS" ]
+
+    grammarFileName = "HedgehogProtocol.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.7")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
