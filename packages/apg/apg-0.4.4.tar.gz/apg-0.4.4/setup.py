afc7d10063a133f288ced3318d3d1c3765506790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:29 +0000

    Add package apg/0.4.4/apg-0.4.4.tar.gz

---
+++ packages/apg/apg-0.4.4.tar.gz/apg-0.4.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='apg',
+    version='0.4.4',
+    description='Awesome project generation tool',
+    author='Y-Bro',
+    url='https://github.com/n0nSmoker/apg',
+    keywords=['apg', 'generate project', 'framework', 'cookie-cutter'],
+    packages=['apg', 'apg.utils'],
+    py_modules=['apg', 'apg.utils'],
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'PyYAML',
+        'cookiecutter'
+    ],
+    entry_points='''
+        [console_scripts]
+        apg=apg.run:cli
+    ''',
+)
