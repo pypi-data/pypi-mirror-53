30bb909a3d787a6dc1b17b30e4544a4f54e875fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:17 +0000

    Add package PySimultan/0.0.82/PySimultan-0.0.82-py3-none-any.whl

---
+++ packages/PySimultan/PySimultan-0.0.82-py3-none-any.whl/PySimultan/config.py
@@ -0,0 +1,20 @@
+
+global layers
+global vertices
+global edges
+global edge_loops
+global faces
+global zones
+global materials
+global mat_layers
+global parts
+
+layers = list()
+vertices = list()
+edges = list()
+edge_loops = list()
+faces = list()
+zones = list()
+materials = list()
+mat_layers = list()
+parts = list()
