6ba96032fec9f6047128bb1319e2262a60c141c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:29 +0000

    Add package sun-test/0.2/sun_test-0.2.tar.gz

---
+++ packages/sun-test/sun_test-0.2.tar.gz/sun_test-0.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r",encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sun_test",
+    version = "0.2",
+    description='测试',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='sun',
+    author_email='syw1894@163.com',
+    include_package_data=True,
+    url='https://pypi.org/project/adsl2/',
+    packages=find_packages('main.py'),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities'
+    ],
+)
