13c2300980a106f08a242f74c37664df1110eb4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package infi.recipe.python/0.6.24/infi.recipe.python-0.6.24-py2-none-any.whl

---
+++ packages/infi.recipe.python/infi.recipe.python-0.6.24-py2-none-any.whl/infi/recipe/python/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.6.24"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.6.24)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ce9efb4e1d14a6be5f179b20de7354eb2b7ec19a'
+__git_head_subject__ = u'Finished Release v0.6.24'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2019-09-25 17:55:23'
