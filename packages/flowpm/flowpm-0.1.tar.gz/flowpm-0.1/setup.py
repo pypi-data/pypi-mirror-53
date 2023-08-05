3ee52e4497243e48cb1159e8ab098cbbea369b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:19 +0000

    Add package flowpm/0.1/flowpm-0.1.tar.gz

---
+++ packages/flowpm/flowpm-0.1.tar.gz/flowpm-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='flowpm',
+      version='0.1',
+      description='Particle Mesh Simulation in TensorFlow',
+      url='https://github.com/modichirag/flowpm',
+      author='Chirag Modi',
+      author_email='modichirag@berkeley.edu',
+      license='MIT',
+      packages=['flowpm'],
+      install_requires=['astropy', 'scipy', 'tensorflow'],
+      tests_require=['fastpm'],
+      extras_require={
+        'testing':  ["fastpm"],
+      })
