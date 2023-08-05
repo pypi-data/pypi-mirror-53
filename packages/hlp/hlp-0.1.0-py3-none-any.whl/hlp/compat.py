63216cc8c2539db727f9f5b5dbd97debc3ae473b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:47 +0000

    Add package hlp/0.1.0/hlp-0.1.0-py3-none-any.whl

---
+++ packages/hlp/hlp-0.1.0-py3-none-any.whl/hlp/compat.py
@@ -0,0 +1,25 @@
+import pkgutil
+import sys
+
+from .typing import MYPY_RUNNING
+
+if MYPY_RUNNING:
+    from typing import Any, Iterator, Optional, Tuple
+
+
+PY2 = sys.version_info < (3,)
+
+
+if PY2:
+
+    def iter_modules(path=None):
+        # type: (Optional[str]) -> Iterator[Tuple[Any, str, bool]]
+        return pkgutil.iter_modules(path)
+
+
+else:
+
+    def iter_modules(path=None):
+        # type: (Optional[str]) -> Iterator[Tuple[Any, str, bool]]
+        for info in pkgutil.iter_modules(path):
+            yield info, info.name, info.ispkg
