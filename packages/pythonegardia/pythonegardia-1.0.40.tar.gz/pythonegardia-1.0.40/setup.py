4d79e95994f037ba50e8ec22512d8856768917d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package pythonegardia/1.0.40/pythonegardia-1.0.40.tar.gz

---
+++ packages/pythonegardia/pythonegardia-1.0.40.tar.gz/pythonegardia-1.0.40/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='pythonegardia',
+      version='1.0.40',
+      description='Python 3 support for Egardia / Woonveilig alarm',
+      url='https://github.com/jeroenterheerdt/python-egardia',
+      author='Jeroen ter Heerdt',
+      license='MIT',
+      install_requires=['requests>=2.0'],
+      tests_require=['mock'],
+      test_suite='tests',
+      packages=find_packages(exclude=["dist", "*.test", "*.test.*", "test.*", "test"]),
+      zip_safe=True)
