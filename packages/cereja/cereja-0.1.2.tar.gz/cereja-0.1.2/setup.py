4255e6556f03febdf193b5db4b679ad3044bfb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package cereja/0.1.2/cereja-0.1.2.tar.gz

---
+++ packages/cereja/cereja-0.1.2.tar.gz/cereja-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cereja",
+    version="0.1.2",
+    author="Joab Leite",
+    author_email="jlsn1@ifal.edu.br",
+    description="My Utils",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jlsneto/cerejeira",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
