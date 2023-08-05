e4cec10b1f46572ab6b5fe8e576cac7a5ebd508f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:17 +0000

    Add package spikely/0.5.2/spikely-0.5.2-py3-none-any.whl

---
+++ packages/spikely/spikely-0.5.2-py3-none-any.whl/spikely/parameter_model.py
@@ -0,0 +1,201 @@
+import re
+
+import numpy as np
+import PyQt5.QtCore as qc
+import PyQt5.QtGui as qg
+import PyQt5.QtWidgets as qw
+
+from . import config
+
+
+# An MVC model representation of an element's parameters
+class ParameterModel(qc.QAbstractTableModel):
+
+    def __init__(self):
+        self._element = None
+        super().__init__()
+
+    # Pythonic setters/getters
+    @property
+    def element(self):
+        return self._element
+
+    @element.setter
+    def element(self, element):
+        # Ensures dependent views are signaled on element changes
+        self.beginResetModel()
+        self._element = element
+        self.endResetModel()
+
+    #
+    # QAbstractTableModel Methods
+    #
+
+    # Count of parameters associated with an element instance
+    def rowCount(self, parent=qc.QModelIndex()):
+        return 0 if self._element is None else len(self._element.param_list)
+
+    # Number of display columns: Parameter, Type, Value
+    def columnCount(self, parent=qc.QModelIndex()):
+        return 3
+
+    # Sets UI policy for columns in table view for element parameters
+    def flags(self, mod_index):
+        column_flags = qc.QAbstractTableModel.flags(self, mod_index)
+        col = mod_index.column()
+
+        # Parameter and Type column cells are read only
+        if col == config.PARAM_COL or col == config.TYPE_COL:
+            column_flags ^= qc.Qt.ItemIsSelectable
+        elif col == config.VALUE_COL:
+            column_flags |= qc.Qt.ItemIsEditable
+        return column_flags
+
+    # Called by Views, get element parameter data based on index and role
+    def data(self, mod_index, role=qc.Qt.DisplayRole):
+        col, row = mod_index.column(), mod_index.row()
+        param_dict = self._element.param_list[row]
+
+        # If no actual result return empty value indicator
+        result = qc.QVariant()
+
+        if role == qc.Qt.DisplayRole or role == qc.Qt.EditRole:
+            if col == config.PARAM_COL:
+                result = param_dict['name']
+            elif col == config.TYPE_COL:
+                result = param_dict['type']
+            elif col == config.VALUE_COL:
+                if 'value' in param_dict.keys():
+                    result = str(param_dict['value']).strip()
+                    if not result and 'default' in param_dict.keys():
+                        result = str(param_dict['default'])
+
+        elif role == qc.Qt.ToolTipRole:
+            if col == config.PARAM_COL and 'title' in param_dict.keys():
+                result = param_dict['title']
+            elif col == config.TYPE_COL:
+                type_str = param_dict['type']
+                if type_str == 'int':
+                    result = 'integer'
+                elif type_str == 'float':
+                    result = 'floating point'
+                elif type_str == 'bool':
+                    result = 'boolean'
+                elif type_str == 'int_list':
+                    result = 'list of integers'
+                elif type_str == 'str':
+                    result = 'text string'
+                elif type_str == 'file':
+                    result = 'pathname of file'
+                elif type_str == 'folder':
+                    result = 'pathname of folder'
+                elif type_str == 'file_or_folder':
+                    result = 'pathname of file or folder'
+                elif type_str == 'int_list_list':
+                    result = 'list of a int_lists'
+                elif type_str == 'dtype':
+                    result = 'numpy dtype object (e.g., int32)'
+
+        # Paints cell red if mandatory parameter value is missing.
+        # Mandatory parameters are those with no default keys
+        elif role == qc.Qt.BackgroundRole:
+            if col == config.VALUE_COL:
+                if ('value' not in param_dict.keys() and
+                        'default' not in param_dict.keys()):
+                    result = qg.QBrush(qg.QColor(255, 192, 192))
+
+        return result
+
+    def headerData(self, section, orientation, role):
+        result = qc.QVariant()
+        if (orientation == qc.Qt.Horizontal and
+                (role == qc.Qt.DisplayRole or role == qc.Qt.EditRole)):
+            result = ['Parameter', 'Type', 'Value'][section]
+
+        return result
+
+    # Called after user edits parameter value to keep model in sync
+    def setData(self, mod_index, value, role=qc.Qt.EditRole):
+        row = mod_index.row()
+        param_dict = self._element.param_list[row]
+        success = True
+
+        # This is a little tricky - if user enters valid value assign it to
+        # 'value' in the param dictionary.  If user enters nothing assign
+        # 'default' to 'value' if 'default' exists, otherwise delete 'value'
+        # from param dictionary.  Talk to Cole if you don't like this.
+        if role == qc.Qt.EditRole:
+            if value.strip():
+                success, cvt_value = self._convert_value(
+                    param_dict['type'], value)
+                if success:
+                    param_dict['value'] = cvt_value
+            else:
+                if 'default' in param_dict.keys():
+                    param_dict['value'] = param_dict['default']
+                else:
+                    param_dict.pop('value', None)
+
+        return success
+
+    #
+    # Helper Methods
+    #
+
+    def _convert_value(self, type_str, value):
+        success, cvt_value = True, None
+        try:
+            if value == 'None':
+                cvt_value = None
+
+            elif type_str in ['str', 'file', 'folder', 'file_or_folder']:
+                cvt_value = value
+
+            elif type_str == 'int':
+                if value == 'inf':
+                    cvt_value = float(value)
+                else:
+                    cvt_value = int(value)
+
+            elif type_str == 'float':
+                cvt_value = float(value)
+
+            elif type_str == 'int_list':
+                cvt_value = self._str_list_to_int_list(value)
+
+            elif type_str == 'int_list_list':
+                # Strip outer sq brackets: '[[1,2],[3,4]]' -> '[1,2],[3,4]'
+                value = re.sub(r'^\[|\]$', '', value)
+                # Disambiguate list separator: '[1,2],[3,4]' -> [1,2]:[3,4]'
+                value = re.sub(r'\] *, *\[', ':', value)
+                # Split into int_list strings and convert into int_lists
+                cvt_value = list(map(
+                    self._str_list_to_int_list, value.split(':')))
+
+            elif type_str == 'bool':
+                if value.lower() in ['true', 'yes']:
+                    cvt_value = True
+                elif value.lower() in ['false', 'no']:
+                    cvt_value = False
+                else:
+                    raise TypeError(f'{value} is not a valid bool type')
+
+            elif type_str == 'dtype':
+                cvt_value = np.dtype(value)
+
+            else:
+                raise TypeError(f'{type_str} is not a Spikely supported type')
+
+        except (TypeError, ValueError) as err:
+            qw.QMessageBox.warning(
+                config.main_window, 'Type Conversion Error', repr(err))
+            success = False
+
+        return success, cvt_value
+
+    # E.g., '[1,2,3]' -> [1,2,3]
+    def _str_list_to_int_list(self, str_list):
+        # Set up split by stripping square brackets
+        str_list = re.sub(r'^\[|\]$', '', str_list)
+        # Convert list of string ints into actual ints
+        return list(map(int, str_list.split(',')))
---
+++ packages/spikely/spikely-0.5.2-py3-none-any.whl/spikely/pipeline_model.py
@@ -0,0 +1,144 @@
+import multiprocessing as mp
+
+import PyQt5.QtCore as qc
+import PyQt5.QtWidgets as qw
+
+from . import config
+from .elements import spike_element as sp_spe
+from .elements import std_element_policy as sp_ste
+
+
+class PipelineModel(qc.QAbstractListModel):
+
+    def __init__(self, parameter_model):
+        super().__init__()
+
+        self._elements = []
+        self._element_policy = sp_ste.StdElementPolicy()
+        self._parameter_model = parameter_model
+
+    def _elem_cls_count(self, target_cls):
+        elem_cls_list = [type(elem) for elem in self._elements]
+        return elem_cls_list.count(target_cls)
+
+    def rowCount(self, parent=None):
+        # Subclassed from base: Count of elements in model
+
+        return len(self._elements)
+
+    def data(self, mod_index, role=qc.Qt.DisplayRole):
+        # Subclassed from base: Returns role specific data from elem at index
+
+        if mod_index.isValid() and mod_index.row() < len(self._elements):
+            element = self._elements[mod_index.row()]
+            data_dict = {
+                qc.Qt.DisplayRole:      element.display_name,
+                qc.Qt.EditRole:         element.display_name,
+                qc.Qt.DecorationRole:   element.display_icon,
+                config.ELEMENT_ROLE:    element}
+
+        return None if not data_dict else data_dict.get(role)
+
+    # Methods called by app to manipulate and operate pipeline
+
+    def run(self):
+        bad_param_count = self._bad_param_count()
+        if bad_param_count:
+            qw.QMessageBox.warning(
+                config.find_main_window(), 'Run Failure',
+                f'Missing mandatory element parameters.  Missing parameter '
+                f'count: {bad_param_count}')
+            return
+
+        for cls in self._element_policy.required_cls_list:
+            if not self._elem_cls_count(cls):
+                qw.QMessageBox.warning(
+                    config.find_main_window(), 'Run Failure',
+                    f'Missing required element: {cls.__name__}')
+                return
+
+        config.find_main_window().statusBar().showMessage(
+            'Running pipeline', config.STATUS_MSG_TIMEOUT)
+
+        p = mp.Process(target=self.async_run)
+        p.start()
+
+    def async_run(self):
+        input_payload = None
+        element_count = len(self._elements)
+
+        for i in range(0, element_count):
+            next_element = self._elements[i+1] \
+                if i < (element_count - 1) else None
+            input_payload = self._elements[i].run(
+                input_payload, next_element
+            )
+
+    def clear(self):
+        self.beginResetModel()
+        self._elements.clear()
+        self.endResetModel()
+        # Synchronize parameter model and view
+        self._parameter_model.element = None
+
+    def add_element(self, add_elem: sp_spe.SpikeElement) -> None:
+        add_cls = type(add_elem)
+        if self._element_policy.is_cls_singleton(add_cls) \
+                and self._elem_cls_count(add_cls):
+            config.find_main_window().statusBar().showMessage(
+                'Only one element of that type allowed in pipeline',
+                config.STATUS_MSG_TIMEOUT)
+            return
+
+        rank = self._element_policy.cls_order_dict
+        add_row = 0
+        while add_row < len(self._elements) \
+                and rank[type(add_elem)] > rank[type(self._elements[add_row])]:
+            add_row += 1
+
+        self.beginInsertRows(qc.QModelIndex(), add_row, add_row)
+        self._elements.insert(add_row, add_elem)
+        self.endInsertRows()
+
+    def move_up(self, elem: sp_spe.SpikeElement) -> None:
+        rank = self._element_policy.cls_order_dict
+        row = self._elements.index(elem)
+
+        if row > 0 and rank[type(elem)] == rank[type(self._elements[row - 1])]:
+            self.beginMoveRows(qc.QModelIndex(), row,
+                row, qc.QModelIndex(), row - 1)  # noqa: E128
+            self._swap(self._elements, row, row - 1)
+            self.endMoveRows()
+        else:
+            config.find_main_window().statusBar().showMessage(
+                "Cannot move element any higher", config.STATUS_MSG_TIMEOUT)
+
+    def move_down(self, elem: sp_spe.SpikeElement) -> None:
+        rank = self._element_policy.cls_order_dict
+        row = self._elements.index(elem)
+
+        if row < len(self._elements) - 1 and \
+                rank[type(elem)] == rank[type(self._elements[row + 1])]:
+            self.beginMoveRows(qc.QModelIndex(), row + 1,
+                row + 1, qc.QModelIndex(), row)   # noqa: E128
+            self._swap(self._elements, row, row + 1)
+            self.endMoveRows()
+        else:
+            config.find_main_window().statusBar().showMessage(
+                "Cannot move element any lower", config.STATUS_MSG_TIMEOUT)
+
+    def delete(self, element: sp_spe.SpikeElement) -> None:
+        index = self._elements.index(element)
+        self.beginRemoveRows(qc.QModelIndex(), index, index)
+        self._elements.pop(index)
+        self.endRemoveRows()
+
+    # Helper methods
+
+    def _swap(self, list, pos1, pos2):
+        list[pos1], list[pos2] = list[pos2], list[pos1]
+
+    def _bad_param_count(self) -> int:
+        bad_list = [param for elem in self._elements for param in
+                    elem.param_list if 'value' not in param.keys()]
+        return len(bad_list)
