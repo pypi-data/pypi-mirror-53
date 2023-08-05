5114a144688bbe8419c6ea368787e805c6a7be58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:58 +0000

    Add package biogeme/3.2.2/biogeme-3.2.2.tar.gz

---
+++ packages/biogeme/biogeme-3.2.2.tar.gz/biogeme-3.2.2/tests/functions/headers.py
@@ -0,0 +1,9 @@
+from biogeme.expressions import *
+Person=Variable('Person')
+Exclude=Variable('Exclude')
+Variable1=Variable('Variable1')
+Variable2=Variable('Variable2')
+Choice=Variable('Choice')
+Av1=Variable('Av1')
+Av2=Variable('Av2')
+Av3=Variable('Av3')
