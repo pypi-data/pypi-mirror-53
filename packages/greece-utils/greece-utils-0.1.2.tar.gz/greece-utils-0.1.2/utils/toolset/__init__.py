70fdaa1f3b8631fbfaaef0013cf364ab642ffdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:09 +0000

    Add package greece-utils/0.1.2/greece-utils-0.1.2.tar.gz

---
+++ packages/greece-utils/greece-utils-0.1.2.tar.gz/greece-utils-0.1.2/utils/toolset/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+""" Module summary description.
+
+More detailed description.
+"""
+
+# __all__ = []
+# __version__ = '0.1'
+__author__ = 'Benjamin Pillot'
+__copyright__ = 'Copyright 2018, Benjamin Pillot'
+__email__ = 'benjaminpillot@riseup.net'
+
+
+from utils.toolset.list import *
