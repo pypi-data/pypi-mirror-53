9d8c23e9baf2ea411636c4b1a17ad614343381fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:20 +0000

    Add package motionSegmentation/2.2.5/motionSegmentation-2.2.5.tar.gz

---
+++ packages/motionSegmentation/motionSegmentation-2.2.5.tar.gz/motionSegmentation-2.2.5/motionSegmentation/__init__.py
@@ -0,0 +1,52 @@
+'''
+File: __init__.py
+Description: load all class for bfmotionsolver
+             Contains linker to main classes
+History:
+    Date    Programmer SAR# - Description
+    ---------- ---------- ----------------------------
+  Author: w.x.chan@gmail.com         31JAN2018           - Created
+Author: w.x.chan@gmail.com         31JAN2019           - v1.2.0
+						-bfSolver version 1.0.0
+						-BsplineFourier version 1.2.0
+Author: w.x.chan@gmail.com         12SEP2019           - v2.0.0
+						-bfSolver version 2.0.0
+						-BsplineFourier version 2.0.0
+Author: w.x.chan@gmail.com         12SEP2019           - v2.1.0
+						-bfSolver version 2.1.0
+						-BsplineFourier version 2.0.0
+Author: w.x.chan@gmail.com         17SEP2019           - v2.2.3
+						-bfSolver version 2.1.0
+						-BsplineFourier version 2.2.0
+Author: w.x.chan@gmail.com         23SEP2019           - v2.2.4
+						-bfSolver version 2.2.4
+						-BsplineFourier version 2.2.0
+Author: jorry.zhengyu@gmail.com    26SEP2019           - v2.2.5
+						-bfSolver version 2.2.4
+						-BsplineFourier version 2.2.5
+Requirements:
+    autoD
+    numpy
+    re
+    scipy
+    BsplineFourier
+    pickle (optional)
+
+Known Bug:
+    HSV color format not supported
+All rights reserved.
+'''
+_version='2.2.5'
+print('motionSegmentation version',_version)
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+import bfSolver as solver
+import BsplineFourier as bsfourier
+
+#linker to classes
+BFSolver=solver.bfSolver
+BsplineFourier=bsfourier.BsplineFourier
+Bspline=bsfourier.Bspline
