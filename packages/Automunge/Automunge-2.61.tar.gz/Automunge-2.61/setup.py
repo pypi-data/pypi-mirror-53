773dba4c239585a1d614bf64712ea4462b4fb6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:58 +0000

    Add package Automunge/2.61/Automunge-2.61.tar.gz

---
+++ packages/Automunge/Automunge-2.61.tar.gz/Automunge-2.61/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Automunge",
+    version="2.61",
+    author="Nicholas Teague",
+    author_email="pitg888@gmail.com",
+    description="A tool for automated data wrangling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Automunge/AutoMunge",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
