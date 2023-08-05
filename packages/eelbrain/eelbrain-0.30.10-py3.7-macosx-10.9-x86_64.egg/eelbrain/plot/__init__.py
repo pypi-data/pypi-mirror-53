0eb1f08d01fc23030ac145ece8f4863d6d454d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:38 +0000

    Add package eelbrain/0.30.10/eelbrain-0.30.10-py3.7-macosx-10.9-x86_64.egg

---
+++ packages/eelbrain/eelbrain-0.30.10-py3.7-macosx-10.9-x86_64.egg/eelbrain/plot/__init__.py
@@ -0,0 +1,21 @@
+"""Plotting for data-objects"""
+# Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+
+from ._base import reset_rc
+from ._colors import (
+    ColorBar, ColorGrid, ColorList,
+    colors_for_categorial, colors_for_oneway, colors_for_twoway,
+    single_hue_colormap,
+)
+from ._figure import Figure
+from ._glassbrain import GlassBrain
+from ._line import LineStack
+from ._sensors import SensorMaps, SensorMap
+from ._topo import TopoArray, TopoButterfly, Topomap, TopomapBins
+from ._uts import UTSStat, UTS, UTSClusters
+from ._utsnd import Array, Butterfly
+from ._uv import (
+    Barplot, Boxplot, Correlation, Histogram, PairwiseLegend, Regression,
+    Timeplot,
+)
+from . import brain
