aa1b4b31857811664de4f93748f80b9ddc1e58d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:42 +0000

    Add package spiderutil/0.1.3/spiderutil-0.1.3.tar.gz

---
+++ packages/spiderutil/spiderutil-0.1.3.tar.gz/spiderutil-0.1.3/spiderutil/structure/dict.py
@@ -0,0 +1,20 @@
+class Dict:
+    def __init__(self, structure=None, **kwargs):
+        if type(structure) is dict:
+            for k, v in kwargs:
+                structure[k] = v
+        self.__initial__ = structure
+
+    def __new__(cls, structure=None, **kwargs):
+        self = super(Dict, cls).__new__(cls)
+        self.__init__(structure=structure, **kwargs)
+        if type(self.__initial__) is dict:
+            self.__dict__ = {key: Dict(self.__initial__[key]) for key in self.__initial__}
+        elif type(self.__initial__) is list:
+            self = [Dict(item) for item in self.__initial__]
+        else:
+            self = self.__initial__
+        return self
+
+    def __str__(self):
+        return str({k: v for k, v in self.__dict__.items() if k[0] != '_'})
