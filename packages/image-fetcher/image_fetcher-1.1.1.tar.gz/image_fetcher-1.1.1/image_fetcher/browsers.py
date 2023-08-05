41984c79e75ce66e7e6ff7410c664de69b824955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:02 +0000

    Add package image-fetcher/1.1.1/image_fetcher-1.1.1.tar.gz

---
+++ packages/image-fetcher/image_fetcher-1.1.1.tar.gz/image_fetcher-1.1.1/image_fetcher/browsers.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from enum import Enum
+from os.path import abspath
+from os import listdir
+
+
+class BrowserType(Enum):
+    CHROME = 1
+    FIREFOX = 2
+
+
+def validate_driver(driver_path):
+    if "/" in driver_path:
+        driver = driver_path.split("/")[-1]
+        directory = driver_path[0:len(driver_path)-len(driver)-1]
+        if not driver in listdir(directory):
+            raise ValueError(driver+" not found in "+directory)
+    else:
+        driver = driver_path
+        if not driver in listdir():
+            raise ValueError(driver+" not found in current directory")
+
+
+class Browser:
+    def __init__(self, browser_type, driver):
+        validate_driver(driver)
+        if browser_type == BrowserType.CHROME:
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--headless')
+            self.webdriver = webdriver.Chrome(driver, options=chrome_options)
+        elif browser_type == BrowserType.FIREFOX:
+            firefox_options = webdriver.firefox.options.Options()
+            firefox_options.headless = True
+            self.webdriver = webdriver.Firefox(executable_path=abspath(driver), options=firefox_options)
+        else:
+            raise ValueError("driver_type not valid, choose from: "+str(list(Browser)))
