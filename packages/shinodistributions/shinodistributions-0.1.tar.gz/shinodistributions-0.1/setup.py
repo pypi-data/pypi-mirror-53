f3d84060f7171b00e53a0b58e865e0d7429c32c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:48 +0000

    Add package shinodistributions/0.1/shinodistributions-0.1.tar.gz

---
+++ packages/shinodistributions/shinodistributions-0.1.tar.gz/shinodistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='shinodistributions',
+      version='0.1',
+      description='Gaussian/Binomial distributions',
+      packages=['shinodistributions'],
+      zip_safe=False)
