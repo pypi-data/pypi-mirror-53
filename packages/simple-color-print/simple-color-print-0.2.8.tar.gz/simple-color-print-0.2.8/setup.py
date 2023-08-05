62cd3d129f151fdba48cbdc8a9d6e32d8f626684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:49 +0000

    Add package simple-color-print/0.2.8/simple-color-print-0.2.8.tar.gz

---
+++ packages/simple-color-print/simple-color-print-0.2.8.tar.gz/simple-color-print-0.2.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple-color-print',
+    version='0.2.8',
+    packages=find_packages(),
+    description='Simple color print',
+    # long_description=read('README.md'),
+    author='bzdvdn',
+    author_email='bzdv.dn@gmail.com',
+    url='https://github.com/bzdvdn/simple-color-print',
+    license='MIT',
+    python_requires=">=3.6",
+)
