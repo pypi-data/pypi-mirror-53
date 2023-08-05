b2212385b2b82f54caa91743dd18e0f200bc8e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:52 +0000

    Add package AddPack/1.0/AddPack-1.0.tar.gz

---
+++ packages/AddPack/AddPack-1.0.tar.gz/AddPack-1.0/README.md
@@ -0,0 +1,14 @@
+#Welcome to my package
+----------------------
+----------------------
+
+->>  This package adds two numbers.
+
+#Example:
+--------
+
+#Input:
+->>2+4
+
+#Output:
+->>6
---
+++ packages/AddPack/AddPack-1.0.tar.gz/AddPack-1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AddPack",
+    version="1.0",
+    author="Hema Negi",
+    author_email="hemanegi444@gmail.com",
+    description="A small add program package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hemanegi444/git_repository",
+    packages=['AddPack'],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
