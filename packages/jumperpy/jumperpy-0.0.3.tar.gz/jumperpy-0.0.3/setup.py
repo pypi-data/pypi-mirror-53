be009a9f3e0ea0b606667d0f37b82f47c0df6031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:06 +0000

    Add package jumperpy/0.0.3/jumperpy-0.0.3.tar.gz

---
+++ packages/jumperpy/jumperpy-0.0.3.tar.gz/jumperpy-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jumperpy",
+    version="0.0.3",
+    author="Hal Jordan",
+    author_email="hal.jordan.py@gmail.com",
+    description="Jumper-python micro framework for App Engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
