a0b22731cc54f8c903b566c07c568a5be7ede5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:13 +0000

    Add package axonius-api-client/2.0.6/axonius_api_client-2.0.6-py2.py3-none-any.whl

---
+++ packages/axonius-api-client/axonius_api_client-2.0.6-py2.py3-none-any.whl/axonius_api_client/cli/grp_objects/cmd_get.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""Command line interface for Axonius API Client."""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import click
+
+from .. import cli_constants, options, serial
+
+
+@click.command(name="get", context_settings=cli_constants.CONTEXT_SETTINGS)
+@options.OPT_URL
+@options.OPT_KEY
+@options.OPT_SECRET
+@options.OPT_EXPORT_FILE
+@options.OPT_EXPORT_PATH
+@options.OPT_EXPORT_FORMAT
+@options.OPT_EXPORT_OVERWRITE
+@options.OPT_EXPORT_DELIM
+@options.OPT_QUERY
+@options.OPT_FIELDS
+@options.OPT_FIELDS_DEFAULT
+@options.OPT_MAX_ROWS
+@click.pass_context
+def cmd(
+    ctx,
+    url,
+    key,
+    secret,
+    export_format,
+    export_file,
+    export_path,
+    export_overwrite,
+    export_delim,
+    query,
+    fields,
+    fields_default,
+    max_rows,
+):
+    """Get assets from a query."""
+    p_grp = ctx.parent.command.name
+
+    client = ctx.obj.start_client(url=url, key=key, secret=secret)
+    api = getattr(client, p_grp)
+
+    with ctx.obj.exc_wrap(wraperror=ctx.obj.wraperror):
+        raw_data = api.get(
+            query=query, fields=fields, fields_default=fields_default, max_rows=max_rows
+        )
+
+    formatters = {"json": serial.to_json, "csv": serial.obj_to_csv}
+
+    ctx.obj.handle_export(
+        raw_data=raw_data,
+        formatters=formatters,
+        export_format=export_format,
+        export_file=export_file,
+        export_path=export_path,
+        export_overwrite=export_overwrite,
+        joiner=export_delim,
+    )
