278a511b29540a6a695d6663e7259238e7bfec6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:17 +0000

    Add package z3-tracker/0.9.4/z3_tracker-0.9.4-py3-none-any.whl

---
+++ packages/z3-tracker/z3_tracker-0.9.4-py3-none-any.whl/z3tracker/config/default.py
@@ -0,0 +1,34 @@
+'''
+Presets for configuration file
+'''
+
+from ..version import __version__ as version
+
+__all__ = 'DEFAULT', 'OVERWRITE'
+
+
+DEFAULT = {
+    'gui': ('gui-tkinter', str),
+    'icon_size': ('1.0', float),
+    'map_size': ('1.0', float),
+    'autosave': ('autosave.json', str),
+    'button_layout': ('buttons.json', str),
+    'window_layout': ('windows.json', str),
+
+    'entrance_randomiser': (True, bool),
+    'world_state': ('Open', str),
+    'glitch_mode': ('None', str),
+    'item_placement': ('Advanced', str),
+    'dungeon_items': ('Standard', str),
+    'goal': ('Defeat Ganon', str),
+    'swords': ('Randomised', str),
+}
+
+
+OVERWRITE = {
+    '0.9.0': set(),
+    '0.9.1': set(),
+    '0.9.2': set(),
+    '0.9.3': set(),
+    '0.9.4': set(),
+}
