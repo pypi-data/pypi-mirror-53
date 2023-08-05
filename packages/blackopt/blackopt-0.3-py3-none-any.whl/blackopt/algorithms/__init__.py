a16e3baba5af5d6ff24a7953472c0e4c32200018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package blackopt/0.3/blackopt-0.3-py3-none-any.whl

---
+++ packages/blackopt/blackopt-0.3-py3-none-any.whl/blackopt/algorithms/__init__.py
@@ -0,0 +1,4 @@
+from blackopt.algorithms.genetic_algorithm import GeneticAlgorithm
+from blackopt.algorithms.multicore_genetic_algorithm import MulticoreGeneticAlgorithm
+from blackopt.algorithms.hill_climb import HillClimber
+from blackopt.algorithms.random_search import RandomSearch, MulticoreRS
---
+++ packages/blackopt/blackopt-0.3-py3-none-any.whl/blackopt/examples/launchers/solve_tsp.py
@@ -0,0 +1,15 @@
+from blackopt.examples.problems import TspProblem, TspSolution
+from blackopt.algorithms import RandomSearch, MulticoreRS, MulticoreGeneticAlgorithm, GeneticAlgorithm
+from blackopt.algorithms import GeneticAlgorithm
+from blackopt.util.document import generate_report
+
+
+import time
+
+problem = TspProblem.random_problem(2, 200)
+
+solver = MulticoreGeneticAlgorithm(problem, TspSolution, 15, 1/200, 1)
+t = time.time()
+solver.solve(100000)
+print("Duration: ", time.time() - t)
+generate_report(problem, {solver: solver.metrics})
