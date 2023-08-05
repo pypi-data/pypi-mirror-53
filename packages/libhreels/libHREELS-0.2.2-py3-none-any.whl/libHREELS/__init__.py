ef282586a06c1198cf13bf8f8a18aa8b3032bdf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:05:54 +0000

    Add package libhreels/0.2.2/libHREELS-0.2.2-py3-none-any.whl

---
+++ packages/libhreels/libHREELS-0.2.2-py3-none-any.whl/libHREELS/__init__.py
@@ -0,0 +1,12 @@
+# necessary for this directory to be a module
+# __init__.py
+
+from libHREELS.HREELS import HREELS
+from libHREELS.HREELS import offset, createDataDictionary, HREELS_elog_Dictionary
+from libHREELS.LEED import LEED, LEED_elog_Dictionary
+from libHREELS.Auger.Auger import Auger, Auger_elog_Dictionary
+from libHREELS.ViewHREELS import HREELS_Window
+import libHREELS.ViewHREELS as ViewHREELS
+# __all__ = ["HREELS","LEED", "Auger/Auger", "ViewHREELS"]
+
+__version__ = '0.2.0'
