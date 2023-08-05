64a8471a748ff0cd72a34af00ee0153968a2e1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:54 +0000

    Add package taboo/0.8.4/taboo-0.8.4.tar.gz

---
+++ packages/taboo/taboo-0.8.4.tar.gz/taboo-0.8.4/README.md
@@ -0,0 +1,5 @@
+# Taboo
+
+This is the procject for taboo. We are still working on the project.
+
+We will try to finish it soon.
---
+++ packages/taboo/taboo-0.8.4.tar.gz/taboo-0.8.4/taboo/ruler.py
@@ -0,0 +1,9 @@
+class TabooStupidRuler:
+    def __init__(self):
+        pass
+
+    def check_relevance(self, sent, sentences):
+        return True
+
+    def check_available(self, sent):
+        return True
