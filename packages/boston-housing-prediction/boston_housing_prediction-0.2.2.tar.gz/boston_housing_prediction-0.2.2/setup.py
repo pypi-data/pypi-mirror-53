74cd0327fd7341f76601e8b8214646a5333648de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:56 +0000

    Add package boston-housing-prediction/0.2.2/boston_housing_prediction-0.2.2.tar.gz

---
+++ packages/boston-housing-prediction/boston_housing_prediction-0.2.2.tar.gz/boston_housing_prediction-0.2.2/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README_PYPI.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+	# Information about project
+    name="boston_housing_prediction",
+    version="0.2.2",
+    author="Lupos",
+    author_email="buisnessgithublupos@gmail.com",
+	url="https://github.com/LuposX/BostonHousingPrediction",
+	license="MIT",
+	keywords="regression meachine-learning housing_prices_boston learning_purpose",
+
+	# Description
+	description="Predict housing prices in boston.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+	# What is in our module/scripts
+	scripts=["boston_housing_prediction/__main__.py"],
+	packages=["boston_housing_prediction"],
+    entry_points = {
+        'console_scripts': [
+            'boston_housing_prediction = boston_housing_prediction.__main__'
+        ]
+    },
+
+	# Dependence and required stuff
+	install_requires=requirements,
+    python_requires='>=3.5',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+		'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
