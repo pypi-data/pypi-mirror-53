7b196520bccfa68f721c7d45521ddf3ff98ac4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:23 +0000

    Add package tiny-tf/0.9.5/tiny-tf-0.9.5.tar.gz

---
+++ packages/tiny-tf/tiny-tf-0.9.5.tar.gz/tiny-tf-0.9.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1-dev'
+if 'VERSION' in os.environ:
+    version = os.environ['VERSION']
+
+setup(
+    name='tiny-tf',
+    version=version,
+    description='Shameless reimplementation of extremely limited API from Tully Foote\'s TF library',
+    author='Jariullah Safi',
+    author_email='safijari@isu.edu',
+    packages=find_packages(),
+    install_requires=['numpy'],  # Optional
+)
