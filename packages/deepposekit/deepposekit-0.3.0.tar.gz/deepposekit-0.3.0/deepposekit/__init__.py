d1d782c37d3df26bd887353edf92b3084bca95c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:14 +0000

    Add package deepposekit/0.3.0/deepposekit-0.3.0.tar.gz

---
+++ packages/deepposekit/deepposekit-0.3.0.tar.gz/deepposekit-0.3.0/deepposekit/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+import sys
+import warnings
+
+from deepposekit.io import TrainingGenerator, DataGenerator
+from deepposekit.augment.FlipAxis import FlipAxis
+
+from deepposekit.annotate.gui.Annotator import Annotator
+from deepposekit.annotate.gui.Skeleton import Skeleton
+from deepposekit.annotate.KMeansSampler import KMeansSampler
+
+from deepposekit.io.video import VideoReader, VideoWriter
+
+__version__ = "0.3.0"
---
+++ packages/deepposekit/deepposekit-0.3.0.tar.gz/deepposekit-0.3.0/deepposekit/models/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+from deepposekit.models.StackedDenseNet import StackedDenseNet
+from deepposekit.models.StackedHourglass import StackedHourglass
+from deepposekit.models.LEAP import LEAP
+from deepposekit.models.DeepLabCut import DeepLabCut
+
+from deepposekit.models.saving import save_model
+from deepposekit.models.loading import load_model
