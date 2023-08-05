bffe4e962ba5c657a35bff0d51aa08163bb11c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:01 +0000

    Add package mc-library/0.0.2/mc_library-0.0.2.tar.gz

---
+++ packages/mc-library/mc_library-0.0.2.tar.gz/mc_library-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='mc_library',
+    version='0.0.2',
+    author='Hugn王海涛',
+    data_files=[('',['mc_library.py'])
+                ],
+    include_package_data=True,
+    zip_safe=False,
+    )
