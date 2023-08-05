b3fe73ccb9aaa495535462c20d6aee27d950c99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:44 +0000

    Add package wawa-logger/1.7/wawa_logger-1.7.tar.gz

---
+++ packages/wawa-logger/wawa_logger-1.7.tar.gz/wawa_logger-1.7/README.md
@@ -0,0 +1,13 @@
+# Uploading to PyPi
+```sh
+python3 setup.py sdist
+
+twine upload  dist/*
+```
+
+# Loading into a new project
+
+```sh
+pip install --no-cache-dir wawa_logger
+```
+
---
+++ packages/wawa-logger/wawa_logger-1.7.tar.gz/wawa_logger-1.7/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name='wawa_logger',  
+    version='1.7',
+    author="Nathan Getty",
+    author_email="gettynathanj@gmail.com",
+    description="internal application",
+    packages=setuptools.find_packages()
+ )
