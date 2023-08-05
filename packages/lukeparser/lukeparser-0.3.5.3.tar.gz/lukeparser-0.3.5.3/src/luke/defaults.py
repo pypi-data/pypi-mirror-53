875f11562c8e82385564a0eda538d7a606fb8ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:25 +0000

    Add package lukeparser/0.3.5.3/lukeparser-0.3.5.3.tar.gz

---
+++ packages/lukeparser/lukeparser-0.3.5.3.tar.gz/lukeparser-0.3.5.3/src/luke/defaults.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+
+home = os.path.expanduser("~")
+config_path = os.path.join(home, ".config","lukeparser")
+
+# set defaults
+defaults = {
+    "default_theme": "default",
+    "default_html_theme": "vctheme",
+    "default_reveal_theme": "reveal",
+    "default_latex_theme": "basic",
+    "default_view": "html",
+    "config_path": config_path,
+    "theme_path": os.path.join(config_path, "themes"),
+
+    # internal
+    "overwrite_theme": False,
+    "out_here": False,
+    "copy_resources": False,
+    "resources_here": False,
+    "resources_with_file": False,
+    "resources_relative": ".",
+    "to_string": False,
+    "resources_relative_append_theme": False,
+}
+
+
+class ArgumentParserWithHelp(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
