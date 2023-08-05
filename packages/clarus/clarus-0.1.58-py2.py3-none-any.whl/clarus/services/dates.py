d0bc3aca81e9e907c697ad466e67d99687360153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:52 +0000

    Add package clarus/0.1.58/clarus-0.1.58-py2.py3-none-any.whl

---
+++ packages/clarus/clarus-0.1.58-py2.py3-none-any.whl/clarus/services/dates.py
@@ -0,0 +1,26 @@
+import clarus.services
+
+def businessdayadjustment(output=None, **params):
+    return clarus.services.api_request('Dates', 'BusinessDayAdjustment', output=output, **params)
+
+def fixingdates(output=None, **params):
+    return clarus.services.api_request('Dates', 'FixingDates', output=output, **params)
+
+def fxforwarddate(output=None, **params):
+    return clarus.services.api_request('Dates', 'FxForwardDate', output=output, **params)
+
+def fxoptiondate(output=None, **params):
+    return clarus.services.api_request('Dates', 'FxOptionDate', output=output, **params)
+
+def irdspotdate(output=None, **params):
+    return clarus.services.api_request('Dates', 'IrdSpotDate', output=output, **params)
+
+def maintenanceperiods(output=None, **params):
+    return clarus.services.api_request('Dates', 'MaintenancePeriods', output=output, **params)
+
+def schedule(output=None, **params):
+    return clarus.services.api_request('Dates', 'Schedule', output=output, **params)
+
+def swapratefixingdates(output=None, **params):
+    return clarus.services.api_request('Dates', 'SwapRateFixingDates', output=output, **params)
+
