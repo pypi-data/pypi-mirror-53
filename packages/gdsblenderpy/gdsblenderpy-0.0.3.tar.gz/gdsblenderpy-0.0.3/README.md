0cace33e7510900c073c55a8cd6134746de26f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:05 +0000

    Add package gdsblenderpy/0.0.3/gdsblenderpy-0.0.3.tar.gz

---
+++ packages/gdsblenderpy/gdsblenderpy-0.0.3.tar.gz/gdsblenderpy-0.0.3/README.md
@@ -0,0 +1,99 @@
+# gdsblenderpy
+
+Translate GDS to Blender files 
+
+# Dependencies
+
++ [Phidl](https://github.com/amccaugh/phidl)
++ [GDSPy](https://github.com/heitzmann/gdspy/)
++ NumPy
++ MatplotLib
+
+While not an explicit dependency, [KLayout](https://klayout.de) is a fantastic open source tool for viewing and creating GDS files.
+
+# Installing Blender
+Follow instructions to install [Blender](https://www.blender.org)
+
+Add blender to path, to allow for commands from the terminal.
+The executable exists in these locations (by default).
+
+## Windows
++ 64-bit
+
+`C:\Program Files\Blender Foundation\Blender\blender.exe`
+
++ 32-bit
+
+`C:\Program Files (x86)\Blender Foundation\Blender\blender.exe`
+
+## Linux
+
+`/usr/bin/blender`
+
+## MacOS
+
+`/Applications/blender/blender.app/Contents/MacOS/blender`
+
+
+# Installation of this Package
+These instructions help to install pip into the python that comes with blender, since python is not released with pip in the first place. More detail can be found [here](https://blender.stackexchange.com/questions/56011/how-to-install-pip-for-blenders-bundled-python) or with a differently worded google search.
+1. First `cd` into your blender path:
+
+    `cd /path/to/blender-2.8/2.80/python`
+
+2. Run ensurepip for your blender's python:
+
+    `/bin/python3.7m lib/python3.7/ensurepip`
+
+    * in MacOS, ./python3.7m will run the executable instead of just the filename
+
+3. Now you have pip and can install modules by:
+
+    `bin/pip3 install --target lib/python3.6   packageName`
+
+4. Install the dependencies manually:
+
+    `bin/pip3 install --target lib/python3.6   numpy`
+    
+    `bin/pip3 install --target lib/python3.6   gdspy`
+
+    `bin/pip3 install --target lib/python3.6   phidl`
+
+5. Run setup.py for this project after downloading from git (untested)
+
+## ALTERNATIVELY (WORKING)
+
+4. Install this module:
+
+    `bin/pip3 install --target lib/python3.6   gdsblenderpy`
+
+# Creating layer_stack file
+
+See the example.
+
+In essence, add the layers you are interested in capturing for your blender project, give them, initial height (z), thickness, color and an etch_target if the layer is intended to cut into another layer (as opposed to being extruded).
+
+At the end of the file (currently - will be changed in the future), add the layer stack and run the draw in blender command.
+
+Then when you run the translator like below, it should work as the example does.
+
+# Running Translator
+`blender filename.blend --python layerstack-file.py`
+
+Where the layerstack-file.py file has been created following instructions.
+See example
+
+# TODO
+## Use
+
++ Neaten plotting of cross section 
+
++ Figure out way to do dopings?
+
++ Fix plot cross section
+
+## Function
++ [Easier execution](https://blender.stackexchange.com/questions/6817/how-to-pass-command-line-arguments-to-a-blender-python-script)
+
+
+
---
+++ packages/gdsblenderpy/gdsblenderpy-0.0.3.tar.gz/gdsblenderpy-0.0.3/gdsblenderpy/importer.py
@@ -0,0 +1,276 @@
+'''
+TODO:
+-Comment the code nicely
+
+Project purpose is to import a gds and export a blender file.
+
+This project depends heavily on:
+
+PHIDL (https://github.com/amccaugh/phidl)
+gdspy (https://github.com/heitzmann/gdspy/)
+
+It is not intended as an individualistic feat, but more as a synthesis of simple
+methods to create nice pictures. 
+
+Author:
+Matthew van Niekerk
+'''
+import numpy as np
+
+try:
+    import bpy
+except:
+    print('You must run this file from the Blender build of python to ustilize interop with Blender.')
+
+from .layer import Layer, LayerStack
+import matplotlib.pyplot as plt
+import phidl.geometry as pg
+import math
+import phidl
+from phidl.device_layout import Device, DeviceReference, Port, Polygon
+import gdspy
+# when this is run from blenders python, need to import the module in this way
+import sys
+# sys.path.append('/Users/matthewvanniekerk/Documents/RIT/GIT/GDS-BLENDER-PY/gds-blender-py/')
+import os
+#test
+
+import sys
+import warnings
+
+from progressbar import *
+  
+'''
+Importer object takes a gds file as a constrcutoor
+
+'''
+class Importer(object):
+    '''
+    Parameters
+    ----------
+    filename (string) - the filename of the gds file
+    layerstack (LayerStack) - Layer Stack for this gds
+    '''
+    def __init__(self, filename, layerstack, cellname = None):
+
+        self.filename = filename
+        self.cellname = cellname
+        self.layerstack = layerstack
+        self.gds = self._import_gds()
+        self._etch_target_helper()
+        self.vertices = self._extract_vertices()
+
+
+    '''
+    Paramters
+    ---------
+    filename (string) - the filename of the GDS
+
+    Returns
+    -------
+    D (list) - list of Device objects
+
+    Purpose
+    -------
+    Import any gds file and select the largest top cell as the top cell.
+    '''
+    def _import_gds(self):
+        gdsii_lib = gdspy.GdsLibrary()
+        gdsii_lib.read_gds(self.filename)
+        top_level_cells = gdsii_lib.top_level()
+        cellname = self.cellname 
+        if cellname is not None:
+            if cellname not in gdsii_lib.cell_dict:
+                raise ValueError('The requested cell (named %s) is not present in file %s' % (cellname,self.filename))
+            topcell = gdsii_lib.cell_dict[cellname]
+        elif cellname is None and len(top_level_cells) == 1:
+            topcell = top_level_cells[0]
+        elif cellname is None and len(top_level_cells) > 1:
+            areas = []
+            for cell in top_level_cells:
+                areas.append(cell.area())
+            ind = areas.index(max(areas))
+            topcell = top_level_cells[ind]
+            
+        D = Device('import_gds')
+        polygons = topcell.get_polygons(by_spec = True)
+
+        for layer_in_gds, polys in polygons.items():
+            D.add_polygon(polys, layer = layer_in_gds)
+        return D
+    '''
+    Remove the drawn etch layers 
+    '''
+    def _remove_etch_layers(self):
+        
+        for lay in self.layerstack.etch_layers:
+            self.gds.remove_layers(layers = (lay.layer,lay.datatype))
+
+    '''
+    Take the etch layer, boolean difference the two layers, and add 
+    that layer to the layer that was removed.
+    '''
+    def _etch_target_helper(self):
+
+        etch_layers = []
+        etch_targets = []
+        layer_target = []
+        for elay in self.layerstack.etch_layers:
+            etch_layers.append(self.gds.get_polygons(by_spec = True, depth = None)[(elay.layer,elay.datatype)])
+            layer_target.append((elay.layer,elay.datatype))
+        for etar in self.layerstack.etch_targets:
+            etch_targets.append(self.gds.get_polygons(by_spec = True, depth = None)[(etar.layer,etar.datatype)])
+            
+        # Do this with gdspy! dont go back to Device.
+
+        etch_layers,etch_targets
+        new_polygons = []
+        for i in range(len(etch_targets)):
+            p = gdspy.fast_boolean(operand1 = etch_targets[i], \
+                                operand2 = etch_layers[i], \
+                                operation = 'not', \
+                                precision = 1e-9, \
+                                max_points = 4000,\
+                                layer = layer_target[i][0],\
+                                datatype = layer_target[i][1] )
+            new_polygons.append(p)
+        self._remove_etch_layers()
+
+        for i in range(len(new_polygons)):
+            self.gds.add_polygon(new_polygons[i],layer = layer_target[i])
+
+    def _extract_vertices(self):
+
+        extracted = {}
+        points = []
+        layers = []
+        gds = self.gds
+        
+        if gds is not list: gds = [gds]
+        for item in gds:
+            if isinstance(item, (Device, DeviceReference, gdspy.CellArray)):
+                polygons_spec = item.get_polygons(by_spec = True, depth = None)
+                for key in sorted(polygons_spec):
+                    polygons = polygons_spec[key]
+                    layers.append(key)
+                    points.append(self._vert_polygons(polygons))
+            elif isinstance(item, Polygon):
+                polygons = item.polygons
+                layers.append(key)
+                points.append(self._vert_polygons(polygons))     
+        extracted['vertices'] = points
+        extracted['layers'] = layers
+        return extracted
+
+    def _vert_polygons(self, polygons):
+        vertices = []
+        for poly in polygons:
+            points = np.concatenate((poly,[poly[0]]),axis=0)
+            points_stack = np.vstack(points)
+            points_out = points_stack[:,0],points_stack[:,1]
+            vertices.append(points_out)
+        return vertices
+    
+    def draw_in_blender(self):
+        # Delete all meshes in the scene
+        for item in bpy.data.meshes:
+            bpy.data.meshes.remove(item)
+        vertices = self.vertices
+        layer_stack = self.layerstack
+        for layer in layer_stack.layers:
+            mat = bpy.data.materials.get(layer.name)
+            if mat is None:
+                mat = bpy.data.materials.new(name=layer.name)
+            mat.diffuse_color = layer.color
+            
+        vert_list = []
+        extrude = []
+        lname = []
+        for i in range(len(vertices['vertices'])):
+            vert = vertices['vertices'][i]
+            
+            z, thickness, layer_name = layer_stack.fetch_params(vertices['layers'][i])
+
+            if z is None:
+                z = -1
+            for v in vert:
+                vert_list.append(np.array((v[0],v[1],[z]*len(v[0]))).transpose())
+                extrude.append(thickness)
+                lname.append(layer_name)
+                
+        
+        
+        widgets = ['Drawing:', Percentage(), ' ', Bar(marker='#', left = '[', right = ']' ), \
+                   ' ', ETA(), ' ', FileTransferSpeed()]
+        pbar = ProgressBar(widgets = widgets, max_value= len(vert_list) + 2*len(lname))
+        pbar.start()
+        for i in range(len(vert_list)):
+            if lname[i] is not None:
+                verts_data = vert_list[i]
+                num_verts = len(verts_data)
+                faces_data = [ np.linspace(0,num_verts-1,num_verts).astype(int) ]
+
+                mesh = bpy.data.meshes.new('myMesh_mesh')
+                mesh.from_pydata(verts_data,[],faces_data)
+                mesh.update()
+
+                new_object = bpy.data.objects.new(lname[i],mesh)
+                new_object.data = mesh
+
+                collection = bpy.context.collection
+                collection.objects.link(new_object)
+                bpy.context.view_layer.objects.active = new_object
+
+                new_object.data.materials.append(bpy.data.materials.get(lname[i]))
+            pbar.update(i)
+            i = i + 1    
+        
+        
+        # Join All like materials to one Mesh
+        context = bpy.context
+        scene = context.scene
+        for layer in lname:
+            mats = [m.name for m in bpy.data.materials if m.name== layer]
+            for mat in mats:
+                obs = [o for o in scene.objects
+                        if o.type == 'MESH'
+                        and mat in o.material_slots]
+
+                if len(obs) > 1:
+                    # clear prior selection
+                    for o in context.selected_objects:
+                        o.select_set(False)
+                    for o in obs:
+                        o.select_set(True)
+                    context.view_layer.objects.active = obs[0]
+                    bpy.ops.object.join()
+            i = i + 1
+        
+        extrude = []
+
+        for layer in self.layerstack.layers:
+            extrude.append((layer.name,layer.thickness))
+
+        for obj in bpy.data.objects:
+            for layer in extrude:
+                if obj.name == layer[0]:
+                    bpy.context.view_layer.objects.active = obj
+                    bpy.ops.object.editmode_toggle()
+
+                    bpy.ops.mesh.extrude_region_move(
+                        TRANSFORM_OT_translate={"value":(0, 0, layer[1] )}
+                    )
+                    bpy.ops.object.editmode_toggle()
+                    pbar.update(i)
+                    i = i + 1
+
+
+        pbar.finish()
+        
+        
+
+
+
+
+
+        
