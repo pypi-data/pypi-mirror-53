96b6f251902e97d16d0cf8f385179673f2000330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:36 +0000

    Add package galgebra/0.4.4/galgebra-0.4.4.tar.gz

---
+++ packages/galgebra/galgebra-0.4.4.tar.gz/galgebra-0.4.4/galgebra/utils.py
@@ -0,0 +1,34 @@
+"""
+Utility Classes
+"""
+
+import sys
+import collections
+
+# From https://github.com/benjaminp/six/blob/master/six.py
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    string_types = str
+    from io import StringIO
+else:
+    string_types = basestring
+    from StringIO import StringIO
+
+# https://stackoverflow.com/questions/16176742/python-3-replacement-for-deprecated-compiler-ast-flatten-function
+
+
+def flatten(x):
+    result = []
+
+    for el in x:
+        if isinstance(x, collections.Iterable) and not isstr(el):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
+
+
+def isstr(s):
+    return isinstance(s, string_types)
---
+++ packages/galgebra/galgebra-0.4.4.tar.gz/galgebra-0.4.4/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+VERSION = '0.4.4'
+LONG_DESCRIPTION = """
+Symbolic Geometric Algebra/Calculus package for SymPy. BSD License.
+"""
+
+setup(name='galgebra',
+      version=VERSION,
+      description='Symbolic Geometric Algebra/Calculus package for SymPy.',
+      author='Alan Bromborsky',
+      author_email='hadfield.hugo@gmail.com',
+      url='https://github.com/pygae/galgebra',
+      license='BSD',
+      packages=find_packages(),
+      package_dir={'galgebra':'galgebra'},
+      install_requires = ['sympy'],
+      long_description=LONG_DESCRIPTION,
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Scientific/Engineering :: Physics'])
