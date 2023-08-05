9fd294301ecd8e39c6a06d82840f3e1bd69b42ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:03 +0000

    Add package scaden/0.9.2/scaden-0.9.2.tar.gz

---
+++ packages/scaden/scaden-0.9.2.tar.gz/scaden-0.9.2/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+version = '0.9.2'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='scaden',
+    version=version,
+    description="Cell type deconvolution using single cell data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['bioinformatics', 'deep learning', 'machine learning', 'single cell sequencing', 'deconvolution'],
+    author='Kevin Menden',
+    author_email='kevin.menden@t-online.de',
+    url='https://github.com/KevinMenden/scaden',
+    license="MIT License",
+    scripts=['scripts/scaden'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires = [
+        'pandas==0.21',
+        'numpy==1.14.5',
+        'scikit-learn',
+        'scipy',
+        'seaborn',
+        'tensorflow==1.12.1',
+        'matplotlib',
+        'scanpy==1.2.2',
+        'tqdm',
+        'click'
+    ]
+)
