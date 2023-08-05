f9e213c67ba5f6e278c65a1fa1ab81164306aed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package timg/1.1.1/timg-1.1.1.tar.gz

---
+++ packages/timg/timg-1.1.1.tar.gz/timg-1.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+from src.timg import meta 
+
+
+with open('README.md', 'r') as f:
+  long_description = f.read()
+
+setup(
+  name=meta.TIMG_PROG_NAME,
+  version=meta.TIMG_VERSION,
+  description=meta.TIMG_DESC,
+  long_description=long_description,
+  url='https://github.com/adzierzanowski/timg',
+  author='Aleksander Dzierżanowski',
+  author_email='a.dzierzanowski1@gmail.com',
+  license='MIT',
+  packages=['timg', 'timg/methods'],
+  package_dir={'': 'src'},
+  include_package_data=True,
+  install_requires=['pillow'],
+  entry_points={
+    'console_scripts': [
+      'timg = timg.__main__:main'
+    ]
+  },
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: End Users/Desktop',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Multimedia :: Graphics :: Viewers',
+  ],
+  zip_safe=False
+)
+
