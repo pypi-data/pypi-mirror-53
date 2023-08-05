dde6cb1406e22228eb81e3b245cdcfd8d0deaf28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:07 +0000

    Add package smartutils/1.0.0/smartutils-1.0.0.tar.gz

---
+++ packages/smartutils/smartutils-1.0.0.tar.gz/smartutils-1.0.0/README.md
@@ -0,0 +1,9 @@
+Simple and light weight module to measure the function execution time in Python.
+
+Usage:
+
+from smartutils.util import get_exec_time
+
+@get_exec_time
+def sample():
+    print("hi")
---
+++ packages/smartutils/smartutils-1.0.0.tar.gz/smartutils-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smartutils",
+    version="1.0.0",
+    author="Arockia Arulnathan",
+    author_email="arockia@scriptinghub.com",
+    description="Smart collection of utilities for your code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
