81ff827fa391f3fdc4a8ed5421303e47d78eec8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package abc-classroom/0.0.8/abc-classroom-0.0.8.tar.gz

---
+++ packages/abc-classroom/abc-classroom-0.0.8.tar.gz/abc-classroom-0.0.8/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name="abc-classroom",
+    version="0.0.8",
+    description="Efficiently manage github classroom assignments from the command line.",
+    long_description="Authoring and grading of notebook assignments",
+    license="BSD",
+    author="Earth Lab, University of Colorado -- Boulder",
+    packages=["abcclassroom"],
+    install_requires=[
+        "nbclean",
+        "jinja2",
+        "papermill",
+        "nbformat",
+        "ruamel.yaml",
+        "github3.py",
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "abc-init = abcclassroom.__main__:init",
+            "abc-grade = abcclassroom.__main__:grade",
+            "abc-author = abcclassroom.__main__:author",
+            "abc-distribute = abcclassroom.__main__:distribute",
+        ]
+    },
+)
