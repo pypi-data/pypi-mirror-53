7b19fb701af38d4ecfb9d6944b1f7e3aa3b94fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:52 +0000

    Add package kagglextensions/0.0.2/kagglextensions-0.0.2.tar.gz

---
+++ packages/kagglextensions/kagglextensions-0.0.2.tar.gz/kagglextensions-0.0.2/README.md
@@ -0,0 +1,2 @@
+# kaggle-extensions
+Descrizione pacchetto
---
+++ packages/kagglextensions/kagglextensions-0.0.2.tar.gz/kagglextensions-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kagglextensions",
+    version="0.0.2",
+    author="Giovanni Borghi",
+    author_email="gio.borghi@gmail.com",
+    description="A package for extending kaggle",
+    long_description="NO DESCRIPTION",
+    long_description_content_type="text/markdown",
+    url="https://github.com/gborghi/kagglextensions.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+    requires=['math']
+)
