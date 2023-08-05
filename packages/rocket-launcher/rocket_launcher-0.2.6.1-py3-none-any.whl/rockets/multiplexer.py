99705eec83df841d216cb240565ee10d716595ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:22 +0000

    Add package rocket-launcher/0.2.6.1/rocket_launcher-0.2.6.1-py3-none-any.whl

---
+++ packages/rocket-launcher/rocket_launcher-0.2.6.1-py3-none-any.whl/rockets/multiplexer.py
@@ -0,0 +1,3 @@
+def multiplexer():
+    os.system('sudo -H -u who configure-multiplexer')
+    os.system('configure-multiplexer')
---
+++ packages/rocket-launcher/rocket_launcher-0.2.6.1-py3-none-any.whl/rockets/secure_shell.py
@@ -0,0 +1,2 @@
+def secure_shell():
+    os.system('configure-secure_shell')
