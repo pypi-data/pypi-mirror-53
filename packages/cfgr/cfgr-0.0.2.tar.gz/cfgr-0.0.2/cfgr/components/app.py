835212396d83a91b17bf6599c26528ff945c8909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package cfgr/0.0.2/cfgr-0.0.2.tar.gz

---
+++ packages/cfgr/cfgr-0.0.2.tar.gz/cfgr-0.0.2/cfgr/components/app.py
@@ -0,0 +1,41 @@
+from cfgr.event import Event
+import time, os
+
+class App:
+  """
+  The cfgr.app's default app component
+  """
+  @staticmethod
+  def cfgr(builder):
+    # inputs
+    builder.addInput('stop').signal_to_method(lambda obj: obj.onStop)
+    builder.addInput('sleep').float_to_method(lambda obj: obj.setSleep)
+    # outputs
+    builder.addOutput('started').from_event(lambda obj: obj.startedEvent)
+    builder.addOutput('update').from_event(lambda obj: obj.updateEvent)
+    builder.addOutput('pid').from_event(lambda obj: obj.pidEvent)
+
+  def __init__(self):
+    self.startedEvent = Event()
+    self.pidEvent = Event()
+    self.updateEvent = Event()
+    self.stopEvent = Event()
+    self.sleeptime = 0.001
+
+  def start(self):
+    pid = os.getpid()
+    # print('pid: ', pid)
+    self.pidEvent(os.getpid())
+    self.startedEvent.fire()
+
+  def update(self):
+    self.updateEvent()
+    # print('update')
+    time.sleep(self.sleeptime)
+
+  def setSleep(self, val):
+    # print('setsleep: {}'.format(val))
+    self.sleeptime = val
+
+  def onStop(self, *args, **kwargs):
+    self.stopEvent()
