fe5eab4f789970bf7ebe569d742c54a170411f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:40 +0000

    Add package jdm-distributions/0.1/jdm_distributions-0.1.tar.gz

---
+++ packages/jdm-distributions/jdm_distributions-0.1.tar.gz/jdm_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jdm_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jdm_distributions'],
+      zip_safe=False)
