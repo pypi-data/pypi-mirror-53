e40e1d667b30134d37a6befcb952c88e5c96d612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:00 +0000

    Add package iredis/0.2.11/iredis-0.2.11.tar.gz

---
+++ packages/iredis/iredis-0.2.11.tar.gz/iredis-0.2.11/iredis/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+__version__ = "0.2.11"
+
+
+logging.basicConfig(
+    filename="iredis.log",
+    filemode="a",
+    format="%(levelname)5s %(message)s",
+    level="DEBUG",
+)
+
+logger = logging.getLogger(__name__)
+logger.info("------ iRedis ------")
