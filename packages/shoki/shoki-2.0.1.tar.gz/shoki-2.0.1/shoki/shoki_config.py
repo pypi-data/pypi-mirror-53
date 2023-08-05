a3766db9bdd91e54598936e2eaded27cdebdccd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:53 +0000

    Add package shoki/2.0.1/shoki-2.0.1.tar.gz

---
+++ packages/shoki/shoki-2.0.1.tar.gz/shoki-2.0.1/shoki/shoki_config.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Config elements for shoki."""
+
+import json
+import pathlib
+
+# Define the root of this directory
+ROOT = pathlib.Path.cwd()
+MINUTES_SAMPLE = "./tests/fixtures/minutes_normal.txt"
+MINUTES_LOCATION = pathlib.Path(ROOT, MINUTES_SAMPLE).as_uri()
+
+# USER CONFIGURATION
+USER_PATH = pathlib.Path.home()
+USER_CONFIG_PATH = USER_PATH / ".shoki"
+CONFIG_NAME = "shoki_config.json"
+USER_CONFIG_FILE = pathlib.Path(USER_CONFIG_PATH, CONFIG_NAME)
+
+if pathlib.Path(USER_CONFIG_FILE).exists():
+    # we use the local user configuration file
+    data = USER_CONFIG_FILE.read_text()
+    default_config = json.loads(data)
+else:
+    # use these defaults
+    default_config = {
+        "end": "--end--",
+        "output_format": "webcompatwiki",
+        "topic_header": "#",
+        "topic_owner": "👹",
+    }
+
+# MESSAGES
+FORMAT_ERROR = """
+The formatter has failed.
+The file you are dealing with is probably in the wrong format.
+shoki accepts only plain text files.
+
+Error: {error}
+"""
