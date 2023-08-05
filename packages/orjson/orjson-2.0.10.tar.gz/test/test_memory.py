d61b66ef355644d62749cbb6629ab353a71c2454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:03 +0000

    Add package orjson/2.0.10/orjson-2.0.10.tar.gz

---
+++ packages/orjson/orjson-2.0.10.tar.gz/test/test_memory.py
@@ -0,0 +1,76 @@
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import gc
+import unittest
+import uuid
+
+import pytest
+import psutil
+import orjson
+
+
+FIXTURE = '{"a":[81891289, 8919812.190129012], "b": false, "c": null, "d": "東京"}'
+
+
+def default(obj):
+    return str(obj)
+
+
+class MemoryTests(unittest.TestCase):
+    def test_memory_loads(self):
+        """
+        loads() memory leak
+        """
+        proc = psutil.Process()
+        gc.collect()
+        val = orjson.loads(FIXTURE)
+        mem = proc.memory_info().rss
+        for _ in range(10000):
+            val = orjson.loads(FIXTURE)
+        gc.collect()
+        self.assertTrue(proc.memory_info().rss <= mem + 1024)
+
+    def test_memory_dumps(self):
+        """
+        dumps() memory leak
+        """
+        proc = psutil.Process()
+        gc.collect()
+        fixture = orjson.loads(FIXTURE)
+        val = orjson.dumps(fixture)
+        mem = proc.memory_info().rss
+        for _ in range(10000):
+            val = orjson.dumps(fixture)
+        gc.collect()
+        self.assertTrue(proc.memory_info().rss <= mem + 1024)
+
+    def test_memory_dumps_default(self):
+        """
+        dumps() default memory leak
+        """
+        proc = psutil.Process()
+        gc.collect()
+        fixture = orjson.loads(FIXTURE)
+        fixture["custom"] = uuid.uuid4()
+        val = orjson.dumps(fixture, default=default)
+        mem = proc.memory_info().rss
+        for _ in range(10000):
+            val = orjson.dumps(fixture, default=default)
+        gc.collect()
+        self.assertTrue(proc.memory_info().rss <= mem + 1024)
+
+    def test_memory_loads_keys(self):
+        """
+        loads() memory leak with number of keys causing cache eviction
+        """
+        proc = psutil.Process()
+        gc.collect()
+        fixture = {"key_%s" % idx: "value" for idx in range(1024)}
+        self.assertEqual(len(fixture), 1024)
+        val = orjson.dumps(fixture)
+        loaded = orjson.loads(val)
+        mem = proc.memory_info().rss
+        for _ in range(100):
+            loaded = orjson.loads(val)
+        gc.collect()
+        self.assertTrue(proc.memory_info().rss <= mem + 256 * 1024)
