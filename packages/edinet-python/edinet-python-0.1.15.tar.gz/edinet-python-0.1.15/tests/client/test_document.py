862e866a69f25d0c596b77306536f20ef53f3f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:35 +0000

    Add package edinet-python/0.1.15/edinet-python-0.1.15.tar.gz

---
+++ packages/edinet-python/edinet-python-0.1.15.tar.gz/edinet-python-0.1.15/tests/client/test_document.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+from edinet.client.document_client import DocumentClient
+from tests.utils import delay
+
+
+class TestDocumentClient(unittest.TestCase):
+
+    @delay
+    def test_get(self):
+        _dir = os.path.dirname(__file__)
+        client = DocumentClient()
+        file_path = client.get("S100FGR9", response_type=1, save_dir=_dir)
+        self.assertTrue(os.path.exists(file_path))
+        os.remove(file_path)
+
+    @delay
+    def test_get_pdf(self):
+        _dir = os.path.dirname(__file__)
+        client = DocumentClient()
+        file_path = client.get_pdf("S100FGR9", save_dir=_dir)
+        self.assertTrue(os.path.exists(file_path))
+        os.remove(file_path)
+
+    @delay
+    def test_get_xbrl(self):
+        _dir = os.path.dirname(__file__)
+        client = DocumentClient()
+        # lang="en" does not exist?
+        file_path = client.get_xbrl("S100FGR9", save_dir=_dir)
+        self.assertTrue(os.path.exists(file_path))
+        os.remove(file_path)
+
+    @delay
+    def test_get_pdf_without_dir(self):
+        client = DocumentClient()
+        file_path = client.get_pdf("S100FGR9")
+        self.assertTrue(os.path.exists(file_path))
+        name = os.path.basename(file_path)
+        self.assertTrue(str(name).startswith("S100FGR9_2__"))
+        self.assertTrue(str(name).endswith(".pdf"))
+        os.remove(file_path)
