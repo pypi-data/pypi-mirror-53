31fc599f04b945d7a02b6afa6f0310b45b00d9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:58 +0000

    Add package moncli/0.1/moncli-0.1-py3.7.egg

---
+++ packages/moncli/moncli-0.1-py3.7.egg/moncli/__init__.py
@@ -0,0 +1,2 @@
+from .entities import MondayClient
+from .enums import *
---
+++ packages/moncli/moncli-0.1-py3.7.egg/moncli/constants.py
@@ -0,0 +1,13 @@
+DATE_FORMAT = '%Y-%m-%d'
+DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+API_V2_ENDPOINT = 'https://api.monday.com/v2'
+
+# Column Types
+COLUMN_NAME = 'name'
+COLUMN_MULTIPERSON = 'multi-person'
+COLUMN_COLOR = 'color'
+COLUMN_DATE = 'date'
+COLUMN_NUMERIC = 'numeric'
+COLUMN_DURATION = 'duration'
+COLUMN_LONGTEXT = 'long-text'
