d30949d4f8b8bdb8a909d13da7cbe40d39dbe9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:54 +0000

    Add package comp-lead/0.1.0/comp-lead-0.1.0.tar.gz

---
+++ packages/comp-lead/comp-lead-0.1.0.tar.gz/comp-lead-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as pkg:
+    long_description = pkg.read()
+
+setuptools.setup(
+    name="comp-lead",
+    version="0.1.0",
+    author="Endormi",
+    author_email="contactendormi@gmail.com",
+    description="Display a handful of tech leaders",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/endormi/comp-lead",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
