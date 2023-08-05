b04ac0470d0773f95dcf6d24bbf296cea5838027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:04:02 +0000

    Add package csf-kit/0.1.3/csf_kit-0.1.3.tar.gz

---
+++ packages/csf-kit/csf_kit-0.1.3.tar.gz/csf_kit-0.1.3/csf_kit/__init__.py
@@ -0,0 +1,8 @@
+from . import news
+
+from csf_kit.news.sample_code.sentimental_factor import *
+
+from csf_kit.news import SAMPLE_NEWS_DATA, SAMPLE_SENTI_SCORE
+
+
+
---
+++ packages/csf-kit/csf_kit-0.1.3.tar.gz/csf_kit-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(name='csf_kit',
+      version='0.1.3',
+      description='StartKit for ChinaScope data',
+      author='ChinaScope',
+      url='https://pypi.org/project/csf-kit/',
+      author_email='song.lu@chinascope.com',
+      packages=find_packages(),
+      include_package_data=True,
+      exclude_package_data={'': ['.gitignore']},
+      install_requires=[
+            'pandas',
+            'numpy ',
+            'zipfile37',
+            'python-dateutil'
+      ]
+      )
