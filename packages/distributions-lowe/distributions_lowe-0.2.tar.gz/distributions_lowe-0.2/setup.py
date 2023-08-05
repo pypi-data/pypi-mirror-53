6ce9f975741b5b330ffcc2c58e1ae8d1dd8b18f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:06 +0000

    Add package distributions-lowe/0.2/distributions_lowe-0.2.tar.gz

---
+++ packages/distributions-lowe/distributions_lowe-0.2.tar.gz/distributions_lowe-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_lowe',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_lowe'],
+      author='Brandon Lowe',
+      zip_safe=False)
