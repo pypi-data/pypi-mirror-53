b50d2081893bcb8af2fdf4360932f9750cc128f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:01:33 +0000

    Add package janis-pipelines.unix/0.6.0/janis-pipelines.unix-0.6.0.tar.gz

---
+++ packages/janis-pipelines.unix/janis-pipelines.unix-0.6.0.tar.gz/janis-pipelines.unix-0.6.0/janis_unix/tools/hello.py
@@ -0,0 +1,41 @@
+from datetime import date
+
+import janis_core as j
+from .echo import Echo
+
+
+class HelloWorkflow(j.Workflow):
+    def constructor(self):
+        self.input("inp", j.String(optional=True), default="Hello, world!")
+        self.step("hello", Echo(inp=self.inp))
+        self.output("out", source=self.hello, output_tag="test")
+
+    def id(self):
+        return "hello"
+
+    def friendly_name(self):
+        return "Hello, World!"
+
+    @staticmethod
+    def tool_module():
+        return "unix"
+
+    def bind_metadata(self):
+
+        self.metadata.version = "v1.0.0"
+        self.metadata.author = "Michael Franklin"
+        self.metadata.maintainer = "Michael Franklin"
+        self.metadata.dateUpdated = date(2019, 8, 12)
+
+        self.metadata.documentation = """\
+This is the 'Hello, world' equivalent workflow that uses the Echo unix
+tool to log "Hello, World!" to the console, and collects the result.
+
+This is designed to be the first example that you can run with janis, ie:
+    
+``janis run hello``
+"""
+
+
+if __name__ == "__main__":
+    print(HelloWorkflow().translate("cwl"))
