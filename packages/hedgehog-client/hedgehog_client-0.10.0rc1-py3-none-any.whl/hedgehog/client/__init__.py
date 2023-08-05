774488b4922756c1b9437aeb3105b645e7d9a80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:48 +0000

    Add package hedgehog-client/0.10.0rc1/hedgehog_client-0.10.0rc1-py3-none-any.whl

---
+++ packages/hedgehog-client/hedgehog_client-0.10.0rc1-py3-none-any.whl/hedgehog/client/__init__.py
@@ -0,0 +1,22 @@
+from contextlib import contextmanager
+from pycreate2 import Create2
+from .sync_client import HedgehogClient, connect
+# for using POWER, BRAKE and VELOCITY
+from hedgehog.protocol.messages import motor
+
+
+@contextmanager
+def connect_create(port='/dev/ttyUSB0', baud=57600):
+    create = Create2(port, baud=baud)
+    create.start()
+    yield create
+    # TODO do proper cleanup instead of waiting for the GC
+    # del create does not seem to work, and create.__del__() raises an exception upon actual GC
+
+
+@contextmanager
+def connect_create2(port='/dev/ttyUSB0', baud=115200):
+    create = Create2(port, baud=baud)
+    create.start()
+    yield create
+    # TODO do proper cleanup instead of waiting for the GC
