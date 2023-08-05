a1e9d92e0561f8b79243c0aeb590b2608c903d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:28 +0000

    Add package hammeroflight/1.0.1/hammeroflight-1.0.1.tar.gz

---
+++ packages/hammeroflight/hammeroflight-1.0.1.tar.gz/hammeroflight-1.0.1/hammeroflight/__init__.py
@@ -0,0 +1,5 @@
+# from MyLib.File1 import ClassA, ClassB, ClassC
+# from MyLib.File2 import ClassX, ClassY, ClassZ
+
+from hammeroflight.arufunctions import cleanandencode, fit_classify, fit_regress
+from hammeroflight.arufunctions import goodness_fit, featureselector, qualityreport
