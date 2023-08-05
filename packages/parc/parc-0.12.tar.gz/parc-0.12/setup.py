ecbdb6a240d3f12b7c2ae4357b3e756fea3ba297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:24 +0000

    Add package parc/0.12/parc-0.12.tar.gz

---
+++ packages/parc/parc-0.12.tar.gz/parc-0.12/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+
+#with open("requirements.txt", "r") as fh:
+#    requirements = fh.read()
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='parc',
+    version='0.12',
+    packages=['parc',],
+    license='MIT',
+    author_email = 'shobana.venkat88@gmail.com',
+    url = 'https://github.com/ShobiStassen/PARC',
+    setup_requires = ['pybind11'],
+    install_requires=['numpy','scipy','pandas','hnswlib','python-igraph','leidenalg'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
