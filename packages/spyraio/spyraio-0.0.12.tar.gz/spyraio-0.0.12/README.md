d92e61be67feb15178708231e313df0137fa85dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:05 +0000

    Add package spyraio/0.0.12/spyraio-0.0.12.tar.gz

---
+++ packages/spyraio/spyraio-0.0.12.tar.gz/spyraio-0.0.12/README.md
@@ -0,0 +1,24 @@
+# Spyraio 
+## Suite Python for Forecasting lightning
+**SpyRaio** is a Python package which provide utils for preprocessing, processing and postprocessing severe weather data. The package also provide functions for lightning forecasting.
+
+### Instalation
+```console
+pip install spyraio
+```
+### Uninstall
+```console
+pip uninstall spyraio
+```
+### Credits
+```
+  ____                        _
+ / ___| _ __  _   _ _ __ __ _(_) ___
+ \___ \| '_ \| | | | '__/ _` | |/ _ \
+  ___) | |_) | |_| | | | (_| | | (_) |
+ |____/| .__/ \__, |_|  \__,_|_|\___/
+       |_|    |___/
+                 By Adriano P. Almeida
+				                 2019
+```
+
---
+++ packages/spyraio/spyraio-0.0.12.tar.gz/spyraio-0.0.12/spyraio/classification/DecisionTree.py
@@ -0,0 +1,42 @@
+from spyraio.classification import ClassificationModel
+
+
+class DecisionTreeClassifier(ClassificationModel):
+#    def __init__(self):
+#        self.model = None
+    
+    
+    def compute_model(X_train, y_train, criterion='gini', max_depth=None, 
+                      max_leaf_nodes=None):
+        
+        from sklearn.tree import DecisionTreeClassifier
+
+        classifier = DecisionTreeClassifier(criterion=criterion, 
+                                            max_depth=max_depth,
+                                            max_leaf_nodes=max_leaf_nodes)
+        classifier.fit(X_train, y_train)
+        
+        return classifier
+    
+    
+    def predict(dt, X):
+        y_pred = ClassificationModel.predict_model(dt, X)
+        return y_pred
+    
+    
+    def evaluate(y_test, y_pred):
+        evaluate = ClassificationModel.evaluate_model(y_test, y_pred)
+        return evaluate
+    
+    
+    def save_dot(dt, columns):
+        from sklearn.tree import DecisionTreeClassifier, export_graphviz
+#        import time
+        
+#        dotfilename = 'dotfile-%s.dot'%str(time.time()).split(('.'))[0]
+        dotfilename = 'dotfile.dot'
+        with open(dotfilename, 'w') as dotfile:
+            export_graphviz(dt, out_file=dotfile, feature_names=columns[2:],
+                            class_names=['class 1', 'class 2', 'class 3'],
+                             impurity=False, filled=True)
+        
