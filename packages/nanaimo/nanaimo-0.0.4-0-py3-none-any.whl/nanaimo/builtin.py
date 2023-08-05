a85661f8677edd80380183adf0f2b2329e799836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:17:49 +0000

    Add package nanaimo/0.0.4/nanaimo-0.0.4-0-py3-none-any.whl

---
+++ packages/nanaimo/nanaimo-0.0.4-0-py3-none-any.whl/nanaimo/builtin.py
@@ -0,0 +1,43 @@
+#
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# This software is distributed under the terms of the MIT License.
+#
+"""
+Built-in :class:`NanaimoTest` for common scenarios.
+"""
+import asyncio
+import pathlib
+
+import nanaimo
+import nanaimo.serial
+import nanaimo.jlink
+
+
+class GTestOverJLink(nanaimo.NanaimoTest):
+
+    @classmethod
+    def on_visit_test_arguments(cls, arguments: nanaimo.Arguments) -> None:
+        nanaimo.serial.AbstractSerial.on_visit_test_arguments(arguments)
+        nanaimo.jlink.ProgramUploaderJLink.on_visit_test_arguments(arguments)
+
+    async def __call__(self, args: nanaimo.Namespace) -> int:
+        import nanaimo
+        import nanaimo.serial
+        import nanaimo.gtest
+        import nanaimo.jlink
+
+        uploader = nanaimo.jlink.ProgramUploaderJLink()
+        jlink_scripts = pathlib.Path(args.base_path).glob(args.jlink_scripts)
+        parser = nanaimo.gtest.Parser(args.test_timeout_seconds)
+
+        result = 0
+        with nanaimo.serial.ConcurrentUart.new_default(args.port, args.port_speed) as monitor:
+            for script in jlink_scripts:
+                if result != 0:
+                    break
+                result = await asyncio.wait_for(uploader.upload(script), timeout=args.upload_timeout_seconds)
+                if result != 0:
+                    break
+                result = await parser.read_test(monitor)
+
+        return result
---
+++ packages/nanaimo/nanaimo-0.0.4-0-py3-none-any.whl/nanaimo/version.py
@@ -0,0 +1,8 @@
+#
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# This software is distributed under the terms of the MIT License.
+#
+
+__version__ = 0, 0, 4
+
+__license__ = 'MIT'
