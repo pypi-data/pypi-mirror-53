c6ae52915616eb353480eb3e1be584023740bbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:33 +0000

    Add package FrictionlessDarwinCore/0.1.3/FrictionlessDarwinCore-0.1.3-py3-none-any.whl

---
+++ packages/FrictionlessDarwinCore/FrictionlessDarwinCore-0.1.3-py3-none-any.whl/FrictionlessDarwinCore/archive.py
@@ -0,0 +1,104 @@
+from FrictionlessDarwinCore import *
+import sys
+import shutil
+import requests
+import zipfile
+import tempfile
+
+
+class DwCArchive:
+    voc = DwCVocabulary()
+
+    def __init__(self, path_or_url):
+        self.valid = True
+        self.dwca = path_or_url
+        self.metadata = None
+        self.structure = None
+        self.tf = None
+        if self.dwca.startswith('http'):
+            self.path = self.download()
+        else:
+            self.path = self.dwca
+
+    def download(self):
+        #  download DwCArchive into temporary file
+        self.tf = tempfile.NamedTemporaryFile()
+        try:
+            response = requests.get(self.dwca)
+            self.tf.write(response.content)
+        except requests.exceptions.RequestException as err:
+            print(err)
+            self.valid = False
+        else:
+            print('downloading ' + self.dwca + ' as ' + self.tf.name)
+        return self.tf.name
+
+    def infer(self):
+        if zipfile.is_zipfile(self.path):
+            self.load()
+        else:
+            self.valid = False
+            print('dwca is not a zipfile')
+
+    def load(self):
+        eml = ''
+        meta = ''
+        zf = zipfile.ZipFile(self.path, mode='r')
+        try:
+            for info in zf.infolist():
+                if info.filename == 'eml.xml':
+                    eml = zf.read(info.filename).decode()
+                if info.filename == 'metadata.xml':
+                    eml = zf.read(info.filename).decode()
+                if info.filename == 'meta.xml':
+                    meta = zf.read(info.filename).decode()
+            if eml != '' and meta != '':
+                self.metadata = DwCMetadata(eml)
+                self.structure = DwCStructure(meta, eml)
+                self.metadata.convert()
+                self.structure.convert()
+                self.valid = self.metadata.valid and self.structure.valid
+        except BaseException:
+            print(sys.exc_info())
+            print('load zip failed')
+            self.valid = False
+        finally:
+            zf.close()
+
+    def save(self, output):
+        # first copy zipfile to output
+        shutil.copyfile(self.path, output)
+        zf = zipfile.ZipFile(output, mode='a')
+        try:
+            # Add a README.md file that describes the package
+            zf.writestr('readme.md', self.metadata.as_markdown())
+            # Add a datapackage.json
+            zf.writestr('datapackage.json', self.structure.as_json())
+        except IOError as e:
+            print("Unable to copy file. %s" % e)
+        else:
+            print('saving zipfile to ' + output)
+        finally:
+            zf.close()
+
+    def to_json(self, output):
+        o = open(output, 'w')
+        try:
+            o.write(self.structure.as_json())
+        except IOError as e:
+            print("Unable to write file. %s" % e)
+        else:
+            print('saving json to ' + output)
+        finally:
+            o.close()
+
+    def to_markdown(self, output):
+        o = open(output, 'w')
+        try:
+            o.write(self.metadata.as_markdown())
+        except IOError as e:
+            print("Unable to write file. %s" % e)
+        else:
+            print('saving markdown to ' + output)
+        finally:
+            o.close()
---
+++ packages/FrictionlessDarwinCore/FrictionlessDarwinCore-0.1.3-py3-none-any.whl/FrictionlessDarwinCore/structure.py
@@ -0,0 +1,158 @@
+from FrictionlessDarwinCore import DwCVocabulary
+import xml.etree.ElementTree as ET
+import re
+import json
+
+
+class DwCStructure:
+    voc = DwCVocabulary()
+    ns = {'dwc': 'http://rs.tdwg.org/dwc/text/'}
+
+    def __init__(self, meta, eml):
+        self.eml = eml
+        self.meta = meta
+        self.descriptor = {}
+        self.corename = ''
+        self.pkey_name = 'id'
+        self.valid = False
+
+    def convert(self):
+        # convert meta.xml into datapackage descriptor
+        self.descriptor = {}
+        if self.eml is not None:
+            dataset = ET.fromstring(self.eml).find('./dataset')
+            self._addheader(dataset)
+        if self.meta is not None:
+            archive = ET.fromstring(self.meta)
+            resources = []
+            core = archive.find('dwc:core', DwCStructure.ns)
+            self.corename = core.get('rowType').rsplit('/', 1)[1].lower()
+
+            resources.append(self._toresource(core, True))
+            for extension in archive.findall('dwc:extension', DwCStructure.ns):
+                resources.append(self._toresource(extension, False))
+            self._add('resources', resources)
+        self.valid = True
+        return self.as_json()
+
+    def as_json(self):
+        return json.dumps(self.descriptor)
+
+    def _add(self, key, value):
+        self.descriptor[key] = value
+
+    def _addheader(self, dataset):
+        licences = []
+        sources = []
+        for ai in dataset.findall('./alternateIdentifier'):
+            m = re.search(r'(?<==)\w+', ai.text)
+            if m is not None:
+                self._add('name', m.group(0))
+            if re.search('^[0-9a-f\-]*$', ai.text):
+                self._add('id', ai.text)
+            if re.search('^https?://', ai.text):
+                source = {'title': 'GBIF IPT', 'path': ai.text}
+                sources.append(source)
+        self._add('title', dataset.findtext('./title'))
+        self._add('profile', 'tabular-data-package')
+        ipr = dataset.find('./intellectualRights')
+        if ipr is not None:
+            licence = {}
+            title = ipr.findtext('./para/ulink/citetitle')
+            if title is not None:
+                name = re.search('\((.+)\)', title)
+                if name is not None:
+                    licence['name'] = name.group(0)
+                licence['title'] = title
+            licence['path'] = ipr.find('./para/ulink').get('url')
+            licences.append(licence)
+        self._add('licences', licences)
+
+        self._add('sources', sources)
+
+    def _delimiter(self, delimiter_string):
+        switcher = {
+            "\\n": '\n',
+            "\\r": '\r',
+            "\\r\\n": '\r\n',
+            "\\t": '\t',
+        }
+        return switcher.get(delimiter_string, delimiter_string)
+
+    def _toresource(self, mfile, core):
+        if mfile is None:
+            return
+        r = {}
+        files = mfile.find('dwc:files', DwCStructure.ns)
+        location = files.find('dwc:location', DwCStructure.ns)
+        r['name'] = location.text.split('.')[0]
+        r['path'] = location.text
+        r['description'] = mfile.get('rowType')
+        r['profile'] = 'tabular-data-resource'
+        r['encoding'] = mfile.get('encoding')
+        r['format'] = 'csv'
+        dialect = {'csvddfVersion': 1.2, 'delimiter': self._delimiter(mfile.get('fieldsTerminatedBy')),
+                   'doubleQuote': True, 'lineTerminator': self._delimiter(mfile.get('linesTerminatedBy')),
+                   'quoteChar': self._delimiter(mfile.get('fieldsEnclosedBy')), 'skipInitialSpace': True,
+                   'header': mfile.get('ignoreHeaderLines') == '1', 'commentChar': '#'}
+        r['dialect'] = dialect
+
+        schema = {}
+        fields = []
+
+        index_id='0'
+        index_name = 'id'
+
+        if core:
+            index = mfile.find('dwc:id', DwCStructure.ns)
+        else:
+            index = mfile.find('dwc:coreid', DwCStructure.ns)
+        if index is not None:
+            index_id = index.get('index')
+
+        need_additional_id_field = True
+        for f in mfile.findall('dwc:field', DwCStructure.ns):
+            if f.get('index') == index_id:
+                need_additional_id_field = False
+                index_name = f.get('term').rsplit('/', 1)[1]
+                if core:
+                    self.pkey_name = index_name
+
+        if need_additional_id_field:
+            print('Adding additional_id_field')
+            field = {'name': index_name, 'type': 'string', 'format': 'default'}
+            fields.append(field)
+
+        for f in mfile.findall('dwc:field', DwCStructure.ns):
+            field = {}
+            mterm = f.get('term')
+            term = self.voc.term(mterm)
+            if term is None:
+                print(mterm, 'unknown Darwin Core term.')
+                field['name'] = mterm.rsplit('/', 1)[1]
+                field['type'] = 'string'
+            else:
+                field['name'] = term['name']
+                field['type'] = term['type']
+                field['description'] = mterm
+                if term['format'] != 'default':
+                    field['format'] = term['format']
+                if term['constraints'] != '':
+                    field['constraints'] = json.loads(term['constraints'])
+            fields.append(field)
+        schema['fields'] = fields
+        r['schema'] = schema
+        if core:
+            r['primaryKey'] = self.pkey_name
+        else:
+            fkeys = []
+            fkey = {}
+
+            fkey['fields'] = index_name
+            ref = {}
+            ref['resource'] = self.corename
+            ref['fields'] = self.pkey_name
+            fkey['reference'] = ref
+            fkeys.append(fkey)
+            r['foreignKeys'] = fkeys
+        return r
