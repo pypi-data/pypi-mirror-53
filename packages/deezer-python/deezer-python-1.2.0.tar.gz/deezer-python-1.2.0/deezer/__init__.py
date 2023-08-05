8df6576c4031d2bb881a686bdf608cb76539d24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:55 +0000

    Add package deezer-python/1.2.0/deezer-python-1.2.0.tar.gz

---
+++ packages/deezer-python/deezer-python-1.2.0.tar.gz/deezer-python-1.2.0/deezer/__init__.py
@@ -0,0 +1,26 @@
+"""Python `Deezer API <http://developers.deezer.com/api>`_ Wrapper.
+
+Implements several classes to interact with all types of Deezer objects,
+do some searches, and build application written in Python on top of it
+
+"""
+from deezer.client import Client
+from deezer.resources import Album, Resource, Artist, Playlist
+from deezer.resources import Genre, Track, User, Comment, Radio
+
+
+__version__ = "1.2.0"
+__all__ = [
+    "Client",
+    "Resource",
+    "Album",
+    "Artist",
+    "Genre",
+    "Playlist",
+    "Track",
+    "User",
+    "Comment",
+    "Radio",
+]
+
+USER_AGENT = "Deezer Python API Wrapper v{}".format(__version__)
