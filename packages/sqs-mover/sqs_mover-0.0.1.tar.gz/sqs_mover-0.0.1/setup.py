dbe5b2b4326965dc7fdfc3818fe9c02650ba9850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:50 +0000

    Add package sqs-mover/0.0.1/sqs_mover-0.0.1.tar.gz

---
+++ packages/sqs-mover/sqs_mover-0.0.1.tar.gz/sqs_mover-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="sqs_mover",
+    version="0.0.1",
+    author="Koby Bass",
+    description="Utility for moving message between SQS queues",
+    url="https://github.com/kobybum/py-sqs-mover",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=["boto3>=1.9.236"],
+)
