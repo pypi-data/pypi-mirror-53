a0798ec20bf85493529f735c1676ea829dcc941d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package redis-yy/0.0.2/redis-yy-0.0.2.tar.gz

---
+++ packages/redis-yy/redis-yy-0.0.2.tar.gz/redis-yy-0.0.2/redis_yy/__about__.py
@@ -0,0 +1,17 @@
+"""
+Create by yy on 2019/9/27
+"""
+
+__title__ = 'redis-yy'
+__description__ = 'A tool which is used to connect redis, designed by yy'
+__url__ = 'https://github.com/guaidashu/redis_yy'
+__version_info__ = ('0', '0', '2')
+__version__ = '.'.join(__version_info__)
+__author__ = 'guaidashu'
+__author_email__ = 'song42960@gmail.com'
+__maintainer__ = 'YY blog'
+__license__ = 'MIT'
+__copyright__ = '(c) 2019 by guaidashu'
+__install_requires__ = [
+    "redis <= 3.3.8"
+]
