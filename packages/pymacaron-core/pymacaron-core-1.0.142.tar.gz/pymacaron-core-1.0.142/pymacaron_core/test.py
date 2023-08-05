e6a4a8013f6256f82f6dce88b4f4aa3572e02193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:10 +0000

    Add package pymacaron-core/1.0.142/pymacaron-core-1.0.142.tar.gz

---
+++ packages/pymacaron-core/pymacaron-core-1.0.142.tar.gz/pymacaron-core-1.0.142/pymacaron_core/test.py
@@ -0,0 +1,27 @@
+def return_token(*args):
+    return http_reply({'token': '123123123'}, 200)
+
+# Dummy implementation of model persistence
+class PersistentFoo():
+
+    @staticmethod
+    def load_from_db(*args, **kwargs):
+        # Load object(s) from storage
+        pass
+
+    @staticmethod
+    def save_to_db(object, *args, **kwargs):
+        # Put object into storage
+        pass
+
+
+# Dummy implementation of model inheritance
+class FunnyGrandad():
+
+    def roflol(self):
+        return 'roflol'
+
+class FunnyDad(FunnyGrandad):
+
+    def lol(self):
+        return 'lol'
