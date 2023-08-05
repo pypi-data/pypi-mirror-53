f10e9496e48a6b1e3b428da28e2ff88777912fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:49 +0000

    Add package ax-platform/0.1.6/ax-platform-0.1.6.tar.gz

---
+++ packages/ax-platform/ax-platform-0.1.6.tar.gz/ax-platform-0.1.6/ax/utils/common/timeutils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+from datetime import datetime
+
+import pandas as pd
+
+
+def _ts_to_pandas(ts: int) -> pd.Timestamp:  # pyre-ignore[11]
+    """Convert int timestamp into pandas timestamp."""
+    return pd.Timestamp(datetime.fromtimestamp(ts))
+
+
+def _pandas_ts_to_int(ts: pd.Timestamp) -> int:  # pyre-ignore[11]
+    """Convert int timestamp into pandas timestamp."""
+    return ts.to_pydatetime().timestamp()
