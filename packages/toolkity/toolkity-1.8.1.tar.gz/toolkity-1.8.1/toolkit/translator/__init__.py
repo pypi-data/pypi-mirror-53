94438b5e95e4db32bf8208817543fc46f0b3cb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package toolkity/1.8.1/toolkity-1.8.1.tar.gz

---
+++ packages/toolkity/toolkity-1.8.1.tar.gz/toolkity-1.8.1/toolkit/translator/__init__.py
@@ -0,0 +1,22 @@
+from ..service.monitors import ProxyPool
+from .translate_adapter import TranslateAdapter
+
+__all__ = ["Translator"]
+
+
+class Translator(ProxyPool, TranslateAdapter):
+    """
+        基于代理池的翻译类
+    """
+    name = "translator"
+    web_site = None
+    translate_timeout = None
+    retry_times = None
+
+    def __init__(self, settings):
+        super(Translator, self).__init__(settings=settings)
+        self.web_site = self.settings.get(
+            "WEBSITE", "baidu,qq,google").split(",")
+        self.retry_times = self.settings.get("TRANSLATE_RETRY_TIMES", 10)
+        self.translate_timeout = self.settings.get("TRANSLATE_TIMEOUT", 10)
+        self.headers = self.settings.get("HEADERS") or self.headers
