a14044f11854f42c7e0c91ffe4d92b3535a4038f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package zathura/0.0.6.0b1/zathura-0.0.6.0b1-py3-none-any.whl

---
+++ packages/zathura/zathura-0.0.6.0b1-py3-none-any.whl/ZathuraProject/bugtracker.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import multiprocessing
+
+
+def send_data(data, url):
+    """
+    Makes a little post request here. Will add new stuff later.
+    """
+    return requests.post(url, data=data)
+
+
+def send_data_to_bugtracker(**kwargs):
+    try:
+        data = {
+            "project_token": kwargs["token"],
+            "error_name": kwargs["name"],
+            "error_description": kwargs["description"],
+            "point_of_origin": kwargs["origin"]
+        }
+        if kwargs["user"] is not None:
+            data["identifier"] = kwargs["user"]
+
+        # process = multiprocessing.Process(target=send_data,
+        #                                   args=(data, kwargs["url"]))
+        # process.start()
+        # process.join()
+        _ = send_data(data, kwargs["url"])
+        return True
+    except Exception as e:
+        # process.join()
+        print("Exception -> {}".format(e))
+        return False
+
+
+def send_verbose_log_to_bugtracker(**kwargs):
+    """
+    sends the data from any calling class to server
+    """
+    try:
+        payload = {
+            "point_of_origin": kwargs["origin"],
+            "log_description": kwargs["description"],
+            "project_token": kwargs["project_token"]
+        }
+        if kwargs["user"] is not None:
+            payload["identifier"] = kwargs["user"]
+        # process = multiprocessing.Process(target=send_data,
+        #                                   args=(payload,
+        #                                         kwargs["bugtracker_url"]))
+        # process.start()
+        # process.join()
+        _ = send_data(payload, kwargs["bugtracker_url"])
+        return True
+    except Exception as e:
+        # process.join()
+        print("Exception occurred! : {}".format(e))
+        return False
