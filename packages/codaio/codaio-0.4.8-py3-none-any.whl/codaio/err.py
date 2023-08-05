574204ac8451bff5f80972599a17b2d1009b9617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:41 +0000

    Add package codaio/0.4.8/codaio-0.4.8-py3-none-any.whl

---
+++ packages/codaio/codaio-0.4.8-py3-none-any.whl/codaio/err.py
@@ -0,0 +1,34 @@
+class CodaError(Exception):
+    pass
+
+
+class NoApiKey(CodaError):
+    pass
+
+
+class DocumentNotFound(CodaError):
+    pass
+
+
+class InvalidFilter(CodaError):
+    pass
+
+
+class NotFound(CodaError):
+    pass
+
+
+class TableNotFound(NotFound):
+    pass
+
+
+class RowNotFound(NotFound):
+    pass
+
+
+class ColumnNotFound(NotFound):
+    pass
+
+
+class AmbiguousName(CodaError):
+    pass
