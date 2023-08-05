4318292db0d619aa0e343f574dce39f2adfbbcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:26 +0000

    Add package pyACA/0.1.4/pyACA-0.1.4-py3-none-any.whl

---
+++ packages/pyACA/pyACA-0.1.4-py3-none-any.whl/pyACA/__init__.py
@@ -0,0 +1,15 @@
+#
+name = "pyACA"
+from .computeBeatHisto import computeBeatHisto
+from .computeFeature import computeFeature
+from .computeKey import computeKey
+from .computeNoveltyFunction import computeNoveltyFunction
+from .computePitch import computePitch
+from .computeBeatHisto import computeBeatHistoCl
+from .computeFeature import computeFeatureCl
+from .computeKey import computeKeyCl
+from .computeNoveltyFunction import computeNoveltyFunctionCl
+from .computePitch import computePitchCl
+from .ToolSimpleDtw import ToolSimpleDtw
+from .ToolReadAudio import ToolReadAudio
+from .ToolPreprocAudio import ToolPreprocAudio
