2fe8f9ea7c5377d9588dfc7d46529f08af3fcb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:05:22 +0000

    Add package DockerBuildSystem/1.1.30/DockerBuildSystem-1.1.30-py2.py3-none-any.whl

---
+++ packages/DockerBuildSystem/DockerBuildSystem-1.1.30-py2.py3-none-any.whl/DockerBuildSystem/DockerImageTools.py
@@ -0,0 +1,52 @@
+from DockerBuildSystem import TerminalTools
+import re
+
+def BuildImage(imageName, dockerfile = 'Dockerfile', context = '.'):
+    dockerCommand = "docker build -f " + dockerfile + " -t " + imageName + " " + context
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def RunImage(imageName, properties = ""):
+    dockerCommand = "docker run " + properties + " " + imageName
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def PullImage(imageName):
+    dockerCommand = "docker pull " + imageName
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def PushImage(imageName):
+    dockerCommand = "docker push " + imageName
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def TagImage(sourceImage, targetImage):
+    dockerCommand = "docker tag " + sourceImage + " " + targetImage
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def SaveImage(imageName, outputPath):
+    dockerCommand = "docker save -o " + outputPath + " " + imageName
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def GetContainerExitCode(containerName):
+    terminalCommand = "docker inspect " + containerName + " --format='{{.State.ExitCode}}'"
+    output = TerminalTools.ExecuteTerminalCommandAndGetOutput(terminalCommand)
+    exitCode = TerminalTools.GetNumbersFromString(output)[0]
+    return int(exitCode)
+
+
+def GetContainerRunningCode(containerName):
+    terminalCommand = "docker inspect " + \
+        containerName + " --format='{{.State.Running}}'"
+    output = TerminalTools.ExecuteTerminalCommandAndGetOutput(terminalCommand)
+    running = bool(re.search('true', str(output).lower()))
+    return running
+
+
+def CopyFromContainerToHost(containerName, containerSrc, hostDest):
+    terminalCommand = "docker cp " + \
+        containerName + ":" + containerSrc + " " + hostDest
+    TerminalTools.ExecuteTerminalCommands([terminalCommand])
