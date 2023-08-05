72d6fe5a731839896068dd73f83fc97e097932f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:57 +0000

    Add package wagtail-readingtime/1.1.1/wagtail-readingtime-1.1.1.tar.gz

---
+++ packages/wagtail-readingtime/wagtail-readingtime-1.1.1.tar.gz/wagtail-readingtime-1.1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Installs the Wagtail readingtime plugin which displays an approximation of
+how long it will take a user to read the text input into a rich text field.
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='wagtail-readingtime',
+      version='1.1.1',
+      description='Determine approximate reading time of a piece of text.',
+      long_description='Determine approximate reading time of a piece of text.',
+      url='https://github.com/vixdigital/wagtail-reading-time',
+      author='VIX Digital',
+      author_email='info@vix.digital',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'wagtail>=2.0',
+      ],
+      zip_safe=False)
