9501f0c9752880c0f6227cac888bdd10c1503980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:53 +0000

    Add package rp3-cache/1.2/rp3_cache-1.2-py3-none-any.whl

---
+++ packages/rp3-cache/rp3_cache-1.2-py3-none-any.whl/test/test_Manager.py
@@ -0,0 +1,24 @@
+import pytest
+from rp3_cache.Manager import Manager
+
+
+def test_init():
+    mgr = Manager() 
+
+
+def test_connect():  # Will not work in the DB is not up!!
+    mgr = Manager()
+    mgr.connect()
+
+
+def test_insert():
+    mgr = Manager()
+    mgr.connect()
+    mgr.insert({"_id": "lala"})
+    mgr.insert({"_id": "lala"})  # Should still work the second time
+
+
+def test_find():
+    mgr = Manager()
+    mgr.connect()
+    mgr.find({"_id": "lala"})
