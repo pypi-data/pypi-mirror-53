83ea9b85b318c3425ad814aac9f673eea15b50f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package rqmts/2.0.2/rqmts-2.0.2-py3-none-any.whl

---
+++ packages/rqmts/rqmts-2.0.2-py3-none-any.whl/rqmts/parsing.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pkg_resources, dis
+from collections import defaultdict
+
+def _import(imports):
+ modules = {}
+ errors = []
+ for module in imports:
+    try:
+        modules[module] = __import__(module)
+    except ImportError:
+        errors.append(module)
+ return modules, errors
+
+def fetch(package):
+ return pkg_resources.get_distribution(package).project_name, pkg_resources.get_distribution(package).version
+
+def parse(code):
+    instructions = dis.get_instructions(code)
+    imports = [__ for __ in instructions if 'IMPORT' in __.opname]
+    grouped = defaultdict(list)
+    for instr in imports:
+        grouped[instr.opname].append(instr.argval)
+    return grouped['IMPORT_NAME']
