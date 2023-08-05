c771e3b9be9546a54bc55b0ff8c4937166711532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:51 +0000

    Add package cloudsync/0.2.23/cloudsync-0.2.23-py3-none-any.whl

---
+++ packages/cloudsync/cloudsync-0.2.23-py3-none-any.whl/cloudsync/types.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from enum import Enum
+from dataclasses import dataclass
+
+
+class OType(Enum):
+    DIRECTORY = "dir"
+    FILE = "file"
+    NOTKNOWN = "trashed"
+
+
+class IgnoreReason(Enum):
+    NONE = "none"
+    DISCARDED = "discarded"
+    CONFLICT = "conflict"
+    TEMP_RENAME = "temp rename"
+    IRRELEVANT = "irrelevant"
+
+
+DIRECTORY = OType.DIRECTORY
+FILE = OType.FILE
+NOTKNOWN = OType.NOTKNOWN                  # only allowed for deleted files!
+
+
+@dataclass
+class OInfo:
+    otype: OType                           # fsobject type     (DIRECTORY or FILE)
+    oid: str                               # fsobject id
+    hash: Optional[bytes]                  # fsobject hash     (better name: ohash)
+    path: Optional[str]                    # path
+
+
+@dataclass
+class DirInfo(OInfo):
+    name: Optional[str] = None
+    mtime: Optional[float] = None
+    shared: bool = False
+    readonly: bool = False
