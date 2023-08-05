416a58c83e32cb28758879a1db94accb7fc2e868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:05 +0000

    Add package tql/2019.9.28.10.15.21/tql-2019.9.28.10.15.21-py3-none-any.whl

---
+++ packages/tql/tql-2019.9.28.10.15.21-py3-none-any.whl/tql/algo_ml/opt_params/demo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-Python.
+# @File         : demo
+# @Time         : 2019-09-16 14:17
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  :
+
+
+from tql.algo_ml.opt_params import LGBMOptimizer, XGBOptimizer, CatBoostOptimizer, Optimizer
+
+from sklearn.datasets import make_classification
+
+X, y = make_classification(1000)
+
+# opt = Optimizer(X, y, cv=5, cv_seed=0)
+# print(opt.maximize(1))
+
+opt = LGBMOptimizer(X, y, cv=5, cv_seed=0)
+print(opt.maximize(1))
+print(opt.res)
+
+# opt = XGBOptimizer(X, y, cv=5, cv_seed=0, use_gpu=0)
+# print(opt.maximize(1))
+#
+# opt = CatBoostOptimizer(X, y, cv=5, cv_seed=0, use_gpu=0)
+# print(opt.maximize(1))
