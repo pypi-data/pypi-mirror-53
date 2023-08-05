e672d2c61118fe70b7c0bdbd6cd24b9c65c2ad04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package aiida-bands-inspect/0.2.0b1/aiida-bands-inspect-0.2.0b1.tar.gz

---
+++ packages/aiida-bands-inspect/aiida-bands-inspect-0.2.0b1.tar.gz/aiida-bands-inspect-0.2.0b1/aiida_bands_inspect/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# © 2017-2019, ETH Zurich, Institut für Theoretische Physik
+# Author: Dominik Gresch <greschd@gmx.ch>
+
+__version__ = "0.2.0b1"
+
+from . import calculations
+from . import parsers
+from . import io
---
+++ packages/aiida-bands-inspect/aiida-bands-inspect-0.2.0b1.tar.gz/aiida-bands-inspect-0.2.0b1/aiida_bands_inspect/parsers/plot.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# © 2017-2019, ETH Zurich, Institut für Theoretische Physik
+# Author: Dominik Gresch <greschd@gmx.ch>
+
+from fsc.export import export
+
+from aiida.plugins import DataFactory
+from aiida.parsers.parser import Parser
+
+from ..calculations.plot import PlotCalculation
+
+
+@export
+class PlotParser(Parser):
+    """
+    Parse ``bands-inspect plot_bands`` output to a 'singlefile' data.
+
+    Returns
+    -------
+    plot : aiida.orm.data.singlefile.SinglefileData
+        File containing the generated plot.
+    """
+    def parse(self, **kwargs):
+        try:
+            out_folder = self.retrieved
+        except KeyError as e:
+            return self.exit_codes.ERROR_NO_RETRIEVED_FOLDER
+
+        try:
+            with out_folder.open(
+                PlotCalculation._OUTPUT_FILE_NAME, 'rb'
+            ) as handle:
+                self.out('plot', DataFactory('singlefile')(file=handle))
+        except IOError:
+            return self.exit_codes.ERROR_OUTPUT_FILE_MISSING
