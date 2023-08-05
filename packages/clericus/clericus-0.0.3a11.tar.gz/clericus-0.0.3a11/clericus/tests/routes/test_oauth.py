cf659fc057b8e9180246bafaa2363fd19688f40d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:24 +0000

    Add package clericus/0.0.3a11/clericus-0.0.3a11.tar.gz

---
+++ packages/clericus/clericus-0.0.3a11.tar.gz/clericus-0.0.3a11/clericus/tests/routes/test_oauth.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append(".")
+
+import unittest
+from aiohttp.test_utils import unittest_run_loop
+from ..test_case import ClericusTestCase
+
+from ...routes.authentication import generateOAuthEndpoint
+
+import faker
+fake = faker.Faker()
+
+
+class OAuthTestCase(ClericusTestCase):
+    async def get_application(self):
+        app = await super().get_application()
+
+        return app
+
+    @unittest_run_loop
+    async def testOauth(self):
+
+        # print(self.app)
+
+        resp = await self.client.request("GET", "/oauth/test/")
+        # self.assertEqual(resp.status, 401)
+        # data = await resp.json()
+        # print(resp)
+        # print(data)
