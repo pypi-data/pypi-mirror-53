c76da0ed51a926b701fe8bd372188913411aec8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package simplicial-operators/0.3.1/simplicial_operators-0.3.1.tar.gz

---
+++ packages/simplicial-operators/simplicial_operators-0.3.1.tar.gz/simplicial_operators-0.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simplicial_operators',
+      version='0.3.1',
+      description='A package to define and use simplicial operators',
+      url='https://github.com/ammedmar/simplicial_operators',
+      author='Anibal M. Medina-Mardones',
+      author_email='anibal.medinamardones@epfl.ch',
+      license='MIT',
+      packages=['simplicial_operators'],
+      zip_safe=False)
