3b82c42b363f0285eb9fae63b71d0cb61b2c27f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:16 +0000

    Add package azureml-contrib-explain-model/1.0.65/azureml_contrib_explain_model-1.0.65-py3-none-any.whl

---
+++ packages/azureml-contrib-explain-model/azureml_contrib_explain_model-1.0.65-py3-none-any.whl/azureml/contrib/explain/model/__init__.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Init file for azureml-contrib-explain-model/azureml/contrib/explain/model."""
+from .text_explainer import TextExplainer
+
+__all__ = ["TextExplainer"]
---
+++ packages/azureml-contrib-explain-model/azureml_contrib_explain_model-1.0.65-py3-none-any.whl/azureml/contrib/explain/model/explanation/explanation_client/__init__.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Init file for azureml-contrib-explain-model/azureml/contrib/explain/model/explanation."""
+from azureml.explain.model._internal.explanation_client import ExplanationClient
+
+__all__ = ["ExplanationClient"]
