8f46affc792aae8f6073daae9a8e7cfc48f1d702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:44 +0000

    Add package spring-config-client/0.2/spring-config-client-0.2.tar.gz

---
+++ packages/spring-config-client/spring-config-client-0.2.tar.gz/spring-config-client-0.2/setup.py
@@ -0,0 +1,39 @@
+from collections import OrderedDict
+
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="spring-config-client",
+    version="0.2",
+    author="realbucksavage",
+    description="A client for Spring Config Server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://realbucksavage.github.io/spring-config-client",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6.0",
+    project_urls=OrderedDict(
+        (
+            ("Documentation", "https://github.com/realbucksavage/spring-config-client"),
+            ("Code", "https://github.com/realbucksavage/spring-config-client"),
+            (
+                "Issue tracker",
+                "https://github.com/realbucksavage/spring-config-client/issues",
+            ),
+        )
+    ),
+    install_requires=["requests>=2.22.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Java Libraries",
+    ],
+)
