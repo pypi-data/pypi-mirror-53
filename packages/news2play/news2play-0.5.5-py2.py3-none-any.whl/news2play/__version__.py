e270b18f1581614e0d4b18ce5d8fb98979f70a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:58 +0000

    Add package news2play/0.5.5/news2play-0.5.5-py2.py3-none-any.whl

---
+++ packages/news2play/news2play-0.5.5-py2.py3-none-any.whl/news2play/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'news2play'
+__description__ = 'News for play.'
+__url__ = 'http://xxx.xxx'
+__version__ = '0.3.9'
+__author__ = 'xxx xxx'
+__author_email__ = 'xxx@xxx.xxx'
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = 'Copyright 2019 xxx xxx'
