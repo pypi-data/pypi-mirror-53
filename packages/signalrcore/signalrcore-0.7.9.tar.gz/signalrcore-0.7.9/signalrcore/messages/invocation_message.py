0ef696ed70443b8962fa256b34b670431d4359cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:51 +0000

    Add package signalrcore/0.7.9/signalrcore-0.7.9.tar.gz

---
+++ packages/signalrcore/signalrcore-0.7.9.tar.gz/signalrcore-0.7.9/signalrcore/messages/invocation_message.py
@@ -0,0 +1,54 @@
+from .base_message import BaseHeadersMessage
+"""
+
+An `Invocation` message is a JSON object with the following properties:
+
+* `type` - A `Number` with the literal value 1, indicating that this message is an Invocation.
+* `invocationId` - An optional `String` encoding the `Invocation ID` for a message.
+* `target` - A `String` encoding the `Target` name, as expected by the Callee's Binder
+* `arguments` - An `Array` containing arguments to apply to the method referred to in Target. This is a sequence of JSON `Token`s, encoded as indicated below in the "JSON Payload Encoding" section
+
+Example:
+
+```json
+{
+    "type": 1,
+    "invocationId": "123",
+    "target": "Send",
+    "arguments": [
+        42,
+        "Test Message"
+    ]
+}
+```
+Example (Non-Blocking):
+
+```json
+{
+    "type": 1,
+    "target": "Send",
+    "arguments": [
+        42,
+        "Test Message"
+    ]
+}
+```
+
+"""
+
+
+class InvocationMessage(BaseHeadersMessage):
+    def __init__(
+            self,
+            headers,
+            invocation_id,
+            target,
+            arguments):
+        super(InvocationMessage, self).__init__(1, headers)
+        self.invocation_id = invocation_id
+        self.target = target
+        self.arguments = arguments
+    
+    def __repr__(self):
+        #print(type(self.arguments))
+        return "InvocationMessage: invocation_id {0}, target {1}, arguments {2}".format(self.invocation_id, self.target, self.arguments)
