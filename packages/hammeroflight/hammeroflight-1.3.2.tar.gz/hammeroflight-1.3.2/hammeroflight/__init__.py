996aa8903008759ff6896de26d686153d36b295a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:23 +0000

    Add package hammeroflight/1.3.2/hammeroflight-1.3.2.tar.gz

---
+++ packages/hammeroflight/hammeroflight-1.3.2.tar.gz/hammeroflight-1.3.2/hammeroflight/__init__.py
@@ -0,0 +1,5 @@
+from hammeroflight.arufunctions import cleanandencode, fit_classify, fit_regress
+from hammeroflight.arufunctions import goodness_fit, qualityreport, featureselector
+from hammeroflight.modelcomparator import reg_comparator, clf_comparator
+
+__version__ = "1.3.2"
