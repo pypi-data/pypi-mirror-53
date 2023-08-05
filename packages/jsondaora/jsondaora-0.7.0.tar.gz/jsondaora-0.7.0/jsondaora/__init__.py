1ef090e3d74151d9dfff89aacbde49b03a4b9d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package jsondaora/0.7.0/jsondaora-0.7.0.tar.gz

---
+++ packages/jsondaora/jsondaora-0.7.0.tar.gz/jsondaora-0.7.0/jsondaora/__init__.py
@@ -0,0 +1,22 @@
+"""Interoperates @dataclass with json objects"""
+
+from jsondaora.base import jsondaora
+from jsondaora.dataclasses import asdataclass
+from jsondaora.schema import integer, string
+from jsondaora.serializers import dataclass_asjson, typed_dict_asjson
+from jsondaora.typed_dict import as_typed_dict, as_typed_dict_field
+
+
+__version__ = '0.7.0'
+
+
+__all__ = [
+    jsondaora.__name__,
+    string.__name__,
+    integer.__name__,
+    asdataclass.__name__,
+    as_typed_dict.__name__,
+    as_typed_dict_field.__name__,
+    dataclass_asjson.__name__,
+    typed_dict_asjson.__name__,
+]
