f95af64cc5e857d8b06ed7ce59af5c802d4d8dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:10 +0000

    Add package WhyPy/0.1.0/WhyPy-0.1.0.tar.gz

---
+++ packages/WhyPy/WhyPy-0.1.0.tar.gz/WhyPy-0.1.0/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name="WhyPy",
+    version="0.1.0",
+    author="HoChiak",
+    author_email="hochiak.whypy@gmail.com",
+    description="Toolbox for Causal Inference with Additive Noise Models",
+    long_description="See [GitHub](https://github.com/HoChiak/whypy) for long description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/HoChiak/whypy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
