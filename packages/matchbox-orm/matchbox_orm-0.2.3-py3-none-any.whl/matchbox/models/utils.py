fbe02600551f4d4545d35337779255ce7660c951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:22 +0000

    Add package matchbox-orm/0.2.3/matchbox_orm-0.2.3-py3-none-any.whl

---
+++ packages/matchbox-orm/matchbox_orm-0.2.3-py3-none-any.whl/matchbox/models/utils.py
@@ -0,0 +1,40 @@
+import re
+import uuid
+import iso8601
+import google
+
+from firebase_admin import firestore
+
+from matchbox import database
+
+
+class GeoPointValue:
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def parse(self):
+        return firestore.GeoPoint(self.latitude, self.longitude)
+
+
+def convert_name(name):
+    return re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
+
+
+def generate_id():
+    return uuid.uuid4().hex
+
+
+def google_datetime_to_datetime(gfd):
+    return iso8601.parse_date(gfd.isoformat())
+
+
+def get_reference_fields(collection, value, db=None):
+    if db is None:
+        db = database.db
+
+    return google.cloud.firestore_v1.document.DocumentReference(
+        collection,
+        value,
+        client=db.conn
+    )
