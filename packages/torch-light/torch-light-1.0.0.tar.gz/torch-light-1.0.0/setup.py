15a1cb158b7fecf9885fc98a49b83e2a25b0ad6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package torch-light/1.0.0/torch-light-1.0.0.tar.gz

---
+++ packages/torch-light/torch-light-1.0.0.tar.gz/torch-light-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='torch-light',
+    version='1.0.0',
+    description='A mini framework for pytorch',
+    packages=find_packages(),
+    install_requires=[])
