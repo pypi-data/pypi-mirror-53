b09480b2dbf6ab13dae42798f5f7756368a0b125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:36 +0000

    Add package fsm-hub/1.7/fsm-hub-1.7.tar.gz

---
+++ packages/fsm-hub/fsm-hub-1.7.tar.gz/fsm-hub-1.7/fsm_hub/core.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+from datetime import datetime, timedelta
+from os import environ
+from pony import orm
+from .entities import Fsm, db
+
+
+try:
+    from loguru import logger
+    log = logger.info
+except ImportError:
+    log = print
+
+
+_redis = None
+_redis_url = environ.get("REDIS")
+if _redis_url:
+    from redis import Redis
+    _redis = Redis.from_url(_redis_url)
+
+
+prefix_locked = "."
+
+
+class NotFound(Warning):
+    pass
+
+
+class NotAllowed(Warning):
+    pass
+
+
+def _broadcast(state, id):
+    if _redis:
+         _redis.publish(f"state:{state}", id)
+
+
+def new(state, data={}):
+    with orm.db_session:
+        i = Fsm(state=state, data=data)
+    log(f"new {state} {i.id}")
+    _broadcast(state, i.id)
+
+
+@orm.db_session
+def lock(state):
+    if state.startswith(prefix_locked):
+        raise NotAllowed(f"'{prefix_locked}*' is not allowed")
+    ts = datetime.now() - timedelta(seconds=300)
+    i = orm.select(
+        i for i in Fsm if i.ts > ts and i.state == state
+    ).order_by(Fsm.ts).for_update(skip_locked=True).first()
+    if not i:
+        raise NotFound(state)
+    prev_info = i.to_dict()
+    i.state = f"{prefix_locked}{i.state}"
+    i.ts = datetime.now()
+    delta = (i.ts - prev_info["ts"]).total_seconds()
+    log(f"lock {state} {i.id} {delta}")
+    return prev_info
+
+
+@orm.db_session
+def transit(id, state, data_patch=None):
+    i = Fsm.get_for_update(id=id)
+    if not i:
+        raise NotFound(id)
+    if not i.state.startswith(prefix_locked):
+        raise NotAllowed(i.state, state)
+    prev_state, prev_ts = i.state, i.ts
+    i.state = state
+    i.ts = datetime.now()
+    if data_patch:
+        i.data.update(data_patch)
+    delta = (i.ts - prev_ts).total_seconds()
+    log(f"transit {state} {i.id} {delta} {prev_state}")
+    _broadcast(state, i.id)
+
+
+@orm.db_session
+def info(id):
+    try:
+        return Fsm[id].to_dict()
+    except orm.ObjectNotFound:
+        raise NotFound(id)
+
+
+def _init_this():
+    #orm.sql_debug(True)
+    from os.path import abspath
+    from yaml import safe_load
+    try:
+        options = safe_load(open("database.yaml"))
+    except FileNotFoundError:
+        options = {"provider": "sqlite", "filename": ":memory:"}
+    fn = options.get("filename")
+    if fn and fn != ":memory:":
+        options["filename"] = abspath(fn)  # $CWD/filename
+    db.bind(**options)
+    db.generate_mapping(create_tables=True)
+
+
+if __name__ == '__main__':
+    db.bind('sqlite', filename=':memory:')
+    db.generate_mapping(create_tables=True)
+else:
+    _init_this()
