534fb9b67c72b6764202eb038846bb1fb2c3aca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:40 +0000

    Add package coinpy/0.3/coinpy-0.3.tar.gz

---
+++ packages/coinpy/coinpy-0.3.tar.gz/coinpy-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='coinpy',
+      version='0.3',
+      description='Gaussian distributions',
+      long_description="README.md",
+      long_description_content_type="text/markdown",
+      packages=['coinpy'],
+      zip_safe=False)
