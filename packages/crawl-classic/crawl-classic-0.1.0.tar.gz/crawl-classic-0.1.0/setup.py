2864aa2a50ea131473b1e83729a7ae915ab4c8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:17:01 +0000

    Add package crawl-classic/0.1.0/crawl-classic-0.1.0.tar.gz

---
+++ packages/crawl-classic/crawl-classic-0.1.0.tar.gz/crawl-classic-0.1.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['crawl_classic']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'crawl-classic',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Seth Woodworth',
+    'author_email': 'seth@sethish.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
