59533a3ddfab8f855810e001114a1e69ca14628f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:15 +0000

    Add package tap2junit/0.1.5/tap2junit-0.1.5.tar.gz

---
+++ packages/tap2junit/tap2junit-0.1.5.tar.gz/tap2junit-0.1.5/setup.py
@@ -0,0 +1,36 @@
+from os import path
+
+import setuptools
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md")) as in_file:
+    long_description = in_file.read()
+
+setuptools.setup(
+    name="tap2junit",
+    version="0.1.5",
+    description="Tap13 to jUnit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nodejs/tap2junit",
+    author="Node.js contributors",
+    author_email="cclauss@me.com",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Build Tools",
+    ],
+    keywords="tap13 junit",
+    packages=["tap2junit"],
+    install_requires=["junit-xml", "yamlish"],
+    entry_points={"console_scripts": ["tap2junit = tap2junit.__main__:main"]},
+)
---
+++ packages/tap2junit/tap2junit-0.1.5.tar.gz/tap2junit-0.1.5/tap2junit/__main__.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+import platform
+
+from junit_xml import TestCase, TestSuite
+from tap2junit.tap13 import TAP13 as tap13
+
+
+def map_yaml_to_junit(test):
+    yaml = test.yaml or {}
+    # Even though the name is `duration_ms` the value is in seconds.
+    elapsed_sec = yaml.get("duration_ms", None)
+    t = TestCase(test.description, classname=None, elapsed_sec=elapsed_sec)
+    if test.result == "ok":
+        if test.directive in ("SKIP", "TODO"):
+            t.add_skipped_info(test.comment)
+        else:
+            t.stdout = test.comment
+
+    elif test.result == "not ok":
+        err_code = yaml.get("exitcode", 0)
+        err_severity = yaml.get("severity", "")
+        err_output = yaml.get("stack", "")
+        error_message = "{} ({})".format(err_severity, err_code)
+        if err_code < 0 or err_severity == "crashed":
+            t.add_error_info(error_message, err_output, err_code)
+        else:
+            t.add_failure_info(error_message, err_output, err_code)
+
+    return t
+
+
+def parse(name, data):
+    tap_parser = tap13()
+    tap_parser.parse(data)
+    junit_tests = [map_yaml_to_junit(t) for t in tap_parser.tests]
+    return TestSuite(name, junit_tests, platform.node())
+
+
+def convert(in_file, out_file):
+    input_file = os.path.splitext(in_file.name)[0]
+    data = in_file.read()
+    result = parse(input_file, data)
+    TestSuite.to_file(out_file, [result], prettyprint=True, encoding="utf-8")
+
+
+def main():
+    arg_parser = argparse.ArgumentParser("tap2junit")
+    arg_parser.add_argument(
+        "--input",
+        "-i",
+        type=argparse.FileType("r"),
+        help="path to tap13 file",
+        required=True,
+    )
+    arg_parser.add_argument(
+        "--output",
+        "-o",
+        type=argparse.FileType("w"),
+        help="output file name",
+        required=True,
+    )
+    args = arg_parser.parse_args()
+    convert(args.input, args.output)
+
+
+if __name__ == "__main__":
+    main()
