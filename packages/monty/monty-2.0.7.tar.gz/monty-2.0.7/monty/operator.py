ce9ed22dcc0da7db80ce4ef42890e8a3456b2ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:48 +0000

    Add package monty/2.0.7/monty-2.0.7.tar.gz

---
+++ packages/monty/monty-2.0.7.tar.gz/monty-2.0.7/monty/operator.py
@@ -0,0 +1,34 @@
+"""
+Useful additional functions for operators
+"""
+from __future__ import absolute_import, division, unicode_literals
+
+import operator
+
+
+def operator_from_str(op):
+    """
+    Return the operator associated to the given string `op`.
+
+    raises:
+        `KeyError` if invalid string.
+    """
+    d = {"==": operator.eq,
+         "!=": operator.ne,
+         ">": operator.gt,
+         ">=": operator.ge,
+         "<": operator.lt,
+         "<=": operator.le,
+         '+': operator.add,
+         '-': operator.sub,
+         '*': operator.mul,
+         '%': operator.mod,
+         '^': operator.xor,
+         }
+
+    try:
+        d['/'] = operator.truediv
+    except AttributeError:
+        pass
+
+    return d[op]
