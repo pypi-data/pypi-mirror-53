62ce017df161c1e69826c2eb90788c457e068edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package pytest-ast-back-to-python/1.2.1/pytest-ast-back-to-python-1.2.1.tar.gz

---
+++ packages/pytest-ast-back-to-python/pytest-ast-back-to-python-1.2.1.tar.gz/pytest-ast-back-to-python-1.2.1/pytest_ast_back_to_python.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from _pytest.assertion.rewrite import rewrite_asserts
+from _pytest.monkeypatch import MonkeyPatch
+
+import codegen
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('ast-back-to-python')
+    group.addoption(
+        '--show-ast-as-python',
+        action='store_true',
+        dest='ast_as_python',
+        default=False,
+        help='Show how assertion rewriting recoded the AST.'
+    )
+
+
+def pytest_configure(config):
+    config._ast_as_python = AstAsPython()
+    config.pluginmanager.register(config._ast_as_python)
+
+
+def make_replacement_rewrite_asserts(store):
+
+    def replacement_rewrite_asserts(mod, *args, **kwargs):
+        rewrite_asserts(mod, *args, **kwargs)
+        store.append(codegen.to_source(mod))
+
+    return replacement_rewrite_asserts
+
+
+class AstAsPython(object):
+    def __init__(self):
+        self.store = []
+
+    def pytest_configure(self, config):
+        if not config.getoption('ast_as_python'):
+            return
+
+        mp = MonkeyPatch()
+        mp.setattr(
+            '_pytest.assertion.rewrite.rewrite_asserts',
+            make_replacement_rewrite_asserts(self.store))
+
+        # written pyc files will bypass our patch, so disable reading them
+        mp.setattr(
+            '_pytest.assertion.rewrite._read_pyc',
+            lambda source, pyc, trace=None: None)
+
+        config._cleanup.append(mp.undo)
+
+    def pytest_terminal_summary(self, terminalreporter):
+        if not terminalreporter.config.getoption('ast_as_python'):
+            return
+
+        for source in self.store:
+            terminalreporter._tw.sep("=", "Rewritten AST as Python")
+            terminalreporter.write(source)
---
+++ packages/pytest-ast-back-to-python/pytest-ast-back-to-python-1.2.1.tar.gz/pytest-ast-back-to-python-1.2.1/tests/test_ast_back_to_python.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+
+def test_ast_as_python_on(testdir):
+    """Given I use the cmd line option, I should see rewritten AST as Python."""
+
+    # create a temporary pytest test module
+    testdir.makepyfile("""
+        def test_ast_as_python_on(request):
+            assert request.config.getoption('ast_as_python')
+    """)
+
+    # run pytest with the following cmd args
+    result = testdir.runpytest(
+        '--show-ast-as-python',
+        '-v'
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_ast_as_python_on PASSED*',
+    ])
+    # The expression within the assert statement should be broken down to
+    # constituent parts like this:
+    result.stdout.fnmatch_lines([
+        '*@py_assert1 = request.config',
+        "*@py_assert3 = @py_assert1.getoption",
+        "*@py_assert5 = 'ast_as_python'",
+        '*@py_assert7 = @py_assert3(@py_assert5)',
+    ])
+
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+def test_ast_as_python_off(testdir):
+    """Given no cmd line option, I should see no rewritten AST as Python."""
+
+    # create a temporary pytest test module
+    testdir.makepyfile("""
+        def test_ast_as_python_off(request):
+            assert not request.config.getoption('ast_as_python')
+    """)
+
+    # run pytest with the following cmd args
+    result = testdir.runpytest(
+        '-v'
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_ast_as_python_off PASSED*',
+    ])
+    assert '@py_assert' not in result.stdout.str()
+
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_help_message(testdir):
+    result = testdir.runpytest(
+        '--help',
+    )
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        'ast-back-to-python:',
+        '*--show-ast-as-python*Show how assertion rewriting recoded the AST.',
+    ])
