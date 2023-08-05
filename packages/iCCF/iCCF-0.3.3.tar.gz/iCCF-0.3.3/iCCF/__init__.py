d920b8942ca9a1a1b13a4a45993cd58b8f3dd2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:20 +0000

    Add package iCCF/0.3.3/iCCF-0.3.3.tar.gz

---
+++ packages/iCCF/iCCF-0.3.3.tar.gz/iCCF-0.3.3/iCCF/__init__.py
@@ -0,0 +1,13 @@
+""" iCCF """
+
+from .bisector import BIS, BISplus, BISminus, BIS_HARPS
+from .gaussian import gauss, gaussfit, RV, FWHM, contrast
+from .bigaussian import bigauss, bigaussfit
+from .vspan import vspan
+from .wspan import wspan
+from .meta import makeCCF
+from .meta_ESPRESSO import calculate_ccf as calculate_ccf_ESPRESSO
+from . import utils
+
+from .iCCF import EPS, nEPS
+from .iCCF import Indicators, indicators_from_files
---
+++ packages/iCCF/iCCF-0.3.3.tar.gz/iCCF-0.3.3/setup.py
@@ -0,0 +1,34 @@
+""" iCCF """
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='iCCF',
+    version='0.3.3',
+    description='Analysis of CCF profiles and activity indicators',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/j-faria/iCCF',
+    author='João Faria',
+    author_email='joao.faria@astro.up.pt',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+      entry_points={
+        'console_scripts': [
+            'iccf-fits-to-rdb = iCCF.scripts:fits_to_rdb',
+            'iccf-make-ccf = iCCF.scripts:make_CCF',
+            ]
+        },
+    packages=find_packages(),
+    install_requires=['numpy', 'cached_property'],
+)
