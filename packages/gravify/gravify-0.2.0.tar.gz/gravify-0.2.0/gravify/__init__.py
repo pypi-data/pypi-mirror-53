bc368570cb35bcb853b050d83c4cf074687d39d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:24 +0000

    Add package gravify/0.2.0/gravify-0.2.0.tar.gz

---
+++ packages/gravify/gravify-0.2.0.tar.gz/gravify-0.2.0/gravify/__init__.py
@@ -0,0 +1,29 @@
+import urllib.parse
+import hashlib
+from validate_email import validate_email
+import sentry_sdk
+
+sentry_sdk.init("https://d1a3441649e64a96b52c441233c47f26@sentry.io/1763873") # Automatic bug reporting
+
+class Gravatar():
+    
+    def __init__(self, email, verify_email=True, default_image=None):
+        if verify_email:
+            if not validate_email(email):
+                raise Exception("Invalid email address")
+        
+        self.email = email
+        self.default_image = default_image
+
+    @property
+    def url(self):
+        params = {}
+        
+        if self.default_image != None:
+            params["d"] = self.default_image
+        
+        return "https://www.gravatar.com/avatar/" + hashlib.md5(self.email.encode("utf-8").lower()).hexdigest() + "?" + urllib.parse.urlencode(params)
+    
+    @property
+    def unsecure_url(self):
+        return self.url.replace("https://", "http://")
