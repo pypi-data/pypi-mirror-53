d6393c563ed05af82ff49f4052659bf649f016f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:57 +0000

    Add package vws-python-mock/2019.9.28.0/VWS%20Python%20Mock-2019.9.28.0.tar.gz

---
+++ packages/vws-python-mock/VWS%20Python%20Mock-2019.9.28.0.tar.gz/VWS Python Mock-2019.9.28.0/setup.py
@@ -0,0 +1,47 @@
+"""Setup script for VWS Python Mock, a mock of Vuforia's Web Services APIs."""
+
+from setuptools import find_packages, setup
+
+import versioneer
+
+# We use requirements.txt instead of just declaring the requirements here
+# because this helps with Docker package caching.
+with open('requirements.txt') as requirements:
+    INSTALL_REQUIRES = requirements.readlines()
+
+# We use dev-requirements.txt instead of just declaring the requirements here
+# because Read The Docs needs a requirements file.
+with open('dev-requirements.txt') as dev_requirements:
+    DEV_REQUIRES = dev_requirements.readlines()
+
+with open('README.rst') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='VWS Python Mock',
+    version=versioneer.get_version(),  # type: ignore
+    cmdclass=versioneer.get_cmdclass(),  # type: ignore
+    author='Adam Dangoor',
+    author_email='adamdangoor@gmail.com',
+    description='A mock for the Vuforia Web Services (VWS) API.',
+    long_description=LONG_DESCRIPTION,
+    license='MIT',
+    packages=find_packages(where='src'),
+    zip_safe=False,
+    url='https://vws-python-mock.readthedocs.io',
+    keywords='vuforia mock fake client',
+    package_dir={'': 'src'},
+    install_requires=INSTALL_REQUIRES,
+    extras_require={
+        'dev': DEV_REQUIRES,
+    },
+    include_package_data=True,
+    classifiers=[
+        'Operating System :: POSIX',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Development Status :: 5 - Production/Stable',
+    ],
+)
---
+++ packages/vws-python-mock/VWS%20Python%20Mock-2019.9.28.0.tar.gz/VWS Python Mock-2019.9.28.0/src/mock_vws/_services_validators/content_length_validators.py
@@ -0,0 +1,63 @@
+"""
+Content-Length header validators to use in the mock.
+"""
+
+import uuid
+from typing import Any, Callable, Dict, Tuple
+
+import wrapt
+from requests import codes
+from requests_mock.request import _RequestObjectProxy
+from requests_mock.response import _Context
+
+from .._constants import ResultCodes
+from .._mock_common import json_dump
+
+
+@wrapt.decorator
+def validate_content_length_header(
+    wrapped: Callable[..., str],
+    instance: Any,  # pylint: disable=unused-argument
+    args: Tuple[_RequestObjectProxy, _Context],
+    kwargs: Dict,
+) -> str:
+    """
+    Validate the ``Content-Length`` header.
+
+    Args:
+        wrapped: An endpoint function for `requests_mock`.
+        instance: The class that the endpoint function is in.
+        args: The arguments given to the endpoint function.
+        kwargs: The keyword arguments given to the endpoint function.
+
+    Returns:
+        The result of calling the endpoint.
+    """
+    request, context = args
+    given_content_length = request.headers.get('Content-Length')
+    if not given_content_length:
+        return wrapped(*args, **kwargs)
+
+    try:
+        int(given_content_length)
+    except ValueError:
+        context.status_code = codes.BAD_REQUEST
+        context.headers = {'Connection': 'Close'}
+        return ''
+
+    body_length = len(request.body if request.body else '')
+    given_content_length_value = int(given_content_length)
+    if given_content_length_value > body_length:
+        context.status_code = codes.GATEWAY_TIMEOUT
+        context.headers = {'Connection': 'keep-alive'}
+        return ''
+
+    if given_content_length_value < body_length:
+        context.status_code = codes.UNAUTHORIZED
+        body = {
+            'transaction_id': uuid.uuid4().hex,
+            'result_code': ResultCodes.AUTHENTICATION_FAILURE.value,
+        }
+        return json_dump(body)
+
+    return wrapped(*args, **kwargs)
