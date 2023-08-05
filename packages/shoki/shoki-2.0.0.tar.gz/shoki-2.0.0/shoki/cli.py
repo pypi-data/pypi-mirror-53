35b22eb3b60c0618ad57bc270ba7bc2cf76f705b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:33 +0000

    Add package shoki/2.0.0/shoki-2.0.0.tar.gz

---
+++ packages/shoki/shoki-2.0.0.tar.gz/shoki-2.0.0/shoki/cli.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Run the minutes."""
+
+import argparse
+import json
+import logging
+import pathlib
+import sys
+
+from shoki import datacore
+from shoki import shoki_config
+
+
+# APPLICATION MESSAGES
+USAGE = """Usage:
+shoki <command> [arg,[...]]
+commands:
+\tcreate
+\t\tWill create the minutes of your meeting
+\tinit
+\t\tWill create a config file the first time
+"""
+CONFIG_EXIST = """User configuration already exists.
+You may manually edit the JSON file at:
+{loc}
+"""
+
+# LOGGING
+log = logging.getLogger(__name__)
+
+
+def create(args):
+    """CLI for creating minutes with shoki."""
+    parser = argparse.ArgumentParser(
+        description="shoki is formatting your minutes.",
+        argument_default=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--location",
+        metavar="location",
+        type=str,
+        nargs="?",
+        help="Location of the minutes (file:// or http://)",
+    )
+    parser.add_argument(
+        "-t", "--test", metavar="", help="Create sample minutes using the test file"
+    )
+    parser.add_argument(
+        "--out_format",
+        metavar="out_format",
+        type=str,
+        nargs="?",
+        help="Format of the output",
+    )
+    parsed_args = vars(parser.parse_args(args))
+    return datacore.create_minutes(**parsed_args)
+
+
+def init():
+    """Initialize shoki project."""
+    user_config_file = shoki_config.USER_CONFIG_FILE
+    user_config_path = shoki_config.USER_CONFIG_PATH
+    if not pathlib.Path(user_config_file).exists():
+        user_config_path.mkdir(mode=0o700, parents=True, exist_ok=True)
+        log.debug("Created user config dir {dir}".format(dir=user_config_path))
+        create_user_config(user_config_file)
+    else:
+        print(CONFIG_EXIST.format(loc=user_config_file))
+
+
+def create_user_config(user_config_file):
+    """Create a user config file."""
+    data = json.dumps(
+        shoki_config.default_config, ensure_ascii=False, sort_keys=True, indent=4
+    )
+    user_config_file.write_text(data)
+    log.info("Created new user config file")
+
+
+def main():
+    """Core for the CLI."""
+    if len(sys.argv) < 2:
+        print(USAGE)
+        sys.exit()
+
+    command = sys.argv[1]
+    args = sys.argv[2:]
+
+    if command == "create":
+        content = create(args)
+        print(content)
+    elif command == "init":
+        init()
+    else:
+        print(USAGE)
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/shoki/shoki-2.0.0.tar.gz/shoki-2.0.0/shoki/datacore.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Modeling the minutes."""
+
+import urllib.request
+
+from shoki import formatter
+from shoki import parsing
+from shoki import shoki_config
+
+
+def minutes_as_data(text):
+    """Return a data structure for the minutes."""
+    minutes_data = {}
+    # Handling the meta data of the meeting
+    meta = parsing.meta_headers(text)
+    mtg_date = parsing.meeting_date(meta["date"])
+    minutes_data["title"] = meta["meeting"]
+    minutes_data["scribe"] = [meta["scribe"]]
+    minutes_data["meeting_date"] = mtg_date
+    minutes_data["attendees"] = []
+    minutes_data["record"] = []
+    # Handling the discussions record
+    blocks = parsing.extract_blocks(text)
+    for block in blocks:
+        discussion = parsing.extract_topic(block["topic_line"])
+        discussion["discussion"], description = parsing.extract_prose(
+            block["prose"]
+        )  # noqa
+        discussion["intro"] = description
+        minutes_data["record"].append(discussion)
+    return minutes_data
+
+
+def create_minutes(location=shoki_config.MINUTES_LOCATION, out_format="webcompatwiki"):
+    """Create Minutes."""
+    minutes = ""
+    with urllib.request.urlopen(location) as f:
+        raw_text = f.read().decode("utf-8")
+
+    try:
+        minutes = formatter.convert(minutes_as_data(raw_text), out_format)
+    except Exception as e:
+        print(shoki_config.FORMAT_ERROR.format(error=e))
+    return minutes
