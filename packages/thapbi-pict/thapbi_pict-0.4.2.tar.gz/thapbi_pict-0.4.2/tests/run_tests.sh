75f4526e0684b66e09235bd30e4808ffb80483b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:56 +0000

    Add package thapbi-pict/0.4.2/thapbi_pict-0.4.2.tar.gz

---
+++ packages/thapbi-pict/thapbi_pict-0.4.2.tar.gz/thapbi_pict-0.4.2/tests/run_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright 2019 by Peter Cock, The James Hutton Institute.
+# All rights reserved.
+# This file is part of the THAPBI Phytophthora ITS1 Classifier Tool (PICT),
+# and is released under the "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
+
+IFS=$'\n\t'
+set -euo pipefail
+
+python -c "import thapbi_pict; print('Direct import says version ' + thapbi_pict.__version__)"
+thapbi_pict -v
+python -m thapbi_pict -v
+
+tests/test_woody_hosts.sh
+
+tests/test_dump.sh
+tests/test_load-tax.sh
+tests/test_legacy-import.sh
+tests/test_ncbi-import.sh
+
+# Currently can't easily install these on TravisCI
+if ! [ -x "$(command -v flash)" ]; then
+    echo 'WARNING: flash not installed, skipping some tests'
+elif ! [ -x "$(command -v cutadapt)" ]; then
+    echo 'WARNING: cutadapt not installed, skipping some tests'
+else
+    tests/test_prepare-reads.sh
+    tests/test_pipeline.sh
+fi
+
+tests/test_classify.sh
+tests/test_seq-import.sh
+tests/test_assess.sh
+tests/test_sample-summary.sh
+tests/test_read-summary.sh
+tests/test_edit-graph.sh
