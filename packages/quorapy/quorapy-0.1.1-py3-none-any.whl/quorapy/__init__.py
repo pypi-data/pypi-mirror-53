2d1d5b4f13571d5e1522f149f3b84241139193d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:40 +0000

    Add package quorapy/0.1.1/quorapy-0.1.1-py3-none-any.whl

---
+++ packages/quorapy/quorapy-0.1.1-py3-none-any.whl/quorapy/__init__.py
@@ -0,0 +1,9 @@
+"""
+Package to fetch information from Quora.
+Supports Python 3.7
+Source: https://github.com/djuneor/python-quora
+"""
+
+from .browser import Browser
+from .scraper import Scraper
+from .quora import Quora
