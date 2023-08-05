e4d4947fdf5f0a77e46ca9b3e3e0377c483e60ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:30 +0000

    Add package viewasuser/1.0.5/viewasuser-1.0.5.tar.gz

---
+++ packages/viewasuser/viewasuser-1.0.5.tar.gz/viewasuser-1.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="viewasuser",
+    version="1.0.5",
+    include_package_data=True,
+    author="Tarikul Islam",
+    author_email="ti.rasel@gmail.com",
+    description="Django View As user is a middleware thats provides login in as any user functionality. It is modification of [django-view-as] package. The django-view-as is not supported on django 2 and newer version, so I have modified the package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raselTarikul/ViewASUser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
