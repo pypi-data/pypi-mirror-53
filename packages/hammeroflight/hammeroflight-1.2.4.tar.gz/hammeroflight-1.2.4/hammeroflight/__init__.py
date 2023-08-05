fd21a926b10573f028fe33ea1eabbf9b8c202434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:10 +0000

    Add package hammeroflight/1.2.4/hammeroflight-1.2.4.tar.gz

---
+++ packages/hammeroflight/hammeroflight-1.2.4.tar.gz/hammeroflight-1.2.4/hammeroflight/__init__.py
@@ -0,0 +1,3 @@
+from hammeroflight.arufunctions import cleanandencode, fit_classify, fit_regress
+from hammeroflight.arufunctions import goodness_fit, featureselector, qualityreport
+__version__ = "1.2.4"
