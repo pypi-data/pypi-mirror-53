b5ba9d1c994339dd0a43a016d0efc951d92914b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:33 +0000

    Add package ain-worker/2.0.0/ain-worker-2.0.0.tar.gz

---
+++ packages/ain-worker/ain-worker-2.0.0.tar.gz/ain-worker-2.0.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+# prod: 2.0.0 , staging 1.4.39
+NAME = "ain-worker"
+VERSION = "2.0.0"
+
+packages = [
+  "setuptools",
+  "wheel",
+  "python-dotenv==0.10.1",
+  "docker==3.7.0",
+  "requests-unixsocket==0.1.5",
+  "requests",
+  "click==7.0"
+]
+
+setup(
+  package_data = {
+    '': ['share/*.env','share/log/*.env'],
+    'share/log': ['*.env'],
+    'share': ['*.env'],
+  },
+  name = NAME,
+  version = VERSION,
+  python_requires='>=3',
+  description = "CLI of " + NAME + " a Python script",
+  license = "BSD",
+  url = "https://bitbucket.org/comcomai/ain-v1-worker",
+  packages=find_packages(),
+  install_requires=packages,
+  entry_points = {
+    'console_scripts' : ['ain = ain.ain:call']
+  },
+  classifiers=[
+    "License :: OSI Approved :: BSD License"
+  ]
+)
