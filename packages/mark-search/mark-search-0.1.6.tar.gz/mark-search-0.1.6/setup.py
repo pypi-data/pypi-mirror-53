5208d2c85ade5fd57690aac1b8dc25b0e2190b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:22 +0000

    Add package mark-search/0.1.6/mark-search-0.1.6.tar.gz

---
+++ packages/mark-search/mark-search-0.1.6.tar.gz/mark-search-0.1.6/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, Extension
+from Cython.Distutils import build_ext
+import numpy as np
+
+NAME = "mark-search"
+VERSION = "0.1.6"
+DESCR = "!"
+REQUIRES = [
+    'numpy', 
+    'cython',
+    'faiss',
+    'google-cloud-bigtable',
+    'google-cloud-storage',
+    'gensim',
+    'cachetools'
+]
+
+AUTHOR = "Mark Hudson"
+EMAIL = "mark.cd.hudson@gmail.com"
+
+LICENSE = "Apache 2.0"
+
+SRC_DIR = "mark_search"
+PACKAGES = [SRC_DIR, SRC_DIR + ".combine_scores"]
+
+ext_1 = Extension(SRC_DIR + ".combine_scores.wrapped",
+                  [SRC_DIR + "/combine_scores/wrapped.pyx"],
+                  libraries=[],
+                  include_dirs=[np.get_include()])
+
+
+EXTENSIONS = [ext_1]
+
+if __name__ == "__main__":
+    setup(install_requires=REQUIRES,
+          packages=PACKAGES,
+          zip_safe=False,
+          name=NAME,
+          version=VERSION,
+          description=DESCR,
+          author=AUTHOR,
+          author_email=EMAIL,
+          license=LICENSE,
+          cmdclass={"build_ext": build_ext},
+          ext_modules=EXTENSIONS
+          )
