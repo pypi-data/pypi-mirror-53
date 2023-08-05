46278239959b98190e70f4d67ea5251c073fbc2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package easierai-common-functions/1.0.3/easierai_common_functions-1.0.3.tar.gz

---
+++ packages/easierai-common-functions/easierai_common_functions-1.0.3.tar.gz/easierai_common_functions-1.0.3/common_functions/constants.py
@@ -0,0 +1,32 @@
+# Algorithm
+LSTM = 'lstm'
+DENSE = 'dense'
+PHASED_LSTM = 'phasedlstm'
+
+# Inference type
+ESTIMATOR = 'Estimator'
+CLASSIFIER = 'Classifier'
+
+# Data types
+TIMESERIES = 'timeseries'
+
+# Activation functions
+LINEAR = 'linear'
+SOFTMAX = 'softmax'
+RELU = 'relu'
+
+# Monitors
+VAL_LOSS = 'val_loss'
+
+# File extensions
+MODEL_EXTENSION = 'h5'
+SCALER_EXTENSION = 'pkl'
+LABELENCODER_EXTENSION = 'e.pkl'
+JSON_EXTENSION = 'json'
+
+# Logging levels
+DEBUG = 'debug'
+INFO = 'info'
+WARNING = 'warning'
+ERROR = 'error'
+PERFORMANCE = 'perf'
