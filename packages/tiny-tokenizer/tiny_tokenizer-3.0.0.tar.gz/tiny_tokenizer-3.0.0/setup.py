114d473d3cf14ba2718af73f99ac1fcb0471acc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:03:24 +0000

    Add package tiny-tokenizer/3.0.0/tiny_tokenizer-3.0.0.tar.gz

---
+++ packages/tiny-tokenizer/tiny_tokenizer-3.0.0.tar.gz/tiny_tokenizer-3.0.0/setup.py
@@ -0,0 +1,29 @@
+"""setup.py."""
+
+from os import getenv
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+try:
+    BUILD_WORD_TOKENIZER = int(getenv("BUILD_WORD_TOKENIZER", '1'))
+except ValueError:
+    raise ValueError("BUILD_WORD_TOKENIZER should be integer")
+
+setup(
+    name="tiny_tokenizer",
+    version="3.0.0",
+    description="Tiny Word/Sentence Tokenizer",
+    author="himkt",
+    author_email="himkt@klis.tsukuba.ac.jp",
+    extras_require={
+        "mecab": ["natto-py"],
+        "kytea": ["kytea"],
+        "sentencepiece": ["sentencepiece"],
+        "sudachi": ["SudachiPy"],
+        "all": ["natto-py", "kytea", "sentencepiece", "SudachiPy"]
+    },
+    url="https://github.com/himkt/tiny_tokenizer",
+    packages=find_packages(),
+)
