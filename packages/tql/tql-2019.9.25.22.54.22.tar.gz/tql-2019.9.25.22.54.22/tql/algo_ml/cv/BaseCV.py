83fd7ea5c7c552122c9f90d948ff358fe448733f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package tql/2019.9.25.22.54.22/tql-2019.9.25.22.54.22.tar.gz

---
+++ packages/tql/tql-2019.9.25.22.54.22.tar.gz/tql-2019.9.25.22.54.22/tql/algo_ml/cv/BaseCV.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-Python.
+# @File         : BaseCV
+# @Time         : 2019-09-25 22:02
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
