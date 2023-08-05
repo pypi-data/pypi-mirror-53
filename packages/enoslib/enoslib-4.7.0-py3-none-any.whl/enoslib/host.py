369b8cf4a58723947cf11ded1263a938908956fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:18 +0000

    Add package enoslib/4.7.0/enoslib-4.7.0-py3-none-any.whl

---
+++ packages/enoslib/enoslib-4.7.0-py3-none-any.whl/enoslib/host.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import copy
+
+
+class Host(object):
+    def __init__(
+        self, address, *, alias=None, user=None, keyfile=None, port=None, extra=None
+    ):
+        self.address = address
+        self.alias = alias
+        if self.alias is None:
+            self.alias = address
+        self.user = user
+        self.keyfile = keyfile
+        self.port = port
+        self.extra = extra or {}
+
+    def to_dict(self):
+        return copy.deepcopy(self.__dict__)
+
+    @classmethod
+    def from_dict(cls, d):
+        _d = copy.deepcopy(d)
+        address = _d.pop("address")
+        return cls(address, **_d)
+
+    def to_host(self):
+        """Copy or coerce to a Host."""
+        return Host(
+            self.address,
+            alias=self.alias,
+            user=self.user,
+            keyfile=self.keyfile,
+            port=self.port,
+            extra=self.extra,
+        )
+
+    def __repr__(self):
+        args = [self.alias, "address=%s" % self.address]
+        return "Host(%s)" % ", ".join(args)
+
+    def __str__(self):
+        args = [
+            self.alias,
+            "address=%s" % self.address,
+            "user=%s" % self.user,
+            "keyfile=%s" % self.keyfile,
+            "port=%s" % self.port,
+            "extra=%s" % self.extra,
+        ]
+        return "Host(%s)" % ", ".join(args)
