3be9c1f0ca5de51a94ec0195c95b1ccbff329c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:03 +0000

    Add package grpcalchemy/0.4.4/grpcalchemy-0.4.4.tar.gz

---
+++ packages/grpcalchemy/grpcalchemy-0.4.4.tar.gz/grpcalchemy-0.4.4/grpcalchemy/__init__.py
@@ -0,0 +1,16 @@
+"""
+    gRPCAlchemy
+    ~~~~~
+
+    The Python micro framework for building gPRC application.
+"""
+
+__author__ = """GuangTian Li"""
+__email__ = "guangtian_li@qq.com"
+__version__ = "0.4.4"
+
+__all__ = ["Blueprint", "Context", "grpcservice", "DefaultConfig", "Server"]
+
+from .blueprint import Blueprint, Context, grpcservice
+from .config import DefaultConfig
+from .server import Server
