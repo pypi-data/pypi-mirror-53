ec5ac2e57d8cd44a83ab075652f83597e9072d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:08 +0000

    Add package Eebuilder/1.0/Eebuilder-1.0.tar.gz

---
+++ packages/Eebuilder/Eebuilder-1.0.tar.gz/Eebuilder-1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Eebuilder",
+    version="1.0",
+    author="Will F",
+    author_email="forsbergw82@gmail.com",
+    description="A Python package for creating HTML files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['web'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    url='https://github.com/SuperMeteorite/Eebuilder',
+)
