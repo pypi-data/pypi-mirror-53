a6658f4d31f974b1f8e2a5f912854b0cc42ca456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:49 +0000

    Add package petersPythonLogger/0.1.1/petersPythonLogger-0.1.1.tar.gz

---
+++ packages/petersPythonLogger/petersPythonLogger-0.1.1.tar.gz/petersPythonLogger-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="petersPythonLogger",
+    version="0.1.1",
+    py_modules=["petersPythonLogger"],
+    packages=find_packages(),
+    include_package_data=True,
+)
