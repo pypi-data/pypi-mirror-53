c8d67aa4e7877c8483687523429d547be6e11b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:59 +0000

    Add package pyACA/0.1.1/pyACA-0.1.1.tar.gz

---
+++ packages/pyACA/pyACA-0.1.1.tar.gz/pyACA-0.1.1/pyACA/__init__.py
@@ -0,0 +1,10 @@
+#
+name = "pyACA"
+from .computeBeatHisto import computeBeatHisto
+from .computeFeature import computeFeature
+from .computeKey import computeKey
+from .computeNoveltyFunction import computeNoveltyFunction
+from .computePitch import computePitch
+from .ToolSimpleDtw import ToolSimpleDtw
+from .ToolReadAudio import ToolReadAudio
+from .ToolPreProcAudio import ToolPreProcAudio
