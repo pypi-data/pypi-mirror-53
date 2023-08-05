44748beecef1b310c85f37d45ce4b272069a0f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:25 +0000

    Add package farmOS/0.1.0/farmOS-0.1.0-py3-none-any.whl

---
+++ packages/farmOS/farmOS-0.1.0-py3-none-any.whl/farmOS/config.py
@@ -0,0 +1,18 @@
+from configparser import ConfigParser, BasicInterpolation
+
+class ClientConfig(ConfigParser):
+    def __init__(self):
+        defaults = {
+            'auto_authenticate': True,
+            'development': False,
+            'oauthlib_insecure_transport': False,
+            'oauth_authorization_url': '%(hostname)s/oauth2/authorize',
+            'oauth_redirect_url': '%(hostname)s/api/authorized',
+            'oauth_token_url': '%(hostname)s/oauth2/token',
+        }
+
+        super(ClientConfig, self).__init__(defaults=defaults, interpolation=BasicInterpolation())
+
+    def write(self, path="farmos_default_config.cfg"):
+        with open(path, "w") as config_file:
+            super(ClientConfig, self).write(config_file)
