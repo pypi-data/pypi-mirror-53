dc46717d6dd0262898d12754401b2c35d4b9a0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:32 +0000

    Add package signalrcore/0.7.8/signalrcore-0.7.8.tar.gz

---
+++ packages/signalrcore/signalrcore-0.7.8.tar.gz/signalrcore-0.7.8/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="signalrcore",
+    version="0.7.8",
+    author="mandrewcito",
+    author_email="anbaalo@gmail.com",
+    description="A Python SignalR Core client ",
+    keywords="signalr core client, with invocation auth and streamming. Also automatic reconnect and manually reconnect ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mandrewcito/signalrcore",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2"
+    ],
+    install_requires=[
+        "requests>=2.21.0",
+        "websocket-client>=0.55.0"
+    ]
+)
