d0e7c831db3a0c5321f65535fd8f07eae2369c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:31 +0000

    Add package xknx/0.11.2/xknx-0.11.2.tar.gz

---
+++ packages/xknx/xknx-0.11.2.tar.gz/xknx-0.11.2/setup.py
@@ -0,0 +1,33 @@
+"""Setup for XKNX python package."""
+from setuptools import find_packages, setup
+
+VERSION = '0.11.2'
+
+REQUIRES = [
+    'pyyaml>=5.1',
+    'netifaces>=0.10.9'
+    ]
+
+setup(
+    name='xknx',
+    description='An Asynchronous Library for the KNX protocol. Documentation: http://xknx.io/',
+    version=VERSION,
+    download_url='https://github.com/XKNX/xknx/archive/{}.zip'.format(VERSION),
+    url='http://xknx.io/',
+    author='Julius Mittenzwei',
+    author_email='julius@mittenzwei.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    packages=find_packages(),
+    install_requires=REQUIRES,
+    # python_requires=">=3.5.2",
+    keywords='knx ip knxip eib home automation',
+    zip_safe=False)
---
+++ packages/xknx/xknx-0.11.2.tar.gz/xknx-0.11.2/xknx/exceptions/__init__.py
@@ -0,0 +1,5 @@
+"""Module for XKNX Exception handling."""
+# flake8: noqa
+from .exception import (
+    ConversionError, CouldNotParseAddress, CouldNotParseKNXIP,
+    CouldNotParseTelegram, DeviceIllegalValue, XKNXException)
