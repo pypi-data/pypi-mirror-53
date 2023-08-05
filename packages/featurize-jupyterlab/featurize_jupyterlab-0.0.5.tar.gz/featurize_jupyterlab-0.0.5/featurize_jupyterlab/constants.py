71f243bc56eb9fcc5029d43875c0d07d5113a733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:05 +0000

    Add package featurize-jupyterlab/0.0.5/featurize_jupyterlab-0.0.5.tar.gz

---
+++ packages/featurize-jupyterlab/featurize_jupyterlab-0.0.5.tar.gz/featurize_jupyterlab-0.0.5/featurize_jupyterlab/constants.py
@@ -0,0 +1,20 @@
+from .proto import minetorch_pb2
+
+
+COMMAND_TRAIN = minetorch_pb2.YoMessage.Command.Value('TRAIN')
+COMMAND_HALT = minetorch_pb2.YoMessage.Command.Value('HALT')
+COMMAND_KILL = minetorch_pb2.YoMessage.Command.Value('KILL')
+COMMAND_NONE = minetorch_pb2.YoMessage.Command.Value('NONE')
+
+STATUS_TRAINING = minetorch_pb2.HeyMessage.Status.Value('TRAINING')
+STATUS_IDLE = minetorch_pb2.HeyMessage.Status.Value('IDLE')
+STATUS_ERROR = minetorch_pb2.HeyMessage.Status.Value('ERROR')
+STATUS_NOT_RUNNING = minetorch_pb2.HeyMessage.Status.Value('NOT_RUNNING')
+STATUS_BOOTING = minetorch_pb2.HeyMessage.Status.Value('BOOTING')
+STATUS_STARTING = minetorch_pb2.HeyMessage.Status.Value('STARTING')
+STATUS_STOPPING = minetorch_pb2.HeyMessage.Status.Value('STOPPING')
+STATUS_KILLING = minetorch_pb2.HeyMessage.Status.Value('KILLING')
+
+TIMER_ITERATION = minetorch_pb2.TimerCategory.Value('ITERATION')
+TIMER_EPOCH = minetorch_pb2.TimerCategory.Value('EPOCH')
+TIMER_SNAPSHOT = minetorch_pb2.TimerCategory.Value('SNAPSHOT')
