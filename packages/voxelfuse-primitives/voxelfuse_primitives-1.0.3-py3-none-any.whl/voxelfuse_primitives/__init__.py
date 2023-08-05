34dc060798d0768b8bd59a48f7f6e9d60d3a1efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:44 +0000

    Add package voxelfuse-primitives/1.0.3/voxelfuse_primitives-1.0.3-py3-none-any.whl

---
+++ packages/voxelfuse-primitives/voxelfuse_primitives-1.0.3-py3-none-any.whl/voxelfuse_primitives/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2019
+Dan Aukes, Cole Brauer
+
+VoxelFuse: The multi-material manufacturing toolkit
+
+Primitive generation classes
+"""
+
+name = "voxelfuse_primitives"
+__version__ = "1.0.3"
