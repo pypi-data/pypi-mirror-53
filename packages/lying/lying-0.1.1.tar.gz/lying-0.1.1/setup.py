5b323067c6bb86d53b69b0f54e747ffbcf5706b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package lying/0.1.1/lying-0.1.1.tar.gz

---
+++ packages/lying/lying-0.1.1.tar.gz/lying-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+if __name__ == '__main__':
+    setup(
+        use_scm_version=True,
+        packages=find_packages(exclude=['tests', 'examples', 'ext']),
+        include_package_data=True,
+        setup_requires=[
+            'setuptools_scm',
+        ],
+        install_requires=[],
+        entry_points={
+            'lying.register': [
+                'wait=lying.render.common:WaitRender',
+                'txt=lying.render.common:TextRender',
+                'txt-status=lying.render.common:TextStatusRender',
+                'cmd=lying.render.common:CmdRender',
+                'input=lying.render.common:InputRender',
+            ],
+            'console_scripts': [
+                'lying=lying.cli:main',
+            ]
+        }
+    )
