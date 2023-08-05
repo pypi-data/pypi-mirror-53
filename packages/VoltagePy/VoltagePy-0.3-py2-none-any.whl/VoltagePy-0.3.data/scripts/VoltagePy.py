2cd54ed9721dc8adbb3001b355db2f8d4fec7667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:38 +0000

    Add package VoltagePy/0.3/VoltagePy-0.3-py2-none-any.whl

---
+++ packages/VoltagePy/VoltagePy-0.3-py2-none-any.whl/VoltagePy-0.3.data/scripts/VoltagePy.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+
+#webdriver
+browser = None
+
+#init webdriver, defining options and capabilities
+#by default the methods will autoload browser if None
+
+def start():
+    #options and capabilities
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    capabilities = options.to_capabilities()
+
+    #init webdrive
+    global browser
+    browser = webdriver.Chrome(executable_path="VoltagePy/webdrivers/Chrome/76/linux_76",desired_capabilities=capabilities)
+
+#open the page ex. browser.get("https://www.duckduckgo.com"), if in headless, there's no window.
+def open(url):
+    if(browser is None): start()
+    browser.get(url)
+
+#copy the contet of a "opened page" and return it
+def copyContent(url = ""):
+    html = "PAGE NOT LOADED"
+    
+    #open a page if a url is provided
+    if(url != "") : open(url)
+    
+    #check if the page is loaded 
+    if(browser.title != ""):
+        #if the url is not null, open and copy     
+        html = browser.page_source
+    return html
+
+#close the browser
+def quit():
+    browser.close()
+    browser.quit()
+
+#--exemple of commands and response--
+#start()
+#open("https://www.duckduckgo.com")
+#print(copyContent())
+#quit()
