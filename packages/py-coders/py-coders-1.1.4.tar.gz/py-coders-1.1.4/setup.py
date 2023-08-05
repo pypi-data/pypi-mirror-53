30b13eb072ce5ef8b981d91bc619cbc355c178df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:45 +0000

    Add package py-coders/1.1.4/py-coders-1.1.4.tar.gz

---
+++ packages/py-coders/py-coders-1.1.4.tar.gz/py-coders-1.1.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="py-coders",
+    version="1.1.4",
+    author="James Liu",
+    author_email="contact@jamessliu.com",
+    description="A simple set of symmetric encoder/decoder classes for serializing to and from bytearrays.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/james7132/py-coders",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
