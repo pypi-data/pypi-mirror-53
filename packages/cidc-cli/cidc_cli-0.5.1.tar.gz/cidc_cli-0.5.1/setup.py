6c8ac82edd3c53c7f72a30441c1a4cb276ff4d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:15 +0000

    Add package cidc-cli/0.5.1/cidc_cli-0.5.1.tar.gz

---
+++ packages/cidc-cli/cidc_cli-0.5.1.tar.gz/cidc_cli-0.5.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Setup for the package
+"""
+
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="cidc_cli",
+    version="0.5.1",
+    packages=find_packages(exclude=("tests")),
+    entry_points={
+        "console_scripts": [
+            "cidc = cli.cli:cidc",
+        ]
+    },
+    description='A command-line interface for interacting with the CIDC.',
+    # TODO: Add a long_description, since external people may use this.
+    install_requires=requirements,
+    python_requires=">=3.6",
+)
