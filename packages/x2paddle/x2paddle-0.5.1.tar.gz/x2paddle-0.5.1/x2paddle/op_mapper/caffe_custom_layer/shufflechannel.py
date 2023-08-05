af8e2346ed313e37f44493cf67ad2c00238322aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:00 +0000

    Add package x2paddle/0.5.1/x2paddle-0.5.1.tar.gz

---
+++ packages/x2paddle/x2paddle-0.5.1.tar.gz/x2paddle-0.5.1/x2paddle/op_mapper/caffe_custom_layer/shufflechannel.py
@@ -0,0 +1,29 @@
+from .register import register
+from x2paddle.core.util import *
+
+
+def shufflechannel_shape(input_shape):
+    return input_shape
+
+
+def shufflechannel_layer(inputs, group=None, input_shape=None, name=None):
+    input = inputs[0]
+    c_fm = fluid.layers.split(input, num_or_sections=input_shape[0][1], dim=1)
+    size = int(input_shape[0][1] / group)
+    new_c_fm = []
+    for i in range(size):
+        for j in range(group):
+            new_c_fm.append(c_fm[j * size + i])
+    out = fluid.layers.concat(new_c_fm, axis=1)
+    return out
+
+
+def shufflechannel_weights(name, data=None):
+    weights_name = []
+    return weights_name
+
+
+register(kind='ShuffleChannel',
+         shape=shufflechannel_shape,
+         layer=shufflechannel_layer,
+         weights=shufflechannel_weights)
