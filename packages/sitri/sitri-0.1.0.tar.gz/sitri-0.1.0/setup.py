fbf39e4b1b43ab64314e228f0c2b80adb8282475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:34 +0000

    Add package sitri/0.1.0/sitri-0.1.0.tar.gz

---
+++ packages/sitri/sitri-0.1.0.tar.gz/sitri-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['sitri', 'sitri.config', 'sitri.credentials']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest>=5.1,<6.0']
+
+setup_kwargs = {
+    'name': 'sitri',
+    'version': '0.1.0',
+    'description': 'Library for one endpoint config and credentials managment',
+    'long_description': None,
+    'author': 'Alexander Lavrov',
+    'author_email': 'egnod@ya.ru',
+    'url': 'https://github.com/Egnod/sitri',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
