80d8087d4ca7a23e634db0be80e355c3060777d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:05:21 +0000

    Add package skcom/0.9.2/skcom-0.9.2-py3-none-win_amd64.whl

---
+++ packages/skcom/skcom-0.9.2-py3-none-win_amd64.whl/skcom/__init__.py
@@ -0,0 +1,11 @@
+"""
+skcom
+"""
+# pylint: disable=invalid-name
+
+import os.path
+import logging.config
+
+cfg_file = os.path.dirname(__file__) + '/conf/logging.ini'
+logging.config.fileConfig(cfg_file)
+logger = logging.getLogger('skcom')
