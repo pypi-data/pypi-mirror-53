bd070c2e49056db56b40cfecc7f8ef1a87fe6a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:11 +0000

    Add package pascal-voc-tools/0.1.28/pascal_voc_tools-0.1.28.tar.gz

---
+++ packages/pascal-voc-tools/pascal_voc_tools-0.1.28.tar.gz/pascal_voc_tools-0.1.28/pascal_voc_tools/__init__.py
@@ -0,0 +1,36 @@
+"""
+Pascal Voc Tools
+
+This package provide some tools using for 
+pascal voc format dataset and some usrful
+functions.
+
+some function in the below list:
+1. XmlParser
+2. 
+3. DatasetResize
+4. DataSplit
+5. DarknetDataset
+6. SplitImageAnnotation
+7. AnnotationTools
+8. AnchorsKMeans
+9. DarknetConfig
+"""
+
+from ._version import version
+from .resize import DatasetResize
+from .datatools import DataSplit
+from .datatools import DarknetDataset
+from .image_annotation_split import SplitImageAnnotation
+from .annotation_tools import AnnotationTools
+from .anchors_kmeans import AnchorsKMeans
+from .darknet_config import DarknetConfig
+from ._xml_parser import XmlParser
+
+__all__ = ['version', 'DatasetResize',
+           'DataSplit', 'DarknetDataset', 'SplitImageAnnotation',
+           'AnnotationTools', 'AnchorsKMeans', 'DarknetConfig', 'XmlParser']
+
+name = 'pascal_voc_tools'
+__version__ = version
+
