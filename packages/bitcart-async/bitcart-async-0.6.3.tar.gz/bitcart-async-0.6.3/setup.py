3fa82a5df18fb43aa6d9772fff933b166db3f8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:00 +0000

    Add package bitcart-async/0.6.3/bitcart-async-0.6.3.tar.gz

---
+++ packages/bitcart-async/bitcart-async-0.6.3.tar.gz/bitcart-async-0.6.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bitcart-async",
+    packages=find_packages(),
+    version="0.6.3",
+    license="MIT",
+    description="Bitcart coins support library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="MrNaif2018",
+    author_email="chuff184@gmail.com",
+    url="https://github.com/MrNaif2018/bitcart-sdk/tree/async",
+    keywords=["electrum", " daemon", "bitcart", "asyncio"],
+    install_requires=["aiohttp", "simplejson"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
