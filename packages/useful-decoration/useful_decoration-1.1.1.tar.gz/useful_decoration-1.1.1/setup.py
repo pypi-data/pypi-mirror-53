f1688c121a2b1c0eb8cdd2bdd454fcb1c1d42ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:03 +0000

    Add package useful-decoration/1.1.1/useful_decoration-1.1.1.tar.gz

---
+++ packages/useful-decoration/useful_decoration-1.1.1.tar.gz/useful_decoration-1.1.1/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*- 
+"""
+@User     : Frank
+@File     : setup.py
+@DateTime : 2019-09-16 11:24 
+@Email    : frank.chang@lexisnexis.com
+"""
+from setuptools import setup,find_packages
+import io
+import re
+
+with io.open('README.rst', 'r', encoding='utf8') as f:
+    long_description = f.read()
+
+with io.open("src/useful_decoration/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+
+setup(
+    name="useful_decoration",
+    license='Apache License 2.0',
+    version=version,
+    packages=find_packages("src"),
+    zip_safe=False,
+    include_package_data=True,
+    package_dir={"": "src"},
+    long_description=long_description,
+    url='https://github.com/changyubiao/useful_decoration',
+    author='frank',
+    author_email='frank.chang@lexisnexis.com',
+    description='powerful and useful decorations',
+    python_requires='>=3.6',
+    install_requires=[
+
+
+        "loguru>=0.3.2",
+
+
+        # "Werkzeug>=0.15",
+        # "Jinja2>=2.10.1",
+        # "itsdangerous>=0.24",
+        # "click>=5.1",
+    ],
+
+)
+
