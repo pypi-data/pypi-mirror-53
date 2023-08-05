395934ad52a51204761d2cf01f7d97ea6e66a702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:51 +0000

    Add package JLpyUtils/0.2.13/JLpyUtils-0.2.13.tar.gz

---
+++ packages/JLpyUtils/JLpyUtils-0.2.13.tar.gz/JLpyUtils-0.2.13/JLpyUtils/ML/NeuralNet/__init__.py
@@ -0,0 +1,9 @@
+
+"""
+sub-modules/functions/classes for streamlining common neural-net architectures implemented in tensorflow/keras.
+"""
+from . import Conv2D
+from . import plot
+from . import search
+from . import DenseNet
+from . import utils
