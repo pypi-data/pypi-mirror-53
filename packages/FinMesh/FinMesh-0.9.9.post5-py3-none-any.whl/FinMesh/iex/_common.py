0464bf736a82be8be7292118ed8f33add7400e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:39 +0000

    Add package FinMesh/0.9.9.post5/FinMesh-0.9.9.post5-py3-none-any.whl

---
+++ packages/FinMesh/FinMesh-0.9.9.post5-py3-none-any.whl/FinMesh/iex/_common.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+def append_iex_token(url):
+    token = os.getenv('IEX_TOKEN')
+    print(token)
+    return f"{url}&token={token}"
+
+def get_iex_json_request(url, vprint=True):
+    url = append_iex_token(url)
+    if vprint: print(f"Making request: {url}")
+    result = requests.get(url)
+    if vprint: print(f"Request status code: {result.status_code}")
+    if result.status_code != 200:
+        raise BaseException(result.text)
+    result = result.json()
+    return result
+
+def replace_url_var(url, **kwargs):
+    for key, value in kwargs.items():
+        url = url.replace('{' + key + '}', value)
+    return url
