e3c13a8c499aa4756b5c5948c7f7463636f49c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:58 +0000

    Add package pyslvs/19.9.0.post4/pyslvs-19.9.0.post4-cp37-cp37m-macosx_10_13_x86_64.whl

---
+++ packages/pyslvs/pyslvs-19.9.0.post4-cp37-cp37m-macosx_10_13_x86_64.whl/pyslvs/graph.pyi
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+from typing import Tuple, List, Iterable
+
+
+def link_assortment(g: Graph) -> List[int]:
+    """Return link assortment of the graph."""
+    ...
+
+
+def contracted_link_assortment(g: Graph) -> List[int]:
+    """Return contracted link assortment of the graph."""
+    ...
+
+
+def labeled_enumerate(g: Graph) -> List[Tuple[int, Graph]]:
+    """Enumerate each node with labeled except isomorphism."""
+    ...
+
+
+class Graph:
+
+    """NetworkX-like graph class."""
+
+    edges: Tuple[Tuple[int, int], ...]
+    nodes: Tuple[int, ...]
+
+    def __init__(self, edges: Iterable[Tuple[int, int]]) -> None: ...
+
+    def add_edge(self, n1: int, n2: int) -> None:
+        """Add two nodes for an edge."""
+        ...
+
+    def add_nodes(self, nodes: Iterable[int]) -> None:
+        """Add nodes from a tuple."""
+        ...
+
+    def dof(self) -> int:
+        """Return degrees of freedom."""
+        ...
+
+    def neighbors(self, n: int) -> Tuple[int, ...]:
+        """Neighbors except the node."""
+        ...
+
+    def is_connected(self, without: int = -1) -> bool:
+        """Return True if the graph is not isolated."""
+        ...
+
+    def has_cut_link(self) -> bool:
+        """Return True if the graph has any cut links."""
+        ...
+
+    def is_degenerate(self) -> bool:
+        """Return True if this kinematic chain is degenerate.
+
+        + Prue all multiple contracted links recursively.
+        + Check the DOF of sub-graph if it is lower then zero.
+        """
+        ...
+
+    def is_isomorphic(self, graph: Graph) -> bool:
+        """Return True if two graphs is isomorphic."""
+        ...
+
+    def duplicate(self, nodes: Iterable[int], times: int) -> Graph:
+        """Make graph duplicate by specific nodes. Return a new graph."""
+        ...
+
+    def copy(self) -> Graph:
+        """Copy the graph."""
+        ...
---
+++ packages/pyslvs/pyslvs-19.9.0.post4-cp37-cp37m-macosx_10_13_x86_64.whl/pyslvs/triangulation.pyi
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from typing import (
+    Tuple,
+    List,
+    Sequence,
+    Dict,
+    Optional,
+)
+from .expression import VPoint
+
+
+class ExpressionStack:
+
+    """The stack of Python wrapper."""
+
+    def as_list(self) -> List[Tuple[str, ...]]:
+        ...
+
+    def __repr__(self) -> str:
+        ...
+
+
+def vpoints_configure(
+    vpoints_: Sequence[VPoint],
+    inputs: Sequence[Tuple[int, int]],
+    status: Optional[Dict[int, bool]] = None
+) -> ExpressionStack:
+    """Auto configuration algorithm."""
+    ...
