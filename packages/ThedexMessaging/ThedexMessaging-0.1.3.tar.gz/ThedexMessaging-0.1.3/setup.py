90d9d814ad9fdeb64c881eb6d0b7724ee2acca50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:07 +0000

    Add package ThedexMessaging/0.1.3/ThedexMessaging-0.1.3.tar.gz

---
+++ packages/ThedexMessaging/ThedexMessaging-0.1.3.tar.gz/ThedexMessaging-0.1.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="ThedexMessaging",
+    version="0.1.3",
+    author="Kai Reichart",
+    author_email="kai@reichart.dev",
+    description="A Package to create Messages for the Thedex Messaging API",
+    long_description="[See the GitHub Page for more Details](https://github.com/KaiReichart/Thedex_Messaging.py)",
+    long_description_content_type="text/markdown",
+    url="https://github.com/KaiReichart/Thedex_Messaging.py",
+    download_url="https://github.com/KaiReichart/Thedex_Messaging.py/archive/v0.1.3.tar.gz",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'datetime',
+      ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
