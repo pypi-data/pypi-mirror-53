7b7a465eec49d5894373f682c5ac69d8c9973097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:49 +0000

    Add package django-server-config-generator/0.7/django-server-config-generator-0.7.tar.gz

---
+++ packages/django-server-config-generator/django-server-config-generator-0.7.tar.gz/django-server-config-generator-0.7/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-server-config-generator',
+    url='https://github.com/Afsal-qburst/django-server-config-generator',
+    version='0.7',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A simple Django app to create server config',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Afsal Salim',
+    author_email='afsals@qburst.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
