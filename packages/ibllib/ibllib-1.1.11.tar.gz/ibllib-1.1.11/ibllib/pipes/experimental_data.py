398371e01676dd66911c2c557d375a96a10fc635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:31 +0000

    Add package ibllib/1.1.11/ibllib-1.1.11.tar.gz

---
+++ packages/ibllib/ibllib-1.1.11.tar.gz/ibllib-1.1.11/ibllib/pipes/experimental_data.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+"""
+Entry point to system commands for IBL behaviour pipeline.
+Each function below corresponds to a command-line tool.
+"""
+
+import logging
+from pathlib import Path, PureWindowsPath
+import subprocess
+
+from ibllib.io import flags
+from ibllib.pipes import extract_session
+from ibllib.ephys import ephysqc, spikes, sync_probes
+from oneibl.registration import RegistrationClient
+from oneibl.one import ONE
+
+logger = logging.getLogger('ibllib')
+# set the logging level to paranoid
+logger.setLevel('INFO')
+
+
+def extract_ephys(root_data_folder, dry=False, max_sessions=10):
+    """
+    Extracts ephys session only
+    """
+    extract_session.bulk(root_data_folder, dry=dry, glob_flag='**/extract_ephys.flag')
+
+
+def extract(root_data_folder, dry=False):
+    """
+    Extracts behaviour only
+    """
+    extract_session.bulk(root_data_folder, dry=dry, glob_flag='**/extract_me.flag')
+
+
+def register(root_data_folder, dry=False, one=None):
+    # registration part
+    if not one:
+        one = ONE()
+    rc = RegistrationClient(one=one)
+    rc.register_sync(root_data_folder, dry=dry)
+
+
+def create(root_data_folder, dry=False, one=None):
+    # create the sessions by lookin
+    if not one:
+        one = ONE()
+    rc = RegistrationClient(one=one)
+    rc.create_sessions(root_data_folder, dry=dry)
+
+
+def compress_audio(root_data_folder, dry=False, max_sessions=None):
+    command = 'ffmpeg -i {file_name}.wav -c:a flac -nostats {file_name}.flac'
+    _compress(root_data_folder, command, 'compress_audio.flag', dry=dry, max_sessions=max_sessions)
+
+
+def compress_video(root_data_folder, dry=False, max_sessions=None):
+    command = ('ffmpeg -i {file_name}.avi -codec:v libx264 -preset slow -crf 29 '
+               '-nostats -loglevel 0 -codec:a copy {file_name}.mp4')
+    _compress(root_data_folder, command, 'compress_video.flag', dry=dry, max_sessions=max_sessions)
+
+
+def _compress(root_data_folder, command, flag_pattern, dry=False, max_sessions=None):
+    #  runs a command of the form command = "ls -1 {file_name}.avi"
+    c = 0
+    for flag_file in Path(root_data_folder).rglob(flag_pattern):
+        ses_path = flag_file.parent
+        files2compress = flags.read_flag_file(flag_file)
+        if isinstance(files2compress, bool):
+            Path(flag_file).unlink()
+            continue
+        for f2c in files2compress:
+            cfile = ses_path.joinpath(PureWindowsPath(f2c))
+            c += 1
+            if max_sessions and c > max_sessions:
+                return
+            print(cfile)
+            if dry:
+                continue
+            if not cfile.exists():
+                logger.error('NON-EXISTING RAW FILE: ' + str(cfile))
+            # run the compression command redirecting output
+            cfile.parent.joinpath(cfile.stem)
+            # if the output file already exists, overwrite it
+            outfile = cfile.parent / (cfile.stem + '.mp4')
+            if outfile.exists():
+                outfile.unlink()
+            command2run = command.format(file_name=cfile.parent.joinpath(cfile.stem))
+            process = subprocess.Popen(command2run, shell=True, stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+            info, error = process.communicate()
+            if process.returncode != 0:
+                logger.error('COMPRESSION FAILED FOR ' + str(cfile))
+                flags.excise_flag_file(flag_file, removed_files=f2c)
+                with open(cfile.parent.joinpath('extract.error'), 'w+') as fid:
+                    fid.write(command2run)
+                    fid.write(error.decode())
+                continue
+            # if the command was successful delete the original file
+            cfile.unlink()
+            # then remove the file from the compress flag file
+            flags.excise_flag_file(flag_file, removed_files=f2c)
+            # and add the file to register_me.flag
+            flags.write_flag_file(ses_path.joinpath('register_me.flag'), file_list=cfile.stem)
+
+
+def qc_ephys(root_data_folder, dry=False, max_sessions=10, force=False):
+    """
+    Computes raw electrophysiology QC
+    """
+    qcflags = Path(root_data_folder).rglob('qc_ephys.flag')
+    c = 0
+    for qcflag in qcflags:
+        session_path = qcflag.parent
+        c += 1
+        if c >= max_sessions:
+            return
+        if dry:
+            print(qcflag.parent)
+            continue
+        qc_files = ephysqc.qc_session(session_path, dry=dry, force=force)
+        qcflag.unlink()
+        flags.write_flag_file(session_path.joinpath('register_me.flag'), file_list=qc_files)
+
+
+def sync_merge_ephys(root_data_folder, dry=False, force=False):
+    """
+    Sync probes and merge spike sorting output.
+    For single probe dataset, output ks2 as ALF dataset
+    """
+    root_data_folder = Path(root_data_folder)
+    sync_probes.sync(root_data_folder)
+    spikes.merge_probes(root_data_folder)
+    flags.write_flag_file(root_data_folder.joinpath('register_me.flag'))
---
+++ packages/ibllib/ibllib-1.1.11.tar.gz/ibllib-1.1.11/ibllib/pipes/extract_session.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Niccolò Bonacchi
+# @Date: Thursday, October 11th 2018, 12:11:13 pm
+"""
+Find task name
+Check if extractors for specific task exist
+Extract data OR return error to user saying that the task has no extractors
+"""
+import logging
+from pathlib import Path
+import traceback
+
+from ibllib.io.extractors import (ephys_trials, ephys_fpga,
+                                  biased_wheel, biased_trials,
+                                  training_trials, training_wheel)
+from ibllib.io import raw_data_loaders as raw
+import ibllib.io.flags as flags
+
+logger_ = logging.getLogger('ibllib.alf')
+
+
+# this is a decorator to add a logfile to each extraction and registration on top of the logging
+def log2sessionfile(func):
+    def func_wrapper(sessionpath, *args, **kwargs):
+        fh = logging.FileHandler(Path(sessionpath).joinpath('extract_register.log'))
+        str_format = '%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s'
+        fh.setFormatter(logging.Formatter(str_format))
+        logger_.addHandler(fh)
+        f = func(sessionpath, *args, **kwargs)
+        fh.close()
+        logger_.removeHandler(fh)
+        return f
+    return func_wrapper
+
+
+def get_task_extractor_type(task_name):
+    """
+    Splits the task name according to naming convention:
+    -   ignores everything
+    _iblrig_tasks_biasedChoiceWorld3.7.0 returns "biased"
+    _iblrig_tasks_trainingChoiceWorld3.6.0 returns "training'
+    :param task_name:
+    :return:
+    """
+    if '_biasedChoiceWorld' in task_name:
+        return 'biased'
+    elif '_trainingChoiceWorld' in task_name:
+        return 'training'
+    elif 'ephysChoiceWorld' in task_name:
+        return 'ephys'
+    elif task_name.startswith('_iblrig_tasks_ephys_certification'):
+        return 'sync_ephys'
+
+
+def get_session_extractor_type(session_path):
+    """
+    From a session path, loads the settings file, finds the task and checks if extractors exist
+    task names examples:
+    :param session_path:
+    :return: bool
+    """
+    settings = raw.load_settings(session_path)
+    if settings is None:
+        logger_.error(f'ABORT: No data found in "raw_behavior_data" folder {session_path}')
+        return False
+    extractor_type = get_task_extractor_type(settings['PYBPOD_PROTOCOL'])
+    if extractor_type:
+        return extractor_type
+    else:
+        logger_.warning(str(session_path) +
+                        f" No extractors were found for {extractor_type} ChoiceWorld")
+        return False
+
+
+def is_extracted(session_path):
+    sp = Path(session_path)
+    if (sp / 'alf').exists():
+        return True
+    else:
+        return False
+
+
+@log2sessionfile
+def from_path(session_path, force=False, save=True):
+    """
+    Extract a session from full ALF path (ex: '/scratch/witten/ibl_witten_01/2018-12-18/001')
+
+    :param force: (False) overwrite existing files
+    :param save: (True) boolean or list of ALF file names to extract
+    :return: None
+    """
+    logger_.info('Extracting ' + str(session_path))
+    extractor_type = get_session_extractor_type(session_path)
+    if is_extracted(session_path) and not force:
+        logger_.info(f"Session {session_path} already extracted.")
+        return
+    if extractor_type == 'training':
+        settings, data = raw.load_bpod(session_path)
+        logger_.info('training session on ' + settings['PYBPOD_BOARD'])
+        training_trials.extract_all(session_path, data=data, save=save)
+        training_wheel.extract_all(session_path, bp_data=data, save=save)
+        logger_.info('session extracted \n')  # timing info in log
+    if extractor_type == 'biased':
+        settings, data = raw.load_bpod(session_path)
+        logger_.info('biased session on ' + settings['PYBPOD_BOARD'])
+        biased_trials.extract_all(session_path, data=data, save=save)
+        biased_wheel.extract_all(session_path, bp_data=data, save=save)
+        logger_.info('session extracted \n')  # timing info in log
+    if extractor_type == 'ephys':
+        data = raw.load_data(session_path)
+        logger_.info('extract bpod for ephys session')
+        ephys_trials.extract_all(session_path, data=data, save=save)
+        logger_.info('extract FPGA information for ephys session')
+        ephys_fpga.extract_all(session_path, save=save)
+
+    if extractor_type == 'sync_ephys':
+        ephys_fpga.extract_sync(session_path, save=save)
+
+
+def bulk(subjects_folder, dry=False, glob_flag='**/extract_me.flag'):
+    ses_path = Path(subjects_folder).glob(glob_flag)
+    for p in ses_path:
+        # the flag file may contains specific file names for a targeted extraction
+        save = flags.read_flag_file(p)
+        if dry:
+            print(p)
+            continue
+        try:
+            from_path(p.parent, force=True, save=save)
+        except Exception as e:
+            error_message = str(p.parent) + ' failed extraction' + '\n    ' + str(e)
+            error_message += traceback.format_exc()
+            err_file = p.parent.joinpath('extract_me.error')
+            p.replace(err_file)
+            with open(err_file, 'w+') as f:
+                f.write(error_message)
+            logger_.error(error_message)
+            continue
+        p.unlink()
+        flags.write_flag_file(p.parent.joinpath('register_me.flag'), file_list=save)
+
+
+if __name__ == "__main__":
+    sess = '/home/nico/Projects/IBL/scratch/test_iblrig_data/Subjects/IBL-T1/2019-02-19/001'  # noqa
+    bulk(sess)
+    print('.')
