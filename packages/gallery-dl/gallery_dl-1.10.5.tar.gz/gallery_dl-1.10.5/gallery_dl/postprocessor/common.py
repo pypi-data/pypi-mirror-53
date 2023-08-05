61d9f71f2a985c2718569e9ae89c27b7a16c23c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:32 +0000

    Add package gallery-dl/1.10.5/gallery_dl-1.10.5.tar.gz

---
+++ packages/gallery-dl/gallery_dl-1.10.5.tar.gz/gallery_dl-1.10.5/gallery_dl/postprocessor/common.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018-2019 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Common classes and constants used by postprocessor modules."""
+
+from . import log
+
+
+class PostProcessor():
+    """Base class for postprocessors"""
+    log = log
+
+    def prepare(self, pathfmt):
+        """ """
+
+    def run(self, pathfmt):
+        """Execute the postprocessor for a file"""
+
+    def finalize(self):
+        """Cleanup"""
+
+    def __repr__(self):
+        return self.__class__.__name__
