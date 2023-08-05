1ccb4b8cdc82f5130d0ff9a402ead8e59e14b4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:13 +0000

    Add package dylan/0.1.0/dylan-0.1.0-py3-none-any.whl

---
+++ packages/dylan/dylan-0.1.0-py3-none-any.whl/dylan/dylan.py
@@ -0,0 +1,11 @@
+def main():
+    print(
+        "Hi! 👋 I'm Dylan Baker \n"
+        "I'm a freelance analytics consultant. \n"
+        "I help businesses with reporting, infrastructure and culture to help them make better data-driven decisions.\n"
+        "Come find me at dbanalytics.co."
+    )
+
+
+if __name__ == "__main__":
+    main()
