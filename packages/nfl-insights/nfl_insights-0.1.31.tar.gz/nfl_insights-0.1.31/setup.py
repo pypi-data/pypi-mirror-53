e22542417065c337a48c19667acb234521f42be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:52 +0000

    Add package nfl-insights/0.1.31/nfl_insights-0.1.31.tar.gz

---
+++ packages/nfl-insights/nfl_insights-0.1.31.tar.gz/nfl_insights-0.1.31/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='nfl_insights',
+      version='0.1.31',
+      description='Contendo.ai nfl insights package',
+      url='https://github.com/sportsight-pro/sportsight-core/tree/master/nfl_insights',
+      author='Yahali Sherman, contendo.ai',
+      author_email='yahali@contendo.ai',
+      license='MIT',
+      packages=['nfl_insights'],
+      install_requires=[
+          'contendo_utils',
+          'gspread_pandas',
+      ],
+      zip_safe=False)
