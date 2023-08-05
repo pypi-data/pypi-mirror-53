e8e7448f011ebd9694f72283b9aec4282ed22f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package spyraio/0.0.8/spyraio-0.0.8-py3-none-any.whl

---
+++ packages/spyraio/spyraio-0.0.8-py3-none-any.whl/spyraio/classification/DecisionTree.py
@@ -0,0 +1,19 @@
+from spyraio.classification.model import ModelClassification
+
+
+class DecisionTreeClassifier(ModelClassification):
+    def __init__(self):
+        print('hello')
+    
+    
+    def compute_model(X_train, y_train, criterion='gini', max_depth=None, 
+                      max_leaf_nodes=None):
+        from sklearn.tree import DecisionTreeClassifier
+
+        classifier = DecisionTreeClassifier(criterion=criterion, 
+                                            max_depth=max_depth,
+                                            max_leaf_nodes=max_leaf_nodes)
+        classifier.fit(X_train, y_train)
+
+        return classifier
+    
