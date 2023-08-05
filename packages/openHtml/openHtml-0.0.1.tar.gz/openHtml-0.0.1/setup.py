5423a19208ba97b58f4434940f30679e709de6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:33 +0000

    Add package openHtml/0.0.1/openHtml-0.0.1.tar.gz

---
+++ packages/openHtml/openHtml-0.0.1.tar.gz/openHtml-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("readme.md","r",encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openHtml",
+    version="0.0.1",
+    author="ygunoil",
+    author_email="ygunoil@163.com",
+    description="auto open html",
+    long_description=long_description ,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypi/openHtml",
+    packages=setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
