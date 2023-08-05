6f618d3b7540190d3578fdb5196c44ce4d853c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:08 +0000

    Add package pyponto/1.0.0/pyponto-1.0.0.tar.gz

---
+++ packages/pyponto/pyponto-1.0.0.tar.gz/pyponto-1.0.0/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pyponto',
+    version='1.0.0',
+    url='https://github.com/hendrixcosta/pyponto',
+    license='MIT License',
+    author='Hendrix Costa',
+    author_email='hendrixcosta@gmail.com',
+    keywords='pyponto registro ponto funcionario',
+    description=u'Registrar ponto de funcionários',
+    packages=['pyponto'],
+    install_requires=[],
+)
