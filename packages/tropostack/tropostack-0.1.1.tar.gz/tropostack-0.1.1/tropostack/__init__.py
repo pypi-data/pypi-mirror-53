978e419fb6c7ec3fe9f42977a77055ea30b1a419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:55 +0000

    Add package tropostack/0.1.1/tropostack-0.1.1.tar.gz

---
+++ packages/tropostack/tropostack-0.1.1.tar.gz/tropostack-0.1.1/tropostack/__init__.py
@@ -0,0 +1,7 @@
+"""
+tropostack - Opionated wrapper around the excellent Troposphere library for easy creation and management of CloudFormation stacks
+"""
+
+__version__ = '0.1.1'
+__author__ = 'tie <tropostack@morp.org>'
+__all__ = []
