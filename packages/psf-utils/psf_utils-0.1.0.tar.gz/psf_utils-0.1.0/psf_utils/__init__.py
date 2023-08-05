5123ccf3de7ca8e8e5e8d752b332838a3aa62142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:42 +0000

    Add package psf-utils/0.1.0/psf_utils-0.1.0.tar.gz

---
+++ packages/psf-utils/psf_utils-0.1.0.tar.gz/psf_utils-0.1.0/psf_utils/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.0'
+__released__ = '2019-09-25'
+
+from .psf import PSF, UnknownSignal
---
+++ packages/psf-utils/psf_utils-0.1.0.tar.gz/psf_utils-0.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='psf_utils',
+    version='0.1.0',
+    description='Cadence PSF file utilities',
+    long_description=readme,
+    author="Ken Kundert",
+    author_email='psf_utils@nurdletech.com',
+    url='https://psf_utils.readthedocs.io',
+    download_url='https://github.com/kenkundert/psf_utils/tarball/master',
+    license='GPLv3+',
+    zip_safe = True,
+    packages='psf_utils'.split(),
+    entry_points = {'console_scripts': [
+        'list_psf=psf_utils.list:list_signals',
+        'plot_psf=psf_utils.plot:plot_signals',
+    ]},
+    install_requires='docopt inform matplotlib numpy ply quantiphy shlib'.split(),
+    python_requires='>=3.6',
+    keywords='cadence spectre PSF simulation'.split(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+        'Topic :: Scientific/Engineering',
+    ],
+)
