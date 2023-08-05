5c476550aadc0189ab80cf3429089ca8d7e2176e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:31 +0000

    Add package sequitur/1.0.0/sequitur-1.0.0.tar.gz

---
+++ packages/sequitur/sequitur-1.0.0.tar.gz/sequitur-1.0.0/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+# from codecs import open
+
+setup(
+    name="sequitur",
+    description="Recurrent Autoencoder for sequence data that works out-of-the-box ",
+    version="v1.0.0",
+    packages=["sequitur"],
+    python_requires=">=3",
+    url="https://github.com/shobrook/sequitur",
+    author="shobrook",
+    author_email="shobrookj@gmail.com",
+    install_requires=['torch'],
+    keywords=["sequitur", "autoencoder", "lstm", "sequence", "sequence-data", "seq2seq"],
+    license="MIT"
+)
