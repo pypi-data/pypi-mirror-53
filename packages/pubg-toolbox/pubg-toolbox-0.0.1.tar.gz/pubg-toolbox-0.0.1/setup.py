06dc3c82405514f1d48c60ab8b489a82935938fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:06 +0000

    Add package pubg-toolbox/0.0.1/pubg-toolbox-0.0.1.tar.gz

---
+++ packages/pubg-toolbox/pubg-toolbox-0.0.1.tar.gz/pubg-toolbox-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'pubg-toolbox',
+    version = '0.0.1',
+    author = 'Junhan Zhu',
+    author_email = 'junhanoct@gmail.com',
+    description = 'Simple queries and handles for PUBG data analysis',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/junhan-z/pubg-toolbox',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+    ],
+    python_requires = '>=3',
+)
