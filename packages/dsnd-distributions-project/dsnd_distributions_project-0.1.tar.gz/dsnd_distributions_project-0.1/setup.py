d01cb07708aa26c9c6b3a77333bb62ede3cb3020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package dsnd-distributions-project/0.1/dsnd_distributions_project-0.1.tar.gz

---
+++ packages/dsnd-distributions-project/dsnd_distributions_project-0.1.tar.gz/dsnd_distributions_project-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_project',
+      version='0.1',
+      description='Gaussian & Binoamial distributions',
+      author = 'Eduardo Gonzalez',
+      author_email = 'gonzandrobles@gmail.com',
+      packages=['dsnd_distributions_project'],
+      zip_safe=False)
