f1abd11000364ab409d7140f947db58bbd1ea275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:02 +0000

    Add package django-terra-geocrud/0.3.10/django-terra-geocrud-0.3.10.tar.gz

---
+++ packages/django-terra-geocrud/django-terra-geocrud-0.3.10.tar.gz/django-terra-geocrud-0.3.10/terra_geocrud/properties/widgets.py
@@ -0,0 +1,121 @@
+import base64
+import inspect
+import mimetypes
+import sys
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import get_storage_class
+from django.template.defaultfilters import date as date_filter
+from django.utils.dateparse import parse_date
+
+from terra_geocrud import settings as app_settings
+
+
+def get_widgets_choices():
+    """ List all widget available in choices format """
+    widgets = []
+
+    for name, obj in inspect.getmembers(sys.modules[__name__], inspect.isclass):
+        if (issubclass(obj, BaseWidget) or issubclass(obj, BaseDataFileWidget)) and (
+                obj in BaseWidget.__subclasses__() or obj in BaseDataFileWidget.__subclasses__()) and not isinstance(
+                obj, BaseDataFileWidget):
+            # get BaseWidget subclasses
+            widgets.append((f"{__name__}.{name}", name))
+
+    return widgets
+
+
+class BaseWidget(object):
+    """ Base widget. Inherit all widget from it, and override render method. """
+    def __init__(self, feature, prop, args=None):
+        if args is None:
+            args = {}
+        self.feature = feature
+        self.property = prop
+        self.args = args
+        self.value = self.feature.properties.get(self.property)
+
+    def render(self):
+        raise NotImplementedError()
+
+
+class BaseDataFileWidget(BaseWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_info, self.file_content = self.get_info_content()
+
+    def get_info_content(self):
+        if self.value:
+            return self.value.split(';base64,')
+        else:
+            return None, None
+
+    def get_storage(self):
+        StorageClass = get_storage_class(import_path=app_settings.TERRA_GEOCRUD['DATA_FILE_STORAGE_CLASS'])
+        return StorageClass()
+
+    def get_storage_file_path(self):
+        if self.file_info:
+            # guess filename and extension
+            infos = self.file_info.split(';') if self.file_info else ''
+            try:
+                # get name
+                file_name = infos[1].split('=')[1]
+            except IndexError:
+                extension = mimetypes.guess_extension(infos[0].split(':')[1])
+                file_name = f"{self.property}{extension}"
+
+            # build name in storage
+            return f'terra_geocrud/data_file/features/{self.feature.pk}/{self.property}/{file_name}'
+
+    def get_storage_file_url(self):
+        # check if there is file in storage, else store it
+        storage = self.get_storage()
+
+        storage_file_path = self.get_storage_file_path()
+        if storage_file_path:
+            if not storage.exists(storage_file_path):
+                # create if not exists
+                storage.save(storage_file_path, ContentFile(base64.b64decode(self.file_content)))
+
+            return storage.url(storage_file_path)
+
+
+class DataUrlToImgWidget(BaseDataFileWidget):
+    help = "Render img html tag with url to get b64 img stored in properties"
+
+    def render(self):
+        if self.value:
+            attrs = self.args.get('attrs', {})
+            final_attrs = ""
+            for key, v in attrs.items():
+                final_attrs += f' {key}="{v}"'
+            url = self.get_storage_file_url()
+            return f'<img src="{url}" {final_attrs} />'
+
+
+class FileAhrefWidget(BaseDataFileWidget):
+    help = "Render html tag with url to download b64 file stored in properties. args: text (string, default 'Download'"
+
+    def render(self):
+        if self.value:
+            # get html attrs
+            attrs = self.args.get('attrs', {})
+            # get text content
+            text = self.args.get('text', 'Download')
+            # set target="_blank" by default
+            attrs.setdefault('target', '_blank')
+            final_attrs = ""
+            for key, v in attrs.items():
+                final_attrs += f' {key}="{v}"'
+            url = self.get_storage_file_url()
+            return f'<a href="{url}" {final_attrs}>{text}</a>'
+
+
+class DateFormatWidget(BaseWidget):
+    help = "Format date with given format. args: format (string, default to SHORT_DATE_FORMAT)"
+
+    def render(self):
+        if self.value:
+            date_format = self.args.get('format', 'SHORT_DATE_FORMAT')
+            return date_filter(parse_date(self.value), date_format)
---
+++ packages/django-terra-geocrud/django-terra-geocrud-0.3.10.tar.gz/django-terra-geocrud-0.3.10/terra_geocrud/serializers.py
@@ -0,0 +1,248 @@
+from collections import OrderedDict
+from copy import deepcopy
+
+from django.utils.translation import gettext_lazy as _
+from pathlib import Path
+from rest_framework import serializers
+from rest_framework.reverse import reverse
+from rest_framework_gis import serializers as geo_serializers
+from template_model.models import Template
+
+from geostore.serializers import LayerSerializer, FeatureSerializer
+from . import models
+from . import settings as app_settings
+
+
+class LayerViewSerializer(LayerSerializer):
+    schema = serializers.JSONField(write_only=True)
+    # disable other fields
+    name = serializers.CharField(read_only=True)
+    layer_groups = None
+    routing_url = None
+    shapefile_url = None
+    geojson_url = None
+    layer_intersects = None
+
+    class Meta(LayerSerializer.Meta):
+        pass
+
+
+class CrudViewSerializer(serializers.ModelSerializer):
+    layer = LayerViewSerializer()
+    extent = serializers.SerializerMethodField()
+    exports = serializers.SerializerMethodField()
+    ui_schema = serializers.JSONField(source='grouped_ui_schema')
+    form_schema = serializers.JSONField(source='grouped_form_schema')
+    map_style = serializers.SerializerMethodField()
+    feature_endpoint = serializers.SerializerMethodField(
+        help_text=_("Url endpoint for view's features")
+    )
+    feature_list_properties = serializers.SerializerMethodField(
+        help_text=_("Available properties for feature datatable. Ordered, {name: {title, type}}")
+    )
+
+    def get_default_map_style(self, obj):
+        style_settings = app_settings.TERRA_GEOCRUD.get('STYLES', {})
+        if obj.layer.is_point:
+            return style_settings.get('point')
+        elif obj.layer.is_linestring:
+            return style_settings.get('line')
+        elif obj.layer.is_polygon:
+            return style_settings.get('polygon')
+
+    def get_map_style(self, obj):
+        style = obj.map_style
+        return style if style else self.get_default_map_style(obj)
+
+    def get_exports(self, obj):
+        return [{
+            "name": "shapefile",
+            "url": reverse('geostore:layer-shapefile', args=[obj.layer_id, ])
+        }, {
+            "name": "geojson",
+            "url": reverse('geostore:layer-geojson', args=[obj.layer_id, ])
+        }]
+
+    def get_extent(self, obj):
+        # TODO: use annotated extent
+        return obj.extent
+
+    def get_feature_list_properties(self, obj):
+        # TODO: keep default properties at first, then order by property title
+        default_list = obj.default_list_properties or obj.list_available_properties[:8]
+        result = {
+            prop: {
+                "title": obj.layer.get_property_title(prop),
+                "selected": True if prop in default_list else False,
+                "type": obj.layer.get_property_type(prop)
+            }
+            for prop in obj.list_available_properties
+        }
+        # order by title
+        return OrderedDict(sorted(result.items(), key=lambda x: x[1]['title']))
+
+    def get_feature_endpoint(self, obj):
+        return reverse('terra_geocrud:feature-list', args=(obj.layer_id,))
+
+    class Meta:
+        model = models.CrudView
+        fields = (
+            'id', 'name', 'pictogram', 'order', 'map_style',
+            'form_schema', 'ui_schema', 'settings', 'layer',
+            'feature_endpoint', 'extent', 'templates', 'exports',
+            'feature_list_properties',
+        )
+
+
+class CrudGroupSerializer(serializers.ModelSerializer):
+    crud_views = CrudViewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.CrudGroupView
+        fields = '__all__'
+
+
+class FeatureDisplayPropertyGroup(serializers.ModelSerializer):
+    title = serializers.CharField(source='label')
+    order = serializers.IntegerField()
+    pictogram = serializers.ImageField()
+    properties = serializers.SerializerMethodField()
+
+    def get_properties(self, obj):
+        """ Get feature properties in group to form { title: rendering(value) } """
+        feature = self.context.get('feature')
+        return {
+            feature.layer.get_property_title(prop):
+                feature.layer.crud_view.render_property_data(feature, prop)
+            for prop in list(obj.properties)
+        }
+
+    class Meta:
+        model = models.FeaturePropertyDisplayGroup
+        fields = ('title', 'slug', 'order', 'pictogram', 'properties')
+
+
+class CrudFeatureListSerializer(FeatureSerializer):
+    geom = None
+    detail_url = serializers.SerializerMethodField()
+    extent = serializers.SerializerMethodField()
+
+    def get_extent(self, obj):
+        geom = obj.geom.transform(4326, clone=True)
+        return geom.extent
+
+    def get_detail_url(self, obj):
+        return reverse('terra_geocrud:feature-detail',
+                       args=(obj.layer_id, obj.identifier))
+
+    class Meta(FeatureSerializer.Meta):
+        exclude = ('source', 'target', 'layer', 'geom')
+        fields = None
+
+
+class DocumentFeatureSerializer(serializers.ModelSerializer):
+    extension = serializers.SerializerMethodField()
+    template_name = serializers.CharField(source='name')
+    template_file = serializers.SerializerMethodField()
+    download_url = serializers.SerializerMethodField()
+
+    def get_extension(self, obj):
+        return Path(obj.template_file.name).suffix
+
+    def get_template_file(self, obj):
+        return Path(obj.template_file.name).name
+
+    def get_download_url(self, obj):
+        return reverse('terra_geocrud:render-template',
+                       args=(obj.pk, self.context.get('feature').pk))
+
+    class Meta:
+        fields = (
+            'extension', 'template_name', 'template_file', 'download_url'
+        )
+        model = Template
+
+
+class CrudFeatureDetailSerializer(FeatureSerializer):
+    title = serializers.SerializerMethodField()
+    geom = geo_serializers.GeometryField()
+    documents = serializers.SerializerMethodField()
+    display_properties = serializers.SerializerMethodField()
+    properties = serializers.JSONField()
+
+    def get_title(self, obj):
+        """ Get Feature title, as feature_title_property content or identifier by default """
+        crud_view_defined_property = obj.layer.crud_view.feature_title_property
+        return obj.properties.get(crud_view_defined_property, '') if crud_view_defined_property else obj.identifier
+
+    def get_properties(self, obj):
+        """ Feature properties as form initial data format (name / value) """
+        results = {}
+        crud_view = obj.layer.crud_view
+        groups = crud_view.feature_display_groups.all()
+        original_properties = deepcopy(obj.properties)
+
+        # get ordered groups filled
+        for group in groups:
+            results[group.slug] = {}
+            for prop in group.properties:
+                results[group.slug][prop] = original_properties.pop(prop, None)
+
+        return {**results, **original_properties}
+
+    def get_display_properties(self, obj):
+        """ Feature properties to display (title / rendered value) """
+        processed_properties = []
+        results = {}
+        crud_view = obj.layer.crud_view
+        groups = crud_view.feature_display_groups.all()
+
+        # get ordered groups filled
+        for group in groups:
+            serializer = FeatureDisplayPropertyGroup(group,
+                                                     context={'request': self.context.get('request'),
+                                                              'feature': obj})
+            results[group.slug] = serializer.data
+            processed_properties += list(group.properties)
+
+        # add default other properties
+        remained_properties = list(set(crud_view.properties) - set(processed_properties))
+        if remained_properties:
+            results['__default__'] = {
+                "title": "",
+                "pictogram": None,
+                "order": 9999,
+                "properties": {
+                    obj.layer.get_property_title(prop): crud_view.render_property_data(obj, prop)
+                    for prop in list(remained_properties)
+                }
+            }
+        return results
+
+    def get_documents(self, obj):
+        serializer = DocumentFeatureSerializer(obj.layer.crud_view.templates.all(),
+                                               many=True,
+                                               context={'request': self.context.get('request'),
+                                                        'feature': obj})
+        return serializer.data
+
+    def validate_properties(self, data):
+        new_data = deepcopy(data)
+        # clean all dict in values
+        for key, value in new_data.items():
+            if isinstance(value, dict):
+                # explode it
+                parsed_data = new_data.pop(key)
+                for parsed_key, parsed_value in parsed_data.items():
+                    new_data[parsed_key] = parsed_value
+        super().validate_properties(new_data)
+        return new_data
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['properties'] = self.get_properties(instance)
+        return data
+
+    class Meta(FeatureSerializer.Meta):
+        exclude = ('source', 'target', 'layer',)
+        fields = None
