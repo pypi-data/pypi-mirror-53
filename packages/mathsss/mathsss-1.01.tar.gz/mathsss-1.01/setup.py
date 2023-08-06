59be64c16c5ef8dd6ffcef52a04eb71acf7a92fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:49 +0000

    Add package mathsss/1.01/mathsss-1.01.tar.gz

---
+++ packages/mathsss/mathsss-1.01.tar.gz/mathsss-1.01/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='mathsss',  # 对外我们模块的名字
+    version='1.01',  # 版本号
+    description='这是第一个对外发布的模块， 测试哦',  # 描述
+    author='JunJunJump',  # 作者
+    author_email='1806758205@qq.com',
+    py_modules=['mathsss.a111', 'mathsss.a222']  # 要发布的模块
+)
