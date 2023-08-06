fee321b68ee566b05cf07b9315656bf7194633fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:02 +0000

    Add package map-boxes/1.0.4a0/map_boxes-1.0.4a0.tar.gz

---
+++ packages/map-boxes/map_boxes-1.0.4a0.tar.gz/map_boxes-1.0.4a0/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='map_boxes',
+    version='1.0.4a',
+    author='Roman Sol (ZFTurbo)',
+    packages=['map_boxes', ],
+    url='https://github.com/ZFTurbo/Mean-Average-Precision-for-Boxes',
+    license='MIT License',
+    description='Function to calculate mAP for set of detected boxes and annotated boxes. ',
+    long_description='Function to calculate mean average precision (mAP) for set of boxes. Useful for object detection pipelines.'
+                     'More details: https://github.com/ZFTurbo/Mean-Average-Precision-for-Boxes',
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+    package_data={'': ['compute_overlap.pyx']},
+    include_package_data=True,
+)
