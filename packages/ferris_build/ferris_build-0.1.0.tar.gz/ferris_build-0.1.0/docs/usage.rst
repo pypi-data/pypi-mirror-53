af821f73a458585786e1fef9fc44e523cb9db9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package ferris_build/0.1.0/ferris_build-0.1.0.tar.gz

---
+++ packages/ferris_build/ferris_build-0.1.0.tar.gz/ferris_build-0.1.0/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use Ferris in a project::
+
+    import ferris
---
+++ packages/ferris_build/ferris_build-0.1.0.tar.gz/ferris_build-0.1.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = []
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Todd Young",
+    author_email='youngmt1@ornl.gov',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Building tool for Rust",
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='ferris',
+    name='ferris_build',
+    packages=find_packages(include=['ferris']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/yngtodd/ferris',
+    version='0.1.0',
+    zip_safe=False,
+)
