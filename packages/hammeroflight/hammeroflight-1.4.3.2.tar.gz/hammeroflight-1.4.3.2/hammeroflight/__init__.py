f7ef3604d39e3866a52b3ea10b0e7cd87078bbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:56 +0000

    Add package hammeroflight/1.4.3.2/hammeroflight-1.4.3.2.tar.gz

---
+++ packages/hammeroflight/hammeroflight-1.4.3.2.tar.gz/hammeroflight-1.4.3.2/hammeroflight/__init__.py
@@ -0,0 +1,7 @@
+from hammeroflight.arufunctions import cleanandencode, impute_encode
+from hammeroflight.arufunctions import qualityreport, featureselector
+from hammeroflight.modelcomparator import reg_comparator, clf_comparator
+from hammeroflight.modelfitter import fit_classify, fit_regress, fittingplot, goodness_fit
+from hammeroflight.modelfitter import kmeans_kfinder, knn_kfinder
+from hammeroflight.forecasting import arimaordertuner, predictionplot
+__version__ = "1.4.3.2"
