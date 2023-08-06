9dff98ba2cbe48f0298578f1036223b088f791f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:18 +0000

    Add package facedet/2.0/facedet-2.0-py3-none-any.whl

---
+++ packages/facedet/facedet-2.0-py3-none-any.whl/facedet/faceext.py
@@ -0,0 +1,65 @@
+import cv2
+import sys
+import os
+import numpy as np
+from numpy import unravel_index
+
+def read():
+ """Extracts the largest face among the total no.of faces detected"""
+# imagePath = sys.argv[1]		
+ image = cv2.imread("facedet/data/data2.jpg")		# For pip package
+# image = cv2.imread(imagePath)
+ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)	#Image grayscaling.
+ assert image is not None,"Image not found!"
+ 
+ condition = image.shape[0] < 1024 and image.shape[1] < 1024
+ if not(condition): 
+  raise Exception('High-resoluted images are not accepted. Enter images less than 1024*1024 resolution.')
+ return image
+
+
+def detection():
+ gray = read()
+ faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+ faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.3,
+    minNeighbors=3,
+    minSize=(30, 30)
+ )	#Haar-cascade: A Face detection algorithm
+ return faces
+
+
+def area(): 
+ image = read() 
+ faces = detection()
+ print('Imagesize:',image.shape[0:2])
+ area = faces[:,2] * faces[:,3]
+ faces = np.c_[faces,area]	#concatenates area values to last column of 'face' array.
+ print('All detected faces\n',faces)
+ i,j = unravel_index(faces.argmax(), faces.shape)	
+ print(i,j)	# gets the position of maximum value from 'face' array.
+ print("Found %d Face%s!" %(len(faces),"s"[len(faces)==1:]))
+ return i,j
+
+
+def extract():
+ image = read()
+ faces = detection() 
+ i,j = area()
+ """Most number of loops and conditional statements are avoided to reduce Time complexity"""
+ X = faces[i,0]
+ Y = faces[i,1]
+ W = faces[i,2]
+ H = faces[i,3]
+ 
+ cv2.rectangle(image, (X, Y), (X + W, Y + H), (0, 255, 0), 2)
+ roi_color = image[Y:Y + H, X:X + W]    
+ print("Face(largest) Extracted. Saved locally!")
+ cv2.imwrite('Extracted_face.jpg', roi_color)	#Image Extraction.
+ status = cv2.imwrite('Output.jpg', image)
+ print("Image 'Output.jpg' written to filesystem: ", status)
+ 
+
+if __name__ == "__main__": 
+    extract()	
