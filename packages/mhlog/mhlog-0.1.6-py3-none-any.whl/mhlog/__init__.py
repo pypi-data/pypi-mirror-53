9b3c2ab0d44aa15ae11652fc8443b98c2eb3bd8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:34 +0000

    Add package mhlog/0.1.6/mhlog-0.1.6-py3-none-any.whl

---
+++ packages/mhlog/mhlog-0.1.6-py3-none-any.whl/mhlog/__init__.py
@@ -0,0 +1,9 @@
+__all__ = ['Logger']
+
+__version__ = '0.1.6'
+__copyright__ = 'Copyright 2019 Magnus Hirth'
+__author__ = 'Magnus Hirth'
+__author_email__ = 'mhmorgan42@gmail.com'
+__description__ = 'Simple logging library.'
+
+from mhlog.logger import Logger
