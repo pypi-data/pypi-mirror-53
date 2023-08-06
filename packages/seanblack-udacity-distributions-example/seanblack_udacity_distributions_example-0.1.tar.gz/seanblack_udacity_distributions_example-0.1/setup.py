381e5344f590ff7bec56c0d12ac8ccc642644ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:09 +0000

    Add package seanblack-udacity-distributions-example/0.1/seanblack_udacity_distributions_example-0.1.tar.gz

---
+++ packages/seanblack-udacity-distributions-example/seanblack_udacity_distributions_example-0.1.tar.gz/seanblack_udacity_distributions_example-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='seanblack_udacity_distributions_example',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = "Sean Black",
+      author_email = "skblackx2@gmail.com",
+      packages=['distributions'],
+      zip_safe=False)
