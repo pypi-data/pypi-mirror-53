6834a8e85bafc8b7a759a09d2f9b67eb1c9863d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:28 +0000

    Add package django-language-fields/1.0.0/django_language_fields-1.0.0-py3-none-any.whl

---
+++ packages/django-language-fields/django_language_fields-1.0.0-py3-none-any.whl/languages/fields.py
@@ -0,0 +1,27 @@
+from django.db.models.fields import CharField
+
+class LanguageField(CharField):
+    """
+    A language field for Django models.
+    """
+    def __init__(self, *args, **kwargs):
+        # Local import so the languages aren't loaded unless they are needed.
+        from .languages import LANGUAGES
+
+        kwargs.setdefault('max_length', 3)
+        kwargs.setdefault('choices', LANGUAGES)
+        super(CharField, self).__init__(*args, **kwargs)
+
+
+class RegionField(CharField):
+    """
+    A region field for Django models.
+    """
+    def __init__(self, *args, **kwargs):
+        # Local import so the regions aren't loaded unless they are needed.
+        from .regions import REGIONS
+
+        kwargs.setdefault('max_length', 3)
+        kwargs.setdefault('choices', REGIONS)
+        super(CharField, self).__init__(*args, **kwargs)
+
