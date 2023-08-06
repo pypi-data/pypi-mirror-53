38fe1f98342f2960bc6a4ffede6e8c92215a93dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:05 +0000

    Add package pym2149/3/pym2149-3.tar.gz

---
+++ packages/pym2149/pym2149-3.tar.gz/pym2149-3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+        name = 'pym2149',
+        version = '3',
+        install_requires = ['mock', 'pyaudio', 'pyrbo', 'diapyr', 'aridity', 'outjack', 'mynblep', 'lagoon', 'splut', 'timelyOSC', 'pyven'],
+        packages = setuptools.find_packages(),
+        package_data = {'': ['*.pxd', '*.pyx', '*.pyxbld', '*.arid']},
+        py_modules = ['ym2jack', 'samples', 'ym2txt', 'midi2jack', 'bpmtool', 'dosound2jack', 'lc2wav', 'dsd2wav', 'lc2txt', 'lc2jack', 'dosound2wav', 'ym2wav', 'ym2portaudio', 'midi2wav'],
+        scripts = ['ym2jack.py', 'samples.py', 'ym2txt.py', 'midi2jack.py', 'bpmtool.py', 'dosound2jack.py', 'lc2wav.py', 'dsd2wav.py', 'lc2txt.py', 'lc2jack.py', 'dosound2wav.py', 'ym2wav.py', 'ym2portaudio.py', 'midi2wav.py'])
