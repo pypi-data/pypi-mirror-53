1251fd073954b363f97b5dc8d18ada2f4933fa47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:28 +0000

    Add package kands/1.0.0/kands-1.0.0-py3-none-any.whl

---
+++ packages/kands/kands-1.0.0-py3-none-any.whl/kands/main.py
@@ -0,0 +1,12 @@
+from .kands import Builder
+
+
+def main():
+    """Main function."""
+    builder = Builder()
+    builder.run()
+
+
+if __name__ == "__main__":
+    """Calls to the main function when its called as a script."""
+    main()
