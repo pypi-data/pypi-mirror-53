257b7f3b2f698eea7fa2bb645ad8afa57af3288b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:59 +0000

    Add package agung96tm-funniest/0.1.3/agung96tm_funniest-0.1.3.tar.gz

---
+++ packages/agung96tm-funniest/agung96tm_funniest-0.1.3.tar.gz/agung96tm_funniest-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='agung96tm_funniest',
+      version='0.1.3',
+      description='The agung96tm_funniest jokes',
+      url='http://localhost:8000',
+      author='king',
+      author_email='king@example.com',
+      license='MIT',
+      packages=['agung96tm_funniest'],
+      install_requires=[
+            'markdown',
+      ],
+      zip_safe=False)
