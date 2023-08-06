3800c07e7f5714c6b36a6bab2be099faaaf5c268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:28 +0000

    Add package jiagu/0.1.8/jiagu-0.1.8.tar.gz

---
+++ packages/jiagu/jiagu-0.1.8.tar.gz/jiagu-0.1.8/jiagu/cluster/base.py
@@ -0,0 +1,74 @@
+# -*-coding:utf-8-*-
+from collections import Counter
+import numpy as np
+
+
+def elu_distance(a, b):
+    """计算两点之间的欧氏距离并返回"""
+    dist = np.sqrt(np.sum(np.square(np.array(a) - np.array(b))))
+    return dist
+
+
+def count_features(corpus, tokenizer=list):
+    """词频特征
+
+    :param corpus: list of str
+    :param tokenizer: function for tokenize, default is `jiagu.cut`
+    :return:
+        features: np.array
+        names: list of str
+
+    example:
+    >>> from jiagu.cluster.base import count_features
+    >>> corpus = ["判断unicode是否是汉字，数字，英文，或者其他字符。", "全角符号转半角符号。"]
+    >>> X, names = count_features(corpus)
+    """
+    tokens = [tokenizer(x) for x in corpus]
+    vocab = [x[0] for x in Counter([x for s in tokens for x in s]).most_common()]
+
+    features = []
+    for sent in tokens:
+        counter = Counter(sent)
+        feature = [counter.get(x, 0) for x in vocab]
+        features.append(feature)
+
+    return np.array(features), vocab
+
+
+def tfidf_features(corpus, tokenizer=list):
+    """文本的 tfidf 特征
+
+    :param corpus: list of str
+    :param tokenizer: function for tokenize, default is `jiagu.cut`
+    :return:
+        features: np.array
+        names: list of str
+
+    example:
+    >>> import jiagu
+    >>> from jiagu.cluster.base import tfidf_features
+    >>> corpus = ["判断unicode是否是汉字。", "全角符号转半角符号。", "一些基于自然语言处理的预处理过程也会在本文中出现。"]
+    >>> X, names = tfidf_features(corpus, tokenizer=jiagu.cut)
+    """
+    tokens = [tokenizer(x) for x in corpus]
+    vocab = [x[0] for x in Counter([x for s in tokens for x in s]).most_common()]
+
+    idf_dict = dict()
+    total_doc = len(corpus)
+    for word in vocab:
+        num = sum([1 if (word in s) else 0 for s in corpus])
+        if num == total_doc:
+            idf = np.log(total_doc / num)
+        else:
+            idf = np.log(total_doc / (num + 1))
+        idf_dict[word] = idf
+
+    features = []
+    for sent in tokens:
+        counter = Counter(sent)
+        feature = [counter.get(x, 0) / len(sent) * idf_dict.get(x, 0) for x in vocab]
+        features.append(feature)
+
+    return np.array(features), vocab
+
+
---
+++ packages/jiagu/jiagu-0.1.8.tar.gz/jiagu-0.1.8/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from setuptools import setup
+
+setup(name='jiagu',
+      version='0.1.8',
+      description='Jiagu Natural Language Processing',
+      author='Yener(Zheng Wenyu)',
+      author_email='help@ownthink.com',
+      url='https://github.com/ownthink/Jiagu',
+      license='MIT',
+      install_requires=['tensorflow>=1.6.0', 'numpy>=1.12.1'],
+      packages=['jiagu'],
+      package_dir={'jiagu': 'jiagu'},
+      package_data={'jiagu': ['*.*', 'cluster/*', 'data/*', 'model/*',
+							'normal/*', 'segment/*', 'segment/dict/*',
+							'sentiment/*', 'sentiment/model/*', 'topic/*']}
+      )
