7a489feba3d9a9b97ffd783c79158b425f5aaf79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:03 +0000

    Add package pysamiterators/1.6/pysamiterators-1.6.tar.gz

---
+++ packages/pysamiterators/pysamiterators-1.6.tar.gz/pysamiterators-1.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(
+	name='pysamiterators',
+	version='1.06',
+	author='Buys de Barbanson',
+	author_email='code@buysdb.nl',
+	description='Pysam related iterators',
+	url='https://github.com/BuysDB/pysamiterators',
+	download_url='https://github.com/BuysDB/pysamiterators/archive/v1.06.tar.gz',
+	packages=['pysamiterators','pysamiterators.iterators'
+        ],
+	install_requires=['pysam'],
+	 keywords = ['mate-pair','pysam','iteration'],
+	license='MIT',
+	classifiers=[
+	 'Development Status :: 4 - Beta',
+	  'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+	  'License :: OSI Approved :: MIT License'
+	]
+)
