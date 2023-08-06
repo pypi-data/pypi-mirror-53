531226537b6b7609856447de4871c6af5de0b841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:10 +0000

    Add package sauing/0.2/sauing-0.2.tar.gz

---
+++ packages/sauing/sauing-0.2.tar.gz/sauing-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sauing',
+      version='0.2',
+      description='This is Test Automation Framework for Selenium.',
+      url='https://github.com/sauing/IIN_Trollers',
+      author='Saurabh Singh',
+      author_email='ssaurabhsingh009@gmail.com',
+      package=['sauing'],
+      zip_safe=False)
