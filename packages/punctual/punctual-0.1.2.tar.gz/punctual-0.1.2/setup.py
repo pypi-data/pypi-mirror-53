ee6010123d45910613a961f4e4ee65795b696a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:02 +0000

    Add package punctual/0.1.2/punctual-0.1.2.tar.gz

---
+++ packages/punctual/punctual-0.1.2.tar.gz/punctual-0.1.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['punctual']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0', 'colorama>=0.4.1,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['pcl = punctual:cli.cli']}
+
+setup_kwargs = {
+    'name': 'punctual',
+    'version': '0.1.2',
+    'description': 'Clean and simple docfile management with a lot of flexibility',
+    'long_description': 'punctual\n========\n\n``punctual`` is an uncomplicated dotfile manager that works on common Linux fundamentals and simplistic JSON configuration files.\n\n\nInstallation\n------------\n\n\nWith pip::\n\n  pip install punctual\n\nWith this you will have an executable named `pcl`.\n\n\nUsage\n-----\n\nMain::\n\n  $ pcl --help\n  Usage: pcl [OPTIONS] COMMAND [ARGS]...\n\n  Options:\n    --help  Show this message and exit.\n\n  Commands:\n    delete\n    install\n    list\n\nInstall::\n\n  $ pcl install --help\n  Usage: pcl install [OPTIONS] PACKAGE_NAME\n\n  Options:\n    --force  Remove any existing files\n    --help   Show this message and exit.\n\n\nDocumentation\n-------------\n\nComing soon!\n',
+    'author': 'Mark Rawls',
+    'author_email': 'markrawls96@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
