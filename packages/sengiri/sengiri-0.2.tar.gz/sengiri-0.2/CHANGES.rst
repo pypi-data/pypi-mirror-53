db5eb8f6236823f8f5a0a597f356844279ced308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:29 +0000

    Add package sengiri/0.2/sengiri-0.2.tar.gz

---
+++ packages/sengiri/sengiri-0.2.tar.gz/sengiri-0.2/CHANGES.rst
@@ -0,0 +1,13 @@
+CHANGES
+=======
+
+0.2 (2019-10-11)
+------------------
+
+- Works well with also a text including emoticon and www (Laughing expression)
+- no longer use emoji package
+
+0.1.1 (2019-10-05)
+------------------
+
+- First release
---
+++ packages/sengiri/sengiri-0.2.tar.gz/sengiri-0.2/sengiri/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from .sengiri import tokenize
+"""sengiri
+
+This module provides the Japanese sentence-level tokenizer.
+
+Author:
+    Yukino Ikegami
+
+Lisence:
+    MIT License
+"""
+
+VERSION = (0, 2)
+__version__ = '0.2'
+__all__ = ['tokenize']
