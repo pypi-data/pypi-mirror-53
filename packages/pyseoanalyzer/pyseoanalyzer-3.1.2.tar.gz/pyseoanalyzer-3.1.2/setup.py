5640f073edbdb56525f1b2ac278289b7e2e2af40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:57 +0000

    Add package pyseoanalyzer/3.1.2/pyseoanalyzer-3.1.2.tar.gz

---
+++ packages/pyseoanalyzer/pyseoanalyzer-3.1.2.tar.gz/pyseoanalyzer-3.1.2/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+# from distutils.command.install import install as _install
+
+setup(
+    name='pyseoanalyzer',
+    version='3.1.2',
+    description='An SEO tool that gives you general Search Engine Optimization directions.',
+    author='Seth Black',
+    author_email='sblack@sethserver.com',
+    url='https://github.com/sethblack/python-seo-analyzer',
+    packages=find_packages(),
+    keywords=['search engine optimization', 'seo', 'website parser', 'crawler', 'scraper',],
+    package_data={'seoanalyzer': ['templates/index.html',]},
+    include_package_data=True,
+    install_requires=[
+        'BeautifulSoup4', 'requests', 'jinja2', 'urllib3',
+    ],
+    entry_points={
+        'console_scripts' : [
+            'seoanalyze = seoanalyzer.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    zip_safe=False,
+    long_description="""\
+SEOAnalyzer
+-----------
+
+An SEO tool that analyzes the structure of a site, crawls the site, count words in the body of the site and warns of any general SEO related issues.
+
+This version requires Python 3.4 or later. C'mon everyone, get with the times, Python 3 is great!
+""",
+)
