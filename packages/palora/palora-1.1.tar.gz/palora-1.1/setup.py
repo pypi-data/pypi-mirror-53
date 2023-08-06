842c4c3dccd49defd485a681f86663c1a6b1791c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:54 +0000

    Add package palora/1.1/palora-1.1.tar.gz

---
+++ packages/palora/palora-1.1.tar.gz/palora-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README.txt') as f:
+    long_description = f.read()
+
+setup(name='palora',
+      version='1.1',
+      description='Wrapper Class around cx_Oracle for Oracle Autonomous Database',
+      long_description=long_description,
+      url='https://github.com/ipal0/palora',
+      author='Pal',
+      author_email='ipal00@outlook.com',
+      license='GPL',
+      install_requires=[ 'cx_Oracle', ],
+      python_requires='>=3',
+      packages=['palora'])
