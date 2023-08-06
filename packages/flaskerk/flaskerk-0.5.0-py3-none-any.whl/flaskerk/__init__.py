228f8ece61839cb2beef5d2e46532cc3c85a611f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:58 +0000

    Add package flaskerk/0.5.0/flaskerk-0.5.0-py3-none-any.whl

---
+++ packages/flaskerk/flaskerk-0.5.0-py3-none-any.whl/flaskerk/__init__.py
@@ -0,0 +1,9 @@
+from flaskerk.base import Flaskerk
+from flaskerk.exception import HTTPException
+
+__version__ = '0.5.0'
+
+__all__ = [
+    Flaskerk,
+    HTTPException,
+]
---
+++ packages/flaskerk/flaskerk-0.5.0-py3-none-any.whl/flaskerk/config.py
@@ -0,0 +1,27 @@
+class Config:
+    def __init__(self):
+        """
+        Flaskerk configuration
+        """
+        self.name = 'docs'
+        #: url path for docs
+        self.endpoint = '/docs/'
+        self.url_prefix = None
+        self.template_folder = 'templates'
+        #: openapi spec file name
+        self.filename = 'openapi.json'
+
+        #: openapi spec version
+        self.openapi_veresion = '3.0.2'
+        #: document title
+        self.title = 'Service Documents'
+        #: service version
+        self.version = '0.1'
+        #: 'redoc' or 'swagger'
+        self.ui = 'redoc'
+
+        #: private
+        self._support_ui = {'redoc', 'swagger'}
+
+
+default_config = Config()
