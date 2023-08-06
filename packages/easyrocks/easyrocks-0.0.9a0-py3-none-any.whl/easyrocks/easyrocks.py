5378714a930accb202e8ce984518860f80b6f4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:21 +0000

    Add package easyrocks/0.0.9a0/easyrocks-0.0.9a0-py3-none-any.whl

---
+++ packages/easyrocks/easyrocks-0.0.9a0-py3-none-any.whl/easyrocks/easyrocks.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rocksdb
+from . import utils
+from rocksdb import DB as RocksDB, Options, WriteBatch
+
+ALLOWED_KEY_TYPES = (int, str)
+
+
+class DB:
+    def __init__(self, path='./rocksdb', opts=None, read_only=False):
+
+        rocks_opts = Options()
+        rocks_opts.create_if_missing = True
+
+        if opts:
+            if not isinstance(opts, dict):
+                raise TypeError
+
+            for key, value in opts.items():
+                setattr(rocks_opts, key, value)
+
+        self._db = RocksDB(f'{path}', rocks_opts, read_only=read_only)
+
+    def put(self, key, value, write_batch=None):
+        if not isinstance(key, ALLOWED_KEY_TYPES):
+            raise TypeError
+
+        if value is None:
+            raise ValueError
+
+        key_bytes = utils._get_key_bytes(key)
+        value_bytes = utils.to_bytes(value)
+
+        if write_batch is not None:
+            write_batch.put(key_bytes, value_bytes)
+        else:
+            self._db.put(key_bytes, value_bytes, sync=True)
+
+    def get(self, key):
+        key_bytes = utils._get_key_bytes(key)
+        value_bytes = self._db.get(key_bytes)
+
+        if value_bytes is not None:
+            return utils.to_object(value_bytes)
+
+    def exists(self, key):
+        if self.get(key) is not None:
+            return True
+        return False
+
+    def delete(self, key):
+        key_bytes = utils.str_to_bytes(key)
+        self._db.delete(key_bytes, sync=True)
+
+    def commit(self, write_batch):
+        if write_batch is not None:
+            self._db.write(write_batch, sync=True)
+
+    def scan(self, prefix=None, start_key=None, end_key=None, reversed_scan=False):
+        iterator = self._db.iterkeys()
+
+        if prefix is None and start_key is None:
+            if reversed_scan:
+                iterator.seek_to_last()
+            else:
+                iterator.seek_to_first()
+        else:
+            if prefix is not None:
+                prefix_bytes = utils.str_to_bytes(prefix)
+            else:
+                prefix_bytes = utils.str_to_bytes(start_key)
+            iterator.seek(prefix_bytes)
+
+        if reversed_scan:
+            iterator = reversed(iterator)
+
+        for key_bytes in iterator:
+            key = utils.bytes_to_str(key_bytes)
+
+            if prefix is not None and key[:len(prefix)] != prefix:
+                return
+
+            if end_key is not None and key > end_key:
+                return
+
+            value_bytes = self._db.get(key_bytes)
+            value = utils.to_object(value_bytes)
+            yield key, value
+
+    def close(self):
+        self._db.close()
