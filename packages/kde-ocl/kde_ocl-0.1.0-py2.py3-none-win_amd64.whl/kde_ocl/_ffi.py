869289cabe3d60987b5d50b71524e8e3634e88bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:25 +0000

    Add package kde-ocl/0.1.0/kde_ocl-0.1.0-py2.py3-none-win_amd64.whl

---
+++ packages/kde-ocl/kde_ocl-0.1.0-py2.py3-none-win_amd64.whl/kde_ocl/_ffi.py
@@ -0,0 +1,8 @@
+# auto-generated file
+__all__ = ['lib', 'ffi']
+
+import os
+from kde_ocl._ffi__ffi import ffi
+
+lib = ffi.dlopen(os.path.join(os.path.dirname(__file__), '_ffi__lib.cp37-win_amd64.pyd'), 0)
+del os
