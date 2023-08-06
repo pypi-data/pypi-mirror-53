455db9046f6afbf5b18bd0231f216e7901b1edea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:48 +0000

    Add package mvp/0.5.0/mvp-0.5.0-py3-none-any.whl

---
+++ packages/mvp/mvp-0.5.0-py3-none-any.whl/mvp/utils.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from Qt import QtWidgets
+import time
+
+
+def get_maya_window(cache=[]):
+    '''Get Maya MainWindow as a QWidget.'''
+
+    if cache:
+        return cache[0]
+
+    for widget in QtWidgets.QApplication.instance().topLevelWidgets():
+        if widget.objectName() == 'MayaWindow':
+            cache.append(widget)
+            return widget
+
+    raise RuntimeError('Could not locate MayaWindow...')
+
+
+def wait(delay=1):
+    '''Delay python execution for a specified amount of time'''
+
+    app = QtWidgets.QApplication.instance()
+
+    s = time.clock()
+    while True:
+        if time.clock() - s >= delay:
+            return
+
+        app.processEvents()
