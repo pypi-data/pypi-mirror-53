3ba46dfcca95bada99098ea0e9770583936b1b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:37 +0000

    Add package pystitchconnect/0.0.5/pystitchconnect-0.0.5.tar.gz

---
+++ packages/pystitchconnect/pystitchconnect-0.0.5.tar.gz/pystitchconnect-0.0.5/README.md
@@ -0,0 +1,31 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Build Status](https://travis-ci.com/dwallace0723/pystitch.svg?branch=master)](https://travis-ci.com/dwallace0723/pystitch)
+[![Python Versions](https://img.shields.io/badge/python-3.6%20%7C%203.7-blue.svg)](https://pypi.python.org/pypi/ansicolortags/)
+
+# pystitch
+a Python SDK for the Stitch Connect API
+
+## Installation
+
+```bash
+pip install pystitchconnect
+```
+
+## Example Usage
+
+```python
+import os
+from pystitch import PyStitch
+
+STITCH_API_TOKEN = os.environ["STITCH_API_TOKEN"]
+
+# Instantiate a PyStitch client using your API token.
+client = PyStitch(token=STITCH_API_TOKEN)
+
+# List all available Source objects.
+sources = client.list_sources()
+
+# Trigger a replication job for a specific Source.
+response = client.start_replication_job(source_id=12345)
+assert(response.ok)
+```
---
+++ packages/pystitchconnect/pystitchconnect-0.0.5.tar.gz/pystitchconnect-0.0.5/setup.py
@@ -0,0 +1,40 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+__version__ = None
+exec(open("pystitch/version.py").read())
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+long_description = ""
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setup(
+    name="pystitchconnect",
+    version=__version__,
+    description="Python SDK for Stitch Connect API.",
+    keywords="stitch",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/dwallace0723/pystitch",
+    author="David Wallace",
+    author_email="dwallace0723@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.22.0',
+        'pytz==2019.2'
+    ],
+    python_requires=">=3.6",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
