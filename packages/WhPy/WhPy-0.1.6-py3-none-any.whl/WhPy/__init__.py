56963c16f15b8d193e3e5f8da1653a85a71c390e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:09 +0000

    Add package WhPy/0.1.6/WhPy-0.1.6-py3-none-any.whl

---
+++ packages/WhPy/WhPy-0.1.6-py3-none-any.whl/WhPy/__init__.py
@@ -0,0 +1,9 @@
+__doc__ = """WhPy - Webhook Python
+
+Webhook Python is a Python 3 module for interacting with various chat
+    applications via established webhooks.
+
+Modules:
+    discord
+    slack *PLANNED*
+"""
