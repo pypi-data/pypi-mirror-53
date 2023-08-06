5f195ad6db09509b1cfa050d79e6b46db0be058f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:41 +0000

    Add package spikely/0.6.1/spikely-0.6.1.tar.gz

---
+++ packages/spikely/spikely-0.6.1.tar.gz/spikely-0.6.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = {}
+with open('spikely/version.py') as fp:
+    exec(fp.read(), version)
+
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
+
+setup(
+    name='spikely',
+    version=version['__version__'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'PyQt5',
+        'spikeextractors',
+        'spiketoolkit',
+        'spikesorters'
+    ],
+    author='Roger Hurwitz',
+    author_email='rogerhurwitz@gmail.com',
+    description='A GUI wrapper for SpikeInterface',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/SpikeInterface/spikely',
+    entry_points={
+        'console_scripts': [
+            'spikely=spikely.spikely_main:launch_spikely'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
