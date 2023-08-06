baf42280e61722dd55b16a6a74ee9ea2cf6ea0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:47 +0000

    Add package MsCoppel/1.3.1/MsCoppel-1.3.1.tar.gz

---
+++ packages/MsCoppel/MsCoppel-1.3.1.tar.gz/MsCoppel-1.3.1/MsCoppel/loggs.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from .loggsWrapper import LoggerWrapper
+
+def Loggs(name):
+    """
+        Metodo para generar un elemento de logger
+        @params name Nombre del modulo/libreria
+    """
+    # Validar si proviene de un ambiente productivo.
+    if not os.environ.get('LOGS_FLUENT', None) is None:
+        return LoggerWrapper(name)
+    elif os.environ.get('PRODUCTION', None) is None or len(os.environ.get('LOGS_FLUENT_DISABLED', '')) > 0:
+        from logbook import Logger, StreamHandler
+        StreamHandler(sys.stdout).push_application()
+        return Logger(name)
+    else:
+        return LoggerWrapper(name)
