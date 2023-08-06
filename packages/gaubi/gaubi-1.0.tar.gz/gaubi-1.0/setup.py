7b563bdad5494a312574e9c72e5440849ddd2edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:33 +0000

    Add package gaubi/1.0/gaubi-1.0.tar.gz

---
+++ packages/gaubi/gaubi-1.0.tar.gz/gaubi-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaubi',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gaubi'],
+      author='Gabriel Estivalet',
+      author_email='gestivalet@gmail.com',
+      zip_safe=False)
