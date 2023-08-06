a3e2b383b4dbee7dcfb52220ae8d0b4acd5327b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:14 +0000

    Add package jupyterhub-share-link/0.1.0/jupyterhub-share-link-0.1.0.tar.gz

---
+++ packages/jupyterhub-share-link/jupyterhub-share-link-0.1.0.tar.gz/jupyterhub-share-link-0.1.0/jupyterhub_share_link/launcher.py
@@ -0,0 +1,150 @@
+"""
+Launch a server with a temporary user via JupyterHub
+
+This file has been vendored from the Python package binderhub. It has been
+vendored rather than imported because binderhub has many other dependencies not
+needed by this hub service.
+"""
+import base64
+import json
+import uuid
+import os
+
+from tornado.log import app_log
+from tornado import web, gen
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest, HTTPError
+
+
+class Launcher():
+
+    retry_delay = 20
+    retries = 100
+    hub_url = "127.0.0.1:8000/"
+
+    def __init__(self, user, auth):
+        self.hub_api_token = auth
+        self.user = user
+
+    def log(self, *args, **kwargs):
+        app_log(*args, **kwargs)
+
+    async def api_request(self, url, *args, **kwargs):
+        """Make an API request to JupyterHub"""
+        headers = kwargs.setdefault('headers', {})
+        headers.update({'Authorization': 'token %s' % self.hub_api_token})
+        hub_api_url = os.getenv('JUPYTERHUB_API_URL', '') or self.hub_url + 'hub/api'
+        if not hub_api_url.endswith('/'):
+            hub_api_url = hub_api_url + '/'
+        request_url = hub_api_url + url
+        req = HTTPRequest(request_url, *args, **kwargs)
+        retry_delay = self.retry_delay
+        for i in range(1, self.retries + 1):
+            try:
+                return await AsyncHTTPClient().fetch(req)
+            except HTTPError as e:
+                # swallow 409 errors on retry only (not first attempt)
+                if i > 1 and e.code == 409 and e.response:
+                    self.log("Treating 409 conflict on retry as success")
+                    return e.response
+                # retry requests that fail with error codes greater than 500
+                # because they are likely intermittent issues in the cluster
+                # e.g. 502,504 due to ingress issues or Hub relocating,
+                # 599 due to connection issues such as Hub restarting
+                if e.code >= 500:
+                    self.log("Error accessing Hub API (using %s): %s", request_url, e)
+                    if i == self.retries:
+                        # last api request failed, raise the exception
+                        raise
+                    await gen.sleep(retry_delay)
+                    # exponential backoff for consecutive failures
+                    retry_delay *= 2
+                else:
+                    raise
+
+    async def get_user_data(self):
+        resp = await self.api_request(
+            'users/%s' % self.user['name'],
+            method='GET',
+        )
+        body = json.loads(resp.body.decode('utf-8'))
+        return body
+
+    async def launch(self, user_options, server_name):
+        """Launch a server for given user_options
+        - creates a temporary user on the Hub if authentication is not enabled
+        - spawns a server for temporary/authenticated user
+        - generates a token
+        - returns a dict containing:
+          - `url`: the URL of the server
+          - `user_options`: dict of user_options for spawner
+          - `token`: the token for the server
+        """
+
+        username = self.user['name']
+
+        # data to be passed into spawner's user_options during launch
+        # and also to be returned into 'ready' state
+        data = {'token': base64.urlsafe_b64encode(
+                    uuid.uuid4().bytes).decode('ascii').rstrip('=\n')}
+        data.update(user_options)
+
+        # # test if exists and early exit if so
+        user_data = await self.get_user_data()
+        started_server = user_data['servers'].get(server_name, None)
+        if started_server and started_server['ready']:
+            redirect_url = started_server['url']
+            return {'status': 'running', 'url': redirect_url}
+
+        # start server
+        app_log.info("Starting server %s for user %s with options %s",
+                     server_name, username, user_options)
+        try:
+            resp = await self.api_request(
+                'users/{}/servers/{}'.format(username, server_name),
+                method='POST',
+                body=json.dumps(data).encode('utf8'),
+            )
+
+            if resp.code == 202:
+                # Server hasn't actually started yet
+                # We wait for it!
+                # NOTE: This ends up being about ten minutes
+                for i in range(64):
+                    user_data = await self.get_user_data()
+
+                    server = user_data['servers'][server_name]
+                    if server['ready']:
+                        # exit, server running
+                        return {'status': 'running',
+                                'url': server['url']}
+
+                    if server['progress_url']:
+                        # exit, server pending with progress url
+                        return {'url': server['progress_url'],
+                                'status': 'pending'}
+
+                    if not server['pending']:
+                        raise web.HTTPError(
+                            500, ("Server with options %s for user %s failed to launch"
+                                  % (user_options, username)))
+                    # FIXME: make this configurable
+                    # FIXME: Measure how long it takes for servers to start
+                    # and tune this appropriately
+                    await gen.sleep(min(1.4 ** i, 10))
+                else:
+                    raise web.HTTPError(
+                        500, ("Server with options %s for user %s took too long to launch"
+                              % (user_options, username)))
+
+        except HTTPError as e:
+            if e.response:
+                body = e.response.body
+            else:
+                body = ''
+
+            app_log.error("Error starting server {} for user {}: {}\n{}".
+                          format(server_name, username, e, body))
+            raise web.HTTPError(500, "Failed to launch with options %s" %
+                    user_options)
+
+        return {'url': '/user/%s/%s' % (username, server_name), 'status': 'running'}
---
+++ packages/jupyterhub-share-link/jupyterhub-share-link-0.1.0.tar.gz/jupyterhub-share-link-0.1.0/jupyterhub_share_link/run.py
@@ -0,0 +1,217 @@
+import base64
+from datetime import datetime, timedelta
+import json
+import os
+import pathlib
+import sys
+import uuid
+
+import jwt
+import tornado.options
+from jupyterhub.services.auth import HubAuthenticated
+from jupyterhub.utils import url_path_join
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest, HTTPError
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.web import Application
+from tornado.web import authenticated
+from tornado.web import RequestHandler
+from urllib.parse import urlparse, quote as urlquote
+
+from .launcher import Launcher
+
+
+HubAuthenticated.hub_auth
+
+private_key_path = os.getenv('JUPYTERHUB_SHARE_LINK_PRIVATE_KEY', "private.pem")
+private_key = pathlib.Path(private_key_path).read_text()
+public_key_path = os.getenv('JUPYTERHUB_SHARE_LINK_PUBLIC_KEY', "public.pem")
+public_key = pathlib.Path(public_key_path).read_text()
+
+
+class CreateSharedLink(HubAuthenticated, RequestHandler):
+    @authenticated
+    async def post(self):
+        data = json.loads(self.request.body.decode('utf-8'))
+
+        now = datetime.utcnow()
+        if data.get('expiration_time') is not None:
+            expiration_time = datetime.fromtimestamp(data['expiration_time'])
+        else:
+            # Default to one hour lifetime.
+            expiration_time = now + timedelta(hours=1)
+        # Enforce a max of two days.
+        # This is not for long-term sharing, galleries, etc.
+        max_time = now + timedelta(days=2)
+        if expiration_time > max_time:
+            raise HTTPError(
+                403, (f"expiration_time must no more than two days "
+                      f"from now (current max: {max_time.timestamp()})")
+            )
+        current_user = self.get_current_user()
+
+        # In JupyterLab 2.0, the front-end will be able to tell us the name of
+        # the server that this request came from, Until then, it can only give
+        # us the server's base URL. To map that to a server name, we have to
+        # loop through the dict of servers and find the matching URL. Once we
+        # have the name, we can look it up directly.
+        launcher = Launcher(current_user, self.hub_auth.api_token)
+        resp = await launcher.api_request(
+            url_path_join('users', current_user['name']),
+            method='GET',
+        )
+        source_user_data = json.loads(resp.body.decode('utf-8'))
+        for server in (source_user_data['servers'] or {}).values():
+            if server['url'] == data['base_url']:
+                source_server = server
+                break
+        else:
+            raise RuntimeError(
+                "The server that issued this request can't be found."
+                "This is likely a bug in jupyter-share-link or "
+                "jupyter-share-link-labextension.")
+
+        payload = {
+            'user': current_user['name'],
+            'path': data['path'],
+            'opts': source_server['user_options'],
+            'exp': expiration_time
+        }
+        token = jwt.encode(payload, private_key, algorithm="RS256")
+        base64_token = base64.urlsafe_b64encode(token)
+        base_url = f'{self.request.protocol}://{self.request.host}'
+        link = url_path_join(base_url,
+                             os.getenv('JUPYTERHUB_SERVICE_PREFIX'),
+                             f'open?token={base64_token.decode()}')
+        self.write({'link': link})
+
+
+class OpenSharedLink(HubAuthenticated, RequestHandler):
+    @authenticated
+    async def get(self):
+        unverified_base64_token = self.get_argument('token')
+        unverified_token = base64.urlsafe_b64decode(unverified_base64_token)
+        try:
+            token = jwt.decode(unverified_token, public_key, algorithms='RS256')
+        except jwt.exceptions.ExpiredSignatureError:
+            raise HTTPError(
+                403, "Sharing link has expired. Ask for a fresh link."
+            )
+        except jwt.exceptions.InvalidSignatureError:
+            raise HTTPError(
+                403, ("Sharing link has an invalid signature. Was it "
+                      "copy/pasted in full?")
+            )
+
+        source_username = token['user']
+        user_options = token['opts']
+        source_path = token['path']
+        dest_path = self.get_argument('dest_path',
+                                      os.path.basename(source_path))
+
+        current_user = self.get_current_user()
+        launcher = Launcher(current_user, self.hub_auth.api_token)
+
+        # HACK
+        # The Jupyter Hub API only gives us a *relative* path to the user
+        # servers. Use self.request to get at the public proxy URL.
+        base_url = f'{self.request.protocol}://{self.request.host}'
+        headers = {'Authorization': f'token {launcher.hub_api_token}'}
+
+        resp = await launcher.api_request(
+            url_path_join('users', source_username),
+            method='GET',
+        )
+        source_user_data = json.loads(resp.body.decode('utf-8'))
+
+        # Find a source server with matching user_options, or start one.
+        resp = await launcher.api_request(
+            url_path_join('users', source_username),
+            method='GET',
+        )
+        source_user_data = json.loads(resp.body.decode('utf-8'))
+        for server in (source_user_data['servers'] or {}).values():
+            if server['user_options'] == user_options:
+                source_server_url = server['url']
+                break
+        else:
+            raise NotImmplementedError("Sender must have a server running.")
+
+        # Ensure destination has a server to share into.
+        # First check to see if any of the currently-running servers have the
+        # same spawner user_options as the ones we need.
+        resp = await launcher.api_request(
+            url_path_join('users', current_user['name']),
+            method='GET',
+        )
+        dest_user_data = json.loads(resp.body.decode('utf-8'))
+        for server in (dest_user_data['servers'] or {}).values():
+            if server['user_options'] == user_options:
+                # Pull the name out specifically because we must obtain it
+                # different in the code path below.
+                target_server_name = server['name']
+                target_server = server
+                break
+        else:
+            # No currently-running server was spawned with the same
+            # user_options as the one being shared from.
+            # Start a new server with a random name.
+            target_server_name = f'shared-link-{str(uuid.uuid4())[:8]}'
+            target_server = await launcher.launch(
+                user_options, target_server_name)
+
+            if target_server['status'] == 'pending':
+                redirect_url = (f"{target_server['url']}"
+                                f"?next={urlquote(self.request.full_url())}")
+                # Redirect to progress bar, and then back here to try again.
+                self.redirect(redirect_url)
+            assert target_server['status'] == 'running'
+
+        # Fetch the content we want to copy.
+        content_url = url_path_join(base_url,
+                                    source_server_url,
+                                    'api/contents',
+                                    source_path)
+        headers = {'Authorization': f'token {launcher.hub_api_token}'}
+        req = HTTPRequest(content_url, headers=headers)
+        resp = await AsyncHTTPClient().fetch(req)
+        content = resp.body
+
+        # Copy content into destination server.
+        to_username = launcher.user['name']
+        dest_url = url_path_join(base_url,
+                                 'user',
+                                 to_username,
+                                 target_server_name,
+                                 'api/contents/',
+                                 dest_path)
+        req = HTTPRequest(dest_url, "PUT", headers=headers, body=content)
+        resp = await AsyncHTTPClient().fetch(req)
+
+        redirect_url = url_path_join(target_server['url'], 'lab', 'tree', dest_path)
+
+        # necessary?
+        redirect_url = (redirect_url if redirect_url.startswith('/')
+                        else '/' + redirect_url)
+
+        self.redirect(redirect_url)
+
+
+def main():
+    app = Application(
+        [
+            (os.environ['JUPYTERHUB_SERVICE_PREFIX'] + r'create/?', CreateSharedLink),
+            (os.environ['JUPYTERHUB_SERVICE_PREFIX'] + r'open/?', OpenSharedLink)
+        ]
+    )
+
+    http_server = HTTPServer(app)
+    url = urlparse(os.environ['JUPYTERHUB_SERVICE_URL'])
+
+    http_server.listen(url.port, url.hostname)
+    tornado.options.parse_command_line(sys.argv)
+
+    IOLoop.current().start()
+
+
+main()
