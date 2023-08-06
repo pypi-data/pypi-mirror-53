e5e763d2c7e2ce7e0ee9d2b7da9f0e7a732c1c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:29 +0000

    Add package foldingathome/0.1.5/foldingathome-0.1.5.tar.gz

---
+++ packages/foldingathome/foldingathome-0.1.5.tar.gz/foldingathome-0.1.5/foldingathome/Donor.py
@@ -0,0 +1,40 @@
+import requests
+
+
+class Donor:
+    def __init__(self, donorname, team=0):
+        r = requests.get(f"https://stats.foldingathome.org/api/team/{team}")
+        self.team = r.json()
+
+        donors = self.team["donors"]
+        found = False
+        for donor in donors:
+            if donor["name"] == str(donorname):
+                self.donor = donor
+                found = True
+        if not found and team == 0:
+            raise Exception(
+                f"\n\nNo user could be fou  nd with that name: {donorname}.\n This could be due to the user not being on the leaderboard for the default team, as the api only displays the top 1000 members of a team."
+            )
+            return
+        elif not found:
+            raise Exception(f"\n\nNo user could be found with that name: {donorname}")
+            return
+
+        self.name = self.donor["name"]
+        self.id = self.donor["id"]
+        self.score = self.donor["score"]
+        self.work_units = self.donor["work_units"]
+        self.team_id = self.team["team"]
+
+    @property
+    def rank(self):
+        donors = self.team["donors"]
+        scores = []
+        order = []
+        for donor in donors:
+            scores.append(donor["credit"])
+        for i in scores:
+            order.append(max(scores))
+            scores.pop(scores.index(max(scores)))
+        return order.index(self.donor["credit"]) + 1
