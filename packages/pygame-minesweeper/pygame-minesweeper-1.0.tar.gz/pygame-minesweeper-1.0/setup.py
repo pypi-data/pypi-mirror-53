d2f29a779aa2a2014371234ad4798a1dcea7c4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:25 +0000

    Add package pygame-minesweeper/1.0/pygame-minesweeper-1.0.tar.gz

---
+++ packages/pygame-minesweeper/pygame-minesweeper-1.0.tar.gz/pygame-minesweeper-1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from os import environ
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    with open(join(dirname(__file__), fname)) as f:
+        return f.read()
+
+setup(
+    # Package
+    name="pygame-minesweeper",
+    version="1.0",
+    packages=find_packages(exclude=("tests", "screenshots")),
+    url="https://github.com/andreasisnes/minesweeper",
+    install_requires=["pygame"],
+    entry_points={
+        "console_scripts": ["minesweeper=minesweeper.__main__:main"],
+    },
+    zip_safe=False,
+    include_package_data=True,
+    
+    # Contact
+    author="Andreas Isnes Nilsen",
+    author_email="andreas.isnes@gmail.com",
+    
+    # Description
+    description="Minesweeper game implemented in python using pygame",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
