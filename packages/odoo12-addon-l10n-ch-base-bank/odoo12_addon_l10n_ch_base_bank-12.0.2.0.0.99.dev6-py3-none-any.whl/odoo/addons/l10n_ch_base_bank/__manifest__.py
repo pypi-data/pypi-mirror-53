1218a80980574f636b9890f4a43607d3538ca6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:56 +0000

    Add package odoo12-addon-l10n-ch-base-bank/12.0.2.0.0.99.dev6/odoo12_addon_l10n_ch_base_bank-12.0.2.0.0.99.dev6-py3-none-any.whl

---
+++ packages/odoo12-addon-l10n-ch-base-bank/odoo12_addon_l10n_ch_base_bank-12.0.2.0.0.99.dev6-py3-none-any.whl/odoo/addons/l10n_ch_base_bank/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2012-2019 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{'name': 'Switzerland - Bank type',
+ 'summary': 'Types and number validation for swiss electronic pmnt. DTA, ESR',
+ 'version': '12.0.2.0.0',
+ 'author': "Camptocamp,Odoo Community Association (OCA)",
+ 'category': 'Localization',
+ 'website': 'https://github.com/OCA/l10n-switzerland',
+ 'license': 'AGPL-3',
+ 'depends': [
+     'base_iban',
+     'l10n_ch'
+ ],
+ 'data': [
+     'views/bank.xml',
+     'views/invoice.xml',
+ ],
+ 'auto_install': False,
+ 'installable': True,
+ }
