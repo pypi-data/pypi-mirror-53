344ca5eed878b05bc52cc6114b6fc97ac5d5bac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:03 +0000

    Add package mpf/0.53.0.dev62/mpf-0.53.0.dev62.tar.gz

---
+++ packages/mpf/mpf-0.53.0.dev62.tar.gz/mpf-0.53.0.dev62/mpf/exceptions/config_file_error.py
@@ -0,0 +1,25 @@
+"""Config file error in a MPF config file."""
+from mpf._version import log_url
+
+
+class ConfigFileError(AssertionError):
+
+    """Error in a config file found."""
+
+    def __init__(self, message, error_no, logger_name, context=None):
+        """Initialise exception."""
+        self._logger_name = logger_name
+        self._error_no = error_no
+        self._context = context
+        super().__init__(message)
+
+    def __str__(self):
+        """Return nice string."""
+        error_slug = "CFE-{}-{}".format(self._logger_name, self._error_no)
+        error_url = log_url.format(error_slug)
+        if self._context:
+            return "Config File Error in {}: {} Context: {} Error Code: {} ({})".format(
+                self._logger_name, super().__str__(), self._context, error_slug, error_url)
+
+        return "Config File Error in {}: {} Error Code: {} ({})".format(
+            self._logger_name, super().__str__(), error_slug, error_url)
