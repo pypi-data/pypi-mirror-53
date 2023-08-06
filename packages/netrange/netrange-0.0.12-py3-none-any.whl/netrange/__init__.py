3214439dae14609469242ab96483b9edaebe99d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:56 +0000

    Add package netrange/0.0.12/netrange-0.0.12-py3-none-any.whl

---
+++ packages/netrange/netrange-0.0.12-py3-none-any.whl/netrange/__init__.py
@@ -0,0 +1,13 @@
+from netrange import ip
+from netrange import port
+
+
+__version__ = '0.0.12'
+__description__ = 'A simple package for reading and ranging IPs and ports.'
+__all__ = [
+    'ip',
+    'port',
+    '__author__',
+    '__version__',
+    '__description__'
+]
