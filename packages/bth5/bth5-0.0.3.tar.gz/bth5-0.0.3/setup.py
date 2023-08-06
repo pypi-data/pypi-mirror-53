6eea744225a78579247fc9b6ad4486954d16c8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:08 +0000

    Add package bth5/0.0.3/bth5-0.0.3.tar.gz

---
+++ packages/bth5/bth5-0.0.3.tar.gz/bth5-0.0.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+from setuptools import setup
+
+
+def main():
+    """The main entry point."""
+    if sys.version_info[:2] < (3, 4):
+        sys.exit("xonsh currently requires Python 3.4+")
+    with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as f:
+        readme = f.read()
+    skw = dict(
+        name="bth5",
+        description="Bitemporal HDF5",
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        license="BSD-3-Clause",
+        version="0.0.3",
+        author="Anthony Scopatz",
+        maintainer="Quansight",
+        author_email="scopatz@gmail.com",
+        url="https://github.com/quansight/bitemporal-h5",
+        platforms="Cross Platform",
+        classifiers=["Programming Language :: Python :: 3"],
+        packages=["bth5"],
+        package_dir={"bth5": "bth5"},
+        zip_safe=True,
+        install_requires=["h5py >= 2.8", "numpy>=1.16", "numba>=0.45"],
+        extras_require={
+            "tests": ["pytest>=3.5", "pytest-black", "pytest-cov"],
+            "docs": ["sphinx", "sphinx_rtd_theme"],
+        },
+    )
+    setup(**skw)
+
+
+if __name__ == "__main__":
+    main()
