d2fb1b85f47a0ecb4e150f2f761324b38f4a80bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:29 +0000

    Add package Somecomfort-Homie-4/0.2.0/Somecomfort%20Homie%204-0.2.0.tar.gz

---
+++ packages/Somecomfort-Homie-4/Somecomfort%20Homie%204-0.2.0.tar.gz/Somecomfort Homie 4-0.2.0/somecomfort_homie_start.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import time
+import yaml 
+
+from somecomfort_homie.somecomfort_homie import Somecomfort_Homie
+
+with open("somecomfort_homie.yml", 'r') as ymlfile:
+    cfg = yaml.full_load(ymlfile)
+
+try:
+    
+    sch = Somecomfort_Homie(username=cfg['somecomfort'] ['username'],password=cfg['somecomfort'] ['password'],mqtt_settings=cfg['mqtt'])
+    
+    while True:
+        time.sleep(10)
+
+except (KeyboardInterrupt, SystemExit):
+    print("Quitting.")     
