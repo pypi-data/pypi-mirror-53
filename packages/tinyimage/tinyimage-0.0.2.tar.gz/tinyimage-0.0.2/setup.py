73942a96d1b5ab02af920b8d528f19a0586b0538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:06 +0000

    Add package tinyimage/0.0.2/tinyimage-0.0.2.tar.gz

---
+++ packages/tinyimage/tinyimage-0.0.2.tar.gz/tinyimage-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+# only used for dev.
+requirements = []
+test_requirements = []
+try:
+  from pipenv.project import Project
+  from pipenv.utils import convert_deps_to_pip
+  pfile = Project(chdir=False).parsed_pipfile
+  requirements = convert_deps_to_pip(pfile['packages'], r=False)
+  print("requirements: {}".format(requirements))
+  test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
+  print(test_requirements)
+except Exception as ex:
+  print("error: {}".format(str(ex)))
+
+setup(name="tinyimage",
+      version="0.0.2",
+      description="a lightweight image object library",
+      keywords="computer vision image",
+      url="https://github.com/VisualDataIO/tinyimage",
+      author="Jie Feng",
+      author_email="jiefeng@perceptance.io",
+      package_dir={"": "src"},
+      packages=find_namespace_packages(where="src"),
+      install_requires=requirements,
+      tests_require=test_requirements,
+      include_package_data=True,
+      zip_safe=False)
