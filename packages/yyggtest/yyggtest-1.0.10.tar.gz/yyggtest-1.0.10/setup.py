958833c72ba6c073fd9458196892c23735e7329f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:57 +0000

    Add package yyggtest/1.0.10/yyggtest-1.0.10.tar.gz

---
+++ packages/yyggtest/yyggtest-1.0.10.tar.gz/yyggtest-1.0.10/setup.py
@@ -0,0 +1,34 @@
+"""Setup for the chocobo package."""
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="GG",
+    author_email="gg@gmail.com",
+    name='yyggtest',
+    license="MIT",
+    description='yyggtest',
+    version='v1.0.10',
+    long_description=README,
+    url='',
+    packages=setuptools.find_packages(),
+    package_data={'demo': ['orthodr_cpp.cpython-36m-darwin.so']},
+    python_requires=">=3.5",
+    install_requires=['requests'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
