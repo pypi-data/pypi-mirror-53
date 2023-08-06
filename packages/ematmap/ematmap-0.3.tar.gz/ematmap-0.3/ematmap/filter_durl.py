8928bc82025d7dc3492a1010c24efdeaeb1192da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:03 +0000

    Add package ematmap/0.3/ematmap-0.3.tar.gz

---
+++ packages/ematmap/ematmap-0.3.tar.gz/ematmap-0.3/ematmap/filter_durl.py
@@ -0,0 +1,155 @@
+import ematmap.filter_engine as engine
+
+@engine.durl_wrap
+def ffltre(d):
+    return(engine.ffltre(d))
+
+
+@engine.durl_wrap
+def xfltre(d):
+    return(engine.xfltre(d))
+
+
+@engine.durl_wrap
+def yfltre(d):
+    return(engine.yfltre(d))
+
+
+@engine.durl_wrap
+def vfltre(d):
+    return(engine.vfltre(d))
+
+
+@engine.durl_wrap
+def ofltre(d):
+    return(engine.ofltre(d))
+
+
+@engine.durl_wrap
+def fxfltre(d):
+    return(engine.fxfltre(d))
+
+
+@engine.durl_wrap
+def fyfltre(d):
+    return(engine.fyfltre(d))
+
+
+@engine.durl_wrap
+def fvfltre(d):
+    return(engine.fvfltre(d))
+
+
+@engine.durl_wrap
+def fofltre(d):
+    return(engine.fofltre(d))
+
+
+@engine.durl_wrap
+def xyfltre(d):
+    return(engine.xyfltre(d))
+
+
+@engine.durl_wrap
+def xvfltre(d):
+    return(engine.xvfltre(d))
+
+
+@engine.durl_wrap
+def xofltre(d):
+    return(engine.xofltre(d))
+
+
+@engine.durl_wrap
+def yvfltre(d):
+    return(engine.yvfltre(d))
+
+
+@engine.durl_wrap
+def yofltre(d):
+    return(engine.yofltre(d))
+
+
+@engine.durl_wrap
+def vofltre(d):
+    return(engine.vofltre(d))
+
+
+@engine.durl_wrap
+def fxyfltre(d):
+    return(engine.fxyfltre(d))
+
+
+@engine.durl_wrap
+def fxvfltre(d):
+    return(engine.fxvfltre(d))
+
+
+@engine.durl_wrap
+def fxofltre(d):
+    return(engine.fxofltre(d))
+
+
+@engine.durl_wrap
+def fyvfltre(d):
+    return(engine.fyvfltre(d))
+
+
+@engine.durl_wrap
+def fyofltre(d):
+    return(engine.fyofltre(d))
+
+
+@engine.durl_wrap
+def fvofltre(d):
+    return(engine.fvofltre(d))
+
+
+@engine.durl_wrap
+def xyvfltre(d):
+    return(engine.xyvfltre(d))
+
+
+@engine.durl_wrap
+def xyofltre(d):
+    return(engine.xyofltre(d))
+
+
+@engine.durl_wrap
+def xvofltre(d):
+    return(engine.xvofltre(d))
+
+
+@engine.durl_wrap
+def yvofltre(d):
+    return(engine.yvofltre(d))
+
+
+@engine.durl_wrap
+def fxyvfltre(d):
+    return(engine.fxyvfltre(d))
+
+
+@engine.durl_wrap
+def fxyofltre(d):
+    return(engine.fxyofltre(d))
+
+
+@engine.durl_wrap
+def fxvofltre(d):
+    return(engine.fxvofltre(d))
+
+
+@engine.durl_wrap
+def fyvofltre(d):
+    return(engine.fyvofltre(d))
+
+
+@engine.durl_wrap
+def xyvofltre(d):
+    return(engine.xyvofltre(d))
+
+
+@engine.durl_wrap
+def fxyvofltre(d):
+    return(engine.fxyvofltre(d))
---
+++ packages/ematmap/ematmap-0.3.tar.gz/ematmap-0.3/ematmap/filter_udlr.py
@@ -0,0 +1,155 @@
+import ematmap.filter_engine as engine
+
+@engine.udlr_wrap
+def ffltre(d):
+    return(engine.ffltre(d))
+
+
+@engine.udlr_wrap
+def xfltre(d):
+    return(engine.xfltre(d))
+
+
+@engine.udlr_wrap
+def yfltre(d):
+    return(engine.yfltre(d))
+
+
+@engine.udlr_wrap
+def vfltre(d):
+    return(engine.vfltre(d))
+
+
+@engine.udlr_wrap
+def ofltre(d):
+    return(engine.ofltre(d))
+
+
+@engine.udlr_wrap
+def fxfltre(d):
+    return(engine.fxfltre(d))
+
+
+@engine.udlr_wrap
+def fyfltre(d):
+    return(engine.fyfltre(d))
+
+
+@engine.udlr_wrap
+def fvfltre(d):
+    return(engine.fvfltre(d))
+
+
+@engine.udlr_wrap
+def fofltre(d):
+    return(engine.fofltre(d))
+
+
+@engine.udlr_wrap
+def xyfltre(d):
+    return(engine.xyfltre(d))
+
+
+@engine.udlr_wrap
+def xvfltre(d):
+    return(engine.xvfltre(d))
+
+
+@engine.udlr_wrap
+def xofltre(d):
+    return(engine.xofltre(d))
+
+
+@engine.udlr_wrap
+def yvfltre(d):
+    return(engine.yvfltre(d))
+
+
+@engine.udlr_wrap
+def yofltre(d):
+    return(engine.yofltre(d))
+
+
+@engine.udlr_wrap
+def vofltre(d):
+    return(engine.vofltre(d))
+
+
+@engine.udlr_wrap
+def fxyfltre(d):
+    return(engine.fxyfltre(d))
+
+
+@engine.udlr_wrap
+def fxvfltre(d):
+    return(engine.fxvfltre(d))
+
+
+@engine.udlr_wrap
+def fxofltre(d):
+    return(engine.fxofltre(d))
+
+
+@engine.udlr_wrap
+def fyvfltre(d):
+    return(engine.fyvfltre(d))
+
+
+@engine.udlr_wrap
+def fyofltre(d):
+    return(engine.fyofltre(d))
+
+
+@engine.udlr_wrap
+def fvofltre(d):
+    return(engine.fvofltre(d))
+
+
+@engine.udlr_wrap
+def xyvfltre(d):
+    return(engine.xyvfltre(d))
+
+
+@engine.udlr_wrap
+def xyofltre(d):
+    return(engine.xyofltre(d))
+
+
+@engine.udlr_wrap
+def xvofltre(d):
+    return(engine.xvofltre(d))
+
+
+@engine.udlr_wrap
+def yvofltre(d):
+    return(engine.yvofltre(d))
+
+
+@engine.udlr_wrap
+def fxyvfltre(d):
+    return(engine.fxyvfltre(d))
+
+
+@engine.udlr_wrap
+def fxyofltre(d):
+    return(engine.fxyofltre(d))
+
+
+@engine.udlr_wrap
+def fxvofltre(d):
+    return(engine.fxvofltre(d))
+
+
+@engine.udlr_wrap
+def fyvofltre(d):
+    return(engine.fyvofltre(d))
+
+
+@engine.udlr_wrap
+def xyvofltre(d):
+    return(engine.xyvofltre(d))
+
+
+@engine.udlr_wrap
+def fxyvofltre(d):
+    return(engine.fxyvofltre(d))
