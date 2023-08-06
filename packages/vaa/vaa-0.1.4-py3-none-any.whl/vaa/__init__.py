bb8687f96a23fbfb70572c5c0b9ae59d4d6e7861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:36 +0000

    Add package vaa/0.1.4/vaa-0.1.4-py3-none-any.whl

---
+++ packages/vaa/vaa-0.1.4-py3-none-any.whl/vaa/__init__.py
@@ -0,0 +1,28 @@
+"""Validators Adapter. The common interface for all validators.
+"""
+
+
+from ._aliases import (
+    cerberus,
+    django,
+    marshmallow,
+    pyschemes,
+    simple,
+    restframework,
+    wtforms,
+)
+from ._internal import ValidationError
+
+
+__version__ = '0.1.4'
+
+__all__ = [
+    'cerberus',
+    'django',
+    'marshmallow',
+    'pyschemes',
+    'restframework',
+    'simple',
+    'ValidationError',
+    'wtforms',
+]
