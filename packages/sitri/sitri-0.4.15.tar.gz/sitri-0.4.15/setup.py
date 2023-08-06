2469d503c33e8aa8bc2ea47c8639938527c028c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:32 +0000

    Add package sitri/0.4.15/sitri-0.4.15.tar.gz

---
+++ packages/sitri/sitri-0.4.15.tar.gz/sitri-0.4.15/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['sitri', 'sitri.config', 'sitri.contrib', 'sitri.credentials']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'sitri',
+    'version': '0.4.15',
+    'description': 'Library for one endpoint config and credentials managment',
+    'long_description': '![Sitri](docs/_static/logo.gif#center)\n\n#  Sitri Configuration Library\n\nSitri - library for managing authorization and configuration data from a single object with possibly different or identical providers\n\n#  Installation\n\n```bash\npoetry add sitri\n```\n\nor\n```bash\npip3 install sitri\n```\n\n# Basics with SystemProvider\n\n```python\nfrom sitri.contrib.system import SystemCredentialProvider, SystemConfigProvider\nfrom sitri import Sitri\n\nconf = Sitri(config_provider=SystemConfigProvider(prefix="basics"),\n             credential_provider=SystemCredentialProvider(prefix="basics"))\n```\nSystem provider use system environment for get config and credential data. For unique sitri lookup to "namespace" by prefix.\n\nExample:\n*In console:*\n```bash\nexport BASICS_NAME=Huey\n```\n*In code:*\n```python\nname = conf.get_config("name")\nprint(name)  # output: Huey\n```\n\n#  Docs\nRead base API references and other part documentation on https://sitri.readthedocs.io/\n',
+    'author': 'Alexander Lavrov',
+    'author_email': 'egnod@ya.ru',
+    'url': 'https://github.com/Egnod/sitri',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
