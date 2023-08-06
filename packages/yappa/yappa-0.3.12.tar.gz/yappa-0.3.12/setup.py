503be3a0f1e22e8015bc042f6350c5ab1e74e5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:16 +0000

    Add package yappa/0.3.12/yappa-0.3.12.tar.gz

---
+++ packages/yappa/yappa-0.3.12.tar.gz/yappa-0.3.12/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['yappa']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.9,<2.0', 'click>=7.0,<8.0', 'python-slugify>=3.0,<4.0']
+
+entry_points = \
+{'console_scripts': ['yappa = yappa.cli:cli']}
+
+setup_kwargs = {
+    'name': 'yappa',
+    'version': '0.3.12',
+    'description': '',
+    'long_description': None,
+    'author': 'Mike',
+    'author_email': 'mikhail.g.novikov@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
