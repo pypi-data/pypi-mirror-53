3683f5c9f11ca2649bd05c1a9709fea3f7a95b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:40 +0000

    Add package wisdoms/2.2.4/wisdoms-2.2.4.tar.gz

---
+++ packages/wisdoms/wisdoms-2.2.4.tar.gz/wisdoms-2.2.4/setup.py
@@ -0,0 +1,28 @@
+# Created by Q-ays.
+# whosqays@gmail.com
+
+
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wisdoms",
+    version="2.2.4",
+    author="tzxd group",
+    author_email="whosqays@qq.com",
+    description="装饰器携带全部信息灵活配置，组织信息添加es 拓展数据",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/soft-project/wisdom-lib",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyYaml'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
