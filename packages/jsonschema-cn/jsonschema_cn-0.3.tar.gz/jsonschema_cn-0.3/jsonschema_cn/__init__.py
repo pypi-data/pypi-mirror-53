682c9d5597371a1d2fc580e8cc1d9cd5abcc5dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:59 +0000

    Add package jsonschema-cn/0.3/jsonschema_cn-0.3.tar.gz

---
+++ packages/jsonschema-cn/jsonschema_cn-0.3.tar.gz/jsonschema_cn-0.3/jsonschema_cn/__init__.py
@@ -0,0 +1,22 @@
+from .grammar import grammar
+from .visitor import JSCNVisitor
+from .tree import Type
+from typing import Any
+
+
+__version__ = "0.3"
+
+
+def to_tree(source: str) -> Type:
+    raw_tree = grammar.parse(source)
+    visitor = JSCNVisitor()
+    parsed_tree = visitor.visit(raw_tree)
+    return parsed_tree
+
+
+def to_schema(source: str) -> Any:
+    return to_tree(source).to_schema()
+
+
+def to_json(source: str) -> str:
+    return to_tree(source).to_json()
