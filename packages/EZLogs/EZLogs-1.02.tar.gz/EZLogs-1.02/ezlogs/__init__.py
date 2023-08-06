c0ec9c9514b4ff462095d7ade299a66028108b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:24 +0000

    Add package EZLogs/1.02/EZLogs-1.02.tar.gz

---
+++ packages/EZLogs/EZLogs-1.02.tar.gz/EZLogs-1.02/ezlogs/__init__.py
@@ -0,0 +1,1 @@
+from .ezlogs import Logger
---
+++ packages/EZLogs/EZLogs-1.02.tar.gz/EZLogs-1.02/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='EZLogs',
+    version='1.02',
+    packages=['ezlogs',],
+    license='MIT',
+    description='A simple wrapper around the Python logging module',
+    url='https://github.com/greencoder/ezlogs',
+    download_url='https://github.com/greencoder/ezlogs/archive/1.02.tar.gz',
+    author='Scott Newman',
+    author_email='snewman18@gmail.com',
+)
