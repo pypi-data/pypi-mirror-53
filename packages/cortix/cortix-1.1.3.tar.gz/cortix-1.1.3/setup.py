dac8a6e1a49ee6b810b45d0edb07c5b804190cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:32 +0000

    Add package cortix/1.1.3/cortix-1.1.3.tar.gz

---
+++ packages/cortix/cortix-1.1.3.tar.gz/cortix-1.1.3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    req = fh.readlines()
+
+setuptools.setup(
+    name="cortix",
+    version="1.1.3",
+    author="Cortix Computing",
+    author_email="tazzaoui@cs.uml.edu",
+    description="Cortix is a Python library for system-level\
+                module coupling, execution, and analysis.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=req,
+    url="https://cortix.org",
+    packages=setuptools.find_namespace_packages(),
+    keywords = ['simulation', 'math'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Topic :: Education',
+        'Topic :: Utilities'
+    ],
+)
