87090bc45189debf274b59cea9c6ac3963eb5daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:16 +0000

    Add package snyk-threadfix/0.0.1/snyk-threadfix-0.0.1.tar.gz

---
+++ packages/snyk-threadfix/snyk-threadfix-0.0.1.tar.gz/snyk-threadfix-0.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['snyk_threadfix']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['arrow', 'pysnyk']
+
+entry_points = \
+{'console_scripts': ['snyk-threadfix = snyk_threadfix.main:run']}
+
+setup_kwargs = {
+    'name': 'snyk-threadfix',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': "# snyk-threadfix\n\nThis tool allows you to generate `.threadfix` file data from Snyk project data. It outputs JSON data in the ThreadFix file format - both printing to standard out and also allowing you to specify an output filename.\n\nIt does not upload directly to ThreadFix at this time. However:\n1. We will likely do that in the future given sufficient demand\n2. There is a ThreadFix API endpoint that you can use for this: [ThreadFix Upload Scan API](https://denimgroup.atlassian.net/wiki/spaces/TDOC/pages/22908335/Upload+Scan+-+API)\n\n## Installation\n```\npip install snyk-threadfix\n```\n\n## Configuration\nYou must first obtain a Snyk API token from your [Snyk account](https://app.snyk.io/login). Once you have a token you must either install the [Snyk CLI](https://github.com/snyk/snyk) and run `snyk auth <your-token>` or simply run:\n```\nexport SNYK_TOKEN=<your-token> \n```\n\n## Usage\nYou must first identify your Snyk org ID. This is easy - simply log into your Snyk account, click on Settings, and find your Organization ID there. If you have multiple orgs in your Snyk account, make sure to first choose the one you want.\n![Snyk Project ID](images/project-id-in-snyk-ui.png)\n\n\nYou must also identify the Snyk project ID's for which you would like to generate ThreadFix data. You can do this using the Snyk API, for example, using the [List all projects](https://snyk.docs.apiary.io/#reference/projects/list-all-projects) endpoint. See also the [pysnyk SDK](https://github.com/snyk-labs/pysnyk). Another way of identifying the project IDs you want to use is simply by browsing to the desired project(s) with the Snyk UI and grabbing the UUID from the address bar of your browser.\n\n![Snyk Project ID](images/snyk-org-id-in-ui.png)\n\n\nOnce you have a project ID or list of project IDs that you would like to generate a threadfix file for, run the following:\n\n*For a single project ID:*\n```\nsnyk-threadfix --orgId=<your-snyk-org-id> --projectIds=<snyk-project-id>\n```\n\n*For multiple IDs:*\n```\nsnyk-threadfix --orgId=<your-snyk-org-id> --projectIds=<snyk-project-id-0>,<snyk-project-id-1>,<snyk-project-id-2>,...\n```\n\nThreadFix JSON data will be output to standard out. If you would like to save the JSON to a file you can either pipe it to a file or use the `--output` parameter, for example:\n```\nsnyk-threadfix --output=<your-desired-output-filename>.threadfix --orgId=<your-snyk-org-id> --projectIds=<snyk-project-id>\n```\n\n\nAdditional input parameters are available:\n```\nsnyk-threadfix main.py --help\n```\n",
+    'author': 'Jeff McLean',
+    'author_email': 'jeff@snyk.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/snyk-labs/snyk-threadfix',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/snyk-threadfix/snyk-threadfix-0.0.1.tar.gz/snyk-threadfix-0.0.1/snyk_threadfix/main.py
@@ -0,0 +1,270 @@
+import sys
+import argparse
+import json
+import snyk
+import hashlib
+import arrow
+from sys import stderr
+from snyk_threadfix.utils import get_token, validate_token
+
+
+snyk_token = None
+client = None
+debug = False
+
+
+class SnykTokenError(Exception):
+    pass
+
+
+class SnykTokenNotFoundError(SnykTokenError):
+    pass
+
+
+class SnykTokenInvalidError(SnykTokenError):
+    pass
+
+
+def log(msg):
+    global debug
+    if debug:
+        print(msg, file=stderr)
+
+
+def parse_command_line_args(command_line_args):
+    parser = argparse.ArgumentParser(description="Snyk API Examples")
+    parser.add_argument(
+        "--orgId", type=str, help="The Snyk Organisation Id", required=True
+    )
+    parser.add_argument(
+        "--projectIds", type=str, help="Comma-separated list of Snyk project IDs", required=True
+    )
+    parser.add_argument('--output', type=str,
+                        help='Optional: name output file to write to (should use .threadfix extension).',
+                        required=False)
+    parser.add_argument(
+        "--debug", action='store_true', help="Send additional debug info to stderr", required=False)
+
+    args = parser.parse_args(command_line_args)
+
+    str_project_ids = args.projectIds
+
+    if not str_project_ids:
+        sys.exit('--projectIds input parameter is required')
+
+    project_ids = str_project_ids.split(',')
+    if len(project_ids) > 0:
+        args.project_ids = project_ids
+    else:
+        sys.exit('--projectIds input parameter is required')
+
+    return args
+
+
+def parse_snyk_project_name(project_name):
+    project_repo_name_and_branch = project_name.split(':')[0]
+    project_target_file = project_name.split(':')[1]
+    project_repo_name = project_repo_name_and_branch.split('(')[0]
+
+    project_branch_name = None
+    if '(' in project_repo_name_and_branch:
+        # after the '(' and before the ')'
+        project_branch_name = project_repo_name_and_branch.split('(')[1].split(')')[0]
+
+    project_meta_data = {
+        'repo': project_repo_name,
+        'targetFile': project_target_file
+    }
+
+    if project_branch_name:
+        project_meta_data['branch'] = project_branch_name
+
+    return project_meta_data
+
+
+def lookup_project_ids_by_repo_name_py_snyk(org_id, repo_name, origin, branch, target_file):
+    """not used, but keeping it around for possible future use"""
+    git_repo_project_origins = [
+        'github',
+        'github-enterprise',
+        'gitlab',
+        'bitbucket-cloud',
+        'bitbucket-server',
+        'azure-repos'
+    ]
+
+    projects = client.organizations.get(org_id).projects.all()
+
+    matching_project_ids = []
+
+    for p in projects:
+        if p.origin in git_repo_project_origins:
+            project_name_details = parse_snyk_project_name(p.name)
+
+            if repo_name == project_name_details['repo'] and \
+                    (not branch or branch and branch == project_name_details['branch']) and \
+                    (not origin or origin and origin == p.origin) and \
+                    (not target_file or target_file and target_file == project_name_details['targetFile']):
+                log('Found project with matching repo name:')
+                log('  %s' % p.name)
+                log('  %s' % p.id)
+                log('  %s\n' % p.origin)
+                matching_project_ids.append(p.id)
+
+    return matching_project_ids
+
+
+def generate_native_id(org_id, project_id, issue_id, path_list):
+    path_str = '>'.join(path_list)
+    unhashed = '%s:%s:%s:%s' % (org_id, project_id, issue_id, path_str)
+
+    sha1_hash = hashlib.sha1()
+    b = bytes(unhashed, 'utf-8')
+    sha1_hash.update(b)
+    hashed_native_id = sha1_hash.hexdigest()
+
+    return hashed_native_id
+
+
+def snyk_identifiers_to_threadfix_mappings(snyk_identifiers_list):
+    mappings = []
+
+    identifier_keys = snyk_identifiers_list.keys()
+    primary_key = ''
+    primary_set = False
+
+    if 'CWE' in identifier_keys and snyk_identifiers_list['CWE']:
+        primary_key = 'CWE'
+    elif 'ALTERNATIVE' in identifier_keys and snyk_identifiers_list['ALTERNATIVE']:
+        primary_key = 'ALTERNATIVE'
+    elif 'CVE' in identifier_keys and snyk_identifiers_list['CVE']:
+        primary_key = 'CVE'
+    elif 'NSP' in identifier_keys and snyk_identifiers_list['NSP']:
+        primary_key = 'NSP'
+
+    for k, v_list in snyk_identifiers_list.items():
+        if v_list:
+            for v in v_list:
+                is_primary = False
+                if k == primary_key and not primary_set:
+                    is_primary = True
+                    primary_set = True
+
+                mapping_value = v.replace("CWE-", "") if k == 'CWE' else v
+
+                mappings.append({
+                    "mappingType": k,
+                    "value": mapping_value,
+                    "primary": is_primary
+                })
+
+    return mappings
+
+
+def create_threadfix_findings_data(org_id, project_id):
+    p = client.organizations.get(org_id).projects.get(project_id)
+    findings = []
+    project_meta_data = parse_snyk_project_name(p.name)
+    target_file = project_meta_data['targetFile']
+
+    for i in p.vulnerabilities:
+        native_id = generate_native_id(org_id, project_id, i.id, i.fromPackages)
+
+        finding = {
+            'nativeId': native_id,
+            'severity': i.severity,
+            'nativeSeverity': i.cvssScore,
+            'summary': i.title,
+            'description': 'You can find the description here: %s' % i.url,
+            'scannerDetail': 'You can find the description here: %s' % i.url,
+            'scannerRecommendation': i.url,  # TBD
+            'dependencyDetails': {
+                'library': i.package,
+                'description': 'You can find the description here: %s' % i.url,
+                'reference': i.id,
+                'referenceLink': i.url,
+                'filePath': target_file,
+                'version': i.version,
+                'issueType': 'VULNERABILITY',
+            },
+            'metadata': {
+                "language": i.language,  # TODO: figure out what this means for CLI/container project types
+                "packageManager": i.packageManager,  # TODO: figure out what this means for CLI/container project types
+                "CVSSv3": i.CVSSv3,
+                "cvssScore": i.cvssScore,
+                "snyk_source": p.origin,
+                "snyk_project_id": project_id,
+                "snyk_project_name": p.name,
+                "snyk_repo": project_meta_data['repo'],
+                "snyk_branch": project_meta_data.get('branch', '(default branch)'),
+                "snyk_target_file": project_meta_data['targetFile'],
+                "snyk_project_url": p.browseUrl,
+                "snyk_organization": org_id
+            },
+
+            'mappings': []
+        }
+
+        mappings = snyk_identifiers_to_threadfix_mappings(i.identifiers)
+        finding['mappings'] = mappings
+        findings.append(finding)
+
+    return findings
+
+
+def write_to_threadfix_file(output_filename, threadfix_json_obj):
+    log('Writing output threadfix file: %s' % output_filename)
+
+    with open(output_filename, 'w') as output_json_file:
+        json.dump(threadfix_json_obj, output_json_file, indent=4)
+
+
+def write_output_to_stdout(threadfix_json_obj):
+    json.dump(threadfix_json_obj, sys.stdout, indent=4)
+
+
+def main(args):
+    global snyk_token, client, debug
+    args = parse_command_line_args(args)
+    debug = args.debug
+
+    snyk_token = get_token()
+    token_is_valid = validate_token(snyk_token)
+    if not token_is_valid:
+        raise SnykTokenInvalidError('invalid token')
+
+    client = snyk.SnykClient(snyk_token)
+
+    # project_ids = lookup_project_ids_by_repo_name_py_snyk(args.orgId, repo_name, origin, branch, target_file)
+    project_ids = args.project_ids
+
+    current_time = arrow.utcnow().replace(microsecond=0)
+    current_time_str = current_time.isoformat().replace("+00:00", "Z")
+
+    threadfix_json_obj = {
+        'created': current_time_str,  # All timestamps are to be in yyyy-MM-dd'T'HH:mm:ss'Z' format
+        'exported': current_time_str,  # All timestamps are to be in yyyy-MM-dd'T'HH:mm:ss'Z' format
+        'collectionType': 'DEPENDENCY',
+        'source': 'Snyk',
+        'findings': []
+    }
+
+    all_threadfix_findings = []
+
+    for p_id in project_ids:
+        threadfix_findings = create_threadfix_findings_data(args.orgId, p_id)
+        all_threadfix_findings.extend(threadfix_findings)
+
+    threadfix_json_obj['findings'] = all_threadfix_findings
+    write_output_to_stdout(threadfix_json_obj)
+    if args.output:
+        write_to_threadfix_file(args.output, threadfix_json_obj)
+
+
+def run():
+    args = sys.argv[1:]
+    main(args)
+
+
+if __name__ == '__main__':
+    run()
