98029b6f5c8d23ece4d6f8efc3c73ab7748c396e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:28 +0000

    Add package chat-app/1.1.10/chat-app-1.1.10.tar.gz

---
+++ packages/chat-app/chat-app-1.1.10.tar.gz/chat-app-1.1.10/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chat-app",
+    version="1.1.10",
+    author="Arin Khare",
+    author_email="arinmkhare@gmail.com",
+    description="Allows people to chat with others through terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lol-cubes/chat-app",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=["cryptography"]
+)
