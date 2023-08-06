912f19d82505afadd1f21be2b443d2adb6b2cbe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:26 +0000

    Add package skiply/0.4.94/skiply-0.4.94.tar.gz

---
+++ packages/skiply/skiply-0.4.94.tar.gz/skiply-0.4.94/skiply/cdm/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding: utf8
+
+# Copyright 2019 Skiply
+
+from .base import db_session
+
+from .base import Base
+
+from .device import Device
+from .entity import Entity
+from .group import Group
+from .keyboard import Keyboard
+from .network import Network
+from .question import Question
