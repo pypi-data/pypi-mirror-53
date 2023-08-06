824e6fce86997c995a0298a985f92feeaa77beea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:51 +0000

    Add package OptGBM/0.1.0/OptGBM-0.1.0.tar.gz

---
+++ packages/OptGBM/OptGBM-0.1.0.tar.gz/OptGBM-0.1.0/README.md
@@ -0,0 +1,29 @@
+# OptGBM
+
+[![CircleCI](https://circleci.com/gh/Y-oHr-N/OptGBM.svg?style=svg)](https://circleci.com/gh/Y-oHr-N/OptGBM)
+
+Optuna + LightGBM = OptGBM
+
+## Examples
+
+```python
+from optgbm import OGBMClassifier
+from sklearn.datasets import load_iris
+
+clf = OGBMClassifier(random_state=0)
+X, y = load_iris(return_X_y=True)
+
+clf.fit(X, y)
+```
+
+## Installation
+
+```
+pip install optgbm
+```
+
+## Testing
+
+```
+python setup.py test
+```
---
+++ packages/OptGBM/OptGBM-0.1.0.tar.gz/OptGBM-0.1.0/tests/test_sklearn.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+import numpy as np
+import optuna
+import pytest
+
+from sklearn.datasets import load_boston
+from sklearn.utils.estimator_checks import check_estimator
+
+from optgbm import OGBMRegressor
+
+
+def test_ogbm_regressor() -> None:
+    check_estimator(OGBMRegressor)
+
+
+@pytest.mark.parametrize('storage', [None, 'sqlite:///:memory:'])
+def test_fit_twice_with_study(storage: Optional[str]) -> None:
+    X, y = load_boston(return_X_y=True)
+    n_trials = 5
+    study = optuna.create_study(storage=storage)
+    reg = OGBMRegressor(n_trials=n_trials, study=study)
+
+    reg.fit(X, y)
+
+    assert len(study.trials) == n_trials
+
+    reg.fit(X, y)
+
+    assert len(study.trials) == 2 * n_trials
+
+
+@pytest.mark.parametrize('n_jobs', [-1, 1])
+def test_feature_importances(n_jobs: int) -> None:
+    X, y = load_boston(return_X_y=True)
+    reg = OGBMRegressor(n_jobs=n_jobs)
+
+    reg.fit(X, y)
+
+    assert isinstance(reg.feature_importances_, np.ndarray)
