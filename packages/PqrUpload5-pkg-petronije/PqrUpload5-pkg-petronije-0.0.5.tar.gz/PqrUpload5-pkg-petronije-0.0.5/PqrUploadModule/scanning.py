ffdc87d47c921a4d07a78cdc3135aaa1de3dfac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:30 +0000

    Add package PqrUpload5-pkg-petronije/0.0.5/PqrUpload5-pkg-petronije-0.0.5.tar.gz

---
+++ packages/PqrUpload5-pkg-petronije/PqrUpload5-pkg-petronije-0.0.5.tar.gz/PqrUpload5-pkg-petronije-0.0.5/PqrUploadModule/scanning.py
@@ -0,0 +1,26 @@
+import _scan
+from start_settings import return_start_time
+
+
+
+class Scan(_scan.HelpScan):
+    def __init__(self,account_id):
+        self.account_id = account_id
+
+        self.start_time = return_start_time()
+
+
+        self.open_at_tickets = self.get_all_non_compl_tickets_from_at()
+        self.open_td_incidents = self.get_all_non_compl_incidents_from_td()
+
+        self.at_tickets_for_counterparts = self.list_at_ticket_for_counterparts()
+
+        self.td_incidents_for_counterparts = self.list_td_incidents_for_counterparts()
+
+
+
+
+
+
+
+
---
+++ packages/PqrUpload5-pkg-petronije/PqrUpload5-pkg-petronije-0.0.5.tar.gz/PqrUpload5-pkg-petronije-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PqrUpload5-pkg-petronije",
+    version="0.0.5",
+    author="Example Author",
+    author_email="petronije2002@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/petronije2002/PqrUpload/archive/0.0.5.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='==3.6',
+)
