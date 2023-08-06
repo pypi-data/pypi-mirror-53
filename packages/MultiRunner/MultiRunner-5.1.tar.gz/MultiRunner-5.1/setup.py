fac263484e9d5f538c382025a244ece21d11a9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:20 +0000

    Add package MultiRunner/5.1/MultiRunner-5.1.tar.gz

---
+++ packages/MultiRunner/MultiRunner-5.1.tar.gz/MultiRunner-5.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "5.1",
+    description= "This is a python package for multi-process running.",
+    long_description= "This is a package for multi-process running, support both python2 and python3.",
+    url='https://github.com/Statham-stone/MultiRunner',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
+#python setup.py register sdist upload
