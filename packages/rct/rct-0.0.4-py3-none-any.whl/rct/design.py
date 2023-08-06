db4ff20f79f55b3db59f9e743875d580107c5732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:57 +0000

    Add package rct/0.0.4/rct-0.0.4-py3-none-any.whl

---
+++ packages/rct/rct-0.0.4-py3-none-any.whl/rct/design.py
@@ -0,0 +1,141 @@
+from hashlib import md5
+import random
+import abc
+
+import lazy_property
+import pandas as pd
+import numpy as np
+
+from .assignment import draw_iid_assignment, draw_shuffled_assignment, \
+    get_assignments_as_positions
+from .balance import BalanceObjective
+from .utils import QuantileTarget
+
+
+class RCTBase:
+    def __init__(self, file_path, weights, seed=0):
+        self.file_path = file_path
+        self.weights = weights
+        self.shift_seed = seed
+        self.df = pd.read_csv(file_path)
+
+    @lazy_property.LazyProperty
+    def file_hash_int(self):
+        hasher = md5()
+        with open(self.file_path, 'rb') as fh:
+            buf = fh.read()
+            hasher.update(buf)
+        return int(hasher.hexdigest(), 16)
+
+    @lazy_property.LazyProperty
+    def seed(self):
+        return (self.file_hash_int + self.shift_seed) % 2 ** 32
+
+    @property
+    @abc.abstractmethod
+    def assignment_from_iid(self):
+        """"""
+
+    def _draw_iid_assignment(self):
+        return self.as_frame(
+            draw_iid_assignment(self.weights, self.sample_size))
+
+    @property
+    @abc.abstractmethod
+    def assignment_from_shuffled(self):
+        """"""
+
+    def _draw_shuffled_assignment(self):
+        return self.as_frame(
+            draw_shuffled_assignment(self.weights, self.sample_size))
+
+    @property
+    def sample_size(self):
+        return len(self.df)
+
+    def as_frame(self, assignment):
+        return pd.DataFrame(
+            data=assignment, index=self.df.index, columns=['t'])
+
+
+class RCT(RCTBase):
+
+    @property
+    def assignment_from_iid(self):
+        np.random.seed(self.seed)
+        return self._draw_iid_assignment()
+
+    @property
+    def assignment_from_shuffled(self):
+        random.seed(self.seed)
+        return self._draw_shuffled_assignment()
+
+
+class BalancedRCTBase(RCTBase):
+    def __init__(self, objective, file_path, weights, k=None, seed=0):
+        super().__init__(file_path, weights, seed)
+        self._balance = objective.balance_func \
+            if isinstance(objective, BalanceObjective) else objective
+        self._k = k
+
+    @property
+    def k(self):
+        if self._k is None:
+            self._k = self.sample_size
+        return self._k
+
+    def balance(self, assignment):
+        return float(self._balance(
+            self.df, get_assignments_as_positions(assignment)).values)
+
+    def assignment_generator(self, draw_fun):
+        return (draw_fun(self.weights, self.sample_size) for _ in range(
+            self.k))
+
+
+class KRerandomizedRCT(BalancedRCTBase):
+    @property
+    def assignment_from_iid(self):
+        np.random.seed(self.seed)
+        assignments = self.assignment_generator(draw_iid_assignment)
+        return self._get_best_assignment(assignments)
+
+    @property
+    def assignment_from_shuffled(self):
+        random.seed(self.seed)
+        assignments = self.assignment_generator(draw_shuffled_assignment)
+        return self._get_best_assignment(assignments)
+
+    def _get_best_assignment(self, assignments):
+        return self.as_frame(max(assignments, key=self.balance))
+
+
+class QuantileTargetingRCT(BalancedRCTBase):
+    def __init__(self, objective, file_path, weights,
+                 quantile_target=None, seed=0, num_monte_carlo=1000):
+        super().__init__(objective, file_path, weights, num_monte_carlo, seed)
+        self.quantile_target = quantile_target
+
+    def balance(self, assignment):
+        return float(self._balance(
+            self.df, get_assignments_as_positions(assignment)).values)
+
+    @property
+    def assignment_from_iid(self):
+        np.random.seed(self.seed)
+        assignments = self.assignment_generator(draw_iid_assignment)
+        return self.get_target_assignments(assignments)
+
+    @property
+    def assignment_from_shuffled(self):
+        random.seed(self.seed)
+        assignments = self.assignment_generator(draw_shuffled_assignment)
+        return self.get_target_assignments(assignments)
+
+    def get_target_assignments(self, assignments):
+        random.seed(self.seed + 1)
+        qtargets = QuantileTarget(
+            self.quantile_target, self.balance, assignments, self.k)
+        qtargets.compute_best()
+        _, selected_assignment = random.choice(qtargets.quantiles)
+        return self.as_frame(selected_assignment)
---
+++ packages/rct/rct-0.0.4-py3-none-any.whl/rct/tests/test_design.py
@@ -0,0 +1,136 @@
+from numpy.testing import TestCase, assert_array_almost_equal
+from os import path
+
+from ..design import RCT, KRerandomizedRCT, QuantileTargetingRCT
+from ..balance import MahalanobisBalance, pvalues_report
+from ..assignment import get_assignments_as_positions
+
+
+class TestRCT(TestCase):
+    def setUp(self) -> None:
+        self.file = path.join(path.dirname(__file__), 'test_data',
+                              'example_covariates.csv')
+        self.rct = RCT(self.file, [.5, .5], 1)
+
+    def test_hash_int(self):
+        assert (self.rct.file_hash_int ==
+                151671729980354795404869707092356732292)
+
+    def test_seed(self):
+        assert (self.rct.seed == 2705298821)
+
+    def test_sample_size(self):
+        assert (self.rct.sample_size == 100)
+
+    def test_draw_iid(self):
+        assert_array_almost_equal(self.rct.assignment_from_iid.mean(), 0.57)
+        assert_array_almost_equal(self.rct.assignment_from_iid[:10].T,
+                                  [[1, 1, 0, 0, 0, 1, 0, 1, 0, 1]])
+        assert_array_almost_equal(self.rct.assignment_from_iid.mean(), 0.57)
+
+    def test_draw_shuffle(self):
+        assert_array_almost_equal(
+            self.rct.assignment_from_shuffled.mean(), [.5])
+        assert_array_almost_equal(self.rct.assignment_from_shuffled[:10].T,
+                                  [[0, 1, 1, 1, 0, 1, 0, 0, 0, 0]])
+        assert_array_almost_equal(self.rct.assignment_from_shuffled[:10].T,
+                                  [[0, 1, 1, 1, 0, 1, 0, 0, 0, 0]])
+
+    def test_iid_balance(self):
+        assert_array_almost_equal(
+            pvalues_report(self.rct.df, get_assignments_as_positions(
+                self.rct.assignment_from_iid)), [[0.898257, 0.7013, 0.177232]])
+
+    def test_shuffled_balance(self):
+        assert_array_almost_equal(
+            pvalues_report(self.rct.df, get_assignments_as_positions(
+                self.rct.assignment_from_shuffled)),
+            [[0.518609, 0.34273, 0.551186]])
+
+
+class TestKRerandomized(TestCase):
+    def setUp(self):
+        self.file = path.join(path.dirname(__file__), 'test_data',
+                              'example_covariates.csv')
+        self.maha = MahalanobisBalance()
+        self.krerand = KRerandomizedRCT(self.maha, self.file, [.5, .5])
+
+    def test_hash_int(self):
+        assert (self.krerand.file_hash_int ==
+                151671729980354795404869707092356732292)
+
+    def test_seed(self):
+        assert (self.krerand.seed == 2705298820)
+
+    def test_k(self):
+        assert self.krerand.k == 100
+
+    def test_iid_krerand(self):
+        assert_array_almost_equal(
+            self.krerand.assignment_from_iid[:10].T,
+            [[1, 1, 1, 1, 0, 0, 0, 1, 0, 0]])
+
+    def test_shuffled_krerand(self):
+        assert_array_almost_equal(
+            self.krerand.assignment_from_shuffled[:10].T,
+            [[0, 0, 1, 1, 0, 1, 1, 1, 1, 0]])
+
+    def test_iid_balance(self):
+        assert_array_almost_equal(
+            pvalues_report(
+                self.krerand.df,
+                get_assignments_as_positions(
+                    self.krerand.assignment_from_iid)),
+            [[0.973345, 0.906685, 0.952433]])
+
+    def test_shuffled_balance(self):
+        assert_array_almost_equal(
+            pvalues_report(
+                self.krerand.df,
+                get_assignments_as_positions(
+                    self.krerand.assignment_from_shuffled)),
+            [[0.719567, 0.895064, 0.842654]])
+
+
+class TestQuantileTargetingRCT(TestCase):
+    def setUp(self):
+        self.file = path.join(path.dirname(__file__),  'test_data',
+                              'example_covariates.csv')
+        self.maha = MahalanobisBalance()
+        self.qt_rct = QuantileTargetingRCT(
+            self.maha, self.file, [.5, .5], .05, num_monte_carlo=100)
+
+    def test_quantile_target(self):
+        assert_array_almost_equal(self.qt_rct.quantile_target, .05)
+
+    def test_hash_int(self):
+        assert (self.qt_rct.file_hash_int ==
+                151671729980354795404869707092356732292)
+
+    def test_seed(self):
+        assert (self.qt_rct.seed == 2705298820)
+
+    def test_k(self):
+        assert self.qt_rct.k == 100
+
+    def test_iid_qt_rct(self):
+        assert_array_almost_equal(
+            self.qt_rct.assignment_from_iid[:10].T,
+            [[0, 1, 0, 1, 0, 1, 0, 1, 1, 1]])
+
+    def test_shuffled_qt_rct(self):
+        assert_array_almost_equal(
+            self.qt_rct.assignment_from_shuffled[:10].T,
+            [[0, 1, 1, 1, 0, 1, 0, 1, 1, 0]])
+
+    def test_iid_balance(self):
+        assert_array_almost_equal(
+            pvalues_report(
+                self.qt_rct.df, self.qt_rct.assignment_from_iid),
+            [[0.87299, 0.741099, 0.842654]])
+
+    def test_shuffled_balance(self):
+        assert_array_almost_equal(
+            pvalues_report(self.qt_rct.df,
+                           self.qt_rct.assignment_from_shuffled),
+            [[0.82268, 0.82947, 0.551186]])
