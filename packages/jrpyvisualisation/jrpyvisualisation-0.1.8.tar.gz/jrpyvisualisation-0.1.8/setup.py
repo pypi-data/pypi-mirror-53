06d3999f39b2a27930b3ba7beea56d7eaf1da352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:02 +0000

    Add package jrpyvisualisation/0.1.8/jrpyvisualisation-0.1.8.tar.gz

---
+++ packages/jrpyvisualisation/jrpyvisualisation-0.1.8.tar.gz/jrpyvisualisation-0.1.8/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['jrpyvisualisation', 'jrpyvisualisation.datasets']
+
+package_data = \
+{'': ['*'],
+ 'jrpyvisualisation': ['vignettes/*'],
+ 'jrpyvisualisation.datasets': ['data/*']}
+
+install_requires = \
+['numpy>=1.17,<2.0',
+ 'pandas>=0.25.0,<0.26.0',
+ 'plotly>=4.1,<5.0',
+ 'scikit-learn>=0.21.3,<0.22.0',
+ 'sklearn>=0.0.0,<0.0.1',
+ 'statsmodels>=0.10.1,<0.11.0']
+
+setup_kwargs = {
+    'name': 'jrpyvisualisation',
+    'version': '0.1.8',
+    'description': '',
+    'long_description': None,
+    'author': 'Jamie',
+    'author_email': 'jamie@jumpingrivers.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
