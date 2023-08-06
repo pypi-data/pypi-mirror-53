6650ec032e445d943172b8031518bd1c5c50bc4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:29 +0000

    Add package importFromParent/0.0.3/importFromParent-0.0.3.tar.gz

---
+++ packages/importFromParent/importFromParent-0.0.3.tar.gz/importFromParent-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="importFromParent",
+    version=input('Type the version (to upload = "0.0.3"): '),
+    author="Cauã S. C. P.",
+    author_email="cauascp37@gmail.com",
+    description="import a package or a module from a py file's parent.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.youtube.com/channel/UCAbwAklWIeuoKKjVBMQVCew",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
