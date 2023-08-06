0cb3b722090ddf805a96ba9e44e832b32e8e64b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:00 +0000

    Add package outjack/1/outjack-1.tar.gz

---
+++ packages/outjack/outjack-1.tar.gz/outjack-1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name = 'outjack',
+        version = '1',
+        install_requires = ['cython', 'nativecommon'],
+        packages = setuptools.find_packages(),
+        py_modules = [],
+        scripts = [])
