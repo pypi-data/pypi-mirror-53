b28644b10d47f537b749c2b6a15bd8be2900573d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:06 +0000

    Add package cornichon/0.9.1/cornichon-0.9.1-py3-none-any.whl

---
+++ packages/cornichon/cornichon-0.9.1-py3-none-any.whl/cornichon/cornichon.py
@@ -0,0 +1,40 @@
+"""A small Gherkin DSL parser that generates stub code against various test frameworks"""
+import gherkin
+
+
+def Settings(output):
+    """Get the default settings for the output type"""
+    mod = gherkin.Import(output)
+    return mod.Settings()
+
+
+def PrintSettings(settings, level="settings"):
+    """Utility that prints all the given settings"""
+    for key in settings:
+        try:
+            if type(settings[key]) is str:
+                print('{}["{}"] = "{}"'.format(level, key, settings[key]))
+            else:
+                PrintSettings(settings[key], '{}["{}"]'.format(level, key))
+        except TypeError:
+            pass
+
+
+def HelpSettings(output):
+    """Utility that prints all the help for individual settings"""
+    mod = gherkin.Import(output)
+    settings = mod.HelpSettings()
+    PrintSettings(settings)
+
+
+def ListModules():
+    """Utility that lists all the output types available to generate stub code"""
+    for mod in gherkin.ListModules():
+        print(mod)
+
+
+def Generate(input, settings, output):
+    """Generate the stub code for the output type"""
+    parsed = gherkin.Parse(input, settings)
+    mod = gherkin.Import(output)
+    return mod.Generate(parsed, settings)
---
+++ packages/cornichon/cornichon-0.9.1-py3-none-any.whl/cornichon/cpp/cpputils.py
@@ -0,0 +1,126 @@
+import common
+import gherkin
+
+
+def Settings():
+    settings = common.Settings()
+    settings["rootnamespace"] = "Cornichon::"
+    settings["cases"]["class"] = "Camel"
+    settings["cases"]["namespace"] = "Camel"
+    settings["cases"]["param"] = "camel"
+    settings["cases"]["step"] = "Camel"
+    settings["types"]["bool"] = "bool {}"
+    settings["types"]["int"] = "int {}"
+    settings["types"]["uint"] = "unsigned int {}"
+    settings["types"]["float"] = "double {}"
+    settings["types"]["string"] = "const std::string& {}"
+    return settings
+
+
+def HelpSettings():
+    settings = common.HelpSettings()
+    settings["rootnamespace"] = "The concatenated C++ 17 namespace ending in ::"
+    settings["cases"]["namespace"] = settings["cases"]["class"]
+    return settings
+
+
+def Macro():
+    settings = {}
+    for type in ["bool", "int", "uint", "float", "string"]:
+        settings[type] = "_{}"
+    return settings
+
+
+def ArgModifier(val, type):
+    if type == "bool":
+        return common.Lower(val)
+    if type == "string":
+        return val.replace('"', '\\"')
+    return val
+
+
+def PrintScenario(className, scenario, arguments, steps, settings, indent):
+    buffer = """
+[[indent]]static void [[scenario]]([[arguments]])
+[[indent]]{
+[[indent]]  Scenarios::[[className]] scenario;
+[[steps]]
+[[indent]]}
+"""[1:]
+    buffer = buffer.replace("[[indent]]", indent)
+    buffer = buffer.replace("[[scenario]]", scenario)
+    buffer = buffer.replace("[[className]]", className)
+    buffer = buffer.replace("[[arguments]]", arguments)
+
+    concat = ""
+    for step in steps:
+        concat += step + "\n"
+    buffer = buffer.replace("[[steps]]", concat.rstrip())
+    return buffer
+
+
+def FeatureName(feature, case):
+    lines = feature.split('\n')
+    camelCase = common.Tokenise(lines[0], case)
+    return camelCase
+
+
+def Scenarios(scenarios, settings, indent):
+    concat = ""
+    # parse the scenarios
+    for s in scenarios:
+        fullArgs = s.examples.ArgumentsList(settings["types"])
+        steps = []
+        for step in s.Steps():
+            lines = step[1].split('\n')
+            st = gherkin.Step(step[0], step[1])
+            camelCase = st.Tokenise(settings["cases"]["step"])
+            arguments = st.ParameterList(s.examples.types)
+            buffer = '[[indent]]  scenario.[[camelCase]]([[arguments]]);'
+            buffer = buffer.replace("[[indent]]", indent)
+            buffer = buffer.replace("[[camelCase]]", camelCase)
+            buffer = buffer.replace("[[arguments]]", arguments)
+            steps.append(buffer)
+            continue
+        lines = s.lines.split('\n')
+        className = common.Tokenise(lines[0], settings["cases"]["class"])
+        scenarioName = common.Tokenise(lines[0], settings["cases"]["scenario"])
+        concat += PrintScenario(className, scenarioName, fullArgs, steps, settings, indent)
+        concat += "\n"
+    return concat.rstrip()
+
+
+def ScenarioInsts(scenarios, settings, stub, indent):
+    concat = ""
+    # parse the sections
+    for s in scenarios:
+        lines = s.lines.split('\n')
+        scenario = common.Tokenise(lines[0], settings["cases"]["scenario"])
+        if s.examples.Exists():
+            lines = s.examples.lines.split('\n')
+            for line in lines[2:]:
+                arguments = s.examples.ArgumentsInstance(settings["values"], line, ArgModifier)
+                if "" == arguments:
+                    continue
+                buffer = """
+[[indent]][[stub]][[testName]])
+[[indent]]{
+[[indent]]  [[scenario]]([[arguments]]);
+[[indent]]}
+"""
+                testName = " ".join([scenario, arguments])
+                testName = common.Tokenise(testName, settings["cases"]["test"])
+                buffer = buffer.replace("[[testName]]", testName)
+                buffer = buffer.replace("[[stub]]", stub)
+                buffer = buffer.replace("[[indent]]", indent)
+                buffer = buffer.replace("[[scenario]]", scenario)
+                buffer = buffer.replace("[[arguments]]", arguments)
+                concat += buffer
+        else:
+            buffer = """
+[[indent]][[scenario]]Inst();
+"""
+            buffer = buffer.replace("[[indent]]", indent)
+            buffer = buffer.replace("[[scenario]]", scenario)
+            concat += buffer
+    return concat.rstrip()
