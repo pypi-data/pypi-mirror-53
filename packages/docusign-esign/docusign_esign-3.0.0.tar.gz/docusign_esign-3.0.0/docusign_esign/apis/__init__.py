c93b45b305a66b5ab9b1b982aacde443f5c479fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:29 +0000

    Add package docusign-esign/3.0.0/docusign_esign-3.0.0.tar.gz

---
+++ packages/docusign-esign/docusign_esign-3.0.0.tar.gz/docusign_esign-3.0.0/docusign_esign/apis/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .accounts_api import AccountsApi
+from .billing_api import BillingApi
+from .bulk_envelopes_api import BulkEnvelopesApi
+from .cloud_storage_api import CloudStorageApi
+from .connect_api import ConnectApi
+from .custom_tabs_api import CustomTabsApi
+from .data_feed_api import DataFeedApi
+from .diagnostics_api import DiagnosticsApi
+from .email_archive_api import EmailArchiveApi
+from .envelopes_api import EnvelopesApi
+from .folders_api import FoldersApi
+from .groups_api import GroupsApi
+from .notary_api import NotaryApi
+from .power_forms_api import PowerFormsApi
+from .signing_groups_api import SigningGroupsApi
+from .templates_api import TemplatesApi
+from .trust_service_providers_api import TrustServiceProvidersApi
+from .users_api import UsersApi
+from .workspaces_api import WorkspacesApi
