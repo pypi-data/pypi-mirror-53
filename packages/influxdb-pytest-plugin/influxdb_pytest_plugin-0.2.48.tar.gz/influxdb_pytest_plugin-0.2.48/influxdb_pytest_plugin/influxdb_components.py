1aa204d68f1eb2a8e30b82c5c79ca11a29e6d621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:40 +0000

    Add package influxdb-pytest-plugin/0.2.48/influxdb_pytest_plugin-0.2.48.tar.gz

---
+++ packages/influxdb-pytest-plugin/influxdb_pytest_plugin-0.2.48.tar.gz/influxdb_pytest_plugin-0.2.48/influxdb_pytest_plugin/influxdb_components.py
@@ -0,0 +1,11 @@
+from influxdb import InfluxDBClient
+
+
+class Influxdb_Components:
+    __client = None
+
+    def __init__(self, host, port, db_user, db_password, db_name):
+        self.__client = InfluxDBClient(host, port, db_user, db_password, db_name)
+
+    def get_client(self):
+        return self.__client
