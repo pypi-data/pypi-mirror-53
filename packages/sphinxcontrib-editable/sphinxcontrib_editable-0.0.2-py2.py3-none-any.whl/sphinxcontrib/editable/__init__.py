6d1e4ddd8b94c99565ebb7d3819957fd77de4127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:58 +0000

    Add package sphinxcontrib-editable/0.0.2/sphinxcontrib_editable-0.0.2-py2.py3-none-any.whl

---
+++ packages/sphinxcontrib-editable/sphinxcontrib_editable-0.0.2-py2.py3-none-any.whl/sphinxcontrib/editable/__init__.py
@@ -0,0 +1,38 @@
+"""
+    sphinxcontrib.editable
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    In-line editable documentation
+
+    :copyright: Copyright 2017 by Metatooling contributors <metatooling@users.noreply.github.com>
+    :license: BSD, see LICENSE for details.
+"""
+
+
+import pbr.version
+
+if False:
+    # For type annotations
+    from typing import Any, Dict  # noqa
+    from sphinx.application import Sphinx  # noqa
+
+__version__ = pbr.version.VersionInfo("editable").version_string()
+
+
+def setup(app):
+    # type: (Sphinx) -> Dict[unicode, Any]
+    app.add_js_file("js/editable.js")
+    app.add_js_file("js/medium-editor.js")
+    app.add_css_file("css/medium-editor.css")
+    themes = [
+        "beagle.css",
+        "bootstrap.css",
+        "default.css",
+        "flat.css",
+        "mani.css",
+        "roman.css",
+        "tim.css",
+    ]
+    for theme in themes:
+        app.add_css_file("css/" + theme)
+    return {"version": __version__, "parallel_read_safe": True}
