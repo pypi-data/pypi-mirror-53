3a8a21686cddf64be4223916524f08d86ada621c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:45 +0000

    Add package contenttypes.basic/1.2/contenttypes.basic-1.2-py3-none-any.whl

---
+++ packages/contenttypes.basic/contenttypes.basic-1.2-py3-none-any.whl/contenttypes/basic/permissions.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+addAnnouncement = 'contenttypes.basic: Add Announcement'
+addBanner = 'contenttypes.basic: Add Banner'
+addIcon = 'contenttypes.basic: Add Icon'
+addSlide = 'contenttypes.basic: Add Slide'
