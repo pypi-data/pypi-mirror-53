61c1480a6c890684c57dd6722d8ac136805153f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:13 +0000

    Add package motivate1234/0.0.3/motivate1234-0.0.3.tar.gz

---
+++ packages/motivate1234/motivate1234-0.0.3.tar.gz/motivate1234-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+__project__ = "motivate1234"
+__version__ = "0.0.3"
+__description__ = "this is a new version"
+__packages__ = ["motivate1234"]
+
+setup(
+	name = __project__,
+	version = __version__,
+	description = __description__,
+	packages = __packages__,
+	)
+
