c7a7ca8c986a00f0a2df82b7041966ff355a2861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:08 +0000

    Add package atomtools/1.8.4/atomtools-1.8.4.tar.gz

---
+++ packages/atomtools/atomtools-1.8.4.tar.gz/atomtools-1.8.4/atomtools/__init__.py
@@ -0,0 +1,14 @@
+"""
+independent chemical symbols
+"""
+
+
+__version__ = '1.8.4'
+def version():
+    return __version__
+
+# from . import name, unit, geo
+# from . import file, string, system
+# from . import types
+# from .filetype import filetype, list_supported_formats, support_multiframe
+# from .status import Status
