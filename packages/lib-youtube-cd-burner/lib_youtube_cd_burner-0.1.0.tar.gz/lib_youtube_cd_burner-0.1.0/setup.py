31905c85e476dc13e6e3eee23cc03571f9600f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:27 +0000

    Add package lib-youtube-cd-burner/0.1.0/lib_youtube_cd_burner-0.1.0.tar.gz

---
+++ packages/lib-youtube-cd-burner/lib_youtube_cd_burner-0.1.0.tar.gz/lib_youtube_cd_burner-0.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lib_youtube_cd_burner',
+    packages=find_packages(),
+    version='0.1.00',
+    author='Justin Furuness',
+    author_email='jfuruness@gmail.com',
+    url='https://github.com/jfuruness/lib_youtube_cd_burner.git',
+    download_url='https://github.com/jfuruness/lib_youtube_cd_burner.git',
+    keywords=['Furuness', 'cd', 'burner', 'youtube', 'audio', 'audio cd'],
+    install_requires=[
+        'setuptools',
+        'setuptools>=40.8.0'
+        'youtube_dl>=2019.1.17'
+        'Flask>=1.1.1'
+        'WTForms>=2.2.1'
+        'Flask_WTF>=0.14.2'
+        'pydub>=0.23.1'
+        'soundfile>=0.10.2'
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3'],
+    entry_points={
+        'console_scripts': [
+            'youtube_cd_burner = lib_youtube_cd_burner.__main__:main'
+        ]},
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
