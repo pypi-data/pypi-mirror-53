99d2e789a04435285d2c925664ee539c474d8862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:19 +0000

    Add package pytest-sanic/1.1.0/pytest-sanic-1.1.0.tar.gz

---
+++ packages/pytest-sanic/pytest-sanic-1.1.0.tar.gz/pytest-sanic-1.1.0/tests/test_simple_fixtures.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+def test_fixture_unused_port(unused_port):
+    assert unused_port > 1024 and unused_port < 65535
+
+
+class MyEventLoop:
+
+    def close(self):
+        pass
+
+
+@pytest.fixture
+def loop():
+    loop = MyEventLoop()
+    yield loop
+    loop.close()
+
+
+def test_loop_override(loop):
+    assert isinstance(loop, MyEventLoop) is True
