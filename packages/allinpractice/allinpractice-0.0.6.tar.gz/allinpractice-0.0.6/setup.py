e2eed54c6005fefc2c49578b88b99291bdd1b7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:11 +0000

    Add package allinpractice/0.0.6/allinpractice-0.0.6.tar.gz

---
+++ packages/allinpractice/allinpractice-0.0.6.tar.gz/allinpractice-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+VERSION = "0.0.6"
+
+setuptools.setup(
+    name="allinpractice",
+    packages=setuptools.find_packages(),
+    version=VERSION,
+    description="All-Inspiration Practice Back-end",
+    author="Hugo Wainwright",
+    author_email="wainwrighthugo@gmail.com",
+    url="https://github.com/frugs/allin-practice-backend",
+    keywords=["sc2", "all-inspiration"],
+    classifiers=[],
+    install_requires=[
+        "Flask-OAuthlib",
+        "flask",
+        "google-cloud-datastore",
+        "allinsso",
+        "firebase-admin",
+        "python-dateutil",
+    ],
+)
