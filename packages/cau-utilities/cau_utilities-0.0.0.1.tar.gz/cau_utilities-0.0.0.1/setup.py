a4c7303bd3365bcea18572a7f7a972fc51909b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:16 +0000

    Add package cau-utilities/0.0.0.1/cau_utilities-0.0.0.1.tar.gz

---
+++ packages/cau-utilities/cau_utilities-0.0.0.1.tar.gz/cau_utilities-0.0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cau_utilities",
+    version="0.0.0.1",
+    author="Cauã S. C. P.",
+    author_email="cauascp37@gmail.com",
+    description="this project has a lot of utilities made by Caua_SCP.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.youtube.com/channel/UCAbwAklWIeuoKKjVBMQVCew",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+    ,
+    install_requires=[
+        'importFromParent',
+    ]
+    ,
+    zip_safe=False
+)
