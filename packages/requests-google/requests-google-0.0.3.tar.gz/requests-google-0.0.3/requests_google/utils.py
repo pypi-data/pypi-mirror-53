db50876062c741bd5e6977e93d6ddacf45956aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:29 +0000

    Add package requests-google/0.0.3/requests-google-0.0.3.tar.gz

---
+++ packages/requests-google/requests-google-0.0.3.tar.gz/requests-google-0.0.3/requests_google/utils.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+requests_google.utils
+"""
+
+import urllib
+
+# Reference: 
+# https://stackoverflow.com/questions/51537063/url-format-for-google-news-rss-feed
+URL_LOCATION = 'https://news.google.com/news/rss/headlines/section/geo/{location}'
+
+
+def get_location_url(city, state):
+    """Generate url for the given location"""
+    location = '{}, {}, {}'.format(city, state, 'United States')
+    return URL_LOCATION.format(location=urllib.quote_plus(location))
