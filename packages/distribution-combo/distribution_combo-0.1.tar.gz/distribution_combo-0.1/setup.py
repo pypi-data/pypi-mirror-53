d51b0e6723dd1e113b37eead6c40016f14337c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:32 +0000

    Add package distribution-combo/0.1/distribution_combo-0.1.tar.gz

---
+++ packages/distribution-combo/distribution_combo-0.1.tar.gz/distribution_combo-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_combo',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distribution_combo'],
+      zip_safe=False)
