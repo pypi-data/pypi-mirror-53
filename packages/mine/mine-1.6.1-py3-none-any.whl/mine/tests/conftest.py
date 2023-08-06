376a43652c1f7067e4d91d566adae7acc93135de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:43 +0000

    Add package mine/1.6.1/mine-1.6.1-py3-none-any.whl

---
+++ packages/mine/mine-1.6.1-py3-none-any.whl/mine/tests/conftest.py
@@ -0,0 +1,44 @@
+"""Unit test configuration file."""
+
+import os
+import platform
+
+import log
+import pytest
+
+
+ENV = 'TEST_INTEGRATION'  # environment variable to enable integration tests
+REASON = "'{0}' variable not set".format(ENV)
+
+ROOT = os.path.dirname(__file__)
+FILES = os.path.join(ROOT, 'files')
+
+
+def pytest_configure(config):
+    """Disable verbose output when running tests."""
+    log.init(
+        level=log.DEBUG, format="[%(levelname)-8s] (%(name)s @%(lineno)4d) %(message)s"
+    )
+    log.silence('yorm', allow_warning=True)
+
+    terminal = config.pluginmanager.getplugin('terminal')
+
+    class QuietReporter(terminal.TerminalReporter):  # type: ignore
+        """A py.test reporting that only shows dots when running tests."""
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.verbosity = 0
+            self.showlongtestinfo = False
+            self.showfspath = False
+
+    terminal.TerminalReporter = QuietReporter
+
+
+def pytest_runtest_setup(item):
+    if 'linux_only' in item.keywords and platform.system() != 'Linux':
+        pytest.skip("test can only be run on Linux")
+    if 'mac_only' in item.keywords and platform.system() != 'Darwin':
+        pytest.skip("test can only be run on OS X")
+    if 'windows_only' in item.keywords and platform.system() != 'Windows':
+        pytest.skip("test can only be run on Windows")
