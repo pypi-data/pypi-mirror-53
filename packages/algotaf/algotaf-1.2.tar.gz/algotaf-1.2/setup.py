694efba09cf08a2809132d47c8891efe0b8edf02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:52 +0000

    Add package algotaf/1.2/algotaf-1.2.tar.gz

---
+++ packages/algotaf/algotaf-1.2.tar.gz/algotaf-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='algotaf',
+	  version='1.2',
+	  url='https://github.com/ramasrirama99/AlgoTradeFramework',
+	  license='MIT',
+	  install_requires=[
+	  	'psycopg2',
+	  	'pgcopy',
+	  	'requests',
+	  	'schedule'
+	  ],
+	  packages=find_packages())
