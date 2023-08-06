da6d45b2e408f4352e6030005491536537786e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:26 +0000

    Add package social-ethosa/0.2.52/social_ethosa-0.2.52-py3-none-any.whl

---
+++ packages/social-ethosa/social_ethosa-0.2.52-py3-none-any.whl/social_ethosa/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# author: ethosa
+
+from .vkcom import *
+from .botwrapper import *
+from .otherUtils import *
+from .jdoodlecom import *
+from .tracemoe import *
+from .tpdne import *
+from .yandex import *
+from .yummyanimeclub import *
