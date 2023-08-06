e7061099dc7366163a1b2ea898bff5df806703a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:34 +0000

    Add package certbot-dns-alidomain/0.1.0.dev0/certbot-dns-alidomain-0.1.0.dev0.tar.gz

---
+++ packages/certbot-dns-alidomain/certbot-dns-alidomain-0.1.0.dev0.tar.gz/certbot-dns-alidomain-0.1.0.dev0/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+version = '0.1.0.dev0'
+
+# Remember to update local-oldest-requirements.txt when changing the minimum
+# acme/certbot version.
+install_requires = [
+    'acme>=0.21.1',
+    'certbot>=0.21.1',
+    'aliyun-python-sdk-core>=2.6.0',
+    'aliyun-python-sdk-alidns>=2.0.7',
+    'setuptools',
+    'zope.interface',
+]
+
+setup(
+    name='certbot-dns-alidomain',
+    version=version,
+    description="Aliyun DNS Authenticator plugin for Certbot",
+    url='https://github.com/ant-sir/certbot-dns-aliyun',
+    author="zhu.yanlei",
+    author_email='zhuyanleigm@gmail.com',
+    license='Apache License 2.0',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+
+    entry_points={
+        'certbot.plugins': [
+            'dns-aliyun = certbot_dns_aliyun.dns_aliyun:Authenticator',
+        ],
+    },
+)
