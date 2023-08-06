2bfe9e0d2a4b4d172cd599d5f9cf1840395d82a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:07 +0000

    Add package lego-mp-extension/0.1.8/lego_mp_extension-0.1.8-py3-none-any.whl

---
+++ packages/lego-mp-extension/lego_mp_extension-0.1.8-py3-none-any.whl/pybricks_ext/tools_ext.py
@@ -0,0 +1,27 @@
+import sys
+
+import pybricks
+from pybricks.tools import StopWatch
+
+def print(*value, sep=' ', end='\n', file=sys.stdout, flush=False):
+    pybricks.tools.print(value, sep=sep, end=end, file=file, flush=flush)
+
+def wait(time):
+    pybricks.tools.wait(time)
+
+class StopWatchExt(StopWatch):
+    """
+    Extension class for the StopWatch Object
+    """
+
+    def wait_until_time_passes(self, time):
+        """Waits until the time counter passes a specified value.
+        If the StopWatch is paused, it will be resumed
+
+        :param time: Time value to pass before continuing
+        :type time: int
+        """
+        super(StopWatchExt, self).resume()
+        while super(StopWatchExt, self).time() < time:
+            wait(10)
+        return
