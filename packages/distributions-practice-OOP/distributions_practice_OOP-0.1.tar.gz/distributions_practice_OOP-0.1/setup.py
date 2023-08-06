8f8cea081341db57bab056c5dd277a1550b303f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:43 +0000

    Add package distributions-practice-OOP/0.1/distributions_practice_OOP-0.1.tar.gz

---
+++ packages/distributions-practice-OOP/distributions_practice_OOP-0.1.tar.gz/distributions_practice_OOP-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_practice_OOP',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_practice_OOP'],
+      zip_safe=False)
