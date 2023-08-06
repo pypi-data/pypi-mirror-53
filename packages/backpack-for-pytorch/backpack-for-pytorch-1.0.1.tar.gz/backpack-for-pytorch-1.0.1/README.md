4b5053c0afe41baa5b50a502fa72d7c7cdc7f767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:00 +0000

    Add package backpack-for-pytorch/1.0.1/backpack-for-pytorch-1.0.1.tar.gz

---
+++ packages/backpack-for-pytorch/backpack-for-pytorch-1.0.1.tar.gz/backpack-for-pytorch-1.0.1/README.md
@@ -0,0 +1,17 @@
+# BACKpropagation PACKage - a backpack for `PyTorch`
+
+A backpack for PyTorch that extends the backward pass of feedforward networks to compute quantities beyond the gradient.
+
+Check out the [examples](https://f-dangel.github.io/backpack/) on how to use the code.
+
+## Installation
+```bash
+pip install backpack-for-pytorch`
+```
+
+Alternative: Clone the repository and run `setup.py`
+```bash
+git clone https://github.com/f-dangel/backpack.git ~/backpack
+cd ~/backpack
+python setup.py install
+```
---
+++ packages/backpack-for-pytorch/backpack-for-pytorch-1.0.1.tar.gz/backpack-for-pytorch-1.0.1/setup.py
@@ -0,0 +1,37 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+# META
+##############################################################################
+AUTHORS = "F. Dangel, F. Kunstner"
+NAME = "backpack-for-pytorch"
+PACKAGES = find_packages()
+
+DESCRIPTION = r"""BACKpropagation PACKage - A backpack for PyTorch to compute quantities beyond the gradient."""
+LONG_DESCR = "https://github.com/f-dangel/backpack"
+
+VERSION = "1.0.1"
+URL = "https://github.com/f-dangel/backpack"
+LICENSE = "MIT"
+
+# DEPENDENCIES
+##############################################################################
+REQUIREMENTS_FILE = "requirements.txt"
+REQUIREMENTS_PATH = path.join(path.abspath(__file__), REQUIREMENTS_FILE)
+
+with open(REQUIREMENTS_FILE) as f:
+    requirements = f.read().splitlines()
+
+setup(author=AUTHORS,
+      name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCR,
+      long_description_content_type="text/markdown",
+      install_requires=requirements,
+      url=URL,
+      license=LICENSE,
+      packages=PACKAGES,
+      zip_safe=False,
+      python_requires='>=3.5')
