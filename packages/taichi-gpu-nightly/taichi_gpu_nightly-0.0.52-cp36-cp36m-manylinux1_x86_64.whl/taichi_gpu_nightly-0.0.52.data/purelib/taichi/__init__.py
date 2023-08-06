84d2ef5d496de9f7b5ef7b07aec5651158c77043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:30 +0000

    Add package taichi-gpu-nightly/0.0.52/taichi_gpu_nightly-0.0.52-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/taichi-gpu-nightly/taichi_gpu_nightly-0.0.52-cp36-cp36m-manylinux1_x86_64.whl/taichi_gpu_nightly-0.0.52.data/purelib/taichi/__init__.py
@@ -0,0 +1,19 @@
+from taichi.main import main
+from taichi.core import tc_core
+from taichi.core import start_memory_monitoring
+from taichi.misc.util import vec, veci, set_gdb_trigger, set_logging_level
+from taichi.tools import *
+from taichi.misc import *
+from taichi.misc.task import Task
+from taichi.misc import settings as settings
+from taichi.misc.settings import *
+from taichi.tools.video import VideoManager
+from taichi.tools.file import *
+from taichi.system import *
+from taichi.lang import *
+
+def test():
+  task = taichi.Task('test')
+  return task.run([])
+
+__all__ = [s for s in dir() if not s.startswith('_')] + ['settings']
