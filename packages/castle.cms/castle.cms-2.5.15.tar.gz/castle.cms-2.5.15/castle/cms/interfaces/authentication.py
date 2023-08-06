aee05465f23db61f114c278f4e697a4002414199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:24 +0000

    Add package castle.cms/2.5.15/castle.cms-2.5.15.tar.gz

---
+++ packages/castle.cms/castle.cms-2.5.15.tar.gz/castle.cms-2.5.15/castle/cms/interfaces/authentication.py
@@ -0,0 +1,31 @@
+from zope.interface import Interface
+
+
+class IAuthenticator(Interface):
+
+    def __init__(context, request):
+        pass
+
+    def change_password(member, new_password):
+        pass
+
+    def issue_country_exception(user, country):
+        pass
+
+    def login(user):
+        pass
+
+    def authenticate(username=None, password=None, country=None, login=True):
+        pass
+
+    def issue_2factor_code(username):
+        pass
+
+    def authorize_2factor(username, code):
+        pass
+
+    def get_supported_auth_schemes():
+        pass
+
+    def get_options():
+        pass
