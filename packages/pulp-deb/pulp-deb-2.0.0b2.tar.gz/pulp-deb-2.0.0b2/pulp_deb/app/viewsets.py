4e1dc83d47a4568af88b98784f1a4a853c261b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:11 +0000

    Add package pulp-deb/2.0.0b2/pulp-deb-2.0.0b2.tar.gz

---
+++ packages/pulp-deb/pulp-deb-2.0.0b2.tar.gz/pulp-deb-2.0.0b2/pulp_deb/app/viewsets.py
@@ -0,0 +1,239 @@
+from gettext import gettext as _  # noqa
+
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import action
+
+from pulpcore.plugin.serializers import (
+    AsyncOperationResponseSerializer,
+    RepositorySyncURLSerializer,
+)
+from pulpcore.plugin.tasking import enqueue_with_reservation
+from pulpcore.plugin.viewsets import (
+    BaseDistributionViewSet,
+    ContentViewSet,
+    ContentFilter,
+    RemoteViewSet,
+    OperationPostponedResponse,
+    PublicationViewSet,
+    SingleArtifactContentUploadViewSet,
+)
+
+from . import models, serializers, tasks
+
+
+class GenericContentFilter(ContentFilter):
+    """
+    FilterSet for GenericContent.
+    """
+
+    class Meta:
+        model = models.GenericContent
+        fields = ["relative_path"]
+
+
+class ReleaseFilter(ContentFilter):
+    """
+    FilterSet for Release.
+    """
+
+    class Meta:
+        model = models.Release
+        fields = ["codename", "suite", "relative_path"]
+
+
+class PackageFilter(ContentFilter):
+    """
+    FilterSet for Package.
+    """
+
+    class Meta:
+        model = models.Package
+        fields = ["relative_path"]
+
+
+class InstallerPackageFilter(ContentFilter):
+    """
+    FilterSet for InstallerPackage.
+    """
+
+    class Meta:
+        model = models.InstallerPackage
+        fields = ["relative_path"]
+
+
+class GenericContentViewSet(SingleArtifactContentUploadViewSet):
+    """
+    A ViewSet for GenericContent.
+    """
+
+    endpoint_name = "generic_contents"
+    queryset = models.GenericContent.objects.prefetch_related("_artifacts")
+    serializer_class = serializers.GenericContentSerializer
+    filter_set_class = GenericContentFilter
+
+
+class ReleaseViewSet(ContentViewSet):
+    """
+    A ViewSet for Release.
+    """
+
+    endpoint_name = "releases"
+    queryset = models.Release.objects.all()
+    serializer_class = serializers.ReleaseSerializer
+
+
+class PackageIndexViewSet(ContentViewSet):
+    """
+    A ViewSet for PackageIndex.
+    """
+
+    endpoint_name = "package_index"
+    queryset = models.PackageIndex.objects.all()
+    serializer_class = serializers.PackageIndexSerializer
+
+
+class InstallerFileIndexViewSet(ContentViewSet):
+    """
+    A ViewSet for InstallerFileIndex.
+    """
+
+    endpoint_name = "installer_file_index"
+    queryset = models.InstallerFileIndex.objects.all()
+    serializer_class = serializers.InstallerFileIndexSerializer
+
+
+class PackageViewSet(SingleArtifactContentUploadViewSet):
+    """
+    A ViewSet for Package.
+    """
+
+    endpoint_name = "packages"
+    queryset = models.Package.objects.prefetch_related("_artifacts")
+    serializer_class = serializers.PackageSerializer
+    filter_set_class = PackageFilter
+
+
+class InstallerPackageViewSet(SingleArtifactContentUploadViewSet):
+    """
+    A ViewSet for InstallerPackage.
+    """
+
+    endpoint_name = "installer_packages"
+    queryset = models.InstallerPackage.objects.prefetch_related("_artifacts")
+    serializer_class = serializers.InstallerPackageSerializer
+    filter_set_class = InstallerPackageFilter
+
+
+class DebRemoteViewSet(RemoteViewSet):
+    """
+    A ViewSet for DebRemote.
+    """
+
+    endpoint_name = "apt"
+    queryset = models.DebRemote.objects.all()
+    serializer_class = serializers.DebRemoteSerializer
+
+    @swagger_auto_schema(
+        operation_description="Trigger an asynchronous task to sync content",
+        responses={202: AsyncOperationResponseSerializer},
+    )
+    @action(detail=True, methods=["post"], serializer_class=RepositorySyncURLSerializer)
+    def sync(self, request, pk):
+        """
+        Synchronizes a repository.
+
+        The ``repository`` field has to be provided.
+        """
+        remote = self.get_object()
+        serializer = RepositorySyncURLSerializer(data=request.data, context={"request": request})
+
+        # Validate synchronously to return 400 errors.
+        serializer.is_valid(raise_exception=True)
+        repository = serializer.validated_data.get("repository")
+        mirror = serializer.validated_data.get("mirror", True)
+        result = enqueue_with_reservation(
+            tasks.synchronize,
+            [repository, remote],
+            kwargs={"remote_pk": remote.pk, "repository_pk": repository.pk, "mirror": mirror},
+        )
+        return OperationPostponedResponse(result, request)
+
+
+class VerbatimPublicationViewSet(PublicationViewSet):
+    """
+    A ViewSet for VerbatimPublication.
+    """
+
+    endpoint_name = "verbatim"
+    queryset = models.VerbatimPublication.objects.exclude(complete=False)
+    serializer_class = serializers.VerbatimPublicationSerializer
+
+    @swagger_auto_schema(
+        operation_description="Trigger an asynchronous task to publish content",
+        responses={202: AsyncOperationResponseSerializer},
+    )
+    def create(self, request):
+        """
+        Publishes a repository.
+
+        Either the ``repository`` or the ``repository_version`` fields can
+        be provided but not both at the same time.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        repository_version = serializer.validated_data.get("repository_version")
+
+        result = enqueue_with_reservation(
+            tasks.publish_verbatim,
+            [repository_version.repository],
+            kwargs={"repository_version_pk": str(repository_version.pk)},
+        )
+        return OperationPostponedResponse(result, request)
+
+
+class DebPublicationViewSet(PublicationViewSet):
+    """
+    A ViewSet for DebPublication.
+    """
+
+    endpoint_name = "apt"
+    queryset = models.DebPublication.objects.exclude(complete=False)
+    serializer_class = serializers.DebPublicationSerializer
+
+    @swagger_auto_schema(
+        operation_description="Trigger an asynchronous task to publish content",
+        responses={202: AsyncOperationResponseSerializer},
+    )
+    def create(self, request):
+        """
+        Publishes a repository.
+
+        Either the ``repository`` or the ``repository_version`` fields can
+        be provided but not both at the same time.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        repository_version = serializer.validated_data.get("repository_version")
+        simple = serializer.validated_data.get("simple")
+        structured = serializer.validated_data.get("structured")
+
+        result = enqueue_with_reservation(
+            tasks.publish,
+            [repository_version.repository],
+            kwargs={
+                "repository_version_pk": str(repository_version.pk),
+                "simple": simple,
+                "structured": structured,
+            },
+        )
+        return OperationPostponedResponse(result, request)
+
+
+class DebDistributionViewSet(BaseDistributionViewSet):
+    """
+    ViewSet for DebDistributions.
+    """
+
+    endpoint_name = "apt"
+    queryset = models.DebDistribution.objects.all()
+    serializer_class = serializers.DebDistributionSerializer
---
+++ packages/pulp-deb/pulp-deb-2.0.0b2.tar.gz/pulp-deb-2.0.0b2/pulp_deb/tests/functional/constants.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+from urllib.parse import urljoin
+
+from pulp_smash.constants import PULP_FIXTURES_BASE_URL
+from pulp_smash.pulp3.constants import (
+    # API_DOCS_PATH,
+    BASE_DISTRIBUTION_PATH,
+    BASE_PATH,
+    BASE_PUBLICATION_PATH,
+    BASE_REMOTE_PATH,
+    CONTENT_PATH,
+)
+
+DOWNLOAD_POLICIES = ["immediate", "streamed", "on_demand"]
+
+DEB_RELEASE_NAME = "deb.release"
+DEB_PACKAGE_INDEX_NAME = "deb.package_index"
+DEB_PACKAGE_NAME = "deb.package"
+DEB_GENERIC_CONTENT_NAME = "deb.generic"
+
+DEB_PACKAGE_PATH = urljoin(CONTENT_PATH, "deb/packages/")
+DEB_GENERIC_CONTENT_PATH = urljoin(CONTENT_PATH, "deb/generic_contents/")
+
+DEB_DISTRIBUTION_PATH = urljoin(BASE_DISTRIBUTION_PATH, "deb/apt/")
+
+DEB_REMOTE_PATH = urljoin(BASE_REMOTE_PATH, "deb/apt/")
+
+DEB_PUBLICATION_PATH = urljoin(BASE_PUBLICATION_PATH, "deb/apt/")
+VERBATIM_PUBLICATION_PATH = urljoin(BASE_PUBLICATION_PATH, "deb/verbatim/")
+
+DEB_SINGLE_REQUEST_UPLOAD_PATH = urljoin(BASE_PATH, "deb/upload/")
+
+DEB_FIXTURE_URL = urljoin(PULP_FIXTURES_BASE_URL, "debian/")
+DEB_FIXTURE_RELEASE = "ragnarok"
+
+DEB_FIXTURE_RELEASE_COUNT = 1
+DEB_FIXTURE_PACKAGE_INDEX_COUNT = 4
+DEB_FIXTURE_PACKAGE_COUNT = 4
+DEB_FIXTURE_GENERIC_CONTENT_COUNT = 0
+
+DEB_FIXTURE_SUMMARY = {
+    DEB_RELEASE_NAME: DEB_FIXTURE_RELEASE_COUNT,
+    DEB_PACKAGE_INDEX_NAME: DEB_FIXTURE_PACKAGE_INDEX_COUNT,
+    DEB_PACKAGE_NAME: DEB_FIXTURE_PACKAGE_COUNT,
+    # DEB_GENERIC_CONTENT_NAME: DEB_FIXTURE_GENERIC_CONTENT_COUNT,
+}
+
+DEB_PACKAGE_RELPATH = "pool/asgard/o/odin/odin_1.0_ppc64.deb"
+DEB_PACKAGE_URL = urljoin(DEB_FIXTURE_URL, DEB_PACKAGE_RELPATH)
+DEB_GENERIC_CONTENT_RELPATH = "dists/ragnarok/asgard/binary-armeb/Release"
+DEB_GENERIC_CONTENT_URL = urljoin(DEB_FIXTURE_URL, DEB_GENERIC_CONTENT_RELPATH)
+
+# FIXME: replace this with your own fixture repository URL and metadata
+DEB_LARGE_FIXTURE_URL = urljoin(PULP_FIXTURES_BASE_URL, "deb_large/")
+
+# FIXME: replace this with the actual number of content units in your test fixture
+DEB_LARGE_FIXTURE_COUNT = 25
