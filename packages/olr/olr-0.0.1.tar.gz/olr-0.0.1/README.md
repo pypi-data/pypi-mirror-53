170bdd756e06ec295535d5bea7a89b5be7fcf435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:37 +0000

    Add package olr/0.0.1/olr-0.0.1.tar.gz

---
+++ packages/olr/olr-0.0.1.tar.gz/olr-0.0.1/README.md
@@ -0,0 +1,3 @@
+The olr function runs all the possible combinations of linear regressions with all of the
+dependent variables against the independent variable and returns the statistical summary
+of either the greatest adjusted R-squared or R-squared term.
---
+++ packages/olr/olr-0.0.1.tar.gz/olr-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="olr",
+    version="0.0.1",
+    author="Mathew Fok",
+    author_email="mfok@stevens.edu",
+    description="olr: Optimal Linear Regression",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.github.com/MatHatter",
+    packages=setuptools.find_packages(),
+    classifers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License",
+        "Operating System :: OS Independent",
+    ],
+)
+    
