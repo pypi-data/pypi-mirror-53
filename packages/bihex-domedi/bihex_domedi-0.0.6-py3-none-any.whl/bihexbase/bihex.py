97445c9ef3e747fb12b79a22b06ebd5a89123e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:02 +0000

    Add package bihex-domedi/0.0.6/bihex_domedi-0.0.6-py3-none-any.whl

---
+++ packages/bihex-domedi/bihex_domedi-0.0.6-py3-none-any.whl/bihexbase/bihex.py
@@ -0,0 +1,28 @@
+class Bihex:
+	def __init__(self, number):
+		self.number = 0
+		if number[0] == "d":
+			exec("self.number = int(number[1:])")
+			self.base = "dec"
+		if number[0] == "b":
+			exec("self.number = int(" + "'0b'" + "number[1:])")
+			self.base = "bin"
+		if number[0] == "x":
+			exec("self.number = int(" + "'0x'" + "number[1:])")
+			self.base = "hex"
+		if number[0] == "o":
+			exec("self.number = int(" + "'0o'" + "number[1:])")
+			self.base = "oct"
+
+	def returnnum(self, base):
+		if base == "d":
+			return str(self.number)
+		if base == "b":
+			return bin(self.number)
+		if base == "x":
+			return hex(self.number)
+		if base == "o":
+			return oct(self.number)
+
+	def returnbase(self):
+		return self.base
