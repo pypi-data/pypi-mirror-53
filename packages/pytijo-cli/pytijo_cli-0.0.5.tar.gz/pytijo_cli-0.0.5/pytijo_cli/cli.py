0b550ad098d84202f5ef4649b99b2c75115c316a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:49 +0000

    Add package pytijo-cli/0.0.5/pytijo_cli-0.0.5.tar.gz

---
+++ packages/pytijo-cli/pytijo_cli-0.0.5.tar.gz/pytijo_cli-0.0.5/pytijo_cli/cli.py
@@ -0,0 +1,85 @@
+import click
+import sys
+import json
+from subprocess import check_output
+from pytijo import parser
+from pytijo_api_client import command
+
+
+@click.command(
+    name="tijo",
+    context_settings=dict(ignore_unknown_options=True, allow_extra_args=True),
+    help="Tijo command line. Just execute 'tijo <any-command>' and tijo will the most suitable template to parse the output of the given command. The output is printed in json format.",
+)
+@click.option(
+    "--tijo-template", type=click.File(), help="A tijo template file to be used"
+)
+@click.option(
+    "--tijo-output-beauty", is_flag=True, default=False, help="Beautify json output"
+)
+@click.option(
+    "--tijo-base-api",
+    type=click.STRING,
+    default="http://api.tijo.io/v1",
+    envvar="TIJO_API",
+    help="Tijo base REST API endpoint",
+)
+@click.option(
+    "--tijo-insecure",
+    is_flag=True,
+    default=False,
+    help="Allow insecure https connections. SSL certificates are not validated if this option is given.",
+)
+@click.option("--tijo-timeout", type=click.INT, default=60, help="HTTP(s) timeout")
+@click.option(
+    "--tijo-disable-cache",
+    is_flag=True,
+    default=False,
+    help="Disable local cache to force obtaining the template from the Tijo API.",
+)
+@click.pass_context
+def tijo(
+    ctx,
+    tijo_template,
+    tijo_output_beauty,
+    tijo_base_api,
+    tijo_insecure,
+    tijo_timeout,
+    tijo_disable_cache,
+):
+    if len(ctx.args) <= 0 and tijo_template is None:
+        print("--tijo-template is required if a command is not provided")
+        raise click.Abort()
+
+    template = None
+    if tijo_template:
+        template = json.load(tijo_template)
+    else:
+        cmd = command.Command(
+            ctx.args,
+            tijo_api=tijo_base_api,
+            insecure=tijo_insecure,
+            timeout=tijo_timeout,
+        )
+        template = cmd.get_template(disable_cache=tijo_disable_cache)
+        if template is None:
+            print("template not found")
+            raise click.Abort()
+
+    output = None
+    if len(ctx.args) <= 0:
+        output = "".join(sys.stdin)
+    else:
+        output = check_output(ctx.args).decode("utf-8")
+
+    result = parser.parse(output, template)
+    output = json.dumps(result, indent=4) if tijo_output_beauty else result
+    click.echo(output)
+
+
+def main():
+    tijo()
+
+
+if __name__ == "__main__":
+    tijo()
---
+++ packages/pytijo-cli/pytijo_cli-0.0.5.tar.gz/pytijo_cli-0.0.5/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+import os
+import re
+
+if os.environ.get("CI_COMMIT_TAG"):
+    version = os.environ["CI_COMMIT_TAG"]
+    if version.startswith("v"):
+        version = version[1:]
+        if not re.search(r"^\d+\.\d+\.\d+$", version):
+            raise AttributeError(
+                "given CI_COMMIT_TAG {} incorrect format. It must be vX.Y.Z or X.Y.Z format".format(
+                    os.environ["CI_COMMIT_TAG"]
+                )
+            )
+elif os.environ.get("CI_JOB_ID"):
+    version = os.environ["CI_JOB_ID"]
+else:
+    version = None
+
+setup(
+    zip_safe=True,
+    name="pytijo_cli",
+    version=version,
+    author="Jon Castro",
+    author_email="jon@tijo.io",
+    packages=["pytijo_cli"],
+    #    data_files=[(TEMPLATES_DIR, templates)],
+    description="Tijo Command line interface",
+    license="LICENSE",
+    install_requires=["click", "pytijo", "pytijo_api_client"],
+    entry_points={
+        "console_scripts": [
+            "tijo = pytijo_cli.cli:main",
+            "tijo-push = pytijo_cli.push:main",
+        ]
+    },
+)
