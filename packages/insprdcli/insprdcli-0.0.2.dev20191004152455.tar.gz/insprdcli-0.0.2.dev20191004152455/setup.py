c3b926b926c0d135c82d75e6fe4fe432a33eaa79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:12 +0000

    Add package insprdcli/0.0.2.dev20191004152455/insprdcli-0.0.2.dev20191004152455.tar.gz

---
+++ packages/insprdcli/insprdcli-0.0.2.dev20191004152455.tar.gz/insprdcli-0.0.2.dev20191004152455/setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup, find_packages
+from insprdcli.core.version import get_version
+
+VERSION = get_version()
+
+f = open('README.md', 'r')
+LONG_DESCRIPTION = f.read()
+f.close()
+
+setup(
+    name='insprdcli',
+    version=VERSION,
+    description='Command-line interface into Inspired Platform',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Jeff Trudeau',
+    author_email='jeff.trudeau@gmail.com',
+    url='https://github.com/InspiredMember/inspired-cli',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    package_data={'insprdcli': ['templates/*']},
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        insprdcli = insprdcli.main:main
+    """,
+)
