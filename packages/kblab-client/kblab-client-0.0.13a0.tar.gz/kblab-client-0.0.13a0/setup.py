63aa2922152bd5bb70835c91c86c2c056d17f54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:23 +0000

    Add package kblab-client/0.0.13a0/kblab-client-0.0.13a0.tar.gz

---
+++ packages/kblab-client/kblab-client-0.0.13a0.tar.gz/kblab-client-0.0.13a0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='kblab-client',
+    version='0.0.13a0',
+    description='KB lab client',
+    author='Martin Malmsten',
+    author_email='martin.malmsten@kb.se',
+    url="https://github.com/kungbib/kblab",
+    install_requires = [
+        'requests',
+        'pyyaml',
+        'lxml',
+        'htfile'
+    ],
+    packages=[ 'kblab' ]
+)
+
