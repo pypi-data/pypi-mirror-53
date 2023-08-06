11cac4b07c579092a6af5727782c1525e28bb1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:36 +0000

    Add package yandex-checkout/1.5.0/yandex_checkout-1.5.0.tar.gz

---
+++ packages/yandex-checkout/yandex_checkout-1.5.0.tar.gz/yandex_checkout-1.5.0/yandex_checkout/domain/models/payment_data/card_type.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+class CardType:
+    """
+    Class representing credit cards available types enum
+    """
+    MASTER_CARD = 'MasterCard'
+    VISA = 'Visa'
+    MIR = 'MIR'
+    UNION_PAY = 'UnionPay'
+    JCB = 'JCB'
+    AMERICAN_EXPRESS = 'AmericanExpress'
+    UNKNOWN = 'Unknown'
+    DINERS_CLUB = 'DinersClub'
