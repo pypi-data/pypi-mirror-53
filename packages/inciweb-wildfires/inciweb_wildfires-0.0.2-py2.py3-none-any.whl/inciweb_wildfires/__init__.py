7ff03c04595fa3df8d93a05c7a81f01a2650cb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:34 +0000

    Add package inciweb-wildfires/0.0.2/inciweb_wildfires-0.0.2-py2.py3-none-any.whl

---
+++ packages/inciweb-wildfires/inciweb_wildfires-0.0.2-py2.py3-none-any.whl/inciweb_wildfires/__init__.py
@@ -0,0 +1,18 @@
+import requests
+import time
+from datetime import datetime, timezone
+from geojson import Feature, FeatureCollection
+
+
+def get_fires():
+    """
+    Get inciweb data for fires
+    """
+    url = 'https://inciweb.nwcg.gov/feeds/json/esri/'
+
+    r = requests.get(url)
+    content = r.json()['markers']
+
+    collection = FeatureCollection([Feature(geometry={'type': 'Point', 'coordinates': [float(d['lng']), float(d['lat'])]}, fireName=d['name'].split(' Fire')[0], inciweb_id=d['id'], properties=d) for d in content])
+    print(collection)
+    return collection
