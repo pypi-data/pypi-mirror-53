7584f50dd44eb620ab275a0f30398519aa17df68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:10 +0000

    Add package labm8/2019.10.14/labm8-2019.10.14.tar.gz

---
+++ packages/labm8/labm8-2019.10.14.tar.gz/labm8-2019.10.14/bazel-out/darwin-fastbuild/bin/build_info_pbtxt_py.py
@@ -0,0 +1,14 @@
+STRING = """\
+# File: //config.proto
+# Proto: BuildInfo
+seconds_since_epoch: 1571011002
+host: "florence"
+user: "cec"
+repo_dirty: true
+git_commit: "293c9b60717b6b71901077912964d8879e7c38f1"
+git_branch: "wip/graph"
+git_tracking: "priv/wip/graph"
+git_remote_url: "git@github.com:ChrisCummins/phd-priv.git"
+git_commit_author: "Chris Cummins <chrisc.101@gmail.com>"
+git_commit_seconds_since_epoch: 1571010941
+"""
