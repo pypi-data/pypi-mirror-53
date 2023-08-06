5077611e4bba0d94603acc5edb8d15171fdaa35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:14 +0000

    Add package freedompop/1.1.0/freedompop-1.1.0-py2.py3-none-any.whl

---
+++ packages/freedompop/freedompop-1.1.0-py2.py3-none-any.whl/freedompop/__main__.py
@@ -0,0 +1,13 @@
+import pprint
+
+import autocommand
+
+import freedompop
+
+
+@autocommand.autocommand(__name__)
+def main(command):
+	method_name = command.replace('-', '_').replace(' ', '_')
+	client = freedompop.Client.from_env()
+	method = getattr(client, method_name)
+	pprint.pprint(method())
