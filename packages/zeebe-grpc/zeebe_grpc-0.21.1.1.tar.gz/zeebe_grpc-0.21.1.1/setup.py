de7ab6f1e6ebbafeaa8b171e204e66fc7a4a8295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:33 +0000

    Add package zeebe-grpc/0.21.1.1/zeebe_grpc-0.21.1.1.tar.gz

---
+++ packages/zeebe-grpc/zeebe_grpc-0.21.1.1.tar.gz/zeebe_grpc-0.21.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zeebe_grpc",
+    version="0.21.1.1",
+    author="Stéphane Ludwig",
+    author_email="gitlab@stephane-ludwig.net",
+    description="zeebe Python gRPC Gateway",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/stephane.ludwig/zeebe_python_grpc",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'grpcio==1.24.1',
+        'protobuf==3.10.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
