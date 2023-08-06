e64b6f00f4eea6bc0024836e9873f7781d7487b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:27 +0000

    Add package snake-terminal-pp/0.2.2/snake-terminal-pp-0.2.2.tar.gz

---
+++ packages/snake-terminal-pp/snake-terminal-pp-0.2.2.tar.gz/snake-terminal-pp-0.2.2/tests/test_snake.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `snake` package."""
+
+
+import unittest
+
+
+class TestSnake(unittest.TestCase):
+    """Tests for `snake` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        pass
