92f23787bb2cb15dd3a695bd2fad54f3c53194f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:58 +0000

    Add package calc1/0.1/calc1-0.1.tar.gz

---
+++ packages/calc1/calc1-0.1.tar.gz/calc1-0.1/kps_calc/kps_calc.py
@@ -0,0 +1,6 @@
+def add(a,b):
+  return(a+b)
+
+def dispInfo():
+  print("Hi this is a demo code by kaustubh sadekar")
+
---
+++ packages/calc1/calc1-0.1.tar.gz/calc1-0.1/setup.py
@@ -0,0 +1,4 @@
+import setuptools
+
+setuptools.setup(name = 'calc1', packages = ['kps_calc'],version = '0.1', author = 'Kaustubh sadekar')
+
