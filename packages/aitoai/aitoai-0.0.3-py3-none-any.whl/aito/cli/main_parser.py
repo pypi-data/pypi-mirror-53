d529b39560bf4a8ae31245fb3d8b544a0f9caf91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:45 +0000

    Add package aitoai/0.0.3/aitoai-0.0.3-py3-none-any.whl

---
+++ packages/aitoai/aitoai-0.0.3-py3-none-any.whl/aito/cli/main_parser.py
@@ -0,0 +1,46 @@
+import logging
+import sys
+
+from aito.cli.client_parser import ClientParserWrapper
+from aito.cli.convert_parser import ConvertParserWrapper
+from aito.cli.infer_table_schema_parser import InferTableSchemaParserWrapper
+from aito.cli.parser import ParserWrapper
+
+
+class MainParserWrapper(ParserWrapper):
+    def __init__(self):
+        super().__init__()
+        self.parser.usage = ''' aito [-h] <action> [<args>]
+        To see help text, you can run:
+            aito -h
+            aito <action> -h
+
+        The most commonly actions are:
+            infer-table-schema  infer Aito table schema from a file
+            convert             convert data of table entries into ndjson (for file-upload) or json (for batch-upload)
+            client              set up a client and perform CRUD operations
+        '''
+        self.parser.add_argument('action', help='action to perform')
+        self.actions_parser = {
+            'infer-table-schema': InferTableSchemaParserWrapper(),
+            'convert': ConvertParserWrapper(),
+            'client': ClientParserWrapper()
+        }
+
+    def parse_and_execute(self, parsing_args):
+        args = self.parser.parse_args(parsing_args[0:1])
+        if args.action not in self.actions_parser:
+            self.parser.error(f"unrecognized action {args.action}")
+        self.actions_parser[args.action].parse_and_execute(parsing_args[1:])
+        return 0
+
+
+def main():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)-5s %(name)-5s %(levelname)-10s %(message)s',
+                        datefmt='%H:%M:%S')
+    main_parser = MainParserWrapper()
+    main_parser.parse_and_execute(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    main()
