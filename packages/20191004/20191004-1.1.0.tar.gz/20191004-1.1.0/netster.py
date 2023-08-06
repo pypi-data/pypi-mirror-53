4237b334d0ba832f6b0f5d6655b947de2d6fda98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:44 +0000

    Add package 20191004/1.1.0/20191004-1.1.0.tar.gz

---
+++ packages/20191004/20191004-1.1.0.tar.gz/20191004-1.1.0/netster.py
@@ -0,0 +1,9 @@
+"""此模块用于打印嵌套列表"""
+def print_lol(the_list,level):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item,level+1)
+		else:
+            for item in range(level):
+                print("\t",end='')
+			print(each_item)
---
+++ packages/20191004/20191004-1.1.0.tar.gz/20191004-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name		= '20191004',
+	version		= '1.1.0',
+	py_modules	= ['netster'],
+	author		= 'hfpython',
+	author_email= 'hfpython@headfirstlabs.com',
+	url			= 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+	)
+	
