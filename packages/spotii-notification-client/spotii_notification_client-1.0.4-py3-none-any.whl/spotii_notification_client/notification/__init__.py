117a34b16f863b716ffd635c2c307da2d26539c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:36 +0000

    Add package spotii-notification-client/1.0.4/spotii_notification_client-1.0.4-py3-none-any.whl

---
+++ packages/spotii-notification-client/spotii_notification_client-1.0.4-py3-none-any.whl/spotii_notification_client/notification/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from spotii_notification_client.notification.email_api import EmailApi
+from spotii_notification_client.notification.sms_api import SmsApi
