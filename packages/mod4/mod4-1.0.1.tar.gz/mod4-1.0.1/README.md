9cf6ef0b0551cf4db9f7167e72e9c2b588211fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:30 +0000

    Add package mod4/1.0.1/mod4-1.0.1.tar.gz

---
+++ packages/mod4/mod4-1.0.1.tar.gz/mod4-1.0.1/README.md
@@ -0,0 +1,8 @@
+# Example Package
+
+This is a simple example package for study purpose.
+to write your content. 
+
+# usage
+
+It is used to print list with or without indentation
---
+++ packages/mod4/mod4-1.0.1.tar.gz/mod4-1.0.1/mod4.py
@@ -0,0 +1,14 @@
+
+'''
+this metod prints any list provided
+'''
+def print_Movies(the_list):
+    for item in the_list:
+        if isinstance(item, list):
+            print_Movies(item)
+        else:
+            print(item)
+
+
+
+
