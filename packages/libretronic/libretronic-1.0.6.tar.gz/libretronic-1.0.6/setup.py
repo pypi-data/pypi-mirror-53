4f477055777ce337f265ac729f183c827bbfceda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:48 +0000

    Add package libretronic/1.0.6/libretronic-1.0.6.tar.gz

---
+++ packages/libretronic/libretronic-1.0.6.tar.gz/libretronic-1.0.6/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'libretronic',
+version = '1.0.6',
+packages=['libretronic'],
+author = 'M. en I. Margarito Navarrete-Mendoza',
+author_email = 'ni_tronic@hotmail.com',
+url = 'https://github.com/Nitronic666/firts.git',
+description = 'Libreria de Trabajo',
+)
