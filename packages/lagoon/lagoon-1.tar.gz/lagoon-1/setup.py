ec258da0093859ff7c78e176dee395cb693707fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:06 +0000

    Add package lagoon/1/lagoon-1.tar.gz

---
+++ packages/lagoon/lagoon-1.tar.gz/lagoon-1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name = 'lagoon',
+        version = '1',
+        install_requires = [],
+        packages = setuptools.find_packages(),
+        py_modules = ['screen', 'lagoon'],
+        scripts = [])
