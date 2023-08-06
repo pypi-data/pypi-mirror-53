2663da74ccc21c4820645a861482074646a3136e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:40 +0000

    Add package userv/0.7.1/userv-0.7.1.tar.gz

---
+++ packages/userv/userv-0.7.1.tar.gz/userv-0.7.1/test/test_socket_server.py
@@ -0,0 +1,27 @@
+import pytest
+from userv import socket_server
+
+@pytest.fixture()
+def server():
+    server = ""
+    yield server
+    # shutdown server
+
+
+
+def test_server(server):
+    raise
+
+def test_get_request(server):
+    raise
+
+
+def test_post_requets(server):
+    raise
+
+def test_serve_static(server):
+    raise
+
+
+def test_memory_useage(server):
+    raise
