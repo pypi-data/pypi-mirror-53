878a5bc7e24f9729c1f7204bd774bcc08dd6f652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:57 +0000

    Add package oru/0.1.3/oru-0.1.3-py3-none-any.whl

---
+++ packages/oru/oru-0.1.3-py3-none-any.whl/oru/__init__.py
@@ -0,0 +1,6 @@
+"""Oru calls the API of the Orange and Rockland Utility smart energy meter to return the current energy usage."""
+
+from .meter import Meter
+from .meter import MeterError
+
+__version__ = '0.1.3'
