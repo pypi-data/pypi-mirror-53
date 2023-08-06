835df665d6e811d6e25cfbc0d8e3e07a355ec502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:42 +0000

    Add package evilunicorn/0.0.1/evilunicorn-0.0.1.tar.gz

---
+++ packages/evilunicorn/evilunicorn-0.0.1.tar.gz/evilunicorn-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'evilunicorn',
+  packages = ['evilunicorn'],
+  version = '0.0.1',
+  description = 'Evil Unicorn',
+  long_description = str("There'll be no place to run when you're "
+                         "caught within the grip of the evil Unicorn..."),
+  author = '',
+  license = '',
+  package_data={},
+  url = '',
+  keywords = [],
+  classifiers = [],
+  install_requires = [],
+)
