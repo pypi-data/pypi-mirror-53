8289860d50097975f7e6e97e5e1d8a511c15afa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:57 +0000

    Add package komand/3.2.0/komand-3.2.0-py3-none-any.whl

---
+++ packages/komand/komand-3.2.0-py3-none-any.whl/komand/action.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .step import Step
+
+
+class Action(Step):
+    """
+    Represents a Komand Action.
+    """
+    pass
---
+++ packages/komand/komand-3.2.0-py3-none-any.whl/tests/unit/test_variables.py
@@ -0,0 +1,18 @@
+from komand.variables import Input
+
+
+def test_input_sample():
+    schema = {
+        "type": "object",
+        "properties": {
+            "foo": {
+                "type": "string"
+            },
+            "bar": {
+                "type": "integer"
+            },
+        }
+    }
+
+    i = Input(schema)
+    print(i.sample())
