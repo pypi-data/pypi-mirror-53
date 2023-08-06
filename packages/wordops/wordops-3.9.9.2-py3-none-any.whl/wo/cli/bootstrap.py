993e3fdc63431707895afae032299221774b752c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:00 +0000

    Add package wordops/3.9.9.2/wordops-3.9.9.2-py3-none-any.whl

---
+++ packages/wordops/wordops-3.9.9.2-py3-none-any.whl/wo/cli/bootstrap.py
@@ -0,0 +1,12 @@
+"""WordOps bootstrapping."""
+
+# All built-in application controllers should be imported, and registered
+# in this file in the same way as WOBaseController.
+
+from cement.core import handler
+
+from wo.cli.controllers.base import WOBaseController
+
+
+def load(app):
+    app.handler.register(WOBaseController)
