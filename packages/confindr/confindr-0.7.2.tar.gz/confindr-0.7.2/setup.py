214bae7fda72feb80e40051ffccb5cd6247cbec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:58 +0000

    Add package confindr/0.7.2/confindr-0.7.2.tar.gz

---
+++ packages/confindr/confindr-0.7.2.tar.gz/confindr-0.7.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="confindr",
+    version="0.7.2",
+    packages=find_packages(),
+    entry_points={
+       'console_scripts': [
+            'confindr.py = confindr_src.confindr:main',
+            'confindr = confindr_src.confindr:main',
+            'confindr_database_setup = confindr_src.database_setup:main',
+            'confindr_create_db = confindr_src.create_genus_specific_db:main'
+       ],
+    },
+    author="Andrew Low",
+    author_email="andrew.low@canada.ca",
+    url="https://github.com/OLC-Bioinformatics/ConFindr",
+    install_requires=['biopython',
+                      'pysam',
+                      'pytest',
+                      'numpy',
+                      'rauth']
+)
