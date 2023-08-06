cef390fb7dbbc851998abf55a919179332d3fe5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:39 +0000

    Add package withings-api/2.0.0b2/withings_api-2.0.0b2.tar.gz

---
+++ packages/withings-api/withings_api-2.0.0b2.tar.gz/withings_api-2.0.0b2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from setuptools import setup
+import os
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+required = [line for line in open('requirements/base.txt').read().split("\n")]
+
+setup(
+    name='withings_api',
+    version=(os.environ.get('TRAVIS_TAG') or 'localdev'),
+    description="Library for the Withings API",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    author='Robbie Van Gorkom',
+    author_email='robbie.van.gorkom@protonmail.com',
+    url="https://github.com/vangorra/python_withings_api",
+    license = "MIT License",
+    packages = ['withings_api'],
+    install_requires = required,
+    test_suite='tests.all_tests',
+    keywords="withings withings",
+    zip_safe = True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ]
+)
