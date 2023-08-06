dce92a0fa4fde1fa35006e819771cfef2d9a4a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:02 +0000

    Add package lukeparser/0.3.7/lukeparser-0.3.7-cp37-cp37m-win_amd64.whl

---
+++ packages/lukeparser/lukeparser-0.3.7-cp37-cp37m-win_amd64.whl/luke/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.3.7"
+
+__title__ = "lukeparser"
+__description__ = "The Style of Markdown with the Power of LaTeX."
+__url__ = "https://lukeparser.github.io"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "David Hartmann"
+
+__license__ = "GPLv2"
+__copyright__ = "Copyright (c) 2019 David Hartmann"
