7dad0b33696ae64b01a391206f1d84004413ab26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:11 +0000

    Add package tony_nester/1.0.0/tony_nester-1.0.0.tar.gz

---
+++ packages/tony_nester/tony_nester-1.0.0.tar.gz/tony_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'tony_nester',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'shq',
+    author_email = 'shenhuiqi163@163.com',
+    url = 'http://www.sparkinfo.top',
+    description = 'A simple printer of nested lists',
+    )
