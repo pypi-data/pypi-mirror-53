45e693af989a0237f947863af6c32c7357bb4ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:56 +0000

    Add package motivate1234/0.0.22/motivate1234-0.0.22.tar.gz

---
+++ packages/motivate1234/motivate1234-0.0.22.tar.gz/motivate1234-0.0.22/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+
+__project__ = "motivate1234"
+__version__ = "0.0.22"
+__description__ = "this version with cmdclass"
+__packages__ = ["motivate1234"]
+
+class PostInstallCommand(install):
+	def run(self):
+		subprocess.call(["sed -e '$i \(sleep 5; su - pi -c /home/pi/Documents/online_radio_1.py)&\n' /etc/rc.local"], shell = True)
+		install.run(self)
+
+
+setup(
+	name = __project__,
+	version = __version__,
+	description = __description__,
+	packages = __packages__,
+	include_package_data = True,
+	cmdclass={
+		'install': PostInstallCommand,
+	}
+)
+
