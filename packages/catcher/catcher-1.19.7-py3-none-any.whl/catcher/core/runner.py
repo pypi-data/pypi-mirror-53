79276a8338fa22365c5a41b8b3cc79de2967010d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:27 +0000

    Add package catcher/1.19.7/catcher-1.19.7-py3-none-any.whl

---
+++ packages/catcher/catcher-1.19.7-py3-none-any.whl/catcher/core/runner.py
@@ -0,0 +1,111 @@
+from os.path import join
+
+from catcher.core.test import Test, Include
+from catcher.modules.compose import DockerCompose
+from catcher.steps import step
+from catcher.utils.file_utils import get_files, read_source_file, get_filename
+from catcher.utils.logger import warning, info
+from catcher.utils.misc import merge_two_dicts, try_get_object, fill_template_str
+from catcher.utils.module_utils import prepare_modules
+
+
+class Runner:
+    def __init__(self,
+                 path: str,
+                 tests_path: str,
+                 inventory: str or None,
+                 modules=None,
+                 environment=None,
+                 resources=None) -> None:
+        if modules is None:
+            modules = []
+        self.environment = environment if environment is not None else {}
+        self.tests_path = tests_path
+        self.path = path
+        self.inventory = inventory
+        self.all_includes = []
+        self.modules = merge_two_dicts(prepare_modules(modules, step.registered_steps), step.registered_steps)
+        self._compose = DockerCompose(resources)
+        self.resources = resources
+
+    def run_tests(self) -> bool:
+        try:
+            self._compose.up()
+            variables = {}
+            if self.inventory is not None:
+                variables = read_source_file(self.inventory)
+                variables['INVENTORY'] = get_filename(self.inventory)
+                variables = try_get_object(fill_template_str(variables, {}))  # fill env vars
+            variables['CURRENT_DIR'] = self.path
+            variables['RESOURCES_DIR'] = self.resources or self.path + '/resources'
+            test_files = get_files(self.tests_path)
+            results = []
+            for file in test_files:
+                self.all_includes = []
+                try:
+                    variables['TEST_NAME'] = file
+                    test = self.prepare_test(file, variables)
+                    test.run()
+                    results.append(True)
+                    info('Test ' + file + ' passed.')
+                except Exception as e:
+                    warning('Test ' + file + ' failed: ' + str(e))
+                    results.append(False)
+            return all(results)
+        finally:
+            self._compose.down()
+
+    def prepare_test(self, file: str, variables: dict, override_vars: None or dict = None) -> Test:
+        body = read_source_file(file)
+        registered_includes = self.process_includes(body.get('include', []), variables)
+        variables = merge_two_dicts(variables, body.get('variables', {}))  # override variables with test's variables
+        if override_vars:  # TODO warn when overriding inventory vars?
+            variables = merge_two_dicts(variables, override_vars)
+        return Test(self.path,
+                    registered_includes,
+                    variables,
+                    body.get('config', {}),
+                    body.get('steps', []),
+                    self.modules,
+                    self.environment)
+
+    def process_includes(self,
+                         includes: list or str or dict,
+                         variables: dict,
+                         registered_includes: dict or None = None) -> (dict, dict):
+        if registered_includes is None:
+            registered_includes = {}
+        if isinstance(includes, str) or isinstance(includes, dict):  # single include
+            self.process_include(includes, registered_includes, variables)
+        elif isinstance(includes, list):  # an array of includes
+            for i in includes:  # run all includes and save includes with alias
+                variables = self.process_include(i, registered_includes, variables)
+        return registered_includes
+
+    def process_include(self, include_file: str or dict, includes: dict, variables: dict) -> dict:
+        include_file = self.path_from_root(include_file)
+        self.check_circular(include_file)
+        include = Include(**include_file)
+        self.all_includes.append(include)
+        include.test = self.prepare_test(include.file, variables, include.variables)
+        if include.alias is not None:
+            includes[include.alias] = include.test
+        if include.run_on_include:
+            try:
+                return include.test.run()
+            except Exception as e:
+                if not include.ignore_errors:
+                    raise Exception('Include ' + include.file + ' failed: ' + str(e))
+        return include.test.variables
+
+    def check_circular(self, current_include: dict):
+        path = current_include['file']
+        if [include for include in self.all_includes if include.file == path]:
+            raise Exception('Circular dependencies for ' + path)
+
+    def path_from_root(self, include_file: str or dict) -> dict:
+        if isinstance(include_file, str):
+            return {'file': join(self.path, include_file)}
+        else:
+            include_file['file'] = join(self.path, include_file['file'])
+            return include_file
---
+++ packages/catcher/catcher-1.19.7-py3-none-any.whl/catcher/core/test.py
@@ -0,0 +1,91 @@
+import traceback
+
+from catcher.steps.step import Step
+from catcher.steps.stop import StopException
+from catcher.utils.logger import debug, info
+from catcher.utils.misc import fill_template_str, merge_two_dicts
+from catcher.core import step_factory
+
+
+class Include:
+    """
+    Include another testcase in include section:
+
+    ::
+        include:
+            - file: simple_file.yaml
+              run_on_include: false
+            - other_simple_file.yaml
+
+    each include file has it's own Include object and attached Test
+    """
+
+    def __init__(self, ignore_errors=False, **keywords) -> None:
+        if 'file' not in keywords:
+            raise RuntimeError('Can\'t include unknown file.')
+        self.file = keywords['file']
+        self.variables = keywords.get('variables', {})
+        self.alias = keywords.get('as', None)
+        self.run_on_include = keywords.get('run_on_include', self.alias is None)
+        self.ignore_errors = ignore_errors
+        self.test = None
+
+
+class Test:
+    """
+    Testcase. Contains variables, includes and steps to run.
+    """
+
+    def __init__(self, path: str, includes: dict, variables: dict,
+                 config: dict, steps: list, modules: dict, override_vars=None) -> None:
+        self.includes = includes
+        self.variables = variables
+        self.config = config
+        self.steps = steps
+        self.path = path
+        self.modules = modules
+        self.override_vars = override_vars if override_vars is not None else {}
+
+    def run(self, tag=None, raise_stop=False) -> dict:
+        for step in self.steps:
+            [action] = step.keys()
+            ignore_errors = get_or_default('ignore_errors', step[action], False)
+            if tag is not None:
+                step_tag = get_or_default('tag', step[action], None)
+                if step_tag != tag:
+                    continue
+            actions = step_factory.get_actions(self.path, step, self.modules)
+            for action_object in actions:
+                # override all variables with cmd variables
+                variables = merge_two_dicts(self.variables, self.override_vars)
+                action_name = get_action_name(action, action_object, variables)
+                try:
+                    self.variables = action_object.action(self.includes, variables)
+                    info('Step ' + action_name + ' OK')
+                except StopException as e:
+                    if raise_stop:
+                        raise e
+                    debug('Skip ' + action_name + ' due to ' + str(e))
+                    info('Step ' + action_name + ' OK')
+                    return self.variables  # stop current test
+                except Exception as e:
+                    if ignore_errors:
+                        debug('Step ' + action_name + ' failed, but we ignore it')
+                        continue
+                    info('Step ' + action_name + ' failed: ' + str(e))
+                    debug(traceback.format_exc())
+                    raise e
+        return self.variables
+
+
+def get_or_default(key: str, body: dict or str, default: any) -> any:
+    if isinstance(body, dict):
+        return body.get(key, default)
+    else:
+        return default
+
+
+def get_action_name(action_type: str, action: Step, variables: dict):
+    if action.name is not None:
+        return fill_template_str(action.name, variables)
+    return action_type
