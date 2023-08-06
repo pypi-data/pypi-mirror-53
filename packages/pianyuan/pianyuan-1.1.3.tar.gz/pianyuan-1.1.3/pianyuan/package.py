cf80f65f7b86b055bc08287af6642d337d09c9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:14 +0000

    Add package pianyuan/1.1.3/pianyuan-1.1.3.tar.gz

---
+++ packages/pianyuan/pianyuan-1.1.3.tar.gz/pianyuan-1.1.3/pianyuan/package.py
@@ -0,0 +1,13 @@
+from pianyuan import getInf
+import os,sys
+version = getInf.getVersion()
+upload_path = "twine upload " + os.getcwd()+"\\dist\\pianyuan-" + version + ".tar.gz"
+def package():
+    print("start packaging =================================")
+    os.system(r"python setup.py sdist")
+
+def upload():
+    package()
+    print("start upload=====================================")
+    os.system(upload_path)
+    os.system(r"pip install --upgrade pianyuan")
