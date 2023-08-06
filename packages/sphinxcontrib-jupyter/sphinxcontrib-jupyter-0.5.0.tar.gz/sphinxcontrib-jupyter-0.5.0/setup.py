6377041ea2f2127360bff2dab5c59a86dbbdf82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:56 +0000

    Add package sphinxcontrib-jupyter/0.5.0/sphinxcontrib-jupyter-0.5.0.tar.gz

---
+++ packages/sphinxcontrib-jupyter/sphinxcontrib-jupyter-0.5.0.tar.gz/sphinxcontrib-jupyter-0.5.0/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+VERSION = 'v0.5.0'
+
+long_desc = '''
+This package contains the Jupyter Sphinx extension.
+'''
+
+requires = ['Sphinx>=0.6']
+
+setup(
+    name='sphinxcontrib-jupyter',
+    version=VERSION,
+    url='https://github.com/QuantEcon/sphinxcontrib-jupyter',
+    download_url='https://github.com/QuantEcon/sphinxcontrib-jupyter/archive/{}.tar.gz'.format(VERSION),
+    license='BSD',
+    author='QuantEcon',
+    author_email='admin@quantecon.org',
+    description='Sphinx "Jupyter" extension: Convert your RST files into executable Jupyter notebooks.',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Sphinx',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Framework :: Sphinx :: Extension',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['docutils', 'nbformat', 'sphinx', 'dask', 'ipython', 'nbconvert', 'jupyter_client'],
+    namespace_packages=['sphinxcontrib'],
+)
