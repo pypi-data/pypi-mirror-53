2d9a317de5f14ea113569737dc483e4ed6ed8bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:45 +0000

    Add package mgv/1.0.29/mgv-1.0.29.tar.gz

---
+++ packages/mgv/mgv-1.0.29.tar.gz/mgv-1.0.29/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+      def run(self):
+            import os
+            import shutil
+            import mangrove
+
+            DESKTOP_FOLDER = os.path.join(os.getenv("HOMEPATH"), "Desktop")
+            NAME = "Mangrove"
+
+            if sys.argv[1] == '-install':
+                  create_shortcut(
+                        target=os.path.join(sys.prefix, 'python.exe') + " " + os.path.join(os.path.dirname(mangrove.__file__), "mgvUI.py"),
+                        description="Mangrove UI",
+                        filename=NAME,
+                        workdir=DESKTOP_FOLDER,
+                        iconpath=os.path.join(os.path.dirname(mangrove.__file__),"icons","mgv.ico"))
+
+                  shutil.move(os.path.join(os.getcwd(), NAME), os.path.join(DESKTOP_FOLDER, NAME))
+                  #file_created(os.path.join(DESKTOP_FOLDER, NAME))
+
+            if sys.argv[1] == '-remove':
+                pass
+
+setuptools.setup(name='mgv',
+      version='1.0.29',
+      scripts=['mgv'],
+      description='An open-source nodal pipeline manager',
+      long_description="""Mangrove is a simple tool to help you to manage your projects.
+It's has been thought to be used by every one, from scholar teams to private companies, with or without any technical skills.
+With a clean and intuitive interface, users create, open, connect or version data easily
+without any knowledge of the nomenclatures and the data structure.
+Based on a nodal interface, you plug pre-scripted nodes by your own TDs or the community to create and manage your files.
+Originaly created for 3d post production ease, it's widely open and can be used for a non limited type of projects.""",
+      url='https://gitlab.com/TheYardVFX/mangrove',
+      author='Bekri Djelloul',
+      author_email='mangrove@theyard-vfx.com',
+      license='LGPL v3',
+      packages=setuptools.find_packages(),
+      install_requires=['xlrd', 'PySide', 'Qt.py', 'pymongo', 'firebase_admin'],
+      include_package_data=True,
+      )
