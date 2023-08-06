8115e53890c56110118ed331e3487fe8ed9de549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:24 +0000

    Add package sourcerio/0.1.1/sourcerio-0.1.1-py3-none-any.whl

---
+++ packages/sourcerio/sourcerio-0.1.1-py3-none-any.whl/lib/git.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+from lib.logging import info, warning
+from lib.utils import exec, get_repo_name
+
+class Git(object):
+    def __init__(self, repo, dir, repo_name, verbose):
+        self.dir = dir
+        self.repo = repo
+        self.repo_name = repo_name
+        self.verbose = verbose
+
+        os.environ['PROJECT_NAME'] = repo_name
+
+    def clone(self):
+        exec('git clone %s %s' % (self.repo, self.dir), verbose=self.verbose)
+
+    def fetch(self):
+        cmd = 'git fetch'
+        if self.verbose:
+            cmd += ' -v'
+
+        exec(cmd, cwd=self.dir, verbose=self.verbose)
