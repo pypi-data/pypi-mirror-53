84c372b37c466c92318124d140ee79a7b62eff41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:35 +0000

    Add package flair-light/0.4.1/flair_light-0.4.1.tar.gz

---
+++ packages/flair-light/flair_light-0.4.1.tar.gz/flair_light-0.4.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['flair', 'flair.models']
+
+package_data = \
+{'': ['*'], 'flair': ['embeddings/en/*']}
+
+install_requires = \
+['bpemb==0.2.9']
+
+import os
+
+os.system('pip install https://download.pytorch.org/whl/cpu/torch-1.2.0%2Bcpu-cp36-cp36m-manylinux1_x86_64.whl')
+
+setup_kwargs = {
+    'name': 'flair_light',
+    'version': '0.4.1',
+    'description': 'A lightweight version of Flair for sequence tagging.',
+    'long_description': None,
+    'author': 'snapADDY GmbH',
+    'author_email': 'info@snapaddy.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+    'data_files': [('embeddings/en', ['flair/embeddings/en/en.wiki.bpe.vs100000.model'])]
+}
+
+
+setup(**setup_kwargs)
