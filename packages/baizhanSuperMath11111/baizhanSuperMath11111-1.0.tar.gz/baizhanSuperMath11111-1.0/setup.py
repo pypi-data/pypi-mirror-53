c1b09e6f3fd11844e64d0ac8dc25812697e4d5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:23 +0000

    Add package baizhanSuperMath11111/1.0/baizhanSuperMath11111-1.0.tar.gz

---
+++ packages/baizhanSuperMath11111/baizhanSuperMath11111-1.0.tar.gz/baizhanSuperMath11111-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="baizhanSuperMath11111",     #对外我们模块的名字
+    version="1.0",#版本号
+    description="测试第一个模块",  #描述
+    author="Dreamer",#作者
+    author_email="1835488236@qq.com",#作者邮箱
+    py_modules=["baizhanSuperMath.dome1","baizhanSuperMath.dome2"]
+
+
+)
