a3948edb15641e1da4ca03dd99ee5728b70afbc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:19 +0000

    Add package nairaland/1.0.0/nairaland-1.0.0-py3-none-any.whl

---
+++ packages/nairaland/nairaland-1.0.0-py3-none-any.whl/nairaland/__init__.py
@@ -0,0 +1,8 @@
+"""
+Package to fetch information from Nairaland.
+Supports Python 3.7
+Source: https://github.com/makinde2013/pynairaland
+"""
+
+from .user import User
+from .nairaland import Nairaland
