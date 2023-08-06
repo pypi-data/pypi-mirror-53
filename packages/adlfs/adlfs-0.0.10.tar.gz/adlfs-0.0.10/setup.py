9d8c2b30fce68afeea3e7b3080405127ee085356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:38 +0000

    Add package adlfs/0.0.10/adlfs-0.0.10.tar.gz

---
+++ packages/adlfs/adlfs-0.0.10.tar.gz/adlfs-0.0.10/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+from os import path
+
+
+setup(name='adlfs',
+      version='0.0.10',
+      description='Access Azure Datalake Gen1 with fsspec and dask',
+      url='https://github.com/hayesgb/adlfs/',
+      maintainer='Greg Hayes',
+      maintainer_email='hayesgb@gmail.com',
+      license='BSD',
+      keywords=['file-system', 'dask', 'azure'],
+      packages=['adlfs'],
+      long_description=open('README.rst').read() if exists('README.rst') else '',
+      install_requires=['azure-datalake-store',
+                        'fsspec>=0.4.0<1.0',
+                        'requests>=2.11',    # After requests 2.11, ints are not allowed in headers, but the ADLSGen2 API expects position to be an int
+                        ],
+      zip_safe=False)
