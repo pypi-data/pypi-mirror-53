5905f453d63ba396d11db7bb9f9919abf68e10ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:18 +0000

    Add package ccm-widgets/0.4.2/ccm_widgets-0.4.2.tar.gz

---
+++ packages/ccm-widgets/ccm_widgets-0.4.2.tar.gz/ccm_widgets-0.4.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+pkg_name = "ccm_widgets"
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.4.2",
+    author="Jeremy Magland",
+    description="Reactopya widgets of relevance to the Center for Computational Mathematics, Flatiron Institute",
+    packages=setuptools.find_packages(),
+    scripts=['bin/ccm_widgets'],
+    include_package_data=True,
+    install_requires=[
+        'simplejson',
+        'numpy',
+        'mountaintools',
+        'vtk'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
