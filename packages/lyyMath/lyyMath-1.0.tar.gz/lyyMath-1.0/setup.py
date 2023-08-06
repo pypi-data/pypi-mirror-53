55ec4d5172d99d7e49c032506b30c45df63e20b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:37 +0000

    Add package lyyMath/1.0/lyyMath-1.0.tar.gz

---
+++ packages/lyyMath/lyyMath-1.0.tar.gz/lyyMath-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='lyyMath',  # 对外我们模块的名字
+    version='1.0',  # 版本号
+    description='这是第一个对外发布的模块，测试哦',  # 描述
+    author='liaoyuanyang',  # 作者
+    author_email='1262607947@qq.com',
+    py_modules=['lyyMath.demo1', 'lyyMath.demo2']  # 要发布的模块
+)
