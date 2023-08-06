55975ce67e4e7354b1f1336202c3f98f6dc24ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:35 +0000

    Add package python-aoc-qq/1.0.0/python-aoc-qq-1.0.0.tar.gz

---
+++ packages/python-aoc-qq/python-aoc-qq-1.0.0.tar.gz/python-aoc-qq-1.0.0/setup.py
@@ -0,0 +1,23 @@
+"""AoC QQ setup."""
+from setuptools import setup, find_packages
+
+setup(
+    name='python-aoc-qq',
+    version='1.0.0',
+    description='AoC QQ API',
+    url='https://github.com/happyleavesaoc/python-aoc-qq/',
+    license='MIT',
+    author='happyleaves',
+    author_email='happyleaves.tfr@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4>=4.6.3',
+        'lxml>=4.3.0',
+        'requests>=2.20.1'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ]
+)
