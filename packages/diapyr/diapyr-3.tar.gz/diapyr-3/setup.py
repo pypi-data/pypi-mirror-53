8f7825114eaa83f571ec17eb549811ec6ba0976c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:00 +0000

    Add package diapyr/3/diapyr-3.tar.gz

---
+++ packages/diapyr/diapyr-3.tar.gz/diapyr-3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name = 'diapyr',
+        version = '3',
+        install_requires = [],
+        packages = setuptools.find_packages(),
+        py_modules = [],
+        scripts = [])
