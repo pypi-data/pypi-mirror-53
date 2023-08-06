e62723f08711a827936e50d5d1c97ac4ae310f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:26 +0000

    Add package sensitive-word/0.3/sensitive-word-0.3.tar.gz

---
+++ packages/sensitive-word/sensitive-word-0.3.tar.gz/sensitive-word-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "sensitive-word",
+    version = "0.3",
+    description = "sensitive-word",
+    # long_description = open('README.md').read(),
+    url = 'https://github.com/wqfff/sensitiveword.git',
+    author = "wqf",
+    packages = find_packages()
+)
