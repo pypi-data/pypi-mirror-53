e524fa7b927a983652dde71e78d920f0bf138bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package memacs/2019.10.2.1/memacs-2019.10.2.1-py3-none-any.whl

---
+++ packages/memacs/memacs-2019.10.2.1-py3-none-any.whl/memacs/tests/filenametimestamps_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2019-01-13 19:37:56 vk>
+
+import os
+import shutil
+import tempfile
+import unittest
+
+from memacs.filenametimestamps import FileNameTimeStamps
+
+
+class TestFileNameTimeStamps(unittest.TestCase):
+
+    def setUp(self):
+        self._tmp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self._tmp_dir)
+
+    def test_functional(self):
+        # touch file
+        tmpfile = os.path.join(self._tmp_dir, '2011-12-19T23.59.12_test1.txt')
+        with open(tmpfile, 'w'):
+            pass
+
+        entry = "** <2011-12-19 Mon 23:59> [[" + tmpfile + \
+            "][2011-12-19T23.59.12_test1.txt]]"
+
+        argv = "-s -f " + self._tmp_dir
+        memacs = FileNameTimeStamps(argv=argv.split())
+        data = memacs.test_get_entries()
+
+        self.assertEqual(data[0], entry)
+        self.assertEqual(data[1], "   :PROPERTIES:")
+        self.assertEqual(data[3], "   :END:")
+
+    def test_functional_with_unusual_year(self):
+        # touch file
+        tmpfile = os.path.join(self._tmp_dir, '1899-12-30T00.00.00_P1000286.jpg')
+        with open(tmpfile, 'w'):
+            pass
+
+        entry = "** <1899-12-30 Sat 00:00> [[" + tmpfile + \
+            "][1899-12-30T00.00.00_P1000286.jpg]]"
+
+        argv = "-s -f " + self._tmp_dir
+        memacs = FileNameTimeStamps(argv=argv.split())
+        data = memacs.test_get_entries()
+
+        self.assertEqual(data[0], entry)
+        self.assertEqual(data[1], "   :PROPERTIES:")
+        self.assertEqual(data[3], "   :END:")
