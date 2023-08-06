816821185794d85f9b465f6dfbc8d6b2b46154bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:19 +0000

    Add package pip-outdated/0.3.0/pip_outdated-0.3.0-py3-none-any.whl

---
+++ packages/pip-outdated/pip_outdated-0.3.0-py3-none-any.whl/pip_outdated/__init__.py
@@ -0,0 +1,31 @@
+import argparse
+
+__version__ = "0.3.0"
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="pip-outdated",
+        description="Find outdated dependencies in your requirements.txt or "
+                    "setup.cfg file.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Print verbose information.")
+    parser.add_argument(
+        "file", nargs="*", default=["requirements.txt", "setup.cfg"], metavar="<file>",
+        help="Read dependencies from requirements files. This option accepts "
+             "glob pattern.")
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    
+    from .verbose import set_verbose
+    set_verbose(args.verbose)
+    
+    from .find_require import find_require
+    from .check_outdated import check_outdated
+    from .print_outdated import print_outdated
+    requires = find_require(args.file)
+    outdated_results = check_outdated(requires)
+    print_outdated(outdated_results)
+    
