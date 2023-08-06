0e63096e34c0b4ca1539b3906df4a3d3decbe42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:04 +0000

    Add package yly-python-sdk/1.2.0/yly_python_sdk-1.2.0-py2-none-any.whl

---
+++ packages/yly-python-sdk/yly_python_sdk-1.2.0-py2-none-any.whl/Lib/Oauth/oauth.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+import uuid
+import time
+import hashlib
+import json
+import requests
+
+class Oauth:
+
+    def __init__(self, config):
+        self.client_id = config.get_client_id()
+        self.client_secret = config.get_client_secret()
+        self.url = config.get_request_url()
+        self.md5 = hashlib.md5()
+
+    def get_token(self, code= None):
+        params = {
+            'client_id': self.client_id,
+            'timestamp': int(time.time()),
+            'id': uuid.uuid4(),
+            'scope': 'all'
+        }
+        if (code == None):
+            params['grant_type'] = 'client_credentials'
+        else:
+            params['grant_type'] = 'authorization_code'
+            params['code'] = code
+        sign_str = str(self.client_id) + str(params['timestamp']) + str(self.client_secret)
+        self.md5.update(sign_str.encode('utf-8'))
+        params['sign'] = self.md5.hexdigest()
+        return self.post(params)
+
+    def refresh_token(self, refresh_token):
+        params = {
+            'client_id': self.client_id,
+            'refresh_token': refresh_token,
+            'grant_type': 'refresh_token',
+            'scope': 'all',
+            'timestamp': int(time.time()),
+            'id': uuid.uuid4()
+        }
+        sign_str = str(self.client_id) + str(params['timestamp']) + str(self.client_secret)
+        self.md5.update(sign_str.encode('utf-8'))
+        params['sign'] = self.md5.hexdigest()
+        return self.post(params)
+
+    def post(self, req_params):
+        try:
+            header = {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+            }
+            req_url = self.url + 'oauth/oauth'
+            res = requests.post(req_url, data=req_params, headers=header)
+            return json.loads(res.text)
+        except Exception as e:
+            raise Exception('yly api response:{}'.format(e))
---
+++ packages/yly-python-sdk/yly_python_sdk-1.2.0-py2-none-any.whl/Lib/Protocol/rpc_client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import requests
+import uuid
+import time
+import hashlib
+import json
+
+
+class RpcClient:
+
+    def __init__(self, config, token):
+        self.access_token = token
+        self.client_id = config.get_client_id()
+        self.client_secret = config.get_client_secret()
+        self.url = config.get_request_url()
+        self.md5 = hashlib.md5()
+
+    def call(self, action, params):
+        params['client_id'] = self.client_id
+        params['access_token'] = self.access_token
+        params['timestamp'] = time.time()
+        params['id'] = uuid.uuid4()
+        sign_str = str(self.client_id) + str(params['timestamp']) + self.client_secret
+        self.md5.update(sign_str.encode('utf-8'))
+        params['sign'] = self.md5.hexdigest()
+        request_url = self.url + action
+        request_data = params
+        return self.post(request_url, request_data)
+
+    def post(self, req_url, req_params):
+        try:
+            header = {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+            }
+            res = requests.post(req_url, data=req_params, headers=header)
+            return json.loads(res.text)
+        except Exception as e:
+            raise Exception('yly api response:{}'.format(e))
