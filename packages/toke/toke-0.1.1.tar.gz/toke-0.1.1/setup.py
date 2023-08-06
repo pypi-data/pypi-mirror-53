4ea5e2cfc10bda0f6bdb1b689a80319ddebe8967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package toke/0.1.1/toke-0.1.1.tar.gz

---
+++ packages/toke/toke-0.1.1.tar.gz/toke-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='toke',
+    version='0.1.1',
+    author='keaising',
+    author_email='keaising@gmail.com',
+    description='Tool Of KEaising',
+    url="https://github.com/keaising/toke",
+    py_modules=['toke'],
+    install_requires=[
+        'Click',
+        'python-dotenv',
+        'tweepy'
+    ],
+    entry_points='''
+        [console_scripts]
+        toke=toke:cli
+    ''',
+)
