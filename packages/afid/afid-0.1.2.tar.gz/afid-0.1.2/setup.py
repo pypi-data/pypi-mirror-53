f86801feb8d16f83b7f24fd1fd4191f32f709a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:17 +0000

    Add package afid/0.1.2/afid-0.1.2.tar.gz

---
+++ packages/afid/afid-0.1.2.tar.gz/afid-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='afid',
+      version='0.1.2',
+      description='Fast affine transformations to incorporate side-information',
+      url='http://github.com/rs239/afid',
+      author='Rohit Singh',
+      author_email='rsingh@alum.mit.edu',
+      license='MIT',
+      packages=['afid'],
+      install_requires = 'numpy,scipy,pandas,sklearn,cvxopt'.split(','),
+      zip_safe=False)
