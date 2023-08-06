e8b2ba18eefd4c5473210d5a45df6f718448ecae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:15 +0000

    Add package PyScaffold/3.2.3/PyScaffold-3.2.3.tar.gz

---
+++ packages/PyScaffold/PyScaffold-3.2.3.tar.gz/PyScaffold-3.2.3/tests/test_templates.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from pyscaffold import templates
+
+
+def test_get_template():
+    template = templates.get_template("setup_py")
+    content = template.safe_substitute()
+    assert content.split('\n', 1)[0] == '# -*- coding: utf-8 -*-'
+
+
+def test_all_licenses():
+    opts = {"email": "test@user",
+            "project": "my_project",
+            "author": "myself",
+            "year": 1832}
+    for license in templates.licenses.keys():
+        opts['license'] = license
+        assert templates.license(opts)
