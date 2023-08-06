43581782c80082c16c5335871badb7ec3d5755aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:27 +0000

    Add package cprior/0.3.1/cprior-0.3.1-py3-none-any.whl

---
+++ packages/cprior/cprior-0.3.1-py3-none-any.whl/cprior/cdist/__init__.py
@@ -0,0 +1,18 @@
+from .beta import BetaABTest
+from .beta import BetaModel
+from .beta import BetaMVTest
+
+from .gamma import GammaABTest
+from .gamma import GammaModel
+from .gamma import GammaMVTest
+
+from .normal_inverse_gamma import NormalInverseGammaABTest
+from .normal_inverse_gamma import NormalInverseGammaModel
+from .normal_inverse_gamma import NormalInverseGammaMVTest
+
+from .pareto import ParetoABTest
+from .pareto import ParetoModel
+from .pareto import ParetoMVTest
+
+# utilities
+from .ci import ci_interval
---
+++ packages/cprior/cprior-0.3.1-py3-none-any.whl/cprior/models/__init__.py
@@ -0,0 +1,35 @@
+from .bernoulli import BernoulliABTest
+from .bernoulli import BernoulliModel
+from .bernoulli import BernoulliMVTest
+
+from .binomial import BinomialABTest
+from .binomial import BinomialModel
+from .binomial import BinomialMVTest
+
+from .exponential import ExponentialABTest
+from .exponential import ExponentialModel
+from .exponential import ExponentialMVTest
+
+from .geometric import GeometricABTest
+from .geometric import GeometricModel
+from .geometric import GeometricMVTest
+
+from .lognormal import LogNormalABTest
+from .lognormal import LogNormalModel
+from .lognormal import LogNormalMVTest
+
+from .negative_binomial import NegativeBinomialABTest
+from .negative_binomial import NegativeBinomialModel
+from .negative_binomial import NegativeBinomialMVTest
+
+from .normal import NormalABTest
+from .normal import NormalModel
+from .normal import NormalMVTest
+
+from .poisson import PoissonABTest
+from .poisson import PoissonModel
+from .poisson import PoissonMVTest
+
+from .uniform import UniformABTest
+from .uniform import UniformModel
+from .uniform import UniformMVTest
