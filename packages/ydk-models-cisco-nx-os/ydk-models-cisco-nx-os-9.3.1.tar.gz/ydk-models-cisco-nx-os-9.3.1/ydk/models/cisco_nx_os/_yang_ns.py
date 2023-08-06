240565407bf12de693eb338029bd88609fa3e1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:56 +0000

    Add package ydk-models-cisco-nx-os/9.3.1/ydk-models-cisco-nx-os-9.3.1.tar.gz

---
+++ packages/ydk-models-cisco-nx-os/ydk-models-cisco-nx-os-9.3.1.tar.gz/ydk-models-cisco-nx-os-9.3.1/ydk/models/cisco_nx_os/_yang_ns.py
@@ -0,0 +1,19 @@
+BUNDLE_NAME = "cisco_nx_os"
+
+CAPABILITIES = {
+    "Cisco-NX-OS-device": "2019-07-18",
+}
+
+ENTITY_LOOKUP = {
+    ("http://cisco.com/ns/yang/cisco-nx-os-device", "System"): "Cisco_NX_OS_device.System",
+    ("Cisco-NX-OS-device", "System"): "Cisco_NX_OS_device.System",
+}
+
+NAMESPACE_LOOKUP = {
+    "Cisco-NX-OS-device": "http://cisco.com/ns/yang/cisco-nx-os-device",
+}
+
+IDENTITY_LOOKUP = {
+    'Cisco-NX-OS-device:inactive':('ydk.models.cisco_nx_os.Cisco_NX_OS_device', 'Inactive'),
+}
+
