641667e234b69f2db9a3f8c3e76d5338073f8920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:16 +0000

    Add package pynxhuy/0.1/pynxhuy-0.1.tar.gz

---
+++ packages/pynxhuy/pynxhuy-0.1.tar.gz/pynxhuy-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pynxhuy',
+      version='0.1',
+      description='The nxhuyiest joke in the world',
+      url='https://github.com/nxhuy-github/pynxhuy',
+      author='Xuan Huy NGUYEN',
+      author_email='nxhuy@example.com',
+      packages=['pynxhuy'],
+      zip_safe=False)
