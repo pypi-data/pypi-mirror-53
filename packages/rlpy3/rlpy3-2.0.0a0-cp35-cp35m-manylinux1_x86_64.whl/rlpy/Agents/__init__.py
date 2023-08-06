0d22544f5596fe6d8ce72e336fd41ff7d34c3d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:55 +0000

    Add package rlpy3/2.0.0a0/rlpy3-2.0.0a0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/rlpy3/rlpy3-2.0.0a0-cp35-cp35m-manylinux1_x86_64.whl/rlpy/Agents/__init__.py
@@ -0,0 +1,5 @@
+from .TDControlAgent import Q_Learning, SARSA
+from .Greedy_GQ import Greedy_GQ
+from .LSPI import LSPI
+from .LSPI_SARSA import LSPI_SARSA
+from .NaturalActorCritic import NaturalActorCritic
