4bdd7d763640ac07fe302c3acc1933a7ea64ce39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:04 +0000

    Add package deepcon/0.1.2/deepcon-0.1.2.tar.gz

---
+++ packages/deepcon/deepcon-0.1.2.tar.gz/deepcon-0.1.2/deepcon/rundeepcon.py
@@ -0,0 +1,33 @@
+import deepcon
+from deepcon import deepconcovariance
+
+################################################################################
+def main(aln, file_rr, png):
+    if len(sys.argv) = 2:
+      print ('Start 2 arg deepcon')
+      deepcon.main("aln","file_rr")
+      print ('Done 2 arg deepcon')
+    
+    if len(sys.argv) = 3:
+      print ('Start 3 arg deepcon')
+      deepcon.main("aln","file_rr", "png")
+      print ('Done 3 arg deepcon')
+      
+################################################################################
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--aln'
+        , help = 'Input Alignment file'
+        , required = True
+    )
+    parser.add_argument('--rr'
+        , help = 'Output RR file (CASP format)'
+        , required = True
+    )
+    parser.add_argument('--png'
+        , help = 'Output PNG graph'
+        , required = False
+    )
+    args = parser.parse_args()
+    arguments = args.__dict__
+    main(arguments['aln'], arguments['rr'], arguments['png'])
