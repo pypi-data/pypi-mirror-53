2ea591026efe3ba921f8f68615cf17110f7476d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:13 +0000

    Add package serpentTools/0.8.0rc0/serpentTools-0.8.0rc0-py2-none-any.whl

---
+++ packages/serpentTools/serpentTools-0.8.0rc0-py2-none-any.whl/serpentTools/utils/compat.py
@@ -0,0 +1,12 @@
+"""
+Minor compatibility between python 2 and python 3 outside of six
+
+Provided objects:
+    - ``Iterable``, ``Callable`: ``collections.x`` or ``collections.abc.x``
+"""
+import six
+
+if six.PY2:
+    from collections import Iterable, Callable
+else:
+    from collections.abc import Iterable, Callable
