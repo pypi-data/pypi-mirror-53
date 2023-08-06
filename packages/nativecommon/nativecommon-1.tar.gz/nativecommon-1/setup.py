882fc4362932ede36833b5607547b78e0ff9267e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:59 +0000

    Add package nativecommon/1/nativecommon-1.tar.gz

---
+++ packages/nativecommon/nativecommon-1.tar.gz/nativecommon-1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name = 'nativecommon',
+        version = '1',
+        install_requires = [],
+        packages = setuptools.find_packages(),
+        py_modules = ['initnative'],
+        scripts = [])
