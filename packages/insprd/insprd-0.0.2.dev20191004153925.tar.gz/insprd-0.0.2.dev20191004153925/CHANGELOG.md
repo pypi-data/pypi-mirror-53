8fa9bdb0365841f1ff7056d60cda66307c03e5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:21 +0000

    Add package insprd/0.0.2.dev20191004153925/insprd-0.0.2.dev20191004153925.tar.gz

---
+++ packages/insprd/insprd-0.0.2.dev20191004153925.tar.gz/insprd-0.0.2.dev20191004153925/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Inspired Python Change History
+
+## 0.0.1
+
+Initial release.
---
+++ packages/insprd/insprd-0.0.2.dev20191004153925.tar.gz/insprd-0.0.2.dev20191004153925/setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup, find_packages
+from insprd.core.version import get_version
+
+VERSION = get_version()
+
+f = open('README.md', 'r')
+LONG_DESCRIPTION = f.read()
+f.close()
+
+setup(
+    name='insprd',
+    version=VERSION,
+    description='Python interface into Inspired Platform',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Jeff Trudeau',
+    author_email='jeff.trudeau@gmail.com',
+    url='https://github.com/InspiredMember/inspired-python',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    package_data={'insprd': ['templates/*']},
+    include_package_data=True,
+    install_requires=[
+        'cryptography==2.7',
+        'PyJWT==1.7.1',
+        'requests==2.22.0',
+    ],
+    entry_points="""
+        [console_scripts]
+        insprd = insprd.main:main
+    """,
+)
