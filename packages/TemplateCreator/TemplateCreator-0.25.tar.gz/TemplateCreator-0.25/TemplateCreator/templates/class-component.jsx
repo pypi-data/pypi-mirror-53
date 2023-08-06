aae17082f0afd08ec0e93bef7c7bcae05fa80e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:56 +0000

    Add package TemplateCreator/0.25/TemplateCreator-0.25.tar.gz

---
+++ packages/TemplateCreator/TemplateCreator-0.25.tar.gz/TemplateCreator-0.25/TemplateCreator/templates/class-component.jsx
@@ -0,0 +1,24 @@
+import React, { component } from 'react';
+import PropTypes from 'prop-types';
+
+class {{ component_name }} extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() {
+    return (
+      <div>{{ component_name }}</div>
+    );
+  }
+}
+
+{{ component_name }}.propTypes = {
+
+};
+
+{{ component_name }}.defaultProps = {
+
+};
+
+export default {{ component_name }};
