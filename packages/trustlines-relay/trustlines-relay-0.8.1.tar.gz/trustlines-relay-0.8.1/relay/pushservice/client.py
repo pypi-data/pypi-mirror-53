07b4f131599b1d2624b5e0ccae04926b4190b6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:17 +0000

    Add package trustlines-relay/0.8.1/trustlines-relay-0.8.1.tar.gz

---
+++ packages/trustlines-relay/trustlines-relay-0.8.1.tar.gz/trustlines-relay-0.8.1/relay/pushservice/client.py
@@ -0,0 +1,36 @@
+import logging
+
+from relay.streams import Client, DisconnectedError, Subscription
+from relay.events import Event
+from relay.logger import get_logger
+from .pushservice import FirebaseRawPushService, InvalidClientTokenException
+
+
+logger = get_logger("pushserviceclient", logging.DEBUG)
+
+
+class PushNotificationClient(Client):
+    """
+    Stream Client that sends events as push notification
+    """
+
+    def __init__(
+        self, rawPushService: FirebaseRawPushService, client_token: str
+    ) -> None:
+        super().__init__()
+        self._rawPushService = rawPushService
+        self.client_token = client_token
+
+    def _execute_send(self, subscription: Subscription, event: Event) -> None:
+        assert isinstance(event, Event)
+        try:
+            self._rawPushService.send_event(self.client_token, event)
+        except InvalidClientTokenException as e:
+            # Token not longer valid means listener is not listing anymore
+            raise DisconnectedError from e
+        except Exception as e:
+            logger.warning(
+                "Could not sent push notification to %s\nerror: %s",
+                self.client_token,
+                e,
+            )
---
+++ packages/trustlines-relay/trustlines-relay-0.8.1.tar.gz/trustlines-relay-0.8.1/tests/chain_integration/test_delegate.py
@@ -0,0 +1,200 @@
+#! pytest
+import pytest
+import attr
+from tldeploy.identity import MetaTransaction, Identity
+from tldeploy.core import deploy_identity
+
+from relay.blockchain.delegate import (
+    Delegate,
+    InvalidMetaTransactionException,
+    InvalidIdentityContractException,
+)
+
+
+@pytest.fixture(scope="session")
+def delegate_address(web3):
+    return web3.eth.coinbase
+
+
+@pytest.fixture(scope="session")
+def delegate(web3, delegate_address, contracts):
+
+    identity_contract_abi = contracts["Identity"]["abi"]
+
+    return Delegate(web3, delegate_address, identity_contract_abi)
+
+
+@pytest.fixture(scope="session")
+def owner(accounts):
+    return accounts[0]
+
+
+@pytest.fixture(scope="session")
+def owner_key(account_keys):
+    return account_keys[0]
+
+
+@pytest.fixture(scope="session")
+def identity_contract(web3, owner):
+
+    identity_contract = deploy_identity(web3, owner)
+    web3.eth.sendTransaction(
+        {"to": identity_contract.address, "from": owner, "value": 1000000}
+    )
+
+    return identity_contract
+
+
+@pytest.fixture(scope="session")
+def identity(identity_contract, owner_key):
+    return Identity(contract=identity_contract, owner_private_key=owner_key)
+
+
+def meta_transaction_for_currency_network_transfer(
+    currency_network, identity, source, destination
+):
+
+    trustlines = [(source, destination, 100, 100)]
+    currency_network.setup_trustlines(trustlines)
+    meta_transaction = currency_network.transfer_meta_transaction(
+        destination, 100, 0, [destination]
+    )
+    meta_transaction = identity.filled_and_signed_meta_transaction(meta_transaction)
+
+    return meta_transaction
+
+
+def test_delegate_meta_transaction(delegate, identity, web3, accounts, owner_key):
+    """"
+    Tests that a transaction is sent by the delegate upon receiving a meta-transaction.
+    """
+
+    meta_transaction = MetaTransaction(
+        from_=identity.address,
+        to=accounts[2],
+        value=123,
+        data=(1234).to_bytes(10, byteorder="big"),
+        nonce=1,
+        extra_data=(123456789).to_bytes(10, byteorder="big"),
+    )
+
+    signed_meta_transaction = meta_transaction.signed(owner_key)
+
+    tx_hash = delegate.send_signed_meta_transaction(signed_meta_transaction)
+    tx = web3.eth.getTransaction(tx_hash)
+
+    assert tx["from"] == web3.eth.coinbase
+    assert tx["to"] == identity.address
+
+
+def test_delegated_transaction_trustlines_flow(
+    currency_network, identity, delegate, accounts
+):
+    """"
+    Tests that the relaying of the metatransaction by the relay server works on a currency network contract
+    """
+
+    source = identity.address
+    destination = accounts[3]
+
+    meta_transaction = meta_transaction_for_currency_network_transfer(
+        currency_network, identity, source, destination
+    )
+
+    delegate.send_signed_meta_transaction(meta_transaction)
+
+    assert currency_network.get_balance(source, destination) == -100
+
+
+def test_deploy_identity(currency_network, delegate, accounts, owner, owner_key):
+    """
+    Tests that the deployment of an identity contract by the relay server delegate works
+    by using it to execute a meta-transaction
+    """
+
+    identity_contract_address = delegate.deploy_identity(owner)
+
+    destination = accounts[3]
+
+    meta_transaction = currency_network.transfer_meta_transaction(
+        destination, 100, 0, [destination]
+    )
+    signed_meta_transaction = attr.evolve(
+        meta_transaction, from_=identity_contract_address, nonce=0
+    ).signed(owner_key)
+
+    currency_network.setup_trustlines(
+        [(identity_contract_address, destination, 100, 100)]
+    )
+    delegate.send_signed_meta_transaction(signed_meta_transaction)
+    assert currency_network.get_balance(identity_contract_address, destination) == -100
+
+
+def test_next_nonce(currency_network, delegate, identity_contract, accounts, owner_key):
+
+    source = identity_contract.address
+    destination = accounts[3]
+
+    meta_transaction = MetaTransaction(
+        from_=source, to=destination, value=123, nonce=delegate.calc_next_nonce(source)
+    )
+    signed_meta_transaction = meta_transaction.signed(owner_key)
+
+    assert delegate.calc_next_nonce(source) == 1
+    delegate.send_signed_meta_transaction(signed_meta_transaction)
+    assert delegate.calc_next_nonce(source) == 2
+
+    meta_transaction = MetaTransaction(
+        from_=source, to=destination, value=123, nonce=delegate.calc_next_nonce(source)
+    )
+    signed_meta_transaction = meta_transaction.signed(owner_key)
+
+    assert delegate.calc_next_nonce(source) == 2
+    delegate.send_signed_meta_transaction(signed_meta_transaction)
+    assert delegate.calc_next_nonce(source) == 3
+
+
+def test_delegated_transaction_invalid_signature(
+    identity, delegate, accounts, account_keys
+):
+    to = accounts[2]
+    value = 1000
+
+    meta_transaction = MetaTransaction(
+        from_=identity.address, to=to, value=value, nonce=0
+    ).signed(account_keys[3])
+
+    with pytest.raises(InvalidMetaTransactionException):
+        delegate.send_signed_meta_transaction(meta_transaction)
+
+
+def test_delegated_transaction_invalid_nonce(identity, delegate, accounts):
+    to = accounts[2]
+    value = 1000
+
+    meta_transaction1 = identity.filled_and_signed_meta_transaction(
+        MetaTransaction(to=to, value=value, nonce=1)
+    )
+    meta_transaction2 = identity.filled_and_signed_meta_transaction(
+        MetaTransaction(to=to, value=value, nonce=1)
+    )
+
+    delegate.send_signed_meta_transaction(meta_transaction1)
+
+    with pytest.raises(InvalidMetaTransactionException):
+        delegate.send_signed_meta_transaction(meta_transaction2)
+
+
+def test_delegated_transaction_invalid_identity_contract(
+    delegate, accounts, account_keys
+):
+    from_ = accounts[1]
+    to = accounts[2]
+    value = 1000
+
+    meta_transaction = MetaTransaction(from_=from_, to=to, value=value, nonce=0).signed(
+        account_keys[3]
+    )
+
+    with pytest.raises(InvalidIdentityContractException):
+        delegate.send_signed_meta_transaction(meta_transaction)
