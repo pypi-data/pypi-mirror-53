f7685e8c7b7198b31b66fb1df459d96d7784443f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:13 +0000

    Add package CoreEngine/0.0.0.2/CoreEngine-0.0.0.2.tar.gz

---
+++ packages/CoreEngine/CoreEngine-0.0.0.2.tar.gz/CoreEngine-0.0.0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'CoreEngine',
+  packages = ['CoreEngine'],
+  version = '0.0.0.2',
+  license='MIT',       
+  description = 'PyGame GameEngine',
+  author = 'CoreTaxxe',
+  author_email = 'coretaxxe@gmail.com',      
+  url = 'https://github.com/coretaxxe/coreengine',
+  download_url = 'https://github.com/coretaxxe/coreengine/archive/v_01.tar.gz',
+  keywords = ['PYGAME', 'GAMEENGINE', 'PYTHON','GAME'],
+  install_requires=[    
+          'pygame',
+      ],
+)
