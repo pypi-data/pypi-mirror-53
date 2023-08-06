ce28ac238afc83b668a0cadd59ab2a4881ea10dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:53 +0000

    Add package nestertyxo90/1.0.0/nestertyxo90-1.0.0.tar.gz

---
+++ packages/nestertyxo90/nestertyxo90-1.0.0.tar.gz/nestertyxo90-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+          name='nestertyxo90',
+          version='1.0.0',
+          py_modules=['nester'],
+          author='Dong Jianyang',
+          author_email='Jianyang_dong@hotmail.com',
+          url='http://www.lightev.com.cn',
+          description='A simple printer of nested lists',
+          )
