e6cea25175cb083d21f995e4c0ce270d6c9fd81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:38 +0000

    Add package notifly-client/0.1.0/notifly_client-0.1.0-py3-none-any.whl

---
+++ packages/notifly-client/notifly_client-0.1.0-py3-none-any.whl/notifly_client/client.py
@@ -0,0 +1,32 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class NotiflyClient:
+    def __init__(self, host, app_id, app_secret):
+        self.host = host
+        self.app_id = app_id
+        self.app_secret = app_secret
+
+    def _post(self, endpoint, payload):
+        url = f'{self.host}{endpoint}'
+        return requests.post(
+            url, json=payload, auth=HTTPBasicAuth(self.app_id, self.app_secret)
+        )
+
+    def send_notification(self, user_id, notification_type, notification_link=None, metadata={}):
+        return self._post(
+            '/api/notification',
+            {
+                'type': notification_type,
+                'link': notification_link,
+                'userId': str(user_id),
+                'appId': self.app_id,
+                'metadata': metadata,
+            },
+        )
+
+    def create_user(self, user_id, email):
+        return self._post(
+            '/api/user', {'appId': self.app_id, 'userId': str(user_id), 'email': email}
+        )
