dd57267213115c195c3bdaaa263768e4f27a905a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:05 +0000

    Add package bwi/0.0.1/bwi-0.0.1-py3-none-any.whl

---
+++ packages/bwi/bwi-0.0.1-py3-none-any.whl/bwi/logging.py
@@ -0,0 +1,18 @@
+def debug(text: str):
+    # FIXME: Send to a queue when we are on BWI Infra
+    print(text)
+
+
+def info(text: str):
+    # FIXME: Send to a queue when we are on BWI Infra
+    print(text)
+
+
+def warning(text: str):
+    # FIXME: Send to a queue when we are on BWI Infra
+    print(text)
+
+
+def error(text: str):
+    # FIXME: Send to a queue when we are on BWI Infra
+    print(text)
