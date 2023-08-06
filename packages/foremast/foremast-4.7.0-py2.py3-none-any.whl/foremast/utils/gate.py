c296d6e7f61bf6bfdb1ffd8469cd715d35375e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:06 +0000

    Add package foremast/4.7.0/foremast-4.7.0-py2.py3-none-any.whl

---
+++ packages/foremast/foremast-4.7.0-py2.py3-none-any.whl/foremast/utils/gate.py
@@ -0,0 +1,47 @@
+#   Foremast - Pipeline Tooling
+#
+#   Copyright 2019 Redbox Automated Retail, LLC
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""Centralized Methods interacting with the Spinnaker Gate API."""
+import logging
+import requests
+
+from ..consts import API_URL, GATE_CA_BUNDLE, GATE_CLIENT_CERT
+
+LOG = logging.getLogger(__name__)
+OAUTH_ENABLED = False
+
+
+def gate_request(method='GET', uri=None, headers=None, data=None, params=None):
+    """Make a request to Gate's API via various auth methods
+
+    Args:
+        method (str): Method to request Gate API; GET or POST
+        uri (str): URI path to gate API
+    """
+    response = None
+
+    url = '{host}{uri}'.format(host=API_URL, uri=uri)
+
+    method = method.upper()
+    if method == 'GET':
+        response = requests.get(url, params=params, headers=headers, verify=GATE_CA_BUNDLE, cert=GATE_CLIENT_CERT)
+    elif method == 'POST':
+        response = requests.post(url, data=data, headers=headers, verify=GATE_CA_BUNDLE, cert=GATE_CLIENT_CERT)
+    elif method == 'DELETE':
+        response = requests.delete(url, headers=headers, verify=GATE_CA_BUNDLE, cert=GATE_CLIENT_CERT)
+    else:
+        raise NotImplementedError
+
+    return response
