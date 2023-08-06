0aca3dba5f23e54f3e83cfa67c6a3a447a53f91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:20 +0000

    Add package sphinxcontrib-editable/0.0.5/sphinxcontrib-editable-0.0.5.tar.gz

---
+++ packages/sphinxcontrib-editable/sphinxcontrib-editable-0.0.5.tar.gz/sphinxcontrib-editable-0.0.5/sphinxcontrib/editable/_static/js/editable.js
@@ -0,0 +1,32 @@
+
+var editor;
+
+window.onload = function() {
+    editor  = new MediumEditor('body p');
+
+
+    editor.subscribe('focus', function(data, editable) { if (!editable.hasAttribute('data-original')) {
+    editable.dataset.original = editable.innerText};
+                                                       });
+
+    editor.subscribe('blur', function(data, editable) {
+    old_html = editable.dataset.original;
+    new_html = editable.innerHTML;
+    if (old_html == new_html) {
+        return;
+    }
+    index = editable.dataset.mediumEditorEditorIndex;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'https://cors-anywhere.herokuapp.com/https://sleepy-harbor-00552.herokuapp.com/', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    data = {
+        index: index,
+        old_html: old_html,
+        new_html: new_html,
+        rendered_html_url: document.URL,
+        rendered_rst_url: document.evaluate('//a[@class="fa fa-github"]', document, null, XPathResult.ANY_TYPE, null).iterateNext().href
+   };
+   xhr.send(JSON.stringify(data))});
+
+
+}
