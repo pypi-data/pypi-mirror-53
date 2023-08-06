0c22fe1c8afd337bbb462e270f9a9596951138d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:50 +0000

    Add package flaskerk/0.5.1/flaskerk-0.5.1.tar.gz

---
+++ packages/flaskerk/flaskerk-0.5.1.tar.gz/flaskerk-0.5.1/flaskerk/view.py
@@ -0,0 +1,28 @@
+from flask import render_template, jsonify, abort
+from flask.views import MethodView
+
+from flaskerk.config import default_config
+
+
+class APIview(MethodView):
+    def __init__(self, *args, **kwargs):
+        view_args = kwargs.pop('view_args', {})
+        self.config = view_args.get('config')
+        super().__init__(*args, **kwargs)
+
+    def get(self):
+        assert self.config.ui in default_config._support_ui
+        ui_file = f'{self.config.ui}.html'
+        return render_template(ui_file, spec_url=self.config.filename)
+
+
+class JSONview(MethodView):
+    def __init__(self, *args, **kwargs):
+        view_args = kwargs.pop('view_args', {})
+        self.config = view_args.get('config')
+        super().__init__(*args, **kwargs)
+
+    def get(self, filename):
+        if filename == self.config.filename:
+            return jsonify(self.config.data)
+        abort(404)
