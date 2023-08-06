f2ce60140ce1cc81fae4ca656d4be96a808d3ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:47 +0000

    Add package sphinx-typlog-theme/0.8.0/sphinx_typlog_theme-0.8.0.tar.gz

---
+++ packages/sphinx-typlog-theme/sphinx_typlog_theme-0.8.0.tar.gz/sphinx_typlog_theme-0.8.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import codecs
+from setuptools import setup
+from sphinx_typlog_theme import __version__
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='sphinx_typlog_theme',
+    version=__version__,
+    description='A typlog Sphinx theme',
+    long_description=readme,
+    author='Hsiaoming Yang',
+    author_email='me@lepture.com',
+    url='https://github.com/typlog/sphinx-typlog-theme',
+    packages=['sphinx_typlog_theme'],
+    include_package_data=True,
+    entry_points={
+        'sphinx.html_themes': [
+            'sphinx_typlog_theme = sphinx_typlog_theme',
+        ]
+    },
+    extras_require={
+        'dev': ['sphinx', 'livereload'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Sphinx',
+        'Framework :: Sphinx :: Theme',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+)
