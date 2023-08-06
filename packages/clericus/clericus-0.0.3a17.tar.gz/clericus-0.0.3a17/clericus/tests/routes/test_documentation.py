98ed5a53d2f5e3cbdd691afdafd316ac14ad837b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:16 +0000

    Add package clericus/0.0.3a17/clericus-0.0.3a17.tar.gz

---
+++ packages/clericus/clericus-0.0.3a17.tar.gz/clericus-0.0.3a17/clericus/tests/routes/test_documentation.py
@@ -0,0 +1,134 @@
+import unittest
+import asyncio
+import json
+from aiohttp.test_utils import make_mocked_request
+
+from ...handler import newMethod, Endpoint
+from ...parsing.fields import BoolField, StringField
+
+from ..test_case import (
+    ClericusTestCase,
+    unittest_run_loop,
+    async_test,
+)
+
+
+class TestDocumentation(ClericusTestCase):
+    async def get_application(self):
+        app = await super().get_application()
+
+        async def process(self, exampleValue, q=""):
+            return {"result": exampleValue + " cow" + q}
+
+        getMethod = newMethod(
+            httpMethod="Get",
+            description="This is a test handler",
+            process=process,
+            urlParameters={
+                "exampleValue": StringField(
+                    description="A string to modify",
+                ),
+            },
+            queryParameters={
+                "q": StringField(
+                    description="A query parameter",
+                    optional=True,
+                ),
+                "moo": StringField(
+                    description="Another query parameter",
+                    optional=False,
+                    default="moo",
+                ),
+            },
+            responseFields={
+                "result": StringField(
+                    description="The value with \"cow\" appended",
+                ),
+            },
+        )
+
+        postMethod = newMethod(
+            httpMethod="Post",
+            description="This is a test handler",
+            process=process,
+            urlParameters={
+                "exampleValue": StringField(
+                    description="A string to modify",
+                ),
+            },
+            bodyParameters={
+                "q": StringField(
+                    description="A body parameter",
+                    optional=True,
+                ),
+                "moo": StringField(
+                    description="Another body parameter",
+                    optional=False,
+                    default="moo",
+                ),
+            },
+            responseFields={
+                "result": StringField(
+                    description="The value with \"cow\" appended",
+                ),
+            },
+        )
+
+        class end(Endpoint):
+            """
+            An example endpoint.
+            """
+            Get = getMethod
+            Post = postMethod
+
+        app.addEndpoint(
+            "/stuff/{exampleValue}/",
+            end,
+            name="Example Endpoint",
+        )
+        return app
+
+    @unittest_run_loop
+    async def testBaseDocumentation(self):
+        resp = await self.client.request("GET", "/")
+        self.assertEqual(resp.status, 200)
+        data = await resp.json()
+        endpoints = data["endpoints"]
+        self.assertGreater(len(endpoints), 1)
+
+        data = next(
+            filter(lambda k: k["path"] == "/stuff/{exampleValue}/", endpoints)
+        )
+        self.assertEqual(data["description"], "An example endpoint.")
+        self.assertEqual(data["name"], "Example Endpoint")
+        self.assertEqual(data["path"], "/stuff/{exampleValue}/")
+        self.assertEqual(
+            data["methods"]["get"]["description"],
+            "This is a test handler",
+        )
+
+        self.assertEqual(
+            data["methods"]["get"]["requestParameters"]["url"]["exampleValue"]
+            ["description"],
+            "A string to modify",
+        )
+
+    @unittest_run_loop
+    async def testEndpointDocumentation(self):
+        resp = await self.client.request("GET", "/documentation/stuff/moo/")
+        self.assertEqual(resp.status, 200)
+        data = await resp.json()
+
+        self.assertEqual(data["description"], "An example endpoint.")
+        self.assertEqual(data["name"], "Example Endpoint")
+        self.assertEqual(data["path"], "/stuff/{exampleValue}/")
+        self.assertEqual(
+            data["methods"]["get"]["description"],
+            "This is a test handler",
+        )
+
+        self.assertEqual(
+            data["methods"]["get"]["requestParameters"]["url"]["exampleValue"]
+            ["description"],
+            "A string to modify",
+        )
