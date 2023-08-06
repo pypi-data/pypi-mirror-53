26cee974051cdac036ac0d108cef617a0787f473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:16 +0000

    Add package z4-solver/2019.10.12/z4-solver-2019.10.12.tar.gz

---
+++ packages/z4-solver/z4-solver-2019.10.12.tar.gz/z4-solver-2019.10.12/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['z4_solver']
+install_requires = \
+['z3-solver>=4.8,<5.0']
+
+setup_kwargs = {
+    'name': 'z4-solver',
+    'version': '2019.10.12',
+    'description': 'z3++',
+    'long_description': None,
+    'author': 'Asger Hautop Drewsen',
+    'author_email': 'asgerdrewsen@gmail.com',
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
