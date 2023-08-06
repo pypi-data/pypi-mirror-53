23bcbab29a183b5cf388649a1d328ce735254440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:08 +0000

    Add package starfish-py/0.5.4/starfish-py-0.5.4.tar.gz

---
+++ packages/starfish-py/starfish-py-0.5.4.tar.gz/starfish-py-0.5.4/tests/integration/test_use_cases/test_13_asset_listing_search.py
@@ -0,0 +1,19 @@
+"""
+    test_13_asset_listing_search
+
+    As a developer building client code to interact with an Ocean marketplace,
+    I need a way to search available asset listings
+
+"""
+
+import secrets
+
+from starfish.asset import DataAsset
+
+
+def test_13_asset_listing_search(resources, surfer_agent):
+    test_data = secrets.token_hex(1024)
+    asset = DataAsset.create('TestAsset', test_data)
+    listing = surfer_agent.register_asset(asset, resources.listing_data)
+    assert(listing)
+    assert(listing.asset)
