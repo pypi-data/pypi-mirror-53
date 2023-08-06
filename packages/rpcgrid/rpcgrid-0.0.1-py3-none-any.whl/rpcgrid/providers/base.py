468c1c7099f0443507fb65b38fdb083870cd6e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:25 +0000

    Add package rpcgrid/0.0.1/rpcgrid-0.0.1-py3-none-any.whl

---
+++ packages/rpcgrid/rpcgrid-0.0.1-py3-none-any.whl/rpcgrid/providers/base.py
@@ -0,0 +1,36 @@
+class BaseProvider:
+    _request_callback = None
+    _response_callback = None
+
+    # TODO: add suppor custom middlewares
+
+    def create(self):
+        raise NotImplementedError("Providers must implement this method")
+
+    def open(self):
+        raise NotImplementedError("Providers must implement this method")
+
+    def close(self):
+        raise NotImplementedError("Providers must implement this method")
+
+    def is_connected(self):
+        raise NotImplementedError("Providers must implement this method")
+
+    def send(self, task):
+        raise NotImplementedError("Providers must implement this method")
+
+    def recv(self):
+        raise NotImplementedError("Providers must implement this method")
+
+    def set_request_callback(self, callback):
+        self._request_callback = callback
+
+    def set_response_callback(self, callback):
+        self._response_callback = callback
+
+    def call_method(self, task):
+        if not self.is_connected():
+            raise ConnectionError('Providers must be connected.')
+        self.send(task)
+        task.status = task.status.PENDING
+        return task
