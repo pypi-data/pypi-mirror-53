a3571077dc58d6d091eca2fb5b84ab695741b01d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:34 +0000

    Add package guardpost/0.0.5/guardpost-0.0.5.tar.gz

---
+++ packages/guardpost/guardpost-0.0.5.tar.gz/guardpost-0.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='guardpost',
+      version='0.0.5',
+      description='Basic framework to handle authentication and authorization in any kind of Python application.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent'
+      ],
+      url='https://github.com/RobertoPrevato/GuardPost',
+      author='RobertoPrevato',
+      author_email='roberto.prevato@gmail.com',
+      keywords='authentication authorization identity claims strategy framework asyncio synchronous',
+      license='MIT',
+      packages=['guardpost',
+                'guardpost.synchronous',
+                'guardpost.asynchronous'],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
