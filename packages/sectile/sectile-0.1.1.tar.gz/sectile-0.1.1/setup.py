1ad6bf10dcc150e55a33cf8272e88c08323e6136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:57 +0000

    Add package sectile/0.1.1/sectile-0.1.1.tar.gz

---
+++ packages/sectile/sectile-0.1.1.tar.gz/sectile-0.1.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sectile',
+    version='0.1.1',
+
+    description='Multidimensional file stitcher',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    license='MIT',
+    url='https://github.com/norm/sectile',
+    author='Mark Norman Francis',
+    author_email='norm@201created.com',
+
+    install_requires=[
+        'toml',
+    ],
+
+    packages=find_packages(exclude=['tests']),
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Pre-processors',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: General',
+    ]
+)
