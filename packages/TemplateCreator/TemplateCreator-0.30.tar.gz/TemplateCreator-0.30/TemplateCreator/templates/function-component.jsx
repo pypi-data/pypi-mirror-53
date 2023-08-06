0bf55074e8360f5f245125b183547ffd27f35d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:17 +0000

    Add package TemplateCreator/0.30/TemplateCreator-0.30.tar.gz

---
+++ packages/TemplateCreator/TemplateCreator-0.30.tar.gz/TemplateCreator-0.30/TemplateCreator/templates/function-component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const {{ component_name }} = (props) => {
+  return (
+    <div>{{ component_name }}</div>
+  );
+};
+
+{{ component_name }}.propTypes = {
+
+};
+
+{{ component_name }}.defaultProps = {
+
+};
+
+export default {{ component_name }};
