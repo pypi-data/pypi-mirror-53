21088b7f1ae665d66cd72941319f2706b92b358a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:58 +0000

    Add package ad-udacitydistributions/1.1/ad_udacitydistributions-1.1.tar.gz

---
+++ packages/ad-udacitydistributions/ad_udacitydistributions-1.1.tar.gz/ad_udacitydistributions-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name = 'ad_udacitydistributions',
+      
+      version = '1.1',
+      
+      description = 'Gaussian and Binomial distributions',
+      
+      packages = ['ad_udacity_distributions'],
+      
+      author = 'Abdel-Dayane Marcos',
+      
+      author_email = 'marcyane7@gmail.com',
+      
+      zip_safe=False)
