36438f4efab53b78603e3d5a51469f51466299d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:53 +0000

    Add package GN/0.1/GN-0.1.tar.gz

---
+++ packages/GN/GN-0.1.tar.gz/GN-0.1/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+with open('README.md') as fh:
+    LONG_DESCRIPTION = fh.read()
+
+
+setup(
+    name='GN',
+    version='0.1',
+    author="zhaofeng-shu33",
+    install_requires=['networkx'],
+    author_email="616545598@qq.com",
+    packages = find_packages(),
+    description="a hierachical community detection algorithm by Girvan Newman",
+    url="https://github.com/zhaofeng-shu33/Girvan-Newman",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    license="Apache License Version 2.0",
+)
