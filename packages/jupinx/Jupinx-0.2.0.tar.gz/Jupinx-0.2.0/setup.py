e7970d99bd7dcaf6c51b3fc558a17c6fc97feddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:51 +0000

    Add package jupinx/0.2.0/Jupinx-0.2.0.tar.gz

---
+++ packages/jupinx/Jupinx-0.2.0.tar.gz/Jupinx-0.2.0/setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import jupinx
+
+from setuptools import setup, find_packages
+
+long_desc = '''
+This package contains the Jupinx cmd and quickstart utilities.
+'''
+VERSION = jupinx.__version__
+
+install_requires = [
+    'docutils', 
+    'nbformat', 
+    'sphinx>=1.8.5', 
+    'dask', 
+    'dask[distributed]',
+    'ipython', 
+    'nbconvert',
+    'notebook',
+    'jupyter_client',
+    'pyzmq>=17.1.3',
+    'sphinxcontrib-bibtex',
+    'sphinxcontrib-jupyter>=0.5.0'
+]
+
+setup(
+    name='Jupinx',
+    version=VERSION,
+    url='https://github.com/QuantEcon/jupinx',
+    download_url='https://github.com/QuantEcon/jupinx/archive/{}.tar.gz'.format(VERSION),
+    license='BSD',
+    author='QuantEcon',
+    author_email='admin@quantecon.org',
+    description='Jupinx extension: Convert your RST files into into different formats like notebook, pdf, html.',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Sphinx',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Framework :: Sphinx :: Extension',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'jupinx-quickstart = jupinx.cmd.quickstart:main',
+            'jupinx = jupinx.cmd.build:main'
+        ]
+    },
+    install_requires=install_requires
+)
