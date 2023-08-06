378cccac6c2d492b6a8a5c7b32846ecb11ffa4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:19 +0000

    Add package onesaitplatform-orange3/1.1.1/onesaitplatform-orange3-1.1.1.tar.gz

---
+++ packages/onesaitplatform-orange3/onesaitplatform-orange3-1.1.1.tar.gz/onesaitplatform-orange3-1.1.1/setup.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+from os import path, walk
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "onesaitplatform-orange3"
+
+VERSION = "1.1.1"
+
+DESCRIPTION = "Add-on containing onesaitplatform widgets"
+LONG_DESCRIPTION = open(path.join(path.dirname(__file__), 'README.md')).read()
+
+LICENSE = "Apache v2.0"
+
+KEYWORDS = (
+    # [PyPi](https://pypi.python.org) packages with keyword "orange3 add-on"
+    # can be installed using the Orange Add-on Manager
+    'onesaitplatform orange3',
+)
+
+PACKAGES = find_packages()
+
+PACKAGE_DATA = {
+    'orangecontrib.onesaitplatform': ['tutorials/*.ows'],
+    'orangecontrib.onesaitplatform.widgets': ['icons/*'],
+}
+
+DATA_FILES = [
+    # Data files that will be installed outside site-packages folder
+]
+
+INSTALL_REQUIRES = [
+    'Orange3',
+    'onesaitplatform-client-services>=1.2.3'
+]
+
+ENTRY_POINTS = {
+    # Entry points that marks this package as an orange add-on. If set, addon will
+    # be shown in the add-ons manager even if not published on PyPi.
+    'orange3.addon': (
+        'onesaitplatform = orangecontrib.onesaitplatform',
+    ),
+    # Entry point used to specify packages containing tutorials accessible
+    # from welcome screen. Tutorials are saved Orange Workflows (.ows files).
+    'orange.widgets.tutorials': (
+        # Syntax: any_text = path.to.package.containing.tutorials
+        'onesaitplatform tutorials = orangecontrib.onesaitplatform.tutorials',
+    ),
+
+    # Entry point used to specify packages containing widgets.
+    'orange.widgets': (
+        # Syntax: category name = path.to.package.containing.widgets
+        # Widget category specification can be seen in
+        #    orangecontrib/example/widgets/__init__.py
+        'OnesaitPlatform = orangecontrib.onesaitplatform.widgets',
+    ),
+
+   
+}
+
+NAMESPACE_PACKAGES = ["orangecontrib"]
+
+TEST_SUITE = "orangecontrib.onesaitplatform.tests.suite"
+
+
+def include_documentation(local_dir, install_dir):
+    global DATA_FILES
+    if 'bdist_wheel' in sys.argv and not path.exists(local_dir):
+        print("Directory '{}' does not exist. "
+              "Please build documentation before running bdist_wheel."
+              .format(path.abspath(local_dir)))
+        sys.exit(0)
+
+    doc_files = []
+    for dirpath, dirs, files in walk(local_dir):
+        doc_files.append((dirpath.replace(local_dir, install_dir),
+                          [path.join(dirpath, f) for f in files]))
+    DATA_FILES.extend(doc_files)
+
+if __name__ == '__main__':
+    #include_documentation('doc/build/html', 'help/orange3-onesaitplatform')
+    setup(
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        license=LICENSE,
+        packages=PACKAGES,
+        package_data=PACKAGE_DATA,
+        data_files=DATA_FILES,
+        install_requires=INSTALL_REQUIRES,
+        entry_points=ENTRY_POINTS,
+        keywords=KEYWORDS,
+        namespace_packages=NAMESPACE_PACKAGES,
+        test_suite=TEST_SUITE,
+        include_package_data=True,
+        zip_safe=False,
+    )
