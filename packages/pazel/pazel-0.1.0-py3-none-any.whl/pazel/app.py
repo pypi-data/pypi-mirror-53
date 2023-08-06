5a6ecb033b4bacfaa09dee5eb2c20e304b222e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:56 +0000

    Add package pazel/0.1.0/pazel-0.1.0-py3-none-any.whl

---
+++ packages/pazel/pazel-0.1.0-py3-none-any.whl/pazel/app.py
@@ -0,0 +1,128 @@
+"""Entrypoint for generating Bazel BUILD files for a Python project."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+
+from pazel.generate_rule import parse_script_and_generate_rule
+from pazel.helpers import get_build_file_path
+from pazel.helpers import is_ignored
+from pazel.helpers import is_python_file
+from pazel.output_build import output_build_file
+from pazel.parse_build import get_ignored_rules
+from pazel.pazel_extensions import parse_pazel_extensions
+
+
+def app(input_path, project_root, contains_pre_installed_packages, pazelrc_path):
+    """Generate BUILD file(s) for a Python script or a directory of Python scripts.
+
+    Args:
+        input_path (str): Path to a Python file or to a directory containing Python file(s) for
+        which BUILD files are generated.
+        project_root (str): Imports in the Python files are relative to this path.
+        contains_pre_installed_packages (bool): Whether the environment is allowed to contain
+            pre-installed packages or whether only the Python standard library is available.
+        pazelrc_path (str): Path to .pazelrc config file for customizing pazel.
+
+    Raises:
+        RuntimeError: input_path does is not a directory or a Python file.
+    """
+    # Parse user-defined extensions to pazel.
+    output_extension, custom_bazel_rules, custom_import_inference_rules, import_name_to_pip_name, \
+        local_import_name_to_dep, requirement_load = parse_pazel_extensions(pazelrc_path)
+
+    # Handle directories.
+    if os.path.isdir(input_path):
+        # Traverse the directory recursively.
+        for dirpath, _, filenames in os.walk(input_path):
+            build_source = ''
+
+            # Parse ignored rules in an existing BUILD file, if any.
+            build_file_path = get_build_file_path(dirpath)
+            ignored_rules = get_ignored_rules(build_file_path)
+
+            for filename in sorted(filenames):
+                path = os.path.join(dirpath, filename)
+
+                # If a Python file is met and it is not in the list of ignored rules,
+                # generate a Bazel rule for it.
+                if is_python_file(path) and not is_ignored(path, ignored_rules):
+                    new_rule = parse_script_and_generate_rule(path, project_root,
+                                                              contains_pre_installed_packages,
+                                                              custom_bazel_rules,
+                                                              custom_import_inference_rules,
+                                                              import_name_to_pip_name,
+                                                              local_import_name_to_dep)
+
+                    # Add the new rule and a newline between it and any previous rules.
+                    if new_rule:
+                        if build_source:
+                            build_source += 2*'\n'
+
+                        build_source += new_rule
+
+            # If Python files were found, output the BUILD file.
+            if build_source != '' or ignored_rules:
+                output_build_file(build_source, ignored_rules, output_extension, custom_bazel_rules,
+                                  build_file_path, requirement_load)
+    # Handle single Python file.
+    elif is_python_file(input_path):
+        build_source = ''
+
+        # Parse ignored rules in an existing BUILD file, if any.
+        build_file_path = get_build_file_path(input_path)
+        ignored_rules = get_ignored_rules(build_file_path)
+
+        # Check that the script is not in the list of ignored rules.
+        if not is_ignored(input_path, ignored_rules):
+            build_source = parse_script_and_generate_rule(input_path, project_root,
+                                                          contains_pre_installed_packages,
+                                                          custom_bazel_rules,
+                                                          custom_import_inference_rules,
+                                                          import_name_to_pip_name,
+                                                          local_import_name_to_dep)
+
+        # If Python files were found, output the BUILD file.
+        if build_source != '' or ignored_rules:
+            output_build_file(build_source, ignored_rules, output_extension, custom_bazel_rules,
+                              build_file_path, requirement_load)
+    else:
+        raise RuntimeError("Invalid input path %s." % input_path)
+
+
+def main():
+    """Parse command-line flags and generate the BUILD files accordingly."""
+    parser = argparse.ArgumentParser(description='Generate Bazel BUILD files for a Python project.')
+
+    working_directory = os.getcwd()
+    default_pazelrc_path = os.path.join(working_directory, '.pazelrc')
+
+    parser.add_argument('input_path', nargs='?', type=str, default=working_directory,
+                        help='Target Python file or directory of Python files.'
+                        ' Defaults to the current working directory.')
+    parser.add_argument('-r', '--project-root', type=str, default=working_directory,
+                        help='Project root directory. Imports are relative to this path.'
+                        ' Defaults to the current working directory.')
+    parser.add_argument('-p', '--pre-installed-packages', action='store_true',
+                        help='Target will be run in an environment with packages pre-installed.'
+                        ' Affects which packages are listed as pip-installable.')
+    parser.add_argument('-c', '--pazelrc', type=str, default=default_pazelrc_path,
+                        help='Path to .pazelrc file.')
+
+    args = parser.parse_args()
+
+    # If the user specified custom .pazelrc file, then check that it exists.
+    custom_pazelrc_path = args.pazelrc != default_pazelrc_path
+
+    if custom_pazelrc_path:
+        assert os.path.isfile(args.pazelrc), ".pazelrc file %s not found." % args.pazelrc
+
+    app(args.input_path, args.project_root, args.pre_installed_packages, args.pazelrc)
+    print('Generated BUILD files for %s.' % args.input_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pazel/pazel-0.1.0-py3-none-any.whl/pazel/helpers.py
@@ -0,0 +1,215 @@
+"""Helper functions for generating Bazel BUILD files for a Python project."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import ast
+import importlib
+import os
+import platform
+import sys
+import traceback
+
+
+def contains_python_file(directory):
+    """Check if the given directory contains at least one .py/.pyc file.
+
+    Args:
+        directory (str):
+
+    Returns:
+        contains_py (bool): Whether the directory contains at least one Python file.
+    """
+    files = os.listdir(directory)
+    contains_py = any(f.endswith('.py') or f.endswith('.pyc') for f in files)
+
+    return contains_py
+
+
+def get_build_file_path(path):
+    """Get path to a BUILD file next to a given path.
+
+    Args:
+        path (str): Path to a file or directory.
+
+    Returns
+        build_file_path (str): Path to the BUILD in the given directory or in the directory
+            containing the given file.
+    """
+    if os.path.isdir(path):
+        directory = path
+    else:
+        directory = os.path.dirpath(path)
+
+    build_file_path = os.path.join(directory, 'BUILD')
+
+    return build_file_path
+
+
+def is_ignored(script_path, ignored_rules):
+    """Check whether the given script is in ignored rules.
+
+    Args:
+        script_path (str): Path to a Python script.
+        ignored_rules (list of str): Ignored Bazel rules.
+
+    Returns:
+        ignored (bool): Whether the script should be ignored.
+    """
+    ignored = False
+
+    script_file_name = os.path.basename(script_path)
+
+    for ignored_rule in ignored_rules:
+        # Parse the rule to an AST node.
+        try:
+            node = ast.parse(ignored_rule)
+        except SyntaxError:
+            raise SyntaxError("Invalid syntax in an ignored rule %s." % ignored_rule)
+
+        assert len(node.body) == 1, "Unsupported rule type %s." % ignored_rule
+
+        # Check keyword arguments in the rule. If the 'srcs' argument contains the script file name,
+        # then the script should be ignored.
+        func_call = node.body[0].value
+
+        for keyword in func_call.keywords:
+            if keyword.arg == 'srcs':
+                elements = keyword.value.elts
+
+                assert len(elements) == 1, \
+                    "Multiple source files not supported in %s." % ignored_rule
+
+                if elements[0].s == script_file_name:
+                    ignored = True
+                    break
+
+        # The script file name may also given as a positional argument.
+        for positional in func_call.args:
+            if positional.s == script_file_name:
+                ignored = True
+                break
+
+    return ignored
+
+
+def is_python_file(path):
+    """Check whether the file in the given path is a Python file.
+
+    Args:
+        path (str): Path to a file.
+
+    Returns:
+        valid (bool): The file in path is a Python file.
+    """
+    valid = False
+
+    if os.path.isfile(path) and path.endswith('.py'):
+        valid = True
+
+    return valid
+
+
+def _is_in_stdlib(module, some_object):
+    """Check if a given module is part of the Python standard library."""
+    # Clear PYTHONPATH temporarily and try importing the given module.
+    original_sys_path = sys.path
+    lib_path = os.path.dirname(traceback.__file__)
+    sys.path = [lib_path]
+
+    # On Mac, some extra library paths are required.
+    if 'darwin' in platform.system().lower():
+        for path in original_sys_path:
+            if 'site-packages' not in path:
+                sys.path.append(path)
+
+    in_stdlib = False
+
+    try:
+        module = importlib.import_module(module)
+
+        if some_object:
+            getattr(module, some_object)
+
+        in_stdlib = True
+    except (ImportError, AttributeError):
+        pass
+
+    sys.path = original_sys_path
+
+    return in_stdlib
+
+
+def is_installed(module, some_object=None, contains_pre_installed_packages=False):
+    """Check if a given module is installed and whether some_object is found in it.
+
+    Args:
+        module (str): Name of a module.
+        some_object (str): Name of some object in the module. Can be None.
+        contains_pre_installed_packages (bool): Whether the environment contains external packages.
+
+    Returns:
+        installed (bool): The module is installed in the current environment.
+    """
+    installed = False
+
+    # If the application runs inside e.g. a virtualenv that already contains some requirements,
+    # then try importing the module. If it fails, then the module is not yet installed.
+    if contains_pre_installed_packages:
+        try:
+            module = importlib.import_module(module)
+
+            if some_object:
+                getattr(module, some_object)
+
+            installed = True
+        except (ImportError, AttributeError):
+            installed = False
+    else:   # If we have a clean install, then check if the module is in the standard library.
+        installed = _is_in_stdlib(module, some_object)
+
+    return installed
+
+
+def parse_enclosed_expression(source, start, opening_token):
+    """Parse an expression enclosed by a token and its counterpart.
+
+    Args:
+        source (str): Source code of a Bazel BUILD file, for example.
+        start (int): Index at which an expression starts.
+        opening_token (str): A character '(' or '[' that opens an expression.
+
+    Returns:
+        expression (str): The whole expression that contains the opening and closing tokens.
+
+    Raises:
+        NotImplementedError: If parsing is not implemented for the given opening token.
+    """
+    if opening_token == '(':
+        closing_token = ')'
+    elif opening_token == '[':
+        closing_token = ']'
+    else:
+        raise NotImplementedError("No closing token defined for %s." % opening_token)
+
+    start2 = source.find(opening_token, start)
+    assert start2 > start, "Could not locate the opening token %s." % opening_token
+    open_tokens = 0
+    end = None
+
+    for end_idx, char in enumerate(source[start2:], start2):
+        if char == opening_token:
+            open_tokens += 1
+        elif char == closing_token:
+            open_tokens -= 1
+
+        if open_tokens == 0:
+            end = end_idx + 1
+            break
+
+    assert end, "Could not locate the closing token %s." % closing_token
+
+    expression = source[start:end]
+
+    return expression
