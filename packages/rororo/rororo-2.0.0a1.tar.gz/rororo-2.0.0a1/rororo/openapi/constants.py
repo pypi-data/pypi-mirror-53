27a1748a54436da9f4fdecdff666b62c6e99d0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:14 +0000

    Add package rororo/2.0.0a1/rororo-2.0.0a1.tar.gz

---
+++ packages/rororo/rororo-2.0.0a1.tar.gz/rororo-2.0.0a1/rororo/openapi/constants.py
@@ -0,0 +1,8 @@
+#: Key to store valid OpenAPI context within the ``web.Request`` instance
+OPENAPI_CONTEXT_REQUEST_KEY = "rororo_openapi_context"
+
+#: Key to store OpenAPI schema within the ``web.Application`` instance
+OPENAPI_SCHEMA_APP_KEY = "rororo_openapi_schema"
+
+#: Key to store OpenAPI spec within the ``web.Application`` instance
+OPENAPI_SPEC_APP_KEY = "rororo_openapi_spec"
