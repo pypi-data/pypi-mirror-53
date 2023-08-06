52e2c83db1293a59a499717bd197c9f366d2d75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package pymatgen/2019.10.3/pymatgen-2019.10.3-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/pymatgen/pymatgen-2019.10.3-cp37-cp37m-macosx_10_9_x86_64.whl/pymatgen/io/tests/test_ase.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+# Copyright (c) Pymatgen Development Team.
+# Distributed under the terms of the MIT License.
+
+
+"""
+Created on Mar 8, 2012
+"""
+
+
+__author__ = "Shyue Ping Ong"
+__copyright__ = "Copyright 2012, The Materials Project"
+__version__ = "0.1"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "shyuep@gmail.com"
+__date__ = "Mar 8, 2012"
+
+import unittest
+import os
+
+from pymatgen import Composition
+from pymatgen.io.vasp.inputs import Poscar
+import pymatgen.io.ase as aio
+
+test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
+                        'test_files')
+
+
+class AseAtomsAdaptorTest(unittest.TestCase):
+
+    @unittest.skipIf(not aio.ase_loaded, "ASE not loaded.")
+    def test_get_atoms(self):
+        p = Poscar.from_file(os.path.join(test_dir, 'POSCAR'))
+        structure = p.structure
+        atoms = aio.AseAtomsAdaptor.get_atoms(structure)
+        ase_composition = Composition(atoms.get_chemical_formula())
+        self.assertEqual(ase_composition, structure.composition)
+
+    @unittest.skipIf(not aio.ase_loaded, "ASE not loaded.")
+    def test_get_structure(self):
+        p = Poscar.from_file(os.path.join(test_dir, 'POSCAR'))
+        atoms = aio.AseAtomsAdaptor.get_atoms(p.structure)
+        self.assertEqual(aio.AseAtomsAdaptor.get_structure(atoms).formula,
+                         "Fe4 P4 O16")
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    if aio.ase_loaded:
+        unittest.main()
+    else:
+        print("ASE not loaded. Skipping tests")
