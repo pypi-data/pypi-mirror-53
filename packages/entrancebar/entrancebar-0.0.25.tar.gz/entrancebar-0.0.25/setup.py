a8f42245a9ba3c443a5ac289c11b3a8c5777804e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:13 +0000

    Add package entrancebar/0.0.25/entrancebar-0.0.25.tar.gz

---
+++ packages/entrancebar/entrancebar-0.0.25.tar.gz/entrancebar-0.0.25/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import pathlib
+
+setuptools.setup(
+    name="entrancebar",
+    version="0.0.25",
+    author="Chenwe_i_lin",
+    author_email="1846913566@qq.com",
+    description="importext",
+    long_description=pathlib.Path("./description.md").read_text("utf-8") if pathlib.Path("./description.md").exists() else "",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
