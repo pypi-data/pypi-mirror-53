4fca4f873b56c161e4482457424371b059efed25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:05 +0000

    Add package bwi/0.0.1/bwi-0.0.1.tar.gz

---
+++ packages/bwi/bwi-0.0.1.tar.gz/bwi-0.0.1/bwi/metrics.py
@@ -0,0 +1,13 @@
+def inc(name: str, value: int):
+    # FIXME: Send to a queue when we are on BWI Infra
+    print(name, value)
+
+
+def dec(name: str, value: int):
+    # FIXME: Send to a queue when we are on BWI Infra
+    print(name, value)
+
+
+def value(name: str, value: int):
+    # FIXME: Send to a queue when we are on BWI Infra
+    print(name, value)
