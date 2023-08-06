17e5cd8c0940ef694291eafc6220a088eb346368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:42 +0000

    Add package SchemaChecker/0.0.3.dev74/SchemaChecker-0.0.3.dev74.tar.gz

---
+++ packages/SchemaChecker/SchemaChecker-0.0.3.dev74.tar.gz/SchemaChecker-0.0.3.dev74/src/schemachecker/stat/exceptions.py
@@ -0,0 +1,29 @@
+class InternalStatError(Exception):
+    def __init__(self) -> None:
+        self.message = None
+
+    def __str__(self):
+        return self.message
+
+
+class TokenizerError(InternalStatError):
+    def __init__(self, expression):
+        self.message = (f'Ошибка при лексическом анализе выражения '
+                        f'{expression}')
+
+
+class InputError(InternalStatError):
+    def __init__(self, xml_file, ex):
+        self.message = f'Ошибка при обработке файла {xml_file} ({ex})'
+
+
+class OkudError(InternalStatError):
+    def __init__(self, xml_file):
+        self.message = (f'Не найден обязательный атрибут "OKUD" '
+                        f'в разделе metaForm в файле {xml_file}')
+
+
+class InterpreterError(InternalStatError):
+    def __init__(self, expression):
+        self.message = (f'Ошибка при интерпретации выражения '
+                        f'{expression}')
