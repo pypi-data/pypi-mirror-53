168578b5c561bf7331f491420b305de4d1c188b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:06 +0000

    Add package netrange/0.0.2/netrange-0.0.2.tar.gz

---
+++ packages/netrange/netrange-0.0.2.tar.gz/netrange-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(name='netrange',
+      version='0.0.2',
+      description='Script to range multiple IPs and ports',
+      url='https://github.com/muhannadengineer/iprange',
+      author='Muhannad Alghamdi',
+      author_email='muhannadengineer@gmail.com',
+      license='MIT',
+      packages=['iprange'],
+      entry_points={
+          'console_scripts': [
+              'iprange = iprange.cli:main'
+          ]
+      },
+      keywords='ip address port range')
