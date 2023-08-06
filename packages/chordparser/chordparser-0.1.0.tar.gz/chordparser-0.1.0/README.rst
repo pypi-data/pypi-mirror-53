f69a4d9fe1acc776a94e3d2329486d984cf94d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:33 +0000

    Add package chordparser/0.1.0/chordparser-0.1.0.tar.gz

---
+++ packages/chordparser/chordparser-0.1.0.tar.gz/chordparser-0.1.0/README.rst
@@ -0,0 +1,22 @@
+===========
+chordparser
+===========
+
+A Python 3 module to parse ChordPro files
+
+.. image:: https://travis-ci.com/titus-ong/chordparser.svg
+   :alt: build status
+   :target: https://travis-ci.org/titus-ong/chordparser
+
+.. image:: https://coveralls.io/repos/github/titus-ong/chordparser/badge.svg
+   :alt: coverage
+   :target: https://coveralls.io/github/titus-ong/chordparser
+
+
+.. image:: https://img.shields.io/pypi/v/chordparser.svg
+   :target: https://pypi.org/pypi/chordparser
+   :alt: downloads
+
+.. image:: https://img.shields.io/pypi/pyversions/chordparser.svg
+   :target: https://pypi.org/pypi/chordparser
+   :alt: downloads
---
+++ packages/chordparser/chordparser-0.1.0.tar.gz/chordparser-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='chordparser',
+      version='0.1.0',
+      description='Parse ChordPro files',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Text Processing :: Markup',
+          ],
+      keywords='chord sheet music parse',
+      url='http://github.com/titus-ong/chordparser',
+      author='Titus Ong',
+      author_email='titusongyl@gmail.com',
+      license='MIT',
+      packages=find_packages(where='src'),
+      package_dir={'': 'src'},
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      install_requires=[],
+      zip_safe=False)
