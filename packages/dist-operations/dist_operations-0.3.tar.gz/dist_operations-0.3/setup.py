4945308f234b193f933c59c5befd24568dec8c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:57 +0000

    Add package dist-operations/0.3/dist_operations-0.3.tar.gz

---
+++ packages/dist-operations/dist_operations-0.3.tar.gz/dist_operations-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_operations',
+      version='0.3',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_operations'],
+      zip_safe=False)
