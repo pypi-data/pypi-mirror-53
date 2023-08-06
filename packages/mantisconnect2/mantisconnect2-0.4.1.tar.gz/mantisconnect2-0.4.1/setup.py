2cd5f504cc85174f8f73ec7e77b24d8f7c3d91b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:42 +0000

    Add package mantisconnect2/0.4.1/mantisconnect2-0.4.1.tar.gz

---
+++ packages/mantisconnect2/mantisconnect2-0.4.1.tar.gz/mantisconnect2-0.4.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools import find_packages
+import mantisconnect2
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name=mantisconnect2.__module_name__,
+    version=mantisconnect2.__version__,
+    description=mantisconnect2.__doc__.strip(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ji Haijun',
+    author_email='tigerking1218@gmail.com',
+    url='https://github.com/tigerking/mantisconnect2',
+    license=mantisconnect2.__license__,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ]
+)
