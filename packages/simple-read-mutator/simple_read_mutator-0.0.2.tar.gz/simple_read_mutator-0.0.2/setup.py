eeb575592eeb58f1abb7e42a84d9fd3732fe4982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:27 +0000

    Add package simple-read-mutator/0.0.2/simple_read_mutator-0.0.2.tar.gz

---
+++ packages/simple-read-mutator/simple_read_mutator-0.0.2.tar.gz/simple_read_mutator-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='simple_read_mutator',
+      version='0.0.2',
+      description='Simple Read Mutator',
+      url='http://github.com/ivargr/simple_read_mutator',
+      author='Ivar Grytten',
+      author_email='',
+      license='MIT',
+      packages=['simple_read_mutator'],
+      zip_safe=False,
+      install_requires=['numpy'],
+      classifiers=[
+            'Programming Language :: Python :: 3'
+      ])
+
+"""
+To update package:
+#Update version number manually in this file
+
+sudo python3 setup.py sdist
+sudo python3 setup.py bdist_wheel
+twine upload dist/simple_read_mutator-0.0.2.tar.gz
+"""
