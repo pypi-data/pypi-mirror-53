21bc43c6fe2ad1c5dc3c9b209a6255fb9ab7c0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:22 +0000

    Add package instrumental-dl/0.1/instrumental_dl-0.1-py3-none-any.whl

---
+++ packages/instrumental-dl/instrumental_dl-0.1-py3-none-any.whl/instrumental_dl/youtube_dl_wrapper.py
@@ -0,0 +1,35 @@
+import youtube_dl
+
+from .url_query import get_urls
+from .logger.logger import Logger
+from .common.path import goto_music
+
+
+class YoutubeDL:
+    """A class used as a wrapper for youtube-dl.
+
+    Attributes:
+        options -- The options used to download and convert
+                   using youtube-dl.
+    """
+
+    def __init__(self, logger):
+        self.options = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'logger': Logger(),
+            'progress_hooks': [logger.hook],
+            'nocheckcertificate': True,
+            'outtmpl': '%(title)s.%(ext)s',
+        }
+
+    def download_songs(self, song_names):
+        """Downloads all of the instrumentals in song_names using youtube-dl."""
+        goto_music()
+        urls = get_urls(song_names)
+        with youtube_dl.YoutubeDL(self.options) as ydl:
+            ydl.download(urls)
