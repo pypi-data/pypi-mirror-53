a026e1957f86a069927fc1fdf153796d6f0af086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:10 +0000

    Add package headliner/0.0.5/headliner-0.0.5-py3-none-any.whl

---
+++ packages/headliner/headliner-0.0.5-py3-none-any.whl/headliner/evaluation/bleu_scorer.py
@@ -0,0 +1,21 @@
+import numpy as np
+from typing import Dict, Union
+from nltk.translate.bleu_score import sentence_bleu
+from headliner.evaluation.scorer import Scorer
+
+
+class BleuScorer(Scorer):
+    """
+    Provides BLEU score for a model prediction.
+    """
+
+    def __init__(self, tokens_to_ignore=None, weights=(0.25, 0.25, 0.25, 0.25)) -> None:
+        self.tokens_to_exclude = tokens_to_ignore or []
+        self.weights = weights
+
+    def __call__(self, prediction: [Dict[str, Union[str, np.array]]]) -> float:
+        tokens_predicted = prediction['predicted_text'].split()
+        tokens_output = prediction['preprocessed_text'][1].split()
+        tokens_predicted_filtered = [t for t in tokens_predicted if t not in self.tokens_to_exclude]
+        tokens_output_filtered = [t for t in tokens_output if t not in self.tokens_to_exclude]
+        return sentence_bleu([tokens_output_filtered], tokens_predicted_filtered, weights=self.weights)
