5803b609c77f338bcc2c838ad3478347c196ef50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:35 +0000

    Add package jdTranslationHelper/1.0/jdTranslationHelper-1.0.tar.gz

---
+++ packages/jdTranslationHelper/jdTranslationHelper-1.0.tar.gz/jdTranslationHelper-1.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import find_packages, setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
+setup(
+    name='jdTranslationHelper',
+    version='1.0',
+    description='A simple API for translating your programs.',
+    long_description="Look at the Homepage for the documentation",
+    url='https://gitlab.com/JakobDev/jdTranslationHelper',
+    author='JakobDev',
+    classifiers=[
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='translation localization JakobDev ',
+    packages=find_packages(),
+)
