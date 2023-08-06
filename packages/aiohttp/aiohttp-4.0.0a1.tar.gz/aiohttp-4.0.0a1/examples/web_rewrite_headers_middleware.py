356f24a96b70b8f602900877346baa52563c0233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:54 +0000

    Add package aiohttp/4.0.0a1/aiohttp-4.0.0a1.tar.gz

---
+++ packages/aiohttp/aiohttp-4.0.0a1.tar.gz/aiohttp-4.0.0a1/examples/web_rewrite_headers_middleware.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Example for rewriting response headers by middleware.
+"""
+
+from aiohttp import web
+
+
+async def handler(request):
+    return web.Response(text="Everything is fine")
+
+
+async def middleware(request, handler):
+    try:
+        response = await handler(request)
+    except web.HTTPException as exc:
+        raise exc
+    if not response.prepared:
+        response.headers['SERVER'] = "Secured Server Software"
+    return response
+
+
+def init():
+    app = web.Application(middlewares=[middleware])
+    app.router.add_get('/', handler)
+    return app
+
+
+web.run_app(init())
