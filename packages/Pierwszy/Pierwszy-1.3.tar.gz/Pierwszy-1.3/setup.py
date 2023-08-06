a6d80f481d05ae998a94b18f60e5116d78490e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:07 +0000

    Add package Pierwszy/1.3/Pierwszy-1.3.tar.gz

---
+++ packages/Pierwszy/Pierwszy-1.3.tar.gz/Pierwszy-1.3/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pierwszy',
+      version='1.3',
+      description='Python Distribution Utilities',
+      author='Jerzy Grynczewski',
+      author_email='jgrynczewski@gmail.com',
+     )
