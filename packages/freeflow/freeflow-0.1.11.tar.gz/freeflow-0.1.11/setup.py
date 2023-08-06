8140772b495057b0d97f574a740497e771d1f05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:44 +0000

    Add package freeflow/0.1.11/freeflow-0.1.11.tar.gz

---
+++ packages/freeflow/freeflow-0.1.11.tar.gz/freeflow-0.1.11/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(name='freeflow',
+      version='0.1.11',
+      description='Airflow development and deployment, simplified.',
+      url='http://github.com/feryandi/freeflow',
+      author='Feryandi Nurdiantoro',
+      author_email='feryandi.n@gmail.com',
+      license='Apache License 2.0',
+      scripts=['freeflow/bin/freeflow'],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+        'flake8>=3.6.0',
+        'google-cloud-storage==1.13.2',
+        'six==1.12.0',
+        'pytest==4.1.1'
+      ],
+      zip_safe=False)
