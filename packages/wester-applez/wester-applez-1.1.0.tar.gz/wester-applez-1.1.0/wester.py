be421b9df2778924496b54e8b938014a6cd9e9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:17 +0000

    Add package wester-applez/1.1.0/wester-applez-1.1.0.tar.gz

---
+++ packages/wester-applez/wester-applez-1.1.0.tar.gz/wester-applez-1.1.0/wester.py
@@ -0,0 +1,10 @@
+"""这是个方法名为print_lol 参数为任何的列表"""
+def print_lol(the_list, level):
+	"""第一个参数是列表，对有内嵌列表的递归输出到屏幕,各占一行，第二个参数进行Tab缩进处理"""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item,level+1)
+		else:
+			for index in range(level):
+				print("\t",end='')
+			print(each_item)
