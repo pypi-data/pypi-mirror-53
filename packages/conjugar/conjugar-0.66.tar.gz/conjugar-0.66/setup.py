3036d67e72d83ca950ed43bfc78ad0f8fa61d636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:03 +0000

    Add package conjugar/0.66/conjugar-0.66.tar.gz

---
+++ packages/conjugar/conjugar-0.66.tar.gz/conjugar-0.66/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+      name="conjugar",
+      version = "0.66",
+      description="handle,.in progressing..,APIs",
+      author="dapeli",
+      url="https://github.com/ihgazni2/conjugar",
+      author_email='terryinzaghi@163.com', 
+      license="MIT",
+      long_description = "refer to .md files in https://github.com/ihgazni2/conjugar",
+      classifiers=[
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Programming Language :: Python',
+          ],
+      packages= find_packages(),
+      py_modules=['conjugar'], 
+      )
+
+
+# python3 setup.py bdist --formats=tar
+# python3 setup.py sdist
+
