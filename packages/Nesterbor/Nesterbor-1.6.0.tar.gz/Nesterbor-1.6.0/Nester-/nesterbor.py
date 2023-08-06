b72a84312dc2ff6bf5430b7ef9379869741124ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:35 +0000

    Add package Nesterbor/1.6.0/Nesterbor-1.6.0.tar.gz

---
+++ packages/Nesterbor/Nesterbor-1.6.0.tar.gz/Nesterbor-1.6.0/Nester-/nesterbor.py
@@ -0,0 +1,16 @@
+'''This is the "nester.py" module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists.'''
+import sys
+def print_lol(the_list,indent = False, level=0,fh=sys.stdout):
+	'''This function takes a positional argument called “the_list", which is any
+	Python list (of, possibly, nested lists). Each data item in the provided list
+	is (recursively) printed to the screen on its own line.'''
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item,indent ,level+1, fh)
+		else:
+			if indent ==True:
+				for tab_stop in range(level):
+					print('\t',end='', file=fh)
+
+			print(each_item, file=fh)
