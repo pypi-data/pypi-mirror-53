359f8402612f5a46abee48d821ea021da7fc385b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:04 +0000

    Add package userstorage/0.3/userstorage-0.3.tar.gz

---
+++ packages/userstorage/userstorage-0.3.tar.gz/userstorage-0.3/userstorage/osutil.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2019 Nir Soffer
+# This program is free software; see LICENSE for more info.
+
+import errno
+import grp
+import logging
+import os
+import pwd
+import subprocess
+
+log = logging.getLogger("userstorage")
+
+
+def chown(path, uid, gid):
+    user = pwd.getpwuid(uid).pw_name
+    group = grp.getgrgid(gid).gr_name
+    user_group = "{}:{}".format(user, group)
+    log.debug("Changing %s ownership to %s", path, user_group)
+    subprocess.check_call(["sudo", "chown", "-R", user_group, path])
+
+
+def create_dir(path):
+    log.debug("Creating directory %s", path)
+    try:
+        os.makedirs(path)
+    except EnvironmentError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+def remove_file(path):
+    log.debug("Removing file %s", path)
+    try:
+        os.remove(path)
+    except EnvironmentError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
+
+def remove_dir(path):
+    log.debug("Removing directory %s", path)
+    try:
+        os.rmdir(path)
+    except EnvironmentError as e:
+        if e.errno != errno.ENOENT:
+            raise
