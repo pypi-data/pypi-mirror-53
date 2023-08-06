abed8876fa0068a806f7e4b3bee77afff0d71668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:54 +0000

    Add package uhome/0.1.5/uhome-0.1.5.tar.gz

---
+++ packages/uhome/uhome-0.1.5.tar.gz/uhome-0.1.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="uhome",
+    version="0.1.5",
+    author="Almir Delkic",
+    description="Wrapper for communication with Uponor Smatrix Wave PLUS Smart Home Gateway R-167 aka U@home.",
+    url="https://github.com/almirdelkic/uhome",
+    packages=setuptools.find_packages(),
+    license='GNU GPLv3',
+    install_requires=[
+        'requests',
+    ],
+)
