66a9e60b29c5597543c159777f3170364d131bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:02 +0000

    Add package galacteek/0.4.16/galacteek-0.4.16-py3-none-any.whl

---
+++ packages/galacteek/galacteek-0.4.16-py3-none-any.whl/galacteek/ui/hashmarks.py
@@ -0,0 +1,58 @@
+from PyQt5.QtCore import QCoreApplication
+
+from galacteek.ipfs.ipfsops import *
+from galacteek.ipfs.cidhelpers import *
+
+from .helpers import *
+from .dialogs import *
+from .widgets import *
+from .i18n import *
+
+
+def iPath():
+    return QCoreApplication.translate('HashmarksViewForm', 'Path')
+
+
+def iTitle():
+    return QCoreApplication.translate('HashmarksViewForm', 'Title')
+
+
+def iShared():
+    return QCoreApplication.translate('HashmarksViewForm', 'Shared')
+
+
+def iDate():
+    return QCoreApplication.translate('HashmarksViewForm', 'Date')
+
+
+def iTimestamp():
+    return QCoreApplication.translate('HashmarksViewForm', 'Timestamp')
+
+
+def iAlreadyHashmarked():
+    return QCoreApplication.translate('HashmarksViewForm',
+                                      'Already Hashmarked')
+
+
+def iImportHashmark():
+    return QCoreApplication.translate('HashmarksViewForm',
+                                      'Import hashmark to')
+
+
+def iNetworkMarks():
+    return QCoreApplication.translate('HashmarksViewForm', 'Network hashmarks')
+
+
+def iFeeds():
+    return QCoreApplication.translate('HashmarksViewForm', 'Feeds')
+
+
+def addHashmark(hashmarks, path, title, description='', stats={},
+                pin=False, pinRecursive=False):
+    if hashmarks.find(path):
+        messageBox(iAlreadyHashmarked())
+        return False
+
+    runDialog(AddHashmarkDialog, hashmarks, path, title, description, stats,
+              pin=pin, pinRecursive=pinRecursive)
+    return True
