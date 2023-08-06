d8877c26cddcbaa4c243088eb55859dbc48284d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:58 +0000

    Add package asyncify/0.1.0/asyncify-0.1.0-py3-none-any.whl

---
+++ packages/asyncify/asyncify-0.1.0-py3-none-any.whl/asyncify.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""Asyncify your code
+
+...or else...
+"""
+from asyncio import coroutine
+from asyncio import get_event_loop
+from functools import partial
+from functools import wraps
+from typing import Any
+from typing import Callable
+from typing import TypeVar
+from typing import cast
+
+FuncType = Callable[..., Any]
+F = TypeVar("F", bound=FuncType)
+
+
+def asyncify(funk: F) -> F:
+    """
+
+    :param funk:
+    :return:
+    """
+
+    @coroutine
+    @wraps(funk)
+    def afunk(*args, loop=None, executor=None, **kwargs):
+        """
+
+        :param args:
+        :param loop:
+        :param executor:
+        :param kwargs:
+        :return:
+        """
+        loop = loop if loop else get_event_loop() 
+        pfunc = partial(funk, *args, **kwargs)
+        return loop.run_in_executor(executor, pfunc)
+
+    return cast(F, afunk)
+
+a = asyncify
+async = async
+
