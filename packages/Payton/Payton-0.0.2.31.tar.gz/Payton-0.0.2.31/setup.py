498b4ee9be51ee1cc8f4645d081a1a932d75f089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:31 +0000

    Add package Payton/0.0.2.31/Payton-0.0.2.31.tar.gz

---
+++ packages/Payton/Payton-0.0.2.31.tar.gz/Payton-0.0.2.31/setup.py
@@ -0,0 +1,29 @@
+# from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
+
+setup(
+    name="Payton",
+    version="0.0.2.31",
+    author="Sinan ISLEKDEMIR",
+    author_email="sinan@islekdemir.com",
+    # Packages
+    packages=find_packages(),
+    # Include additional files into the package
+    include_package_data=True,
+    # Details
+    url="https://github.com/sinanislekdemir/payton",
+    #
+    license="GNU GPLv3",
+    description="Payton 3D Kickstart Toolkit",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    # Dependent packages (distributions)
+    install_requires=[
+        "numpy",
+        "Pillow",
+        "PyOpenGL",
+        "pyrr",
+        "PySDL2",
+        "read",
+    ],
+)
