7d0fe94d49347a5ec7c2b144e67a0426c3c55d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:50 +0000

    Add package sciexp2-expdef/2.0.1/sciexp2-expdef-2.0.1.tar.gz

---
+++ packages/sciexp2-expdef/sciexp2-expdef-2.0.1.tar.gz/sciexp2-expdef-2.0.1/sciexp2/version.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""SciExp² version information."""
+
+__author__ = "Lluís Vilanova"
+__copyright__ = "Copyright 2013-2019, Lluís Vilanova"
+__license__ = "GPL version 3 or later"
+
+__maintainer__ = "Lluís Vilanova"
+__email__ = "llvilanovag@gmail.com"
+
+
+__version_info__ = (1, 1, 14)
+__version__ = ".".join([str(i) for i in __version_info__])
