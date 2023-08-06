c5996d6659a30e1d193cafb6a8eecd8412b71ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:45 +0000

    Add package removio/1.0.1/removio-1.0.1.tar.gz

---
+++ packages/removio/removio-1.0.1.tar.gz/removio-1.0.1/README.md
@@ -0,0 +1,12 @@
+# removio
+
+#### Remove any app from your Android device without root access.
+
+
+
+![](https://imgur.com/5x8Q5LY.png)
+
+
+
+![](https://imgur.com/QQVb9V8.png)
+
---
+++ packages/removio/removio-1.0.1.tar.gz/removio-1.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name='removio',
+    version='1.0.1',
+    scripts=['removio'] ,
+    author="Vladimir Mikulic",
+    author_email="vladimir.mikulic2@gmail.com",
+    description="Remove any app from your Android device without root access.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/VladimirMikulic/removio",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "android",
+        "app",
+        "remove"
+    ]
+ )
