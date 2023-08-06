56adf5d4dbfd596df25c9533cc092c9bdfb8310a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:10 +0000

    Add package weatherapplicationarorarajat/0.0.2/weatherapplicationarorarajat-0.0.2.tar.gz

---
+++ packages/weatherapplicationarorarajat/weatherapplicationarorarajat-0.0.2.tar.gz/weatherapplicationarorarajat-0.0.2/configurations/config.py
@@ -0,0 +1,3 @@
+API_DAILY_BASE_ADDRESS = 'http://api.openweathermap.org/data/2.5/forecast'
+API_BASE_ADDRESS = 'http://api.openweathermap.org/data/2.5/weather'
+API_KEY = '9913e808cecc75f67a5358e6e16f3de8'
