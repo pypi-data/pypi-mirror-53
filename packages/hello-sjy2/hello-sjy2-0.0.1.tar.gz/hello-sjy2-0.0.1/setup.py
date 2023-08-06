82ed71777f4ab503dff7a3f21e0b8a2dca401161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:11 +0000

    Add package hello-sjy2/0.0.1/hello-sjy2-0.0.1.tar.gz

---
+++ packages/hello-sjy2/hello-sjy2-0.0.1.tar.gz/hello-sjy2-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='hello-sjy2',
+    version='0.0.1',
+    packages=find_packages(),
+    author='sjy',
+    author_email='coderinfo@163.com',
+    license='Apache',
+    url='http://pycn.me'
+)
