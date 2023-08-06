35ccf01417bcffdb6469cdfaf000fd8e383456f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:07 +0000

    Add package unleashed-py/0.0.8/unleashed-py-0.0.8.tar.gz

---
+++ packages/unleashed-py/unleashed-py-0.0.8.tar.gz/unleashed-py-0.0.8/unleashed-py/__init__.py
@@ -0,0 +1,8 @@
+"""unleashed-py - Python package to connect to the Unleashed Software inventory management API"""
+
+__version__ = '0.0.8'
+__author__ = 'Jonathan Mucha <jonmucha@gmail.com>'
+__all__ = []
+from .unleashed_py import UnleashedBase
+from .unleashed_py import Resource
+from .unleashed_py import EditableResource
