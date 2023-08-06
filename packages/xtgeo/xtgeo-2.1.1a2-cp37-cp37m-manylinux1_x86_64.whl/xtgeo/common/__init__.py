5460257dccd47323d7ea9d0946a7025af8d775e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:54 +0000

    Add package xtgeo/2.1.1a2/xtgeo-2.1.1a2-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/xtgeo/xtgeo-2.1.1a2-cp37-cp37m-manylinux1_x86_64.whl/xtgeo/common/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""The XTGeo common package"""
+from __future__ import division, absolute_import
+from __future__ import print_function
+
+# flake8: noqa
+from xtgeo.common.xtgeo_dialog import XTGeoDialog
+from xtgeo.common.xtgeo_dialog import XTGDescription
+from xtgeo.common.xtgeo_dialog import XTGShowProgress
+
+from xtgeo.common.exceptions import WellNotFoundError
