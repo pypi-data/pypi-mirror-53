ec2c1ce7be459fed50be8beb8a1cd6300fa9fc06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:33 +0000

    Add package func-adl-xAOD.backend/1.0.0a3/func_adl.xAOD.backend-1.0.0a3.tar.gz

---
+++ packages/func-adl-xAOD.backend/func_adl.xAOD.backend-1.0.0a3.tar.gz/func_adl.xAOD.backend-1.0.0a3/README.md
@@ -0,0 +1,7 @@
+# func_adl.xAOD.backend
+ Backend to run the xAOD transformer
+
+[![Travis Build Badge](https://travis-ci.org/iris-hep/func_adl.xAOD.backend.svg?branch=master)](https://travis-ci.org/iris-hep/func_adl.xAOD.backend)
+[![Code Coverage](https://codecov.io/gh/iris-hep/func_adl.xAOD.backend/graph/badge.svg)](https://codecov.io/gh/iris-hep/func_adl.xAOD.backend)
+
+This backend implementation of the `func_adl` language will run against ATLAS xAOD files, using C++ to run against.
---
+++ packages/func-adl-xAOD.backend/func_adl.xAOD.backend-1.0.0a3.tar.gz/func_adl.xAOD.backend-1.0.0a3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_namespace_packages
+from distutils.core import setup
+from os import listdir
+
+# Use the readme as the long description.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+xaod_template_files = listdir('func_adl/xAOD/backend/R21Code')
+setup(name="func_adl.xAOD.backend",
+      version='1.0.0-alpha.3',
+      packages=find_namespace_packages(exclude=['tests']),
+      scripts=[],
+      description="Functional Analysis Description Language for xAOD backends",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="G. Watts (IRIS-HEP/UW Seattle)",
+      author_email="gwatts@uw.edu",
+      maintainer="Gordon Watts (IRIS-HEP/UW Seattle)",
+      maintainer_email="gwatts@uw.edu",
+      url="https://github.com/iris-hep/func_adl.xAOD.backend",
+      license="TBD",
+      test_suite="tests",
+      install_requires=["requests>=2.0.0", "pandas>=0.24.0", "uproot>=3.7.0", "retry>=0.9.2", "Jinja2>=2.10", "func_adl.xAOD==1.0.0a4"],
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest>=3.9"],
+      classifiers=[
+                   # "Development Status :: 1 - Planning",
+                   "Development Status :: 2 - Pre-Alpha",
+                   # "Development Status :: 3 - Alpha",
+                   # "Development Status :: 4 - Beta",
+                   # "Development Status :: 5 - Production/Stable",
+                   # "Development Status :: 6 - Mature",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: Information Technology",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development",
+                   "Topic :: Utilities",
+      ],
+      data_files=[('func_adl/xAOD/backend/R21Code', [f'func_adl/xAOD/backend/R21Code/{f}' for f in xaod_template_files])],
+      platforms="Any",
+      )
