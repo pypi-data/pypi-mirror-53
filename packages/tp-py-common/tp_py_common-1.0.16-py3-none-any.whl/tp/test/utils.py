448397cf6b4c5b4fe20feea9aaaff2e527c7d74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:33 +0000

    Add package tp-py-common/1.0.16/tp_py_common-1.0.16-py3-none-any.whl

---
+++ packages/tp-py-common/tp_py_common-1.0.16-py3-none-any.whl/tp/test/utils.py
@@ -0,0 +1,11 @@
+import os
+
+def load_config():
+    config_path = None
+    try:
+        config_path = os.environ['MODELS_CONFIG']
+    except KeyError:
+        config_path = "config/database.json"
+
+    with open(config_path) as cred_file:
+        return json.loads(cred_file.read())
