7354973b0bc3ca2c94b0b1f1cea410595c339684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:43 +0000

    Add package LbDevTools/0.5.5/LbDevTools-0.5.5.tar.gz

---
+++ packages/LbDevTools/LbDevTools-0.5.5.tar.gz/LbDevTools-0.5.5/LbDevTools/data/cmake/GaudiDefaultToolchain.cmake
@@ -0,0 +1,19 @@
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_MODULE_PATH})
+
+include(GaudiToolchainMacros)
+
+init()
+find_projects(projects tools ${CMAKE_SOURCE_DIR}/CMakeLists.txt)
+
+if(heptools_version)
+  include(UseHEPTools)
+  use_heptools(${heptools_version})
+else()
+  include(InheritHEPTools)
+  inherit_heptools()
+endif()
+
+set_paths_from_projects(${tools} ${projects})
+
+# make sure we pick a compatible "GaudiProjectConfig.cmake"
+set(GaudiProject_DIR ${CMAKE_CURRENT_LIST_DIR})
