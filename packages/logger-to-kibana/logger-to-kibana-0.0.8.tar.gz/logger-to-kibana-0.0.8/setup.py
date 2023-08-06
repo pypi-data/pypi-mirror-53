42697337f57dc06e8cf3c95c5091f5b259e9deff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:07 +0000

    Add package logger-to-kibana/0.0.8/logger-to-kibana-0.0.8.tar.gz

---
+++ packages/logger-to-kibana/logger-to-kibana-0.0.8.tar.gz/logger-to-kibana-0.0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='logger-to-kibana',
+    version='0.0.8',
+    description='Import logger messages from a file and \
+                 generates a Kibana Visualisation',
+    author='Ismael Martinez Ramos',
+    author_email='ismaelmartinez@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': [
+            'logger-to-kibana = main:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+)
