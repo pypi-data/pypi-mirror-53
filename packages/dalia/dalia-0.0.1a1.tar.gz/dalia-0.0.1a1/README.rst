7ce8a46aad2711c127a06d8f837cd1cd19ba50a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:41 +0000

    Add package dalia/0.0.1a1/dalia-0.0.1a1.tar.gz

---
+++ packages/dalia/dalia-0.0.1a1.tar.gz/dalia-0.0.1a1/README.rst
@@ -0,0 +1,35 @@
+=====
+dalia
+=====
+
+
+.. image:: https://img.shields.io/pypi/v/dalia.svg
+        :target: https://pypi.python.org/pypi/dalia
+
+.. image:: https://img.shields.io/travis/brews/dalia.svg
+        :target: https://travis-ci.org/brews/dalia
+
+.. image:: https://readthedocs.org/projects/dalia/badge/?version=latest
+        :target: https://dalia.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/brews/dalia/shield.svg
+     :target: https://pyup.io/repos/github/brews/dalia/
+     :alt: Updates
+
+
+Mythic goddess of fate. Giver and taker of goods and property.
+
+
+* Free software: GNU General Public License v3
+* Documentation: https://dalia.readthedocs.io.
+
+
+
+Setting up this package
+-----------------------
+
+Features
+--------
+
+* TODO
---
+++ packages/dalia/dalia-0.0.1a1.tar.gz/dalia-0.0.1a1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(brews): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='dalia',
+    version='0.0.1a1',
+    description="Goddess of fate - giver and taker of goods",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+    author="Brewster Malevich",
+    author_email='bmalevich@rhg.com',
+    url='https://github.com/brews/dalia',
+    packages=find_packages(include=['dalia']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='dalia',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7'
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
