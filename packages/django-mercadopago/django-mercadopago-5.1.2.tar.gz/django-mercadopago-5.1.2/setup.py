4794d2ed1754d4571a359e5fb7c990546c1e7ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:34 +0000

    Add package django-mercadopago/5.1.2/django-mercadopago-5.1.2.tar.gz

---
+++ packages/django-mercadopago/django-mercadopago-5.1.2.tar.gz/django-mercadopago-5.1.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='django-mercadopago',
+    description='MercadoPago integration for django',
+    author='Hugo Osvaldo Barrera',
+    author_email='hbarrera@z47.io',
+    url='https://github.com/WhyNotHugo/django-mercadopago',
+    license='ISC',
+    packages=find_packages(),
+    long_description=open('README.rst').read(),
+    install_requires=open('requirements.txt').readlines(),
+    use_scm_version={'version_scheme': 'post-release'},
+    setup_requires=['setuptools_scm'],
+    extras_require={
+        'fixtures': ['factory-boy'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
