5eac1b02d5f0157a5418d190c4713829f4e41c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:58 +0000

    Add package gravify/0.3.0/gravify-0.3.0-py3-none-any.whl

---
+++ packages/gravify/gravify-0.3.0-py3-none-any.whl/gravify/__init__.py
@@ -0,0 +1,45 @@
+import hashlib
+import urllib.parse
+import urllib.request
+
+import sentry_sdk
+from validate_email import validate_email
+
+sentry_sdk.init("https://d1a3441649e64a96b52c441233c47f26@sentry.io/1763873")  # Automatic bug reporting
+
+
+class Gravatar():
+
+    def __init__(self, email, verify_email=True, default_image=None, size=None):
+        if verify_email:
+            if not validate_email(email):
+                raise Exception("Invalid email address")
+
+        self.email = email
+        self.default_image = default_image
+        self.size = size
+
+    @property
+    def url(self):
+        params = {
+            "s": self.size,
+            "d": self.default_image
+        }
+
+        url_params = urllib.parse.urlencode({k: v for k, v in params.items() if v is not None})
+
+        _url = f"https://www.gravatar.com/avatar/{self.hash}"
+
+        return f"{_url}?{url_params}" if url_params else _url
+
+    @property
+    def unsecure_url(self):
+        return self.url.replace("https://", "http://")
+
+    @property
+    def hash(self):
+        return hashlib.md5(self.email.encode("utf-8").lower()).hexdigest()
+
+    @property
+    def file(self):
+        return urllib.request.urlopen(self.url)
