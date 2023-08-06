7e4f0ef86b6d7b0732e4a24ff47268507150df88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:35 +0000

    Add package kinlin/0.0.1/kinlin-0.0.1-py3-none-any.whl

---
+++ packages/kinlin/kinlin-0.0.1-py3-none-any.whl/kinlin/__init__.py
@@ -0,0 +1,10 @@
+from .core import Project, Experiment, Model, Metric, RunningEpochMetric, TrainingEvents, SupervisedTraining, Callback, Dataset
+from .core import utils
+from . import metrics
+from . import callbacks
+from . import networks
+from . import datasets
+from . import losses
+
+
+__version__ = "0.0.1"
