b87f12a179aab2066002bf776d024a147c0acd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:45 +0000

    Add package auto-abi-checker/0.1.0/auto_abi_checker-0.1.0-py2-none-any.whl

---
+++ packages/auto-abi-checker/auto_abi_checker-0.1.0-py2-none-any.whl/auto_abi_checker/utils.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# Copyright 2018 Open Robotics
+# Licensed under the Apache License, Version 2.0
+
+from subprocess import check_call
+from sys import stderr
+
+
+def main_step_info(msg):
+    print("* %s" % msg)
+
+
+def info(msg):
+    print(" - %s" % msg)
+
+
+def subinfo(msg):
+    print("   - %s" % msg)
+
+
+def _check_call(cmd):
+    info("Run " + " ".join(cmd))
+
+    try:
+        check_call(cmd)
+    except Exception as e:
+        print(str(e))
+        return e
+
+    return 0
+
+
+def error(msg):
+    print("\n [err] " + msg + "\n", file=stderr)
+    exit(-1)
+
+
+def warn(msg):
+    print("\n [warn] " + msg + "\n", file=stderr)
+
+
+def comma_list_to_array(str_input):
+    if "," in str_input:
+        return str_input.split(",")
+
+    return [str_input]
