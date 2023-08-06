1047b85a19505a1ae3d573aab19d377f361c3d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:03 +0000

    Add package mothertongues/0.14.20191003/mothertongues-0.14.20191003.tar.gz

---
+++ packages/mothertongues/mothertongues-0.14.20191003.tar.gz/mothertongues-0.14.20191003/mtd/languages/__init__.py
@@ -0,0 +1,63 @@
+import json
+import os
+from mtd import languages as ldir
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+from urllib.parse import urlparse
+from typing import Union
+import requests
+from string import ascii_uppercase, ascii_lowercase
+
+ldir = os.path.dirname(ldir.__file__)
+
+with open(os.path.join(ldir, 'config_schema.json'), encoding='utf8') as f:
+    CONFIG_SCHEMA = json.load(f)
+
+with open(os.path.join(ldir, 'manifest_schema.json'), encoding='utf8') as f:
+    MANIFEST_SCHEMA = json.load(f)
+
+class LanguageConfig():
+    '''The configuration information necessary for making a Dictionary from a language.
+
+    Args:
+        :param (str or dict): Path to a valid language config file according to MTD config schema
+
+    '''
+    def __init__(self, config_object: Union[str, dict]):
+        self.config_schema = CONFIG_SCHEMA
+        self.language_default_dir = os.path.dirname(ldir)
+        if isinstance(config_object, dict):
+            self._config = config_object
+        elif 'http' in urlparse(config_object).scheme:
+            r = requests.get(config_object)
+            self._config = r.json()
+        else:
+            if not os.path.isabs(config_object):
+                config_object = os.path.join(self.language_default_dir, config_object)
+            with open(config_object, 'r', encoding='utf8') as f:
+                self._config = json.load(f)
+        self._config = self.validate_config_object(self._config)
+
+    def __getitem__(self, position):
+        return self._config[position] 
+
+    @property
+    def config(self):
+        '''Get the validated config object
+        '''
+        return self._config
+
+    @config.setter
+    def config(self, value):
+        self._config = self.validate_config_object(value)
+
+    def validate_config_object(self, co: Union[str, dict]) -> dict:
+        """Validate manifest json against manifest json schema
+        """
+        try:
+            validate(co, self.config_schema)
+            if "alphabet" not in co['config']:
+                co['config']['alphabet'] = list(ascii_uppercase) + list(ascii_lowercase)
+            return co
+        except ValidationError as e:
+            raise ValidationError(f"Attempted to validate the {self._config} configuration file, but got {e}. Please refer to the Mother Tongues data manifest schema.")
---
+++ packages/mothertongues/mothertongues-0.14.20191003.tar.gz/mothertongues-0.14.20191003/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import mtd
+import datetime as dt
+import os
+
+build_no = dt.datetime.today().strftime('%Y%m%d')
+version_path = os.path.join(os.path.dirname(mtd.__file__), 'version.py')
+VERSION = mtd.VERSION + "." + build_no
+
+with open(version_path, 'w') as f:
+    f.write(f'__version__ = "{VERSION}"')
+
+with open('requirements.txt') as f:
+    REQS = f.read().splitlines()
+
+setup(
+    name='mothertongues',
+    version=VERSION,
+    long_description='Mother Tongues',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQS,
+    python_requires='>=3.6.2',
+    entry_points='''
+        [console_scripts]
+        mtd=mtd.cli:cli
+        mothertongues=mtd.cli:cli
+    '''
+)
