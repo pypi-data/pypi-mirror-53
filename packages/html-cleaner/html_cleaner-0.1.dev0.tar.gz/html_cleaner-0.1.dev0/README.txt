176a57e6e48ce991d4a4dd4f1fc4f437f82ac2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:14 +0000

    Add package html-cleaner/0.1.dev0/html_cleaner-0.1.dev0.tar.gz

---
+++ packages/html-cleaner/html_cleaner-0.1.dev0.tar.gz/html_cleaner-0.1.dev0/README.txt
@@ -0,0 +1,16 @@
+usage: html_cleaner.py [-h] [--processor PROCESSOR] [--encoding ENCODING]
+                       [--no-script] [-r] [-p PATTERN]
+                       files [files ...]
+
+positional arguments:
+  files                 files
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --processor PROCESSOR
+                        number of process
+  --encoding ENCODING   fallback to this encoding
+  --no-script           remove js
+  -r, --recursive       recursive into directory
+  -p PATTERN, --pattern PATTERN
+                        glob pattern
---
+++ packages/html-cleaner/html_cleaner-0.1.dev0.tar.gz/html_cleaner-0.1.dev0/setup.py
@@ -0,0 +1,39 @@
+import sys
+from setuptools import setup
+
+if sys.version_info[:2] < (3, 5):
+    raise SystemExit('require Python3.5+')
+
+setup(
+    name='html_cleaner',
+    url='https://github.com/account-login/html_cleaner',
+    author='account-login',
+    author_email='',
+    version='0.1dev',
+    py_modules=['html_cleaner'],
+    install_requires=['beautifulsoup4', 'html5lib'],
+    entry_points='''
+        [console_scripts]
+        html_cleaner=html_cleaner:main
+    ''',
+    license='MIT',
+    description='clean up html files',
+    long_description=open('README.txt').read(),
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='html cleaner tidy',
+)
