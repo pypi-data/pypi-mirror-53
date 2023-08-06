8e3e0c7b26d68ff41567e64add9cc257e7add0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package SimilarityLab/0.0.5.1/SimilarityLab-0.0.5.1.tar.gz

---
+++ packages/SimilarityLab/SimilarityLab-0.0.5.1.tar.gz/SimilarityLab-0.0.5.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SimilarityLab", #Este es el nombre que figura en PyPI
+    version="0.0.5.1",
+    author="CID",
+    author_email="author@example.com",
+    description="NPL Package in development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='NLP corpus meaning',
+    url="https://github.com/CID-ITBA",
+    packages=["SimiLab"],#setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'numpy',
+          'scipy',
+      ],
+    python_requires='>=3.6',
+)
