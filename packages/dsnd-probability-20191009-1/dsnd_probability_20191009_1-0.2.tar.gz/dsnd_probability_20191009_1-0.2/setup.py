4336f76dd16c4ad374a95d30103df055385458c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:19 +0000

    Add package dsnd-probability-20191009-1/0.2/dsnd_probability_20191009_1-0.2.tar.gz

---
+++ packages/dsnd-probability-20191009-1/dsnd_probability_20191009_1-0.2.tar.gz/dsnd_probability_20191009_1-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_20191009_1',
+      version='0.2',
+      description='Gaussian and Binominal distributions',
+      packages=['dsnd_probability_20191009_1'],
+      author = 'Takeshi Ogawa',
+      zip_safe=False)
