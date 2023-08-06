9b4a1521d647f7499756f862d6f8d500e35b23ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:45 +0000

    Add package pyRadialPlot/0.2/pyRadialPlot-0.2.tar.gz

---
+++ packages/pyRadialPlot/pyRadialPlot-0.2.tar.gz/pyRadialPlot-0.2/pyRadialPlot/__init__.py
@@ -0,0 +1,2 @@
+from .radialplot import radialplot
+from .utilities import read_radialplotter_file
---
+++ packages/pyRadialPlot/pyRadialPlot-0.2.tar.gz/pyRadialPlot-0.2/pyRadialPlot/utilities.py
@@ -0,0 +1,34 @@
+
+import csv
+
+def read_radialplotter_file(filename):
+    """ Parser P. Vermeesh RadialPlotter csv file
+
+        returns: Ns, Ni, dpars as python lists"""
+
+    with open(filename, "r") as f:
+        file = csv.reader(f)
+        name = next(file)
+        line = next(file)
+        zeta, zeta_err = float(line[0]), float(line[1])
+        line = next(file)
+        rhod, rhod_err = float(line[0]), int(float(line[1]))
+        
+        Ns = []
+        Ni = []
+        dpars = []
+        
+        for line in file:
+            Ns.append(int(line[0]))
+            Ni.append(int(line[1]))
+            try:
+                dpars.append(float(line[2]))
+            except:
+                pass
+
+    return {"Ns": Ns,
+            "Ni": Ni,
+            "zeta": zeta,
+            "zeta_err": zeta_err,
+            "rhod": rhod,
+            "rhod_err": rhod_err}
