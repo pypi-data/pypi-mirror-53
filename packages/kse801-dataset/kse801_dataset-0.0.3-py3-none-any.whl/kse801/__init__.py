5d930e808ad62dd2b35f7f5d65b7d77739dbaa29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:25 +0000

    Add package kse801-dataset/0.0.3/kse801_dataset-0.0.3-py3-none-any.whl

---
+++ packages/kse801-dataset/kse801_dataset-0.0.3-py3-none-any.whl/kse801/__init__.py
@@ -0,0 +1,9 @@
+from ._load import load_crowdsignal_accel_phone, load_crowdsignal_accel_watch, load_crowdsignal_battery_phone, \
+    load_crowdsignal_battery_watch, load_crowdsignal_bluetooth_phone, load_crowdsignal_connectivity_phone, \
+    load_crowdsignal_gsm_phone, load_crowdsignal_gyro_phone, load_crowdsignal_gyro_watch, \
+    load_crowdsignal_heartrate_watch, load_crowdsignal_label, load_crowdsignal_light_phone, \
+    load_crowdsignal_location_phone, load_crowdsignal_magnet_phone, load_crowdsignal_magnet_watch, \
+    load_crowdsignal_pressure_phone, load_crowdsignal_proximity_phone, load_crowdsignal_screen_phone, \
+    load_crowdsignal_sms_phone, load_crowdsignal_wlan_phone, load_kaist_accel, load_kaist_activity, \
+    load_kaist_app_usage, load_kaist_data_traffic, load_kaist_gsr, load_kaist_heartrate, load_kaist_location, \
+    load_kaist_rr_interval, load_kaist_skin_temperature
