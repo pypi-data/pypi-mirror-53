6c25a405b18ab3a4a1887102e3128badb0603a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:46 +0000

    Add package retaggr/2.3.0/retaggr-2.3.0.tar.gz

---
+++ packages/retaggr/retaggr-2.3.0.tar.gz/retaggr-2.3.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import subprocess
+
+setup(
+    name="retaggr",
+    version="2.3.0",
+    url="https://github.com/booru-utils/reverse-search",
+    license="LGPLv3",
+    description="Reverse image searching utility for images.",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    author="Valentijn 'noirscape' V.",
+    author_email="neko@catgirlsin.space",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="reverse image search booru",
+    project_urls={
+        "Source": "https://github.com/booru-utils/retaggr",
+        "Tracker": "https://github.com/booru-utils/retaggr/issues",
+        "Documentation": "https://retaggr.rtfd.org"
+    },
+    packages=find_packages('src'),
+    package_dir={'':'src',},
+    install_requires=[
+        "lxml",
+        "fake-useragent",
+        "requests",
+    ]
+)
