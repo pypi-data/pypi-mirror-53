422afea063376a3edbe121afd7ccec9b5fabc4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:48 +0000

    Add package docker-image-py/0.1.10/docker-image-py-0.1.10.tar.gz

---
+++ packages/docker-image-py/docker-image-py-0.1.10.tar.gz/docker-image-py-0.1.10/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+install_requires = [
+    'regex>=2019.4.14',
+]
+
+setup(
+    name="docker-image-py",
+    version='0.1.10',
+    description="Parse docker image as distribution does.",
+    url='https://github.com/realityone/docker-image-py',
+    packages=['docker_image'],
+    install_requires=install_requires,
+    zip_safe=False,
+)
