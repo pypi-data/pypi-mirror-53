3e5fcd96cb2b7a32a5b6e562b33138ccd8d19773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:56 +0000

    Add package eccw-gui/1.1.0.post13/eccw_gui-1.1.0.post13.tar.gz

---
+++ packages/eccw-gui/eccw_gui-1.1.0.post13.tar.gz/eccw_gui-1.1.0.post13/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+from setuptools import setup, find_packages
+import sys
+from distutils.command.install_data import install_data
+import subprocess
+import os
+
+def get_data_files():
+    """Return data_files in a platform dependent manner"""
+    if sys.platform.startswith("linux"):
+        data_files = [
+            ("share/applications", ["scripts/eccw.desktop"]),
+            ("share/pixmaps", ["eccw_gui/images/icon_eccw.svg"]),
+            ("share/metainfo", ["scripts/eccw.appdata.xml"]),
+        ]
+    else:
+        data_files = []
+    return data_files
+
+# Make Linux detect ECCW desktop file
+class MyInstallData(install_data):
+    def run(self):
+        install_data.run(self)
+        if sys.platform.startswith("linux"):
+            try:
+                subprocess.call(["update-desktop-database"])
+            except:
+                print("ERROR: unable to update desktop database", file=sys.stderr)
+
+CMDCLASS = {"install_data": MyInstallData}
+
+SCRIPTS = ["eccw", "eccw_win_post_install.py"]
+#if sys.platform.startswith('linux'):
+#    SCRIPTS.append("eccw")
+
+setup(
+    entry_points={"console_scripts": ["eccw=eccw_gui.main:launch"]},
+    data_files=get_data_files(),
+    #scripts=[os.path.join("scripts", "eccw")],
+    scripts=[os.path.join('scripts', fname) for fname in SCRIPTS],
+    cmdclass=CMDCLASS,
+)
