7904320b74ba75921a4a051f86e3794987118256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:24 +0000

    Add package tensorflow-rocm/2.0.0/tensorflow_rocm-2.0.0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.0.0-cp35-cp35m-manylinux1_x86_64.whl/tensorflow_core/include/external/local_config_rocm/rocm/rocm/include/rocblas-version.h
@@ -0,0 +1,18 @@
+/* ************************************************************************
+ * Copyright 2016-2019 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+/* the configured version and settings
+ */
+#ifndef ROCBLAS_VERSION_H_
+#define ROCBLAS_VERSION_H_
+
+// clang-format off
+#define ROCBLAS_VERSION_MAJOR       2
+#define ROCBLAS_VERSION_MINOR       6
+#define ROCBLAS_VERSION_PATCH       3
+#define ROCBLAS_VERSION_TWEAK       1503-rocm-rel-2.9-6-39b5e1e
+#define ROCBLAS_VERSION_COMMIT_ID   
+// clang-format on
+
+#endif
