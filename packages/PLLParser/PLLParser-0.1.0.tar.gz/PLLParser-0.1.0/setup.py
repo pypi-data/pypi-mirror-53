6351ee1e7534eb355cff067f9fa373d90d3d18d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:59 +0000

    Add package PLLParser/0.1.0/PLLParser-0.1.0.tar.gz

---
+++ packages/PLLParser/PLLParser-0.1.0.tar.gz/PLLParser-0.1.0/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+import sys, pathlib
+from setuptools import setup, find_packages
+
+# --- Get the text of README.md
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+	name = "PLLParser",
+	version = "0.1.0",
+	author = "John Deighan",
+	author_email = "john.deighan@gmail.com",
+	description = "Parse a Python-like language",
+	long_description = README,
+	long_description_content_type = "text/markdown",
+	license="MIT",
+
+	url = "https://github.com/johndeighan/PLLParser",
+	packages = find_packages(),
+	py_modules = ['TreeNode','RETokenizer','PLLParser'],
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		],
+	python_requires = '>=3.6',
+	)
