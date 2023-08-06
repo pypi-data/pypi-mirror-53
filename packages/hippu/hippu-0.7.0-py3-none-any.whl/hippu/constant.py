86ce26a2d14951c9c0c36ff350adb10521ccf7c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package hippu/0.7.0/hippu-0.7.0-py3-none-any.whl

---
+++ packages/hippu/hippu-0.7.0-py3-none-any.whl/hippu/constant.py
@@ -0,0 +1,35 @@
+""" Constants. """
+
+class HTTP:
+    """ HTTP constants. """
+
+    #
+    # HTTP headers
+    #
+    AGE = 'Age'
+    CACHE_CONTROL = 'Cache-Control'
+    CONNECTION = 'connection'
+    CONTENT_ENCODING = 'Content-Encoding'
+    CONTENT_TYPE = 'Content-Type'
+    CONTENT_LENGTH = 'Content-Length'
+    KEEP_ALIVE = 'Keep-Alive'
+    LOCATION = 'Location'
+    PRAGMA = 'Pragma'
+    SET_COOKIE = 'Set-Cookie'
+    COOKIE = 'Cookie'
+    ACCESS_CONTROL_ALLOW_ORIGIN = 'Access-Control-Allow-Origin'
+    #
+    # HTTP header values
+    #
+    APPLICATION_JSON = 'application/json'
+    BOUNDARY = 'boundary'
+    GZIP = 'gzip'
+    IMAGE_GIF = 'image/gif'
+    IMAGE_PNG = 'image/png'
+    IMAGE_JPEG = 'image/jpeg'
+    IMAGE_BMP = 'image/bmp'
+    IMAGE_ICO = 'image/ico'
+    MULTIPART_X_MIXED_REPLACE = 'multipart/x-mixed-replace'
+    NO_CACHE = 'no-cache'
+    PRIVATE = 'private'
+    TEXT_HTML = 'text/html'
