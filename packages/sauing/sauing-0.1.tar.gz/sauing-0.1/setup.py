cba635c4670377975657acc5e13edcae5ebf21e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:02 +0000

    Add package sauing/0.1/sauing-0.1.tar.gz

---
+++ packages/sauing/sauing-0.1.tar.gz/sauing-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sauing',
+      version='0.1',
+      description='Test file to create a package',
+      url='https://github.com/sauing/IIN_Trollers',
+      author='Saurabh Singh',
+      author_email='ssaurabhsingh009@gmail.com',
+      package=['sauing'],
+      zip_safe=False)
