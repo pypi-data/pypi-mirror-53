666ec36462a02058b349527254a542b43cbfb361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:40 +0000

    Add package mathMB/1.0.9/mathMB-1.0.9.tar.gz

---
+++ packages/mathMB/mathMB-1.0.9.tar.gz/mathMB-1.0.9/mathMB/__init__.py
@@ -0,0 +1,10 @@
+from .interpu import interpu
+from .minmaxmean import minmaxmean
+from .randomdeviates import random_deviates_1d, random_deviates_2d
+from .rotation_matrix import rotation_matrix
+
+
+name = 'mathMB'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '1.0.9'
