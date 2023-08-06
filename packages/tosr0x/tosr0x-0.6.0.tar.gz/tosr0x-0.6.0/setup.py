fc9123751ea73e31b2786de4f78dc8525c42ec05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:27 +0000

    Add package tosr0x/0.6.0/tosr0x-0.6.0.tar.gz

---
+++ packages/tosr0x/tosr0x-0.6.0.tar.gz/tosr0x-0.6.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='tosr0x',
+    version='0.6.0',
+    author='James Stewart',
+    author_email='jstewart101@gmail.com',
+    url='https://github.com/amorphic/tosr0x',
+    description='An interface to tinyos.com TOSR0x relay modules.',
+    long_description=open('README.md').read(),
+    license='LICENSE.txt',
+    py_modules=['tosr0x'],
+    install_requires=[
+        'pyserial>=2.0,<3.0',
+    ],
+)
