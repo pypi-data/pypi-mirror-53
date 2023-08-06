6275c8361850041badbce5599005c36775b5e696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:13 +0000

    Add package commandict/0.1.0/commandict-0.1.0-py3.7.egg

---
+++ packages/commandict/commandict-0.1.0-py3.7.egg/commandict/get_result.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+""" get_result.py """
+
+import click
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+
+DAUM_DICT_HOST = "https://alldic.daum.net/"
+
+# TODO: 키워드 입력받기
+# TODO: 설정에서 언어 입력받고 설정 저장할 수 있게
+LANG = 'eng'
+
+
+def parse(html: str):
+    bs = BeautifulSoup(html, 'html.parser')
+    return bs.findAll('meta',
+                      attrs={'property': 'og:description'})[0].get('content')
+
+
+@click.command()
+def main():
+    KEYWORD = sys.argv[1]
+    click.echo('Searching...')
+    url = f'{DAUM_DICT_HOST}search.do?q={KEYWORD}&dic={LANG}'
+    response = requests.get(url)
+    meanings = parse(response.text)
+    print(meanings)
+
+
+if __name__ == "__main__":
+    main()
