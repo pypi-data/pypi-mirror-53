065b65e120bd70b735d9bf7b0df9b99c5ec6e194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:42 +0000

    Add package asyncclick/7.0.9/asyncclick-7.0.9.tar.gz

---
+++ packages/asyncclick/asyncclick-7.0.9.tar.gz/asyncclick-7.0.9/tests/conftest.py
@@ -0,0 +1,18 @@
+from asyncclick.testing import CliRunner
+
+import pytest
+import anyio
+from functools import partial
+
+class SyncCliRunner(CliRunner):
+    def invoke(self,*a,_sync=False,**k):
+        fn = super().invoke
+        if _sync:
+            return fn(*a,**k)
+        if k:
+            fn = partial(fn, **k)
+        return anyio.run(fn, *a)
+
+@pytest.fixture(scope='function')
+def runner(request):
+    return SyncCliRunner()
