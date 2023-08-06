d123e178e849201d273e42cd81ce0807761003a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:21 +0000

    Add package beatmachine/2.1.1/beatmachine-2.1.1-py2.py3-none-any.whl

---
+++ packages/beatmachine/beatmachine-2.1.1-py2.py3-none-any.whl/beatmachine/__main__.py
@@ -0,0 +1,20 @@
+import json
+
+from click import command, option
+
+import beatmachine as bm
+
+
+@command()
+@option("--input", help="File to process.", required=True)
+@option("--effects", help="JSON representation of effects to apply.", required=True)
+@option("--output", help="Output mp3 file path.", required=True)
+def main(input, effects, output):
+    beats = bm.loader.load_beats_by_signal(input)
+    effects = [bm.effects.load_from_dict(e) for e in json.loads(effects)]
+    result = bm.editor.apply_effects(beats, effects)
+    return sum(result).export(output)
+
+
+if __name__ == "__main__":
+    main()
