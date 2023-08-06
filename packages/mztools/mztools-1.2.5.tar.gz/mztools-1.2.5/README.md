ea0dcf8b24ed1373b1589246b13d387992b2cac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:44 +0000

    Add package mztools/1.2.5/mztools-1.2.5.tar.gz

---
+++ packages/mztools/mztools-1.2.5.tar.gz/mztools-1.2.5/README.md
@@ -0,0 +1,27 @@
+# mztools
+[![Build Status](https://travis-ci.org/digitalroute/mztools.svg?branch=master)](https://travis-ci.org/digitalroute/mztools)
+
+## Introduction
+
+mztools is used to control your PaaS environment for Digital Routes MediationZone.
+
+## Prerequisites
+
+Make sure you have Python3.
+
+Configure AWS CLI with the credentials passed to you for your environment:
+`aws configure`
+
+## Installation
+
+Run command:  
+
+    $ pip install mztools
+
+## Unittesting
+
+Install tox:
+    $ pip install tox
+
+Run tox:
+    $ tox
---
+++ packages/mztools/mztools-1.2.5.tar.gz/mztools-1.2.5/mztools/info.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from termcolor import colored
+from .common import get_parameter
+
+
+def run_info(args):
+    print(colored('Company: ',
+          'white', attrs=['bold']) + get_parameter('/customer/name'))
+    print('')
+
+    print(colored('Platform versions: ',
+          'white', attrs=['bold']))
+
+    print('')
+
+    print('Dev:  ' + get_parameter('/dev/platform/version'))
+    print('Test: ' + get_parameter('/test/platform/version'))
+    print('Prod: ' + get_parameter('/prod/platform/version'))
+
+    print('')
+
+    print(colored('EC versions: ',
+          'white', attrs=['bold']))
+
+    print('')
+
+    print('Dev:  ' + get_parameter('/dev/ec/version'))
+    print('Test: ' + get_parameter('/test/ec/version'))
+    print('Prod: ' + get_parameter('/prod/ec/version'))
+
+    return True
