aba98fe565007dc48d7fffdcc75a15b31cda15cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:21 +0000

    Add package pySerasa/0.1/pySerasa-0.1.tar.gz

---
+++ packages/pySerasa/pySerasa-0.1.tar.gz/pySerasa-0.1/pyserasa/crednet.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+from erros import BlocoInexistenteError
+from blocosDados import pendenciasInternas
+from blocosDados import pendenciasFinanceiras
+from blocosDados import protestosEstados
+from blocosDados import chequesSemFundos
+
+
+class Crednet(object):
+    def __init__(self):
+        self.blocos = []
+        self.blocos.append(pendenciasInternas())
+        self.blocos.append(pendenciasFinanceiras())
+        self.blocos.append(protestosEstados())
+        self.blocos.append(chequesSemFundos())
+
+    def __getattr__(self, name):
+        bloco = ([c for c in self.blocos if c.nome == name] or [None])[0]
+        if not bloco:
+            print BlocoInexistenteError().exibirErro(name)
+        else:
+            if bloco.nome == 'pendenciasInternas':
+                print bloco.nome_bloco + "\n"
+                for registro in bloco.blocos:
+                    for campos in registro.campos.campos:
+                        print campos._nome,
+                        print ": ",
+                        print campos._valor
+
+                    print " "
+            if bloco.nome == 'pendenciasFinanceiras':
+                print bloco.nome_bloco + "\n"
+                for registro in bloco.blocos:
+                    for campos in registro.campos.campos:
+                        print campos._nome,
+                        print ": ",
+                        print campos._valor
+
+                    print " "
+            if bloco.nome == 'protestosEstados':
+                print bloco.nome_bloco + "\n"
+                for registro in bloco.blocos:
+                    for campos in registro.campos.campos:
+                        print campos._nome,
+                        print ": ",
+                        print campos._valor
+
+                    print " "
+            if bloco.nome == 'chequesSemFundos':
+                print bloco.nome_bloco + "\n"
+                for registro in bloco.blocos:
+                    for campos in registro.campos.campos:
+                        print campos._nome,
+                        print ": ",
+                        print campos._valor
+
+                    print " "
+            else:
+                return bloco
+
+    def get_string(self, bloco=None):
+        string_retorno = ""
+        if bloco is None:
+            for bloco in self.blocos:
+                if bloco.nome == 'pendenciasInternas':
+                    string_retorno += "Pendencias Internas\n" + "------------" \
+                             "-----------------------------------------------\n"
+                    for registro in bloco.blocos:
+                        for campos in registro.campos.campos:
+                            string_retorno += campos._nome + ": " + str(
+                                campos._valor) + "\n"
+                elif bloco.nome == 'pendenciasFinanceiras':
+                    string_retorno += "Pendencias Financeiras\n" + "---------" \
+                          "--------------------------------------------------\n"
+                    for registro in bloco.blocos:
+                        for campos in registro.campos.campos:
+                            string_retorno += campos._nome + ": " + str(
+                                campos._valor) + "\n"
+                elif bloco.nome == 'protestosEstados':
+                    string_retorno += "Protestos dos Estados\n" + "---------" \
+                          "--------------------------------------------------\n"
+                    for registro in bloco.blocos:
+                        for campos in registro.campos.campos:
+                            string_retorno += campos._nome + ": " + str(
+                                campos._valor) + "\n"
+                elif bloco.nome == 'chequesSemFundos':
+                    string_retorno += "Cheques sem Fundos\n" + "-------------" \
+                              "----------------------------------------------\n"
+                    for registro in bloco.blocos:
+                        for campos in registro.campos.campos:
+                            string_retorno += campos._nome + ": " + str(
+                                campos._valor) + "\n"
+                else:
+                    string_retorno += bloco.nome_bloco + "-------------------" \
+                                    "----------------------------------------\n"
+                    for campo in bloco.campos.campos:
+                        string_retorno += campo._nome + ": " + str(
+                            campo._valor) + "\n"
+            else:
+                if bloco.nome == 'pendenciasInternas':
+                    string_retorno += "Pendencias Internas\n" + "------------" \
+                             "-----------------------------------------------\n"
+                    for registro in bloco.blocos:
+                        for campos in registro.campos.campos:
+                            string_retorno += campos._nome + ": " + str(
+                                campos._valor) + "\n"
+                        string_retorno += "\n"
+                elif bloco.nome == '\npendenciasFinanceiras':
+                    string_retorno += "Pendencias Financeiras\n" + "---------" \
+                          "--------------------------------------------------\n"
+                    for registro in bloco.blocos:
+                        for campos in registro.campos.campos:
+                            string_retorno += campos._nome + ": " + str(
+                                campos._valor) + "\n"
+                        string_retorno += "\n"
+                elif bloco.nome == 'protestosEstados':
+                    string_retorno += "\nProtestos dos Estados\n" + "--------" \
+                         "---------------------------------------------------\n"
+                    for registro in bloco.blocos:
+                        for campos in registro.campos.campos:
+                            string_retorno += campos._nome + ": " + str(
+                                campos._valor) + "\n"
+                    string_retorno += "\n"
+                elif bloco.nome == 'chequesSemFundos':
+                    string_retorno += "\nCheques sem Fundos\n" + "-----------" \
+                            "------------------------------------------------\n"
+                    for registro in bloco.blocos:
+                        for campos in registro.campos.campos:
+                            string_retorno += campos._nome + ": " + str(
+                                campos._valor) + "\n"
+                    string_retorno += "\n"
+                elif bloco is not None:
+                    string_retorno += "\n" + bloco.nome_bloco + "\n----------" \
+                           "-------------------------------------------------\n"
+                    for campo in bloco.campos.campos:
+                        string_retorno += campo._nome + ": " + str(
+                            campo._valor) + "\n"
+                    string_retorno += "\n"
+        return string_retorno
+
+    def get_bloco_de_registros(self, nome):
+
+        for bloco in self.blocos:
+            if bloco.nome == nome:
+                return bloco
+
+        return None
---
+++ packages/pySerasa/pySerasa-0.1.tar.gz/pySerasa-0.1/pyserasa/parserStringDados.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import re
+import requests
+
+
+class ParserStringDados(object):
+
+    def gerar_string_envio(self, documento_consultado,
+                           tipo_pessoa_busca, documento_consultor, uf_cliente,
+                           login, senha):
+        
+        dados = 'https://sitenet43.serasa.com.br/Prod/consultahttps?p=' \
+                + login + senha + '        B49C      ' + documento_consultado +\
+                tipo_pessoa_busca + 'C     FI0001000000000000000N99SINIAN    ' \
+                                    '                          D             ' \
+                                    'N                                       ' \
+                                    '     ' \
+                + documento_consultor + '                                    ' \
+                                        '                                    ' \
+                                        '                                    ' \
+                                        '                                    ' \
+                                        '                                    ' \
+                                        '                                    ' \
+                                        '                   P002RE02         ' \
+                                        '                                    ' \
+                                        '                                    ' \
+                                        '                          N00100PPX2' \
+                                        '1P 0                                ' \
+                                        '                                    ' \
+                                        '                                 ' \
+                                        'N00300                     ' \
+                + uf_cliente + '                                             ' \
+                               '                                         T999'
+
+        return dados
+
+    def realizar_busca_serasa(self, dados):
+        request = requests.get(dados, verify=False)
+
+        return request.text
+
+    def montar_bloco(self, bloco, arquivo):
+        nome = bloco[0:4]
+        nome_classe = "bloco"+nome
+        if bloco[0:4] != u'B49C' and bloco[0:4] != u'T999' \
+                and bloco[0:4] != u'P002' and bloco[0:4] != u'N001' \
+                and bloco[0:4] != u'N003':
+            nome_classe = nome_classe + "_subtipo" + bloco[4:6]
+        mod_serializer = __import__('blocosDados', globals(), locals())
+        func = getattr(mod_serializer, nome_classe)
+        bloco_montado = func(nome, bloco)
+
+        if nome == 'N230':
+            arquivo.blocos[0].blocos.append(bloco_montado)
+        elif nome == 'N240':
+            arquivo.blocos[1].blocos.append(bloco_montado)
+        elif nome == 'N250':
+            arquivo.blocos[2].blocos.append(bloco_montado)
+        elif nome == 'N270':
+            arquivo.blocos[3].blocos.append(bloco_montado)
+        else:
+            arquivo.blocos.append(bloco_montado)
+
+        return arquivo
+
+    def parser_string_dados_retorno(self, string_dados_retorno, arquivo):
+        string_dados_retorno = string_dados_retorno[
+                               0:len(string_dados_retorno)-2] + 'T999'
+        vetor_string_dados = re.findall(
+            "([B,P,N]\d{2}.*?)(?=[B,P,N][1-9][0-9][0-9]|T999)", string_dados_retorno)
+
+        arquivo_crednet = self.montar_objeto_crednet(
+            vetor_string_dados, arquivo)
+
+        return arquivo_crednet
+
+    def montar_objeto_crednet(self, vetor_string_dados, arquivo):
+
+        for bloco in vetor_string_dados:
+            arquivo = self.montar_bloco(bloco, arquivo)
+
+        return arquivo
