99f813abd61f44081548e56ea4ba10baea4d373a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:03 +0000

    Add package marian/0.0.16/marian-0.0.16.tar.gz

---
+++ packages/marian/marian-0.0.16.tar.gz/marian-0.0.16/marian/version.py
@@ -0,0 +1,3 @@
+"""!!!AUTOMATICALLY GENERATED!!! DO NOT EDIT.
+see marian/release.py."""
+__version__ = "0.0.16"
---
+++ packages/marian/marian-0.0.16.tar.gz/marian-0.0.16/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from marian.version import __version__
+
+with open('README.rst', 'r') as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name='marian',
+    version=__version__,
+    author='Tom Spalding',
+    author_email='tom@blackforestbotanics.com',
+    description='a personal Robinhood API server built on Fast Arrow.',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/nebulousdog/marian',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        'Framework :: Flask',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
