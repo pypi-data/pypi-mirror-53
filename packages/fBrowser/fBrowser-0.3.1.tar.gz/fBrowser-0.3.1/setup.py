c936a25f942ab9f23d67e0a1f62d4d52a8b27607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:25 +0000

    Add package fBrowser/0.3.1/fBrowser-0.3.1.tar.gz

---
+++ packages/fBrowser/fBrowser-0.3.1.tar.gz/fBrowser-0.3.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    longDescription = f.read()
+
+setuptools.setup(
+    name="fBrowser",
+    version="0.3.1",
+    author="Abe",
+    author_email="",
+    description="Python3 library to make selenium webscraping easier",
+    long_description=longDescription,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FastestMolasses/fBrowser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
