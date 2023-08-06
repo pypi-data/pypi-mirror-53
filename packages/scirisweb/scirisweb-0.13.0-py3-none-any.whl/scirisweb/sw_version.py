b5d78067e29407c0254c366818a9711b5110086c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:53 +0000

    Add package scirisweb/0.13.0/scirisweb-0.13.0-py3-none-any.whl

---
+++ packages/scirisweb/scirisweb-0.13.0-py3-none-any.whl/scirisweb/sw_version.py
@@ -0,0 +1,5 @@
+__all__ = ['__version__', '__versiondate__', '__license__']
+
+__version__      = '0.13.0'
+__versiondate__  = '2019-09-30'
+__license__      = 'ScirisWeb %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
