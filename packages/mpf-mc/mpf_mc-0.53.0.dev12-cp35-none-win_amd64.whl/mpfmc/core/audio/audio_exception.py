9a8e07ce59e0bd74cddef5e9e9abde5fdca46618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:09 +0000

    Add package mpf-mc/0.53.0.dev12/mpf_mc-0.53.0.dev12-cp35-none-win_amd64.whl

---
+++ packages/mpf-mc/mpf_mc-0.53.0.dev12-cp35-none-win_amd64.whl/mpfmc/core/audio/audio_exception.py
@@ -0,0 +1,9 @@
+"""General audio exception class for exceptions thrown in the audio module."""
+
+
+# ---------------------------------------------------------------------------
+#    AudioException class
+# ---------------------------------------------------------------------------
+class AudioException(Exception):
+    """Exception returned by the audio module"""
+    pass
