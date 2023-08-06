6314d8e60cc9f34ac5fe35f9b55753cb9ee6683c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:21 +0000

    Add package larousse-api-sunbro/0.0.5/larousse-api-sunbro-0.0.5.tar.gz

---
+++ packages/larousse-api-sunbro/larousse-api-sunbro-0.0.5.tar.gz/larousse-api-sunbro-0.0.5/README.md
@@ -0,0 +1,24 @@
+## Larousse API
+
+### Installation
+* `pip install larousse-api-sunbro`
+
+### Usage
+```python3
+from larousse_api import larousse
+
+# Print the array containing all defintions of "Fromage"
+print(larousse.get_definitions("Fromage"))
+```
+
+### Why ?
+Because the Larousse website doesn't supply an api to look up definitions :(
+
+### How ?
+* python3
+* requests
+* re
+* BeautifulSoup
+
+### Possible improvements
+* Suggest words if the word is misspelled
---
+++ packages/larousse-api-sunbro/larousse-api-sunbro-0.0.5.tar.gz/larousse-api-sunbro-0.0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="larousse-api-sunbro",
+    version="0.0.5",
+    author="Quentin Barbarat",
+    author_email="q.barbarat@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/quentin-dev/larousse_api",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests>=2.22.0",
+        "bs4>=0.0.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
