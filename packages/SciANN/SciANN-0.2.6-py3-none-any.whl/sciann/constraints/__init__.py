6f0c862632dd3b889d4a17bef2b890b77286ee59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:23 +0000

    Add package SciANN/0.2.6/SciANN-0.2.6-py3-none-any.whl

---
+++ packages/SciANN/SciANN-0.2.6-py3-none-any.whl/sciann/constraints/__init__.py
@@ -0,0 +1,20 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+from . import constraint
+from . import data
+from . import dirichlet
+from . import neumann
+from . import pde
+from . import tie
+
+from .constraint import Constraint
+from .data import Data
+from .tie import Tie
+from .dirichlet import Dirichlet
+from .neumann import Neumann
+from .pde import PDE
+from .minmax import MinMax
