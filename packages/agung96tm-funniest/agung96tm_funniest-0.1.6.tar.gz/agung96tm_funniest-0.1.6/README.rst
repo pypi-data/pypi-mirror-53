bf8e1179e7c233270f4dbeb5f49330d3f75958ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:05 +0000

    Add package agung96tm-funniest/0.1.6/agung96tm_funniest-0.1.6.tar.gz

---
+++ packages/agung96tm-funniest/agung96tm_funniest-0.1.6.tar.gz/agung96tm_funniest-0.1.6/README.rst
@@ -0,0 +1,7 @@
+Funniest
+--------
+
+To use (with caution), simply do::
+
+    >>> import agung96tm_funniest
+    >>> print agung96tm_funniest.joke()
---
+++ packages/agung96tm-funniest/agung96tm_funniest-0.1.6.tar.gz/agung96tm_funniest-0.1.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='agung96tm_funniest',
+      version='0.1.6',
+      description='The agung96tm_funniest jokes',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          # 'Licence :: OSI Approved :: MIT Licence',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke comedy flying circus',
+      url='http://localhost:8000',
+      author='king',
+      author_email='king@example.com',
+      license='MIT',
+      packages=['agung96tm_funniest'],
+      install_requires=[
+          'markdown',
+      ],
+      test_suite='nose.collector',
+      tests_requires=['nose'],
+      scripts=[
+          'bin/funniest-joke',
+      ],
+      zip_safe=False)
