55683e85eb21a1c4393d02c67b14c5670bf3448c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:31 +0000

    Add package GenTree/0.0.1/GenTree-0.0.1.tar.gz

---
+++ packages/GenTree/GenTree-0.0.1.tar.gz/GenTree-0.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Basic setup.py
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="GenTree",
+    version="0.0.1",
+    description="GenTree",
+    long_description="GenTree",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    author="Peter Badida",
+    author_email="keyweeusr@gmail.com",
+    url="https://github.com/KeyWeeUsr/GenTree",
+    download_url="https://github.com/KeyWeeUsr/GenTree/tarball/0.0.1",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython"
+    ]
+)
