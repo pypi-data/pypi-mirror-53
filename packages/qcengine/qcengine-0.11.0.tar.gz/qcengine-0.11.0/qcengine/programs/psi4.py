9866b3251e4e6dd46b575a9eb18c35194ed93f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:47 +0000

    Add package qcengine/0.11.0/qcengine-0.11.0.tar.gz

---
+++ packages/qcengine/qcengine-0.11.0.tar.gz/qcengine-0.11.0/qcengine/programs/psi4.py
@@ -0,0 +1,144 @@
+"""
+Calls the Psi4 executable.
+"""
+import json
+import os
+from typing import Dict
+
+from qcelemental.models import Result
+from qcelemental.util import parse_version, safe_version, which
+
+from ..exceptions import InputError, RandomError, ResourceError, UnknownError
+from ..util import execute, popen, temporary_directory
+from .model import ProgramHarness
+
+
+class Psi4Harness(ProgramHarness):
+
+    _defaults = {
+        "name": "Psi4",
+        "scratch": True,
+        "thread_safe": False,
+        "thread_parallel": True,
+        "node_parallel": False,
+        "managed_memory": True,
+    }
+    version_cache: Dict[str, str] = {}
+
+    class Config(ProgramHarness.Config):
+        pass
+
+    @staticmethod
+    def found(raise_error: bool = False) -> bool:
+        return which('psi4',
+                     return_bool=True,
+                     raise_error=raise_error,
+                     raise_msg='Please install via `conda install psi4 -c psi4`.')
+
+    def get_version(self) -> str:
+        self.found(raise_error=True)
+
+        which_prog = which('psi4')
+        if which_prog not in self.version_cache:
+            with popen([which_prog, '--version']) as exc:
+                exc["proc"].wait(timeout=15)
+            self.version_cache[which_prog] = safe_version(exc["stdout"])
+
+        candidate_version = self.version_cache[which_prog]
+
+        if "undef" in candidate_version:
+            raise TypeError(
+                "Using custom build without tags. Please pull git tags with `git pull origin master --tags`.")
+
+        return candidate_version
+
+    def compute(self, input_model: 'ResultInput', config: 'JobConfig') -> 'Result':
+        """
+        Runs Psi4 in API mode
+        """
+        self.found(raise_error=True)
+
+        if parse_version(self.get_version()) < parse_version("1.2"):
+            raise ResourceError("Psi4 version '{}' not understood.".format(self.get_version()))
+
+        # Setup the job
+        input_data = input_model.dict(encoding="json")
+        input_data["nthreads"] = config.ncores
+        input_data["memory"] = int(config.memory * 1024 * 1024 * 1024 * 0.95)  # Memory in bytes
+        input_data["success"] = False
+        input_data["return_output"] = True
+
+        if input_data["schema_name"] == "qcschema_input":
+            input_data["schema_name"] = "qc_schema_input"
+
+        # Location resolution order config.scratch_dir, $PSI_SCRATCH, /tmp
+        parent = config.scratch_directory
+        if parent is None:
+            parent = os.environ.get("PSI_SCRATCH", None)
+
+        with temporary_directory(parent=parent, suffix="_psi_scratch") as tmpdir:
+
+            caseless_keywords = {k.lower(): v for k, v in input_model.keywords.items()}
+            if (input_model.molecule.molecular_multiplicity != 1) and ("reference" not in caseless_keywords):
+                input_data["keywords"]["reference"] = "uhf"
+
+            # Execute the program
+            success, output = execute([which("psi4"), "--scratch", tmpdir, "--json", "data.json"],
+                                      {"data.json": json.dumps(input_data)}, ["data.json"],
+                                      scratch_directory=tmpdir)
+
+            if success:
+                output_data = json.loads(output["outfiles"]["data.json"])
+                if "extras" not in output_data:
+                    output_data["extras"] = {}
+
+                # Check QCVars
+                local_qcvars = output_data.pop("psi4:qcvars", None)
+                if local_qcvars:
+                    # Edge case where we might already have qcvars, should not happen
+                    if "qcvars" in output_data["extras"]:
+                        output_data["extras"]["local_qcvars"] = local_qcvars
+                    else:
+                        output_data["extras"]["qcvars"] = local_qcvars
+
+                if output_data["success"] is False:
+                    if "error_message" not in output_data["error"]:
+                        # older c. 1.3 message-only run_json
+                        output_data["error"] = {"error_type": "internal_error", "error_message": output_data["error"]}
+            else:
+                output_data = input_data
+                output_data["error"] = {"error_type": "execution_error", "error_message": output["stderr"]}
+
+        # Reset the schema if required
+        output_data["schema_name"] = "qcschema_output"
+
+        # Dispatch errors, PSIO Errors are not recoverable for future runs
+        if output_data["success"] is False:
+            error_message = output_data["error"]["error_message"]
+
+            if "PSIO Error" in error_message:
+                if "scratch directory" in error_message:
+                    # Psi4 cannot access the folder or file
+                    raise ResourceError(error_message)
+                else:
+                    # Likely a random error, worth retrying
+                    raise RandomError(error_message)
+            elif ("SIGSEV" in error_message) or ("SIGSEGV" in error_message) or (
+                    "segmentation fault" in error_message):
+                raise RandomError(error_message)
+            elif "TypeError: set_global_option" in error_message:
+                raise InputError(error_message)
+            elif "RHF reference is only for singlets" in error_message:
+                raise InputError(error_message)
+            else:
+                raise UnknownError(error_message)
+
+        # Move several pieces up a level
+        output_data["provenance"]["memory"] = round(output_data.pop("memory") / (1024**3), 3)  # Move back to GB
+        output_data["provenance"]["nthreads"] = output_data.pop("nthreads")
+        output_data["stdout"] = output_data.pop("raw_output", None)
+
+        # Delete keys
+        output_data.pop("return_output", None)
+
+        return Result(**output_data)
---
+++ packages/qcengine/qcengine-0.11.0.tar.gz/qcengine-0.11.0/qcengine/programs/tests/test_terachem.py
@@ -0,0 +1,60 @@
+import os
+
+import pytest
+
+import qcelemental as qcel
+import qcengine as qcng
+from qcelemental.testing import compare_recursive
+from qcengine.testing import using_terachem
+
+# qcenginerecords not required, skips whole file
+qcer = pytest.importorskip("qcenginerecords")
+
+# Prep globals
+terachem_info = qcer.get_info('terachem')
+
+
+@pytest.mark.skipif(qcel.__version__ > "v.0.3.3", reason="qcelemental version is too new")
+@pytest.mark.parametrize('test_case', terachem_info.list_test_cases())
+def test_terachem_output_parser(test_case):
+    # Get output file data
+    data = terachem_info.get_test_data(test_case)
+    inp = qcel.models.ResultInput.parse_raw(data["input.json"])
+
+    output = qcng.get_program('terachem', check=False).parse_output(data, inp)
+
+    output_ref = qcel.models.Result.parse_raw(data["output.json"])
+
+    assert compare_recursive(output_ref.dict(), output.dict())
+
+
+@pytest.mark.parametrize('test_case', terachem_info.list_test_cases())
+def test_terachem_input_formatter(test_case):
+    # Get input file data
+    data = terachem_info.get_test_data(test_case)
+    inp = qcel.models.ResultInput.parse_raw(data["input.json"])
+
+    # TODO add actual comparison of generated input file
+    input_file = qcng.get_program('terachem', check=False).build_input(inp, qcng.get_config())
+    assert input_file.keys() >= {"commands", "infiles"}
+
+
+@using_terachem
+@pytest.mark.parametrize('test_case', terachem_info.list_test_cases())
+def test_terachem_executor(test_case):
+    # Get input file data
+    data = terachem_info.get_test_data(test_case)
+    inp = qcel.models.ResultInput.parse_raw(data["input.json"])
+
+    # Run Terachem
+    result = qcng.compute(inp, 'terachem')
+    # result = qcng.get_program('terachem').compute(inp, qcng.get_config())
+    assert result.success is True
+
+    # Get output file data
+    output_ref = qcel.models.Result.parse_raw(data["output.json"])
+
+    atol = 1e-6
+    if result.driver == "gradient":
+        atol = 1e-3
+    assert compare_recursive(output_ref.return_result, result.return_result, atol=atol)
