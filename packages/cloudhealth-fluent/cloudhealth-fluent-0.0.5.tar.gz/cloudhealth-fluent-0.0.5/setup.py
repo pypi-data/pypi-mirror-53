470f2f298a34ead16e31af5a0a3b5f3d46e4d118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:21 +0000

    Add package cloudhealth-fluent/0.0.5/cloudhealth-fluent-0.0.5.tar.gz

---
+++ packages/cloudhealth-fluent/cloudhealth-fluent-0.0.5.tar.gz/cloudhealth-fluent-0.0.5/setup.py
@@ -0,0 +1,26 @@
+import os
+from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+  name='cloudhealth-fluent',
+  install_requires=required,
+  version='0.0.5',
+  description='CloudHealth fluent interface',
+  author='troylar',
+  author_email='troylar@pm.me',
+  url='https://github.com/awslabs/cloudhealth-fluent',
+  keywords=['cloudhealth'],
+  license="Apache-2.0",
+  classifiers=[
+      "Development Status :: 5 - Production/Stable",
+      "Topic :: Utilities",
+      "License :: OSI Approved :: Apache Software License",
+  ]
+)
