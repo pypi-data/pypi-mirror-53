771ad517409058e5dd779f45a5a0ad9729591568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:58 +0000

    Add package pyGRNN/v0.1/pyGRNN-v0.1.tar.gz

---
+++ packages/pyGRNN/pyGRNN-v0.1.tar.gz/pyGRNN-v0.1/pyGRNN/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  8 15:03:06 2019
+
+@author: famato1
+"""
+from .GRNN import GRNN
+from .feature_selection import Isotropic_selector, Anisotropic_selector
---
+++ packages/pyGRNN/pyGRNN-v0.1.tar.gz/pyGRNN-v0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'pyGRNN',         
+  packages = ['pyGRNN'],   
+  version = 'v0.1',      
+  license='MIT',
+  description = 'Python implementation of General Regression Neural Network (Nadaraya-Watson Estimator). A Feature Selection module based on GRNN is also provided',   # Give a short description about your library
+  author = 'Federico Amato',                   
+  author_email = 'federico.amato@unil.ch',      #
+  url = 'https://github.com/federhub/pyGRNN',  
+  download_url = 'https://github.com/federhub/pyGRNN/archive/v0.1.tar.gz',    
+  keywords = ['Machine Learning', 'General Regression Neural Network', 'Kernel Regression', 'Feature Selection'],   
+  install_requires=[
+          'pandas',
+          'numpy',
+          'seaborn',
+          'scikit-learn',
+          'matplotlib',
+          'scipy',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',     
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.6',
+  ],
+)
