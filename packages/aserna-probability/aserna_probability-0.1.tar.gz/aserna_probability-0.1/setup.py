74c2db5e6031400d7c288ec6a4d32d3d58fe3b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:21 +0000

    Add package aserna-probability/0.1/aserna_probability-0.1.tar.gz

---
+++ packages/aserna-probability/aserna_probability-0.1.tar.gz/aserna_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='aserna_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['aserna_probability'],
+      author='Santiago Serna',
+      zip_safe=False)
