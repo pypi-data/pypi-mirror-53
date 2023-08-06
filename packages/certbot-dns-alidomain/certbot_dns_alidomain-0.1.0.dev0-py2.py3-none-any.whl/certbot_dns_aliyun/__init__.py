8477767825fdb60dcf149a7fe882256000f3e813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:34 +0000

    Add package certbot-dns-alidomain/0.1.0.dev0/certbot_dns_alidomain-0.1.0.dev0-py2.py3-none-any.whl

---
+++ packages/certbot-dns-alidomain/certbot_dns_alidomain-0.1.0.dev0-py2.py3-none-any.whl/certbot_dns_aliyun/__init__.py
@@ -0,0 +1,14 @@
+"""
+The `~certbot_dns_aliyun.dns_aliyun` plugin automates the process of
+completing a ``dns-01`` challenge (`~acme.challenges.DNS01`) by creating, and
+subsequently removing, TXT records using the Aliyun API.
+
+
+Credentials
+-----------
+
+# Aliyun API credentials used by Certbot
+certbot_dns_aliyun:dns_aliyun_api_key = xxxxxx
+certbot_dns_aliyun:dns_aliyun_secret_key = xxxxxxx
+
+"""
