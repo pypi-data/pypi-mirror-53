f50f6db7a89153ba3515850ac0a36d67a58f68b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:51 +0000

    Add package nuclear-python/0.11.3/nuclear_python-0.11.3-py3-none-any.whl

---
+++ packages/nuclear-python/nuclear_python-0.11.3-py3-none-any.whl/npy/utils/datastructure/d_of.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+from abc import ABCMeta
+
+
+__all__ = ['d_of_l', 'd_of_f', 'd_of_i', 'attrdict']
+
+
+class attrdict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+
+    def as_dict(self):
+        return dict(self)
+
+
+class d_of_sth(defaultdict):
+    __metaclass__ = ABCMeta
+    __getattr__ = dict.__getitem__
+
+    def as_dict(self):
+        return dict(self)
+
+
+#############################
+
+class d_of_l(d_of_sth):
+    def __init__(self):
+        super().__init__(list)
+
+    def appends(self, d):
+        for key, value in d.items():
+            self[key].append(value)
+
+
+class d_of_f(d_of_sth):
+    def __init__(self):
+        super().__init__(float)
+
+
+class d_of_i(d_of_sth):
+    def __init__(self):
+        super().__init__(int)
---
+++ packages/nuclear-python/nuclear_python-0.11.3-py3-none-any.whl/nuclear_python-0.11.3.data/scripts/pp_startup.py
@@ -0,0 +1,11 @@
+import numpy as np
+import os
+import shutil
+from glob import glob
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from imageio import imread, imsave
+import tensorflow as tf
+import npy
+from npy.ns import *
+import ntf
