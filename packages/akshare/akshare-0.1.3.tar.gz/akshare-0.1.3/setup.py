20e3ec7d3aed29a3fa910bd32f8e7ad922fffde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:33 +0000

    Add package akshare/0.1.3/akshare-0.1.3.tar.gz

---
+++ packages/akshare/akshare-0.1.3.tar.gz/akshare-0.1.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="akshare",
+    version="0.1.3",
+    author="Albert King",
+    author_email="jindaxiang@163.com",
+    description="a tools of downloading futures data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jindaxiang/akshare",
+    packages=setuptools.find_packages(),
+    package_data={'': ['*.py', '*.json']},
+    keywords=['futures', 'finance', 'spider'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7.3',
+)
