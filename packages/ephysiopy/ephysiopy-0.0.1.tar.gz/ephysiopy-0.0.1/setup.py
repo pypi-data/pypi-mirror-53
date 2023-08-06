9a961c0df4ab65d8e78fa7683d08794503bd976c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:04 +0000

    Add package ephysiopy/0.0.1/ephysiopy-0.0.1.tar.gz

---
+++ packages/ephysiopy/ephysiopy-0.0.1.tar.gz/ephysiopy-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from ephysiopy import __version__
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(name='ephysiopy',
+	  version=__version__,
+	  description='Analysis of electrophysiological data recorded with the Axona or OpenEphys recording systems',
+	  long_description=long_description,
+	  long_description_content_type='text/markdown',
+	  url='https://github.com/rhayman/ephysiopy',
+	  author='Robin Hayman',
+	  author_email='r.hayman@ucl.ac.uk',
+	  license='MIT',
+	  packages=['ephysiopy'],
+	  python_requires='>=3.5',
+	  install_requires=[
+	  	'numpy',
+	  	'scipy',
+	  	'matplotlib',
+	  	'astropy',
+	  	'scikit-image',
+	  	'mahotas',
+	  	'sklearn'
+	  ],
+	  zip_safe=False)
