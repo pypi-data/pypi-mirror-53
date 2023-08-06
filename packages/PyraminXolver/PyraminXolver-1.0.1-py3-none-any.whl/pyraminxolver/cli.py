d5cc04edfe3bfa1f1c144d98cb75996fe58e5148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:01 +0000

    Add package PyraminXolver/1.0.1/PyraminXolver-1.0.1-py3-none-any.whl

---
+++ packages/PyraminXolver/PyraminXolver-1.0.1-py3-none-any.whl/pyraminxolver/cli.py
@@ -0,0 +1,7 @@
+from lib.pyraminxolver import PyraminXolver
+
+while True:
+    pyra = PyraminXolver()
+    max_slack = int(input('How much slack do you allow per solution?'))
+    while True:
+        pass
