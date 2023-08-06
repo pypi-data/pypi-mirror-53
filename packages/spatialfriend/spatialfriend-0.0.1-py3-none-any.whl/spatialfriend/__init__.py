9538917ce174c421f54a83334d1ec6edaccd014c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:08 +0000

    Add package spatialfriend/0.0.1/spatialfriend-0.0.1-py3-none-any.whl

---
+++ packages/spatialfriend/spatialfriend-0.0.1-py3-none-any.whl/spatialfriend/__init__.py
@@ -0,0 +1,5 @@
+from spatialfriend.spatialfriend import Elevation, elev_gain, elevation_smooth, grade_smooth, grade_raw
+
+
+__all__ = ['Elevation', 'elev_gain', 'elevation_smooth', 
+           'grade_smooth', 'grade_raw']
