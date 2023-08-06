2ea3a5ffebac9a59b4e0eac355da15d9dd15cd43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:10 +0000

    Add package remita-billing-gateway/1.0.1/remita-billing-gateway-1.0.1.tar.gz

---
+++ packages/remita-billing-gateway/remita-billing-gateway-1.0.1.tar.gz/remita-billing-gateway-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="remita-billing-gateway",
+    version="1.0.1",
+    author="SystemSpecs Limited",
+    author_email="ipgtechnologyteam@gmail.com",
+    description="Python SDK for Remita Billing Gateway Service simple APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RemitaNet/billing-gateway-sdk-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Office/Business :: Financial :: Point-Of-Sale",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+    requests='>=2.22.0',
+)
