42cc734385ea9eae3b37f3d1b1f07eb1d579ee34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:34 +0000

    Add package dmx485/1.2.1/dmx485-1.2.1.tar.gz

---
+++ packages/dmx485/dmx485-1.2.1.tar.gz/dmx485-1.2.1/dmx/__init__.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import threading
+import serial
+import time
+import os
+
+if os.name == "posix":
+    import fcntl
+
+class DMX_Serial:
+    def __init__(self, port="/dev/ttyUSB0"):
+        if isinstance(port, str):
+            self.ser = serial.Serial(port)
+        else:
+            self.ser = port
+        self.ser.baudrate = 250000
+        self.ser.bytesize = serial.EIGHTBITS
+        self.ser.parity = serial.PARITY_NONE
+        self.ser.stopbits = serial.STOPBITS_TWO
+        self.ser.xonoff = False
+        self.desc = self.ser.fileno()
+        self.enabled = False
+        self.data = bytes((0,)*512)
+        self.nextdata = None
+        self.send_thread = threading.Thread(target=self.sender)
+        self.send_thread.daemon = True
+        self.send_thread.start()
+
+    def start(self):
+        self.enabled = True
+
+    def stop(self):
+        self.enabled = False
+
+    def sender(self):
+        while True:
+            if not(self.enabled):
+                continue
+            if os.name == "posix":
+                fcntl.ioctl(self.desc, 0x5427) # Yeah, it's magic. Start Break (TIOCSBRK)
+                time.sleep(0.0001)
+                fcntl.ioctl(self.desc, 0x5428) # Yeah, it's magic. End Break (TIOCCBRK)
+            else:
+                self.ser.send_break(0.0001)
+            self.ser.write(bytes((0,)))
+            self.ser.write(self.data)
+            self.ser.flush()
+            if self.nextdata:
+                self.data = self.nextdata
+                self.nextdata = None
+
+    def set_data(self, data):
+        self.nextdata = data
---
+++ packages/dmx485/dmx485-1.2.1.tar.gz/dmx485-1.2.1/setup.py
@@ -0,0 +1,29 @@
+import sys
+from setuptools import setup, find_packages
+
+with open("README.md") as FILE:
+    long_description = FILE.read()
+
+setup(
+    name='dmx485',
+    packages=find_packages(),
+    version='1.2.1',
+    description='Simple DMX driver using an RS485 adapter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author='Mark Murnane',
+    author_email='mark@hackafe.net',
+    url='https://github.com/bitbyt3r/dmx',
+    keywords=[
+        'dmx',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    install_requires=[
+        'pyserial'
+    ],
+)
