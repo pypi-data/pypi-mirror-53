6844a532be20a641b3cf090f45f4bd7b30c9d95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:56 +0000

    Add package EMenus/0.0.81/EMenus-0.0.81.tar.gz

---
+++ packages/EMenus/EMenus-0.0.81.tar.gz/EMenus-0.0.81/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='EMenus',
+    version='0.0.81',
+    description='EMenus - fexible menus, encryption and much more!',
+    license='MIT',
+    packages=['EMenus'],
+    author='Artur Zaytsev',
+    author_email='huskielunar@gmail.com',
+    keywords=['menus','easier','Artur Zaytsev','EMenus'],
+    url='https://github.com/HUSKI3/EMenus'
+)
