7eaaf5c3ea0d2a119a251dccc60d33a4e936b07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:57 +0000

    Add package rlpy3/2.0.0a0/rlpy3-2.0.0a0-cp37-cp37m-win_amd64.whl

---
+++ packages/rlpy3/rlpy3-2.0.0a0-cp37-cp37m-win_amd64.whl/rlpy/__init__.py
@@ -0,0 +1,10 @@
+import rlpy.Agents
+import rlpy.Tools
+import rlpy.Domains
+import rlpy.MDPSolvers
+import rlpy.Experiments
+import rlpy.Policies
+import rlpy.Representations
+
+
+__version__ = "2.0.0a0"
