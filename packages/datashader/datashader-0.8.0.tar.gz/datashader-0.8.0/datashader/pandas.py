3833e42299f04d8973ef478232b0263c46bf7976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:34 +0000

    Add package datashader/0.8.0/datashader-0.8.0.tar.gz

---
+++ packages/datashader/datashader-0.8.0.tar.gz/datashader-0.8.0/datashader/pandas.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import, division
+
+import pandas as pd
+
+from .core import bypixel
+from .compiler import compile_components
+from .glyphs.points import _PointLike
+from .glyphs.area import _AreaToLineLike
+from .utils import Dispatcher
+from collections import OrderedDict
+
+__all__ = ()
+
+
+@bypixel.pipeline.register(pd.DataFrame)
+def pandas_pipeline(df, schema, canvas, glyph, summary):
+    return glyph_dispatch(glyph, df, schema, canvas, summary)
+
+
+glyph_dispatch = Dispatcher()
+
+
+@glyph_dispatch.register(_PointLike)
+@glyph_dispatch.register(_AreaToLineLike)
+def default(glyph, df, schema, canvas, summary):
+    create, info, append, _, finalize = compile_components(summary, schema, glyph)
+    x_mapper = canvas.x_axis.mapper
+    y_mapper = canvas.y_axis.mapper
+    extend = glyph._build_extend(x_mapper, y_mapper, info, append)
+
+    x_range = canvas.x_range or glyph.compute_x_bounds(df)
+    y_range = canvas.y_range or glyph.compute_y_bounds(df)
+
+    width = canvas.plot_width
+    height = canvas.plot_height
+
+    x_st = canvas.x_axis.compute_scale_and_translate(x_range, width)
+    y_st = canvas.y_axis.compute_scale_and_translate(y_range, height)
+
+    x_axis = canvas.x_axis.compute_index(x_st, width)
+    y_axis = canvas.y_axis.compute_index(y_st, height)
+
+    bases = create((height, width))
+    extend(bases, df, x_st + y_st, x_range + y_range)
+
+    return finalize(bases,
+                    coords=OrderedDict([(glyph.x_label, x_axis),
+                                        (glyph.y_label, y_axis)]),
+                    dims=[glyph.y_label, glyph.x_label])
