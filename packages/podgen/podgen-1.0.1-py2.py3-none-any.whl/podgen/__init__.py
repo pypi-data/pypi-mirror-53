b9a5dcf386282df34aabcbc4452645ed4868198f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:24 +0000

    Add package podgen/1.0.1/podgen-1.0.1-py2.py3-none-any.whl

---
+++ packages/podgen/podgen-1.0.1-py2.py3-none-any.whl/podgen/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+    podgen
+    ~~~~~~
+
+    Package which makes it easy to generate podcast RSS using Python.
+
+    See the official documentation at https://podgen.readthedocs.org
+
+    :copyright: 2016, Thorben Dahl <thorben@sjostrom.no>
+    :license: FreeBSD and LGPL, see license.* for more details.
+"""
+# Support for Python 2.7
+from __future__ import absolute_import, division, print_function, unicode_literals
+from builtins import *
+
+from .podcast import Podcast
+from .episode import Episode
+from .media import Media
+from .person import Person
+from .not_supported_by_itunes_warning import NotSupportedByItunesWarning
+from .category import Category
+from .util import htmlencode
