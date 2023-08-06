f1abf74d46255085e1c5c5a8086495473383b421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:23 +0000

    Add package othertools/1.0.1/othertools-1.0.1.tar.gz

---
+++ packages/othertools/othertools-1.0.1.tar.gz/othertools-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='othertools',
+    version='1.0.1',
+    packages=['othertools'],
+    url='https://github.com/zeuscronos/python-other-tools',
+    license='Apache 2.0',
+    author='Zeus Cronos',
+    author_email='noreply@notavailable.com',
+    description='Other Tools',
+    long_description='Other Tools'
+)
