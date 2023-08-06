826c15d4648ebc664bfcb5bb239c7abcfa77006b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:21 +0000

    Add package presalytics/0.1.19/presalytics-0.1.19-py3-none-any.whl

---
+++ packages/presalytics/presalytics-0.1.19-py3-none-any.whl/test/test_client.py
@@ -0,0 +1,27 @@
+"""
+Unit test for Client class
+"""
+import unittest
+import os
+from presalytics.client.api import Client
+
+class test_client(unittest.TestCase):
+    """ Test whether client accurately creates a configuration
+        and establishes a connection to the server
+    """
+    def setUp(self):
+        self.config_file = os.path.join(os.path.dirname(__file__), 'files', 'config.py')
+
+    def test_client(self):
+        """ 
+        Tests for client configuration into presalytics api
+        """
+        client = Client(config_file=self.config_file)
+        username = os.environ["PRESALYTICS_USERNAME"]
+        self.assertEqual(client.doc_converter.api_client.username, username)
+        self.assertEqual(client.ooxml_automation.api_client.configuration.host, os.environ["OOXML_AUTOMATION_HOST"])
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
