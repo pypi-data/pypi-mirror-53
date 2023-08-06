102b7b35ea4979cceaa760789e53cfecd7da706e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:38 +0000

    Add package vcoclient/0.1.8/vcoclient-0.1.8.tar.gz

---
+++ packages/vcoclient/vcoclient-0.1.8.tar.gz/vcoclient-0.1.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setup(
+     name='vcoclient',  
+     version='0.1.8',
+     scripts=['vcoclient.py'] ,
+     author="Iddo Cohen",
+     author_email="iddocohen@gmail.com",
+     description="A simple VeloCloud Orchestrator (VCO) Python client",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/iddocohen/vcoclient",
+     packages=find_packages(),
+     python_requires=">=3.6",
+     install_requires=requirements,
+     classifiers=[
+         "Programming Language :: Python :: 3.6",
+         "Programming Language :: Python :: 3.7",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
