6da86f45b98e8506fee2fdaa4f215050cfda3f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:17 +0000

    Add package mdbl/1.0.7/mdbl-1.0.7.tar.gz

---
+++ packages/mdbl/mdbl-1.0.7.tar.gz/mdbl-1.0.7/mdbl/mdbl.py
@@ -0,0 +1,12 @@
+import requests
+
+class MDBL:
+
+    def botlink(self, bot_id):
+        url = "https://mdbl.ml/botok/{}/data.json".format(bot_id)
+        link = requests.get(url).json()
+        return link['link']
+
+
+if __name__ == '__main__':
+    main()
