354e1af93d8a0bfdff1ae4e9544a28e984701396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:35 +0000

    Add package ambition-prn/0.1.63/ambition_prn-0.1.63-py3-none-any.whl

---
+++ packages/ambition-prn/ambition_prn-0.1.63-py3-none-any.whl/ambition_prn/models/death_report_tmg.py
@@ -0,0 +1,43 @@
+from django.db import models
+from edc_adverse_event.constants import DEATH_REPORT_TMG_SECOND_ACTION
+from edc_adverse_event.model_mixins import DeathReportTmgModelMixin
+from edc_model.models import BaseUuidModel
+from django.db.models.deletion import PROTECT
+from edc_constants.constants import QUESTION_RETIRED, NOT_APPLICABLE
+from ambition_prn.choices import TB_SITE_DEATH
+
+
+class DeathReportTmg(DeathReportTmgModelMixin, BaseUuidModel):
+
+    death_report = models.ForeignKey(
+        f"ambition_prn.deathreport", on_delete=PROTECT)
+
+    cause_of_death_old = models.CharField(
+        verbose_name="Main cause of death",
+        max_length=50,
+        default=QUESTION_RETIRED,
+        blank=True,
+        null=True,
+        help_text="Main cause of death in the opinion of TMG member",
+    )
+
+    tb_site = models.CharField(
+        verbose_name="If cause of death is TB, specify site of TB disease",
+        max_length=25,
+        choices=TB_SITE_DEATH,
+        default=NOT_APPLICABLE,
+        blank=True,
+    )
+
+    class Meta(DeathReportTmgModelMixin.Meta):
+        pass
+
+
+class DeathReportTmgSecond(DeathReportTmg):
+
+    action_name = DEATH_REPORT_TMG_SECOND_ACTION
+
+    class Meta:
+        proxy = True
+        verbose_name = "Death Report TMG (Second)"
+        verbose_name_plural = "Death Report TMG (Second)"
