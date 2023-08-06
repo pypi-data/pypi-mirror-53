32a9f1c787178ded954004b6cce32cac1e6343e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:01 +0000

    Add package mynblep/3/mynblep-3.tar.gz

---
+++ packages/mynblep/mynblep-3.tar.gz/mynblep-3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name = 'mynblep',
+        version = '3',
+        install_requires = ['numpy', 'pyrbo'],
+        packages = setuptools.find_packages(),
+        py_modules = [],
+        scripts = [])
