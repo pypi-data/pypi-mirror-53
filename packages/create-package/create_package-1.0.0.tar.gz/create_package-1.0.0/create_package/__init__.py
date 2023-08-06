4185d7193d74ac22da17b5b0a1bf847959f13dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:07 +0000

    Add package create-package/1.0.0/create_package-1.0.0.tar.gz

---
+++ packages/create-package/create_package-1.0.0.tar.gz/create_package-1.0.0/create_package/__init__.py
@@ -0,0 +1,1 @@
+'name="create_package"'
---
+++ packages/create-package/create_package-1.0.0.tar.gz/create_package-1.0.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="create_package",
+    version="1.0.0",
+    author="Rui Aguiar",
+    author_email="raguiar1000@gmail.com",
+    description="A package that makes it easy to create Pypi packages",
+    url="https://github.com/raguiar2/create-python-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+) 
