1af09553c1069e4e0a8ff6d35082fabf85a7c42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:49 +0000

    Add package ccy_nester/1.1.0/ccy_nester-1.1.0.tar.gz

---
+++ packages/ccy_nester/ccy_nester-1.1.0.tar.gz/ccy_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'ccy_nester',
+    version         = '1.1.0',
+    py_modules      = ['ccy_nester'],
+    author          = 'bruce chen',
+    author_email    = '2005ccy@163.com',
+    url             = 'http://www.baidu.com',
+    description     = 'A simple printer of nested lists',
+)
