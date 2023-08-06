99ad9d180bde097abc51ebe58eb5639d3b3d279e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:40 +0000

    Add package pytest-sanic/1.1.2/pytest-sanic-1.1.2.tar.gz

---
+++ packages/pytest-sanic/pytest-sanic-1.1.2.tar.gz/pytest-sanic-1.1.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+base = os.path.dirname(os.path.abspath(__file__))
+
+README_PATH = os.path.join(base, "README.rst")
+
+install_requires = [
+    'pytest',
+    'aiohttp',
+    'async_generator',
+]
+
+tests_require = []
+
+setup(name='pytest-sanic',
+      version='1.1.2',
+      description='a pytest plugin for Sanic',
+      long_description=open(README_PATH).read(),
+      author='Yun Xu',
+      author_email='yunxu1992@gmail.com',
+      url='https://github.com/yunstanford/pytest-sanic/',
+      packages=find_packages(exclude=["tests.*", "tests"]),
+      install_requires=install_requires,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Operating System :: MacOS',
+          'Operating System :: POSIX :: Linux',
+          'Topic :: System :: Software Distribution',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      entry_points={
+        'pytest11': ['sanic = pytest_sanic.plugin'],
+      },
+)
