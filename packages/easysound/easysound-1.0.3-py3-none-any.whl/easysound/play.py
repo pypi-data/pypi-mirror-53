4cdeaaca54e90174e0ab459f663f3640e78514e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:27 +0000

    Add package easysound/1.0.3/easysound-1.0.3-py3-none-any.whl

---
+++ packages/easysound/easysound-1.0.3-py3-none-any.whl/easysound/play.py
@@ -0,0 +1,13 @@
+from playsound import playsound
+import os
+
+
+def play(sound):
+    playsound(sound)
+def ui_play(sound):
+    os.system(sound)
+
+
+
+
+
---
+++ packages/easysound/easysound-1.0.3-py3-none-any.whl/easysound/rec.py
@@ -0,0 +1,42 @@
+import pyaudio
+import wave
+
+
+
+def rec(file_name,seconds,startmsg,endmsg):
+
+
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 16000
+    RECORD_SECONDS = seconds
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print(startmsg)
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print(endmsg)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(file_name, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
