1036f4820c017252b79a58b446d4fd9008f5bccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:47 +0000

    Add package edc-adverse-event/0.1.6/edc_adverse_event-0.1.6-py3-none-any.whl

---
+++ packages/edc-adverse-event/edc_adverse_event-0.1.6-py3-none-any.whl/edc_adverse_event/form_validators/death_report_tmg.py
@@ -0,0 +1,91 @@
+from django import forms
+from edc_constants.constants import OTHER, CLOSED, NO, YES
+from edc_form_validators import FormValidator
+
+
+class DeathReportTmgFormValidator(FormValidator):
+    def clean(self):
+
+        self.required_if(
+            CLOSED,
+            field="report_status",
+            field_required="cause_of_death",
+            inverse=False,
+        )
+
+        self.validate_other_specify(
+            field="cause_of_death",
+            other_specify_field="cause_of_death_other",
+            other_stored_value=OTHER,
+        )
+
+        if self.cause_of_death:
+            if (
+                self.cleaned_data.get("cause_of_death_agreed") == NO
+                and self.death_report_cause_of_death == self.tmg_cause_of_death
+            ):
+                raise forms.ValidationError(
+                    {
+                        "cause_of_death_agreed": (
+                            "Cause of death reported by the study doctor matches "
+                            "your assessment."
+                        )
+                    }
+                )
+            elif (
+                self.cleaned_data.get("cause_of_death_agreed") == YES
+                and self.cause_of_death != self.tmg_cause_of_death
+            ):
+                raise forms.ValidationError(
+                    {
+                        "cause_of_death_agreed": (
+                            "Cause of death reported by the study doctor "
+                            "does not match your assessment."
+                        )
+                    }
+                )
+
+        self.required_if(
+            CLOSED,
+            field="report_status",
+            field_required="cause_of_death_agreed",
+            inverse=False,
+        )
+
+        self.required_if(
+            NO, field="cause_of_death_agreed", field_required="narrative", inverse=False
+        )
+
+        self.required_if(
+            CLOSED, field="report_status", field_required="report_closed_datetime"
+        )
+
+    @property
+    def cause_of_death(self):
+        try:
+            return self.cleaned_data.get("cause_of_death").short_name
+        except AttributeError:
+            return None
+
+    @property
+    def death_report_cause_of_death(self):
+        death_report = (
+            self.cleaned_data.get("death_report") or self.instance.death_report
+        )
+        if death_report.cause_of_death.short_name == OTHER:
+            death_report_cause_of_death = (
+                (death_report.cause_of_death_other or "").strip().lower()
+            )
+        else:
+            death_report_cause_of_death = death_report.cause_of_death.short_name
+        return death_report_cause_of_death
+
+    @property
+    def tmg_cause_of_death(self):
+        if self.cause_of_death == OTHER:
+            tmg_cause_of_death = (
+                (self.cleaned_data.get("cause_of_death_other") or "").strip().lower()
+            )
+        else:
+            tmg_cause_of_death = self.cause_of_death
+        return tmg_cause_of_death
---
+++ packages/edc-adverse-event/edc_adverse_event-0.1.6-py3-none-any.whl/edc_adverse_event/model_mixins/death_report_tmg_model_mixin.py
@@ -0,0 +1,115 @@
+from django.conf import settings
+from django.db import models
+from django.db.models.deletion import PROTECT
+from edc_action_item.managers import (
+    ActionIdentifierSiteManager,
+    ActionIdentifierManager,
+)
+from edc_action_item.models import ActionModelMixin
+from edc_adverse_event.models import CauseOfDeath
+from edc_constants.choices import YES_NO
+from edc_identifier.model_mixins import (
+    TrackingModelMixin,
+    NonUniqueSubjectIdentifierFieldMixin,
+)
+from edc_model.models import ReportStatusModelMixin
+from edc_model.validators.date import datetime_not_future
+from edc_protocol.validators import datetime_not_before_study_start
+from edc_sites.models import SiteModelMixin
+from edc_utils import get_utcnow
+
+from ..constants import DEATH_REPORT_TMG_ACTION
+
+
+class DeathReportTmgFieldsModelMixin(models.Model):
+
+    death_report = models.ForeignKey(
+        f"{settings.ADVERSE_EVENT_APP_LABEL}.deathreport", on_delete=PROTECT
+    )
+
+    report_datetime = models.DateTimeField(
+        verbose_name="Report Date",
+        validators=[datetime_not_before_study_start, datetime_not_future],
+        default=get_utcnow,
+    )
+
+    cause_of_death = models.ForeignKey(
+        CauseOfDeath,
+        on_delete=PROTECT,
+        verbose_name=("Main cause of death"),
+        help_text=(
+            "Main cause of death in the opinion of the "
+            "local study doctor and local PI"
+        ),
+        null=True,
+    )
+
+    cause_of_death_other = models.CharField(
+        verbose_name='If "Other" above, please specify',
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+
+    cause_of_death_agreed = models.CharField(
+        verbose_name=(
+            "Is the cause of death agreed between study doctor and TMG member?"
+        ),
+        max_length=15,
+        choices=YES_NO,
+        blank=True,
+        null=True,
+        help_text="If No, explain in the narrative below",
+    )
+
+    narrative = models.TextField(verbose_name="Narrative", blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class DeathReportTmgMethodsModelMixin(models.Model):
+    def __str__(self):
+        return str(self.death_report)
+
+    def save(self, *args, **kwargs):
+        self.subject_identifier = self.death_report.subject_identifier
+        super().save(*args, **kwargs)
+
+    def natural_key(self):
+        return (self.action_identifier,)
+
+    natural_key.dependencies = ["edc_adverse_event.causeofdeath"]
+
+    class Meta:
+        abstract = True
+
+
+class DeathReportTmgModelMixin(
+    NonUniqueSubjectIdentifierFieldMixin,
+    ActionModelMixin,
+    TrackingModelMixin,
+    DeathReportTmgFieldsModelMixin,
+    DeathReportTmgMethodsModelMixin,
+    ReportStatusModelMixin,
+    SiteModelMixin,
+    models.Model,
+):
+
+    action_name = DEATH_REPORT_TMG_ACTION
+
+    tracking_identifier_prefix = "DT"
+
+    on_site = ActionIdentifierSiteManager()
+
+    objects = ActionIdentifierManager()
+
+    class Meta:
+        abstract = True
+        verbose_name = "Death Report TMG"
+        verbose_name_plural = "Death Report TMG"
+        indexes = [
+            models.Index(
+                fields=["subject_identifier", "action_identifier", "site", "id"]
+            )
+        ]
