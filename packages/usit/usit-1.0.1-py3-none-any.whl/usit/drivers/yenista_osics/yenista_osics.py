1c52b79e543653de80a0f4cfe1556687bcbd025c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:39 +0000

    Add package usit/1.0.1/usit-1.0.1-py3-none-any.whl

---
+++ packages/usit/usit-1.0.1-py3-none-any.whl/usit/drivers/yenista_osics/yenista_osics.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Supported instruments (identified):
+- 
+"""
+
+from module_sld import SLD
+from module_t100 import T100
+
+modules = {'sld':SLD,'t100':T100}
+
+class Device():
+    
+    slotNaming = 'slot<NUM> = <MODULE_NAME>,<SLOT_NAME>'
+
+    def __init__(self,**kwargs):
+        
+        self.write('*RST') # The input buffer is cleared. The command interpreter is reset and a reset instruction is sent to every module. The status and event registers are cleared. Sets the OPC bit to 1.
+        self.write('*CLS') # Clears the Event Status Register and the output queue. Sets the OPC bit to 1.
+        
+        # Submodules loading
+        self.slotnames = []
+        prefix = 'slot'
+        for key in kwargs.keys():
+            if key.startswith(prefix):
+                slot_num = key[len(prefix):]
+                module = modules[ kwargs[key].split(',')[0].strip() ]
+                name = kwargs[key].split(',')[1].strip()
+                setattr(self,name,module(self,slot_num))
+                self.slotnames.append(name)
+
+
+    def getID(self):
+        return self.query('*IDN?')
+
+
+    def getDriverConfig(self):
+        config = []
+        for name in self.slotnames :
+            config.append({'element':'module','name':name,'object':getattr(self,name)})
+        return config
+
+
+#################################################################################
+############################## Connections classes ##############################
+class Device_VISA(Device):
+    def __init__(self, address='GPIB0::2::INSTR',**kwargs):
+        
+        import visa
+        
+        
+        self.TIMEOUT = 60000 #ms
+        
+        rm = visa.ResourceManager()
+        self.controller = rm.open_resource(address)
+        self.controller.timeout = self.TIMEOUT
+        
+        Device.__init__(self,**kwargs)
+        
+
+    def close(self):
+        try : self.controller.close()
+        except : pass
+
+
+    def write(self,command):
+        self.controller.write(command)
+        
+    def read(self):
+        result = self.controller.read()
+        result = result.strip('\n')
+        return result
+    
+    def query(self,command):
+        result = self.controller.query(command)
+        result = result.strip('\n')
+        return result
+        
+############################## Connections classes ##############################
+#################################################################################
+
+
+
+#ADDRESS = 'GPIB0::11::INSTR'
+        
+    
+    
+  
+  
