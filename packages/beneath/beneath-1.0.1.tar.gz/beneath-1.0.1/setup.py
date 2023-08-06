ac0d53a87c088222a3570ff39f049d783ff14b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:26 +0000

    Add package beneath/1.0.1/beneath-1.0.1.tar.gz

---
+++ packages/beneath/beneath-1.0.1.tar.gz/beneath-1.0.1/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+# Get long_description
+with open('README.md', 'r') as f:
+  LONG_DESCRIPTION = f.read()
+
+# Get version
+with open('beneath/_version.py', 'r') as f:
+  for line in f:
+    if line.startswith('__version__'):
+      _, _, VERSION = line.replace('"', '').split()
+      break
+
+setuptools.setup(
+  name="beneath",
+  version=VERSION,
+  author="Beneath Systems",
+  author_email="hello@beneath.network",
+  description="Python client for Beneath (https://beneath.network/)",
+  url="https://gitlab.com/_beneath/beneath-python",
+  long_description=LONG_DESCRIPTION,
+  long_description_content_type='text/markdown',
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  scripts=['beneath/bin/beneath'],
+  install_requires=[
+    'apache_beam[gpc]>=2.14.0',
+    'argparse>=1.1',
+    'fastavro>=0.21.24',
+    'grpcio>=1.23.0',
+    'pandas>=0.24.2',
+    'six>=1.12.0',
+  ]
+)
