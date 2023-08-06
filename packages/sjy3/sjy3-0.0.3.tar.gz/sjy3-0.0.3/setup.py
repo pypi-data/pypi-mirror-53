ad2846e8c2370253cf281f8de1948c32d16ac877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:18 +0000

    Add package sjy3/0.0.3/sjy3-0.0.3.tar.gz

---
+++ packages/sjy3/sjy3-0.0.3.tar.gz/sjy3-0.0.3/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='sjy3',
+    version='0.0.3',
+    packages=find_packages(),
+    author='sjy',
+    author_email='coderinfo@163.com',
+    license='Apache',
+    url='http://pycn.me'
+)
