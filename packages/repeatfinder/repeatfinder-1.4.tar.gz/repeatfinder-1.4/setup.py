0db35abf42936682ed2014c966dca602a41a84af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:06 +0000

    Add package repeatfinder/1.4/repeatfinder-1.4.tar.gz

---
+++ packages/repeatfinder/repeatfinder-1.4.tar.gz/repeatfinder-1.4/setup.py
@@ -0,0 +1,30 @@
+import os
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
+
+os.environ["CC"] = "g++"
+os.environ["CXX"] = "g++"
+#os.environ["CPP"] = "g++"
+#os.environ["LDSHARED"] = "g++"
+
+compile_args = ["-O3", "-std=c++11"]
+#compile_args = ["-O3", "-g"]
+link_args    = ["-lstdc++"]
+
+repeatfinder_module = Extension('repeatfinder',
+                    language='g++',
+                    extra_compile_args=compile_args,
+                    extra_link_args=link_args,
+                    include_dirs=[
+                             '.',
+                             '...',
+                             os.path.join(os.getcwd(), 'include'),
+                    ],
+                    library_dirs = [os.getcwd(),],
+                    sources = ['src/repeatfinder.cpp'])
+
+setup (name = 'repeatfinder',
+       version = '1.4',
+       description = 'A package for finding repeats in a DNA sequence',
+       python_requires='>3.5.2',
+       ext_modules = [repeatfinder_module])
