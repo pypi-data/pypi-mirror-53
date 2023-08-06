4d67e240a645556c876632bc2705fb12aa241445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:41 +0000

    Add package ngboost/0.1.1/ngboost-0.1.1-py3-none-any.whl

---
+++ packages/ngboost/ngboost-0.1.1-py3-none-any.whl/ngboost/distns/__init__.py
@@ -0,0 +1,4 @@
+from .normal import Normal, HomoskedasticNormal
+from .laplace import Laplace
+from .lognormal import LogNormal, HomoskedasticLogNormal
+from .loglaplace import LogLaplace
---
+++ packages/ngboost/ngboost-0.1.1-py3-none-any.whl/ngboost/learners.py
@@ -0,0 +1,27 @@
+import jax.numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression, Ridge
+from scipy.interpolate import UnivariateSpline
+
+
+class ConstantLearner(object):
+
+    def fit(self, x, y):
+        self.const = 1
+
+    def predict(self, x):
+        return self.const
+
+
+tree_learner_with_depth = lambda d: DecisionTreeRegressor(
+    criterion='friedman_mse',
+    min_samples_split=2,
+    min_samples_leaf=1,
+    min_weight_fraction_leaf=0.0,
+    max_depth=d,
+    splitter='best')
+
+default_tree_learner = lambda: tree_learner_with_depth(3)
+# default_linear_learner = LinearRegression
+default_linear_learner = lambda: Ridge(alpha = 1)
+default_constant_learner = ConstantLearner
