54a6ec670ede75ceb00e19eaf3249e617abba7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:25 +0000

    Add package thoraxe/0.2.0/thoraxe-0.2.0.tar.gz

---
+++ packages/thoraxe/thoraxe-0.2.0.tar.gz/thoraxe-0.2.0/tests/test_subexons.py
@@ -0,0 +1,85 @@
+import os
+import pytest
+
+from thoraxe import transcript_info
+from thoraxe import subexons
+
+
+@pytest.fixture(scope='module')
+def clustered_trx_data(request):
+    filename = request.module.__file__
+
+    def _get_clustered_trx_data(folder):
+        test_dir = os.path.dirname(filename)
+        datadir = os.path.join(test_dir, 'data')
+        folder_path = os.path.join(datadir, folder, 'Ensembl')
+        trx_data = transcript_info.read_transcript_info(
+            os.path.join(folder_path, 'tsl.csv'),
+            os.path.join(folder_path, 'exonstable.tsv'),
+            os.path.join(folder_path, 'sequences.fasta'),
+            remove_na=False)
+        return transcript_info.exon_clustering(trx_data)
+
+    return _get_clustered_trx_data
+
+
+def test_subexon_table(clustered_trx_data):
+    data = clustered_trx_data('GPRIN1')
+    subexon_table = subexons.create_subexon_table(data)
+
+    assert 'SubexonID' in subexon_table.columns
+    assert 'SubexonIDCluster' in subexon_table.columns
+
+    assert 'ENSMUST00000099506/ENSMUST00000135343' in subexon_table[
+        'TranscriptIDCluster'].values  # .values, otherwise in
+    # checks whether the value is in the Series' index.
+
+    assert all(subexon_table['TranscriptFraction'] == 1.0)
+
+
+def test_subexon_clusters(clustered_trx_data):
+    data = clustered_trx_data('MAPK8')
+    subexon_table = subexons.create_subexon_table(data)
+    assert len(
+        subexon_table.loc[subexon_table['SubexonIDCluster'] ==
+                          'ENSMMUE00000040028_SE_0/ENSMMUE00000387789_SE_0',
+                          'SubexonID'].unique()) == 1
+
+    not_merged = subexons.create_subexon_table(data, merge_non_redundant=False)
+    assert not_merged.shape[0] > subexon_table.shape[0]
+
+    # QVQQ
+    assert len(
+        not_merged[not_merged['SubexonID'] == 'ENSMUSE00000689835_SE_2']) > 0
+    assert len(
+        not_merged[not_merged['SubexonID'] == 'ENSMUSE00000689835_SE_1']) > 0
+    assert len(subexon_table[subexon_table['SubexonID'] ==
+                             'ENSMUSE00000689835_SE_2']) == 0
+    assert len(subexon_table[subexon_table['SubexonID'] ==
+                             'ENSMUSE00000689835_SE_1']) == 0
+    qvqq = subexon_table[subexon_table['SubexonID'] ==
+                         'ENSMUSE00000689835_SE_2_1']
+    assert len(qvqq) == 2
+    assert sorted(
+        qvqq['TranscriptID']) == ['ENSMUST00000111943', 'ENSMUST00000111945']
+    for index in [0, 1]:
+        assert qvqq['Strand'].iloc[index] == -1
+        assert str(qvqq['SubexonProteinSequence'].iloc[index]) == 'QVQQ*'
+        assert str(qvqq['SubexonSequence'].iloc[index]) == 'CACAGGTGCAGCAATGA'
+        assert qvqq['ExonIDCluster'].iloc[
+            index] == 'ENSMUSE00000689835/ENSMUSE00000689841'
+
+    # VINGSQHPSSSSSVNDVSSMSTDPTLASDTDSSLEASAGPLGCCR
+    exon_row = subexon_table[subexon_table['ExonID'] ==
+                             'ENSMMUE00000040064'].iloc[0]
+    assert exon_row['SubexonID'] == 'ENSMMUE00000040064_SE_15_16'
+    assert exon_row['SubexonIDCluster'] == 'ENSMMUE00000040064_SE_15_16'
+    assert str(exon_row['SubexonProteinSequence']
+               ) == 'VINGSQHPSSSSSVNDVSSMSTDPTLASDTDSSLEASAGPLGCCR*'
+    assert str(exon_row['SubexonSequence']) == (
+        'CAGTGATCAATGGCTCTCAGCATCCATCGTCATCGTCGTCTGTCAATGATGTGTCTTCAA'
+        'TGTCAACAGATCCGACTTTGGCCTCGGATACAGACAGCAGTCTAGAAGCATCAGCTGGGC'
+        'CTCTGGGCTGCTGTAGATGA')
+    assert exon_row['Strand'] == 1
+    assert exon_row['SubexonCodingStart'] == 42539779
+    assert exon_row['SubexonCodingEnd'] == 42539918
---
+++ packages/thoraxe/thoraxe-0.2.0.tar.gz/thoraxe-0.2.0/thoraxe/subexons/graph.py
@@ -0,0 +1,94 @@
+"""
+graph: Functions to store the splice graph with conservation information.
+"""
+
+import collections
+import logging
+
+from functools import reduce
+
+
+def nodes_and_edges2genes(data):
+    """
+    Return two dictionaries, ones from nodes and other from edges to genes.
+    """
+    node2genes = collections.defaultdict(set)
+    edge2genes = collections.defaultdict(set)
+    for gene_id, gene in data.groupby('GeneID'):  # noqa pylint: disable=too-many-nested-blocks
+        for _, transcript in gene.groupby('TranscriptID'):
+            subexons = transcript.sort_values('SubexonRank')['S_exons']
+
+            previous = 'start'
+            node2genes[previous].update({gene_id})
+            for _, subexon in enumerate(subexons):
+                orthologs = subexon.split('/')
+                for ortholog in orthologs:
+                    node2genes[ortholog].update({gene_id})
+                    edge2genes[(previous, ortholog)].update({gene_id})
+                    previous = ortholog
+
+            node2genes['stop'].update({gene_id})
+            edge2genes[(previous, 'stop')].update({gene_id})
+
+    return node2genes, edge2genes
+
+
+def _get_genes(node2genes):
+    """
+    Return a set of genes from the node to (set of) genes dictionary.
+
+    >>> sorted(_get_genes({'a':{1, 2}, 'b':{2, 3}, 'c':{1}, 'd':{4}}))
+    [1, 2, 3, 4]
+    """
+    return reduce(set.union, node2genes.values())
+
+
+def splice_graph_gml(filename, node2genes, edge2genes):
+    """
+    Store the splice graph in GML (Graph Modelling Language) format.
+
+    It stores the conservation level information for nodes and edges, where
+    conservation is the fraction of species that has that particular feature.
+    """
+    if not filename.endswith('.gml'):
+        filename += '.gml'
+        logging.warning(
+            '.gml extension was added, the splice graph will be stored at %s',
+            filename)
+
+    n_genes = len(_get_genes(node2genes))
+
+    with open(filename, 'w') as gml:
+        gml.write('''
+        graph [
+            directed 1
+            id 42
+            label "splice graph of s-exons"
+        ''')
+        node2id = {}
+        node_id = 1
+        for node, genes in node2genes.items():
+            conservation = 100.0 * (len(genes) / n_genes)
+            gml.write('''
+                node [
+                    id {}
+                    label "{}"
+                    conservation {}
+                ]
+            '''.format(node_id, node, conservation))
+            node2id[node] = node_id
+            node_id += 1
+        for edge, genes in edge2genes.items():
+            conservation = 100.0 * (len(genes) / n_genes)
+            gml.write('''
+                edge [
+                    source {}
+                    target {}
+                    conservation {}
+                ]
+            '''.format(node2id[edge[0]], node2id[edge[1]], conservation))
+        gml.write('''
+        ]
+        ''')
+
+    return filename
