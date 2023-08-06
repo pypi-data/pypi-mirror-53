cad8ddabcf6bf0eaa2afe9ad7ccb3154ef22a0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:20 +0000

    Add package procrastinate/0.2.1/procrastinate-0.2.1.tar.gz

---
+++ packages/procrastinate/procrastinate-0.2.1.tar.gz/procrastinate-0.2.1/procrastinate/__init__.py
@@ -0,0 +1,21 @@
+from procrastinate import metadata as _metadata_module
+from procrastinate.app import App
+from procrastinate.psycopg2_connector import PostgresJobStore
+from procrastinate.retry import BaseRetryStrategy, RetryStrategy
+from procrastinate.store import BaseJobStore
+
+__all__ = [
+    "App",
+    "BaseJobStore",
+    "BaseRetryStrategy",
+    "PostgresJobStore",
+    "RetryStrategy",
+]
+
+
+_metadata = _metadata_module.extract_metadata()
+__author__ = _metadata["author"]
+__author_email__ = _metadata["email"]
+__license__ = _metadata["license"]
+__url__ = _metadata["url"]
+__version__ = _metadata["version"]
