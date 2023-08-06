ff615869bfd9e48084b6accad0c8727dd7cf58f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:42 +0000

    Add package holiday-jp/19.10.14/holiday_jp-19.10.14.tar.gz

---
+++ packages/holiday-jp/holiday_jp-19.10.14.tar.gz/holiday_jp-19.10.14/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='holiday_jp',
+      version='19.10.14',
+      url='https://github.com/LUXEYS/holiday_jp-python',
+      license='MIT',
+      author='Luxeys',
+      author_email='tessier@luxeys.com',
+      description='Japanese holiday for Python',
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      long_description=open('README.md').read(),
+      zip_safe=False,
+      install_requires=[
+        "python-dateutil==2.7.5",
+      ],
+      classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+])
