f0caaf514326986b28ff25bd1e0cf821adca7bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:05 +0000

    Add package lpcdbeclient/0.5/lpcdbeclient-0.5.tar.gz

---
+++ packages/lpcdbeclient/lpcdbeclient-0.5.tar.gz/lpcdbeclient-0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='lpcdbeclient',
+    version='0.5',
+    license='BSD 3-Clause License',
+    description='Client for inferencing the LPC DBE',
+    url='https://github.com/tklijnsma/lpcdbeclient.git',
+    download_url='https://github.com/tklijnsma/lpcdbeclient/archive/v0_5.tar.gz',
+    author='Thomas Klijnsma',
+    author_email='tklijnsm@gmail.com',
+    packages=['lpcdbeclient'],
+    zip_safe=False,
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    scripts=[
+        'bin/lpcdbe-run',
+        'bin/lpcdbe-run-test',
+        'bin/lpcdbe-deploymodule',
+        ],
+    )
