84f8391ff6fe8a7b18206bdcce442b907d0dcdf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:09 +0000

    Add package paulo-distributions/0.1/paulo_distributions-0.1.tar.gz

---
+++ packages/paulo-distributions/paulo_distributions-0.1.tar.gz/paulo_distributions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='paulo_distributions',
+    version='0.1',
+    description='Gaussian and Binomial distributions',
+    packages=['paulo_distributions'],
+    author="Paulo Arantes",
+    zip_safe=False
+)
