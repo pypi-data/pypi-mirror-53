eae6a3e7c7c80460cb8afa1ddabd5b871efcd800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:31 +0000

    Add package wqrfnium/0.1.5/wqrfnium-0.1.5.tar.gz

---
+++ packages/wqrfnium/wqrfnium-0.1.5.tar.gz/wqrfnium-0.1.5/wqrfnium/test.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from wqrfnium import *
+
+begin_wqrf('./MyElements2.xls')
+driver = webdriver.Chrome()
+driver.get("http://www.baidu.com/")
+time.sleep(2)
+getelement(driver,"seachinput").send_keys('xiaozhu')
+
+
