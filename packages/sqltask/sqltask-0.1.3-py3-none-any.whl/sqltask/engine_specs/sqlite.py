208678f56047ac0c5deb17697f598fa75c3b9265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:00 +0000

    Add package sqltask/0.1.3/sqltask-0.1.3-py3-none-any.whl

---
+++ packages/sqltask/sqltask-0.1.3-py3-none-any.whl/sqltask/engine_specs/sqlite.py
@@ -0,0 +1,8 @@
+from sqltask.engine_specs.base import BaseEngineSpec
+
+
+class SqliteEngineSpec(BaseEngineSpec):
+    engine = 'sqlite'
+    supports_column_comments = False
+    supports_table_comments = False
+    supports_schemas = False
