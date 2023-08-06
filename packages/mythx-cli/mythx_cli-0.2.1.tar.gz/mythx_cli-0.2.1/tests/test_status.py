4b1c7e5b4dfda01ab92c2606598e405c3d8ded73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:31 +0000

    Add package mythx-cli/0.2.1/mythx_cli-0.2.1.tar.gz

---
+++ packages/mythx-cli/mythx_cli-0.2.1.tar.gz/mythx_cli-0.2.1/tests/test_status.py
@@ -0,0 +1,58 @@
+import json
+from unittest.mock import patch
+
+from click.testing import CliRunner
+
+from mythx_cli.cli import cli
+
+from .testdata import STATUS_RESPONSE_OBJ, STATUS_RESPONSE_SIMPLE, STATUS_RESPONSE_TABLE
+
+
+def test_status_tabular():
+    runner = CliRunner()
+    with patch("pythx.Client.status") as status_patch:
+        status_patch.return_value = STATUS_RESPONSE_OBJ
+        result = runner.invoke(cli, ["status", "381eff48-04db-4f81-a417-8394b6614472"])
+        assert result.exit_code == 0
+        assert result.output == STATUS_RESPONSE_TABLE
+
+
+def test_status_json():
+    runner = CliRunner()
+    with patch("pythx.Client.status") as status_patch:
+        status_patch.return_value = STATUS_RESPONSE_OBJ
+        result = runner.invoke(
+            cli, ["--format", "json", "status", "381eff48-04db-4f81-a417-8394b6614472"]
+        )
+        assert result.exit_code == 0
+        assert json.loads(result.output) == STATUS_RESPONSE_OBJ.to_dict()
+
+
+def test_status_json_pretty():
+    runner = CliRunner()
+    with patch("pythx.Client.status") as status_patch:
+        status_patch.return_value = STATUS_RESPONSE_OBJ
+        result = runner.invoke(
+            cli,
+            [
+                "--format",
+                "json-pretty",
+                "status",
+                "381eff48-04db-4f81-a417-8394b6614472",
+            ],
+        )
+        assert result.exit_code == 0
+        assert json.loads(result.output) == STATUS_RESPONSE_OBJ.to_dict()
+
+
+def test_status_simple():
+    runner = CliRunner()
+    with patch("pythx.Client.status") as status_patch:
+        status_patch.return_value = STATUS_RESPONSE_OBJ
+        result = runner.invoke(
+            cli,
+            ["--format", "simple", "status", "381eff48-04db-4f81-a417-8394b6614472"],
+        )
+        assert result.exit_code == 0
+        print(result.output)
+        assert result.output == STATUS_RESPONSE_SIMPLE
