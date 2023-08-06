936331cae7369f11a804942648985ebb402233be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:13 +0000

    Add package springheel/5.0.2/springheel-5.0.2.tar.gz

---
+++ packages/springheel/springheel-5.0.2.tar.gz/springheel-5.0.2/springheel/gettemplatenames.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+########
+##  Springheel - Get HTML template names and paths based on the language
+########
+##  Copyright 2017 garrick. Some rights reserved.
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU Lesser General Public License
+## along with this program. If not, see <http://www.gnu.org/
+
+import os
+
+def getTemplateNames():
+    base = "base-template"
+    characters = "characters-template"
+    archive = "archive-template"
+    index = "index-template"
+    extra = "extras-template"
+
+    patterns=[base,characters,archive,index,extra]
+
+    extension = "html"
+
+    root="templates"
+
+    fulls=[]
+
+    for pattern in patterns:
+        file=".".join([pattern,extension])
+        full=os.path.join(root,file)
+        fulls.append(full)
+
+    templates=tuple(fulls)
+
+    return(templates)
