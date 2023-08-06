b7655cc5495e847d82f39b09ddd33c38d13866ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:28 +0000

    Add package zuper-typing-z5/5.0.8/zuper-typing-z5-5.0.8.tar.gz

---
+++ packages/zuper-typing-z5/zuper-typing-z5-5.0.8.tar.gz/zuper-typing-z5-5.0.8/src/zuper_typing/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '5.0.8'
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    # noinspection PyUnresolvedReferences
+    from dataclasses import dataclass
+
+    # noinspection PyUnresolvedReferences
+    from typing import Generic
+
+    raise Exception()
+else:
+    # noinspection PyUnresolvedReferences
+    from .monkey_patching_typing import my_dataclass as dataclass
+
+    # noinspection PyUnresolvedReferences
+    from .monkey_patching_typing import ZenericFix as Generic
+
+from .debug_print_ import debug_print
