727c65ca4ac82080a53547465f536b0a53b1bf29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:55 +0000

    Add package byprice-scraping-tools/0.0.2/byprice_scraping_tools-0.0.2-py3-none-any.whl

---
+++ packages/byprice-scraping-tools/byprice_scraping_tools-0.0.2-py3-none-any.whl/scraping-tools/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# File name: __init__.py
+# Author: Oswaldo Cruz Simon
+# Email: oswaldo_cs_94@hotmail.com
+# Maintainer: Oswaldo Cruz Simon
+# Date created: 03/10/19
+# Date last modified: 03/10/19
+# Project Name: scraping-tools
+
+from .utils import product_counter
+from .utils import TTL_DAY
+from .utils import TTL_WEEK
+from .utils import transform
