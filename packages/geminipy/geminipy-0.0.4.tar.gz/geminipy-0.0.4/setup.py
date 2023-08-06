758479f1a776b1d407a11e729ef59c73c24e9f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:44 +0000

    Add package geminipy/0.0.4/geminipy-0.0.4.tar.gz

---
+++ packages/geminipy/geminipy-0.0.4.tar.gz/geminipy-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.md') as f:
+  readme = f.read().replace('```', '')
+
+setup(
+    name='geminipy',
+    version='0.0.4',
+    packages=['geminipy'],
+    url='https://github.com/geminipy/geminipy',
+    license='GNU GPL',
+    author='Mike Marzigliano',
+    author_email='marzig76@gmail.com',
+    zip_safe=False,
+    long_description=readme,
+    description='API client for Gemini',
+)
