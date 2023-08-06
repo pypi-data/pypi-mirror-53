a27921c991f62918be5873a7768440502d7d332b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:25 +0000

    Add package Deduce-Ingest/1.1/Deduce-Ingest-1.1.tar.gz

---
+++ packages/Deduce-Ingest/Deduce-Ingest-1.1.tar.gz/Deduce-Ingest-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='Deduce-Ingest',
+      version='1.1',
+      description='Deduce data ingestion',
+      url='http://github.com/deduce-com/python-deduce-ingest',
+      author='Deduce',
+      author_email='support@deduce.com',
+      license='MIT',
+      packages=['deduce'],
+      zip_safe=False)
+
