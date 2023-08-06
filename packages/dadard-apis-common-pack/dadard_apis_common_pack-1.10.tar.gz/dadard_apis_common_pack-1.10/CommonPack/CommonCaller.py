9be5f2c15b7be1d8a00fc146e4b6099a7a36650d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:35 +0000

    Add package dadard-apis-common-pack/1.10/dadard_apis_common_pack-1.10.tar.gz

---
+++ packages/dadard-apis-common-pack/dadard_apis_common_pack-1.10.tar.gz/dadard_apis_common_pack-1.10/CommonPack/CommonCaller.py
@@ -0,0 +1,60 @@
+import json
+
+import requests
+
+BASE_URL = 'http://dadard.fr:7000/api/v1/'
+SUB_CHECK_ENDPOINT = 'sub'
+PROFILE_CHECK_ENDPOINT = 'profile/infos'
+STATUS_OK = 'ok'
+STATUS_KO = 'ko'
+TRUE = 'true'
+FALSE = 'false'
+
+
+def get_auth_header(profile_key):
+    return {
+        "Authorization": profile_key
+    }
+
+
+def check_profile_key(profile_key):
+    headers = get_auth_header(profile_key)
+    url = f"{BASE_URL}{PROFILE_CHECK_ENDPOINT}"
+
+    r = requests.get(url, headers=headers)
+    status = json.loads(r.content.decode())['status']
+    if status == STATUS_OK:
+        return True
+
+    return False
+
+
+def call_sub(profile_key, api_name):
+    headers = get_auth_header(profile_key)
+    params = "api_name={an}".format(an=api_name)
+    url = f"{BASE_URL}{SUB_CHECK_ENDPOINT}?{params}"
+
+    r = requests.get(url, headers=headers)
+    return json.loads(r.content.decode())
+
+
+def check_is_subscribed(profile_key, api_name):
+    response = call_sub(profile_key, api_name)
+    status = response['status']
+    if status == STATUS_OK:
+        is_subscribed = response['content']['is_subscribed']
+        if is_subscribed == TRUE:
+            return True
+
+    return False
+
+
+def check_is_not_subscribed(profile_key, api_name):
+    response = call_sub(profile_key, api_name)
+    status = response['status']
+    if status == STATUS_OK:
+        is_subscribed = response['content']['is_subscribed']
+        if is_subscribed == FALSE:
+            return True
+
+    return False
