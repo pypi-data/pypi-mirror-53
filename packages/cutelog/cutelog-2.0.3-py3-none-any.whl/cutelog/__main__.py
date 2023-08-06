828a376536b165ffe6db62afdf1a08d628062b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:08 +0000

    Add package cutelog/2.0.3/cutelog-2.0.3-py3-none-any.whl

---
+++ packages/cutelog/cutelog-2.0.3-py3-none-any.whl/cutelog/__main__.py
@@ -0,0 +1,33 @@
+import sys
+
+import qtpy
+
+
+if not qtpy.PYQT5 and not qtpy.PYSIDE2:
+    if sys.platform == 'linux':
+        sys.exit("Error: a compatible Qt library couldn't be imported.\n"
+                 "Please install python3-pyqt5 (or just python-pyqt5) from your package manager.")
+    else:  # this technically shouldn't ever happen
+        sys.exit("Error: a compatible Qt library couldn't be imported.\n"
+                 "Please install it by running `pip install pyqt5")
+
+
+def main():
+    import signal
+    from .config import ROOT_LOG
+    from .main_window import MainWindow
+    from .resources import qCleanupResources
+    from qtpy.QtGui import QIcon
+    from qtpy.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(':/cutelog.png'))
+    mw = MainWindow(ROOT_LOG, app)
+    signal.signal(signal.SIGINT, mw.signal_handler)
+
+    sys.exit(app.exec_())
+    qCleanupResources()
+
+
+if __name__ == '__main__':
+    main()
