7b7748dc7f9f24fd1da9272bf0064efee986dab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:48 +0000

    Add package pydens/0.1.1/pydens-0.1.1-py3-none-any.whl

---
+++ packages/pydens/pydens-0.1.1-py3-none-any.whl/pydens/__init__.py
@@ -0,0 +1,11 @@
+""" PyDEns Init-file. """
+
+#pylint: disable=no-name-in-module, import-error
+from .syntax_tree import *
+from .tokens import add_tokens
+from .letters import *
+from .model_tf import TFDeepGalerkin
+from .wrapper import Solver
+from .batchflow.sampler import *
+
+__version__ = '0.1.1'
