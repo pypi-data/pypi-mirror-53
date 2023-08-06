915118d37d61e894c66022b60066cb8b54c08e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:14 +0000

    Add package katpoint/0.9/katpoint-0.9-py2.py3-none-any.whl

---
+++ packages/katpoint/katpoint-0.9-py2.py3-none-any.whl/katpoint/__init__.py
@@ -0,0 +1,77 @@
+################################################################################
+# Copyright (c) 2009-2019, National Research Foundation (Square Kilometre Array)
+#
+# Licensed under the BSD 3-Clause License (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#   https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+"""Module that abstracts pointing and related coordinate transformations.
+
+This module provides a simplified interface to the underlying coordinate
+library, and provides functionality lacking in it. It defines a Target and
+Antenna class, analogous to the Body and Observer classes in PyEphem, and
+provides spherical coordinate transformations and spherical projections.
+
+Currently it only caters for PyEphem, but it could be extended to include ACSM
+and CASA.
+
+"""
+from __future__ import print_function, division, absolute_import
+
+import logging as _logging
+
+from .target import Target, construct_azel_target, construct_radec_target, NonAsciiError
+from .antenna import Antenna
+from .timestamp import Timestamp
+from .flux import FluxDensityModel, FluxError
+from .catalogue import Catalogue, specials
+from .ephem_extra import lightspeed, rad2deg, deg2rad, wrap_angle, is_iterable
+from .conversion import (lla_to_ecef, ecef_to_lla, enu_to_ecef, ecef_to_enu,
+                         azel_to_enu, enu_to_azel, hadec_to_enu, enu_to_xyz)
+from .projection import sphere_to_plane, plane_to_sphere
+from .model import Parameter, Model, BadModelFile
+from .pointing import PointingModel
+from .refraction import RefractionCorrection
+from .delay import DelayModel, DelayCorrection
+
+# Hide submodules in module namespace, to avoid confusion with corresponding class names
+# If the module is reloaded, this will fail - ignore the resulting NameError
+try:
+    _target, _antenna, _timestamp, _flux, _catalogue, _ephem_extra, \
+        _conversion, _projection, _pointing, _refraction, _delay = \
+        target, antenna, timestamp, flux, catalogue, ephem_extra, \
+        conversion, projection, pointing, refraction, delay
+    del target, antenna, timestamp, flux, catalogue, ephem_extra, \
+        conversion, projection, pointing, refraction, delay
+except NameError:
+    pass
+
+
+# Setup library logger and add a print-like handler used when no logging is configured
+class _NoConfigFilter(_logging.Filter):
+    """Filter which only allows event if top-level logging is not configured."""
+    def filter(self, record):
+        return 1 if not _logging.root.handlers else 0
+
+
+_no_config_handler = _logging.StreamHandler()
+_no_config_handler.setFormatter(_logging.Formatter(_logging.BASIC_FORMAT))
+_no_config_handler.addFilter(_NoConfigFilter())
+logger = _logging.getLogger(__name__)
+logger.addHandler(_no_config_handler)
+
+
+# Automatically added by katversion
+__version__ = '0.8'
+
+# Automatically added by katversion
+__version__ = '0.9'
---
+++ packages/katpoint/katpoint-0.9-py2.py3-none-any.whl/katpoint/test/test_delay.py
@@ -0,0 +1,153 @@
+################################################################################
+# Copyright (c) 2009-2019, National Research Foundation (Square Kilometre Array)
+#
+# Licensed under the BSD 3-Clause License (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#   https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+"""Tests for the model module."""
+from __future__ import print_function, division, absolute_import
+
+import json
+import unittest
+try:
+    from StringIO import StringIO  # python2
+except ImportError:
+    from io import StringIO  # python3
+
+import numpy as np
+
+import katpoint
+
+
+class TestDelayModel(unittest.TestCase):
+    """Test antenna delay model."""
+    def test_construct_save_load(self):
+        """Test construction / save / load of delay model."""
+        m = katpoint.DelayModel('1.0, -2.0, -3.0, 4.123, 5.0, 6.0')
+        m.header['date'] = '2014-01-15'
+        # An empty file should lead to a BadModelFile exception
+        cfg_file = StringIO()
+        self.assertRaises(katpoint.BadModelFile, m.fromfile, cfg_file)
+        m.tofile(cfg_file)
+        cfg_str = cfg_file.getvalue()
+        cfg_file.close()
+        # Load the saved config file
+        cfg_file = StringIO(cfg_str)
+        m2 = katpoint.DelayModel()
+        m2.fromfile(cfg_file)
+        self.assertEqual(m, m2, 'Saving delay model to file and loading it again failed')
+        params = m.delay_params
+        m3 = katpoint.DelayModel()
+        m3.fromdelays(params)
+        self.assertEqual(m, m3, 'Converting delay model to delay parameters and loading it again failed')
+        try:
+            self.assertEqual(hash(m), hash(m3), 'Delay model hashes not equal')
+        except TypeError:
+            self.fail('DelayModel object not hashable')
+
+
+class TestDelayCorrection(unittest.TestCase):
+    """Test correlator delay corrections."""
+    def setUp(self):
+        self.target1 = katpoint.construct_azel_target('45:00:00.0', '75:00:00.0')
+        self.target2 = katpoint.Target('Sun, special')
+        self.ant1 = katpoint.Antenna('A1, -31.0, 18.0, 0.0, 12.0, 0.0 0.0 0.0')
+        self.ant2 = katpoint.Antenna('A2, -31.0, 18.0, 0.0, 12.0, 10.0 -10.0 0.0')
+        self.ant3 = katpoint.Antenna('A3, -31.0, 18.0, 0.0, 12.0, 5.0 10.0 3.0')
+        self.ts = katpoint.Timestamp('2013-08-14 08:25')
+        self.delays = katpoint.DelayCorrection([self.ant2, self.ant3], self.ant1, 1.285e9)
+
+    def test_construction(self):
+        """Test construction of DelayCorrection object."""
+        descr = self.delays.description
+        delays2 = katpoint.DelayCorrection(descr)
+        delays_dict = json.loads(descr)
+        delays2_dict = json.loads(delays2.description)
+        self.assertEqual(delays2_dict, delays_dict,
+                         'Objects created through description strings differ')
+        self.assertRaises(ValueError, katpoint.DelayCorrection, [self.ant1, self.ant2], self.ant3)
+        self.assertRaises(ValueError, katpoint.DelayCorrection, [self.ant1, self.ant2])
+        self.assertRaises(ValueError, katpoint.DelayCorrection, '')
+        delays3 = katpoint.DelayCorrection([], self.ant1)
+        self.assertEqual(delays3._params.shape, (0, len(katpoint.DelayModel())),
+                         "Delay correction with no antennas should fail gracefully")
+
+    def test_correction(self):
+        """Test delay correction."""
+        extra_delay = self.delays.extra_delay
+        delay0, phase0 = self.delays.corrections(self.target1, self.ts)
+        delay1, phase1 = self.delays.corrections(self.target1, self.ts, self.ts + 1.0)
+        # This target is special - direction perpendicular to baseline (and stationary)
+        self.assertEqual(delay0['A2h'], extra_delay, 'Delay for ant2h should be zero')
+        self.assertEqual(delay0['A2v'], extra_delay, 'Delay for ant2v should be zero')
+        self.assertEqual(delay1['A2h'][0], extra_delay, 'Delay for ant2h should be zero')
+        self.assertEqual(delay1['A2v'][0], extra_delay, 'Delay for ant2v should be zero')
+        self.assertEqual(delay1['A2h'][1], 0.0, 'Delay rate for ant2h should be zero')
+        self.assertEqual(delay1['A2v'][1], 0.0, 'Delay rate for ant2v should be zero')
+        # Compare to target geometric delay calculations
+        delay0, phase0 = self.delays.corrections(self.target2, self.ts)
+        delay1, phase1 = self.delays.corrections(self.target2, self.ts - 0.5, self.ts + 0.5)
+        tgt_delay, tgt_delay_rate = self.target2.geometric_delay(self.ant2, self.ts, self.ant1)
+        np.testing.assert_almost_equal(delay0['A2h'], extra_delay - tgt_delay, decimal=15)
+        np.testing.assert_almost_equal(delay1['A2h'][1], -tgt_delay_rate, decimal=13)
+        # Test vector version
+        delay2, phase2 = self.delays.corrections(self.target2, (self.ts - 0.5, self.ts + 0.5))
+        np.testing.assert_equal(delay2['A2h'][0], delay1['A2h'])
+        np.testing.assert_equal(phase2['A2h'][0], phase1['A2h'])
+
+    def test_delay_cache(self):
+        """Test delay correction cache limit."""
+        max_size = katpoint.DelayCorrection.CACHE_SIZE
+        for n in range(max_size + 10):
+            delay0, phase0 = self.delays.corrections(self.target1, self.ts + n)
+        self.assertEqual(len(self.delays._cache), max_size, 'Delay cache grew past limit')
+
+    def test_offset(self):
+        """Test target offset."""
+        az, el = self.target1.azel(self.ts, self.ant1)
+        offset = dict(projection_type='SIN')
+        target3 = katpoint.construct_azel_target(az - katpoint.deg2rad(1.0),
+                                                 el - katpoint.deg2rad(1.0))
+        x, y = target3.sphere_to_plane(az, el, self.ts, self.ant1, **offset)
+        offset['x'] = x
+        offset['y'] = y
+        extra_delay = self.delays.extra_delay
+        delay0, phase0 = self.delays.corrections(target3, self.ts, offset=offset)
+        delay1, phase1 = self.delays.corrections(target3, self.ts,
+                                                 self.ts + 1.0, offset)
+        # Conspire to return to special target1
+        self.assertEqual(delay0['A2h'], extra_delay, 'Delay for ant2h should be zero')
+        self.assertEqual(delay0['A2v'], extra_delay, 'Delay for ant2v should be zero')
+        self.assertEqual(delay1['A2h'][0], extra_delay, 'Delay for ant2h should be zero')
+        self.assertEqual(delay1['A2v'][0], extra_delay, 'Delay for ant2v should be zero')
+        self.assertEqual(delay1['A2h'][1], 0.0, 'Delay rate for ant2h should be zero')
+        self.assertEqual(delay1['A2v'][1], 0.0, 'Delay rate for ant2v should be zero')
+        # Now try (ra, dec) coordinate system
+        ra, dec = self.target1.radec(self.ts, self.ant1)
+        offset = dict(projection_type='ARC', coord_system='radec')
+        target4 = katpoint.construct_radec_target(ra - katpoint.deg2rad(1.0),
+                                                  dec - katpoint.deg2rad(1.0))
+        x, y = target4.sphere_to_plane(ra, dec, self.ts, self.ant1, **offset)
+        offset['x'] = x
+        offset['y'] = y
+        extra_delay = self.delays.extra_delay
+        delay0, phase0 = self.delays.corrections(target4, self.ts, offset=offset)
+        delay1, phase1 = self.delays.corrections(target4, self.ts,
+                                                 self.ts + 1.0, offset)
+        # Conspire to return to special target1
+        np.testing.assert_almost_equal(delay0['A2h'], extra_delay, decimal=15)
+        np.testing.assert_almost_equal(delay0['A2v'], extra_delay, decimal=15)
+        np.testing.assert_almost_equal(delay1['A2h'][0], extra_delay, decimal=15)
+        np.testing.assert_almost_equal(delay1['A2v'][0], extra_delay, decimal=15)
+        np.testing.assert_almost_equal(delay1['A2h'][1], 0.0, decimal=15)
+        np.testing.assert_almost_equal(delay1['A2v'][1], 0.0, decimal=15)
