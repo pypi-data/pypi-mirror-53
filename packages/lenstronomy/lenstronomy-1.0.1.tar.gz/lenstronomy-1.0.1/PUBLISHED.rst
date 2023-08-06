d47a4ef8997308c41b1914e591ccaa73b510a727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:52 +0000

    Add package lenstronomy/1.0.1/lenstronomy-1.0.1.tar.gz

---
+++ packages/lenstronomy/lenstronomy-1.0.1.tar.gz/lenstronomy-1.0.1/PUBLISHED.rst
@@ -0,0 +1,37 @@
+===============================
+Published work with lenstronomy
+===============================
+
+In this section you can find the concept papers **lenstronomy** is based on the list of science publications that made
+use of **lenstronomy**. Please let the developers know when you publish a paper that made use of **lenstronomy**.
+We are happy to include your publication in this list.
+
+
+Method papers
+-------------
+
+* Gravitational Lens Modeling with Basis Sets; `Birrer et al. 2015 <http://adsabs.harvard.edu/abs/2015ApJ...813..102B>`_
+    *This is the method paper **lenstronomy** is primary based on. Please cite this paper whenever you publish results with **lenstronomy** by using
+    Shapelet basis sets and/or the PSO and MCMC chain.*
+
+* Line-of-sight effects in strong lensing: putting theory into practice; `Birrer et al. 2017a <http://adsabs.harvard.edu/abs/2017JCAP...04..049B>`_
+    *This paper formulates an effective parameterization of line-of-sight structure for strong gravitational lens modelling and applies this technique to an Einstein ring in the COSMOS field*
+
+* Cosmic Shear with Einstein Rings; `Birrer et al. 2018a <http://adsabs.harvard.edu/abs/2018ApJ...852L..14B>`_
+    *Forecast paper to measure cosmic shear with Einstein ring lenses. The forecast is made based on **lenstronomy** simulations.
+
+* Is every strong lens model unhappy in its own way? Uniform modelling of a sample of 12 quadruply+ imaged quasars; `Shajib et al. 2018 <https://ui.adsabs.harvard.edu//#abs/2018arXiv180709278S/>`_
+    *This work presents a uniform modelling framework to model 12 quadruply lensed quasars in three HST bands.*
+
+
+Science papers
+--------------
+
+* The mass-sheet degeneracy and time-delay cosmography: analysis of the strong lens RXJ1131-1231; `Birrer et al. 2016 <http://adsabs.harvard.edu/abs/2016JCAP...08..020B>`_
+    *This paper performs a cosmographic analysis and applies the Shapelet basis set scaling to marginalize over a major lensing degeneracy.*
+
+* Lensing substructure quantification in RXJ1131-1231: a 2 keV lower bound on dark matter thermal relic mass; `Birrer et al. 2017b <http://adsabs.harvard.edu/abs/2017JCAP...05..037B>`_
+    *This paper quantifies the substructure content of a lens by a sub-clump scanning procedure and the application of Approximate Bayesian Computing.*
+
+* H0LiCOW - IX. Cosmographic analysis of the doubly imaged quasar SDSS 1206+4332 and a new measurement of the Hubble constant; `Birrer et al. 2019 <https://ui.adsabs.harvard.edu/#abs/2018arXiv180901274B/abstract>`_
+    *This paper performs a cosmographic analysis with power-law and composite models and covers a range in complexity in the source reconstruction*
---
+++ packages/lenstronomy/lenstronomy-1.0.1.tar.gz/lenstronomy-1.0.1/lenstronomy/LensModel/lens_model.py
@@ -0,0 +1,247 @@
+__author__ = 'sibirrer'
+
+from lenstronomy.LensModel.single_plane import SinglePlane
+from lenstronomy.LensModel.multi_plane import MultiPlane
+from lenstronomy.Cosmo.lens_cosmo import LensCosmo
+from astropy.cosmology import default_cosmology
+
+
+class LensModel(object):
+    """
+    class to handle an arbitrary list of lens models
+    """
+
+    def __init__(self, lens_model_list, z_lens=None, z_source=None, lens_redshift_list=None, cosmo=None,
+                 multi_plane=False, numerical_alpha_class=None, observed_convention_index=None, z_source_convention=None):
+        """
+
+        :param lens_model_list: list of strings with lens model names
+        :param z_lens: redshift of the deflector (only considered when operating in single plane mode).
+        Is only needed for specific functions that require a cosmology.
+        :param z_source: redshift of the source: Needed in multi_plane option only,
+        not required for the core functionalities in the single plane mode.
+        :param lens_redshift_list: list of deflector redshift (corresponding to the lens model list),
+        only applicable in multi_plane mode.
+        :param cosmo: instance of the astropy cosmology class. If not specified, uses the default cosmology.
+        :param multi_plane: bool, if True, uses multi-plane mode. Default is False.
+        :param numerical_alpha_class: an instance of a custom class for use in NumericalAlpha() lens model
+        (see documentation in Profiles/numerical_alpha)
+        :param observed_convention_index: a list of lens indexes that correspond to observed positions on the sky, not
+        physical positions
+        :param z_source_convention: float, redshift of a source to define the reduced deflection angles of the lens
+        models. If None, 'z_source' is used.
+        """
+        self.lens_model_list = lens_model_list
+        self.z_lens = z_lens
+        if z_source_convention is None:
+            z_source_convention = z_source
+        self.z_source = z_source
+        self._z_source_convention = z_source_convention
+        self.redshift_list = lens_redshift_list
+
+        if cosmo is None:
+            cosmo = default_cosmology.get()
+        self.cosmo = cosmo
+        self.multi_plane = multi_plane
+        if multi_plane is True:
+            if z_source is None:
+                raise ValueError('z_source needs to be set for multi-plane lens modelling.')
+
+            self.lens_model = MultiPlane(z_source, lens_model_list, lens_redshift_list, cosmo=cosmo,
+                                         numerical_alpha_class=numerical_alpha_class,
+                                         observed_convention_index=observed_convention_index,
+                                         z_source_convention=z_source_convention)
+        else:
+            self.lens_model = SinglePlane(lens_model_list, numerical_alpha_class=numerical_alpha_class,
+                                          lens_redshift_list=lens_redshift_list, z_source_convention=z_source_convention)
+        if z_lens is not None and z_source is not None:
+            self._lensCosmo = LensCosmo(z_lens, z_source, cosmo=cosmo)
+
+    def ray_shooting(self, x, y, kwargs, k=None):
+        """
+        maps image to source position (inverse deflection)
+
+        :param x: x-position (preferentially arcsec)
+        :type x: numpy array
+        :param y: y-position (preferentially arcsec)
+        :type y: numpy array
+        :param kwargs: list of keyword arguments of lens model parameters matching the lens model classes
+        :param k: only evaluate the k-th lens model
+        :return: source plane positions corresponding to (x, y) in the image plane
+        """
+        return self.lens_model.ray_shooting(x, y, kwargs, k=k)
+
+    def fermat_potential(self, x_image, y_image, x_source, y_source, kwargs_lens):
+        """
+        fermat potential (negative sign means earlier arrival time)
+
+        :param x_image: image position
+        :param y_image: image position
+        :param x_source: source position
+        :param y_source: source position
+        :param kwargs_lens: list of keyword arguments of lens model parameters matching the lens model classes
+        :return: fermat potential in arcsec**2 without geometry term (second part of Eqn 1 in Suyu et al. 2013) as a list
+        """
+        if hasattr(self.lens_model, 'fermat_potential'):
+            return self.lens_model.fermat_potential(x_image, y_image, x_source, y_source, kwargs_lens)
+        else:
+            raise ValueError("Fermat potential is not defined in multi-plane lensing. Please use single plane lens models.")
+
+    def arrival_time(self, x_image, y_image, kwargs_lens):
+        """
+
+        :param x_image: image position
+        :param y_image: image position
+        :param kwargs_lens: lens model parameter keyword argument list
+        :return: arrival time of image positions in units of days
+        """
+        try:
+            arrival_time = self.lens_model.arrival_time(x_image, y_image, kwargs_lens)
+        except:
+            x_source, y_source = self.lens_model.ray_shooting(x_image, y_image, kwargs_lens)
+            fermat_pot = self.lens_model.fermat_potential(x_image, y_image, x_source, y_source, kwargs_lens)
+            if not hasattr(self, '_lensCosmo'):
+                raise ValueError("LensModel class was not initalized with lens and source redshifts!")
+            arrival_time = self._lensCosmo.time_delay_units(fermat_pot)
+        return arrival_time
+
+    def potential(self, x, y, kwargs, k=None):
+        """
+        lensing potential
+
+        :param x: x-position (preferentially arcsec)
+        :type x: numpy array
+        :param y: y-position (preferentially arcsec)
+        :type y: numpy array
+        :param kwargs: list of keyword arguments of lens model parameters matching the lens model classes
+        :param k: only evaluate the k-th lens model
+        :return: lensing potential in units of arcsec^2
+        """
+        return self.lens_model.potential(x, y, kwargs, k=k)
+
+    def alpha(self, x, y, kwargs, k=None):
+        """
+        deflection angles
+
+        :param x: x-position (preferentially arcsec)
+        :type x: numpy array
+        :param y: y-position (preferentially arcsec)
+        :type y: numpy array
+        :param kwargs: list of keyword arguments of lens model parameters matching the lens model classes
+        :param k: only evaluate the k-th lens model
+        :return: deflection angles in units of arcsec
+        """
+        return self.lens_model.alpha(x, y, kwargs, k=k)
+
+    def hessian(self, x, y, kwargs, k=None):
+        """
+        hessian matrix
+
+        :param x: x-position (preferentially arcsec)
+        :type x: numpy array
+        :param y: y-position (preferentially arcsec)
+        :type y: numpy array
+        :param kwargs: list of keyword arguments of lens model parameters matching the lens model classes
+        :param k: only evaluate the k-th lens model
+        :return: f_xx, f_xy, f_yy components
+        """
+        return self.lens_model.hessian(x, y, kwargs, k=k)
+
+    def kappa(self, x, y, kwargs, k=None):
+        """
+        lensing convergence k = 1/2 laplacian(phi)
+
+        :param x: x-position (preferentially arcsec)
+        :type x: numpy array
+        :param y: y-position (preferentially arcsec)
+        :type y: numpy array
+        :param kwargs: list of keyword arguments of lens model parameters matching the lens model classes
+        :param k: only evaluate the k-th lens model
+        :return: lensing convergence
+        """
+
+        f_xx, f_xy, f_yx, f_yy = self.hessian(x, y, kwargs, k=k)
+        kappa = 1./2 * (f_xx + f_yy)
+        return kappa
+
+    def gamma(self, x, y, kwargs, k=None):
+        """
+        shear computation
+        g1 = 1/2(d^2phi/dx^2 - d^2phi/dy^2)
+        g2 = d^2phi/dxdy
+
+        :param x: x-position (preferentially arcsec)
+        :type x: numpy array
+        :param y: y-position (preferentially arcsec)
+        :type y: numpy array
+        :param kwargs: list of keyword arguments of lens model parameters matching the lens model classes
+        :param k: only evaluate the k-th lens model
+        :return: gamma1, gamma2
+        """
+
+        f_xx, f_xy, f_yx, f_yy = self.hessian(x, y, kwargs, k=k)
+        gamma1 = 1./2 * (f_xx - f_yy)
+        gamma2 = f_xy
+        return gamma1, gamma2
+
+    def magnification(self, x, y, kwargs, k=None):
+        """
+        magnification
+        mag = 1/det(A)
+        A = 1 - d^2phi/d_ij
+
+        :param x: x-position (preferentially arcsec)
+        :type x: numpy array
+        :param y: y-position (preferentially arcsec)
+        :type y: numpy array
+        :param kwargs: list of keyword arguments of lens model parameters matching the lens model classes
+        :param k: only evaluate the k-th lens model
+        :return: magnification
+        """
+
+        f_xx, f_xy, f_yx, f_yy = self.hessian(x, y, kwargs, k=k)
+        det_A = (1 - f_xx) * (1 - f_yy) - f_xy*f_yx
+        return 1./det_A  # attention, if dividing by zero
+
+    def flexion(self, x, y, kwargs, diff=0.000001):
+        """
+        third derivatives (flexion)
+
+        :param x: x-position (preferentially arcsec)
+        :type x: numpy array
+        :param y: y-position (preferentially arcsec)
+        :type y: numpy array
+        :param kwargs: list of keyword arguments of lens model parameters matching the lens model classes
+        :param diff: numerical differential length of Hessian
+        :return: f_xxx, f_xxy, f_xyy, f_yyy
+        """
+        f_xx, f_xy, f_yx, f_yy = self.hessian(x, y, kwargs)
+
+        f_xx_dx, f_xy_dx, f_yx_dx, f_yy_dx = self.hessian(x + diff, y, kwargs)
+        f_xx_dy, f_xy_dy, f_yx_dy, f_yy_dy = self.hessian(x, y + diff, kwargs)
+
+        f_xxx = (f_xx_dx - f_xx) / diff
+        f_xxy = (f_xx_dy - f_xx) / diff
+        f_xyy = (f_xy_dy - f_xy) / diff
+        f_yyy = (f_yy_dy - f_yy) / diff
+        return f_xxx, f_xxy, f_xyy, f_yyy
+
+    def set_static(self, kwargs):
+        """
+        set this instance to a static lens model. This can improve the speed in evaluating lensing quantities at
+        different positions but must not be used with different lens model parameters!
+
+        :param kwargs: lens model keyword argument list
+        :return: kwargs_updated (in case of image position convention in multiplane lensing this is changed)
+        """
+        return self.lens_model.set_static(kwargs)
+
+    def set_dynamic(self):
+        """
+        deletes cache for static setting and makes sure the observed convention in the position of lensing profiles in
+        the multiplane setting is enabled. Dynamic is the default setting of this class enabling an accurate computation
+        of lensing quantities with different parameters in the lensing profiles.
+
+        :return: None
+        """
+        self.lens_model.set_dynamic()
