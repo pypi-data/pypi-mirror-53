e8ce30b1ebf46da22d082297a37f4b7dcf561bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:47 +0000

    Add package akshare/0.1.13/akshare-0.1.13.tar.gz

---
+++ packages/akshare/akshare-0.1.13.tar.gz/akshare-0.1.13/akshare/__init__.py
@@ -0,0 +1,64 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Author: Albert King
+date: 2019/9/30 13:58
+contact: jindaxiang@163.com
+desc: 初始化文件, 主要用于在导入 package 的时候导入需要运行的函数
+"""
+
+"""
+版本改动记录:
+0.1.13
+更新所有基于 fushare 的接口
+"""
+
+__version__ = '0.1.13'
+__author__ = 'Albert King'
+
+"""
+奇货可查模块
+"""
+from akshare.qhkc import get_qhkc_data
+
+"""
+大宗商品现货价格及基差
+"""
+from akshare.basis import (get_spot_price_daily,
+                           get_spot_price)
+
+"""
+期货持仓成交排名数据
+"""
+from akshare.cot import (get_rank_sum_daily,
+                         get_rank_sum,
+                         get_shfe_rank_table,
+                         get_czce_rank_table,
+                         get_dce_rank_table,
+                         get_cffex_rank_table)
+
+"""
+大宗商品仓单数据
+"""
+from akshare.receipt import (get_receipt)
+
+"""
+大宗商品展期收益率数据
+"""
+from akshare.roll_yield import (get_roll_yield_bar, get_roll_yield)
+
+"""
+交易所行情数据日线
+"""
+from akshare.daily_bar import (get_cffex_daily,
+                               get_czce_daily,
+                               get_shfe_v_wap,
+                               get_shfe_daily,
+                               get_dce_daily,
+                               get_futures_daily)
+
+"""
+发邮件模块
+"""
+from akshare.send_email import send_email
+
