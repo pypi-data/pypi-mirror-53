bcd59115b6bad50b14bcb466955acf64244cc935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:12 +0000

    Add package GH-IO-stubs/7.0.19277/GH_IO-stubs-7.0.19277.tar.gz

---
+++ packages/GH-IO-stubs/GH_IO-stubs-7.0.19277.tar.gz/GH_IO-stubs-7.0.19277/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import os
+
+long_description = '''
+# GH_IO-stubs
+Stubs for GH_IO
+'''
+
+
+def find_stubs(package):
+    stubs = []
+    for root, dirs, files in os.walk(package):
+        for f in files:
+            stubs.append(os.path.relpath(os.path.join(root, f), package))
+    return {package: stubs}
+
+
+setuptools.setup(
+    name="GH_IO-stubs",
+    version="7.0.19277",
+    package_data=find_stubs("GH_IO-stubs"),
+    packages=['GH_IO-stubs'],
+    author="Robert McNeel & Associates",
+    author_email="steve@mcneel.com",
+    description="Stubs for GH_IO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcneel/pythonstubs",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python"
+    ],
+)
