683a95187fb252066135e0a73194aaf39ad711d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:09 +0000

    Add package fhirstore/0.0.2/fhirstore-0.0.2.tar.gz

---
+++ packages/fhirstore/fhirstore-0.0.2.tar.gz/fhirstore-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fhirstore",
+    version="0.0.2",
+    author="Arkhn",
+    author_email="contact@arkhn.org",
+    description="Manipulating FHIR data leveraging MongoDB as storage layer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arkhn/pyfhirstore/",
+    packages=setuptools.find_packages(),
+    # include_package_data=True,
+    install_requires=[
+        'pymongo==3.9.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
