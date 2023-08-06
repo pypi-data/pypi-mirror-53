51c498f0fc6041102ad1efdcc2d726b76a3280ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:05 +0000

    Add package pyven/1/pyven-1.tar.gz

---
+++ packages/pyven/pyven-1.tar.gz/pyven-1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+        name = 'pyven',
+        version = '1',
+        install_requires = ['aridity'],
+        packages = setuptools.find_packages(),
+        package_data = {'': ['*.pxd', '*.pyx', '*.pyxbld', '*.arid']},
+        py_modules = ['gclean', 'tasks', 'release', 'runtests', 'travis_ci', 'pyven', 'toplevel', 'tests'],
+        scripts = ['gclean.py', 'tasks.py', 'pyven', 'release.py', 'runtests.py', 'travis_ci.py', 'pyven.py', 'toplevel.py', 'tests', 'foreignsyms'])
