3489df71416123890265a67055db361908eed1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:27 +0000

    Add package radontestprint/2.0/radontestprint-2.0.tar.gz

---
+++ packages/radontestprint/radontestprint-2.0.tar.gz/radontestprint-2.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='radontestprint',
+      version='2.0',
+      py_modules=['radontestprint'],
+      author       ='radonma',
+      author_email ='rnmar@163.com',
+      description  ='test pypi function',
+      )
