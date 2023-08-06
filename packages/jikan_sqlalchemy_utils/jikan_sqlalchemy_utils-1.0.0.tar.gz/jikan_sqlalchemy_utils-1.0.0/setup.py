0823f34a8db6e37a91ff76d0dd7e3e21c882dea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:20 +0000

    Add package jikan_sqlalchemy_utils/1.0.0/jikan_sqlalchemy_utils-1.0.0.tar.gz

---
+++ packages/jikan_sqlalchemy_utils/jikan_sqlalchemy_utils-1.0.0.tar.gz/jikan_sqlalchemy_utils-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+pkg = "jikan_sqlalchemy_utils"
+ver = '1.0.0'
+setup(
+    name             = pkg,
+    version          = ver,
+    description      = "jikan sqlalchemy utilities",
+    author           = "jikan@cock.li",
+    author_email     = "jikan@cock.li",
+    license          = "LGPLv3",
+    url              = "https://hydra.ecd.space/jikan/jikan_sqlalchemy_utils/",
+    packages         = find_packages(),
+    install_requires = ['sqlalchemy>=1'],
+    classifiers      = ["Programming Language :: Python :: 3 :: Only"])
