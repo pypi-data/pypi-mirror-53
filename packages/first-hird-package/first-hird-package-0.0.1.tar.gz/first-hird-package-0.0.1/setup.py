e6ee857c9b97b96879b8f70275c12505ad901dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:58 +0000

    Add package first-hird-package/0.0.1/first-hird-package-0.0.1.tar.gz

---
+++ packages/first-hird-package/first-hird-package-0.0.1.tar.gz/first-hird-package-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='first-hird-package',
+    version='0.0.1',
+    packages=find_packages(),
+    author='xieyanke',
+    author_email='coderinfo@163.com',
+    license='Apache',
+    url='http://pycn.me'
+)
