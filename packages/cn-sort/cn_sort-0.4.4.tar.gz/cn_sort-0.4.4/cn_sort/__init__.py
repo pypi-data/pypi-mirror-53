ba843d39e747f7d86051f4e52bf579ed7c306451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:35 +0000

    Add package cn-sort/0.4.4/cn_sort-0.4.4.tar.gz

---
+++ packages/cn-sort/cn_sort-0.4.4.tar.gz/cn_sort-0.4.4/cn_sort/__init__.py
@@ -0,0 +1,11 @@
+from cn_sort import process_cn_word
+from cn_sort import decorator
+from cn_sort import chinese_words_dao
+
+__all__ = [
+    "process_cn_word",
+    "decorator",
+    "chinese_words_dao"
+]
+
+name="cn_sort"
