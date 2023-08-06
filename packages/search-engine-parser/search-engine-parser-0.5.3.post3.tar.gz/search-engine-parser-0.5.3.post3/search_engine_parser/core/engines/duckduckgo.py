84b62c2fd06d1e9de6214728312ad5834da65c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:03 +0000

    Add package search-engine-parser/0.5.3.post3/search-engine-parser-0.5.3.post3.tar.gz

---
+++ packages/search-engine-parser/search-engine-parser-0.5.3.post3.tar.gz/search-engine-parser-0.5.3.post3/search_engine_parser/core/engines/duckduckgo.py
@@ -0,0 +1,52 @@
+"""@desc
+		Parser for DuckDuckGo search results
+"""
+from search_engine_parser.core.base import BaseSearch
+
+
+class DuckDuckGoSearch(BaseSearch):
+    """
+    Searches DuckDuckGo for string
+    """
+    name = "DuckDuckGo"
+    base_url = "https://www.duckduckgo.com"
+    search_url = "https://www.duckduckgo.com/html/?q={query}"
+    summary = "\tHas a number of advantages over the other search engines. \n\tIt has a clean "\
+        "interface, it does not track users, it is not fully loaded with ads and has a number "\
+        "of very nice features (only one page of results, you can search directly other web "\
+        "sites etc).\n\tAccording to DuckDuckGo traffic stats [December, 2018], they are "\
+        "currently serving more than 30 million searches per day."
+
+    def parse_soup(self, soup):
+        """
+        Parses DuckDuckGo Search Soup for a query results
+        """
+        # find all div tags
+        return soup.find_all('div', class_='result')
+
+    def parse_single_result(self, single_result):
+        """
+        Parses the source code to return
+
+        :param single_result: single result found in <div id="r1-{id}">
+        :type single_result: `bs4.element.ResultSet`
+        :return: parsed title, link and description of single result
+        :rtype: dict
+        """
+        h2 = single_result.find('h2', class_="result__title") #pylint: disable=invalid-name
+        link_tag = single_result.find('a', class_="result__url")
+        desc = single_result.find(class_='result__snippet')
+
+        # Get the text and link
+        title = h2.text.strip()
+
+        # raw link is of format "/url?q=REAL-LINK&sa=..."
+        link = self.base_url + link_tag.get('href')
+
+        desc = desc.text
+        rdict = {
+            "titles": title,
+            "links": link,
+            "descriptions": desc,
+        }
+        return rdict
---
+++ packages/search-engine-parser/search-engine-parser-0.5.3.post3.tar.gz/search-engine-parser-0.5.3.post3/search_engine_parser/tests/test_search.py
@@ -0,0 +1,101 @@
+"""@desc
+	Tests or individual search engines
+"""
+
+from search_engine_parser.core import (
+    YahooSearch,
+    GoogleSearch,
+    BingSearch,
+    DuckDuckGoSearch,
+    AolSearch,
+    YandexSearch,
+    StackOverflowSearch,
+    BaiduSearch,
+    GitHubSearch,
+    YouTubeSearch,
+    MyAnimeListSearch,
+    AskSearch)
+from .base import EngineTestBase, EngineTests
+
+
+class YahooEngineTest(EngineTestBase, EngineTests):
+    engine_class = YahooSearch
+
+
+class GoogleEngineTest(EngineTestBase, EngineTests):
+    engine_class = GoogleSearch
+
+    def test_returned_results(self):
+        self.assertTrue(len(self.results['titles']) >= 9)
+        self.assertTrue(len(self.results['links']) >= 9)
+        self.assertTrue(len(self.results['descriptions']) >= 9)
+
+
+class BingEngineTest(EngineTestBase, EngineTests):
+    engine_class = BingSearch
+
+
+class AolSearchTest(EngineTestBase, EngineTests):
+    engine_class = AolSearch
+
+    def test_returned_results(self):
+        self.assertTrue(len(self.results['titles']) >= 9)
+        self.assertTrue(len(self.results['links']) >= 9)
+        self.assertTrue(len(self.results['descriptions']) >= 9)
+
+
+class DuckDuckGoSearchTest(EngineTestBase, EngineTests):
+    engine_class = DuckDuckGoSearch
+
+    def test_returned_results(self):
+        self.assertTrue(len(self.results['titles']) >= 10)
+        self.assertTrue(len(self.results['links']) >= 10)
+        self.assertTrue(len(self.results['descriptions']) >= 10)
+
+
+class YandexSearchTest(EngineTestBase, EngineTests):
+    engine_class = YandexSearch
+
+
+class StackOverFlowSearchTest(EngineTestBase, EngineTests):
+    engine_class = StackOverflowSearch
+
+    def test_returned_results(self):
+        self.assertTrue(len(self.results['titles']) >= 15)
+        self.assertTrue(len(self.results['links']) >= 15)
+        self.assertTrue(len(self.results['descriptions']) >= 15)
+
+
+class BaiduSearchTest(EngineTestBase, EngineTests):
+    engine_class = BaiduSearch
+
+    def test_returned_results(self):
+        self.assertTrue(len(self.results['titles']) >= 8)
+        self.assertTrue(len(self.results['links']) >= 8)
+        self.assertTrue(len(self.results['descriptions']) >= 8)
+
+
+class GithubSearchTest(EngineTestBase, EngineTests):
+    engine_class = GitHubSearch
+
+
+class YoutubeSearchTest(EngineTestBase, EngineTests):
+    engine_class = YouTubeSearch
+
+    def test_returned_results(self):
+        self.assertTrue(len(self.results['titles']) >= 10)
+        self.assertTrue(len(self.results['links']) >= 10)
+        self.assertTrue(len(self.results['descriptions']) >= 10)
+
+
+class MyAnimeListSearchTest(EngineTestBase, EngineTests):
+    engine_class = MyAnimeListSearch
+
+
+class AskEngineSearchTest(EngineTestBase, EngineTests):
+    engine_class = AskSearch
+
+    def test_returned_results(self):
+        self.assertTrue(len(self.results['titles']) >= 8)
+        self.assertTrue(len(self.results['links']) >= 8)
+        self.assertTrue(len(self.results['descriptions']) >= 8)
