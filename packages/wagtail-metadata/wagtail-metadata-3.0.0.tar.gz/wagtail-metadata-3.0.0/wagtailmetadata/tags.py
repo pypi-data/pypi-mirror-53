c5eea735580a5358b224b1b0f53419fe79b8faf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:03 +0000

    Add package wagtail-metadata/3.0.0/wagtail-metadata-3.0.0.tar.gz

---
+++ packages/wagtail-metadata/wagtail-metadata-3.0.0.tar.gz/wagtail-metadata-3.0.0/wagtailmetadata/tags.py
@@ -0,0 +1,31 @@
+from django.template import TemplateSyntaxError
+from django.template.loader import render_to_string
+
+
+def get_meta_image_url(request, image):
+    """
+    Resize an image for metadata tags, and return an absolute URL to it.
+    """
+    rendition = image.get_rendition(filter='original')
+    return request.build_absolute_uri(rendition.url)
+
+
+def meta_tags(request, model):
+    if not request:
+        raise TemplateSyntaxError(
+            "'meta_tags' missing request from context")
+    if not model:
+        raise TemplateSyntaxError(
+            "'meta_tags' tag is missing a model or object")
+    context = {
+        'site_name': request.site.site_name,
+        'object': model,
+    }
+
+    meta_image = model.get_meta_image()
+    if meta_image:
+        meta_image = get_meta_image_url(request, meta_image)
+    context['meta_image'] = meta_image
+
+    return render_to_string('wagtailmetadata/parts/tags.html',
+                            context, request=request)
