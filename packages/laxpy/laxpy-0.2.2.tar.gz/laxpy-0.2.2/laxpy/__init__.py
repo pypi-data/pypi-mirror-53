62d222eca194b6423e29b853f941f0b22507e1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:00 +0000

    Add package laxpy/0.2.2/laxpy-0.2.2.tar.gz

---
+++ packages/laxpy/laxpy-0.2.2.tar.gz/laxpy-0.2.2/laxpy/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+__version__ = '0.2.2'
+
+from laxpy import file
+from laxpy import tree
+from laxpy.query import IndexedLAS
---
+++ packages/laxpy/laxpy-0.2.2.tar.gz/laxpy-0.2.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'laxpy',
+    version = '0.2.2',
+    description = 'A Python API for .lax files',
+    license = 'MIT',
+    author = 'Bryce Frank',
+    author_email = 'bfrank70@gmail.com',
+    url = 'https://github.com/brycefrank/laxpy',
+    packages = ['laxpy', 'laxpytest'],
+    install_requires = [
+            'laspy',
+            'shapely',
+            'numpy',
+            'numba'
+        ],
+    data = [('laxpytest/data'), ['test.las', 'test.lax']],
+)
