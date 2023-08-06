bcdff9cd48393f178e89d944c4f1599e4ac46d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:52 +0000

    Add package filterpicker/1.0.1/filterpicker-1.0.1.tar.gz

---
+++ packages/filterpicker/filterpicker-1.0.1.tar.gz/filterpicker-1.0.1/filterpicker/__init__.py
@@ -0,0 +1,4 @@
+__name__ = "filterpicker"
+__author__ = "Matteo Bagagli"
+__version__ = "1.0.1"
+__date__ = "06/2019"
---
+++ packages/filterpicker/filterpicker-1.0.1.tar.gz/filterpicker-1.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required_list = f.read().splitlines()
+
+setuptools.setup(
+    name="filterpicker",
+    version="1.0.1",
+    author="Matteo Bagagli",
+    author_email="matteo.bagagli@gmail.com",
+    description="Python implementation of the A.Lomax Filter Picker",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.ethz.ch/mbagagli/filterpicker",
+    python_requires='>=3.6',
+    install_requires=required_list,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Intended Audience :: Science/Research",
+    ],
+)
