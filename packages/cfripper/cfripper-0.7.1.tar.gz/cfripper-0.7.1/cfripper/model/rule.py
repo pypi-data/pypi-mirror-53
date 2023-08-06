fa5b6dc7f60cca0cbdafb240457ecb74731250c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:50 +0000

    Add package cfripper/0.7.1/cfripper-0.7.1.tar.gz

---
+++ packages/cfripper/cfripper-0.7.1.tar.gz/cfripper-0.7.1/cfripper/model/rule.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from cfripper.config.config import Config
+from cfripper.model.enums import RuleMode, RuleRisk, RuleGranularity
+from cfripper.model.result import Result
+
+
+class Rule(ABC):
+    RULE_MODE = RuleMode.BLOCKING
+    RISK_VALUE = RuleRisk.MEDIUM
+    GRANULARITY = RuleGranularity.STACK
+
+    def __init__(self, config: Optional[Config], result: Result):
+        self._config = config if config else Config()
+        self._result = result
+
+    @abstractmethod
+    def invoke(self, resources, parameters):
+        pass
+
+    def add_failure(self, rule: str, reason: str, granularity=None):
+
+        if granularity is None:
+            granularity = self.GRANULARITY
+
+        self._result.add_failure(
+            rule=rule,
+            reason=reason,
+            rule_mode=self.RULE_MODE,
+            risk_value=self.RISK_VALUE,
+            granularity=granularity
+        )
+
+    def add_warning(self, warning):
+        self._result.add_warning(warning)
