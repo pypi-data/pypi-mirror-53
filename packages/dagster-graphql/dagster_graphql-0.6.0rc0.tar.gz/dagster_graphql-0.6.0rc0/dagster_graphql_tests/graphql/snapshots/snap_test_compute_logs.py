21e0463b7d90c575305e3ccbdd348fa6dd7a8fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:03 +0000

    Add package dagster-graphql/0.6.0rc0/dagster_graphql-0.6.0rc0.tar.gz

---
+++ packages/dagster-graphql/dagster_graphql-0.6.0rc0.tar.gz/dagster_graphql-0.6.0rc0/dagster_graphql_tests/graphql/snapshots/snap_test_compute_logs.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+snapshots = Snapshot()
+
+snapshots['test_get_compute_logs_over_graphql 1'] = {
+    'stdout': {
+        'data': '''HELLO WORLD
+'''
+    }
+}
+
+snapshots['test_compute_logs_subscription_graphql 1'] = [
+    {
+        'computeLogs': {
+            'stdout': {
+                'data': '''HELLO WORLD
+'''
+            }
+        }
+    }
+]
