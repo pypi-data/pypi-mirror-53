22d2ad0183430755fc6f0441f0abdd896302b64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:34 +0000

    Add package slack-entities/0.0.30/slack_entities-0.0.30.tar.gz

---
+++ packages/slack-entities/slack_entities-0.0.30.tar.gz/slack_entities-0.0.30/slack_entities/client/client.py
@@ -0,0 +1,43 @@
+import logging
+import os
+
+from slack import WebClient
+
+logger = logging.Logger("slack_client", level=15)
+logger.setLevel(15)
+
+
+class SlackApiError(Exception):
+    pass
+
+
+class SlackNotFoundError(Exception):
+    pass
+
+
+class NoSlackTokenError(Exception):
+    pass
+
+
+class SlackClientWithLogging(WebClient):
+    def api_call(self, api_method, **kwargs):
+        logger.info(f"Fetching `{api_method}` with params `{kwargs}`")
+        response = super().api_call(api_method, json=kwargs)
+        logger.info(f"Response:\n {response}")
+        return response
+
+
+def get_client(token=None):
+    # Trying to get token from environment
+    if not token:
+        token = os.environ.get("SLACK_TOKEN")
+
+    # If there is still no token - it's bad.
+    # Need to raise appropriate error
+    if not token:
+        raise NoSlackTokenError(
+            """You need to set Slack token. You can do this in the following ways:
+            1) set up a 'SLACK_TOKEN' environment variable
+            2) directly pass Slack token into get_client() function""")
+
+    return SlackClientWithLogging(token=token)
---
+++ packages/slack-entities/slack_entities-0.0.30.tar.gz/slack_entities-0.0.30/slack_entities/tests/entities/test_action.py
@@ -0,0 +1,154 @@
+"""
+We need new tests framework
+"""
+from unittest import TestCase
+from slack_entities.entities.action import action_from_webhook, Action, ButtonAction, SelectAction, MessageAction, \
+    DialogSubmissionAction
+
+
+def return_original_message():
+    return {
+        "text": "some text :)",
+        "username": "BOT_USERNAME",
+        "bot_id": "BOT_ID",
+        "attachments": [{
+            "callback_id": "first_callback",
+            "fallback": "Try again later :(",
+            "text": "text for action:",
+            "id": 1,
+            "color": "3AA3E3",
+            "actions": [{
+                "id": "1",
+                "name": "ACTION_NAME",
+                "text": "placeholder",
+                "type": "select",
+                "data_source": "static",
+                "options": [{
+                    "text": "1",
+                    "value": "1"
+                }]
+            }]
+        }],
+        "type": "message",
+        "subtype": "bot_message"
+    }
+
+
+WEBHOOK_TEMPLATE = {
+    "type": "interactive_message",
+    "callback_id": "test_callback_id",
+    "team": {
+        "id": "TEAM_ID",
+        "domain": "TEAM_DOMAIN"
+    },
+    "channel": {
+        "id": "CHANNEL_ID",
+        "name": "CHANNEL_NAME"
+    },
+    "user": {
+        "id": "USER_ID",
+        "name": "USER_NAME"
+    },
+    "action_ts": "1539762867.466414",
+    "token": "SLACK_TOKEN",
+    "response_url": 'test_url'
+}
+WEBHOOK_SELECT = dict(WEBHOOK_TEMPLATE, **{
+    'actions': [{
+        "name": "emails_list",
+        "type": "select",
+        "selected_options": [{
+            "value": "5"
+        }]
+    }],
+    "original_message": return_original_message(),
+    'response_url': 'test_url',
+    'trigger_id': 'test_trigger_id'
+})
+WEBHOOK_BUTTON = dict(WEBHOOK_TEMPLATE, **{
+    'actions': [{
+        "name": "ok",
+        "type": "button",
+        "value": "ok"
+    }],
+    "original_message": return_original_message(),
+    'response_url': 'test_url',
+    'trigger_id': 'test_trigger_id'
+})
+WEBHOOK_ACTION = dict(WEBHOOK_TEMPLATE, **{
+    'actions': [{
+        'type': 'something else'
+    }],
+    'original_message': return_original_message(),
+    'response_url': 'test_url'
+})
+WEBHOOK_MESSAGE_ACTION = dict(WEBHOOK_TEMPLATE, **{
+    'type': 'message_action',
+    'callback_id': 'pivotal',
+    'trigger_id': '472869436786.47444502659.884405da2a25311a4bcae9ddfaf23c07',
+    'message': {
+            'text': "some message",
+            'username': 'USERNAME',
+            'user': 'USER_ID',
+            'type': 'message',
+            'ts': '1541507772.003300'
+        },
+    'response_url': 'test_url'
+})
+WEBHOOK_DIALOG_SUBMISSION_ACTION = dict(WEBHOOK_TEMPLATE, **{
+    'type': 'dialog_submission',
+    'callback_id': 'pivotal-ticket',
+    'submission': {
+            'field_1': 'val 1',
+            'field_2': 'val 2'
+        },
+    'response_url': 'test_url'
+})
+
+
+class ActionTestCase(TestCase):
+    def action_from_webhook_ButtonAction(self):
+        action = action_from_webhook(WEBHOOK_BUTTON)
+
+        self.assertTrue(action.trigger_id == 'test_trigger_id')
+        self.assertTrue(action.response_url == 'test_url')
+        self.assertTrue(type(action) == ButtonAction)
+
+    def action_from_webhook_SelectAction(self):
+        action = action_from_webhook(WEBHOOK_SELECT)
+
+        self.assertTrue(action.trigger_id == 'test_trigger_id')
+        self.assertTrue(action.response_url == 'test_url')
+        self.assertTrue(type(action) == SelectAction)
+
+    def action_from_webhook_Action(self):
+        action = action_from_webhook(WEBHOOK_ACTION)
+
+        self.assertTrue(action.callback_id == 'test_callback_id')
+        self.assertTrue(type(action) == Action)
+
+    def action_from_webhook_MessageAction(self):
+        action = action_from_webhook(WEBHOOK_MESSAGE_ACTION)
+
+        self.assertTrue(action.callback_id == 'pivotal')
+        self.assertTrue(action.trigger_id == '472869436786.47444502659.884405da2a25311a4bcae9ddfaf23c07')
+        self.assertTrue(action.response_url == 'test_url')
+        self.assertTrue(type(action) == MessageAction)
+
+    def action_from_webhook_DialogSubmissionAction(self):
+        action = action_from_webhook(WEBHOOK_DIALOG_SUBMISSION_ACTION)
+
+        self.assertTrue(action.callback_id == 'pivotal-ticket')
+        self.assertTrue(action.response_url == 'test_url')
+        self.assertTrue(action.submission == {
+            'field_1': 'val 1',
+            'field_2': 'val 2'
+        })
+        self.assertTrue(type(action) == DialogSubmissionAction)
+
+    def test_action_from_webhook(self):
+        self.action_from_webhook_ButtonAction()
+        self.action_from_webhook_SelectAction()
+        self.action_from_webhook_Action()
+        self.action_from_webhook_MessageAction()
+        self.action_from_webhook_DialogSubmissionAction()
