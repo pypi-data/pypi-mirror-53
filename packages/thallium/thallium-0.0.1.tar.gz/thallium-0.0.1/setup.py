08bc03850759e75f7406eb403d3ef07eea7c6122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:20 +0000

    Add package thallium/0.0.1/thallium-0.0.1.tar.gz

---
+++ packages/thallium/thallium-0.0.1.tar.gz/thallium-0.0.1/setup.py
@@ -0,0 +1,17 @@
+	# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 alvations
+# URL:
+# For license information, see LICENSE.md
+
+from distutils.core import setup
+
+setup(
+    name='thallium',
+    version='0.0.1',
+    packages=['thallium'],
+    url = 'https://github.com/alvations/thallium',
+    description='Lazy python recipes with batteries',
+    license="MIT",
+    install_requires = ['beautifulsoup4', 'requests'],
+)
