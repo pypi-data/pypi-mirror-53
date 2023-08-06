da762a83c668b79e5d96365616301fa68444605a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:45 +0000

    Add package packstack/12.0.2/packstack-12.0.2.tar.gz

---
+++ packages/packstack/packstack-12.0.2.tar.gz/packstack-12.0.2/packstack/puppet/modules/packstack/manifests/nova/vncproxy.pp
@@ -0,0 +1,23 @@
+class packstack::nova::vncproxy ()
+{
+    $vnc_bind_host = hiera('CONFIG_IP_VERSION') ? {
+      'ipv6'  => '::0',
+      default => '0.0.0.0',
+      # TO-DO(mmagr): Add IPv6 support when hostnames are used
+    }
+
+    class { '::nova::vncproxy':
+      enabled => true,
+      host    => $vnc_bind_host,
+    }
+
+    class { '::nova::consoleauth':
+      enabled => true,
+    }
+
+    firewall { '001 novncproxy incoming':
+      proto  => 'tcp',
+      dport  => ['6080'],
+      action => 'accept',
+    }
+}
