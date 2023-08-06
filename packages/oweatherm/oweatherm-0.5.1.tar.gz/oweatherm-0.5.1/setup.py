a668bb78f6166ae29b45a61e2a0f3f5f320d9ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:43 +0000

    Add package oweatherm/0.5.1/oweatherm-0.5.1.tar.gz

---
+++ packages/oweatherm/oweatherm-0.5.1.tar.gz/oweatherm-0.5.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from io import open
+from setuptools import setup
+
+
+
+version = '0.5.1'
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='oweatherm',
+    version=version,
+
+    author='nedogimov',
+    author_email='mrfoxit01@gmail.com',
+
+    description=(
+        u'Weather from OpenWeatherMap'
+    ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    license='Apache License, Version 2.0',
+
+    packages=['weather'],
+    install_requires=['requests'],
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
---
+++ packages/oweatherm/oweatherm-0.5.1.tar.gz/oweatherm-0.5.1/weather/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import requests
+owmkey = "fe198ba65970ed3877578f728f33e0f9"
+from .weather import Weather
+
+
+__author__ = 'nedogimov'
+__version__ = '0.1'
+__email__ = 'mrfoxit01@gmail.com'
