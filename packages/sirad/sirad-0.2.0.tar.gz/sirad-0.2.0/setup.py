d894fe62d218d489397cb34b7a5c02c5793028fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:06 +0000

    Add package sirad/0.2.0/sirad-0.2.0.tar.gz

---
+++ packages/sirad/sirad-0.2.0.tar.gz/sirad-0.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="sirad",
+    author="Mark Howison",
+    author_email="mhowison@ripl.org",
+    version="0.2.0",
+    url="https://github.com/ripl-org/sirad",
+    description="Secure Infrastructure for Research with Administrative Data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: Free for non-commercial use",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering"],
+    provides=["sirad"],
+    install_requires=[
+        "openpyxl",
+        "pandas",
+        "PyYAML"],
+    packages=find_packages(),
+    scripts=["scripts/sirad"]
+)
