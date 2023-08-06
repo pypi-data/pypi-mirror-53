89be9e5853d86c72600d19c92265aeb4ab34fc9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:48 +0000

    Add package slow-motion-mein/0.1/slow-motion-mein-0.1.tar.gz

---
+++ packages/slow-motion-mein/slow-motion-mein-0.1.tar.gz/slow-motion-mein-0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='slow-motion-mein',  
+     version='0.1',
+     scripts=['slow-motion-mein'] ,
+     author="Wacky Bot",
+     author_email="IItkabaapiet@gmail.com",
+     description="And AWS plus some script cool utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/shinhacker/slow-motion-mein",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
