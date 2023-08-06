d617fb4747254ba32b816bc766018c28f0d55279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:41 +0000

    Add package micropy-cli/3.0.0/micropy_cli-3.0.0-py2.py3-none-any.whl

---
+++ packages/micropy-cli/micropy_cli-3.0.0-py2.py3-none-any.whl/micropy/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Micropy Cli
+~~~~~~~~~~~~~~~~~~~~~
+
+Micropy Cli is a project management/generation tool for writing Micropython
+code in modern IDEs such as VSCode. Its primary goal is to automate the
+process of creating a workspace complete with:
+
+Linting compatible with Micropython,
+VSCode Intellisense,
+Autocompletion,
+Dependency Management,
+VCS Compatibility
+and more.
+"""
+
+__author__ = """Braden Mars"""
+__version__ = '3.0.0'
