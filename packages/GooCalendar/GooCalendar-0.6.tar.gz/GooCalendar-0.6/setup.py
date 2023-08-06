071ea2d7987154ff9756806a5f2b79421a114b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:34 +0000

    Add package GooCalendar/0.6/GooCalendar-0.6.tar.gz

---
+++ packages/GooCalendar/GooCalendar-0.6.tar.gz/GooCalendar-0.6/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of GooCalendar.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+import io
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return io.open(
+        os.path.join(os.path.dirname(__file__), fname),
+        'r', encoding='utf-8').read()
+
+
+setup(name='GooCalendar',
+    version='0.6',
+    author='Cédric Krier',
+    author_email='cedric.krier@b2ck.com',
+    url='https://goocalendar.tryton.org/',
+    description='A calendar widget for GTK using PyGoocanvas',
+    long_description=read('README'),
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Widget Sets',
+        ],
+    license='GPL-2',
+    python_requires='>=3.4',
+    )
