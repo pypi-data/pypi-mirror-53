aeefe945d2923a88911b0c47e6d420e5b042ea9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:52 +0000

    Add package augmentation/0.6/augmentation-0.6.tar.gz

---
+++ packages/augmentation/augmentation-0.6.tar.gz/augmentation-0.6/README.md
@@ -0,0 +1,10 @@
+# GPU accelerated data augmentation
+
+This code provides standard augmentation strategies on the GPU, as some of these can be intensive on the CPU. Our GPU translation is based on [PyTorch](https://pytorch.org/). The current version supports both 2D and 3D data augmentation. 
+
+## Installation
+Install the dependencies and you are ready to go! 
+<pre><code>pip install augmentation</code></pre>
+
+## Usage
+Check out the provided [notebook examples](https://github.com/JorisRoels/augmentation/tree/master/notebooks)! 
---
+++ packages/augmentation/augmentation-0.6.tar.gz/augmentation-0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="augmentation",
+    version="0.6",
+    author="Joris Roels",
+    author_email="jorisb.roels@ugent.be",
+    description="A library, based on PyTorch, that performs data augmentation on the GPU",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JorisRoels/augmentation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
