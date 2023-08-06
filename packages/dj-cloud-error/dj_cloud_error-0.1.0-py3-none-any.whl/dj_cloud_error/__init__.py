74eaa13ca92851f092cffb013932afddf50796be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:50 +0000

    Add package dj-cloud-error/0.1.0/dj_cloud_error-0.1.0-py3-none-any.whl

---
+++ packages/dj-cloud-error/dj_cloud_error-0.1.0-py3-none-any.whl/dj_cloud_error/__init__.py
@@ -0,0 +1,9 @@
+from .handler import handler500
+
+"""Top-level package for Django Google Cloud Error Reporting."""
+
+__author__ = """Prismatic Digital"""
+__email__ = "contact@prismaticdigital.com"
+__version__ = "0.1.0"
+
+__all__ = ["handler500"]
