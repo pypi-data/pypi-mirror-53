e4642d90003244906fb86fa2ca2f327c6f1e34f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:07 +0000

    Add package oasislmf/1.4.2/oasislmf-1.4.2-py3-none-macosx_10_6_intel.whl

---
+++ packages/oasislmf/oasislmf-1.4.2-py3-none-macosx_10_6_intel.whl/oasislmf-1.4.2.data/purelib/oasislmf/model_execution/runner.py
@@ -0,0 +1,90 @@
+import logging
+import multiprocessing
+import os
+import shutil
+
+import subprocess
+
+from ..utils.defaults import KTOOLS_ALLOC_RULE_IL, KTOOLS_ALLOC_RULE_GUL
+from ..utils.exceptions import OasisException
+from ..utils.log import oasis_log
+from .bash import genbash
+
+
+@oasis_log()
+def run(
+    analysis_settings,
+    number_of_processes=-1,
+    num_reinsurance_iterations=0,
+    ktools_mem_limit=False,
+    set_alloc_rule_gul=KTOOLS_ALLOC_RULE_GUL,
+    set_alloc_rule_il=KTOOLS_ALLOC_RULE_IL,
+    fifo_tmp_dir=True,
+    run_debug=False,
+    filename='run_ktools.sh'
+):
+    if number_of_processes == -1:
+        number_of_processes = multiprocessing.cpu_count()
+
+    custom_gulcalc_cmd = "{}_{}_gulcalc".format(
+        analysis_settings.get('module_supplier_id'),
+        analysis_settings.get('model_version_id'))
+
+    # Check for custom gulcalc
+    if shutil.which(custom_gulcalc_cmd):
+
+        def custom_get_getmodel_cmd(
+            number_of_samples,
+            gul_threshold,
+            use_random_number_file,
+            coverage_output,
+            item_output,
+            process_id,
+            max_process_id,
+            gul_alloc_rule,
+            **kwargs
+        ):
+
+            cmd = "{} -e {} {} -a {} -p {}".format(
+                custom_gulcalc_cmd,
+                process_id,
+                max_process_id,
+                os.path.abspath("analysis_settings.json"),
+                "input")
+            if coverage_output != '' and not gul_alloc_rule:
+                cmd = '{} -c {}'.format(cmd, coverage_output)
+            if item_output != '':
+                cmd = '{} -i {}'.format(cmd, item_output)
+
+            return cmd
+
+        genbash(
+            number_of_processes,
+            analysis_settings,
+            num_reinsurance_iterations=num_reinsurance_iterations,
+            fifo_tmp_dir=fifo_tmp_dir,
+            mem_limit=ktools_mem_limit,
+            gul_alloc_rule=set_alloc_rule_gul,
+            il_alloc_rule=set_alloc_rule_il,
+            bash_trace=run_debug,
+            filename=filename,
+            _get_getmodel_cmd=custom_get_getmodel_cmd,
+        )
+    else:
+        genbash(
+            number_of_processes,
+            analysis_settings,
+            num_reinsurance_iterations=num_reinsurance_iterations,
+            fifo_tmp_dir=fifo_tmp_dir,
+            mem_limit=ktools_mem_limit,
+            gul_alloc_rule=set_alloc_rule_gul,
+            il_alloc_rule=set_alloc_rule_il,
+            bash_trace=run_debug,
+            filename=filename
+        )
+
+    try:
+        bash_trace = subprocess.check_output(['bash', filename], stderr=subprocess.STDOUT)
+        logging.info(bash_trace.decode('utf-8'))
+    except subprocess.CalledProcessError as e:
+        raise OasisException('Error running ktools: {}'.format(e.output.decode('utf-8').strip()))
---
+++ packages/oasislmf/oasislmf-1.4.2-py3-none-macosx_10_6_intel.whl/oasislmf-1.4.2.data/purelib/oasislmf/utils/defaults.py
@@ -0,0 +1,144 @@
+__all__ = [
+    'get_config_profile',
+    'get_default_accounts_profile',
+    'get_default_deterministic_analysis_settings',
+    'get_default_exposure_profile',
+    'get_default_fm_aggregation_profile',
+    'get_default_unified_profile',
+    'get_loc_dtypes',
+    'get_acc_dtypes',
+    'get_scope_dtypes',
+    'get_info_dtypes',
+    'KTOOLS_ALLOC_RULE_IL',
+    'KTOOLS_ALLOC_RULE_GUL',
+    'KTOOLS_FIFO_RELATIVE',
+    'KTOOLS_DEBUG',
+    'KTOOLS_MEM_LIMIT',
+    'KTOOLS_NUM_PROCESSES',
+    'OASIS_FILES_PREFIXES',
+    'SUMMARY_MAPPING',
+    'SUMMARY_OUTPUT',
+    'SOURCE_IDX',
+    'SOURCE_FILENAMES',
+    'STATIC_DATA_FP',
+    'WRITE_CHUNKSIZE'
+]
+
+import os
+
+from collections import OrderedDict
+
+from .data import (
+    get_json,
+)
+
+
+SOURCE_FILENAMES = OrderedDict({
+    'loc': 'location.csv',
+    'acc': 'account.csv',
+    'info': 'reinsinfo.csv',
+    'scope': 'reinsscope.csv'
+})
+
+# Store index from merged source files (for later slice & dice)
+SOURCE_IDX = OrderedDict({
+    'loc': 'loc_idx',
+    'acc': 'acc_idx',
+    'info': 'info_idx',
+    'scope': 'scope_idx'
+})
+
+SUMMARY_MAPPING = OrderedDict({
+    'gul_map_fn': 'gul_summary_map.csv',
+    'fm_map_fn': 'fm_summary_map.csv'
+})
+
+SUMMARY_OUTPUT = OrderedDict({
+    'gul': 'gulsummaryxref.csv',
+    'il': 'fmsummaryxref.csv'
+})
+
+# Path for storing static data/metadata files used in the package
+STATIC_DATA_FP = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)), '_data')
+
+# Default profiles that describe the financial terms in the OED acc. and loc.
+# (exposure) files, as well as how aggregation of FM input items is performed
+# in the different OED FM levels
+
+
+def get_default_accounts_profile(path=False):
+    fp = os.path.join(STATIC_DATA_FP, 'default_acc_profile.json')
+    return get_json(src_fp=fp) if not path else fp
+
+
+def get_default_exposure_profile(path=False):
+    fp = os.path.join(STATIC_DATA_FP, 'default_loc_profile.json')
+    return get_json(src_fp=fp) if not path else fp
+
+
+def get_config_profile(path=False):
+    fp = os.path.join(STATIC_DATA_FP, 'config_compatibility_profile.json')
+    return get_json(src_fp=fp) if not path else fp
+
+
+def get_default_unified_profile(path=False):
+    fp = os.path.join(STATIC_DATA_FP, 'default_unified_profile.json')
+    return get_json(src_fp=fp) if not path else fp
+
+
+def get_default_fm_aggregation_profile(path=False):
+    fp = os.path.join(STATIC_DATA_FP, 'default_fm_agg_profile.json')
+    return {int(k): v for k, v in get_json(src_fp=fp).items()} if not path else fp
+
+
+def get_loc_dtypes():
+    fp = os.path.join(STATIC_DATA_FP, 'loc_dtypes.json')
+    return get_json(src_fp=fp)
+
+
+def get_acc_dtypes():
+    fp = os.path.join(STATIC_DATA_FP, 'acc_dtypes.json')
+    return get_json(src_fp=fp)
+
+
+def get_scope_dtypes():
+    fp = os.path.join(STATIC_DATA_FP, 'scope_dtypes.json')
+    return get_json(src_fp=fp)
+
+
+def get_info_dtypes():
+    fp = os.path.join(STATIC_DATA_FP, 'info_dtypes.json')
+    return get_json(src_fp=fp)
+
+
+WRITE_CHUNKSIZE = 2 * (10 ** 5)
+
+# Default name prefixes of the Oasis input files (GUL + IL)
+OASIS_FILES_PREFIXES = OrderedDict({
+    'gul': {
+        'complex_items': 'complex_items',
+        'items': 'items',
+        'coverages': 'coverages',
+    },
+    'il': {
+        'fm_policytc': 'fm_policytc',
+        'fm_profile': 'fm_profile',
+        'fm_programme': 'fm_programme',
+        'fm_xref': 'fm_xref',
+    }
+})
+
+
+# Default analysis settings for deterministic loss generation
+def get_default_deterministic_analysis_settings(path=False):
+    fp = os.path.join(STATIC_DATA_FP, 'analysis_settings.json')
+    return get_json(src_fp=fp) if not path else fp
+
+
+# Defaults for Ktools runtime parameters
+KTOOLS_NUM_PROCESSES = -1
+KTOOLS_MEM_LIMIT = False
+KTOOLS_FIFO_RELATIVE = False
+KTOOLS_ALLOC_RULE_IL = 2
+KTOOLS_ALLOC_RULE_GUL = 1     # 1 = new item stream, 0 = use prev Coverage stream
+KTOOLS_DEBUG = False
