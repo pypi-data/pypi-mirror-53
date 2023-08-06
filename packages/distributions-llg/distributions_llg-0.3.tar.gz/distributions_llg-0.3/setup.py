e311ed8488adeb40fd48ccda34be5030521e5358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:57 +0000

    Add package distributions-llg/0.3/distributions_llg-0.3.tar.gz

---
+++ packages/distributions-llg/distributions_llg-0.3.tar.gz/distributions_llg-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_llg',
+      version='0.3',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_llg'],
+      author='Udacity---modified by myself as part of the exercise',
+      zip_safe=False)
