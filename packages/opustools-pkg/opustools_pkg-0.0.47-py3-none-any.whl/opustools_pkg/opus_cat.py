bf2d5acd0d36830a34a8842527e4a93782495bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:47 +0000

    Add package opustools-pkg/0.0.47/opustools_pkg-0.0.47-py3-none-any.whl

---
+++ packages/opustools-pkg/opustools_pkg-0.0.47-py3-none-any.whl/opustools_pkg/opus_cat.py
@@ -0,0 +1,171 @@
+import argparse
+import zipfile
+from .opus_get import OpusGet
+from .parse.sentence_parser import SentenceParser
+
+class SentenceParser(SentenceParser):
+    
+    def __init__(self, document, args):
+        super().__init__(document, '', '', '', '', args.pa, args.sa, args.ca,
+            False)
+        self.args = args
+
+        self.stopit = False
+
+        self.parser.StartElementHandler = self.start_element_opuscat
+        self.parser.EndElementHandler = self.end_element_opuscat
+        
+    def start_element_opuscat(self, name, attrs):
+        self.start_element(name, attrs)
+        if name == 's':
+            self.posses = []
+
+    def end_element_opuscat(self, name):
+        self.end_element(name)
+        if name in ['document', 'text']:
+            self.stopit = True
+
+    def processTokenizedSentence(self, sentence):
+        newSentence, stop = sentence, 0
+        if self.efound:
+            self.sfound = False
+            self.efound = False
+            stop = -1
+#            if newSentence == "":
+#                newSentence = self.chara
+#                self.chara = ""
+
+        newSentence = self.addToken(sentence)
+
+        return newSentence, stop
+
+    def readSentence(self):
+        sentence = ''
+        while True:
+            line = self.document.readline()
+            self.parseLine(line)
+            newSentence, stop = self.processTokenizedSentence(sentence)
+            sentence = newSentence
+            if stop == -1 or self.stopit:
+                break
+
+        sentence = sentence.strip()
+
+        if sentence == '':
+            return ''
+
+        if self.args.i:
+            return sentence
+        else:
+            return '("' + self.sid + '")>' + sentence
+
+class OpusCat:
+
+    def __init__(self, arguments):
+        parser = argparse.ArgumentParser(prog='opus_cat', 
+            description='Read a document from OPUS and print to STDOUT')
+
+        parser.add_argument('-d', help='Corpus name', required=True)
+        parser.add_argument('-l', help='Language', required=True)
+        parser.add_argument('-i', help='Print without ids when using -p', 
+            action='store_true')
+        parser.add_argument('-m', help='Maximum number of sentences', 
+            default='all')
+        parser.add_argument('-p', help='Print in plain txt', 
+            action='store_true')
+        parser.add_argument('-f', 
+            help='File name (if not given, prints all files)')
+        parser.add_argument('-r', help='Release (default=latest)', 
+            default='latest')
+        parser.add_argument('-pa', help='Print annotations, if they exist', 
+            action='store_true')
+        parser.add_argument('-sa', 
+            help='Set sentence annotation attributes to be printed'
+                ', e.g. -sa pos lem. To print all available attributes '
+                'use -sa all_attrs (default=pos,lem)', 
+            nargs='+',
+            default=['pos', 'lem'])
+        parser.add_argument('-ca', 
+            help='Change annotation delimiter (default=|)', 
+            default='|')
+
+        if len(arguments) == 0:
+            self.args = parser.parse_args()
+        else:
+            self.args = parser.parse_args(arguments)
+            
+        self.openFiles(
+            self.args.d+'_'+self.args.r+'_xml_'+self.args.l+'.zip',
+            '/proj/nlpl/data/OPUS/'+self.args.d+'/latest/xml/'+self.args.l
+            +'.zip')
+
+        if self.args.m == 'all':
+            self.maximum = -2
+        else:
+            self.maximum = int(self.args.m)
+
+    def openFiles(self, localfile, defaultpath):
+        try:
+            try:
+                self.lzip = zipfile.ZipFile(localfile)
+            except FileNotFoundError:
+                self.lzip = zipfile.ZipFile(defaultpath)
+        except FileNotFoundError:
+            print('\nRequested file not found. The following files are '
+                'availble for downloading:\n')
+            arguments = ['-d', self.args.d, '-s', self.args.l, '-t', ' ', 
+                '-p', 'xml', '-l', '-r', self.args.r]
+            og = OpusGet(arguments)
+            og.get_files()
+            arguments.remove('-l')
+            og = OpusGet(arguments)
+            og.get_files()
+            try:
+                self.lzip = zipfile.ZipFile(localfile)
+            except FileNotFoundError:
+                print('No file found with parameters '+str(self.args.__dict__))
+
+    def printFile(self, f, n):
+        xml_break = False
+        if self.args.p:
+            spar = SentenceParser(f, self.args)
+            print('\n# '+n+'\n')
+            while True:
+                sent = spar.readSentence()
+                if sent != '':
+                    print(sent)
+                    self.maximum -= 1
+                if spar.stopit or self.maximum == 0:
+                    break
+            spar.document.close()
+        else:
+            for line in f:
+                line = line.decode('utf-8')
+                if '<s id=' in line or '<s hun=' in line:
+                    self.maximum -= 1
+                    if self.maximum == -1:
+                        xml_break = True
+                        break
+                print(line, end='')
+        return xml_break
+
+            
+    def printSentences(self):
+        try:
+            if self.args.f:
+                with self.lzip.open(self.args.f, 'r') as f:
+                    self.printFile(f, self.args.f)
+            else:
+                for n in self.lzip.namelist():
+                    if n[-4:] == '.xml':
+                        with self.lzip.open(n, 'r') as f:
+                            xml_break = self.printFile(f, n)
+                        if xml_break:
+                            break
+
+                    if self.maximum == 0:
+                        break
+        except AttributeError as e:
+            print('Necessary files not found.')
+
+
---
+++ packages/opustools-pkg/opustools_pkg-0.0.47-py3-none-any.whl/opustools_pkg/parse/links_alignment_parser.py
@@ -0,0 +1,65 @@
+import zipfile
+import gzip
+import xml.parsers.expat
+import re
+
+from .alignment_parser import AlignmentParser
+from .sentence_parser import SentenceParser
+from .exhaustive_sentence_parser import ExhaustiveSentenceParser
+from ..opus_get import OpusGet
+
+class LinksAlignmentParser(AlignmentParser):
+
+    def __init__(self, source, target, args, result, mosessrc, mosestrg,
+            fromto, switch_langs):
+        super().__init__(source, target, args, result, mosessrc,
+            mosestrg, fromto, switch_langs)
+
+        self.end = ''
+
+        self.alignParser.EndElementHandler = self.end_element
+
+    def end_element(self, name):
+        self.end = ''
+        if name == 'linkGrp':
+            self.end = name
+
+    def initializeSentenceParsers(self, attrs):
+        #if link printing mode is activated, no need to open 
+        #zipfiles and create sentence parsers
+        if self.testConfidenceOn:
+            self.openSentenceParsers(attrs)
+
+    def readPair(self):
+        #tags other than link are printed in link printing mode, 
+        #otherwise they are skipped
+        if self.start != 'link':
+            return 1
+
+        srcAttrs, trgAttrs = {}, {}
+        #no need to parse sentences in link printing mode
+        if self.testConfidenceOn:
+            sourceSen, srcAttrs = self.sPar.readSentence(self.fromids)
+            targetSen, trgAttrs = self.tPar.readSentence(self.toids)
+
+        #if either side of the alignment is outside of the sentence limit, 
+        #or the attribute value is under the given attribute
+        #threshold, return -1, which skips printing of the alignment in 
+        #PairPrinter.outputPair()
+        if (self.sentencesOutsideLimit() or
+                (self.args.attribute != 'any' and
+                    self.overThreshold == False)):
+            return -1
+        elif (self.testConfidenceOn and
+                not self.langIdConfidence(srcAttrs, trgAttrs)):
+            return -1
+        #if filtering non-alignments is set to True and either side of 
+        #the alignment has no sentences:
+        #return -1
+        elif (self.nonAlignments and
+                (self.fromids[0] == '' or self.toids[0] == '')):
+            return -1
+        else:
+            self.overThreshold = False
+            return 1
+
