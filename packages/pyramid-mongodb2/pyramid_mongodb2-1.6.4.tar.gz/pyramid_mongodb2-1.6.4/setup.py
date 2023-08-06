358764cbf1d5fce0f3c70ef3a0d5f3b9046b42ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:58 +0000

    Add package pyramid-mongodb2/1.6.4/pyramid_mongodb2-1.6.4.tar.gz

---
+++ packages/pyramid-mongodb2/pyramid_mongodb2-1.6.4.tar.gz/pyramid_mongodb2-1.6.4/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+
+def readfile(name):
+    with open(name) as f:
+        return f.read()
+
+
+README = readfile('README.md')
+
+install_requires = [
+    'pyramid',
+    'pymongo'
+]
+
+testing_extras = [
+    'WebTest',
+    'nose',
+    'coverage',
+]
+
+setup(name='pyramid_mongodb2',
+      version='1.6.4',
+      description='An improved package that provides mongodb connectivity. Not compatible with pyramid_mongo or pyramid_mongodb',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP :: WSGI",
+          "License :: OSI Approved :: MIT License",
+      ],
+      keywords='wsgi pylons pyramid mongodb pymongo',
+      author="Jonathan Mackenzie",
+      author_email="pylons-discuss@googlegroups.com",
+      url="https://github.com/jonnoftw/pyramid_mongodb2",
+      license="MIT",
+      packages=find_packages('pyramid_mongodb2', exclude=['tests']),
+      package_dir={'':'.'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={
+          'testing': testing_extras,
+      },
+      test_suite="tests",
+      )
