2d68e5f8662e71aa4d4114bf203875b9947153fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:13 +0000

    Add package xiModule/1.0/xiModule-1.0.tar.gz

---
+++ packages/xiModule/xiModule-1.0.tar.gz/xiModule-1.0/setup.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='xiModule',
+    version="1.0",
+    description="李传熙的对外发布模块",
+    author="李传熙",
+    author_email="924974944@qq.com",
+    py_modules=['baiZHanSuperMath.demo1']
+)
+
