7dc7648ab999c1a96c2bd3a26968d8aece5310d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:37 +0000

    Add package docxmerge-sdk/1.1.0/docxmerge_sdk-1.1.0.tar.gz

---
+++ packages/docxmerge-sdk/docxmerge_sdk-1.1.0.tar.gz/docxmerge_sdk-1.1.0/README.md
@@ -0,0 +1,9 @@
+# Nodejs library
+
+Install library
+
+```
+pip install docxmerge_sdk
+```
+
+Look at samples directory to get started
---
+++ packages/docxmerge-sdk/docxmerge_sdk-1.1.0.tar.gz/docxmerge_sdk-1.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="docxmerge_sdk",
+    version="1.1.0",
+    author="David viejo pomata",
+    author_email="davidviejopomata@gmail.com",
+    description="Sdk for docxmerge",
+    url="https://github.com/Docxmerge/docxmerge-sdk",
+    packages=['docxmerge_sdk'],
+    package_dir={
+        'docxmerge_sdk': 'docxmerge_sdk'
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+    ],
+
+)
