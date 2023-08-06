b509b61ba0351a65dd9a0d567d090e79558ad689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package PqrUploadModule/0.0.5b0/PqrUploadModule-0.0.5b0.tar.gz

---
+++ packages/PqrUploadModule/PqrUploadModule-0.0.5b0.tar.gz/PqrUploadModule-0.0.5b0/PqrUploadModule/__init__.py
@@ -0,0 +1,18 @@
+name = "PqrUploadModule"
+from . import _scan
+from . import action_notes_counterparts
+from . import api_functions
+from . import attachment_counterparts
+from . import attachments
+from . import authorizations
+from . import contacts_update
+from . import contacts
+from . import incidents
+from . import notes
+from . import scanning
+from . import start_settings
+from . import tickets_incidents_counterparts
+from . import tickets
+
+
+from scanning import Scan
