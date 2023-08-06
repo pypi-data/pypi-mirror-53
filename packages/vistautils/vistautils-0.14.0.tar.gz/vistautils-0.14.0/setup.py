29394fd97aa4c145395d097253555d3d2c9eaf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:22 +0000

    Add package vistautils/0.14.0/vistautils-0.14.0.tar.gz

---
+++ packages/vistautils/vistautils-0.14.0.tar.gz/vistautils-0.14.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+from os.path import abspath, dirname, join
+
+with open(join(dirname(abspath(__file__)), 'vistautils', 'version.py')) as version_file:
+    exec(compile(version_file.read(), "version.py", 'exec'))
+
+setup(name='vistautils',
+      version=version,
+      author='Ryan Gabbard <gabbard@isi.edu> and Constantine Lignos <lignos@isi.edu>',
+      author_email='gabbard@isi.edu',
+      description="Python utilities developed by USC ISI's VISTA center",
+      url='https://github.com/isi-vista/vistautils',
+      packages=['vistautils'],
+      # 3.6 and up, but not Python 4
+      python_requires='~=3.6',
+      install_requires=[
+          'immutablecollections>=0.7.0',
+          'attrs>=19.1.0',
+          'pyyaml>=5.1',
+          'typing_extensions',
+          'sortedcontainers>=2.1.0',
+      ],
+      package_data={'vistautils': ['py.typed']},
+      scripts=["scripts/join_key_value_stores.py",
+          "scripts/join_binary_key_value_stores.py",
+          "scripts/join_character_key_value_stores.py",
+          "scripts/split_key_value_store.py",
+          "scripts/tar_gz_to_zip.py"],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+      )
