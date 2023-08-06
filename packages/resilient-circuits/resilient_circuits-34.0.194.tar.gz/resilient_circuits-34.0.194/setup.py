5d2acbaedbb22b5eee5504342d609f05b5bc296c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:02 +0000

    Add package resilient-circuits/34.0.194/resilient_circuits-34.0.194.tar.gz

---
+++ packages/resilient-circuits/resilient_circuits-34.0.194.tar.gz/resilient_circuits-34.0.194/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2018. All Rights Reserved.
+
+""" setup.py for resilient-circuits module """
+
+from __future__ import print_function
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires_resilient_version = "29.0"
+major, minor = requires_resilient_version.split('.', 2)[:2]
+
+setup(
+    name='resilient_circuits',
+    use_scm_version={"root": "../", "relative_to": __file__},
+    setup_requires=['setuptools_scm'],
+    url='https://developer.ibm.com/resilient',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    author='IBM Resilient',
+    install_requires=[
+        'stompest>=2.3.0',
+        'requests>=2.6.0',
+        'circuits',
+        'pytz',
+        'jinja2>=2.10.0',
+        'pysocks',
+        'filelock>=2.0.5',
+        'setuptools>=41.0.0',
+        'resilient>={}.{}'.format(major, minor)
+    ],
+    author_email='support@resilientsystems.com',
+    description='Resilient Circuits Framework for Custom Integrations',
+    long_description='Resilient Circuits Framework for Custom Integrations',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    entry_points={
+        'console_scripts': ['res-action-test = resilient_circuits.bin.res_action_test:main',
+                            'resilient-circuits = resilient_circuits.bin.resilient_circuits_cmd:main']
+    }
+)
