5b8b38faf655b99b0ac5109345bd2e7d789eb73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:37 +0000

    Add package pyGDM2/1.0.10/pyGDM2-1.0.10-cp35-cp35m-win_amd64.whl

---
+++ packages/pyGDM2/pyGDM2-1.0.10-cp35-cp35m-win_amd64.whl/pyGDM2/__init__.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+#
+#Copyright (C) 2017-2019, P. R. Wiecha
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+    pyGDM2 - a full-field electrodynamical solver python toolkit.
+    Based on the Green Dyadic Method.
+    
+"""
+__name__ = 'pyGDM2'
+__version__ = '1.0.10'
+__date__ = "09/19/2019"
+__author__ = 'Peter R. Wiecha'
+
+__all__ = ["core", "materials", "structures", "fields", "linear", "tools", "visu", "EO"]
