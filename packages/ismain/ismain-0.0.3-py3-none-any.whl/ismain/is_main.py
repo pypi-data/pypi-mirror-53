4291a9058d33832f6be4466c732885bbf5ceadaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:42 +0000

    Add package ismain/0.0.3/ismain-0.0.3-py3-none-any.whl

---
+++ packages/ismain/ismain-0.0.3-py3-none-any.whl/ismain/is_main.py
@@ -0,0 +1,12 @@
+import inspect
+
+
+def is_main() -> bool:
+    """
+
+    Prettier version of __name__ == "__main__"
+
+    :return: True if application is running as 'main'
+
+    """
+    return inspect.stack()[1].frame.f_locals.get("__name__") == "__main__"
