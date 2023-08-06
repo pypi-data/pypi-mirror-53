e086648935c036d634b1f8584d5a04199f234823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:40 +0000

    Add package query-postgresql/0.1.0.1/query_postgresql-0.1.0.1.tar.gz

---
+++ packages/query-postgresql/query_postgresql-0.1.0.1.tar.gz/query_postgresql-0.1.0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'query_postgresql',         
+  packages = ['query_postgresql'],   
+  version = '0.1.0.1',      
+  license='MIT',        
+  description = 'Querying using Postgresql made easy',   
+  author = 'Reza',                   
+  author_email = 'reza16.sugiarto@gmail.com',      
+  url = 'http://github.com/rezasugiarto/query_postgresql',   
+  download_url = 'https://github.com/rezasugiarto/query_postgresql/archive/0.1.0.1.tar.gz',    
+  keywords = ['postgresql'],   
+  install_requires=[           
+          'pandas',
+          'psycopg2',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
