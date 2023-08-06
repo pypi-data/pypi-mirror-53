2de5cef775f281164017b0987151c3e1b7df3d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:45 +0000

    Add package pyven/6/pyven-6.tar.gz

---
+++ packages/pyven/pyven-6.tar.gz/pyven-6/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+def long_description():
+    with open('README.md') as f:
+        return f.read()
+
+setuptools.setup(
+        name = 'pyven',
+        version = '6',
+        description = 'Management of PYTHONPATH for simultaneous dev of multiple projects',
+        long_description = long_description(),
+        long_description_content_type = 'text/markdown',
+        url = 'https://github.com/combatopera/pyven',
+        author = 'Andrzej Cichocki',
+        packages = setuptools.find_packages(),
+        py_modules = ['gclean', 'tasks', 'release', 'runtests', 'travis_ci', 'pyven', 'toplevel', 'tests'],
+        install_requires = ['aridity'],
+        package_data = {'': ['*.pxd', '*.pyx', '*.pyxbld', '*.arid', '*.aridt']},
+        scripts = ['gclean.py', 'tasks.py', 'pyven', 'release.py', 'runtests.py', 'travis_ci.py', 'pyven.py', 'toplevel.py', 'tests', 'foreignsyms'])
