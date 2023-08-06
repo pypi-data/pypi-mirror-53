7e3d5647ecffcc62a6c875922dc07471c5a45b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:08 +0000

    Add package splut/1/splut-1.tar.gz

---
+++ packages/splut/splut-1.tar.gz/splut-1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name = 'splut',
+        version = '1',
+        install_requires = [],
+        packages = setuptools.find_packages(),
+        py_modules = ['bg', 'delay'],
+        scripts = [])
