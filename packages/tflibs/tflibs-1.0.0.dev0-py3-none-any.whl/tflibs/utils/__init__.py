c62b79b4c9f1876eb9e5d97f70e4321cdab60ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:02 +0000

    Add package tflibs/1.0.0.dev0/tflibs-1.0.0.dev0-py3-none-any.whl

---
+++ packages/tflibs/tflibs-1.0.0.dev0-py3-none-any.whl/tflibs/utils/__init__.py
@@ -0,0 +1,4 @@
+from tflibs.utils.data_structure import *
+from tflibs.utils.decorators import *
+from tflibs.utils.ioio import *
+from tflibs.utils.summary import *
---
+++ packages/tflibs/tflibs-1.0.0.dev0-py3-none-any.whl/tflibs/utils/decorators.py
@@ -0,0 +1,64 @@
+""" Decorators """
+from typing import Callable, TypeVar
+from functools import wraps
+
+import tensorflow as tf
+
+T = TypeVar('T')
+
+
+class CachedProperty:
+    def __init__(self, fget: Callable):
+        self.fget = fget
+
+    @property
+    def fget(self) -> Callable:
+        return self._fget
+
+    @fget.setter
+    def fget(self, fn):
+        self._fget = fn
+        self._name = fn.__name__
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def __get__(self, instance, owner) -> T:
+        """
+        Called once when the corresponding property is first accessed, and sets the value as attribute.
+        :param instance:
+        :param owner:
+        :return: Value of the property
+        """
+        val = self.fget(instance)  # type: T
+
+        setattr(instance, self.name, val)
+
+        return val
+
+
+def coroutine(f: Callable):
+    @wraps(f)
+    async def wrapper(*args, **kwargs):
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def distributed_run(strategy: tf.distribute.MirroredStrategy):
+    def decorator(f: Callable):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            return strategy.experimental_run_v2(f, args=args, kwargs=kwargs)
+
+        return tf.function(wrapper)
+
+    return decorator
+
+
+def unpack_tuple(original_fn):
+    def wrapper(tup):
+        return original_fn(*tup)
+
+    return wrapper
