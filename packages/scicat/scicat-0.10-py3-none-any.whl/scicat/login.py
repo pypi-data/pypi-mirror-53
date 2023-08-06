1c709f3a273ce39ff1551c6b569b745dc39d1ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:15 +0000

    Add package scicat/0.10/scicat-0.10-py3-none-any.whl

---
+++ packages/scicat/scicat-0.10-py3-none-any.whl/scicat/login.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""login and get token"""
+import os
+import sys
+
+import getpass
+import requests
+import keyring
+
+from scicat import Api
+
+
+class Login:
+    """login and get token"""
+    token = ""
+    username = ""
+    password = ""
+
+    def get_username(self):
+        """get username"""
+        if "JUPYTERHUBUSER" in os.environ:
+            self.username = os.environ["JUPYTERHUBUSER"]
+        else:
+            try:
+                self.username = getpass.getuser()
+                # username = username.replace(".", "")
+                print("Username", self.username)
+            except Exception as error:
+                print(error)
+                self.username = input("Username:")
+
+        
+    def login(self):
+        """login and return token"""
+        credentials = {}
+        self.get_username()
+        credentials["username"] = self.username
+        password = keyring.get_password('scicat', self.username)
+        if not password:
+            print("No password found in keychain, please enter it now to store it.")
+            password = getpass.getpass()
+            keyring.set_password('scicat', self.username, password)
+        credentials["password"] = password
+        api = Api()
+        url = os.path.join(api.base, "auth/msad")
+        token = ""
+        response = requests.post(url, json=credentials)
+        result = response.json()
+        print(result)
+        token = ""
+        if response.status_code == 200:
+            pass
+        else:
+            print("Login failed")
+            print(response.status_code)
+            sys.exit()
+        if isinstance(result, str):
+            print("token", token)
+        else:
+            token = result.get("access_token")
+            print("token", token)
+
+        return token
+
+
+def login():
+    """main login"""
+    scicatlogin = Login()
+    scicatlogin.login()
+
+
+if __name__ == "__main__":
+    login()
