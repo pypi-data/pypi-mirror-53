8633c72343394b5e5c34d0771be508ebd1ab7a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:34 +0000

    Add package pandas-ml-utils/0.0.8/pandas_ml_utils-0.0.8-py3-none-any.whl

---
+++ packages/pandas-ml-utils/pandas_ml_utils-0.0.8-py3-none-any.whl/pandas_ml_utils/pandas_utils_extension.py
@@ -0,0 +1,36 @@
+import logging
+import re
+from typing import Union, Callable
+
+import pandas as pd
+
+log = logging.getLogger(__name__)
+
+
+def add_apply(df, **kwargs: Callable[[pd.DataFrame], Union[pd.Series, pd.DataFrame]]):
+    df2 = pd.DataFrame()
+    for k, v in kwargs.items():
+        df2[k] = v(df)
+
+    return df.join(df2)
+
+
+def shift_inplace(df, **kwargs: int):
+    for k, v in kwargs.items():
+        df[k] = df[k].shift(v)
+
+    return df
+
+
+def drop_re(df, *args: str):
+    drop_list = []
+
+    for regex in args:
+        drop_list.extend(list(filter(re.compile(regex).match, df.columns)))
+
+    return df.drop(drop_list, axis=1)
+
+
+def extend_forecast(df, periods: int):
+    df_ext = pd.DataFrame(index=pd.date_range(df.index[-1], periods=periods+1, closed='right'))
+    return pd.concat([df, df_ext], axis=0, sort=True)
