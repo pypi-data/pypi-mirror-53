e326dfbbf6adeaf7034db17db2904f95883e1c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:41 +0000

    Add package quma/0.1.0b4/quma-0.1.0b4-py3-none-any.whl

---
+++ packages/quma/quma-0.1.0b4-py3-none-any.whl/quma/__init__.py
@@ -0,0 +1,41 @@
+import platform
+
+from .database import (  # noqa: F401
+    Database,
+    connect,
+)
+from .exc import (  # noqa: F401
+    APIError,
+    DoesNotExistError,
+    FetchError,
+    MultipleRowsError,
+    OperationalError,
+    TimeoutError,
+)
+from .namespace import Namespace
+
+try:
+    import psycopg2  # noqa: F401
+except ImportError:
+    try:
+        from psycopg2cffi import compat  # noqa: F401
+    except ImportError:
+        pass
+    else:
+        compat.register()
+
+PLATFORM = platform.python_implementation()
+
+__all__ = [
+    'Database',
+    'Namespace',
+    'connect'
+    'APIError',
+    'DoesNotExistError',
+    'FetchError',
+    'MultipleRowsError',
+    'OperationalError',
+    'TimeoutError',
+]
+
+__version__ = '0.1.0b4'
