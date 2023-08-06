44c3b0b257d4e6f7fa7b1f2b8ce01105db395859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:56 +0000

    Add package scicat/0.6/scicat-0.6-py3-none-any.whl

---
+++ packages/scicat/scicat-0.6-py3-none-any.whl/scicat/__init__.py
@@ -0,0 +1,6 @@
+"""import all"""
+from .search import search
+from .api import Api
+from .login import login
+from .attach import attach
+from .upload import upload
---
+++ packages/scicat/scicat-0.6-py3-none-any.whl/scicat/api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""fetch api """
+
+
+class Api:
+    """ fetch api"""
+
+    api = ""
+    base = ""
+
+    def __init__(self):
+        self.base = "https://scicat.esss.se/"
+        self.api = self.base + "api/v3"
+
+    def set_api(self, api):
+        """set api"""
+        self.api = api
+
+
+def main():
+    """main"""
+    api = Api()
+    print(api.api)
+
+
+if __name__ == "__main__":
+    main()
