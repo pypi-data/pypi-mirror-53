11415efb8e6f3b02a8e265f786fb3da4ad63fa97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:22 +0000

    Add package docker-harpoon/0.15.1/docker-harpoon-0.15.1.tar.gz

---
+++ packages/docker-harpoon/docker-harpoon-0.15.1.tar.gz/docker-harpoon-0.15.1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+from harpoon import VERSION
+
+# fmt: off
+
+setup(
+      name = "docker-harpoon"
+    , version = VERSION
+    , packages = find_packages(include="harpoon.*", exclude=["tests*"])
+    , include_package_data = True
+
+    , install_requires =
+      [ "delfick_project==0.5.1"
+
+      , "docker==3.5.0"
+
+      , "humanize"
+
+      , "ruamel.yaml==0.16.5"
+      , "rainbow_logging_handler==2.2.2"
+      ]
+
+    , extras_require =
+      { "tests":
+        [ "noseOfYeti>=1.7"
+        , "nose"
+        , "mock==1.0.1"
+        , "nose-pattern-exclude"
+        , "nose-focus==0.1.3"
+        , "tox"
+        ]
+      }
+
+    , entry_points =
+      { 'console_scripts' :
+        [ 'harpoon = harpoon.executor:main'
+        ]
+      }
+
+    # metadata for upload to PyPI
+    , url = "https://github.com/delfick/harpoon"
+    , author = "Stephen Moore"
+    , author_email = "delfick755@gmail.com"
+    , description = "Opinionated wrapper around docker"
+    , long_description = open("README.rst").read()
+    , license = "MIT"
+    , keywords = "docker"
+    )
+
+# fmt: on
