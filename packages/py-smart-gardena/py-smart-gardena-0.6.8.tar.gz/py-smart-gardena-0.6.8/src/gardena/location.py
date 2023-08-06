0bcbcd6aa836850501e5c8a9165b91c62585bec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:16 +0000

    Add package py-smart-gardena/0.6.8/py-smart-gardena-0.6.8.tar.gz

---
+++ packages/py-smart-gardena/py-smart-gardena-0.6.8.tar.gz/py-smart-gardena-0.6.8/src/gardena/location.py
@@ -0,0 +1,20 @@
+from .base_gardena_class import BaseGardenaClass
+
+
+class Location(BaseGardenaClass):
+    """Keep informations about gardena locations (gardens, ..) and devices"""
+
+    id = "N/A"
+    name = "N/A"
+    devices = {}
+
+    def __init__(self, smart_system, location):
+        self.smart_system = smart_system
+        self.update_location_data(location)
+
+    def update_location_data(self, location):
+        self.id = location["id"]
+        self.name = location["attributes"]["name"]
+
+    def add_device(self, device):
+        self.devices[device.id] = device
