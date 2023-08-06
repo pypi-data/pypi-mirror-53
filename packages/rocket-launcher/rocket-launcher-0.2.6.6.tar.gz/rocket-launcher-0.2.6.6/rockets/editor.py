4659b9f20acdc1b5222322ff4bf2ba582fb2c9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:00 +0000

    Add package rocket-launcher/0.2.6.6/rocket-launcher-0.2.6.6.tar.gz

---
+++ packages/rocket-launcher/rocket-launcher-0.2.6.6.tar.gz/rocket-launcher-0.2.6.6/rockets/editor.py
@@ -0,0 +1,3 @@
+def editor():
+    os.system('sudo -H -u who set-text-editor >/dev/null 2>&1')
+    os.system('set-text-editor >/dev/null 2>&1')
---
+++ packages/rocket-launcher/rocket-launcher-0.2.6.6.tar.gz/rocket-launcher-0.2.6.6/rockets/multiplexer.py
@@ -0,0 +1,3 @@
+def multiplexer():
+    os.system('sudo -H -u who configure-multiplexer >/dev/null 2>&1')
+    os.system('configure-multiplexer >/dev/null 2>&1')
