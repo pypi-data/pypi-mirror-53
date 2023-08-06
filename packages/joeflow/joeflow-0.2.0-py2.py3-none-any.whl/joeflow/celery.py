dd6033bd884088ddceabf31c6cc4821ec8472251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:33 +0000

    Add package joeflow/0.2.0/joeflow-0.2.0-py2.py3-none-any.whl

---
+++ packages/joeflow/joeflow-0.2.0-py2.py3-none-any.whl/joeflow/celery.py
@@ -0,0 +1,57 @@
+import logging
+import random
+
+from celery import shared_task
+from django.apps import apps
+from django.db import transaction
+
+from joeflow.contrib.reversion import with_reversion
+
+from . import locking
+
+logger = logging.getLogger('joeflow')
+
+
+def jitter():
+    """Return a random number between 0 and 1."""
+    return random.randrange(2)  # nosec
+
+
+def backoff(retries):
+    """Return an exponentially growing number limited to 600 plus a random jitter."""
+    return min(600, 2 ** retries) + jitter()
+
+
+@shared_task(bind=True, ignore_results=True, max_retries=None)
+def task_wrapper(self, task_pk, process_pk):
+    with locking.lock(process_pk) as lock_result:
+        countdown = backoff(self.request.retries)
+        if not lock_result:
+            logger.info("Process is locked, retrying in %s seconds", countdown)
+            self.retry(countdown=countdown)
+        Task = apps.get_model('joeflow', 'Task')
+        task = Task.objects.get(pk=task_pk, completed=None)
+        process = task.process
+
+        try:
+            logger.info("Executing %r", task)
+            node = getattr(type(process), task.name)
+            with_task = getattr(node, 'with_task', False)
+            kwargs = {}
+            if with_task:
+                kwargs['task'] = task
+            with with_reversion(task):
+                result = node(process, **kwargs)
+        except:  # NoQA
+            task.fail()
+            logger.exception("Execution of %r failed", task)
+        else:
+            if result is False:
+                logger.info("Task returned False, retrying in %s seconds", countdown)
+                transaction.on_commit(lambda: self.retry(countdown=countdown))
+                return
+            elif result is True:
+                result = None
+            logger.info("Task completed successful, starting next tasks: %s", result)
+            task.start_next_tasks(next_nodes=result)
+            task.finish()
---
+++ packages/joeflow/joeflow-0.2.0-py2.py3-none-any.whl/joeflow/conf.py
@@ -0,0 +1,32 @@
+from appconf import AppConf
+from django.conf import settings
+
+__all__ = (
+    'settings',
+)
+
+
+class JoeflowAppConfig(AppConf):
+    """
+    List of available settings.
+
+    To change the default values just set the setting in your settings file.
+    """
+
+    JOEFLOW_REDIS_LOCK_URL = 'redis://'
+    """
+    Redis database your for Redis database that is used for process locking.
+    """
+
+    JOEFLOW_REDIS_LOCK_TIMEOUT = 60
+    """
+    Process lock timeout in seconds.
+
+    Processes are lock and only one machine task at a time can change the
+    process state.
+    """
+
+    JOEFLOW_CELERY_QUEUE_NAME = 'celery'
+    """
+    Queue name in which all machine tasks will be queued.
+    """
