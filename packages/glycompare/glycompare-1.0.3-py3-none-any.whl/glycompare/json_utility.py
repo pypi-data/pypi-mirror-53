9731222b3f56ed00a9dedc9529523f17577b8f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:36 +0000

    Add package glycompare/1.0.3/glycompare-1.0.3-py3-none-any.whl

---
+++ packages/glycompare/glycompare-1.0.3-py3-none-any.whl/glycompare/json_utility.py
@@ -0,0 +1,13 @@
+import json
+import hjson
+
+
+def store_json(address, dic):
+    with open(address, 'w') as fp:
+        json.dump(dic, fp)
+
+
+def load_json(address):
+    with open(address, 'r') as f:
+        return json.load(f)
+
