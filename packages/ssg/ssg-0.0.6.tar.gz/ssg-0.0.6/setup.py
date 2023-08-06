68d888c6d29ecb5fe38b8e298b7b4948ba014402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:31 +0000

    Add package ssg/0.0.6/ssg-0.0.6.tar.gz

---
+++ packages/ssg/ssg-0.0.6.tar.gz/ssg-0.0.6/setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+"""
+Thai syllable segmentation using Conditional Random Fields
+"""
+
+from setuptools import setup
+
+setup(
+    name="ssg",
+    packages=["ssg"],
+    package_dir={"ssg": "ssg"},
+    package_data={"ssg": ["artifacts/*"]},
+    scripts=["scripts/ssg-cli"],
+    include_package_data=True,
+    version="0.0.6",
+    install_requires=[
+        'fire>=0.1.3',
+        'python-crfsuite>=0.9.6',
+        'tqdm>=4.32.2'
+    ],
+    description="Thai syllable segmentation using Conditional Random Fields",
+    author="Ponrawee Prasertsom",
+    author_email="ponrawee.pra@gmail.com",
+    url="https://github.com/ponrawee/ssg",
+    license="Apache Software License 2.0",
+    download_url="https://github.com/ponrawee/ssg.git",
+    keywords=["crf", "syllable", "tokenization", "thai"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: Thai",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: General",
+        "Topic :: Text Processing :: Linguistic"
+    ],
+)
