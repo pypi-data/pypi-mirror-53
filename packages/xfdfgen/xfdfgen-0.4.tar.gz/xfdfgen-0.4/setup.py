3acb9a249ddf09a2447131c67ab565b822c127b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:58 +0000

    Add package xfdfgen/0.4/xfdfgen-0.4.tar.gz

---
+++ packages/xfdfgen/xfdfgen-0.4.tar.gz/xfdfgen-0.4/setup.py
@@ -0,0 +1,32 @@
+"""Setup for the xfdf package."""
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Nguyen Ba Long",
+    author_email="Nguyen.Ba.Long13@gmail.com",
+    name='xfdfgen',
+    license="APACHE 2.0",
+    description='xfdfgen is a Python library for creating xfdf files that can be used to populate pdf form fields.',
+    version='v0.4',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Balonger/xfdfgen',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
---
+++ packages/xfdfgen/xfdfgen-0.4.tar.gz/xfdfgen-0.4/xfdfgen/xfdfgen.py
@@ -0,0 +1,89 @@
+import os
+import xml.dom.minidom
+from typing import Dict
+from shutil import copy2
+
+
+def xfdfgen(name: str, field_dictionary: Dict[str, str]) -> xml.dom.minidom.Document:
+    """
+    Generates an xml.dom.minidom xml document in xfdf format given a pdf containing form fields
+    and a dictionary containing the fields
+
+    Args:
+        name (str): path to the pdf document
+        field_dictionary (Dict[str, str]): a dictionary with form field id as key and their values as value
+
+    Returns: an xml.dom.minidom.Document in xfdf format
+
+    """
+    xfdf = xml.dom.minidom.Document()
+
+    # xfdf peculiarities   #############################################################################################
+    # see https://docs.aspose.com/display/pdfnet/Whats+the+Difference+Between+XML%2C+FDF+and+XFDF
+
+    top = xfdf.createElement("xfdf")
+    top.setAttribute("xmlns", "http://ns.adobe.com/xfdf/")
+    top.setAttribute("xml:space", "preserve")
+    xfdf.appendChild(top)
+
+    f = xfdf.createElement("f")
+    f.setAttribute("href", name)
+    top.appendChild(f)
+
+    # insert fields from field dictionary   ############################################################################
+    fields = xfdf.createElement("fields")
+    for field_name, field_value in field_dictionary.items():
+        field = xfdf.createElement("field")
+        field.setAttribute("name", field_name)
+        fields.appendChild(field)
+
+        value = xfdf.createElement("value")
+        field.appendChild(value)
+
+        text1 = xfdf.createTextNode(field_value)
+        value.appendChild(text1)
+    top.appendChild(fields)
+
+    return xfdf
+
+
+class Xfdf:
+    def __init__(self, name: str, field_dictionary: Dict[str, str]) -> None:
+        """
+        Initializes an xfdf form xml.
+        Use .write_xfdf() to write to a file
+        Use .get_xfdf to return the raw xfdf string
+        Use .pretty_print to pretty print the xfdf file to console
+
+        Args:
+            name (str): path to the pdf document
+            field_dictionary (Dict[str, str]): a dictionary with form field id as key and their values as value
+        """
+        # Sanity checks
+        if not name.endswith(".pdf"):
+            raise ValueError("The document name must end with .pdf")
+
+        self.name: str = name
+        self.field_dictionary: Dict[str, str] = field_dictionary
+        self.xfdf = xfdfgen(self.name, self.field_dictionary)
+
+    def write_xfdf(self, output_path: str) -> None:
+        """
+        Writes the xfdf document to a file
+
+        Args:
+            output_path: must be .xfdf extension
+        """
+        # Sanity checks
+        if not output_path.endswith(".xfdf"):
+            raise ValueError(f"Output must end with .xfdf\n{output_path} is not a valid output path.")
+
+        with open(output_path, "a", encoding="UTF-8") as xfdf_file:
+            self.xfdf.writexml(xfdf_file, encoding="UTF-8")
+
+    def pretty_print(self) -> None:
+        """
+        Prints the xfdf document in a human legible format onto the console
+
+        """
+        print(self.xfdf.toprettyxml())
