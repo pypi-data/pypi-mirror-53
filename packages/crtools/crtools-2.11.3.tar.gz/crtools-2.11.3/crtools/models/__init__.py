960cc9d5d159a1ac6b929ca432aa44bf3540fcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:42 +0000

    Add package crtools/2.11.3/crtools-2.11.3.tar.gz

---
+++ packages/crtools/crtools-2.11.3.tar.gz/crtools-2.11.3/crtools/models/__init__.py
@@ -0,0 +1,9 @@
+# import models into model package
+from crtools.models.demerit import Demerit
+from crtools.models.formermember import FormerMember
+from crtools.models.memberevent import MemberEvent
+from crtools.models.vacation import MemberVacation
+from crtools.models.clan import ProcessedClan
+from crtools.models.processedmember import ProcessedMember
+from crtools.models.war import ProcessedCurrentWar
+from crtools.models.warparticipation import WarParticipation
