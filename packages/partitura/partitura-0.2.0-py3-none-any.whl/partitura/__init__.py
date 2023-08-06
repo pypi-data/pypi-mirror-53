952f064bfba8aa2927fb5c8b8649ae3589f65836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:45 +0000

    Add package partitura/0.2.0/partitura-0.2.0-py3-none-any.whl

---
+++ packages/partitura/partitura-0.2.0-py3-none-any.whl/partitura/__init__.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+import pkg_resources
+
+from .importmusicxml import load_musicxml
+from .exportmusicxml import save_musicxml
+from .importmidi import load_midi
+from .display import show
+from . import musicanalysis
+
+# define a version variable
+__version__ = pkg_resources.get_distribution("partitura").version
+
+#: An example MusicXML file for didactic purposes  
+EXAMPLE_MUSICXML = pkg_resources.resource_filename("partitura", 'assets/score_example.musicxml')
+
+__all__ = ['load_musicxml', 'save_musicxml', 'load_midi', 'show', 'EXAMPLE_MUSICXML']
