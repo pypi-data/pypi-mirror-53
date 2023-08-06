56d03bdf9efa1377e15ec66cc133edce72deca03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:32 +0000

    Add package ass-tag-parser/2.2/ass_tag_parser-2.2.tar.gz

---
+++ packages/ass-tag-parser/ass_tag_parser-2.2.tar.gz/ass_tag_parser-2.2/setup.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+setup(
+    author="rr-",
+    author_email="rr-@sakuya.pl",
+    name="ass_tag_parser",
+    version="2.2",
+    description="Parser of ASS tags",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rr-/ass_tag_parser",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+    ],
+    packages=find_packages(),
+    package_dir={"ass_tag_parser": "ass_tag_parser"},
+    install_requires=["dataclasses"],
+)
