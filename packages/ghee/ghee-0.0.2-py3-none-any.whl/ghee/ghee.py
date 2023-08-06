3e81d20c30e5fb66fe5b4b5b54496ba3c636479d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:52 +0000

    Add package ghee/0.0.2/ghee-0.0.2-py3-none-any.whl

---
+++ packages/ghee/ghee-0.0.2-py3-none-any.whl/ghee/ghee.py
@@ -0,0 +1,27 @@
+
+import http.client
+import json
+
+from urllib.parse import urlparse
+
+
+class Ghee:
+    def __init__(self, webhook_url):
+        self.headers = {'Content-type': 'application/json; charset=UTF-8'}
+        self.url = urlparse(webhook_url)
+        self.connection = http.client.HTTPSConnection(self.url.netloc)
+
+    def __call__(self, message):
+        return self.echo(message)
+
+    def echo(self, message):
+        return self._send(message)
+    
+    def _send(self, message):
+        foo = {'text': message}
+        json_data = json.dumps(foo)
+
+        self.connection.request('POST', self.url.geturl(), json_data, self.headers)
+
+        response = self.connection.getresponse()
+        print(response.read().decode())
