c66bd663eb874784aed67f00fc765284f0315795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:03 +0000

    Add package ssmpy/0.1.1/ssmpy-0.1.1-py3-none-any.whl

---
+++ packages/ssmpy/ssmpy-0.1.1-py3-none-any.whl/ssmpy/__init__.py
@@ -0,0 +1,36 @@
+from .semanticbase import create as create_semantic_base
+
+from .annotations import get_uniprot_annotations
+
+from .ssm import (
+    semantic_base,
+    ssm_multiple,
+    get_id,
+    ssm_resnik,
+    ssm_lin,
+    ssm_jiang_conrath,
+    run_query,
+    get_id,
+    get_name,
+    common_ancestors,
+    information_content_intrinsic,
+    information_content_extrinsic,
+)
+
+name = "ssmpy"
+__all__ = [
+    "create_semantic_base",
+    "get_uniprot_annotations",
+    "intrinsic",
+    "mica",
+    "get_id",
+    "ssm_resnik",
+    "ssm_lin",
+    "ssm_jiang_conrath",
+    "run_query",
+    "get_id",
+    "get_name",
+    "common_ancestors",
+    "information_content_intrinsic",
+    "information_content_extrinsic",
+]
