6c00b20da9a7347259531a1ba1c673fca9b32568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:26 +0000

    Add package scylla-client/0.1.1/scylla_client-0.1.1.tar.gz

---
+++ packages/scylla-client/scylla_client-0.1.1.tar.gz/scylla_client-0.1.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['scylla']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['potion-client>=2.5,<3.0', 'requests>=2.22,<3.0', 'sitri>=0.1.1,<0.2.0']
+
+setup_kwargs = {
+    'name': 'scylla-client',
+    'version': '0.1.1',
+    'description': 'Scylla - client for charybdis permission manager',
+    'long_description': None,
+    'author': 'Alexander Lavrov',
+    'author_email': 'egnod@ya.ru',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
