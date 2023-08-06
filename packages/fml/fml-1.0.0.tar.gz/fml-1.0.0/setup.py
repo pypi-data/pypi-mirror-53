ddc7ef4db667f5a6f042fc9b8fb1c1cb189114ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:14 +0000

    Add package fml/1.0.0/fml-1.0.0.tar.gz

---
+++ packages/fml/fml-1.0.0.tar.gz/fml-1.0.0/setup.py
@@ -0,0 +1,91 @@
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+NAME = "fml"
+DESCRIPTION = "A small CLI app to see a joke on your terminal when you are down."
+URL = "https://github.com/slash-arun/fml/"
+EMAIL = "slash-arun@outlook.com"
+AUTHOR = "Arun"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = "1.0.0"
+
+REQUIRED = ["requests", "cowsay"]
+
+EXTRAS = {}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {"__version__": VERSION}
+
+
+class UploadCommand(Command):
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=["fml"],
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    cmdclass={"upload": UploadCommand},
+    entry_points={"console_scripts": ["fml=fml:main"]},
+)
