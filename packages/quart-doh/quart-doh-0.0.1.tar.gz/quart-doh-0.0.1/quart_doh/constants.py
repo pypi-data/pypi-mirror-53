05407be5e3f072123d20ddbe52ba743cd2a61f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:08 +0000

    Add package quart-doh/0.0.1/quart-doh-0.0.1.tar.gz

---
+++ packages/quart-doh/quart-doh-0.0.1.tar.gz/quart-doh-0.0.1/quart_doh/constants.py
@@ -0,0 +1,5 @@
+DOH_CONTENT_TYPE = "application/dns-message"
+DOH_JSON_CONTENT_TYPE = "application/dns-json"
+DOH_DNS_PARAM = "dns"
+DOH_DNS_JSON_PARAM = {"name": "name", "type": "type"}
+AUTHORITY = "quart_doh"
