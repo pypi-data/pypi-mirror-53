fce4d6f892cc9a023a601fde1f28ec0b81c68be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:52 +0000

    Add package zookeeper/0.5.5/zookeeper-0.5.5.tar.gz

---
+++ packages/zookeeper/zookeeper-0.5.5.tar.gz/zookeeper-0.5.5/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(
+    name="zookeeper",
+    version="0.5.5",
+    author="Plumerai",
+    author_email="lukas@plumerai.co.uk",
+    description="A small library for managing deep learning models, hyper parameters and datasets",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/larq/zookeeper",
+    packages=find_packages(),
+    license="Apache 2.0",
+    python_requires=">=3.6",
+    install_requires=[
+        "click>=7.0",
+        "click-completion>=0.5.1",
+        "matplotlib>=3.0.3",
+        "tensorflow-datasets>=1.1.0",
+    ],
+    extras_require={
+        "tensorflow": ["tensorflow>=1.13.1"],
+        "tensorflow_gpu": ["tensorflow-gpu>=1.13.1"],
+        "test": ["pytest>=4.3.1", "pytest-cov>=2.6.1"],
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+    ],
+)
