1edb15faf9c51f08ac670319dc867be725ef3113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:25 +0000

    Add package genetica/0.1/genetica-0.1.tar.gz

---
+++ packages/genetica/genetica-0.1.tar.gz/genetica-0.1/genetica/example.py
@@ -0,0 +1,8 @@
+# from rocket_phenotype import Rocket
+# from model import Genetica 
+
+# model = Genetica(Rocket, 5, 100, lambda x: x.final_params['full_mass'], 20)
+# model.run()
+# print(model.get_best_fit())
+# print(model.get_best_specie().show_params())
+# model.plot()
