1dbc6123ddf1aa9d6f7bb29b91b35fd7b3d3b6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:02 +0000

    Add package conllu/2.1/conllu-2.1-py2.py3-none-any.whl

---
+++ packages/conllu/conllu-2.1-py2.py3-none-any.whl/conllu/__init__.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+
+from conllu.compat import string_to_file
+from conllu.models import TokenList
+from conllu.parser import parse_sentences, parse_token_and_metadata
+
+
+def parse(data, fields=None, field_parsers=None, metadata_parsers=None):
+    return list(parse_incr(
+        string_to_file(data),
+        fields=fields,
+        field_parsers=field_parsers,
+        metadata_parsers=metadata_parsers
+    ))
+
+def parse_incr(in_file, fields=None, field_parsers=None, metadata_parsers=None):
+    for sentence in parse_sentences(in_file):
+        yield TokenList(*parse_token_and_metadata(
+            sentence,
+            fields=fields,
+            field_parsers=field_parsers,
+            metadata_parsers=metadata_parsers
+        ))
+
+def parse_tree(data):
+    return list(parse_tree_incr(string_to_file(data)))
+
+def parse_tree_incr(in_file):
+    for tokenlist in parse_incr(in_file):
+        yield tokenlist.to_tree()
