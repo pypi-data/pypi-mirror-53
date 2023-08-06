16b8dbe1d7e4155a6a2f89ecfe737222b4c888a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:15 +0000

    Add package twipper/0.1.5/twipper-0.1.5.tar.gz

---
+++ packages/twipper/twipper-0.1.5.tar.gz/twipper-0.1.5/twipper/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2018-2019 Alvaro Bartolome
+# See LICENSE for details.
+
+__author__ = 'Alvaro Bartolome @ alvarob96 on GitHub'
+__version__ = '0.1.5'
+
+from twipper.credentials import Twipper
