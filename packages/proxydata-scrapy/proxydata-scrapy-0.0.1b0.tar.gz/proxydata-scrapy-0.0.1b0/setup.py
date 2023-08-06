032c8ca9a9ffc999ff858bac267f63aaabaf0fbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:41 +0000

    Add package proxydata-scrapy/0.0.1b0/proxydata-scrapy-0.0.1b0.tar.gz

---
+++ packages/proxydata-scrapy/proxydata-scrapy-0.0.1b0.tar.gz/proxydata-scrapy-0.0.1b0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="proxydata-scrapy",
+    version="0.0.1b",
+    author="nekidaem",
+    author_email="order@nekidaem.ru",
+    description="Middleware for proxying scrapy requests through the service proxyfordevelopers.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://gitlab.com/mkispb/proxydata-scrapy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
