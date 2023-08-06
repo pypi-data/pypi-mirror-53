a58df010dfebb9c5aaec20ce21cb114b2cd73375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package olr/0.0.8/olr-0.0.8-py3-none-any.whl

---
+++ packages/olr/olr-0.0.8-py3-none-any.whl/olr_package/__init__.py
@@ -0,0 +1,16 @@
+from . olr_function import olr
+from . olr_function import olrmodels
+from . olr_function import olrformulas
+from . olr_function import olrformulasorder
+from . olr_function import adjr2list
+from . olr_function import r2list
+
+import pandas as pd
+import numpy as np
+import statsmodels.formula.api as sm
+
+import itertools
+
+from itertools import combinations
+
+import functools
