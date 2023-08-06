a3cfd2d8956e0ab3b9d53deff2b2085b233e6670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:39 +0000

    Add package pycspro/0.0.7/pycspro-0.0.7-py3-none-any.whl

---
+++ packages/pycspro/pycspro-0.0.7-py3-none-any.whl/pycspro/__init__.py
@@ -0,0 +1,8 @@
+from .DictionaryParser import DictionaryParser.DictionaryParser
+from .CaseParser import CaseParser.CaseParser
+
+name = 'pycspro'
+__all__ = [
+    'DictionaryParser', 
+    'CaseParser'
+]
