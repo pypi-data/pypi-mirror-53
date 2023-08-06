711db0e07914aacc5b88be6df4730652c8a3e7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:31 +0000

    Add package eccw-gui/1.1.1/eccw_gui-1.1.1-py3-none-any.whl

---
+++ packages/eccw-gui/eccw_gui-1.1.1-py3-none-any.whl/eccw_gui/shared/viewers/checkBox.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '../uis/checkBox.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(94, 28)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(Form)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setSpacing(6)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.checkBox = QtWidgets.QCheckBox(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.checkBox.sizePolicy().hasHeightForWidth())
+        self.checkBox.setSizePolicy(sizePolicy)
+        self.checkBox.setMinimumSize(QtCore.QSize(0, 28))
+        self.checkBox.setMaximumSize(QtCore.QSize(16777215, 28))
+        self.checkBox.setBaseSize(QtCore.QSize(0, 31))
+        self.checkBox.setAutoExclusive(False)
+        self.checkBox.setObjectName("checkBox")
+        self.horizontalLayout.addWidget(self.checkBox)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.checkBox.setToolTip(_translate("Form", "Activate ruin mecanism drawing"))
+        self.checkBox.setText(_translate("Form", "&label"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
