30860018fd515be00c36b3e9a80d3ee1230b97cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:20 +0000

    Add package gym-push/0.0.11/gym-push-0.0.11.tar.gz

---
+++ packages/gym-push/gym-push-0.0.11.tar.gz/gym-push-0.0.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='gym-push',
+      packages=find_packages(),
+      package_data={
+        '': ['*.csv', '*.npy'],
+      },      
+      version='0.0.11',
+      install_requires=['gym', 'numpy', 'pandas', 'joblib', 'eel', 'json-tricks'], 
+      author='Kieran Fraser',
+      author_email='kfraser@tcd.ie',
+      download_url='https://github.com/kieranfraser/gym-push/raw/master/dist/gym-push-0.0.10.tar.gz'
+) 
