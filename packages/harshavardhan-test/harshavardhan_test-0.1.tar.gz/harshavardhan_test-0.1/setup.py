be727f4cea32e1540d07e610df62df007d4d4f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:09 +0000

    Add package harshavardhan-test/0.1/harshavardhan_test-0.1.tar.gz

---
+++ packages/harshavardhan-test/harshavardhan_test-0.1.tar.gz/harshavardhan_test-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+import os
+
+setup(
+    name="harshavardhan_test",
+    version="0.1",
+    packages=find_packages(),
+    author="Me",
+    author_email="harshavardhanm03@gmail.com",
+    description="This is an Example Package",
+    keywords="hello world example examples",
+    license='MIT',
+    url="https://github.com/harshavardhanm03/python_package"
+)
