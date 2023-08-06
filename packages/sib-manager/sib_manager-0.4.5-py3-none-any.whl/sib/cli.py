ced0c49eb6977a8ce97dbd5179e453e210f4ba70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:30 +0000

    Add package sib-manager/0.4.5/sib_manager-0.4.5-py3-none-any.whl

---
+++ packages/sib-manager/sib_manager-0.4.5-py3-none-any.whl/sib/cli.py
@@ -0,0 +1,90 @@
+import click
+import os
+import sys
+import logging
+from urllib.parse import urlparse
+from . import __version__
+from .project import Project
+from .package import Package
+
+
+logger = logging.getLogger(__name__)
+
+
+# click entrypoint
+@click.group()
+@click.option('-v', '--verbose', count=True, help='Set verbosity level.')
+@click.version_option(__version__)
+def main(verbose):
+
+    """Startin'Blox installer"""
+
+    # set log level
+    if verbose == 2:
+        logging.basicConfig(level='DEBUG', format='%(levelname)s:%(module)s:%(message)s')
+    elif verbose == 1:
+        logging.basicConfig(level='INFO', format='[%(levelname)s]: %(message)s')
+    else:
+        logging.basicConfig(level='WARNING', format='[%(levelname)s]: %(message)s')
+
+
+@main.command()
+@click.argument('name', nargs=1)
+@click.argument('directory', required=False)
+@click.option('--production', is_flag=True, default=False, help='Use a production template')
+def startproject(name, production, directory):
+
+    """Start a new startin'blox project"""
+
+    # set absolute path to project directory
+    if directory:
+        directory = os.path.abspath(directory)
+    else:
+        # set a directory from project name in pwd
+        directory = os.path.join(os.getcwd(), name)
+
+    project = Project(name, directory)
+    project.create(production)
+
+
+@main.command()
+@click.argument('name', nargs=1)
+@click.argument('directory', required=False)
+def install(name, directory):
+
+    """Initialize a startin'blox project"""
+
+    # set absolute path to project directory
+    if directory:
+        directory = os.path.abspath(directory)
+    else:
+        # get path from current dir
+        directory = os.getcwd()
+
+    project = Project(name, directory)
+    project.install()
+    project.load()
+
+
+@main.command()
+@click.argument('name', nargs=1)
+@click.argument('directory', required=False)
+def startpackage(name, directory):
+
+    try:
+
+        """Create a new startin'blox package"""
+
+        # set absolute path to package directory (given directory is the project path)
+        if directory:
+            directory = os.path.join(os.path.abspath(directory), name)
+        else:
+            # get path from current dir
+            directory = os.path.join(os.getcwd(), name)
+
+        package = Package(name, directory)
+        package.create()
+
+    except:
+        sys.exit(1)
+
