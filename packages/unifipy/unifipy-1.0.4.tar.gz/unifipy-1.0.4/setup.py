6dfbd184125167c48d22336ac44f4f30510e34e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:13 +0000

    Add package unifipy/1.0.4/unifipy-1.0.4.tar.gz

---
+++ packages/unifipy/unifipy-1.0.4.tar.gz/unifipy-1.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='unifipy',
+    version='1.0.4',
+    packages=find_packages(),
+    url='https://github.com/fronbasal/unifipy',
+    license='MIT License',
+    author='Daniel Malik',
+    author_email='mail@fronbasal.de',
+    description='Unifi Python API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/unifipy/unifipy-1.0.4.tar.gz/unifipy-1.0.4/unifipy/__init__.py
@@ -0,0 +1,4 @@
+from .unifipy import Unifi
+
+__version__ = "1.0.4"
+__name__ = "unifipy"
