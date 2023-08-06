caf676e2666d0cde4d477f2345c3bebbb288dabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:16 +0000

    Add package pvoutput/0.0.3/pvoutput-0.0.3.tar.gz

---
+++ packages/pvoutput/pvoutput-0.0.3.tar.gz/pvoutput-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pvoutput",
+    version="0.0.3",
+    author="James Hodgkinson",
+    author_email="yaleman@ricetek.net",
+    description="PVOutput.org API interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yaleman/pvoutput",
+    packages=setuptools.find_packages(),
+    install_requires=["requests"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
