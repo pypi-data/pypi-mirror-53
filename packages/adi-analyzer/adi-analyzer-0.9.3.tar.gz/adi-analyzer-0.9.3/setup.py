54ee6927c5b2a7309e21396adb59985f23dfca84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:41 +0000

    Add package adi-analyzer/0.9.3/adi-analyzer-0.9.3.tar.gz

---
+++ packages/adi-analyzer/adi-analyzer-0.9.3.tar.gz/adi-analyzer-0.9.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/10/12 5:41 下午
+# @Author  : kewen
+# @File    : setup.py.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='adi-analyzer',
+    version='0.9.3',
+    author='zkwlx',
+    author_email='zkwlixue@163.com',
+    url='https://github.com/zkwlx/ADI',
+    description='A powerful tool to help Android engineers optimize their performance',
+    packages=find_packages(),
+    install_requires=['bokeh'],
+    entry_points={
+        'console_scripts': [
+            'adi-analyzer=main.analyzer:main'
+        ]
+    },
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
