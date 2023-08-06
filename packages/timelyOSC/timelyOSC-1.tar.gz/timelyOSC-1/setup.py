926642d64424d9cbc0c8291539de1a4157fb27f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:08 +0000

    Add package timelyOSC/1/timelyOSC-1.tar.gz

---
+++ packages/timelyOSC/timelyOSC-1.tar.gz/timelyOSC-1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name = 'timelyOSC',
+        version = '1',
+        install_requires = [],
+        packages = setuptools.find_packages(),
+        py_modules = ['timelyOSC'],
+        scripts = [])
