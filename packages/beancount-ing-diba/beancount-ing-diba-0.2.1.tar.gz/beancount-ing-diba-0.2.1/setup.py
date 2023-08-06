01cb9c43a2fdbd652a4794aa073bd4043dcbddad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:46 +0000

    Add package beancount-ing-diba/0.2.1/beancount-ing-diba-0.2.1.tar.gz

---
+++ packages/beancount-ing-diba/beancount-ing-diba-0.2.1.tar.gz/beancount-ing-diba-0.2.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['beancount_ing_diba']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'beancount-ing-diba',
+    'version': '0.2.1',
+    'description': 'Beancount Importer for ING-DiBa (DE) CSV exports',
+    'long_description': None,
+    'author': 'Siddhant Goel',
+    'author_email': 'me@sgoel.org',
+    'url': 'https://github.com/siddhantgoel/beancount-ing-diba',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
