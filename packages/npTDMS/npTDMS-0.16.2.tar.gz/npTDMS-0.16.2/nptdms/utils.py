79bdc188be8a643a13777ab629467e010ac89f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:52 +0000

    Add package npTDMS/0.16.2/npTDMS-0.16.2.tar.gz

---
+++ packages/npTDMS/npTDMS-0.16.2.tar.gz/npTDMS-0.16.2/nptdms/utils.py
@@ -0,0 +1,25 @@
+import time
+
+
+class Timer(object):
+    def __init__(self, log, description):
+        self._log = log
+        self._description = description
+
+    def __enter__(self):
+        try:
+            self._start_time = time.perf_counter()
+        except AttributeError:
+            # Python < 3.3
+            self._start_time = time.clock()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            end_time = time.perf_counter()
+        except AttributeError:
+            # Python < 3.3
+            end_time = time.clock()
+
+        elapsed_time = (end_time - self._start_time) * 1.0e3
+        self._log.info("{0}: Took {1} ms".format(
+            self._description, elapsed_time))
