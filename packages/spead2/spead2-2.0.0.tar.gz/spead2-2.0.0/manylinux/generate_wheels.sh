5cf1e2e4d8576cc6fd21d4a8fe785150149898e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:56 +0000

    Add package spead2/2.0.0/spead2-2.0.0.tar.gz

---
+++ packages/spead2/spead2-2.0.0.tar.gz/spead2-2.0.0/manylinux/generate_wheels.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+cd /tmp/spead2
+mkdir -p /output
+for d in /opt/python/cp{35,36,37}*; do
+    git clean -xdf
+    PATH=$d/bin:$PATH ./bootstrap.sh
+    $d/bin/python ./setup.py bdist_wheel -d .
+    auditwheel repair --plat manylinux2010_x86_64 -w /output spead2-*-`basename $d`-linux_*.whl
+done
