a98440aa4e76f93c29aa0029514c1f7651ff7db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:55 +0000

    Add package deal/3.0.0/deal-3.0.0.tar.gz

---
+++ packages/deal/deal-3.0.0.tar.gz/deal-3.0.0/deal/__init__.py
@@ -0,0 +1,30 @@
+"""
+**Deal** -- python library for design by contract (DbC) programming.
+https://en.wikipedia.org/wiki/Design_by_contract
+
+This library contain 3 main conception from DbC:
+
+* Precondition -- condition that must be true before function is executed.
+* Postcondition -- condition that must be true after function executed.
+* Invariant -- condition that can be relied upon to be true during execution
+  of a program. In this library invariant condition check in 3 cases:
+    1. Before class method execution.
+    2. After class method execution.
+    3. After some class attribute setting.
+"""
+
+# main package info
+__title__ = 'deal'
+__version__ = '3.0.0'
+__author__ = 'Gram Orsinium'
+__license__ = 'MIT'
+
+
+# version synonym
+VERSION = __version__
+
+
+from .aliases import *              # noQA
+from .exceptions import *           # noQA
+from .schemes import Scheme         # noQA
+from .state import reset, switch    # noQA
