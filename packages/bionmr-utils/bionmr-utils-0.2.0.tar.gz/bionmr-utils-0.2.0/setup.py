a3ffeacb6af3f59777e88fdf570d56e7586973f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:56 +0000

    Add package bionmr-utils/0.2.0/bionmr-utils-0.2.0.tar.gz

---
+++ packages/bionmr-utils/bionmr-utils-0.2.0.tar.gz/bionmr-utils-0.2.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bionmr-utils",
+    version="0.2.0",
+    author="Sergei Izmailov",
+    author_email="sergei.a.izmailov@gmail.com",
+    description="Aggregate package of internal bionmr utils",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/sizmailov/bionmr-utils",
+    include_package_data = True,
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ),
+    install_requires=[
+        "pyxmolpp2>=0.10.1,<1.0",
+        "pyxmolpp2-stubs",
+        "tqdm",
+        "pandas",
+    ],
+    package_data = {
+        "bionmr_utils": [ 'data/rename_tables/*.csv' ]
+    }
+)
