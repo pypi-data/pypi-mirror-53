c3edc873b70841dd32a066f6674ba89724649678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:10 +0000

    Add package numba-scipy/0.2.0/numba-scipy-0.2.0.tar.gz

---
+++ packages/numba-scipy/numba-scipy-0.2.0.tar.gz/numba-scipy-0.2.0/README.rst
@@ -0,0 +1,22 @@
+***********
+numba-scipy
+***********
+
+.. image:: https://badges.gitter.im/numba/numba.svg
+   :target: https://gitter.im/numba/numba?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge
+   :alt: Gitter
+
+Numba + SciPy = numba-scipy
+###########################
+
+``numba-scipy`` extends Numba to make it aware of SciPy. Numba is an open
+source, NumPy-aware optimizing compiler for Python sponsored by Anaconda, Inc.
+It uses the LLVM compiler project to generate machine code from Python syntax.
+
+For more information about Numba, see the Numba homepage:
+http://numba.pydata.org
+
+Dependencies
+============
+
+* numba
---
+++ packages/numba-scipy/numba-scipy-0.2.0.tar.gz/numba-scipy-0.2.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import versioneer
+
+
+_install_requires = ['scipy>=0.16', 'numba>=0.45']
+
+
+metadata = dict(
+    name='numba-scipy',
+    description="numba-scipy extends Numba to make it aware of SciPy",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Compilers",
+    ],
+    package_data={},
+    scripts=[],
+    author="Anaconda, Inc.",
+    author_email="numba-users@continuum.io",
+    url="https://github.com/numba/numba-scipy",
+    download_url="https://github.com/numba/numba-scipy",
+    packages=find_packages(),
+    setup_requires=[],
+    install_requires=_install_requires,
+    entry_points={
+        "numba_extensions": [
+            "init = numba_scipy:_init_extension",
+        ],
+    },
+    license="BSD",
+    zip_safe=False,
+)
+
+
+with open('README.rst') as f:
+    metadata['long_description'] = f.read()
+
+
+setup(**metadata)
