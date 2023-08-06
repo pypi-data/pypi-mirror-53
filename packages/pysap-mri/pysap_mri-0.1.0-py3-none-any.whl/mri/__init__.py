015c1e8a4a025832f0f8e2dc5af4872e70982b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:32 +0000

    Add package pysap-mri/0.1.0/pysap_mri-0.1.0-py3-none-any.whl

---
+++ packages/pysap-mri/pysap_mri-0.1.0-py3-none-any.whl/mri/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##########################################################################
+# pySAP - Copyright (C) CEA, 2017 - 2018
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
+"""
+This module defines all the MRI related plugins.
+"""
+version_info = (0, 1, 0)
+__version__ = '.'.join(str(c) for c in version_info)
