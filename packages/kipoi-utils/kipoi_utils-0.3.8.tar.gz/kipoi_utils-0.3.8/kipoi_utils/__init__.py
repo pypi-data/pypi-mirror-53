8d90d87dcfdd1d94201d39927e20d551797f06f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:56 +0000

    Add package kipoi-utils/0.3.8/kipoi_utils-0.3.8.tar.gz

---
+++ packages/kipoi-utils/kipoi_utils-0.3.8.tar.gz/kipoi_utils-0.3.8/kipoi_utils/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+__author__ = 'Kipoi team'
+__email__ = 'thorsten.beier@embl.de'
+
+from ._version import __version__
+
+
+# available modules
+from . import external
+
+
+from . utils import _get_arg_name_values
+from . utils import *
